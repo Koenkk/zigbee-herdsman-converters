a1978e0e77d02a73bd748ec7188d2d29d4cc9569
Koen Kanters <koenkanters94@gmail.com>
2018-07-14 20:11:51 +0200

    Lint

--- devices.js
+++ devices.js
@@ -877,7 +877,7 @@ const devices = [
         fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
         toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
     },
-    
+
     // SmartThings
     {
         zigbeeModel: ['PGC313'],
