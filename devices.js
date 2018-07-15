24409f21aeb52bf241965fe7284f79885d45c094
okueng <olivier.kueng@gmail.com>
2018-07-15 17:43:31 +0200

    added support for hue LCT001, LCT007
    See the different model ID at : https://developers.meethue.com/documentation/supported-lights

--- devices.js
+++ devices.js
@@ -461,6 +461,15 @@ const devices = [
         toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
     },
     {
+        zigbeeModel: ['LCT001', 'LCT007'],
+        model: '9290011420',
+        vendor: 'Philips',
+        description: 'Hue white and color ambiance E27',
+        supports: generic.light_onoff_brightness_colortemp_colorxy().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
+    },
+    {
         zigbeeModel: ['LCT010', 'LCT015'],
         model: '9290012573A',
         vendor: 'Philips',
