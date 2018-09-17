91e2c8d9984509fb7b96a151f3a067e81d74c4dc
kimonm <40135180+kimonm@users.noreply.github.com>
2018-09-16 23:22:02 -0700

    Added power reporting for Iris 3210-L plug (#71)
    * Add power reporting to Iris 3210-L
    
    Added reporting of power for Iris 3210-L
    
    * Add power reporting to Iris 3210-L
    
    Added power reporting to Iris 3210-L plug

--- devices.js
+++ devices.js
@@ -1150,8 +1150,16 @@ const devices = [
         vendor: 'Iris',
         description: 'Smart plug',
         supports: 'on/off',
-        fromZigbee: [fz.ignore_onoff_change, fz.generic_state],
+        fromZigbee: [fz.ignore_onoff_change, fz.ignore_electrical_change, fz.generic_state, fz.iris_3210L_power],
         toZigbee: [tz.onoff],
+        configure: (ieeeAddr, shepherd, coordinator, callback) => {
+            const device = shepherd.find(ieeeAddr, 1);                                                                            
+            const actions = [                                                                                                     
+                (cb) => device.report('haElectricalMeasurement', 'activePower', 10, 1000, 1, cb),                                 
+            ];
+            
+            execute(device, actions, callback);                                                                                   
+        },
     },
 ];
 
