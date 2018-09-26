5720a7d32b5c3792b6c323629e8fc3d8b2eaa2e1
arteck <6681528+arteck@users.noreply.github.com>
2018-09-26 14:02:53 +0200

    LIGHTIFY Indoor Flex RGBW

--- devices.js
+++ devices.js
@@ -755,7 +755,7 @@ const devices = [
         },
     },
     {
-        zigbeeModel: ['Flex RGBW'],
+        zigbeeModel: ['Flex RGBW', 'LIGHTIFY Indoor Flex RGBW'],
         model: '4052899926110',
         vendor: 'OSRAM',
         description: 'Flex RGBW',
