a804fa675cc47dfcdb0f8c9f005adfd8b0a5646f
Koenkk <koenkanters94@gmail.com>
2018-09-29 21:24:41 +0200

    Support AB35996. https://github.com/Koenkk/zigbee-shepherd-converters/issues/88

--- devices.js
+++ devices.js
@@ -844,6 +844,15 @@ const devices = [
         fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
         toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
     },
+    {
+        zigbeeModel: ['PAR 16 50 RGBW - LIGHTIFY'],
+        model: 'AB35996',
+        vendor: 'OSRAM',
+        description: 'Smart+ Spot GU10 Multicolor',
+        supports: generic.light_onoff_brightness_colortemp_colorxy().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
+    },
 
 
     // Hive
