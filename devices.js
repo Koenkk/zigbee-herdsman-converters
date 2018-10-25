049780aa7dbcd05f8d2ae581179403553bb7caab
arteck <6681528+arteck@users.noreply.github.com>
2018-10-25 19:54:59 +0200

    Remove SJCGQ11LM water leak basic (always reports true) (#109)
    * Added device OSRAM Surface Light TW
    
    * LIGHTIFY Indoor Flex RGBW
    
    * Lint
    
    * Gardenpole RGBW-Lightify
    
    * Gardenpole RGBW-Lightify Stripe
    
    * Update devices.js
    
    * hue LTW012
    
    * water_leak -> water_leak_rep
    
    * Update fromZigbee.js
    
    * Update devices.js
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -252,10 +252,7 @@ const devices = [
         vendor: 'Xiaomi',
         description: 'Aqara water leak sensor',
         supports: 'water leak true/false',
-        fromZigbee: [
-            fz.xiaomi_battery_3v, fz.SJCGQ11LM_water_leak_basic, fz.SJCGQ11LM_water_leak_iaszone,
-            fz.ignore_basic_change,
-        ],
+        fromZigbee: [fz.xiaomi_battery_3v, fz.SJCGQ11LM_water_leak_iaszone, fz.ignore_basic_change],
         toZigbee: [],
     },
     {
