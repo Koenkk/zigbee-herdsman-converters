67cbb597e6919220d3bcad76269287f85024f7d8
Ryan Holt <ryan@ryanholt.net>
2018-11-15 22:58:31 -0500

    Update devices.js

--- devices.js
+++ devices.js
@@ -416,6 +416,15 @@ const devices = [
         toZigbee: generic.light_onoff_brightness().toZigbee,
     },
     {
+        zigbeeModel: ['TRADFRI bulb E12 W op/ch 400lm'],
+        model: 'LED1649C5',
+        vendor: 'IKEA',
+        description: 'TRADFRI LED bulb E12/E14 400 lumen, dimmable, white spectrum, opal white',
+        supports: generic.light_onoff_brightness_colortemp().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp().toZigbee,
+    },
+    {
         zigbeeModel: ['TRADFRI wireless dimmer'],
         model: 'ICTC-G-1',
         vendor: 'IKEA',
