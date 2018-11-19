8d68e18102bc4bab0d328be9a0f76c18cb3dffef
arteck <6681528+arteck@users.noreply.github.com>
2018-11-19 19:49:22 +0100

    FLS-PP3 (#137)
    * FLS-PP3
    
    * Update devices.js
    
    * Update devices.js
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -1473,6 +1473,17 @@ const devices = [
         toZigbee: generic.light_onoff_brightness_colortemp().toZigbee,
     },
 
+    // Dresden Elektronik
+    {
+        zigbeeModel: ['FLS-PP3 '],
+        model: 'Mega23M12',
+        vendor: 'Dresden Elektronik',
+        description: 'ZigBee Light Link wireless electronic ballast',
+        supports: generic.light_onoff_brightness_colortemp_colorxy().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
+    },
+
     // Centralite Swiss Plug
     {
         zigbeeModel: ['4256251-RZHAC'],
