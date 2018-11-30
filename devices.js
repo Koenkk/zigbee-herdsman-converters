130a451aa384e46643a232cf35d7c145bb7cd541
brubaked <37672083+brubaked@users.noreply.github.com>
2018-11-30 11:10:16 -0500

    Add support for sengled Element Plus (A19) (#149)
    * Add support for sengled Element Plus (A19)
    
    All features of the bulb work using the "generic.light_onoff_brightness_colortemp" converter. More info about the bulb can be found here - https://www.amazon.com/Sengled-2700-6500K-Equivalent-Assistant-SmartThings/dp/B01MQVYNFL
    
    * Lint

--- devices.js
+++ devices.js
@@ -1282,6 +1282,15 @@ const devices = [
         toZigbee: generic.light_onoff_brightness().toZigbee,
     },
     {
+        zigbeeModel: ['Z01-A19NAE26'],
+        model: 'Z01-A19NAE26',
+        vendor: 'Sengled',
+        description: 'Element Plus (A19)',
+        supports: generic.light_onoff_brightness_colortemp().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp().toZigbee,
+    },
+    {
         zigbeeModel: ['E11-N1EA'],
         model: 'E11-N1EA',
         vendor: 'Sengled',
