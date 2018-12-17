e87e9334022f70f4db4571a5e5f17ff93ee387a6
boojew <boojew@gmail.com>
2018-12-16 23:12:56 -0500

    Add support for EcoSmart A19 RGB Bulb

--- devices.js
+++ devices.js
@@ -1789,6 +1789,15 @@ const devices = [
         fromZigbee: generic.light_onoff_brightness().fromZigbee,
         toZigbee: generic.light_onoff_brightness().toZigbee,
     },
+    {                                                
+        zigbeeModel: ['zhaRGBW'],                                                       
+        model: 'D1821',                                                                                      
+        vendor: 'ecosmart',
+        description: 'EcoSmart RGBW',
+        supports: 'On,off and colour',               
+        fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
+    },  
 ];
 
 module.exports = devices;
