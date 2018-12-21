8eb51cb544b982293d13f38a08c377d907b57c6a
Koen Kanters <koenkanters94@gmail.com>
2018-12-21 19:31:55 +0100

    Export converters. https://github.com/Koenkk/zigbee2mqtt/issues/15

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const devices = require('./devices');
+const toZigbee = require('./converters/toZigbee');
+const fromZigbee = require('./converters/fromZigbee');
 
 module.exports = {
     devices: devices,
     findByZigbeeModel: (model) => devices.find((d) => d.zigbeeModel.includes(model)),
+    toZigbeeConverters: toZigbee,
+    fromZigbeeConverters: fromZigbee,
 };
