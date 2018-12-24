b630ee2ab4ef102a8caf028202fd6f951b735731
qm3ster <qm3ster@gmail.com>
2018-12-25 01:57:01 +0200

    Dangling coma for eslint

--- devices.js
+++ devices.js
@@ -334,7 +334,7 @@ const devices = [
             fz.JTQJBF01LMBW_gas,
             fz.JTQJBF01LMBW_sensitivity,
             fz.JTQJBF01LMBW_gas_density,
-            fz.ignore_basic_change
+            fz.ignore_basic_change,
         ],
         toZigbee: [tz.JTQJBF01LMBW_sensitivity, tz.JTQJBF01LMBW_selfest],
     },
