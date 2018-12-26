dbc153ae4bf07e8a50322df298d83200b2f1d68b
Koen Kanters <koenkanters94@gmail.com>
2018-12-26 13:00:29 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1484,7 +1484,7 @@ const devices = [
         zigbeeModel: ['PGC313'],
         model: 'STSS-MULT-001',
         vendor: 'SmartThings',
-        description: 'SmartSense multi sensor',
+        description: 'Multipurpose sensor',
         supports: 'contact',
         fromZigbee: [fz.smartthings_contact],
         toZigbee: [],
@@ -1493,7 +1493,7 @@ const devices = [
         zigbeeModel: ['tagv4'],
         model: 'STS-PRS-251',
         vendor: 'SmartThings',
-        description: 'SmartThings arrival sensor',
+        description: 'Arrival sensor',
         supports: 'presence',
         fromZigbee: [fz.STS_PRS_251_presence, fz.STS_PRS_251_battery, fz.ignore_power_change, fz.STS_PRS_251_beeping],
         toZigbee: [tz.STS_PRS_251_beep],
@@ -1504,6 +1504,30 @@ const devices = [
                 (cb) => device.report('genPowerCfg', 'batteryVoltage', 1800, 3600),
             ];
 
+            execute(device, actions, callback);
+        },
+    },
+    {
+        zigbeeModel: ['3325-S'],
+        model: '3325-S',
+        vendor: 'SmartThings',
+        description: 'Motion sensor (2015 model)',
+        supports: 'occupancy and temperature',
+        fromZigbee: [
+            fz.generic_temperature, fz.ignore_temperature_change, fz.ias_zone_motion_dev_change,
+            fz.ias_zone_motion_status_change,
+        ],
+        toZigbee: [],
+        configure: (ieeeAddr, shepherd, coordinator, callback) => {
+            const device = shepherd.find(ieeeAddr, 1);
+            const actions = [
+                (cb) => device.write('ssIasZone', 'iasCieAddr', coordinator.device.getIeeeAddr(), cb),
+                (cb) => device.functional('ssIasZone', 'enrollRsp', { enrollrspcode: 0, zoneid: 23 }, cb),
+                (cb) => device.bind('msTemperatureMeasurement', coordinator, cb),
+                (cb) => device.report('msTemperatureMeasurement', 'measuredValue', 30, 600, 1, cb),
+                (cb) => device.bind('genPowerCfg', coordinator, cb),
+                (cb) => device.report('genPowerCfg', 'batteryPercentageRemaining', 0, 1000, 0, cb),
+            ];
             execute(device, actions, callback);
         },
     },
@@ -1712,7 +1736,7 @@ const devices = [
         toZigbee: [tz.on_off],
         configure: (ieeeAddr, shepherd, coordinator, callback) => {
             const device = shepherd.find(ieeeAddr, 1);
-            const cfg = { direction: 0, attrId: 0, dataType: 16, minRepIntval: 0, maxRepIntval: 1000, repChange: 0 };
+            const cfg = {direction: 0, attrId: 0, dataType: 16, minRepIntval: 0, maxRepIntval: 1000, repChange: 0};
             const actions = [
                 (cb) => device.bind('genOnOff', coordinator, cb),
                 (cb) => device.foundation('genOnOff', 'configReport', [cfg], foundationCfg, cb),
@@ -1721,32 +1745,6 @@ const devices = [
                 (cb) => device.report('haElectricalMeasurement', 'activePower', 10, 1000, 1, cb),
             ];
 
-            execute(device, actions, callback);
-        },
-    },
-
-    // Centralite Motion & Temperature sensor
-    {
-        zigbeeModel: ['3325-S'],
-        model: '3325-S',
-        vendor: 'SmartThings',
-        description: 'Motion sensor (2015 model)',
-        supports: 'occupancy and temperature',
-        fromZigbee: [
-            fz.generic_temperature, fz.ignore_temperature_change, fz.ias_zone_motion_dev_change,
-            fz.ias_zone_motion_status_change,
-        ],
-        toZigbee: [],
-        configure: (ieeeAddr, shepherd, coordinator, callback) => {
-            const device = shepherd.find(ieeeAddr, 1);
-            const actions = [
-                (cb) => device.write('ssIasZone', 'iasCieAddr', coordinator.device.getIeeeAddr(), cb),
-                (cb) => device.functional('ssIasZone', 'enrollRsp', { enrollrspcode: 0, zoneid: 23 }, cb),
-                (cb) => device.bind('msTemperatureMeasurement', coordinator, cb),
-                (cb) => device.report('msTemperatureMeasurement', 'measuredValue', 30, 600, 1, cb),
-                (cb) => device.bind('genPowerCfg', coordinator, cb),
-                (cb) => device.report('genPowerCfg', 'batteryPercentageRemaining', 0, 1000, 0, cb),
-            ];
             execute(device, actions, callback);
         },
     },
