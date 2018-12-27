b56bc152390c6161f77e4e4f1d0951405f6c70c4
arteck <6681528+arteck@users.noreply.github.com>
2018-12-27 08:51:44 +0100

    GL-D-003Z

--- devices.js
+++ devices.js
@@ -1488,6 +1488,25 @@ const devices = [
             }
         },
     },
+    
+    {
+        zigbeeModel: ['GL-D-003Z'], 
+        model: 'GL-D-003Z',
+        vendor: 'Gledopto',
+        description: 'Zigbee LED controller RGB + CCT / RGBW / WWCW / Dimmer',
+        supports: generic.light_onoff_brightness_colortemp_colorxy().supports,
+        fromZigbee: generic.light_onoff_brightness_colortemp_colorxy().fromZigbee,
+        toZigbee: generic.light_onoff_brightness_colortemp_colorxy().toZigbee,
+        ep: (device) => {
+            if (device.epList.toString() === '11,12,13') {
+                return {'': 12};
+            } else if (device.epList.toString() === '10,11,13') {
+                return {'': 11};
+            } else {
+                return {};
+            }
+        },
+    },
 
     // SmartThings
     {
