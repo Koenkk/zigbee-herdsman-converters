7241648a61390929398fcacd034330a34779dc44
Koen Kanters <koenkanters94@gmail.com>
2018-12-27 21:04:26 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -356,7 +356,7 @@ const devices = [
     {
         zigbeeModel: ['lumi.curtain'],
         model: 'ZNCLDJ11LM',
-        description: 'Aqara smart curtain',
+        description: 'Aqara curtain motor',
         supports: 'open, close, stop, position',
         vendor: 'Xiaomi',
         fromZigbee: [
