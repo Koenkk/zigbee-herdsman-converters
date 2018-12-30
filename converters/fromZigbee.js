ec0f2689368135e8c742ffeb9765cf8d4eb9443b
simonrath <44604276+simonrath@users.noreply.github.com>
2018-12-30 14:28:30 +0100

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -106,10 +106,10 @@ const holdUpdateBrightness324131092621 = (deviceID) => {
 
 const converters = {
     bitron_power: {
-	cid: 'seMetering',
-	type: 'attReport',
-	convert: (model, msg, publish, options) => {
-	    return {power: parseFloat(msg.data.data['instantaneousDemand']) / 10.0};
+        cid: 'seMetering',
+        type: 'attReport',
+        convert: (model, msg, publish, options) => {
+            return {power: parseFloat(msg.data.data['instantaneousDemand']) / 10.0};
 	},
     },
     bitron_occupancy: {
