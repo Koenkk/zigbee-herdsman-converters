96eced724a0521cda3f73441e25983542ffe244b
Koen Kanters <koenkanters94@gmail.com>
2019-01-01 21:15:23 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1501,7 +1501,7 @@ const devices = [
 
     // Gledopto
     {
-        zigbeeModel: ['GLEDOPTO', 'GL-C-008', 'GL-C-007'], 
+        zigbeeModel: ['GLEDOPTO', 'GL-C-008', 'GL-C-007'],
         model: 'GL-C-008',
         vendor: 'Gledopto',
         description: 'Zigbee LED controller RGB + CCT / RGBW / WWCW / Dimmer',
