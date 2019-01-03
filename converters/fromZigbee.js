79ee093b407069bbe62017ff07bec6f399a6d0da
Joscha Middendorf <Joscha.middendorf@gmail.com>
2019-01-03 08:22:32 +0100

    Requested PR for "Incorrect Battery Readings #568" (#191)
    * Requested PR for "Incorrect Battery Readings #568" 
    
    This is my "Second attempt" first try on Java Script Please have a look on the xiaomi_battery_3v block beginning in line 168 and the function toPercentageCR2032 beginning in line 43.
    I'd love if this works. ;-)
    
    * Update fromZigbee.js
    
    * Update fromZigbee.js
    
    * Update fromZigbee.js
    
    * Update fromZigbee.js
    
    * Update fromZigbee.js
    
    changed "Percentage" to "percentage"
    
    * Use Math.round instead of toFixed

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -40,6 +40,26 @@ const toPercentage = (value, min, max) => {
     return (normalised * 100).toFixed(2);
 };
 
+const toPercentageCR2032 = (voltage) => {
+    let percentage = null;
+
+    if (voltage < 2100) {
+        percentage = 0;
+    } else if (voltage < 2440) {
+        percentage = 6 - ((2440 - voltage) * 6) / 340;
+    } else if (voltage < 2740) {
+        percentage = 18 - ((2740 - voltage) * 12) / 300;
+    } else if (voltage < 2900) {
+        percentage = 42 - ((2900 - voltage) * 24) / 160;
+    } else if (voltage < 3000) {
+        percentage = 100 - ((3000 - voltage) * 58) / 100;
+    } else if (voltage >= 3000) {
+        percentage = 100;
+    }
+
+    return Math.round(percentage);
+};
+
 const numberWithinRange = (number, min, max) => {
     if (number > max) {
         return max;
@@ -159,7 +179,7 @@ const converters = {
 
             if (voltage) {
                 return {
-                    battery: parseFloat(toPercentage(voltage, 2700, 3000)),
+                    battery: parseFloat(toPercentageCR2032(voltage)),
                     voltage: voltage,
                 };
             }
