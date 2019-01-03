b43756e9fd5d04abb49652a3b0f4ba4ad922daf1
allofmex <allofmex@web.de>
2019-01-03 14:37:33 +0100

    Occupancy pirOtoUdelay (#185)
    * Fix occupancy implementation for Hue SML001
    
    Old implementation fails if there is continous motion because timer
    interupts.
    Hue motion sensor sends both messages (occupancy true and false), so we
    don't need the publish timer.
    
    * occupancy_pirOToUDelay implemented (for hue motion sensor)
    
    * Travis CI errors fixed
    
    * More Travis fixes
    
    * Hopefully the last indent fix...
    
    * Indents fixed
    
    * Reverted changes in bind
    
    Its not needed anymore since not timeout used for sml001
    
    * Renamed keys to occupancy_timeout
    
    Lint fixes
    
    * Lint fix
    
    * default endpoint for sml001

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -55,6 +55,42 @@ const converters = {
             }
         },
     },
+    generic_occupancy_timeout: {
+        // set delay after motion detector changes from occupied to unoccupied
+        key: ['occupancy_timeout'],
+        convert: (key, value, message, type) => {
+            const cid = 'msOccupancySensing'; // 1030
+            const attrId = zclId.attr(cid, 'pirOToUDelay').value; // = 16
+
+            if (type === 'set') {
+                return {
+                    cid: cid,
+                    cmd: 'write',
+                    cmdType: 'foundation',
+                    zclData: [{
+                        attrId: attrId,
+                        dataType: 33, // uint16
+                        // hue_sml001:
+                        // in seconds, minimum 10 seconds, <10 values result
+                        // in 10 seconds delay
+                        // make sure you write to second endpoint!
+                        attrData: value,
+                    }],
+                    cfg: cfg.default,
+                };
+            } else if (type === 'get') {
+                return {
+                    cid: cid,
+                    cmd: 'read',
+                    cmdType: 'foundation',
+                    zclData: [{
+                        attrId: attrId,
+                    }],
+                    cfg: cfg.default,
+                };
+            }
+        },
+    },
     light_brightness: {
         key: ['brightness', 'brightness_percent'],
         convert: (key, value, message, type) => {
@@ -197,7 +233,10 @@ const converters = {
             }
         },
     },
-    /* Note when send the command to set sensitivity, press button on the device to make it wakeup*/
+    /*
+     * Note when send the command to set sensitivity, press button on the device
+     * to make it wakeup
+     */
     DJT11LM_vibration_sensitivity: {
         key: ['sensitivity'],
         convert: (key, value, message, type) => {
