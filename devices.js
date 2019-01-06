3e0c2f61dbddbbe4374de142e743c1f961d4302e
arteck <6681528+arteck@users.noreply.github.com>
2019-01-06 14:53:27 +0100

    FNB56-SKT1EHG1.2 (#198)
    * GLEDOPTO GL-B-008Z
    
    * HGZB-20-DE
    
    https://www.amazon.de/Intelligente-Steckdose-kompatibel-SmartThings-Steuerung/dp/B07GYG5GRP
    
    * Update devices.js
    
    * Update devices.js
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -2110,6 +2110,17 @@ const devices = [
         fromZigbee: generic.light_onoff_brightness.fromZigbee,
         toZigbee: generic.light_onoff_brightness.toZigbee,
     },
+
+    // Smart Home Pty
+    {
+        zigbeeModel: ['FNB56-SKT1EHG1.2'],
+        model: 'HGZB-20-DE',
+        vendor: 'Smart Home Pty',
+        description: 'Power plug',
+        supports: 'on/off',
+        fromZigbee: [fz.generic_state_change],
+        toZigbee: [tz.on_off],
+    },
 ];
 
 module.exports = devices;
