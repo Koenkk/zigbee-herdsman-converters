3651404b48fb3612aea621293942373626cfa0b8
Chrischi- <Chrischi-@users.noreply.github.com>
2019-01-06 14:13:20 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1865,23 +1865,31 @@ const devices = [
     {
         zigbeeModel: ['902010/32'],
         model: 'AV2010/32',
-        vendor: 'Bitron Home',
+        vendor: 'Bitron',
         description: 'Wall thermostat with relay',
         supports: 'temperature, heating/cooling system control',
         fromZigbee: [
-            fz.bitron_bitron_battery, fz.bitron_thermostat_att_report,
-            fz.bitron_thermostat_dev_change, fz.ignore_power_change,
+            fz.ignore_basic_change, fz.bitron_thermostat_att_report,
+            fz.bitron_thermostat_dev_change, fz.bitron_battery,
         ],
-        toZigbee: [tz.thermostat_occupied_heating_setpoint, tz.thermostat_local_temperature_calibration],
+        toZigbee: [
+            tz.thermostat_occupied_heating_setpoint, tz.thermostat_local_temperature_calibration,
+            tz.thermostat_local_temperature,
+        ],
         configure: (ieeeAddr, shepherd, coordinator, callback) => {
             const device = shepherd.find(ieeeAddr, 1);
             const actions = [
+                (cb) => device.bind('genBasic', coordinator, cb),
                 (cb) => device.bind('genPowerCfg', coordinator, cb),
+                (cb) => device.bind('genIdentify', coordinator, cb),
+                (cb) => device.bind('genTime', coordinator, cb),
+                (cb) => device.bind('genPollCtrl', coordinator, cb),
                 (cb) => device.bind('hvacThermostat', coordinator, cb),
+                (cb) => device.bind('hvacUserInterfaceCfg', coordinator, cb),
                 (cb) => device.report('hvacThermostat', 'localTemp', 300, 3600, 0, cb),
-                (cb) => device.report('hvacThermostat', 'local_temperature_calibration', 1, 0, 0, cb),
-                (cb) => device.report('hvacThermostat', 'runningState', 1, 0, 0, cb),
+                (cb) => device.report('hvacThermostat', 'localTemperatureCalibration', 1, 0, 0, cb),
                 (cb) => device.report('hvacThermostat', 'occupiedHeatingSetpoint', 1, 0, 1, cb),
+                (cb) => device.report('hvacThermostat', 'runningState', 1, 0, 0, cb),
                 (cb) => device.report('genPowerCfg', 'batteryVoltage', 1800, 43200, 0, cb),
                 (cb) => device.report('genPowerCfg', 'batteryAlarmState', 1, 0, 1, cb),
             ];
