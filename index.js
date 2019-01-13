985119a1feb803c9a57905561ac6c19a41e9dab6
Mihal Malostanidis <qm3ster@gmail.com>
2019-01-13 18:41:49 +0200

    Refactors (#207)
    * Add Iris 3326-L motion and temperature sensor
    
    * Use scoped zcl-id
    
    * Only package needed files
    
    * refactor: Use a Map for findByZigbeeModel
    
    * Add `extend` attribute
    
    The attribute shallow-merges the model object,
    with the explicitly defined properties taking precidence
    Converted all uses of `generic` to `extend` format.

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const devices = require('./devices');
 const toZigbee = require('./converters/toZigbee');
 const fromZigbee = require('./converters/fromZigbee');
 
+const byZigbeeModel = new Map();
+for (const device of devices) {
+    for (const zigbeeModel of device.zigbeeModel) {
+        byZigbeeModel.set(zigbeeModel, device);
+    }
+}
+
 module.exports = {
-    devices: devices,
-    findByZigbeeModel: (model) => devices.find((d) => d.zigbeeModel.includes(model)),
+    devices,
+    findByZigbeeModel: (model) => byZigbeeModel.get(model),
     toZigbeeConverters: toZigbee,
     fromZigbeeConverters: fromZigbee,
 };
