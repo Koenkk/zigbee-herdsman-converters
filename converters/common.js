9a1e891156fbe48a3909023742659225ac5b74e3
Simon Rasmussen <zetlon@gmail.com>
2019-01-22 20:06:22 +0100

    Implemented thermostat enums (#226)
    * Implemented thermostat enums
    
    * Fixed linting issues
    
    * Hopefully no more linting problems
    
    * Fixed review comments
    
    * Fixed more uppercase
    
    * Update toZigbee.js

---
+++ converters/common.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const thermostatControlSequenceOfOperations = {
+    0: 'cooling only',
+    1: 'cooling with reheat',
+    2: 'heating only',
+    3: 'heating with reheat',
+    4: 'cooling and heating 4-pipes',
+    5: 'cooling and heating 4-pipes with reheat',
+};
+const thermostatSystemModes = {
+    0: 'off',
+    1: 'auto',
+    3: 'cool',
+    4: 'heat',
+    5: 'emergency heating',
+    6: 'precooling',
+    7: 'fan only',
+    8: 'dry',
+    9: 'Sleep',
+};
+const thermostatRunningModes = {
+    0: 'off',
+    3: 'cool',
+    4: 'heat',
+};
+
+module.exports = {
+    thermostatControlSequenceOfOperations,
+    thermostatSystemModes,
+    thermostatRunningModes,
+};
