3e1c29d941f21dc70a0149c546bf6ab5697b221c
Maxim Milakov <maxim.milakov@gmail.com>
2019-01-29 21:59:43 +0300

    Added support for Gledopto GL-B-007Z bulb (#251)

--- devices.js
+++ devices.js
@@ -1611,6 +1611,22 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['GL-B-007Z'],
+        model: 'GL-B-007Z',
+        vendor: 'Gledopto',
+        description: 'Smart 6W E27 RGB / CW LED bulb',
+        extend: generic.light_onoff_brightness_colortemp_colorxy,
+        ep: (device) => {
+            if (device.epList.toString() === '11,12,13') {
+                return {'': 12};
+            } else if (device.epList.toString() === '10,11,13' || device.epList.toString() === '11,13') {
+                return {'': 11};
+            } else {
+                return {};
+            }
+        },
+    },
+    {
         zigbeeModel: ['GL-B-008Z'],
         model: 'GL-B-008Z',
         vendor: 'Gledopto',
