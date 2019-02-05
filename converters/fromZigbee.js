4379ffc42d602c444e6ec8b3282ff6c02e0bcdc6
Mad-Andy <47221633+Mad-Andy@users.noreply.github.com>
2019-02-05 14:30:19 +0100

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -920,6 +920,22 @@ const converters = {
             return results;
         },
     },
+    heiman_gas: {    
+        cid: 'ssIasZone',
+        type: 'statusChange',
+        convert: (model, msg, publish, options) => {
+            const zoneStatus = msg.data.zoneStatus;
+            return {
+                gas: (zoneStatus & 1) > 0, // Bit 1 = Alarm: Gas
+                battery_low: (zoneStatus & 1<<3) > 0, // Bit 4 = Battery LOW indicator
+            };
+        },                
+    }, 
+    ignore_iaszone_change: {
+        cid: 'ssIasZone',
+        type: 'devChange',
+        convert: (model, msg, publish, options) => null,
+    },
     heiman_water_leak: {
         cid: 'ssIasZone',
         type: 'statusChange',
