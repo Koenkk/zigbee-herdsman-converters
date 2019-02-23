df7212e86a218b2912a5d180b7a46a683790722a
Koen Kanters <koenkanters94@gmail.com>
2019-02-23 14:39:09 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1854,6 +1854,24 @@ const devices = [
             }
         },
     },
+    
+    // Smart Home Pty
+    {
+        zigbeeModel: ['FB56-ZCW11HG1.2'],
+        model: 'HGZB-07A',
+        vendor: 'Smart Home Pty',
+        description: 'RGBW Downlight',
+        extend: generic.light_onoff_brightness_colortemp_colorxy,
+    },
+    {
+        zigbeeModel: ['FNB56-SKT1EHG1.2'],
+        model: 'HGZB-20-DE',
+        vendor: 'Smart Home Pty',
+        description: 'Power plug',
+        supports: 'on/off',
+        fromZigbee: [fz.state_change],
+        toZigbee: [tz.on_off],
+    },
 
     // SmartThings
     {
