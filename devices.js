c662ba2e42bf30d1d1e3d467dab442d420d1de33
Koen Kanters <koenkanters94@gmail.com>
2019-02-23 14:41:35 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1694,7 +1694,25 @@ const devices = [
         description: 'ZigBee smart light controller',
         extend: generic.light_onoff_brightness,
     },
-    
+
+    // Smart Home Pty
+    {
+        zigbeeModel: ['FB56-ZCW11HG1.2'],
+        model: 'HGZB-07A',
+        vendor: 'Smart Home Pty',
+        description: 'RGBW Downlight',
+        extend: generic.light_onoff_brightness_colortemp_colorxy,
+    },
+    {
+        zigbeeModel: ['FNB56-SKT1EHG1.2'],
+        model: 'HGZB-20-DE',
+        vendor: 'Smart Home Pty',
+        description: 'Power plug',
+        supports: 'on/off',
+        fromZigbee: [fz.state_change],
+        toZigbee: [tz.on_off],
+    },
+
     // Gledopto
     {
         zigbeeModel: ['GLEDOPTO', 'GL-C-008', 'GL-C-007'],
@@ -1937,24 +1955,6 @@ const devices = [
         },
     },
 
-    // Smart Home Pty
-    {
-        zigbeeModel: ['FB56-ZCW11HG1.2'],
-        model: 'HGZB-07A',
-        vendor: 'Smart Home Pty',
-        description: 'RGBW Downlight',
-        extend: generic.light_onoff_brightness_colortemp_colorxy,
-    },
-    {
-        zigbeeModel: ['FNB56-SKT1EHG1.2'],
-        model: 'HGZB-20-DE',
-        vendor: 'Smart Home Pty',
-        description: 'Power plug',
-        supports: 'on/off',
-        fromZigbee: [fz.state_change],
-        toZigbee: [tz.on_off],
-    },
-
     // Trust
     {
         zigbeeModel: ['ZLL-DimmableLigh'],
@@ -1998,24 +1998,6 @@ const devices = [
 
             execute(device, actions, callback);
         },
-    },
-    
-    // Smart Home Pty
-    {
-        zigbeeModel: ['FB56-ZCW11HG1.2'],
-        model: 'HGZB-07A',
-        vendor: 'Smart Home Pty',
-        description: 'RGBW Downlight',
-        extend: generic.light_onoff_brightness_colortemp_colorxy,
-    },
-    {
-        zigbeeModel: ['FNB56-SKT1EHG1.2'],
-        model: 'HGZB-20-DE',
-        vendor: 'Smart Home Pty',
-        description: 'Power plug',
-        supports: 'on/off',
-        fromZigbee: [fz.state_change],
-        toZigbee: [tz.on_off],
     },
 
     // Paulmann
