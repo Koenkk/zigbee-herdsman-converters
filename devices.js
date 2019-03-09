89527e0c354788d93063ba28321983a2ea22b358
Koen Kanters <koenkanters94@gmail.com>
2019-03-09 16:23:09 +0100

    Ignore basic dev change for all lights. https://github.com/Koenkk/zigbee2mqtt/issues/1064

--- devices.js
+++ devices.js
@@ -15,13 +15,16 @@ const coordinatorGroup = 99;
 const generic = {
     light_onoff_brightness: {
         supports: 'on/off, brightness',
-        fromZigbee: [fz.brightness, fz.state_change, fz.state, fz.brightness_report, fz.ignore_genGroups_devChange],
+        fromZigbee: [
+            fz.brightness, fz.state_change, fz.state, fz.brightness_report,
+            fz.ignore_genGroups_devChange, fz.ignore_basic_change,
+        ],
         toZigbee: [tz.light_onoff_brightness, tz.ignore_transition, tz.light_alert],
     },
     light_onoff_brightness_colortemp: {
         supports: 'on/off, brightness, color temperature',
         fromZigbee: [
-            fz.brightness, fz.color_colortemp, fz.state_change, fz.state,
+            fz.brightness, fz.color_colortemp, fz.state_change, fz.state, fz.ignore_basic_change,
             fz.brightness_report, fz.color_colortemp_report, fz.ignore_genGroups_devChange,
         ],
         toZigbee: [tz.light_onoff_brightness, tz.light_colortemp, tz.ignore_transition, tz.light_alert],
@@ -29,7 +32,7 @@ const generic = {
     light_onoff_brightness_colorxy: {
         supports: 'on/off, brightness, color xy',
         fromZigbee: [
-            fz.brightness, fz.color_colortemp, fz.state_change, fz.state,
+            fz.brightness, fz.color_colortemp, fz.state_change, fz.state, fz.ignore_basic_change,
             fz.brightness_report, fz.color_colortemp_report, fz.ignore_genGroups_devChange,
         ],
         toZigbee: [tz.light_onoff_brightness, tz.light_color, tz.ignore_transition, tz.light_alert],
@@ -37,7 +40,7 @@ const generic = {
     light_onoff_brightness_colortemp_colorxy: {
         supports: 'on/off, brightness, color temperature, color xy',
         fromZigbee: [
-            fz.brightness, fz.color_colortemp, fz.state_change, fz.state,
+            fz.brightness, fz.color_colortemp, fz.state_change, fz.state, fz.ignore_basic_change,
             fz.brightness_report, fz.color_colortemp_report, fz.ignore_genGroups_devChange,
         ],
         toZigbee: [
