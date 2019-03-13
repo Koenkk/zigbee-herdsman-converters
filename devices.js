ec8c8700cb8b861c75ad41917b7be00e684d6414
highground88 <emaillj@hotmail.com>
2019-03-14 03:54:55 +1000

    Changed to correct external model details (#335)
    * Changed to correct external model details
    
    * Fix model numbers
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -1825,7 +1825,7 @@ const devices = [
     },
     {
         zigbeeModel: ['FB56+ZSW05HG1.2'],
-        model: 'FB56+ZSW05HG1.2',
+        model: 'HGZB-01A/02A',
         vendor: 'Nue / 3A',
         description: 'ZigBee one gang wall / in-wall smart switch',
         supports: 'on/off',
