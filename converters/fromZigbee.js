17610033d067d650721c968fcc7edc15f632874d
Qosmio <you@example.com>
2019-03-15 16:11:52 -0500

    Add converters for ignoring 'diagnostic' and 'genscenes' dev changes

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -2330,6 +2330,16 @@ const converters = {
         type: 'devChange',
         convert: (model, msg, publish, options) => null,
     },
+    ignore_diagnostic_change: {
+        cid: 'haDiagnostic',
+        type: 'devChange',
+        convert: (model, msg, publish, options) => null,
+    },
+    ignore_genscenes_change: {
+        cid: 'genScenes',
+        type: 'devChange',
+        convert: (model, msg, publish, options) => null,
+    },
 };
 
 module.exports = converters;
