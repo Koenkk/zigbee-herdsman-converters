a39dd9f1b913a2bddb45475c426d8b09a204f1e4
Aaron Godfrey <aaronjg@gmail.com>
2019-04-09 19:37:22 -0700

    Fixed lint error

--- devices.js
+++ devices.js
@@ -3718,7 +3718,7 @@ const devices = [
         supports: 'on/off, battery',
         fromZigbee: [
             fz.ignore_onoff_change, fz.state, fz.ignore_genIdentify_change,
-            fz.ignore_basic_change
+            fz.ignore_basic_change,
         ],
         toZigbee: [tz.on_off, tz.ignore_transition],
     },
