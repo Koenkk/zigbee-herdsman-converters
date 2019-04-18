5a78f95b714c789065c54fa3be8eb707fb5f3c8b
Sebastian G <sebastian@goodrick.ch>
2019-04-18 17:32:05 +0200

    Support for Climax SCM-5ZBS (#403)
    * Support for Climax SCM-5ZBS
    
    Maybe "brightness" is not the correct description, but therefore we can re-use code. Device see http://www.climax.com.tw/scm-5zbs-zb.php or https://www.lupus-electronics.de/shop/Alarm-Smarthome/Smarthome-Hausautomation/LUPUS-Rollladenrelais-V2-p.html
    
    * Addes supports for Climax SCM-5ZBS
    
    * Fixed fromZigbee entries
    
    * One more devState change
    
    * Some fixes
    
    * using cover converters
    
    * physical value on ZigBee should be byte, mqtt message should be percent
    
    * added OPEN/CLOSED converters for cover
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -2944,6 +2944,15 @@ const devices = [
         fromZigbee: [fz.state_change],
         toZigbee: [tz.on_off],
     },
+    {
+        zigbeeModel: ['SCM-3_00.00.03.15'],
+        model: 'SCM-5ZBS',
+        vendor: 'Climax',
+        description: 'Roller shutter',
+        supports: 'open/close',
+        fromZigbee: [fz.cover_position_report, fz.cover_position, fz.cover_state_change, fz.cover_state_report],
+        toZigbee: [tz.cover_position, tz.cover_open_close],
+    },
 
     // HEIMAN
     {
