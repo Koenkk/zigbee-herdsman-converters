bcf58c15120508bf9de33f89206c81c10a86582f
mccoy88f <50622699+mccoy88f@users.noreply.github.com>
2019-06-08 19:25:08 +0200

    Cosmetic Fix for Blaupunkt SCM-S1 and Adding support Lupus 12031 (#459)
    * Cosmetic Fix for Blaupunkt SCM-S1 and Adding support Lupus 12031
    
    Cosmetic Fix for Blaupunkt SCM-S1 (SCM-R_00.00.03.15TC) and Adding support Lupus 12031 (SCM_00.00.03.11TC)
    Removed WarningDevice from Heiman (need more job on device to make it work)
    Heiman M1 Door Sensor work but need a fix because now show inverted status (someone can help?)
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -3100,6 +3100,17 @@ const devices = [
         toZigbee: [tz.cover_position, tz.cover_open_close],
     },
 
+    // Lupus
+    {
+        zigbeeModel: ['SCM_00.00.03.11TC'],
+        model: '12031',
+        vendor: 'Lupus',
+        description: 'Roller shutter',
+        supports: 'open/close',
+        fromZigbee: [fz.cover_position_report, fz.cover_position, fz.cover_state_change, fz.cover_state_report],
+        toZigbee: [tz.cover_position, tz.cover_open_close],
+    },
+
     // Climax
     {
         zigbeeModel: ['PSS_00.00.00.15TC'],
@@ -3182,15 +3193,6 @@ const devices = [
 
             execute(device, actions, callback);
         },
-    },
-    {
-        zigbeeModel: ['WarningDevice'],
-        model: 'HS1WD',
-        description: 'Smart indoor light siren',
-        supports: 'on/off',
-        vendor: 'HEIMAN',
-        fromZigbee: [fz.state],
-        toZigbee: [tz.on_off],
     },
     {
         zigbeeModel: ['SMOK_V16', 'b5db59bfd81e4f1f95dc57fdbba17931', 'SMOK_YDLV10', 'SmokeSensor-EM'],
