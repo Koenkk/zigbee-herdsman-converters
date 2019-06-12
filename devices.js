d568460f36f0b5d1c8c11a46d0da744e9538e783
poma <Semenov.Roman@mail.ru>
2019-06-12 23:35:48 +0300

    Support hold mode for QBKG04LM (Aqara single light switch)

--- devices.js
+++ devices.js
@@ -219,7 +219,7 @@ const devices = [
         description: 'Aqara single key wired wall switch without neutral wire. Doesn\'t work as a router and doesn\'t support power meter',
         supports: 'on/off',
         fromZigbee: [
-            fz.QBKG04LM_QBKG11LM_state, fz.ignore_onoff_change,
+            fz.QBKG04LM_QBKG11LM_state, fz.QBKG04LM_buttons,
             fz.QBKG04LM_QBKG11LM_operation_mode, fz.ignore_basic_report,
         ],
         toZigbee: [tz.on_off, tz.xiaomi_switch_operation_mode],
