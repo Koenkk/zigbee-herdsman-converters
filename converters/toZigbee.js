ad7eccc95422172372ef62bf86d100c752305c85
allofmex <allofmex@web.de>
2019-06-19 18:24:14 +0200

    Hue sml sensitivity (#484)
    * hue motion sensor sensitivity added
    
    * Update toZigbee.js
    
    * Update devices.js

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -18,6 +18,10 @@ const cfg = {
         manufSpec: 1,
         manufCode: 4151,
     },
+    hue: {
+        manufSpec: 1,
+        manufCode: 4107,
+    },
     osram: {
         manufSpec: 1,
         manufCode: 0x110c,
@@ -1411,6 +1415,46 @@ const converters = {
                         attrData: value,
                     }],
                     cfg: cfg.default,
+                }];
+            }
+        },
+    },
+    hue_motion_sensitivity: {
+        // motion detect sensitivity, philips specific
+        key: ['motion_sensitivity'],
+        convert: (key, value, message, type, postfix) => {
+            const cid = 'msOccupancySensing'; // 1030
+            const attrId = 48;
+            if (type === 'set') {
+                // hue_sml:
+                // 0: low, 1: medium, 2: high (default)
+                // make sure you write to second endpoint!
+                const lookup = {
+                    'low': 0,
+                    'medium': 1,
+                    'high': 2,
+                };
+
+                return [{
+                    cid: cid,
+                    cmd: 'write',
+                    cmdType: 'foundation',
+                    zclData: [{
+                        attrId: attrId,
+                        dataType: 32,
+                        attrData: lookup[value],
+                    }],
+                    cfg: cfg.hue,
+                }];
+            } else if (type === 'get') {
+                return [{
+                    cid: cid,
+                    cmd: 'read',
+                    cmdType: 'foundation',
+                    zclData: [{
+                        attrId: attrId,
+                    }],
+                    cfg: cfg.hue,
                 }];
             }
         },
