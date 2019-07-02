7a44a649225f4f1b8192a8a7ada570a317b2df3a
csseal <csseal@users.noreply.github.com>
2019-07-02 15:52:47 +0100

    Changed the model name

--- devices.js
+++ devices.js
@@ -1483,7 +1483,7 @@ const devices = [
     },
     {
         zigbeeModel: ['TWBulb01UK'],
-        model: 'HALIGHTTUNEW-E27-B22',
+        model: 'HV-GSCXZB279 || HV-GSCXZB229',
         vendor: 'Hive',
         description: 'Active light, warm to cool white (E27 & B22)',
         extend: generic.light_onoff_brightness_colortemp,
