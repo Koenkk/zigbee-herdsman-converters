7b7d7dd6bfdf8a80bacb4f9effff9c77e3177e7e
Koen Kanters <koenkanters94@gmail.com>
2019-07-11 19:31:29 +0200

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1505,54 +1505,36 @@ const converters = {
             // initialize stored defaults with observed values
             if (!store[deviceID]) {
                 store[deviceID] = {
-                    voltageMultiplier: 180, voltageDivisor: 39321,
-                    currentMultiplier: 72, currentDivisor: 39321,
-                    powerMultiplier: 10255, powerDivisor: 39321,
-                    voltage: 0, current: 0, power: 0,
+                    acVoltageMultiplier: 180, acVoltageDivisor: 39321, acCurrentMultiplier: 72,
+                    acCurrentDivisor: 39321, acPowerMultiplier: 10255, acPowerDivisor: 39321,
                 };
             }
 
             // if new multipliers/divisors come in, replace prior values or defaults
-            if (msg.data.data.hasOwnProperty('acVoltageMultiplier')) {
-                store[deviceID].voltageMultiplier = msg.data.data['acVoltageMultiplier'];
-            }
-            if (msg.data.data.hasOwnProperty('acVoltageDivisor')) {
-                store[deviceID].voltageDivisor = msg.data.data['acVoltageDivisor'];
-            }
-            if (msg.data.data.hasOwnProperty('acCurrentMultiplier')) {
-                store[deviceID].currentMultiplier = msg.data.data['acCurrentMultiplier'];
-            }
-            if (msg.data.data.hasOwnProperty('acCurrentDivisor')) {
-                store[deviceID].currentDivisor = msg.data.data['acCurrentDivisor'];
-            }
-            if (msg.data.data.hasOwnProperty('acPowerMultiplier')) {
-                store[deviceID].powerMultiplier = msg.data.data['acPowerMultiplier'];
-            }
-            if (msg.data.data.hasOwnProperty('acPowerDivisor')) {
-                store[deviceID].powerDivisor = msg.data.data['acPowerDivisor'];
-            }
+            Object.keys(store[deviceID]).forEach((key) => {
+                if (msg.data.data.hasOwnProperty(key)) {
+                    store[deviceID][key] = msg.data.data[key];
+                }
+            })
 
             // if raw measurement comes in, apply stored/default multiplier and divisor
             if (msg.data.data.hasOwnProperty('rmsVoltage')) {
-                store[deviceID].voltage = (msg.data.data['rmsVoltage']
-                    * store[deviceID].voltageMultiplier
-                    / store[deviceID].voltageDivisor).toFixed(2);
-            }
-            if (msg.data.data.hasOwnProperty('rmsCurrent')) {
-                store[deviceID].current = (msg.data.data['rmsCurrent']
-                    * store[deviceID].currentMultiplier
-                    / store[deviceID].currentDivisor).toFixed(2);
-            }
-            if (msg.data.data.hasOwnProperty('activePower')) {
-                store[deviceID].power = (msg.data.data['activePower']
-                    * store[deviceID].powerMultiplier
-                    / store[deviceID].powerDivisor).toFixed(2);
+                result.voltage = (msg.data.data['rmsVoltage']
+                    * store[deviceID].acVoltageMultiplier
+                    / store[deviceID].acVoltageDivisor).toFixed(2);
             }
 
-            // return calculated measurements
-            result.voltage = store[deviceID].voltage;
-            result.current = store[deviceID].current;
-            result.power = store[deviceID].power;
+            if (msg.data.data.hasOwnProperty('rmsCurrent')) {
+                result.current = (msg.data.data['rmsCurrent']
+                    * store[deviceID].acCurrentMultiplier
+                    / store[deviceID].acCurrentDivisor).toFixed(2);
+            }
+
+            if (msg.data.data.hasOwnProperty('activePower')) {
+                result.power = (msg.data.data['activePower']
+                    * store[deviceID].acPowerMultiplier
+                    / store[deviceID].acPowerDivisor).toFixed(2);
+            }
 
             return result;
         },
