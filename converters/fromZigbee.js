645544725f61cd037634dba0216fb01d74acba62
codegraffiti <chandankp@gmail.com>
2019-07-11 20:51:37 +0530

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -2878,12 +2878,12 @@ const converters = {
         },
     },
     RH3040_motion_dev_change: {
-        cid: 'ssIasZone',
-        type: 'devChange',
-        convert: (model, msg, publish, options) => {
-            if (msg.data.data.zoneType === 0x000D) { // type 0x000D = motion sensor
-                const zoneStatus = msg.data.data.zoneStatus;
-                return {
+            cid: 'ssIasZone',
+            type: 'devChange',
+            convert: (model, msg, publish, options) => {
+                    if (msg.data.data.zoneType === 0x000D) { // type 0x000D = motion sensor
+                            const zoneStatus = msg.data.data.zoneStatus;
+                            return {
                     occupancy: (zoneStatus & 1) > 0, // Bit 0 = Alarm 1: Presence Indication
 					tamper: (zoneStatus & 1<<2) > 0, // Bit 2 = Tamper status
                     battery_low: (zoneStatus & 1<<3) > 0, // Bit 3 = Battery LOW indicator (trips around 2.4V)
