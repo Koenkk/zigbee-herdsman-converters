d1dbbddb3bde410a847b3c7fd65db914a95a51af
mihir8786 <42723514+mihir8786@users.noreply.github.com>
2019-07-21 08:23:22 -0400

    Support for LivingWise LVS-ZB15S Smart Switch and Lutron LZL4BWHL01 Remote (#526)
    * Add files via upload
    
    * Lutron LZL-4B-WH-L01 Light Control
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -4100,6 +4100,15 @@ const devices = [
         ],
     },
     {
+        zigbeeModel: ['545df2981b704114945f6df1c780515a'],
+        model: 'LVS-ZB15S',
+        vendor: 'LivingWise',
+        description: 'ZigBee smart in-wall switch',
+        supports: 'on/off',
+        toZigbee: [tz.on_off],
+        fromZigbee: [fz.state, fz.ignore_onoff_change, fz.ignore_basic_report, fz.ignore_basic_change],
+    },
+    {
         zigbeeModel: ['e70f96b3773a4c9283c6862dbafb6a99'],
         model: 'LVS-SM10ZW',
         vendor: 'LivingWise',
@@ -4731,6 +4740,17 @@ const devices = [
             ];
             execute(device, actions, callback);
         },
+    },
+
+    // Lutron
+    {
+        zigbeeModel: ['LZL4BWHL01 Remote'],
+        model: 'LZL4BWHL01',
+        vendor: 'Lutron',
+        description: 'Connected bulb remote control',
+        supports: 'on/off, brightness',
+        fromZigbee: [fz.GIRA2430_down_hold, fz.GIRA2430_up_hold, fz.E1524_hold, fz.GIRA2430_stop],
+        toZigbee: [],
     },
 ];
 
