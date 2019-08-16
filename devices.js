54a8d73aecd9e9ca711dee05adf5b22d64009b3e
Kiall Mac Innes <kiall@macinnes.ie>
2019-08-16 17:43:02 +0100

    IKEA TRADFRI E1810 (#569)
    Add the IKEA E1810, which appears to be a replacement for the E1524. Functionally, they appear to
    work identically, the only difference I can see is front finish is matte rather than glossy, and
    the internals behind the battery cover are slightly different.

--- devices.js
+++ devices.js
@@ -664,7 +664,7 @@ const devices = [
     },
     {
         zigbeeModel: ['TRADFRI remote control'],
-        model: 'E1524',
+        model: 'E1524/E1810',
         description: 'TRADFRI remote control',
         supports:
             'toggle, arrow left/right click/hold/release, brightness up/down click/hold/release',
