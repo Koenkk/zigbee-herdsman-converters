6f58de5761b1047ed9f725a9eab4394b998a0329
asgothian <45667167+asgothian@users.noreply.github.com>
2019-09-08 16:02:14 +0200

    Update devices.js

--- devices.js
+++ devices.js
@@ -856,7 +856,7 @@ const devices = [
         extend: hue.light_onoff_brightness_colortemp_colorxy,
     },
     {
-        zigbeeModel: ['LST003'],
+        zigbeeModel: ['LST003', 'LST004'],
         model: '9290018187B',
         vendor: 'Philips',
         description: 'Hue white and color ambiance LightStrip Outdoor',
@@ -2485,7 +2485,7 @@ const devices = [
         extend: generic.light_onoff_brightness_colortemp_colorxy,
     },
     {
-        zigbeeModel: ['FNB56-SKT1EHG1.2'],
+        zigbeeModel: ['FNB56-SKT1EHG1.2', 'FNB56-SKT1JXN1.0'],
         model: 'HGZB-20-DE',
         vendor: 'Smart Home Pty',
         description: 'Power plug',
