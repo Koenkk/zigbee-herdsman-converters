0071faa3fe8c59e6f131166cc0539903d390c0be
alwashe <15383159+alwashe@users.noreply.github.com>
2019-09-21 18:11:41 +0200

    Update devices.js

--- devices.js
+++ devices.js
@@ -1494,6 +1494,25 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['Plug Z3'],
+        model: 'AC10691',
+        description: 'Smart+ plug',
+        supports: 'on/off',
+        vendor: 'OSRAM',
+        fromZigbee: [fz.ignore_onoff_change, fz.state],
+        toZigbee: [tz.on_off],
+        configure: (ieeeAddr, shepherd, coordinator, callback) => {
+            const device = shepherd.find(ieeeAddr, 3);
+            const cfg = {direction: 0, attrId: 0, dataType: 16, minRepIntval: 0, maxRepIntval: 1000, repChange: 0};
+            const actions = [
+                (cb) => device.bind('genOnOff', coordinator, cb),
+                (cb) => device.foundation('genOnOff', 'configReport', [cfg], foundationCfg, cb),
+            ];
+
+            execute(device, actions, callback);
+        },
+    },
+    {
         zigbeeModel: ['Flex RGBW', 'LIGHTIFY Indoor Flex RGBW', 'LIGHTIFY Flex RGBW'],
         model: '4052899926110',
         vendor: 'OSRAM',
