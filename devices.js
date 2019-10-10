3c07f59e2d77cf52af081ae82eca2814d4544e3a
Tommy Goode <tommy@tgoode.com>
2019-10-10 02:01:58 -0500

    Add temperature reporting to Iris 3320-L contact sensor.

--- devices.js
+++ devices.js
@@ -3375,10 +3375,16 @@ const devices = [
         zigbeeModel: ['3320-L'],
         model: '3320-L',
         vendor: 'Iris',
-        description: 'Contact sensor',
-        supports: 'contact',
-        fromZigbee: [fz.iris_3320L_contact],
+        description: 'Contact and temperature sensor',
+        supports: 'contact and temperature',
+        fromZigbee: [fz.iris_3320L_contact, fz.temperature],
         toZigbee: [],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['msTemperatureMeasurement']);
+            await configureReporting.temperature(endpoint);
+        },
     },
 
     // ksentry
