072126952e952c58b0677f600f0d7078d31086c3
mrshoop <stephanhoop@gmail.com>
2019-10-28 22:06:47 +0100

    Added support for Philips Hue model LWO001 (#719)
    * Added support for Philips Hue model LWO001
    
    Added support for Philips Hue Single Filament bulb - G93 E27 
    Model : LWO001
    
    * Update devices.js
    
    * Update devices.js

--- devices.js
+++ devices.js
@@ -1158,6 +1158,13 @@ const devices = [
         extend: hue.light_onoff_brightness,
     },
     {
+        zigbeeModel: ['LWO001'],
+        model: '8718699688882',
+        vendor: 'Philips',
+        description: 'Hue white Filament bulb G93 E27 bluetooth',
+        extend: hue.light_onoff_brightness,
+    },
+    {
         zigbeeModel: ['LST001'],
         model: '7299355PH',
         vendor: 'Philips',
