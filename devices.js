629c728ddbf8c161e0449f3e68f0a43693a68695
Hans-Wilhelm Warlo <hanswilw@users.noreply.github.com>
2019-11-19 21:49:50 +0100

    Add Sunricher In-Wall Switch

--- devices.js
+++ devices.js
@@ -4741,6 +4741,15 @@ const devices = [
         extend: generic.light_onoff_brightness,
     },
     {
+        zigbeeModel: ['ON/OFF'],
+        model: 'ZG9101SAC-HP-Switch',
+        vendor: 'Sunricher',
+        description: 'ZigBee AC In-Wall Switch',
+        supports: 'on/off',
+        fromZigbee: [fz.state],
+        toZigbee: [tz.on_off],
+    },
+    {
         zigbeeModel: ['ZG2833K4_EU06'],
         model: 'SR-ZG9001K4-DIM2',
         vendor: 'Sunricher',
