69fdd0ffc1875a2ec9b1ca1079a4c73fbadf3a41
Shaoranlaos <Shaoranlaos@users.noreply.github.com>
2019-12-02 19:20:00 +0100

    Update fromZigbee.js
    remove trailing Spaces

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -3226,7 +3226,7 @@ const converters = {
             };
         },
     },
-    
+
     // Ignore converters (these message dont need parsing).
     ignore_onoff_report: {
         cluster: 'genOnOff',
