de3ae50d96ce54d8bb62dd5a7473afd384210dcb
Koen Kanters <koenkanters94@gmail.com>
2019-12-05 21:16:18 +0100

    Update YMF40 definition. https://github.com/Koenkk/zigbee2mqtt/issues/2456

--- devices.js
+++ devices.js
@@ -4546,9 +4546,10 @@ const devices = [
         supports: 'lock/unlock, battery',
         fromZigbee: [fz.lock_operation_event, fz.battery_200],
         toZigbee: [tz.generic_lock],
-        meta: {configureKey: 1},
+        meta: {options: {disableDefaultResponse: true}, configureKey: 2},
         configure: async (device, coordinatorEndpoint) => {
             const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
             await configureReporting.lockState(endpoint);
             await configureReporting.batteryPercentageRemaining(endpoint);
         },
