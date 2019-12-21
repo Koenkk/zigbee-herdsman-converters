04b82c09cc7665da82f651466098f6db6570c94c
CSchlipp <christian@schlipp.de>
2019-12-21 15:44:44 +0100

    Add support for OSRAM LED Panel TW 595 UGR22

--- devices.js
+++ devices.js
@@ -2160,6 +2160,14 @@ const devices = [
         description: 'OSRAM SubstiTUBE T8 Advanced UO Connected',
         extend: osram.light_onoff_brightness,
     },
+    {
+        zigbeeModel: ['Panel TW 595 UGR22'],
+        model: '595UGR22',
+        vendor: 'OSRAM',
+        description: 'OSRAM LED Panel TW 595 UGR22',
+        extend: osram.light_onoff_brightness_colortemp,
+    },
+
 
 
     // Hive
