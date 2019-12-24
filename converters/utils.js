dd91efca0c9c97dd2580325efafe63ccdc3bc74a
jasperro <42558625+jasperro@users.noreply.github.com>
2019-12-24 19:37:43 +0100

    Hopefully finally fixed ESlint complaints

--- converters/utils.js
+++ converters/utils.js
@@ -43,28 +43,24 @@ function rgbToXY(red, green, blue) {
 
 // Input numbers between 0-1
 function hsvToRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
+    let r; let g; let b;
     if (arguments.length === 1) {
         s = h.s, v = h.v, h = h.h;
     }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
+    const i = Math.floor(h * 6);
+    const f = h * 6 - i;
+    const p = v * (1 - s);
+    const q = v * (1 - f * s);
+    const t = v * (1 - (1 - f) * s);
     switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
     }
-    return {
-        r: Math.round(r * 255),
-        g: Math.round(g * 255),
-        b: Math.round(b * 255),
-    };
+    return {r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255)};
 }
 
 function hsToXY(hue, saturation) {
