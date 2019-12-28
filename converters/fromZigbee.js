83b02da05a3bc7bb19ea05367e694665881fc406
Carl de Billy <carl.debilly@gmail.com>
2019-12-28 20:37:03 +0000

    Fix lint problem

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -2173,10 +2173,10 @@ const converters = {
             if (mode == 'number') {
                 result.stelpro_mode = mode;
                 switch (mode) {
-                    case 5:
-                        // "Eco" mode is translated into "auto" here
-                        result.system_mode = common.thermostatSystemModes[1];
-                        break;
+                case 5:
+                    // "Eco" mode is translated into "auto" here
+                    result.system_mode = common.thermostatSystemModes[1];
+                    break;
                 }
             }
             const piHeatingDemand = msg.data['pIHeatingDemand'];
