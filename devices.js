57b793b15660eb07a85a7c5e1a82cbd31a02874e
GiffE <giff1118@gmail.com>
2020-01-03 13:00:15 -0500

    Add Support for Sengled Smart Plug (#855)
    * Add support for Sengled Smart Plug
    
    * Added configure to Sengled Smart Plug (E1C-NB6)

--- devices.js
+++ devices.js
@@ -2816,6 +2816,21 @@ const devices = [
         description: 'Element downlight smart LED bulb',
         extend: generic.light_onoff_brightness,
     },
+    {
+        zigbeeModel: ['E1C-NB6'],
+        model: 'E1C-NB6',
+        vendor: 'Sengled',
+        description: 'Smart plug',
+        supports: 'on/off',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await configureReporting.onOff(endpoint);
+        },
+    },
 
     // Swann
     {
