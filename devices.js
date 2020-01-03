f93dba404fb4db84389cf3f0ba7746efbbccc5d8
Carl de Billy <carl.debilly@gmail.com>
2020-01-03 13:18:40 -0500

    Added support for Leviton Lumina-RF DL15S (#866)
    * Added support for Leviton Lumina-RF DL15S
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -2670,6 +2670,23 @@ const devices = [
         extend: generic.light_onoff_brightness,
     },
 
+    // Leviton
+    {
+        zigbeeModel: ['DL15S'],
+        model: 'DL15S-1BZ',
+        vendor: 'Leviton',
+        description: 'Lumina RF 15A switch, 120/277V',
+        supports: 'on/off',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await configureReporting.onOff(endpoint);
+        },
+    },
+
     // GE
     {
         zigbeeModel: ['SoftWhite'],
