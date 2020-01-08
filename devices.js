1d4a28318e0c6e21101ab3cc9ec7296384aaafcd
Boris Pruessmann <boris@pruessmann.org>
2020-01-08 09:10:48 +0100

    Fixed issue with illuminize 511.202

--- devices.js
+++ devices.js
@@ -5686,9 +5686,15 @@ const devices = [
         toZigbee: [tz.on_off],
         meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(3);
-            await bind(endpoint, coordinatorEndpoint, ['genOnOff']);
-            await configureReporting.onOff(endpoint);
+            const ep1 = device.getEndpoint(1);
+            if (ep1 != null) {
+                await bind(ep1, coordinatorEndpoint, ['genOnOff']);
+                await configureReporting.onOff(ep1);
+            } else {
+                const ep3 = device.getEndpoint(3);
+                await bind(ep3, coordinatorEndpoint, ['genOnOff']);
+                await configureReporting.onOff(ep3);
+            }
         },
     },
 
