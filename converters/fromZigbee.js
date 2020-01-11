5bd81fe35a0266fc62918bdf5daf4d7ce8e1d9c7
Pedro Lamas <pedrolamas@gmail.com>
2020-01-11 21:40:05 +0000

    Fixes #897: SP600 now uses generic_power converter

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1571,25 +1571,6 @@ const converters = {
             };
         },
     },
-    SP600_power: {
-        cluster: 'seMetering',
-        type: ['attributeReport', 'readResponse'],
-        convert: (model, msg, publish, options) => {
-            // Cannot use generic_power, divisor/multiplier is not according to ZCL.
-            // https://github.com/Koenkk/zigbee2mqtt/issues/2233
-            const result = {};
-            if (msg.data.hasOwnProperty('instantaneousDemand')) {
-                result.power = msg.data['instantaneousDemand'];
-            }
-            // Summation is reported in Watthours
-            if (msg.data.hasOwnProperty('currentSummDelivered')) {
-                const data = msg.data['currentSummDelivered'];
-                const value = (parseInt(data[0]) << 32) + parseInt(data[1]);
-                result.energy = value / 1000.0;
-            }
-            return result;
-        },
-    },
     SP120_power: {
         cluster: 'haElectricalMeasurement',
         type: ['attributeReport', 'readResponse'],
