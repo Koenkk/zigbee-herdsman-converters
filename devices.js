09b742c88606845bb7f39dbe37c37d647c3dea81
jungmonam <34467593+jungmonam@users.noreply.github.com>
2020-01-15 22:59:25 +0900

    Update devices.js

--- devices.js
+++ devices.js
@@ -6458,7 +6458,7 @@ const devices = [
         zigbeeModel: ['PM-B530-ZB'],
         model: 'PM-B530-ZB',
         vendor: 'Dawon DNS',
-        description: 'IOT smart plug',
+        description: 'IOT smart plug 16A',
         supports: 'on/off, power and energy measurement',
         fromZigbee: [fz.on_off, fz.generic_power],
         toZigbee: [tz.on_off],
@@ -6470,7 +6470,23 @@ const devices = [
             await endpoint.read('seMetering', ['multiplier', 'divisor']);
         },
     },
-
+    {
+        zigbeeModel: ['PM-B430-ZB'],
+        model: 'PM-B430-ZB',
+        vendor: 'Dawon DNS',
+        description: 'IOT smart plug 10A',
+        supports: 'on/off, power and energy measurement',
+        fromZigbee: [fz.on_off, fz.generic_power],
+        toZigbee: [tz.on_off],
+        meta: {configureKey: 3},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'seMetering']);
+            await configureReporting.instantaneousDemand(endpoint);
+            await endpoint.read('seMetering', ['multiplier', 'divisor']);
+        },
+    },
+    
     // CREE
     {
         zigbeeModel: ['Connected A-19 60W Equivalent ', 'Connected A-19 60W Equivalent   '],
