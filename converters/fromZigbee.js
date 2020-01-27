706181e3cd449e0a8f2e9341fde4cbd8efcdcc9d
Ilya Kirov <kirovilya@gmail.com>
2020-01-27 14:19:50 +0300

    All buttons events for Aqara Opple Switch
    https://github.com/Koenkk/zigbee-herdsman/issues/132

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -3363,94 +3363,41 @@ const converters = {
             };
         },
     },
-    aqara_opple_on: {
-        cluster: 'genOnOff',
-        type: 'commandOn',
+    aqara_opple_report: {
+        cluster: 'aqaraOpple',
+        type: ['attributeReport', 'readResponse'],
         convert: (model, msg, publish, options, meta) => {
-            return {action: 'button_2_single'};
-        },
-    },
-    aqara_opple_off: {
-        cluster: 'genOnOff',
-        type: 'commandOff',
-        convert: (model, msg, publish, options, meta) => {
-            return {action: 'button_1_single'};
-        },
-    },
-    aqara_opple_step: {
-        cluster: 'genLevelCtrl',
-        type: 'commandStep',
-        convert: (model, msg, publish, options, meta) => {
-            const button = msg.data.stepmode === 0 ? 'button_4' : 'button_3';
-            return {action: `${button}_single`};
-        },
-    },
-    aqara_opple_stop: {
-        cluster: 'genLevelCtrl',
-        type: 'commandStop',
-        convert: (model, msg, publish, options, meta) => {
-            const deviceID = msg.device.ieeeAddr;
-            if (store[deviceID]) {
-                const duration = Date.now() - store[deviceID].start;
-                const button = store[deviceID].button;
-                return {action: `${button}_release`, duration: duration};
+            // it is like xiaomi_battery_3v, but not parsed
+            // https://github.com/Koenkk/zigbee-herdsman/blob/master/src/zcl/buffaloZcl.ts#L93
+            // data: { '247': <Buffer 01 21 b8 0b 03 28 19 04 21 a8 13 05 21 44 01 06 24 02 00 00 00 00 08 21 11 01 0a 21 00 00 0c 20 01 64 10 00> }
+            let voltage = null;
+
+            if (msg.data['247']) {
+                voltage = msg.data['247'][2] + msg.data['247'][3]*256;
             }
-        },
+
+            if (voltage) {
+                return {
+                    battery: parseFloat(toPercentageCR2032(voltage)),
+                    voltage: voltage,
+                };
+            }
+        }
     },
-    aqara_opple_move: {
-        cluster: 'genLevelCtrl',
-        type: 'commandMove',
+    aqara_opple_multistate: {
+        cluster: 'genMultistateInput',
+        type: ['attributeReport', 'readResponse'],
         convert: (model, msg, publish, options, meta) => {
-            // store button and start moment
-            const deviceID = msg.device.ieeeAddr;
-            if (!store[deviceID]) {
-                store[deviceID] = {};
-            }
-            const button = msg.data.movemode === 0 ? 'button_4' : 'button_3';
-            store[deviceID].button = button;
-            store[deviceID].start = Date.now();
-            return {action: `${button}_hold`};
-        },
-    },
-    aqara_opple_step_color_temp: {
-        cluster: 'lightingColorCtrl',
-        type: 'commandStepColorTemp',
-        convert: (model, msg, publish, options, meta) => {
-            let act;
-            if (model.model === 'WXCJKG12LM') {
-                // for WXCJKG12LM model it's double click event on buttons 3 and 4
-                act = (msg.data.stepmode === 1) ? 'button_3_double' : 'button_4_double';
-            } else {
-                // but for WXCJKG13LM model it's single click event on buttons 5 and 6
-                act = (msg.data.stepmode === 1) ? 'button_5_single' : 'button_6_single';
-            }
-            return {action: act};
-        },
-    },
-    aqara_opple_move_color_temp: {
-        cluster: 'lightingColorCtrl',
-        type: 'commandMoveColorTemp',
-        convert: (model, msg, publish, options, meta) => {
-            const deviceID = msg.device.ieeeAddr;
-            if (!store[deviceID]) {
-                store[deviceID] = {};
-            }
-            const stop = msg.data.movemode === 0;
-            let button;
-            const result = {};
-            if (stop) {
-                button = store[deviceID].button;
-                const duration = Date.now() - store[deviceID].start;
-                result.action = `${button}_release`;
-                result.duration = duration;
-            } else {
-                button = msg.data.movemode === 3 ? 'button_6' : 'button_5';
-                result.action = `${button}_hold`;
-                // store button and start moment
-                store[deviceID].button = button;
-                store[deviceID].start = Date.now();
-            }
-            return result;
+            const actionLookup = {
+                0: 'hold',
+                255: 'release',
+                1: 'single',
+                2: 'double',
+                3: 'triple',
+            };
+            const btn = msg.endpoint.ID;
+            const value = msg.data.presentValue;
+            return {click: `${btn}_${actionLookup[value]}`};
         },
     },
     SmartButton_skip: {
