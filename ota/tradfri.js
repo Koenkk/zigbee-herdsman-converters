59aa330b9180bb8a27b70582ec0c5f0234d0fc55
Koen Kanters <koenkanters94@gmail.com>
2020-02-03 22:10:56 +0100

    First working version of update checker. https://github.com/Koenkk/zigbee-herdsman/issues/60

--- ota/tradfri.js
+++ ota/tradfri.js
@@ -1,4 +1,8 @@
+const axios = require('axios');
+const url = 'http://fw.ota.homesmart.ikea.net/feed/version_info.json';
+
 async function updateAvailable(device, logger) {
+    // Find endpoint
     logger.debug(`Check if update available for '${device.ieeeAddr}' (${device.modelID})`);
 
     const endpoint = device.endpoints.find((e) => e.supportsOutputCluster('genOta'));
@@ -7,8 +11,17 @@ async function updateAvailable(device, logger) {
     }
     logger.debug(`Using endpoint '${endpoint.ID}'`);
 
-    const imageType = await endpoint.read('genOta', ['imageUpgradeStatus']);
-    console.log(imageType);
+    // Query available images
+    const images = (await axios.get(url)).data;
+
+    // Check which image is installed
+    const queryNextImageRequest = endpoint.waitForCommand('genOta', 'queryNextImageRequest', 10000);
+    await endpoint.commandResponse('genOta', 'imageNotify', {payloadType: 0, queryJitter: 100});
+    const current = await queryNextImageRequest;
+
+    const image = images.find((i) => i.fw_image_type === current.imageType);
+    const imageFileVersion = (image.fw_file_version_MSB << 16) | image.fw_file_version_LSB;
+    console.log(imageFileVersion, current);
 }
 
 module.exports = {
