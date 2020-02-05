a48cbe1af904825a46ecb3523bbebbc180932048
Koen Kanters <koenkanters94@gmail.com>
2020-02-05 19:32:24 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -4795,7 +4795,7 @@ const devices = [
         },
     },
     {
-        zigbeeModel: ['WarningDevice','WarningDevice-EF-3.0'],
+        zigbeeModel: ['WarningDevice', 'WarningDevice-EF-3.0'],
         model: 'HS2WD-E',
         vendor: 'HEIMAN',
         description: 'Smart siren',
