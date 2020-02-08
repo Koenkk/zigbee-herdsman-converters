86a333360c958f644825370443b73c04a774f385
Koen Kanters <koenkanters94@gmail.com>
2020-02-08 18:48:07 +0100

    TRADFRI OTA (#965)
    * Start on OTA implementation. https://github.com/Koenkk/zigbee-herdsman/issues/60
    
    * First working version of update checker. https://github.com/Koenkk/zigbee-herdsman/issues/60
    
    * Updates.
    
    * Working TRADFRI OTA. https://github.com/Koenkk/zigbee-herdsman/issues/60
    
    * Fixes
    
    * Finish ota
    
    * Lint

---
+++ ota/common.js
@@ -0,0 +1,142 @@
+const upgradeFileIdentifier = Buffer.from([0x1E, 0xF1, 0xEE, 0x0B]);
+const assert = require('assert');
+
+function parseSubElement(buffer, position) {
+    const tagID = buffer.readUInt16LE(position);
+    const length = buffer.readUInt32LE(position + 2);
+    const data = buffer.slice(position + 6, position + 6 + length);
+    return {tagID, length, data};
+}
+
+function parseOtaImage(buffer) {
+    const headerLength = 56;
+    const header = {
+        otaUpgradeFileIdentifier: buffer.subarray(0, 4),
+        otaHeaderVersion: buffer.readUInt16LE(4),
+        otaHeaderLength: buffer.readUInt16LE(6),
+        otaHeaderFieldControl: buffer.readUInt16LE(8),
+        manufacturerCode: buffer.readUInt16LE(10),
+        imageType: buffer.readUInt16LE(12),
+        fileVersion: buffer.readUInt32LE(14),
+        zigbeeStackVersion: buffer.readUInt16LE(18),
+        otaHeaderString: buffer.toString('utf8', 20, 52),
+        totalImageSize: buffer.readUInt32LE(52),
+    };
+
+    const raw = buffer.slice(0, header.totalImageSize);
+
+    assert(Buffer.compare(header.otaUpgradeFileIdentifier, upgradeFileIdentifier) === 0, 'Not an OTA file');
+    assert(header.otaHeaderFieldControl === 0, 'Non zero field control not implemented yet');
+
+    let remaining = header.totalImageSize - headerLength;
+    const elements = [];
+    while (remaining !== 0) {
+        const element = parseSubElement(buffer, headerLength);
+        elements.push(element);
+        remaining -= element.data.length + 6;
+    }
+
+    assert(remaining === 0, 'Size mismatch');
+    return {header, elements, raw};
+}
+
+function update(endpoint, logger, otaImage, onProgress) {
+    return new Promise((resolve, reject) => {
+        let imageBlockRequest = null;
+        const logLevels = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100];
+        const waitAndAnswerNextImageBlockRequest = () => {
+            imageBlockRequest = endpoint.waitForCommand('genOta', 'imageBlockRequest', null, 30000);
+            const fulfilled = (response) => {
+                waitAndAnswerNextImageBlockRequest();
+
+                const start = response.payload.fileOffset;
+                let end = start + response.payload.maximumDataSize;
+                if (end > otaImage.raw.length) {
+                    end = otaImage.raw.length;
+                }
+
+                endpoint.commandResponse('genOta', 'imageBlockResponse',
+                    {
+                        status: 0,
+                        manufacturerCode: response.payload.manufacturerCode,
+                        imageType: response.payload.imageType,
+                        fileVersion: response.payload.fileVersion,
+                        fileOffset: response.payload.fileOffset,
+                        dataSize: end - start,
+                        data: otaImage.raw.slice(start, end),
+                    },
+                    null,
+                    response.header.transactionSequenceNumber,
+                );
+
+                const percentage = (response.payload.fileOffset / otaImage.header.totalImageSize) * 100;
+                const logLevel = logLevels.find((l) => percentage > l);
+                if (logLevel) {
+                    logLevels.splice(logLevels.indexOf(logLevel), 1);
+                    logger.debug(`OTA update at ${logLevel}%`);
+                    onProgress(logLevel);
+                }
+            };
+
+            const rejected = () => {
+                reject(new Error('Did not receive imageBlockRequest'));
+            };
+
+            imageBlockRequest.promise.then(fulfilled, rejected);
+        };
+
+        logger.debug('Starting upgrade');
+        waitAndAnswerNextImageBlockRequest();
+
+        const queryNextImageResponse = () => endpoint.commandResponse('genOta', 'queryNextImageResponse',
+            {
+                status: 0,
+                manufacturerCode: otaImage.header.manufacturerCode,
+                imageType: otaImage.header.imageType,
+                fileVersion: otaImage.header.fileVersion,
+                imageSize: otaImage.header.totalImageSize,
+            },
+        );
+
+        queryNextImageResponse();
+        setTimeout(queryNextImageResponse, 5000);
+
+        const upgradeEndRequest = endpoint.waitForCommand('genOta', 'upgradeEndRequest', null, 1000 * 3600);
+        const fulfilled = (response) => {
+            if (imageBlockRequest) {
+                imageBlockRequest.cancel();
+            }
+
+            if (response.payload.status === 0) {
+                endpoint.commandResponse('genOta', 'upgradeEndResponse',
+                    {
+                        manufacturerCode: otaImage.header.manufacturerCode,
+                        imageType: otaImage.header.imageType,
+                        fileVersion: otaImage.header.fileVersion,
+                        imageSize: otaImage.header.totalImageSize,
+                        currentTime: 0,
+                        upgradeTime: 1,
+                    },
+                );
+                logger.debug(`Update succeeded`);
+                resolve();
+            } else {
+                const error = `Update failed with code '${response.payload.status}'`;
+                logger.debug(error);
+                reject(new Error(error));
+            }
+        };
+
+        const rejected = () => {
+            reject(new Error('Upgrade end request timeout'));
+        };
+
+        upgradeEndRequest.promise.then(fulfilled, rejected);
+    });
+}
+
+module.exports = {
+    parseOtaImage,
+    upgradeFileIdentifier,
+    update,
+};
---
+++ ota/tradfri.js
@@ -0,0 +1,95 @@
+const axios = require('axios');
+const url = 'http://fw.ota.homesmart.ikea.net/feed/version_info.json';
+const assert = require('assert');
+const common = require('./common');
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function getEndpoint(device) {
+    return device.endpoints.find((e) => e.supportsOutputCluster('genOta'));
+}
+
+async function getDeviceInfo(endpoint) {
+    const queryNextImageRequest = endpoint.waitForCommand('genOta', 'queryNextImageRequest', null, 10000);
+    try {
+        await endpoint.commandResponse('genOta', 'imageNotify', {payloadType: 0, queryJitter: 100});
+        return await queryNextImageRequest.promise;
+    } catch (e) {
+        queryNextImageRequest.cancel();
+        throw e;
+    }
+}
+
+async function getImage(imageType) {
+    const images = (await axios.get(url)).data;
+    const image = images.find((i) => i.fw_image_type === imageType);
+    return {
+        fileVersion: (image.fw_file_version_MSB << 16) | image.fw_file_version_LSB,
+        fileSize: image.fw_filesize,
+        url: image.fw_binary_url,
+    };
+}
+
+async function isUpdateAvailable(device, logger) {
+    logger.debug(`Check if update available for '${device.ieeeAddr}' (${device.modelID})`);
+
+    const endpoint = getEndpoint(device);
+    assert(endpoint !== null, `Failed to find endpoint which support OTA cluster`);
+    logger.debug(`Using endpoint '${endpoint.ID}'`);
+
+    const info = await getDeviceInfo(endpoint);
+    logger.debug(`Got device info '${JSON.stringify(info.payload)}'`);
+
+    const image = await getImage(info.payload.imageType);
+    logger.debug(`Got image '${JSON.stringify(image)}'`);
+
+    return image.fileVersion > info.payload.fileVersion;
+}
+
+async function updateToLatest(device, logger, onProgress) {
+    logger.debug(`Update to latest '${device.ieeeAddr}' (${device.modelID})`);
+
+    const endpoint = getEndpoint(device);
+    assert(endpoint !== null, `Failed to find endpoint which support OTA cluster`);
+    logger.debug(`Using endpoint '${endpoint.ID}'`);
+
+    const info = await getDeviceInfo(endpoint);
+    logger.debug(`Got device info '${JSON.stringify(info.payload)}'`);
+    const image = await getImage(info.payload.imageType);
+    logger.debug(`Got image '${JSON.stringify(image)}'`);
+
+    assert(image.fileVersion > info.payload.fileVersion, 'No update available');
+
+    const imageResponse = await axios.get(image.url, {responseType: 'arraybuffer'});
+    const start = imageResponse.data.indexOf(common.upgradeFileIdentifier);
+
+    const otaImage = common.parseOtaImage(imageResponse.data.slice(start));
+    assert(otaImage.header.fileVersion === image.fileVersion, 'File version mismatch');
+    assert(otaImage.header.totalImageSize === image.fileSize, 'Image size mismatch');
+    assert(otaImage.header.manufacturerCode === 4476, 'Manufacturer code mismatch');
+    assert(otaImage.header.imageType === info.payload.imageType, 'Image type mismatch');
+    logger.debug('Image parsed and verified');
+
+    onProgress(0);
+    await common.update(endpoint, logger, otaImage, onProgress);
+
+    // Give device some time to reboot after update
+    await wait(30000);
+
+    const from_ = {softwareBuildID: device.softwareBuildID, dateCode: device.dateCode};
+
+    const genBasicEndpoint = device.endpoints.find((e) => e.supportsInputCluster('genBasic'));
+    const result = await genBasicEndpoint.read('genBasic', ['dateCode', 'swBuildId']);
+    device.softwareBuildID = result.swBuildId;
+    device.dateCode = result.dateCode;
+    device.save();
+
+    const to = {softwareBuildID: device.softwareBuildID, dateCode: device.dateCode};
+
+    logger.debug(`Updated from '${JSON.stringify(from_)}' to '${JSON.stringify(to)}'`);
+    return {from: from_, to: to};
+}
+
+module.exports = {
+    isUpdateAvailable,
+    updateToLatest,
+};
