803afc9083e45231a6bea542ef05acc3d403b05e
rbrack42 <53448758+rbrack42@users.noreply.github.com>
2020-02-14 10:52:20 -0500

    Add support for Sylvania 72567 Flush Mount Light (#986)
    * Add support for Sylvania 72567 Flush Mount Light
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -2987,6 +2987,13 @@ const devices = [
         description: 'SMART+ Zigbee adjustable white edge-lit under cabinet light',
         extend: generic.light_onoff_brightness_colortemp,
     },
+    {
+        zigbeeModel: ['Flushmount TW'],
+        model: '72567',
+        vendor: 'Sylvania',
+        description: 'SMART+ Zigbee adjustable white edge-lit flush mount light',
+        extend: generic.light_onoff_brightness_colortemp,
+    },
 
     // Leviton
     {
