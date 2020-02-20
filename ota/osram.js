3cd8a7cf7df33c0b5ce6c4aa94f7d55b5b8a0d8a
Pedro Lamas <pedrolamas@gmail.com>
2020-02-20 16:14:05 +0000

    Adds OTA update support for Osram/Ledvance devices

--- ota/osram.js
+++ ota/osram.js
@@ -1,15 +1,69 @@
+const axios = require('axios');
+const updateCheckUrl = 'https://api.update.ledvance.com/v1/zigbee/firmwares/newer';
+const updateDownloadUrl = 'https://api.update.ledvance.com/v1/zigbee/firmwares/download';
+const assert = require('assert');
 const common = require('./common');
 
 /**
  * Helper functions
  */
 
+function fileVersionToVersion(fileVersion) {
+    const version = ('00000000' + fileVersion.toString(16)).substr(-8);
+
+    return {
+        major: parseInt(version.substr(0, 3)),
+        minor: parseInt(version.substr(3, 2)),
+        build: parseInt(version.substr(5, 3)),
+    };
+}
+
+function versionToFileVersion(version) {
+    return parseInt(version.major * 100000 + version.minor * 1000 + version.build, 16);
+}
+
+function versionToString(version) {
+    return `${version.major}.${version.minor}.${version.build}`;
+}
+
+async function getImageMeta(manufacturerCode, imageType, fileVersion) {
+    const version = fileVersionToVersion(fileVersion);
+
+    const {data} = await axios.get(updateCheckUrl +
+        `?company=${manufacturerCode}&product=${imageType}&version=${versionToString(version)}`);
+
+    assert(data && data.firmwares && data.firmwares.length > 0,
+        `No image available for manufacturerCode '${manufacturerCode}' imageType '${imageType}'`);
+
+    const {identity, length} = data.firmwares[0];
+
+    return {
+        fileVersion: versionToFileVersion(identity.version),
+        fileSize: length,
+        url: updateDownloadUrl +
+            `?company=${identity.company}&product=${identity.product}&version=${versionToString(identity.version)}`,
+    };
+}
+
+async function getNewImage(current, logger, device) {
+    const meta = await getImageMeta(current.manufacturerCode, current.imageType, current.fileVersion);
+    assert(meta.fileVersion > current.fileVersion, 'No new image available');
+
+    const download = await axios.get(meta.url, {responseType: 'arraybuffer'});
+
+    const image = common.parseImage(download.data);
+    assert(image.header.fileVersion === meta.fileVersion, 'File version mismatch');
+    assert(image.header.totalImageSize === meta.fileSize, 'Image size mismatch');
+    assert(image.header.manufacturerCode === current.manufacturerCode, 'Manufacturer code mismatch');
+    assert(image.header.imageType === current.imageType, 'Image type mismatch');
+    return image;
+}
+
 async function isNewImageAvailable(current, logger, device) {
-    const metaS = JSON.stringify(current);
-
-    logger.debug(` ++ meta to send '${metaS}'`);
-
-    return false;
+    const meta = await getImageMeta(current.manufacturerCode, current.imageType, current.fileVersion);
+    const [currentS, metaS] = [JSON.stringify(current), JSON.stringify(meta)];
+    logger.debug(`Is new image available for '${device.ieeeAddr}', current '${currentS}', latest meta '${metaS}'`);
+    return meta.fileVersion > current.fileVersion;
 }
 
 /**
@@ -21,7 +75,7 @@ async function isUpdateAvailable(device, logger, requestPayload=null) {
 }
 
 async function updateToLatest(device, logger, onProgress) {
-    return Promise.resolve();
+    return common.updateToLatest(device, logger, onProgress, getNewImage);
 }
 
 module.exports = {
