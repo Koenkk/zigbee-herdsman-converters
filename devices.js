819a76bcde76ce5ec00a86673366a0586d9780ef
Sander <3908728+sheyden@users.noreply.github.com>
2020-02-23 20:18:39 +0100

    ICZB-KPD18S - Update 8s (8-)button model to match 4S Scene/brightness etc support (#1011)

--- devices.js
+++ devices.js
@@ -5339,8 +5339,11 @@ const devices = [
         model: 'ICZB-KPD18S',
         vendor: 'iCasa',
         description: 'Zigbee 3.0 Keypad Pulse 8S',
-        supports: 'click',
-        fromZigbee: [fz.scenes_recall_click, fz.genOnOff_cmdOn, fz.genOnOff_cmdOff, fz.battery_percentage_remaining],
+        supports: 'click, action, brightness, scenes',
+        fromZigbee: [
+            fz.scenes_recall_click, fz.genOnOff_cmdOn, fz.genOnOff_cmdOff, fz.battery_percentage_remaining,
+            fz.cmd_move_with_onoff, fz.cmd_stop_with_onoff,
+        ],
         toZigbee: [],
     },
     {
