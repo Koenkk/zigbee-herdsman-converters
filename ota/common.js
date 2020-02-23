d5127218ec7e0bf1c33837b40a252834c8ce7d46
Andreas Brett <andreasbrett@users.noreply.github.com>
2020-02-23 19:06:47 +0100

    OTA update check: inform about uncommon version mismatch (#1005)
    * Update tradfri.js
    
    return -1/0/1 for version check
    
    * Update salus.js
    
    return -1/0/1 for version check
    
    * Update common.js
    
    spot beta firmwares or version mismatch
    
    * Update common.js
    
    * Update common.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- ota/common.js
+++ ota/common.js
@@ -143,8 +143,11 @@ async function isUpdateAvailable(device, logger, isNewImageAvailable, requestPay
     }
 
     const available = await isNewImageAvailable(requestPayload, logger, device);
-    logger.debug(`Updata available for '${device.ieeeAddr}': ${available ? 'YES' : 'NO'}`);
-    return available;
+    logger.debug(`Update available for '${device.ieeeAddr}': ${available < 0 ? 'YES' : 'NO'}`);
+    if (available > 0) {
+        logger.warn(`Firmware on '${device.ieeeAddr}' is newer than latest firmware online.`);
+    }
+    return (available < 0);
 }
 
 async function updateToLatest(device, logger, onProgress, getNewImage) {
