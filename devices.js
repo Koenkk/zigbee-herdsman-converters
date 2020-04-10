0d3dfcfa7ed0535160c0d9615451739ec2e43b8e
Koen Kanters <koenkanters94@gmail.com>
2020-04-10 17:28:53 +0200

    Update devices.js

--- devices.js
+++ devices.js
@@ -2007,7 +2007,7 @@ const devices = [
         vendor: 'Philips',
         description: 'Hue smart button',
         supports: 'action',
-        fromZigbee: [fz.command_on, fz.command_off_with_effect, fz.SmartButton_skip, fz.battery],
+        fromZigbee: [fz.command_on, fz.command_off, fz.command_off_with_effect, fz.SmartButton_skip, fz.battery],
         toZigbee: [],
         meta: {configureKey: 4},
         configure: async (device, coordinatorEndpoint) => {
