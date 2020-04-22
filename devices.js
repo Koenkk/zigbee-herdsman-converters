471ba3f05923e152013e070bbb242c445066b1c4
ChrisChoke <63064620+ChrisChoke@users.noreply.github.com>
2020-04-22 19:39:15 +0200

    add Viessmann thermostat

--- devices.js
+++ devices.js
@@ -9586,6 +9586,28 @@ const devices = [
         description: 'RGBW LED bulb with dimmer',
         extend: generic.light_onoff_brightness_colortemp_colorxy,
     },
+    //Viessmann
+    {
+        zigbeeModel: ['7637434'],
+        model: 'ZK03840',
+        vendor: 'Viessmann',
+        description: 'Vicare Radiator Thermostat Valve',
+        supports: 'Temperature, Battery',
+        fromZigbee: [fz.thermostat_att_report, fz.battery],
+        toZigbee: [tz.thermostat_occupied_heating_setpoint, tz.thermostat_local_tmperature_calibration],
+        meta:{configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, [
+                'genBasic', 'genPowerCfg', 'genIdentify', 'genTime', 'genPollCtrl', 'hvacThermostat',
+                'hvacUserInterfaceCfg',
+            ]);
+            await configureReporting.thermostatTemperature(endpoint);
+            await configureReporting.thermostatOccupiedHeatingSetpoint(endpoint);
+            await configureReporting.thermostatPIHeatingDemand(endpoint);
+           },
+
+    },
 ];
 
 module.exports = devices.map((device) =>
