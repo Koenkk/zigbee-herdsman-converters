cf921808e43df8bcecc9aba20c6ded3ec5b8a118
jasperro <jasper.albering@gmail.com>
2020-04-24 17:52:10 +0200

    Added light utilities for HSV

--- converters/utils.js
+++ converters/utils.js
@@ -11,20 +11,15 @@ const kelvinToXyLookup = require('./lookup/kelvinToXy');
  * @return {Array} Array that contains the CIE color values for x and y
  */
 function rgbToXY(red, green, blue) {
-    // The RGB values should be between 0 and 1. So convert them.
-    // The RGB color (255, 0, 100) becomes (1.0, 0.0, 0.39)
-    red /= 255; green /= 255; blue /= 255;
 
     // Apply a gamma correction to the RGB values, which makes the color
     // more vivid and more the like the color displayed on the screen of your device
-    red = (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
-    green = (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
-    blue = (blue > 0.04045) ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : (blue / 12.92);
+    const rgb = gammaCorrectRGB(red, green, blue)
 
     // RGB values to XYZ using the Wide RGB D65 conversion formula
-    const X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
-    const Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
-    const Z = red * 0.000088 + green * 0.072310 + blue * 0.986039;
+    const X = rgb.r * 0.664511 + rgb.g * 0.154324 + rgb.b * 0.162028;
+    const Y = rgb.r * 0.283881 + rgb.g * 0.668433 + blue * 0.047685;
+    const Z = rgb.r * 0.000088 + rgb.g * 0.072310 + blue * 0.986039;
 
     // Calculate the xy values from the XYZ values
     let x = (X / (X + Y + Z)).toFixed(4);
@@ -40,6 +35,79 @@ function rgbToXY(red, green, blue) {
 
     return {x: Number.parseFloat(x), y: Number.parseFloat(y)};
 }
+
+
+function gammaCorrectRGB(r, g, b) {
+    // The RGB values should be between 0 and 1. So convert them.
+    // The RGB color (255, 0, 100) becomes (1.0, 0.0, 0.39)
+    r /= 255; g /= 255; b /= 255;
+
+    r = (r > 0.04045) ? Math.pow((r + 0.055) / (1.0 + 0.055), 2.4) : (r / 12.92);
+    g = (g > 0.04045) ? Math.pow((g + 0.055) / (1.0 + 0.055), 2.4) : (g / 12.92);
+    b = (b > 0.04045) ? Math.pow((b + 0.055) / (1.0 + 0.055), 2.4) : (b / 12.92);
+
+    return {r: Math.round(r*255),
+	    g: Math.round(g*255),
+	    b: Math.round(b*255)};
+}
+
+function hsvToRGB(h, s, v) {
+    h = h % 360;
+    s = s / 100;
+    v = v / 100;
+    
+    let r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
+
+function rgbToHSV(r, g, b) {
+    if (arguments.length === 1) {
+        g = r.g, b = r.b, r = r.r;
+    }
+    var max = Math.max(r, g, b), min = Math.min(r, g, b),
+        d = max - min,
+        h,
+        s = (max === 0 ? 0 : d / max),
+        v = max / 255;
+
+    switch (max) {
+        case min: h = 0; break;
+        case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
+        case g: h = (b - r) + d * 2; h /= 6 * d; break;
+        case b: h = (r - g) + d * 4; h /= 6 * d; break;
+    }
+
+    return {
+        h: h * 360,
+        s: s * 100,
+        v: v * 100
+    };
+}
+
+function gammaCorrectHSV(h, s, v) {
+    return rgbToHSV(gammaCorrectRGB(hsvToRGB(h, s, v)));
+}
+
 
 function hexToXY(hex) {
     const rgb = hexToRgb(hex);
@@ -134,3 +202,5 @@ module.exports = {
     convertMultiByteNumberPayloadToSingleDecimalNumber,
     convertDecimalValueTo2ByteHexArray,
 };
+
+console.log(gammaCorrectRGB(255,125,0))
