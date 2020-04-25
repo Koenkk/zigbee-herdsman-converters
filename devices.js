23edd6f84a210f2a303eebc55367e5e1168ac7d7
Iain Matchett <iain.matchett@beeksfx.com>
2020-04-25 23:43:22 +0100

    Added support for Hive MOT003 motion Sensor

--- devices.js
+++ devices.js
@@ -2793,6 +2793,27 @@ const devices = [
 
     // Hive
     {
+        zigbeeModel: ['MOT003'],
+        model: 'MOT003',
+        vendor: 'HiveHome.com',
+        description: 'Motion sensor',
+        supports: 'occupancy, temperature, battery, illuminance',
+        fromZigbee: [
+            fz.temperature, fz.ias_occupancy_alarm_1_with_timeout, fz.illuminance, fz.battery, fz.ignore_basic_report,
+        ],
+        toZigbee: [],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(6);
+            await bind(endpoint, coordinatorEndpoint, [
+                'msTemperatureMeasurement', 'genPowerCfg', 'msIlluminanceMeasurement',
+            ]);
+            await configureReporting.temperature(endpoint);
+            await configureReporting.batteryVoltage(endpoint);
+            await configureReporting.illuminance(endpoint);
+        },
+    },
+    {
         zigbeeModel: ['FWBulb01'],
         model: 'HALIGHTDIMWWE27',
         vendor: 'Hive',
