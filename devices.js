567d7e0286cd2edc71d75f152cc474e673a0da72
Tomek Pur <8378332+tomekpur@users.noreply.github.com>
2020-04-28 17:44:48 +0200

    Removed reporting-config (unnecessary addition)
            meta: {configureKey: 1},
            configure: async (device, coordinatorEndpoint) => {
                const endpoint = device.getEndpoint(1);
                await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
                await configureReporting.onOff(endpoint);
            },

--- devices.js
+++ devices.js
@@ -7220,12 +7220,6 @@ const devices = [
         vendor: 'Sunricher',
         description: 'ZigBee LED-controller',
         extend: generic.light_onoff_brightness_colortemp,
-        meta: {configureKey: 1},
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-            await configureReporting.onOff(endpoint);
-        },
     },
     {
         zigbeeModel: ['ZG9101SAC-HP'],
