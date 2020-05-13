f2e50105f9e900ae20142443b80bc15e53c6ec3a
Ryan Kim(True-World) <nukusinji@naver.com>
2020-05-13 22:07:39 +0900

    Update devices.js
    Add Bandi 1 Gang Switch(WhiteLabel in Tuya TS0001)

--- devices.js
+++ devices.js
@@ -1002,6 +1002,7 @@ const devices = [
         whiteLabel: [
             {vendor: 'CR Smart Home', model: 'TS0001'},
             {vendor: 'Lonsonho', model: 'TS0001'},
+            {vendor: 'Bandi', description: '1 Gang Switch', model: 'BDS03G1'},
         ],
         fromZigbee: [fz.on_off],
         toZigbee: [tz.on_off],
