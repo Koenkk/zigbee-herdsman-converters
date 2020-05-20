041c03ea21827140af8c9061adaef5f0bef7249f
Koen Kanters <koenkanters94@gmail.com>
2020-05-20 16:45:19 +0200

    Saturation value 0 - 255. https://github.com/Koenkk/zigbee2mqtt/issues/2038

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -895,7 +895,7 @@ const converters = {
                 action: getProperty(`enhanced_move_to_hue_and_saturation`, msg, model),
                 action_enhanced_hue: msg.data.enhancehue,
                 action_hue: msg.data.enhancehue * 360 / 65536 % 360,
-                action_saturation: msg.data.saturation * 200 / 256 % 100,
+                action_saturation: msg.data.saturation,
                 action_transition_time: msg.data.transtime,
             };
 
