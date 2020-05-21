48d1f098438964127f4daf152c10df9f5ec5dd28
hyj <122722629@qq.com>
2020-05-21 17:51:44 +0800

    add Aqara D1 3 gang smart wall switch( QBKG25LM ) supported 0521

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1774,7 +1774,7 @@ const converters = {
             if ([1, 2, 3, 0, 255].includes(msg.data.presentValue)) {
                 const mapping = {41: 'left', 42: 'center', 43: 'right'};
                 const times = {1: 'single', 2: 'double', 3: 'triple', 0: 'hold', 255: 'release'};
-                const button = mapping[msg.endpoint.ID];
+                const button = mapping[msg.endpoint.ID]; 
                 return {action: `${button}_${times[msg.data.presentValue]}`};
             }
         },
