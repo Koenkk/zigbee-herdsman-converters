e81c35b16dd455be7501b1c22a65f4145ff96f2e
Ac <47967077+ant-car@users.noreply.github.com>
2020-05-31 14:47:12 +1000

    Added TuYa TS0003 and TS0004

--- devices.js
+++ devices.js
@@ -1155,6 +1155,27 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['TS0003'],
+        model: 'TS0003',
+        vendor: 'TuYa',
+        description: '3 gang switch',
+        whiteLabel: [
+            {vendor: 'Tuya', model: 'TS0003'}
+        ],
+        supports: 'on/off',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        endpoint: (device) => {
+            return {'l1': 1, 'l2': 2, 'l3': 3};
+        },
+        meta: {configureKey: 3, multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint) => {
+            await bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
+            await bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+            await bind(device.getEndpoint(3), coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
         zigbeeModel: ['TS0004'],
         model: 'TS0004',
         vendor: 'TuYa',
