ccd63799c68141a341cebd122d776c175d324884
Koen Kanters <koenkanters94@gmail.com>
2020-06-06 09:46:32 +0200

    Update devices.js

--- devices.js
+++ devices.js
@@ -3034,11 +3034,13 @@ const devices = [
         extend: osram.light_onoff_brightness_colortemp,
         ota: ota.ledvance,
     },
+
+    // Ledvance
     {
         zigbeeModel: ['Panel TW Z3'],
         model: '4058075181472',
         vendor: 'LEDVANCE',
-        description: 'SMART+ PANEL 60 x 60cm Tunable White',
+        description: 'SMART+ panel 60 x 60cm tunable white',
         extend: osram.light_onoff_brightness_colortemp,
         ota: ota.ledvance,
     },
