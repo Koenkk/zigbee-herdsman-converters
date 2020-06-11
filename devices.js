477d94cfa5af88abcea15b5d00526f358e41daaf
Jorge Schrauwen <sjorge@blackdot.be>
2020-06-11 17:16:16 +0200

    Report pi_heating_demand on change for ViCare TRV

--- devices.js
+++ devices.js
@@ -10801,9 +10801,9 @@ const devices = [
                 'genBasic', 'genPowerCfg', 'genIdentify', 'genTime', 'genPollCtrl', 'hvacThermostat',
                 'hvacUserInterfaceCfg',
             ]);
-            await configureReporting.thermostatTemperature(endpoint);
+            await configureReporting.thermostatTemperature(endpoint, 0, repInterval.MINUTES_15, 25);
             await configureReporting.thermostatOccupiedHeatingSetpoint(endpoint);
-            await configureReporting.thermostatPIHeatingDemand(endpoint);
+            await configureReporting.thermostatPIHeatingDemand(endpoint, 0, repInterval.MINUTES_5, 1);
         },
     },
 
