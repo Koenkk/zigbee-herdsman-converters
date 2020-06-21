e0115a2517d30cea1bcbd90ba4740187c1f30f8f
ChrisChoke <christoph.schuering@gmail.com>
2020-06-21 21:33:51 +0200

    update viessmann trv integration

--- devices.js
+++ devices.js
@@ -182,6 +182,15 @@ const configureReporting = {
         }];
         await endpoint.configureReporting('seMetering', payload);
     },
+    thermostatControlSequanceOfOperation: async (endpoint, min=0, max=repInterval.HOUR, change=1) => {
+        const payload = [{
+            attribute: 'ctrlSeqeOfOper',
+            minimumReportInterval: min,
+            maximumReportInterval: max,
+            reportableChange: change,
+        }];
+        await endpoint.configureReporting('hvacThermostat', payload);
+    },
     thermostatSystemMode: async (endpoint, min=10, max=repInterval.HOUR) => {
         const payload = [{
             attribute: 'systemMode',
@@ -10949,9 +10958,22 @@ const devices = [
         vendor: 'Viessmann',
         description: 'ViCare radiator thermostat valve',
         supports: 'thermostat',
+<<<<<<< HEAD
         fromZigbee: [fz.viessmann_thermostat_att_report, fz.battery],
         toZigbee: [tz.thermostat_occupied_heating_setpoint, tz.thermostat_local_temperature_calibration],
         meta: {configureKey: 2},
+=======
+        fromZigbee: [fz.viessmann_thermostat_att_report, fz.battery, fz.hvac_user_interface],
+        toZigbee: [
+            tz.thermostat_local_temperature,
+            tz.thermostat_occupied_heating_setpoint,
+            tz.thermostat_occupied_cooling_setpoint,
+            tz.thermostat_control_sequence_of_operation,
+            tz.thermostat_system_mode,
+            tz.thermostat_keypad_lockout,
+        ],
+        meta: {configureKey: 1},
+>>>>>>> update viessmann trv integration
         configure: async (device, coordinatorEndpoint) => {
             const endpoint = device.getEndpoint(1);
             await bind(endpoint, coordinatorEndpoint, [
@@ -10960,7 +10982,13 @@ const devices = [
             ]);
             await configureReporting.thermostatTemperature(endpoint);
             await configureReporting.thermostatOccupiedHeatingSetpoint(endpoint);
+<<<<<<< HEAD
             await configureReporting.thermostatPIHeatingDemand(endpoint, 0, repInterval.MINUTES_5, 1);
+=======
+            await configureReporting.thermostatOccupiedCoolingSetpoint(endpoint);
+            await configureReporting.thermostatPIHeatingDemand(endpoint);
+            await configureReporting.thermostatKeypadLockMode(endpoint);
+>>>>>>> update viessmann trv integration
         },
     },
 
