68ca4812e48b19c709f6415dadb15d6a514450c4
nur <nur.php@gmail.com>
2020-06-25 18:55:17 +0800

    implement addDeviceDefinition method

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function arrayEquals(as, bs) {
     return true;
 }
 
-for (const device of devices) {
+function addToLookupMaps(device) {
     if (device.hasOwnProperty('fingerprint')) {
         withFingerprint.push(device);
     }
@@ -23,6 +23,9 @@ for (const device of devices) {
             byZigbeeModel.set(zigbeeModel.toLowerCase(), device);
         }
     }
+}
+for (const device of devices) {
+    addToLookupMaps(device);
 }
 
 function findByZigbeeModel(model) {
@@ -92,12 +95,19 @@ function fingerprintMatch(fingerprint, device) {
     return match;
 }
 
+
+function addDeviceDefinition(device) {
+    devices.push(device);
+    addToLookupMaps(device);
+}
+
 module.exports = {
     devices,
     findByZigbeeModel,
     findByDevice,
     toZigbeeConverters: toZigbee,
     fromZigbeeConverters: fromZigbee,
+    addDeviceDefinition,
     // Can be used to handle events for devices which are not fully paired yet (no modelID).
     // Example usecase: https://github.com/Koenkk/zigbee2mqtt/issues/2399#issuecomment-570583325
     onEvent: async (type, data, device) => {
