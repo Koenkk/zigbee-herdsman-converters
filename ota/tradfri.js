20c6f9179e49e834fbedf74ecfde1195e0df5084
Felix Storm <felix.storm@glueckkanja.com>
2020-07-09 16:32:19 +0200

    cleanup duplicate OTA code for ledvance.js, salus.js and tradfri.js

--- ota/tradfri.js
+++ ota/tradfri.js
@@ -7,7 +7,8 @@ const axios = common.getAxios();
  * Helper functions
  */
 
-async function getImageMeta(imageType) {
+async function getImageMeta(current, logger, device) {
+    const imageType = current.imageType;
     const images = (await axios.get(url)).data;
     const image = images.find((i) => i.fw_image_type === imageType);
     assert(image !== undefined, `No image available for imageType '${imageType}'`);
@@ -18,38 +19,16 @@ async function getImageMeta(imageType) {
     };
 }
 
-async function getNewImage(current, logger, device) {
-    const meta = await getImageMeta(current.imageType);
-    assert(meta.fileVersion > current.fileVersion, 'No new image available');
-
-    const download = await axios.get(meta.url, {responseType: 'arraybuffer'});
-    const start = download.data.indexOf(common.upgradeFileIdentifier);
-
-    const image = common.parseImage(download.data.slice(start));
-    assert(image.header.fileVersion === meta.fileVersion, 'File version mismatch');
-    assert(image.header.totalImageSize === meta.fileSize, 'Image size mismatch');
-    assert(image.header.manufacturerCode === 4476, 'Manufacturer code mismatch');
-    assert(image.header.imageType === current.imageType, 'Image type mismatch');
-    return image;
-}
-
-async function isNewImageAvailable(current, logger, device) {
-    const meta = await getImageMeta(current.imageType);
-    const [currentS, metaS] = [JSON.stringify(current), JSON.stringify(meta)];
-    logger.debug(`Is new image available for '${device.ieeeAddr}', current '${currentS}', latest meta '${metaS}'`);
-    return Math.sign(current.fileVersion - meta.fileVersion);
-}
-
 /**
  * Interface implementation
  */
 
 async function isUpdateAvailable(device, logger, requestPayload=null) {
-    return common.isUpdateAvailable(device, logger, isNewImageAvailable, requestPayload);
+    return common.isUpdateAvailable(device, logger, common.isNewImageAvailable, requestPayload, getImageMeta);
 }
 
 async function updateToLatest(device, logger, onProgress) {
-    return common.updateToLatest(device, logger, onProgress, getNewImage);
+    return common.updateToLatest(device, logger, onProgress, common.getNewImage, getImageMeta);
 }
 
 module.exports = {
