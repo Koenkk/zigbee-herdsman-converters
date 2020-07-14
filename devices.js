b75d1f471a495f88966bada8e92a24f85597e31f
Koen Kanters <koenkanters94@gmail.com>
2020-07-14 20:18:09 +0200

    Correct acPowerDivisor

--- devices.js
+++ devices.js
@@ -1154,7 +1154,7 @@ const devices = [
                 acCurrentMultiplier: 1,
                 acCurrentDivisor: 1000,
                 acPowerMultiplier: 1,
-                acPowerDivisor: 1000,
+                acPowerDivisor: 1,
             });
         },
         onEvent: async (type, data, device) => {
