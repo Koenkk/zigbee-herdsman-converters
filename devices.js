4e8927b217183da36e4e5ed38586bd684850a9c5
Mark Snelling <mark@bakedbeans.com>
2020-07-16 15:36:18 +0100

    Added support for AOne 250W Smart Rotary Dimmer Module

--- devices.js
+++ devices.js
@@ -11056,6 +11056,13 @@ const devices = [
         fromZigbee: [fz.ias_contact_alarm_1, fz.battery],
         toZigbee: [],
     },
+    {
+        zigbeeModel: ['WallDimmerMaster'],
+        model: 'AU-A1ZB2WDM',
+        vendor: 'Aurora Lighting',
+        description: 'AOne 250W Smart Rotary Dimmer Module',
+        extend: generic.light_onoff_brightness,
+    },
 
     // Wally
     {
