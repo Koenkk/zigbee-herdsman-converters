848c51a4c3e5dc28f511d2c69b2bd579bb0acfcb
Koen Kanters <koenkanters94@gmail.com>
2020-07-29 16:50:40 +0200

    Fix battery converter exception when device has no meta. https://github.com/Koenkk/zigbee2mqtt/issues/4003

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -388,7 +388,7 @@ const converters = {
                 // Deprecated: voltage is = mV now but should be V
                 payload.voltage = msg.data['batteryVoltage'] * 100;
 
-                if (model.meta.hasOwnProperty('battery') && model.meta.battery.hasOwnProperty('voltageToPercentage')) {
+                if (model.meta && model.meta.batery && model.meta.battery.voltageToPercentage) {
                     if (model.meta.battery.voltageToPercentage === 'CR2032') {
                         payload.battery = toPercentageCR2032(payload.voltage);
                     }
