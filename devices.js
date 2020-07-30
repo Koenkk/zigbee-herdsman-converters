3608a57124a6d15b1586916705b9e7abd495883e
arodus <sebastian@karasek.io>
2020-07-30 07:00:50 +0200

    Add support for NLG Q-FRAMELESS

--- devices.js
+++ devices.js
@@ -7205,6 +7205,13 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['NLG-RGB-TW light'],
+        model: '100.470.92',
+        vendor: 'Paul Neuhaus',
+        description: 'Q-FRAMELESS LED-Panel, Smart-Home RGB-TW',
+        extend: generic.light_onoff_brightness_colortemp_colorxy,
+    },
+    {
         zigbeeModel: ['NLG-plug '],
         model: '100.425.90',
         vendor: 'Paul Neuhaus',
