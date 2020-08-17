01429dea83a32cd59e90953da640c3d6d03ffe67
alasota <artur.lasota@gmail.com>
2020-08-17 13:35:38 +0200

    Add support for Blaupunkt PSM-S1 (#1474)
    * Update devices.js
    
    Add support for Blaupunkt Power Switch meter PSM-S1, The same as Climax PSM-29ZBSR
    
    * Revert "Update devices.js"
    
    This reverts commit b8852da0faf660501c5ff997ab63837fc4081053.
    
    * PSM-S1
    
    Add support for blaupunkt Power swith meter PSM-S1
    
    * add whiteLabel info

--- devices.js
+++ devices.js
@@ -6919,7 +6919,7 @@ const devices = [
         toZigbee: [tz.cover_position_via_brightness, tz.cover_open_close_via_brightness],
     },
     {
-        zigbeeModel: ['PSM_00.00.00.35TC', 'PSMP5_00.00.02.02TC', 'PSMP5_00.00.05.10TC'],
+        zigbeeModel: ['PSM_00.00.00.35TC', 'PSMP5_00.00.02.02TC', 'PSMP5_00.00.05.01TC', 'PSMP5_00.00.05.10TC'],
         model: 'PSM-29ZBSR',
         vendor: 'Climax',
         description: 'Power plug',
@@ -6934,6 +6934,9 @@ const devices = [
             await readMeteringPowerConverterAttributes(endpoint);
             await configureReporting.instantaneousDemand(endpoint, {min: 10, change: 2});
         },
+        whiteLabel: [
+            {vendor: 'Blaupunkt', model: 'PSM-S1'},
+        ],
     },
     {
         zigbeeModel: ['RS_00.00.02.06TC'],
