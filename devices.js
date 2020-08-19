2bc173f26500158505b21f7bbc894e14bbc12012
Dvad <Dvad@users.noreply.github.com>
2020-08-19 00:15:59 -0700

    Update devices.js

--- devices.js
+++ devices.js
@@ -4282,7 +4282,15 @@ const devices = [
         extend: generic.light_onoff_brightness_colortemp_colorxy,
         meta: {applyRedFix: true, turnsOffAtBrightness1: true},
     },
-
+    {                                                                                                 
+        zigbeeModel: ['BE 220'],                                                                           
+        model: 'BE 220',                                                                                                
+        vendor: 'Innr',                                                                                                
+        description: 'E26/E24 white bulb',                                                                         
+        extend: generic.light_onoff_brightness,                                                                   
+        meta: {applyRedFix: true, turnsOffAtBrightness1: true},                                                    
+    },
+    
     // Digi
     {
         fingerprint: [
