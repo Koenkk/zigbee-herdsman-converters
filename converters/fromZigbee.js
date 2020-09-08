8b63b467f360013ba610f6f7f13a1f3b1a72ae5c
Koen Kanters <koenkanters94@gmail.com>
2020-09-08 19:27:11 +0200

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -2540,26 +2540,26 @@ const converters = {
             }
         },
     },
-    xiaomi_power_from_basic: {                                                     
-        cluster: 'genBasic',                                                       
-        type: ['attributeReport', 'readResponse'],                                 
-        convert: (model, msg, publish, options, meta) => {                         
-            if (msg.data['65281']) {                                               
-                const data = msg.data['65281'];                                    
-                const result = {};                                                 
-                if (data['152']) {                                                 
-                    result.power = precisionRound(data['152'], 2);                 
-                }                                                                  
-                if (data['149']) {                                                 
-                    result.consumption = precisionRound(data['149'], 2);           
-                }                                                                  
-                if (data['3']) {                                                   
+    xiaomi_power_from_basic: {
+        cluster: 'genBasic',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            if (msg.data['65281']) {
+                const data = msg.data['65281'];
+                const result = {};
+                if (data['152']) {
+                    result.power = precisionRound(data['152'], 2);
+                }
+                if (data['149']) {
+                    result.consumption = precisionRound(data['149'], 2);
+                }
+                if (data['3']) {
                     result.temperature = calibrateAndPrecisionRoundOptions(data['3'], options, 'temperature');
-                }                                                                  
-                return result;                                                     
-            }                                                                      
-        },                                                                         
-    },   
+                }
+                return result;
+            }
+        },
+    },
     QBKG04LM_buttons: {
         cluster: 'genOnOff',
         type: ['attributeReport', 'readResponse'],
