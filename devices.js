afc740cf08758e150d84541c2a0f107aba601bee
RealPy <manuel.barraud@me.com>
2020-09-30 14:05:20 +0200

    Correct syntax function

--- devices.js
+++ devices.js
@@ -8309,7 +8309,7 @@ const devices = [
         supports: 'on/off, brightness up/down and click/hold/release',
         fromZigbee: [
             fz.command_on, fz.command_off, fz.command_step, fz.command_move,
-            fz.commandstop, fz.command_recall, fz.ignore_basic_report,
+            fz.command_stop, fz.command_recall, fz.ignore_basic_report,
         ],
         toZigbee: [],
         meta: {configureKey: 1},
