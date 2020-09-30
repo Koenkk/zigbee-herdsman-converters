2c6a813cd2ac6aa985740941db0dea344c7f7e6a
Koen Kanters <koenkanters94@gmail.com>
2020-09-30 19:23:35 +0200

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -6097,17 +6097,16 @@ const converters = {
             }
         },
     },
-    // Zemismart curtain switch - Garage controller
-    zemismart_curtain: {
+    TS0003_curtain_switch: {
         cluster: 'genOnOff',
         type: ['attributeReport', 'readResponse'],
         convert: (model, msg, publish, options, meta) => {
             if (msg.data.hasOwnProperty('onOff')) {
-                // const property = postfixWithEndpointName('state', msg, model);
                 return {'running': msg.data['onOff'] === 1 ? true : false};
             }
         },
     },
+
     // Ignore converters (these message dont need parsing).
     ignore_onoff_report: {
         cluster: 'genOnOff',
