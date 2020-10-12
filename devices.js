8d548e3ccffbf2e126ebf1d93b28a9c0a2e414fc
Koen Kanters <koenkanters94@gmail.com>
2020-10-12 18:13:07 +0200

    Add alternative zigbeeModel for ZG2835RAC. https://github.com/Koenkk/zigbee-herdsman-converters/issues/1653

--- devices.js
+++ devices.js
@@ -10375,7 +10375,7 @@ const devices = [
         },
     },
     {
-        zigbeeModel: ['Micro Smart Dimmer'],
+        zigbeeModel: ['Micro Smart Dimmer', 'SM311'],
         model: 'ZG2835RAC',
         vendor: 'Sunricher',
         description: 'ZigBee knob smart dimmer',
