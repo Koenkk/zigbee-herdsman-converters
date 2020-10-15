2c8dadfd7740d77b3fb20d393c0950462cf00b6d
mvjt <34924009+mvjt@users.noreply.github.com>
2020-10-15 16:38:57 +0200

    Add Xiaomi SSM-U01 (#1663)
    * Adding Xiaomi SSM-U01
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -1274,6 +1274,24 @@ const devices = [
         description: 'Aqara zigbee LED-controller ',
         extend: generic.light_onoff_brightness,
     },
+    {
+        zigbeeModel: ['lumi.switch.n0agl1'],
+        model: 'SSM-U01',
+        vendor: 'Xiaomi',
+        description: 'Aqara single switch module T1 (with neutral)',
+        supports: 'on/off, power measurement',
+        fromZigbee: [fz.on_off, fz.metering_power],
+        exposes: [exposes.switch(), exposes.numeric('energy').withUnit('kWh')],
+        toZigbee: [tz.on_off],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'seMetering']);
+            await configureReporting.onOff(endpoint);
+            await readMeteringPowerConverterAttributes(endpoint);
+            await configureReporting.currentSummDelivered(endpoint);
+        },
+    },
 
     // TuYa
     {
