841973f2c4611344ce349d4bd0daa41f2eb8d2a3
EmbeddedSwGuy <github@flook.eu>
2020-10-17 20:45:12 +0200

    Add Hornbach Flair viyu Smart LED E14 candle

--- devices.js
+++ devices.js
@@ -13350,6 +13350,13 @@ const devices = [
         extend: generic.light_onoff_brightness_colortemp_colorxy,
     },
 
+    {
+        zigbeeModel: ['VIYU-C35-470-CCT-10011722'],
+        model: '10011722',
+        vendor: 'HORNBACH',
+        description: 'FLAIR Viyu Smarte LED candle E14',
+        extend: generic.light_onoff_brightness_colortemp,
+    },
     // LifeControl
     {
         zigbeeModel: ['Leak_Sensor'],
