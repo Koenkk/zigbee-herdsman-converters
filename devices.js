a668cb210bf9fa79ecf21ce496c06e0ea7ad66b7
Kevin Bowman <kbowman@newedenconsultinggroup.com>
2020-10-23 11:58:09 -0500

    Adding support to devices.js for Xfinity XHS2-UE (#1689)
    * Adding support to devices.js for Xfinity XHS2-UE
    
    * Updating XHS2-UE with correct manufacturer name
    
    * Moving XHS2-UE to a more logical place in devices.js
    
    * CI failing - no idea why
    
    * Update devices.js
    
    Co-authored-by: Kevin Bowman <kbowman@kauffman.org>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -10968,6 +10968,25 @@ const devices = [
         exposes: [e.occupancy(), e.battery_low(), e.tamper(), e.battery()],
     },
 
+    // Universal Electronics Inc
+    {
+        zigbeeModel: ['URC4460BC0-X-R'],
+        model: 'XHS2-UE',
+        vendor: 'Universal Electronics Inc',
+        description: 'Magnetic door & window contact sensor',
+        supports: 'contact, temperature',
+        fromZigbee: [fz.ias_contact_alarm_1, fz.temperature, fz.battery],
+        toZigbee: [],
+        meta: {configureKey: 1, battery: {voltageToPercentage: '3V_2100'}},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['msTemperatureMeasurement', 'genPowerCfg']);
+            await configureReporting.temperature(endpoint);
+            await configureReporting.batteryPercentageRemaining(endpoint);
+        },
+        exposes: [e.contact(), e.battery_low(), e.tamper(), e.temperature(), e.battery()],
+    },
+
     // Leedarson
     {
         zigbeeModel: ['LED_GU10_OWDT'],
