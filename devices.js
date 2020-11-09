a07bb2047452bfad87ece6b25134297115288bb2
Joshua Schnabel <github@joshua-schnabel.de>
2020-11-09 17:46:48 +0100

    Adding Ikea E1812 (#1739)
    * Add E1812
    
    * Update devices.js
    
    Fix spacing
    
    * Update devices.js
    
    Fix spacing
    
    * Update devices.js
    
    Fix Lint errors
    
    * Added OTA + Removed legacy fromZigbee
    
    * Added OTA + Removed legacy fromZigbee
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -2670,6 +2670,26 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['TRADFRI SHORTCUT Button'],
+        model: 'E1812',
+        vendor: 'IKEA',
+        description: 'TRADFRI shortcut button',
+        supports: 'action',
+        fromZigbee: [fz.command_on, fz.command_move, fz.command_stop, fz.battery],
+        exposes: [e.battery(), e.action(['on', 'brightness_move_up', 'brightness_stop'])],
+        toZigbee: [],
+        ota: ota.tradfri,
+        meta: {configureKey: 1, disableActionGroup: true, battery: {dontDividePercentage: true}},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            // By default this device controls group 0, some devices are by default in
+            // group 0 causing the remote to control them.
+            // By binding it to a random group, e.g. 901, it will send the commands to group 901 instead of 0
+            await bind(endpoint, defaultBindGroup, ['genPowerCfg']);
+            await configureReporting.batteryPercentageRemaining(endpoint);
+        },
+    },
+    {
         zigbeeModel: ['SYMFONISK Sound Controller'],
         model: 'E1744',
         vendor: 'IKEA',
