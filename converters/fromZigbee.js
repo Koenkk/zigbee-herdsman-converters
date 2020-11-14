82a2763d377453385e675dedea9139a3821414a5
ae <ae@Flatscreen.lan>
2020-11-14 16:14:05 +0100

    linter
    Linter error removal

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -3880,47 +3880,47 @@ const converters = {
         },
     },
     danfoss_thermostat_att_report: {
-      cluster: 'hvacThermostat',
-      type: ['attributeReport', 'readResponse'],
-      convert: (model, msg, publish, options, meta) => {
-          const result = {};
-          // Danfoss sends pi_heating_demand as raw %
-          if (typeof msg.data['pIHeatingDemand'] == 'number') {
-              result[postfixWithEndpointName('pi_heating_demand', msg, model)] =
-                  precisionRound(msg.data['pIHeatingDemand'], 0);
-          }
-          if (typeof msg.data[0x4000] == 'number') {
-              result[postfixWithEndpointName('window_open_internal', msg, model)] = (msg.data[0x4000]);
-          }
-          if (typeof msg.data[0x4003] == 'number') {
-              result[postfixWithEndpointName('window_open_external', msg, model)] = (msg.data[0x4003] == 0x01);
-          }
-          if (typeof msg.data[0x4010] == 'number') {
-              result[postfixWithEndpointName('day_of_week', msg, model)] = msg.data[0x4010];
-          }
-          if (typeof msg.data[0x4011] == 'number') {
-              result[postfixWithEndpointName('trigger_time', msg, model)] = msg.data[0x4011];
-          }
-          if (typeof msg.data[0x4012] == 'number') {
-              result[postfixWithEndpointName('mounted_mode', msg, model)] = (msg.data[0x4012]==1);
-          }
-          if (typeof msg.data[0x4013] == 'number') {
-              result[postfixWithEndpointName('mounted_mode_control', msg, model)] = (msg.data[0x4013]==0x00);
-          }
-          if (typeof msg.data[0x4014] == 'number') {
-              result[postfixWithEndpointName('thermostat_orientation', msg, model)] = msg.data[0x4014];
-          }
-          if (typeof msg.data[0x4020] == 'number') {
-              result[postfixWithEndpointName('algorithm_scale_factor', msg, model)] = msg.data[0x4020];
-          }
-          if (typeof msg.data[0x4030] == 'number') {
-              result[postfixWithEndpointName('heat_available', msg, model)] = (msg.data[0x4030]==0x01);
-          }
-          if (typeof msg.data[0x4031] == 'number') {
-              result[postfixWithEndpointName('heat_required', msg, model)] = (msg.data[0x4031]==0x01);
-          }
-          return result;
-       },
+        cluster: 'hvacThermostat',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            // Danfoss sends pi_heating_demand as raw %
+            if (typeof msg.data['pIHeatingDemand'] == 'number') {
+                result[postfixWithEndpointName('pi_heating_demand', msg, model)] =
+                    precisionRound(msg.data['pIHeatingDemand'], 0);
+            }
+            if (typeof msg.data[0x4000] == 'number') {
+                result[postfixWithEndpointName('window_open_internal', msg, model)] = (msg.data[0x4000]);
+            }
+            if (typeof msg.data[0x4003] == 'number') {
+                result[postfixWithEndpointName('window_open_external', msg, model)] = (msg.data[0x4003] == 0x01);
+            }
+            if (typeof msg.data[0x4010] == 'number') {
+                result[postfixWithEndpointName('day_of_week', msg, model)] = msg.data[0x4010];
+            }
+            if (typeof msg.data[0x4011] == 'number') {
+                result[postfixWithEndpointName('trigger_time', msg, model)] = msg.data[0x4011];
+            }
+            if (typeof msg.data[0x4012] == 'number') {
+                result[postfixWithEndpointName('mounted_mode', msg, model)] = (msg.data[0x4012]==1);
+            }
+            if (typeof msg.data[0x4013] == 'number') {
+                result[postfixWithEndpointName('mounted_mode_control', msg, model)] = (msg.data[0x4013]==0x00);
+            }
+            if (typeof msg.data[0x4014] == 'number') {
+                result[postfixWithEndpointName('thermostat_orientation', msg, model)] = msg.data[0x4014];
+            }
+            if (typeof msg.data[0x4020] == 'number') {
+                result[postfixWithEndpointName('algorithm_scale_factor', msg, model)] = msg.data[0x4020];
+            }
+            if (typeof msg.data[0x4030] == 'number') {
+                result[postfixWithEndpointName('heat_available', msg, model)] = (msg.data[0x4030]==0x01);
+            }
+            if (typeof msg.data[0x4031] == 'number') {
+                result[postfixWithEndpointName('heat_required', msg, model)] = (msg.data[0x4031]==0x01);
+            }
+            return result;
+         },
     },
 
     eurotronic_thermostat: {
