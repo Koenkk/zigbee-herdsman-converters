0c7855f0701a0ae040502204619052d6e7f50fc4
Caizzii <contact@caizzii.com>
2020-11-16 14:04:32 +0100

    Trailing space removed

--- devices.js
+++ devices.js
@@ -9041,22 +9041,22 @@ const devices = [
             await endpoint.read('genPowerCfg', ['batteryPercentageRemaining']);
         },
     },
-    {                                   
+    {
         fingerprint: [{modelID: 'RC-EM', manufacturerName: 'HEIMAN'}],
-        model: 'HS1RC-EM',                                       
-        vendor: 'HEIMAN',                                                        
+        model: 'HS1RC-EM',
+        vendor: 'HEIMAN',
         description: 'Smart remote controller',
-        supports: 'action',    
+        supports: 'action',
         fromZigbee: [fz.battery, fz.heiman_smart_controller_armmode, fz.command_emergency],
-        toZigbee: [],                         
+        toZigbee: [],
         exposes: [e.battery(), e.action(['emergency', 'disarm', 'arm_partial_zones', 'arm_all_zones'])],
-        meta: {configureKey: 1},                       
+        meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);                               
+            const endpoint = device.getEndpoint(1);
             await bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
             await configureReporting.batteryPercentageRemaining(endpoint, {min: repInterval.MINUTES_5, max: repInterval.HOUR});
             await endpoint.read('genPowerCfg', ['batteryPercentageRemaining']);
-        },                                                                
+        },
     },
     {
         zigbeeModel: ['COSensor-EM', 'COSensor-N'],
