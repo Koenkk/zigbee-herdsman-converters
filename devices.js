50368b9d464e2f3de5b1b22e4db95b7465118e03
xyzroe <me@kogan.in.ua>
2020-11-16 18:29:23 +0200

    Added converter for LeTV 8key FreePad mod
    [LeTV 8key FreePad mod](https://modkam.ru/?p=1791)

--- devices.js
+++ devices.js
@@ -4440,6 +4440,55 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['FreePad_LeTV_8'],
+        model: 'FreePad_LeTV_8',
+        vendor: 'DIYRuZ',
+        description: '[LeTV 8key FreePad mod](https://modkam.ru/?p=1791)',
+        supports: 'single, double, triple, quadruple, many, hold/release',
+        fromZigbee: [fz.diyruz_freepad_clicks, fz.diyruz_freepad_config, fz.battery],
+        exposes: [e.battery(), e.action(['*_single', '*_double', '*_triple', '*_quadruple', '*_release'])].concat(((enpoinsCount) => {
+            const features = [];
+            for (let i = 1; i <= enpoinsCount; i++) {
+                const epName = `button_${i}`;
+                features.push(
+                    exposes.enum('switch_type', exposes.access.ALL, ['toggle', 'momentary', 'multifunction']).withEndpoint(epName));
+                features.push(exposes.enum('switch_actions', exposes.access.ALL, ['on', 'off', 'toggle']).withEndpoint(epName));
+            }
+            return features;
+        })(8)),
+        toZigbee: [tz.diyruz_freepad_on_off_config, tz.factory_reset],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            if (device.applicationVersion < 3) { // Legacy PM2 firmwares
+                const payload = [{
+                    attribute: 'batteryPercentageRemaining',
+                    minimumReportInterval: 0,
+                    maximumReportInterval: 3600,
+                    reportableChange: 0,
+                }, {
+                    attribute: 'batteryVoltage',
+                    minimumReportInterval: 0,
+                    maximumReportInterval: 3600,
+                    reportableChange: 0,
+                }];
+                await endpoint.configureReporting('genPowerCfg', payload);
+            }
+            device.endpoints.forEach(async (ep) => {
+                if (ep.outputClusters.includes(18)) {
+                    await bind(ep, coordinatorEndpoint, ['genMultistateInput']);
+                }
+            });
+        },
+        endpoint: (device) => {
+            return {
+                button_1: 1, button_2: 2, button_3: 3, button_4: 4, button_5: 5,
+                button_6: 6, button_7: 7, button_8: 8
+            };
+        },
+    },
+    {
         zigbeeModel: ['DIYRuZ_Geiger'],
         model: 'DIYRuZ_Geiger',
         vendor: 'DIYRuZ',
