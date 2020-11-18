898fe0304bd5ec3910e23d9dea292098c5b1ef48
arteck <6681528+arteck@users.noreply.github.com>
2020-11-18 14:06:55 +0100

    Alternative zigbeeModel for 4058075816459 (#1776)

--- devices.js
+++ devices.js
@@ -4995,7 +4995,7 @@ const devices = [
         },
     },
     {
-        zigbeeModel: ['Switch 4x EU-LIGHTIFY', 'Switch 4x-LIGHTIFY'],
+        zigbeeModel: ['Switch 4x EU-LIGHTIFY', 'Switch 4x-LIGHTIFY', 'Switch-LIGHTIFY'],
         model: '4058075816459',
         vendor: 'OSRAM',
         description: 'Smart+ switch',
