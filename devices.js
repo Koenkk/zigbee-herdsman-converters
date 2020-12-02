605c0c470cf969c84bbb8a205c56563c5ae5750c
cyber289 <53240706+cyber289@users.noreply.github.com>
2020-12-02 17:39:53 -0500

    Update devices.js

--- devices.js
+++ devices.js
@@ -10557,12 +10557,7 @@ const devices = [
         supports: 'on/off',
         fromZigbee: [fz.on_off],
         toZigbee: [tz.on_off],
-        meta: {configureKey: 1},
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            await bind(endpoint, coordinatorEndpoint, ['genOnOff']);
-            await configureReporting.onOff(endpoint);
-        },
+        meta: {configureKey: 1}
     },
     {
         zigbeeModel: ['DM2500ZB'],
