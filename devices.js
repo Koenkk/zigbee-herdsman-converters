6edf5d79d59449c2bfab84c071e8bf79521535e7
Koen Kanters <koenkanters94@gmail.com>
2020-12-08 20:02:37 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -1044,7 +1044,7 @@ const devices = [
         toZigbee: [tz.on_off, tz.xiaomi_power, tz.xiaomi_switch_power_outage_memory, tz.xiaomi_auto_off, tz.xiaomi_led_disabled_night],
         exposes: [
             e.switch(), e.power(), e.energy(), e.temperature(), e.voltage(), e.current(), e.auto_off(),
-            e.consumer_connected(), e.consumer_overload(), e.led_disabled_night(), e.power_outage_memory()
+            e.consumer_connected(), e.consumer_overload(), e.led_disabled_night(), e.power_outage_memory(),
         ],
     },
     {
