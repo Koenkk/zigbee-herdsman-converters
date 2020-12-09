64c9b5c5330f6a6810d3e17c656ebe0ccf87f3ef
Kike Herrera <kike.herrera@gmail.com>
2020-12-09 18:46:34 +0100

    Support for Useelink SM-SO306EZ-10 (#1882)
    * Update devices.js
    
    Add this device
    https://es.aliexpress.com/item/4000981707898.html
    It's the same model as the Useelink SM-SO306E but the usb enpoint changes from 7 to 5 (7 does not work).
    
    * Update devices.js
    
    * Update devices.js
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -2186,6 +2186,28 @@ const devices = [
         ],
     },
 
+    // UseeLink
+    {
+        fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_o005nuxx'}],
+        model: 'SM-SO306EZ-10',
+        vendor: 'UseeLink',
+        description: '4 gang switch, with USB',
+        exposes: [
+            e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2'), e.switch().withEndpoint('l3'),
+            e.switch().withEndpoint('l4'), e.switch().withEndpoint('l5'),
+        ],
+        extend: generic.switch,
+        meta: {configureKey: 1, multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint) => {
+            for (const ID of [1, 2, 3, 4, 5]) {
+                await bind(device.getEndpoint(ID), coordinatorEndpoint, ['genOnOff']);
+            }
+        },
+        endpoint: (device) => {
+            return {'l1': 1, 'l2': 2, 'l3': 3, 'l4': 4, 'l5': 5};
+        },
+    },
+
     // Neo
     {
         zigbeeModel: ['0yu2xgi'],
