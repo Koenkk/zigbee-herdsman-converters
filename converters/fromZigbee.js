63fe50197e16fe4889395c31dafb1eb3ba882531
Koen Kanters <koenkanters94@gmail.com>
2020-12-16 08:37:18 +0100

    Fix xiaomi_on_off_action

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -3110,8 +3110,9 @@ const converters = {
             if (['QBKG03LM', 'QBKG12LM', 'QBKG22LM'].includes(model.model)) mapping = {4: 'left', 5: 'right', 6: 'both'};
             if (['WXKG02LM', 'WXKG07LM'].includes(model.model)) mapping = {1: 'left', 2: 'right', 3: 'both'};
 
-            const actionLookup = isLegacyEnabled(options) && !['QBKG03LM', 'QBKG22LM'].includes(model.model) ?
-                {0: 'single', 1: 'single'} : {0: 'hold', 1: 'release'};
+            // Maybe other QKBG also support release/hold? Confirmed that release/hold doesn't work on WXKG02LM
+            const actionLookup = !isLegacyEnabled(options) && ['QBKG03LM', 'QBKG22LM'].includes(model.model) ?
+                {0: 'hold', 1: 'release'} : {0: 'single', 1: 'single'};
 
             // Dont' use postfixWithEndpointName here, endpoints don't match
             if (mapping) {
