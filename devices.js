18e53b8dad97096ff7159889020313a50467c25d
Jan P. Schümann <33527753+Schuemi@users.noreply.github.com>
2020-12-20 13:19:03 +0100

    Support for Revolt Thermostatic Radiator NX-4911
    It seems to be a white labled GS361A-H04 from Siterwell

--- devices.js
+++ devices.js
@@ -15127,8 +15127,8 @@ const devices = [
 
     // Siterwell
     {
-        zigbeeModel: ['ivfvd7h', 'eaxp72v\u0000'],
-        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_zivfvd7h'}],
+        zigbeeModel: ['ivfvd7h', 'eaxp72v\u0000','kfvq6avy\u0000'],
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_zivfvd7h'}, {modelId: 'TS0601', manufacturerName: '_TZE200_kfvq6avy'}],
         model: 'GS361A-H04',
         vendor: 'Siterwell',
         description: 'Radiator valve with thermostat',
@@ -15153,6 +15153,7 @@ const devices = [
         whiteLabel: [
             {vendor: 'Essentials', description: 'Smart home heizkörperthermostat premium', model: '120112'},
             {vendor: 'Tuya', description: 'Głowica termostatyczna', model: 'GTZ02'},
+            {vendor: 'Revolt', description: 'Thermostatic Radiator Valve Controller', model: 'NX-4911'},
         ],
         exposes: [
             e.child_lock(), e.window_detection(), e.battery(), e.valve_detection(), e.position(),
