12725a1e779a1beeee9b22f84ee5def48ade8192
Jan P. Schümann <33527753+Schuemi@users.noreply.github.com>
2020-12-20 13:30:32 +0100

    Support for Revolt Thermostatic Radiator NX-4911
    It seems to be a white labled GS361A-H04 from Siterwell
    (corrected typo)

--- devices.js
+++ devices.js
@@ -15127,7 +15127,7 @@ const devices = [
 
     // Siterwell
     {
-        zigbeeModel: ['ivfvd7h', 'eaxp72v\u0000','kfvq6avy\u0000'],
+        zigbeeModel: ['ivfvd7h', 'eaxp72v\u0000', 'kfvq6avy\u0000'],
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_zivfvd7h'}, {modelId: 'TS0601', manufacturerName: '_TZE200_kfvq6avy'}],
         model: 'GS361A-H04',
         vendor: 'Siterwell',
