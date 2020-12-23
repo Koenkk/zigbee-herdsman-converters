507b7b22060614936ca2a58f8e8d4132b5406513
graystevens <retrostyle@gmail.com>
2020-12-23 13:05:54 +0000

    Add Jiawen LED strip controller (FB56-ZBW02KU1.5)

--- devices.js
+++ devices.js
@@ -5735,6 +5735,13 @@ const devices = [
         description: 'Wireless Bulb E27 9W RGBW',
         extend: preset.light_onoff_brightness_colortemp_colorxy,
     },
+    {
+        zigbeeModel: ['FB56-ZBW02KU1.5'],
+        model: 'JW-A04-CT',
+        vendor: 'JIAWEN',
+        description: 'LED strip light controller',
+        extend: preset.light_onoff_brightness,
+    },
 
     // Netvox
     {
