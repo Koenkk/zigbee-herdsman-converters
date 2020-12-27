3342f1c913343d5317b345e0d82c390f3e313797
mgrom <marcin.grom@gmail.com>
2020-12-27 17:34:15 +0100

    fix

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -3119,6 +3119,23 @@ const converters = {
             }
         },
     },
+    tuya_thermostat_force_to_mode: {
+        key: ['system_mode'],
+        convertSet: async (entity, key, value, meta) => {
+            // const modeId = utils.getKey(tuya.thermostatForceMode, value, null, Number);
+            // if (modeId !== null) {
+            //     await tuya.sendDataPointEnum(entity, tuya.dataPoints.forceMode, parseInt(modeId));
+            // } else {
+            //     throw new Error(`TRV force mode ${value} is not recognized.`);
+            // }
+            const modeId = utils.getKey(utils.getMetaValue(entity, meta.mapped, 'tuyaThermostatSystemMode'), value, null, Number);
+            if (modeId !== null) {
+                await tuya.sendDataPointEnum(entity, tuya.dataPoints.mode, parseInt(modeId));
+            } else {
+                throw new Error(`TRV system mode ${value} is not recognized.`);
+            }
+        },
+    },
     tuya_thermostat_away_preset: {
         key: ['away_preset_temperature', 'away_preset_days'],
         convertSet: async (entity, key, value, meta) => {
