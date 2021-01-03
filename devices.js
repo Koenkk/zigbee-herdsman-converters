4657eec5eb4b369f85f4164f51d25f45a3e79792
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2021-01-03 20:17:15 +0100

    device: Added support for Calex smart wall switch (#2015)
    * device: Added support for Calex smart wall switch
    
    No battery level just yet.
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -9104,6 +9104,22 @@ const devices = [
         description: 'LED A60 Zigbee RGB lamp',
         extend: preset.light_onoff_brightness_colortemp_colorxy,
     },
+    {
+        zigbeeModel: ['Smart Wall Switch '], // Yes, it has a space at the end :(
+        model: '421782',
+        vendor: 'Calex',
+        description: 'Smart Wall Switch, wall mounted RGB controller',
+        toZigbee: [],
+        fromZigbee: [fz.command_off, fz.command_on, fz.command_step, fz.command_move_to_color_temp,
+            fz.command_move, fz.command_stop, fz.command_ehanced_move_to_hue_and_saturation,
+        ],
+        exposes: [e.action([
+            'on', 'off', 'color_temperature_move', 'brightness_step_up', 'brightness_step_down',
+            'brightness_move_up', 'brightness_move_down', 'brightness_stop',
+            'enhanced_move_to_hue_and_saturation',
+        ])],
+        meta: {configureKey: 1, disableActionGroup: true},
+    },
 
     // EcoSmart
     {
