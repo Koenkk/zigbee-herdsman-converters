dd213becae6f9fa5064e8b591f4e7d1a3eafbb02
Koen Kanters <koenkanters94@gmail.com>
2021-01-08 21:28:22 +0100

    Update 404002 action. https://github.com/Koenkk/zigbee2mqtt.io/pull/553/files

--- devices.js
+++ devices.js
@@ -9644,7 +9644,7 @@ const devices = [
         vendor: 'Müller Licht',
         fromZigbee: [fz.command_on, fz.command_off, fz.command_step, fz.command_move, fz.command_stop, fz.command_recall],
         exposes: [e.action(['on', 'off', 'brightness_step_up', 'brightness_step_down', 'brightness_move_up', 'brightness_move_down',
-            'brightness_stop', 'recall_*'])],
+            'brightness_stop', 'recall_1'])],
         toZigbee: [],
         meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint, logger) => {
