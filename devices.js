3966b57054c79ce4fdff4b71ab00ab6b026867b1
ae <ae@Flatscreen.lan>
2021-01-09 14:07:46 +0100

    fix from previous merge

--- devices.js
+++ devices.js
@@ -85,15 +85,6 @@ const preset = {
             tz.light_hue_saturation_move, tz.light_hue_saturation_step,
         ],
     },
-    light_onoff_brightness_color: {
-        exposes: [e.light_brightness_color(), e.effect()],
-        fromZigbee: [fz.color_colortemp, fz.on_off, fz.brightness, fz.ignore_basic_report],
-        toZigbee: [
-            tz.light_onoff_brightness, tz.light_color, tz.ignore_transition, tz.ignore_rate, tz.effect,
-            tz.light_brightness_move, tz.light_brightness_step,
-            tz.light_hue_saturation_move, tz.light_hue_saturation_step,
-        ],
-    },
     light_onoff_brightness_colorxy: {
         exposes: [e.light_brightness_colorxy(), e.effect()],
         fromZigbee: [fz.color_colortemp, fz.on_off, fz.brightness, fz.ignore_basic_report],
@@ -125,15 +116,6 @@ const preset = {
                 await light.readColorTempMinMax(endpoint);
             }
         },
-    },
-    light_onoff_brightness_colortemp_color: {
-        exposes: [e.light_brightness_colortemp_color(), e.effect()],
-        fromZigbee: [fz.color_colortemp, fz.on_off, fz.brightness, fz.ignore_basic_report],
-        toZigbee: [
-            tz.light_onoff_brightness, tz.light_color_colortemp, tz.ignore_transition, tz.ignore_rate,
-            tz.effect, tz.light_brightness_move, tz.light_colortemp_move, tz.light_brightness_step,
-            tz.light_colortemp_step, tz.light_hue_saturation_move, tz.light_hue_saturation_step,
-        ],
     },
     light_onoff_brightness_colortemp_colorxy: {
         exposes: [e.light_brightness_colortemp_colorxy(), e.effect()],
