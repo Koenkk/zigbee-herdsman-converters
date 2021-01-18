c076efb7888f2274c8a36121ecbacda3bec1821a
Schmakus <schmakus@gmail.com>
2021-01-18 11:38:56 +0100

    Update devices.js
    changed model and description of Paulmann RGBW Reflektor 500.63

--- devices.js
+++ devices.js
@@ -7944,9 +7944,9 @@ const devices = [
     },
     {
         zigbeeModel: ['RGBW light', '500.49'],
-        model: '50049',
+        model: '500.63',
         vendor: 'Paulmann',
-        description: 'SmartHome Yourled RGB Controller',
+        description: 'Smart Home Zigbee LED Reflektor 3,5W GU10 RGBW dimmbar',
         extend: preset.light_onoff_brightness_colortemp_colorxy(),
     },
     {
