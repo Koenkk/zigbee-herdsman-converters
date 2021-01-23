600ace2ca9a518964246258c78ea2f7de6689585
Michael Barlock <barlock@users.noreply.github.com>
2021-01-23 09:24:38 -0500

    Add support for yookee cover (#2131)

--- devices.js
+++ devices.js
@@ -13144,6 +13144,24 @@ const devices = [
         exposes: [e.cover_position(), e.battery()],
     },
 
+    // Yookee
+    {
+        zigbeeModel: ['D10110'],
+        model: 'D10110',
+        vendor: 'Yookee',
+        description: 'Smart blind controller',
+        fromZigbee: [fz.cover_position_tilt, fz.battery],
+        toZigbee: [tz.cover_state, tz.cover_position_tilt],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.currentPositionLiftPercentage(endpoint);
+        },
+        exposes: [e.cover_position(), e.battery()],
+    },
+
     // SONOFF
     {
         zigbeeModel: ['BASICZBR3'],
