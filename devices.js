46e31c3d7206870fa2984fd84564ac53faed6355
Kamil Trzciński <ayufan@ayufan.eu>
2021-01-23 14:56:44 +0100

    Configure away preset temperature for Tuya (#2127)

--- devices.js
+++ devices.js
@@ -1521,7 +1521,7 @@ const devices = [
                 .withLocalTemperatureCalibration(ea.STATE_SET)
                 .withAwayMode().withPreset(['schedule', 'manual', 'boost', 'complex', 'comfort', 'eco']),
             e.auto_lock(), e.away_mode(), e.away_preset_days(), e.boost_time(), e.comfort_temperature(), e.eco_temperature(), e.force(),
-            e.max_temperature(), e.min_temperature(), e.week()],
+            e.max_temperature(), e.min_temperature(), e.week(), e.away_preset_temperature()],
     },
     {
         fingerprint: [{modelID: 'v90ladg\u0000', manufacturerName: '_TYST11_wv90ladg'}],
