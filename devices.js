28fcdb5d89427e60a494463c32187a8a539e4b74
Koen Kanters <koenkanters94@gmail.com>
2021-01-25 19:25:05 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -8481,40 +8481,6 @@ const devices = [
         },
     },
     {
-        zigbeeModel: ['URC4450BC0-X-R'],
-        model: 'URC4450BC0-X-R',
-        vendor: 'Xfinity',
-        description: 'Alarm Security Keypad',
-        supports: 'action, arm',
-        meta: {configureKey: 1, battery: {voltageToPercentage: '3V_2100'}},
-        fromZigbee: [fz.command_arm, fz.temperature, fz.battery, fz.ias_occupancy_alarm_1, fz.identify, fz.ias_contact_alarm_1,
-            fz.ias_occupancy_alarm_2, fz.ias_occupancy_alarm_1_with_timeout],
-        exposes: [e.battery(), e.voltage(), e.occupancy(), e.battery_low(), e.tamper(), e.presence(), e.contact(),
-            exposes.numeric('action_code', ea.STATE), exposes.text('action_zone', ea.STATE), e.temperature(), e.action([
-                'disarm', 'arm_day_zones', 'identify', 'arm_night_zones', 'arm_all_zones', 'invalid_code', 'emergency',
-            ])],
-        toZigbee: [tz.arm_mode],
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            const clusters = ['msTemperatureMeasurement', 'genPowerCfg', 'ssIasZone', 'ssIasAce', 'genBasic', 'genIdentify'];
-            await reporting.bind(endpoint, coordinatorEndpoint, clusters);
-            await reporting.temperature(endpoint);
-            await reporting.batteryVoltage(endpoint);
-        },
-        onEvent: async (type, data, device) => {
-            if (type === 'message' && data.type === 'commandGetPanelStatus' && data.cluster === 'ssIasAce' &&
-                globalStore.hasValue(device.getEndpoint(1), 'panelStatus')) {
-                const payload = {
-                    panelstatus: globalStore.getValue(device.getEndpoint(1), 'panelStatus'),
-                    secondsremain: 0x00, audiblenotif: 0x00, alarmstatus: 0x00,
-                };
-                await device.getEndpoint(1).commandResponse(
-                    'ssIasAce', 'getPanelStatusRsp', payload, {}, data.meta.zclTransactionSequenceNumber,
-                );
-            }
-        },
-    },
-    {
         zigbeeModel: ['3420'],
         model: '3420-G',
         vendor: 'Centralite',
@@ -8555,6 +8521,41 @@ const devices = [
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+
+    // Xfinity
+    {
+        zigbeeModel: ['URC4450BC0-X-R'],
+        model: 'URC4450BC0-X-R',
+        vendor: 'Xfinity',
+        description: 'Alarm security keypad',
+        meta: {configureKey: 1, battery: {voltageToPercentage: '3V_2100'}},
+        fromZigbee: [fz.command_arm, fz.temperature, fz.battery, fz.ias_occupancy_alarm_1, fz.identify, fz.ias_contact_alarm_1,
+            fz.ias_occupancy_alarm_2, fz.ias_occupancy_alarm_1_with_timeout],
+        exposes: [e.battery(), e.voltage(), e.occupancy(), e.battery_low(), e.tamper(), e.presence(), e.contact(),
+            exposes.numeric('action_code', ea.STATE), exposes.text('action_zone', ea.STATE), e.temperature(), e.action([
+                'disarm', 'arm_day_zones', 'identify', 'arm_night_zones', 'arm_all_zones', 'invalid_code', 'emergency',
+            ])],
+        toZigbee: [tz.arm_mode],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            const clusters = ['msTemperatureMeasurement', 'genPowerCfg', 'ssIasZone', 'ssIasAce', 'genBasic', 'genIdentify'];
+            await reporting.bind(endpoint, coordinatorEndpoint, clusters);
+            await reporting.temperature(endpoint);
+            await reporting.batteryVoltage(endpoint);
+        },
+        onEvent: async (type, data, device) => {
+            if (type === 'message' && data.type === 'commandGetPanelStatus' && data.cluster === 'ssIasAce' &&
+                globalStore.hasValue(device.getEndpoint(1), 'panelStatus')) {
+                const payload = {
+                    panelstatus: globalStore.getValue(device.getEndpoint(1), 'panelStatus'),
+                    secondsremain: 0x00, audiblenotif: 0x00, alarmstatus: 0x00,
+                };
+                await device.getEndpoint(1).commandResponse(
+                    'ssIasAce', 'getPanelStatusRsp', payload, {}, data.meta.zclTransactionSequenceNumber,
+                );
+            }
         },
     },
 
