44b4f78bdc1a63dce3959b03cd86709351969322
Koen Kanters <koenkanters94@gmail.com>
2021-02-16 19:12:37 +0100

    Revert "Fix configure failing for some TuYa TS0041 switches. https://github.com/Koenkk/zigbee2mqtt/issues/6313"
    This reverts commit 5f4e93bd5c70f2f5d12e425e00af54ba654b8f4c.

--- devices.js
+++ devices.js
@@ -1487,11 +1487,9 @@ const devices = [
         toZigbee: [],
         meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint, logger) => {
-            try {
-                const endpoint = device.getEndpoint(1);
-                await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
-                await reporting.batteryPercentageRemaining(endpoint);
-            } catch (error) {/* Fails for some: https://github.com/Koenkk/zigbee2mqtt/issues/6313 */}
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
         },
     },
     {
