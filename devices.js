c22e4477a90fc998d2a60885e999fd6a03a93c63
M.J. Wydra <tedioremaliwan@gmail.com>
2021-02-22 21:45:33 -0600

    Add support for Brimate motion sensor
    Product link: https://www.amazon.com/gp/product/B01MXTH3WY
    (I'll add an image for this product to the zigbee2mqtt.io repo)

--- devices.js
+++ devices.js
@@ -258,6 +258,16 @@ const preset = {
 }
 
 const devices = [
+    // Brimate
+    {
+        zigbeeModel: ['FB56-BOT02HM1A5'],
+        model: 'FZB8708HD-S1',
+        vendor: 'Brimate',
+        description: 'Smart motion sensor',
+        fromZigbee: [fz.ias_occupancy_alarm_1],
+        toZigbee: [],
+        exposes: [e.occupancy(), e.battery_low()],
+    },
     // Xiaomi
     {
         zigbeeModel: ['lumi.light.aqcn02'],
