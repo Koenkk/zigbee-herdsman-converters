6e19d520b89d17157128cebac4df317ca42323a3
Alexey <26709+alp@users.noreply.github.com>
2021-02-23 21:19:00 -0800

    Support 1746230V7 Added support for Philips Hue Lily XL outdoor spot light (1746230V7)

--- devices.js
+++ devices.js
@@ -4229,6 +4229,15 @@ const devices = [
         ota: ota.zigbeeOTA,
     },
     {
+        zigbeeModel: ['1746230V7'],
+        model: '1746230V7',
+        vendor: 'Philips',
+        description: 'Hue Lily XL outdoor spot light',
+        meta: {turnsOffAtBrightness1: true},
+        extend: preset.hue.light_onoff_brightness_colortemp_color(),
+        ota: ota.zigbeeOTA,
+    },
+    {
         zigbeeModel: ['LCL003'],
         model: '9290022891',
         vendor: 'Philips',
