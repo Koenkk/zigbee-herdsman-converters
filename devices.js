727b2152202f520e0798a237831bde48493ee805
Aria Stewart <aredridel@dinhe.net>
2021-03-05 21:54:02 -0500

    Support Enbrighten 43082 Zigbee 3.0 dimmer

--- devices.js
+++ devices.js
@@ -61,6 +61,14 @@ const preset = {
             tz.light_brightness_move, tz.light_brightness_step, tz.level_config, tz.power_on_behavior,
         ],
     }),
+    light_onoff_brightness_nofx: (options={}) => ({
+        exposes: [e.light_brightness()],
+        fromZigbee: [fz.on_off, fz.brightness, fz.level_config, fz.power_on_behavior, fz.ignore_basic_report],
+        toZigbee: [
+            tz.light_onoff_brightness, tz.ignore_transition, tz.ignore_rate,
+            tz.light_brightness_move, tz.light_brightness_step, tz.level_config, tz.power_on_behavior,
+        ],
+    }),
     light_onoff_brightness_colortemp: (options={}) => {
         const exposes = [e.light_brightness_colortemp(options.colorTempRange), e.effect()];
         const toZigbee = [
@@ -16133,6 +16141,19 @@ const devices = [
             await reporting.bind(endpoint2, coordinatorEndpoint, ['genOnOff']);
             await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff']);
             await reporting.onOff(endpoint1);
+        },
+    },
+    {
+        zigbeeModel: ['43082'],
+        model: '43082',
+        vendor: 'Enbrighten',
+        description: 'Zigbee in-wall smart dimmer',
+        extend: preset.light_onoff_brightness_nofx(),
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
         },
     },
     {
