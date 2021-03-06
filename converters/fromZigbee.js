743a010d460fa57c64c574fd05a720c012b077ac
Jorge Schrauwen <sjorge@blackdot.be>
2021-03-06 23:17:02 +0100

    fromZigbee: use hasOwnProperty as constants.keypadLockoutMode is not an array

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -140,7 +140,7 @@ const converters = {
         convert: (model, msg, publish, options, meta) => {
             const result = {};
             if (msg.data.hasOwnProperty('keypadLockout')) {
-                result.keypad_lockout = constants.keypadLockoutMode.includes(msg.data['keypadLockout']) ?
+                result.keypad_lockout = constants.keypadLockoutMode.hasOwnProperty(msg.data['keypadLockout']) ?
                     constants.keypadLockoutMode[msg.data['keypadLockout']] : msg.data['keypadLockout'];
             }
             return result;
