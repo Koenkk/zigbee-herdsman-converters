5556c8fa1b1110881bd6d1c8685147cf751d2d6b
Jorge Schrauwen <sjorge@blackdot.be>
2021-03-11 19:56:18 +0100

    Enable window_open detection for Viessman TRV (#2351)
    * converters: viessmann window_open
    
    * devices: report viessmannCustom0
    
    * exposes: window_open for viessman trv
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -4501,6 +4501,12 @@ const converters = {
             return {state: {sensitivity: value}};
         },
     },
+    viessmann_window_open: {
+        key: ['window_open'],
+        convertGet: async (entity, key, meta) => {
+            await entity.read('hvacThermostat', ['viessmannCustom0'], {manufacturerCode: 0x1221});
+        },
+    },
     dawondns_only_off: {
         key: ['state'],
         convertSet: async (entity, key, value, meta) => {
