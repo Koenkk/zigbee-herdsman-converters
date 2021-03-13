dbbe682567b06aad06c2178f3f21e02c212d9cc2
Mario Cao <mariocao@gmail.com>
2021-03-13 19:15:59 +0100

    feat(xfinity): add support for occupancy with timeout

--- devices.js
+++ devices.js
@@ -9223,7 +9223,7 @@ const devices = [
         description: 'Alarm security keypad',
         meta: {configureKey: 1, battery: {voltageToPercentage: '3V_2100'}},
         fromZigbee: [fz.command_arm, fz.temperature, fz.battery, fz.ias_occupancy_alarm_1, fz.identify, fz.ias_contact_alarm_1,
-            fz.ias_occupancy_alarm_2, fz.ias_occupancy_alarm_1_with_timeout],
+            fz.ias_ace_occupancy_with_timeout],
         exposes: [e.battery(), e.battery_voltage(), e.occupancy(), e.battery_low(), e.tamper(), e.presence(), e.contact(),
             exposes.numeric('action_code', ea.STATE), exposes.text('action_zone', ea.STATE), e.temperature(), e.action([
                 'disarm', 'arm_day_zones', 'identify', 'arm_night_zones', 'arm_all_zones', 'exit_delay', 'emergency',
