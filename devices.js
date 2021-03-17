a9f099ad39241f28f5323c3ae60ba457549f46d2
Wojtek Woźniak <wwozniak92@gmail.com>
2021-03-17 20:20:28 +0100

    Adding awox 33943 (#2382)
    * Added support for AwoX 33943
    
    * Removed unsupported feature
    
    * Added fingerprint
    
    * Added space after ','
    
    * Removed trailing spaces.
    
    * Updating fingerprint
    
    * Update devices.js
    
    * Review fixes
    
    Co-authored-by: Kacper Sobek <kacper.sobek2@gmail.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -13405,6 +13405,18 @@ const devices = [
     {
         fingerprint: [
             {type: 'Router', manufacturerName: 'AwoX', modelID: 'TLSR82xx', endpoints: [
+                {ID: 1, profileID: 260, deviceID: 258, inputClusters: [0, 3, 4, 5, 6, 8, 768, 4096], outputClusters: [6, 25]},
+                {ID: 3, profileID: 49152, deviceID: 258, inputClusters: [65360, 65361], outputClusters: [65360, 65361]},
+            ]},
+        ],
+        model: '33943',
+        vendor: 'AwoX',
+        description: 'LED RGB & brightness',
+        extend: preset.light_onoff_brightness_colortemp_color(),
+    },
+    {
+        fingerprint: [
+            {type: 'Router', manufacturerName: 'AwoX', modelID: 'TLSR82xx', endpoints: [
                 {ID: 1, profileID: 260, deviceID: 269, inputClusters: [0, 3, 4, 5, 6, 8, 768, 4096, 64599], outputClusters: [6]},
                 {ID: 3, profileID: 4751, deviceID: 269, inputClusters: [65360, 65361], outputClusters: [65360, 65361]},
             ]},
