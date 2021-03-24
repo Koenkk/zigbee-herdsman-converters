6e8fc863bc25ac87d87af13048481d131d0f465b
gohlas <50915149+gohlas@users.noreply.github.com>
2021-03-24 16:54:20 +0100

    Added 4512705 (#2413)
    * Added support for new Namron Device
    
     Added support for new Namron Device Zigbee 4 channel Remote Control
    
    * Update devices.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices.js
+++ devices.js
@@ -8144,6 +8144,24 @@ const devices = [
         },
     },
     {
+        zigbeeModel: ['4512705'],
+        model: '4512705',
+        vendor: 'Namron',
+        description: 'Zigbee 4 channel remote control',
+        fromZigbee: [fz.command_on, fz.command_off, fz.battery, fz.command_move, fz.command_stop, fz.command_recall],
+        toZigbee: [],
+        exposes: [e.battery(), e.action([
+            'on_l1', 'off_l1', 'brightness_move_up_l1', 'brightness_move_down_l1', 'brightness_stop_l1',
+            'on_l2', 'off_l2', 'brightness_move_up_l2', 'brightness_move_down_l2', 'brightness_stop_l2',
+            'on_l3', 'off_l3', 'brightness_move_up_l3', 'brightness_move_down_l3', 'brightness_stop_l3',
+            'on_l4', 'off_l4', 'brightness_move_up_l4', 'brightness_move_down_l4', 'brightness_stop_l4',
+            'recall_*'])],
+        meta: {multiEndpoint: true},
+        endpoint: (device) => {
+            return {l1: 1, l2: 2, l3: 3, l4: 4};
+        },
+    },
+    {
         zigbeeModel: ['3802962'],
         model: '3802962',
         vendor: 'Namron',
