05829aaf486f04feffdef32e6c5ef5eb0d376cd7
Stigh Aarstein <stigh@aarstein.no>
2021-03-27 20:38:18 +0000

    Updated Datek APEX and added Datek HAN for firmware 0.2, next firmware for HAN will include temperature

--- devices.js
+++ devices.js
@@ -17260,16 +17260,45 @@ const devices = [
         model: 'HLU2909K',
         vendor: 'Datek',
         description: 'APEX smart plug 16A',
-        fromZigbee: [fz.electrical_measurement, fz.on_off, fz.metering],
+        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.temperature],
         toZigbee: [tz.on_off],
         meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'seMetering']);
-            endpoint.saveClusterAttributeKeyValue('seMetering', {divisor: 100, multiplier: 1});
-            endpoint.saveClusterAttributeKeyValue('haElectricalMeasurement', {acVoltageDivisor: 100});
+           const endpoint = device.getEndpoint(1);
+           await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement', 'msTemperatureMeasurement']);
+           await endpoint.read('haElectricalMeasurement', ['acVoltageMultiplier', 'acVoltageDivisor']);
+           await endpoint.read('haElectricalMeasurement', ['acCurrentMultiplier', 'acCurrentDivisor']);
+           await endpoint.read('haElectricalMeasurement', ['acPowerMultiplier', 'acPowerDivisor']);
+           await reporting.onOff(endpoint);
+           await reporting.rmsVoltage(endpoint);
+           await reporting.rmsCurrent(endpoint);
+           await reporting.activePower(endpoint);
+           await reporting.temperature(endpoint);
         },
-        exposes: [e.power(), e.current(), e.voltage(), e.switch()],
+        exposes: [e.power(), e.current(), e.voltage(), e.switch(), e.temperature()],
+    },
+
+    {
+        zigbeeModel: ['HAN'],
+        model: 'Datek HAN',
+        vendor: 'Datek',
+        description: 'Datek Eva AMS HAN power-meter sensor',
+        fromZigbee: [fz.metering, fz.electrical_measurement],
+        toZigbee: [],
+        ota: ota.zigbeeOTA,
+        meta: {configureKey: 3},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['haElectricalMeasurement', 'seMetering']);
+            await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
+            await reporting.rmsVoltage(endpoint);
+            await reporting.rmsCurrent(endpoint);
+            await reporting.readMeteringMultiplierDivisor(endpoint);
+            await reporting.instantaneousDemand(endpoint);
+            await reporting.currentSummDelivered(endpoint);
+            await reporting.currentSummReceived(endpoint);
+        },
+        exposes: [e.power(), e.energy(), e.current(), e.voltage(), e.current_phase_b(), e.voltage_phase_b(), e.current_phase_c(), e.voltage_phase_c()],
     },
 
     // Prolight
