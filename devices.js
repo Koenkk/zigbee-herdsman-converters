0a2f569d8a45d3a5b54a740913b1f08b9a4ebeca
Koen Kanters <koenkanters94@gmail.com>
2021-03-27 19:53:18 +0100

    Update devices.js

--- devices.js
+++ devices.js
@@ -12570,7 +12570,7 @@ const devices = [
         zigbeeModel: ['511.202'],
         model: '511.202',
         vendor: 'Iluminize',
-        description: 'Zigbee 3.0 Schalt-Aktor mini 1x230V, 200W/400W',
+        description: 'Zigbee 3.0 switch mini 1x230V, 200W/400W',
         extend: preset.switch(),
         meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint, logger) => {
@@ -12583,7 +12583,7 @@ const devices = [
         zigbeeModel: ['5120.1210'],
         model: '5120.1210',
         vendor: 'Iluminize',
-        description: 'Zigbee 3.0 Schalt-Aktor mini 1x230V ohne N, 200W/400W',
+        description: 'Zigbee 3.0 switch mini 1x230V without neutral, 200W/400W',
         extend: preset.switch(),
         meta: {configureKey: 1},
         configure: async (device, coordinatorEndpoint, logger) => {
