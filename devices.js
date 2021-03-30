72e390f367f184228086d082c092df988db4f21b
aofc <robin.sylvestre@polymtl.ca>
2021-03-30 15:51:33 -0400

    Update devices.js
    Fixes Exposes for Hampton Bay 99432

--- devices.js
+++ devices.js
@@ -12530,7 +12530,7 @@ const devices = [
         description: 'Universal wink enabled white ceiling fan premier remote control',
         fromZigbee: preset.light_onoff_brightness().fromZigbee.concat([fz.fan]),
         toZigbee: preset.light_onoff_brightness().toZigbee.concat([tz.fan_mode]),
-        exposes: [e.light_brightness(), e.fan()],
+        exposes: [e.light_brightness(), exposes.climate().withFanMode(["off", "low", "medium", "high", "on", "auto", "smart"])],
         meta: {disableDefaultResponse: true, configureKey: 1},
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
@@ -12538,6 +12538,8 @@ const devices = [
             await reporting.onOff(endpoint);
             await reporting.brightness(endpoint);
             await reporting.fanMode(endpoint);
+            device.powerSource = 'Mains (single phase)';
+            device.save();
         },
     },
     {
