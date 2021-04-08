ccca1bacd26e03a29c4703ddeec1e162f74dfa2e
C64Axel <33828760+C64Axel@users.noreply.github.com>
2021-04-08 19:56:07 +0200

    Add 141L100RC (#2462)
    Co-authored-by: root <root@debian8.sydow.homeip.net>

--- devices.js
+++ devices.js
@@ -17545,6 +17545,16 @@ const devices = [
         extend: preset.light_onoff_brightness_colortemp_color({disableColorTempStartup: true}),
         meta: {applyRedFix: true},
     },
+    {
+        fingerprint: [{modelID: 'TS1001', manufacturerName: '_TZ3000_ztrfrcsu'}],
+        model: '141L100RC',
+        vendor: 'Aldi',
+        description: 'MEGOS switch and dimming light remote control',
+        exposes: [e.action(['on', 'off', 'brightness_stop', 'brightness_step_up', 'brightness_step_down', 'brightness_move_up',
+            'brightness_move_down'])],
+        fromZigbee: [fz.command_on, fz.command_off, fz.command_step, fz.command_move, fz.command_stop],
+        toZigbee: [],
+    },
 
     // SOHAN Electric
     {
