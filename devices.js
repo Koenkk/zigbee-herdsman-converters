6fbc27793946aedd4f8458c3e6d1eda1735e83e7
Koen Kanters <koenkanters94@gmail.com>
2021-04-10 17:23:09 +0200

    Add 4512729. https://github.com/Koenkk/zigbee2mqtt/issues/7023

--- devices.js
+++ devices.js
@@ -8202,7 +8202,21 @@ const devices = [
         zigbeeModel: ['4512719'],
         model: '4512719',
         vendor: 'Namron',
-        description: 'Zigbee 2 channel switch K4',
+        description: 'Zigbee 2 channel switch K4 white',
+        fromZigbee: [fz.command_on, fz.command_off, fz.battery, fz.command_move, fz.command_stop],
+        meta: {multiEndpoint: true},
+        exposes: [e.battery(), e.action(['on_l1', 'off_l1', 'brightness_move_up_l1', 'brightness_move_down_l1', 'brightness_stop_l1',
+            'on_l2', 'off_l2', 'brightness_move_up_l2', 'brightness_move_down_l2', 'brightness_stop_l2'])],
+        toZigbee: [],
+        endpoint: (device) => {
+            return {l1: 1, l2: 2};
+        },
+    },
+    {
+        zigbeeModel: ['4512729'],
+        model: '4512729',
+        vendor: 'Namron',
+        description: 'Zigbee 2 channel switch K4 white',
         fromZigbee: [fz.command_on, fz.command_off, fz.battery, fz.command_move, fz.command_stop],
         meta: {multiEndpoint: true},
         exposes: [e.battery(), e.action(['on_l1', 'off_l1', 'brightness_move_up_l1', 'brightness_move_down_l1', 'brightness_stop_l1',
