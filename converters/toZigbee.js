c28843cf5f9d543e0cd560f33c48c99c07732e77
PxPert <pxpert@gmail.com>
2021-04-17 14:52:00 +0200

    Fixed inverted_position + optional partial position support for bticino 4027C

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -3318,7 +3318,13 @@ const converters = {
     bticino_4027C_cover_state: {
         key: ['state'],
         convertSet: async (entity, key, value, meta) => {
-            const lookup = {'open': 'upOpen', 'close': 'downClose', 'stop': 'stop', 'on': 'upOpen', 'off': 'downClose'};
+            const invert = !(utils.getMetaValue(entity, meta.mapped, 'coverInverted', 'allEqual', false) ?
+                !meta.options.invert_cover : meta.options.invert_cover);
+            const lookup = invert ?
+              {'open': 'upOpen', 'close': 'downClose', 'stop': 'stop', 'on': 'upOpen', 'off': 'downClose'}
+              :
+              {'open': 'downClose', 'close': 'upOpen', 'stop': 'stop', 'on': 'downClose', 'off': 'upOpen'};
+
             value = value.toLowerCase();
             utils.validateValue(value, Object.keys(lookup));
 
@@ -3335,8 +3341,17 @@ const converters = {
     bticino_4027C_cover_position: {
         key: ['position'],
         convertSet: async (entity, key, value, meta) => {
-            const position = value >= 50 ? 100 : 0;
-            await entity.command('closuresWindowCovering', 'goToLiftPercentage', {percentageliftvalue: position},
+            const invert = !(utils.getMetaValue(entity, meta.mapped, 'coverInverted', 'allEqual', false) ?
+                !meta.options.invert_cover : meta.options.invert_cover);
+            let new_position = value;
+            if (meta.options.no_position_support) {
+                new_position = value >= 50 ? 100 : 0;
+            }
+            const position = new_position;
+            if (invert) {
+                new_position = 100 - new_position;
+            }
+            await entity.command('closuresWindowCovering', 'goToLiftPercentage', {percentageliftvalue: new_position},
                 utils.getOptions(meta.mapped, entity));
             return {state: {['position']: position}, readAfterWriteTime: 0};
         },
