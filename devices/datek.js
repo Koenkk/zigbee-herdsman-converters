8523dd317d964039d70f987894c65f215c5edfa1
Stigh Aarstein <stigh@aarstein.no>
2021-04-28 16:21:52 +0000

    Updated datek.js with expose.access

--- devices/datek.js
+++ devices/datek.js
@@ -4,6 +4,7 @@ const tz = require('../converters/toZigbee');
 const ota = require('../lib/ota');
 const reporting = require('../lib/reporting');
 const e = exposes.presets;
+const ea = exposes.access;
 
 module.exports = [
     {
