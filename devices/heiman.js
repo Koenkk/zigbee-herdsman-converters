11e4d1ceb9785ef46e5dbe5c027dfa6419719dde
accelle17 <joey.caldozo@gmail.com>
2021-05-05 14:43:03 +0800

    add support for HS1MS-M #509
    I am migrating from deconz to z2m and got some HS1MS-M Heiman sensors. I have tested this using external converter and was able to made it work.

--- devices/heiman.js
+++ devices/heiman.js
@@ -650,4 +650,13 @@ module.exports = [
         },
         exposes: [e.cover_position().setAccess('state', ea.ALL)],
     },
+    {
+        zigbeeModel: ['PIR_TPV16'], 
+        model: 'HS1MS-M', 
+        vendor: 'Heiman', 
+        description: 'Smart Motion Sensor', 
+        fromZigbee: [fz.ias_occupancy_alarm_1],
+        toZigbee: [],
+        exposes: [e.occupancy(), e.battery_low(), e.tamper()],
+    },
 ];
