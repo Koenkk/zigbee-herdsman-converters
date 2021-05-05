69c4e50d50b4ce662c823e672e9f2a28096a7969
Koen Kanters <koenkanters94@gmail.com>
2021-05-05 19:46:34 +0200

    Update roome.js

--- devices/roome.js
+++ devices/roome.js
@@ -1,17 +1,15 @@
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const e = exposes.presets;
 
 module.exports = [
-	{
-      	  zigbeeModel: ['MULTI-MECI--EA01'],
-      	  model: 'HSC1-WD-0',
-     	   vendor: 'ROOME',
-      	  description: 'Door or window contact switch',
-      	  supports: 'contact，tamper',
-      	  fromZigbee: [fz.ias_contact_alarm_1,fz.battery,fz.ignore_iaszone_attreport],
-      	  toZigbee: [],
-       	 exposes: [e.contact(), e.battery_low(), e.tamper()],      
-	};
-];    
+    {
+        zigbeeModel: ['MULTI-MECI--EA01'],
+        model: 'HSC1-WD-0',
+        vendor: 'ROOME',
+        description: 'Door or window contact switch',
+        fromZigbee: [fz.ias_contact_alarm_1, fz.battery, fz.ignore_iaszone_attreport],
+        toZigbee: [],
+        exposes: [e.contact(), e.battery_low(), e.tamper()],
+    },
+];
