ca7fecc870cc17319124a6f4146f5a19ab010e99
H3buss <83210485+H3buss@users.noreply.github.com>
2021-05-06 14:40:09 +0000

    Add Schneider Electric CCTFR6700

--- devices/schneider_electric.js
+++ devices/schneider_electric.js
@@ -182,4 +182,36 @@ module.exports = [
             });
         },
     },
+    {
+        fingerprint: [{modelID: 'CCTFR6700', manufacturerName: 'Schneider Electric'}],
+        model: 'CCTFR6700',
+        vendor: 'Schneider Electric',
+        description: 'Heating thermostat',
+        fromZigbee: [fz.thermostat, fz.metering],
+        toZigbee: [tz.thermostat_system_mode, tz.thermostat_running_state, tz.thermostat_local_temperature,
+            tz.thermostat_occupied_heating_setpoint, tz.thermostat_control_sequence_of_operation],
+        exposes: [
+                    exposes.climate().withSetpoint('occupied_heating_setpoint', 4, 30, 0.5)
+                                     .withLocalTemperature()
+                                     .withSystemMode(['off', 'auto', 'heat'])
+                                     .withRunningState(['idle', 'heat'])
+                                     .withPiHeatingDemand(),
+                    exposes.power(),
+                    exposes.energy(),
+                    exposes.enum('schneider_pilot_mode', ea.ALL, ['relay', 'pilot'])
+                           .withDescription('Controls piloting mode'),
+                ],
+        meta: {configureKey: 1},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            var endpoint1 = device.getEndpoint(1);
+            var endpoint2 = device.getEndpoint(2);
+            
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['hvacThermostat']);
+            await reporting.thermostatOccupiedHeatingSetpoint(endpoint1, {min: 1, max: 60, change: 1});
+            await reporting.thermostatPIHeatingDemand(endpoint1, {min: 1, max: 60, change: 1});
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['seMetering']);
+            await reporting.instantaneousDemand(endpoint2, {min: 1, max: 60, change: 1});
+            await reporting.currentSummDelivered(endpoint2, {min: 1, max: 60, change: 1});
+        },
+    },
 ];
