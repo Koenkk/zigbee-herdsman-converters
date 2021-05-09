c339347985fd691c4a33adf8d82c4ad6c8730bdc
Andrey Mityagin <andrey.mityagin@gmail.com>
2021-05-09 01:06:47 +0100

    Added brightness reporting

--- devices/samotech.js
+++ devices/samotech.js
@@ -37,6 +37,7 @@ module.exports = [
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
             await reporting.onOff(endpoint);
+            await reporting.brightness(endpoint);
         },
     },
 ];
