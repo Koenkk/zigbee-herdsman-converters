01225c2c642422dbeea9a8f7267b03fa5f5101ae
avee87 <andrey.mityagin@gmail.com>
2021-05-10 17:23:55 +0100

    Added support for Samotech SM323 retrofit dimmer (#2559)
    * Added support for Samotech SM323 retrofit dimmer
    
    * Added brightness reporting
    
    * Use fingerprint to disambiguate from ecoDim

--- devices/samotech.js
+++ devices/samotech.js
@@ -26,4 +26,18 @@ module.exports = [
             await reporting.onOff(endpoint);
         },
     },
+    {
+        fingerprint: [{modelID: 'Dimmer-Switch-ZB3.0', manufacturerName: 'Samotech'}],
+        model: 'SM323',
+        vendor: 'Samotech',
+        description: 'ZigBee retrofit dimmer 250W',
+        extend: extend.light_onoff_brightness({noConfigure: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+            await reporting.brightness(endpoint);
+        },
+    },
 ];
