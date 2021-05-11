900f59299571777a9924e86b93ee4541e71b3619
Sicco van Sas <siccovansas@users.noreply.github.com>
2021-05-11 11:34:32 +0200

    Use fingerprint as model 'Dimmer-Switch-ZB3.0' already exists

--- devices/idinio.js
+++ devices/idinio.js
@@ -2,7 +2,7 @@ const extend = require('../lib/extend');
 
 module.exports = [
     {
-        zigbeeModel: ['Dimmer-Switch-ZB3.0'],
+        fingerprint: [{modelID: 'Dimmer-Switch-ZB3.0', manufacturerName: 'idinio'}],
         model: '0140302',
         vendor: 'Idinio',
         description: 'Zigbee LED Foot Dimmer',
