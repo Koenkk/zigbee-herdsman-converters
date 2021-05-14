5a27d1303045d1dd4de0ed2668e4f4f1743a6f0d
Jorge Schrauwen <sjorge@blackdot.be>
2021-05-14 21:50:19 +0200

    Fix XY->RGB tests

--- test/color.test.js
+++ test/color.test.js
@@ -102,11 +102,11 @@ describe('lib/color.js', () => {
         });
 
         test.each([
-            ['red', {x: 0.7006, y: 0.299}, {red: 1.0, green: 0, blue: 0}],
+            ['red', {x: 0.7006, y: 0.2993}, {red: 1.0, green: 0, blue: 0}],
             ['green', {x: 0.1724, y: 0.7468}, {red: 0, green: 1.0, blue: 0}],
             ['blue', {x: 0.1355, y: 0.0399}, {red: 0, green: 0, blue: 1.0}],
         ])('.toRGB() - %s', (_name, xy, rgb) => {
-            expect(libColor.ColorXY.fromObject(xy).toRGB().toObject()).toStrictEqual(rgb);
+            expect(libColor.ColorXY.fromObject(xy).toRGB().rounded(4).toObject()).toStrictEqual(rgb, 4);
         });
 
         test.each([
