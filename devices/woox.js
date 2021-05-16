c5384f7b3f8d724543dcba7d4829e4c1cae80059
Olivér Falvai <ofalvai@gmail.com>
2021-05-16 11:44:43 +0200

    Move Woox devices to separate file

---
+++ devices/woox.js
@@ -0,0 +1,28 @@
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const tuya = require('../lib/tuya');
+const extend = require('../lib/extend');
+const e = exposes.presets;
+
+module.exports = [
+    {
+        fingerprint: [{modelID: 'TS0101', manufacturerName: '_TZ3210_eymunffl'}],
+        model: 'R7060',
+        vendor: 'Woox',
+        description: 'Smart garden irrigation control',
+        fromZigbee: [fz.on_off, fz.ignore_tuya_set_time, fz.ignore_basic_report, fz.woox_R7060],
+        toZigbee: [tz.on_off],
+        onEvent: tuya.onEventSetTime,
+        exposes: [e.switch()],
+        meta: {disableDefaultResponse: true},
+    },
+    {
+        fingerprint: [{modelID: 'TS0505A', manufacturerName: '_TZ3000_keabpigv'}],
+        model: 'R9077',
+        vendor: 'Woox',
+        description: 'RGB+CCT LED',
+        extend: extend.light_onoff_brightness_colortemp_color({disableColorTempStartup: true}),
+        meta: {applyRedFix: true},
+    },
+]
