c4fff7d17148a5633ef3346a8578efe2134b4880
Robert Alexa <mail@robertalexa.me>
2021-05-18 15:15:56 +0100

    Linting

--- devices/hive.js
+++ devices/hive.js
@@ -117,10 +117,12 @@ module.exports = [
         fromZigbee: [fz.battery, fz.legacy.thermostat_att_report, fz.danfoss_thermostat],
         toZigbee: [tz.thermostat_occupied_heating_setpoint, tz.thermostat_local_temperature, tz.danfoss_mounted_mode_control,
             tz.danfoss_thermostat_orientation, tz.danfoss_algorithm_scale_factor, tz.danfoss_heat_available, tz.danfoss_day_of_week,
-            tz.danfoss_trigger_time, tz.danfoss_window_open_internal, tz.danfoss_window_open_external, tz.danfoss_display_orientation, tz.thermostat_keypad_lockout],
+            tz.danfoss_trigger_time, tz.danfoss_window_open_internal, tz.danfoss_window_open_external, tz.danfoss_display_orientation,
+            tz.thermostat_keypad_lockout],
         exposes: [e.battery(), e.keypad_lockout(),
             exposes.binary('mounted_mode_active', ea.STATE, true, false).withDescription(
-                'Is the unit in mounting mode. This is set to `false` for mounted (already on the radiator) or `true` for not mounted (after factory reset)'),
+                'Is the unit in mounting mode. This is set to `false` for mounted (already on the radiator) or `true` ' +
+                'for not mounted (after factory reset)'),
             exposes.binary('mounted_mode_control', ea.ALL, true, false).withDescription(
                 'Set the unit mounting mode. `false` Go to Mounting Mode or `true` Go to Mounted Mode'),
             exposes.binary('heat_required', ea.STATE, true, false).withDescription('Wether or not the unit needs warm water'),
