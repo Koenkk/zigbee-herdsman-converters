26b0f45acb077203ee258b1e40f2eee625504d01
Robert Alexa <mail@robertalexa.me>
2021-05-21 15:25:37 +0100

    Typo

--- devices/hive.js
+++ devices/hive.js
@@ -127,7 +127,7 @@ module.exports = [
             exposes.binary('mounted_mode_control', ea.ALL, true, false)
                 .withDescription('Set the unit mounting mode. `false` Go to Mounting Mode or `true` Go to Mounted Mode'),
             exposes.binary('thermostat_vertical_orientation', ea.ALL, true, false)
-                .withDescription('Thermostat Orientation. This is important for the PID in how it assesses temperatuea. ' +
+                .withDescription('Thermostat Orientation. This is important for the PID in how it assesses temperature. ' +
                     '`false` Horizontal or `true` Vertical'),
             exposes.numeric('viewing_direction', ea.ALL).withValueMin(0).withValueMax(1)
                 .withDescription('Viewing/Display Direction. `0` Horizontal or `1` Vertical'),
