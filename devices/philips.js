923a4ac4ebf9053257b041fdf1c74b1f4fe75f7b
baahver <49638791+baahver@users.noreply.github.com>
2021-05-30 13:48:28 +0200

    Update philips.js
    added {colorTempRange: [153, 500]}
    But I'm not an expert on this.

--- devices/philips.js
+++ devices/philips.js
@@ -339,7 +339,7 @@ module.exports = [
         vendor: 'Philips',
         description: 'Hue Flourish white and color ambiance table light with Bluetooth',
         meta: {turnsOffAtBrightness1: true},
-        extend: hueExtend.light_onoff_brightness_colortemp_color(),
+        extend: hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
         ota: ota.zigbeeOTA,
     },
     {
