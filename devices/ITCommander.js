95be630bb9063e7f6cc15863b0669b4f481d453d
5high <5high.net@gmail.com>
2021-06-08 21:18:04 +0800

    Update ITCommander.js

--- devices/ITCommander.js
+++ devices/ITCommander.js
@@ -1,4 +1,4 @@
-const fz = {...require('../converters/fromZigbee');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const tz = require('../converters/toZigbee');
 const exposes = require('../lib/exposes');
 const reporting = require('../lib/reporting');
