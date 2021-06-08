676ca0b0a95dd07e792c5a99600cbd91eba5ea22
5high <5high.net@gmail.com>
2021-06-08 11:42:44 +0800

    Add files via upload

---
+++ devices/ITCommander.js
@@ -0,0 +1,19 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+module.exports = [
+    {
+        zigbeeModel: ['ITCMDR_Contact'],
+        model: 'ITCMDR_Contact',
+        vendor: 'sumju.net',
+        description: 'Contact Sensor by IT Commander',
+        fromZigbee: ['fz.contact'],
+        toZigbee: [],
+        exposes: [e.battery(), e.contact()]
+    }
+]
