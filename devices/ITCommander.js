276c8e16eb5c8fa2bf48beeb976c7a53621fb448
5high <5high.net@gmail.com>
2021-06-08 16:23:13 +0800

    Update ITCommander.js

--- devices/ITCommander.js
+++ devices/ITCommander.js
@@ -1,7 +1,3 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const extend = require('zigbee-herdsman-converters/lib/extend');
 const e = exposes.presets;
 const ea = exposes.access;
