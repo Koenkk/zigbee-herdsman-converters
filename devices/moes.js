e58036f5224139a6d26184e5d16b299572ebbeb7
Koen Kanters <koenkanters94@gmail.com>
2021-06-10 19:26:53 +0200

    Update moes.js

--- devices/moes.js
+++ devices/moes.js
@@ -182,7 +182,7 @@ module.exports = [
                 'preset week programming temperature time and temperature. '),
             exposes.binary('boost_heating', ea.STATE_SET).withDescription('Boost Heating: press and hold "+" for 3 seconds, ' +
                 'the device will enter the boost heating mode, and the ▷╵◁ will flash. The countdown will be displayed in the APP'),
-            exposes.numeric('boost_heating_countdown', ea.STATE).withUnit('min').withDescription('Countdown in minutes'),
+            exposes.numeric('boost_heating_countdown', ea.STATE_SET).withUnit('min').withDescription('Countdown in minutes'),
             exposes.numeric('boost_time_set', ea.STATE_SET).withUnit('second')
                 .withDescription('Boost Time Setting 100 sec - 900 sec, (default = 300 sec)'),
             exposes.binary('eco_mode', ea.STATE).withDescription('ECO mode')],
