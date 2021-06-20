41a3297d5de386e46d7349df638e79ed64dcecf3
Tommi Savikko <dementik@gmail.com>
2021-06-20 21:38:31 +0300

    Add _TZE200_yvx5lh6k to TS0601_air_quality_sensor (#2701)

--- devices/tuya.js
+++ devices/tuya.js
@@ -20,7 +20,8 @@ module.exports = [
         exposes: [e.temperature(), e.humidity(), e.battery()],
     },
     {
-        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_8ygsuhe1'}],
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_8ygsuhe1'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_yvx5lh6k'}],
         model: 'TS0601_air_quality_sensor',
         vendor: 'Tuya',
         description: 'Air quality sensor',
