40c0257d216545d4f352f2fd17e4254b064eff34
5high <5high.net@gmail.com>
2021-06-23 15:56:05 +0800

    Fix button doesn't work

--- devices/ITCommander.js
+++ devices/ITCommander.js
@@ -3,12 +3,47 @@ const exposes = require('../lib/exposes');
 const e = exposes.presets;
 const reporting = require('../lib/reporting');
 
+const getKey = (object, value) => {
+    for (const key in object) {
+        if (object[key] == value) return key;
+    }
+};
+
+const bind = async (endpoint, target, clusters) => {
+    for (const cluster of clusters) {
+        await endpoint.bind(cluster, target);
+    }
+};
+
+const fzclick = {
+    diyruz_freepad_clicks: {
+        cluster: 'genMultistateInput',
+        type: ['readResponse', 'attributeReport'],
+        convert: (model, msg, publish, options, meta) => {
+            const button = getKey(model.endpoint(msg.device), msg.endpoint.ID);
+            const lookup = {
+                0: 'hold',
+                1: 'single',
+                2: 'double',
+                3: 'triple',
+                4: 'quadruple',
+                255: 'release',
+            };
+            const clicks = msg.data['presentValue'];
+            const action = lookup[clicks] ? lookup[clicks] : `many_${clicks}`;
+            return {
+                action: `${action}`,
+            };
+        },
+    },
+};
+
 module.exports = [
     {
         zigbeeModel: ['ITCMDR_Contact'],
         model: 'ITCMDR_Contact',
         vendor: 'IT Commander',
-        description: 'Contact Sensor',
+        description: '[Contact Sensor](https://sumju.net)',
         fromZigbee: [fz.ias_contact_alarm_1, fz.battery],
         toZigbee: [],
         exposes: [e.contact(), e.battery(), e.voltage()],
@@ -22,9 +57,25 @@ module.exports = [
         zigbeeModel: ['ITCMDR_Click'],
         model: 'ITCMDR_Click',
         vendor: 'IT Commander',
-        description: 'Button',
-        fromZigbee: [fz.ignore_basic_report, fz.ptvo_multistate_action],
+        description: '[Button](https://sumju.net)',
+        supports: 'single, double, triple, quadruple, many, hold/release',
+        fromZigbee: [fzclick.diyruz_freepad_clicks, fz.battery],
         toZigbee: [],
-        exposes: [e.action(['single', 'double', 'triple', 'hold', 'release']), e.battery()],
+        meta: {
+            configureKey: 1,
+        },
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            device.endpoints.forEach(async (ep) => {
+            await bind(ep, coordinatorEndpoint, ['genMultistateInput']);
+        });
+        },
+        endpoint: (device) => {
+        return {
+            button_1: 1,
+        };
+    },
+        exposes: [e.battery(), e.action()]
     },
 ];
