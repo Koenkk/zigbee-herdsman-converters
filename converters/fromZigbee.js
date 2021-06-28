24a219fc18ff71d2601c12764d93621fb5b8b425
Koen Kanters <koenkanters94@gmail.com>
2021-06-28 17:44:06 +0200

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -5650,6 +5650,17 @@ const converters = {
             }
         },
     },
+    itcmdr_clicks: {
+        cluster: 'genMultistateInput',
+        type: ['readResponse', 'attributeReport'],
+        convert: (model, msg, publish, options, meta) => {
+            const lookup = {0: 'hold', 1: 'single', 2: 'double', 3: 'triple',
+                4: 'quadruple', 255: 'release'};
+            const clicks = msg.data['presentValue'];
+            const action = lookup[clicks] ? lookup[clicks] : `many`;
+            return {action};
+        },
+    },
     ZB003X: {
         cluster: 'manuSpecificTuya',
         type: ['commandActiveStatusReport'],
@@ -6160,26 +6171,6 @@ const converters = {
         type: ['commandSetTimeRequest'],
         convert: (model, msg, publish, options, meta) => null,
     },
-    itcmdr_clicks: {
-        cluster: 'genMultistateInput',
-        type: ['readResponse', 'attributeReport'],
-        convert: (model, msg, publish, options, meta) => {
-            const lookup = {
-                0: 'hold',
-                1: 'single',
-                2: 'double',
-                3: 'triple',
-                4: 'quadruple',
-                255: 'release',
-            };
-            const clicks = msg.data['presentValue'];
-            const action = lookup[clicks] ? lookup[clicks] : `many`;
-            return {
-                action: `${action}`,
-            };
-        },
-    },
-
     // #endregion
 };
 
