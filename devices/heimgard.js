e1a543157015bfd92aefc77dcbf1159a1712645e
Stigh <stigh@stigh-ubuntu-20.10>
2021-06-30 01:06:57 +0200

    Added support for Heimgard Door Pro

---
+++ devices/heimgard.js
@@ -0,0 +1,27 @@
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const ota = require('../lib/ota');
+const reporting = require('../lib/reporting');
+const constants = require('../lib/constants');
+const {repInterval} = require('../lib/constants');
+const e = exposes.presets;
+const ea = exposes.access;
+
+module.exports = [
+    {
+        zigbeeModel: ['HC-SLM-1'],
+        model: 'HC-SLM-1',
+        vendor: 'Home Control AS',
+        description: 'Heimgard (Wattle) Door Lock Pro',
+        fromZigbee: [fz.lock, fz.battery],
+        toZigbee: [tz.lock],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
+            await reporting.lockState(endpoint);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+        exposes: [e.lock(), e.battery()],
+    },
+];
