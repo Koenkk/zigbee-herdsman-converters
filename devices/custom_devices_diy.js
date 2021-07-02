6b71a05325e0bf1b5a0604122e92a63e59a266ca
TrimVa <55333193+TrimVa@users.noreply.github.com>
2021-07-02 20:11:55 +0200

    Update custom_devices_diy.js

--- devices/custom_devices_diy.js
+++ devices/custom_devices_diy.js
@@ -155,4 +155,31 @@ module.exports = [
         toZigbee: [],
         exposes: [e.action(['toggle'])],
     },
+    {
+    zigbeeModel: ['ZeeFlora'],
+    model: 'ZeeFlora',
+    vendor: 'Tech4You',
+    description: '[Flower sensor with rechargeable battery]',
+    supports: '',
+    fromZigbee: [fz.temperature, fz.illuminance, fz.soil_moisture, fz.battery],
+    toZigbee: [tz.factory_reset],
+    meta: {
+        configureKey: 1,
+        multiEndpoint: true,
+    },
+    configure: async (device, coordinatorEndpoint, logger) => {
+        const firstEndpoint = device.getEndpoint(1);
+        await reporting.bind(firstEndpoint, coordinatorEndpoint, [
+            'genPowerCfg', 'msTemperatureMeasurement',
+            'msIlluminanceMeasurement', 'msSoilMoisture']);
+        const overides = {min: 0, max: 3600, change: 0};
+        await reporting.batteryVoltage(firstEndpoint, overides);
+        await reporting.batteryPercentageRemaining(firstEndpoint, overides);
+        await reporting.temperature(firstEndpoint, overides);
+        await reporting.illuminance(firstEndpoint, overides);
+        await reporting.soil_moisture(firstEndpoint, overides);
+    },
+    exposes: [e.soil_moisture(), e.battery(), e.illuminance(), e.temperature(),
+    ],
+},
 ];
