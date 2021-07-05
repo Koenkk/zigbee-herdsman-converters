3099bebc422b54d044d069d5dcb4972c4b6889f3
Koen Kanters <koenkanters94@gmail.com>
2021-07-05 21:21:30 +0200

    Add TRADFRIbulbT120E27WSopal470lm to LED1937T5. https://github.com/Koenkk/zigbee2mqtt/issues/7841

--- devices/ikea.js
+++ devices/ikea.js
@@ -91,7 +91,7 @@ module.exports = [
         extend: tradfriExtend.light_onoff_brightness(),
     },
     {
-        zigbeeModel: ['TRADFRI bulb E27 opal 470lm', 'TRADFRI bulb E27 W opal 470lm'],
+        zigbeeModel: ['TRADFRI bulb E27 opal 470lm', 'TRADFRI bulb E27 W opal 470lm', 'TRADFRIbulbT120E27WSopal470lm'],
         model: 'LED1937T5',
         vendor: 'IKEA',
         description: 'TRADFRI LED bulb E27 470 lumen, dimmable, opal white',
