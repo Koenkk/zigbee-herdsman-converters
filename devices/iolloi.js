e08fb949ac203f67adbfdf19aaf77fdee600f239
Pedro Lamas <pedrolamas@gmail.com>
2021-07-06 18:23:49 +0100

    Adds support for ID-UK21FW09 (#2766)
    * Adds support for ID-UK21FW09
    
    * Update iolloi.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

---
+++ devices/iolloi.js
@@ -0,0 +1,19 @@
+const reporting = require('../lib/reporting');
+const extend = require('../lib/extend');
+
+module.exports = [
+    {
+        fingerprint: [{modelID: 'Dimmer-Switch-ZB3.0', manufacturerName: 'HZC'}],
+        model: 'ID-UK21FW09',
+        vendor: 'Iolloi',
+        description: 'Zigbee LED smart dimmer switch',
+        extend: extend.light_onoff_brightness({noConfigure: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+            await reporting.brightness(endpoint);
+        },
+    },
+];
