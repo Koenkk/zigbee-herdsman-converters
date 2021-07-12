c8636ad0e00f7bcfe201699c42893459de03201a
Bayview252 <2550321+bayview252@users.noreply.github.com>
2021-07-12 18:19:13 +1000

    Update fromZigbee.js
    fixed for test

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1263,7 +1263,7 @@ const converters = {
         type: 'commandMoveHue',
         convert: (model, msg, publish, options, meta) => {
             const movestop = msg.data.movemode == 1 ? 'move' : 'stop';
-            const action = postfixWithEndpointName(`hue_${movestop}`, msg, model); 
+            const action = postfixWithEndpointName(`hue_${movestop}`, msg, model);
             const payload = {action, action_rate: msg.data.rate};
             addActionGroup(payload, msg, model);
             return payload;
