f55fc68f8d1f3862ba87d1f7fa7510357cc7f893
Ilya Kirov <kirovilya@gmail.com>
2021-07-21 23:15:13 +0300

    Add QBKG41LM (#2819)

--- devices/xiaomi.js
+++ devices/xiaomi.js
@@ -1230,4 +1230,34 @@ module.exports = [
             await endpoint.configureReporting('genAnalogInput', payload);
         },
     },
+    {
+        zigbeeModel: ['lumi.switch.b2nc01'],
+        model: 'QBKG41LM',
+        vendor: 'Xiaomi',
+        description: 'Aqara E1 2 gang switch (with neutral)',
+        fromZigbee: [fz.on_off, fz.xiaomi_power, fz.xiaomi_multistate_action],
+        toZigbee: [tz.on_off, tz.xiaomi_power, tz.xiaomi_switch_operation_mode_opple, tz.xiaomi_switch_power_outage_memory],
+        meta: {multiEndpoint: true},
+        endpoint: (device) => {
+            return {'left': 1, 'right': 2};
+        },
+        exposes: [
+            e.switch().withEndpoint('left'), e.switch().withEndpoint('right'),
+            exposes.enum('operation_mode', ea.ALL, ['control_relay', 'decoupled'])
+                .withDescription('Decoupled mode for left button')
+                .withEndpoint('left'),
+            exposes.enum('operation_mode', ea.ALL, ['control_relay', 'decoupled'])
+                .withDescription('Decoupled mode for right button')
+                .withEndpoint('right'),
+            e.action(['single_left', 'double_left', 'single_right', 'double_right', 'single_both', 'double_both']),
+            e.power_outage_memory(),
+        ],
+        onEvent: preventReset,
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            // set "event" mode
+            await endpoint1.write('aqaraOpple', {'mode': 1}, {manufacturerCode: 0x115f,
+                disableDefaultResponse: true, disableResponse: true});
+        },
+    },
 ];
