3fea660dbf063bd2bb9fdf2853ea049b9ba65c10
SHxKM <6573474+SHxKM@users.noreply.github.com>
2021-07-24 11:23:38 +0300

    Add 5047430P6 (#2826)
    * add support for 5047430P6
    
    * Update philips.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/philips.js
+++ devices/philips.js
@@ -1870,6 +1870,15 @@ module.exports = [
         ota: ota.zigbeeOTA,
     },
     {
+        zigbeeModel: ['5047430P6'],
+        model: '5047430P6',
+        vendor: 'Philips',
+        description: 'Hue White ambiance Buckram quadruple spotlight',
+        meta: {turnsOffAtBrightness1: true},
+        extend: hueExtend.light_onoff_brightness_colortemp({colorTempRange: [153, 454]}),
+        ota: ota.zigbeeOTA,
+    },
+    {
         zigbeeModel: ['5633030P6'],
         model: '5633030P6',
         vendor: 'Philips',
