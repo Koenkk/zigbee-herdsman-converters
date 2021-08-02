c03f938dd8b84994f437216e7fec0bc3bdb764b7
Koen Kanters <koenkanters94@gmail.com>
2021-08-02 17:52:33 +0200

    Add _TZ3000_psqjayrd to TS0203. https://github.com/Koenkk/zigbee2mqtt/issues/8212

--- devices/tuya.js
+++ devices/tuya.js
@@ -13,7 +13,8 @@ module.exports = [
     {
         fingerprint: [{modelID: 'TS0203', manufacturerName: '_TYZB01_xph99wvr'},
             {modelID: 'TS0203', manufacturerName: '_TZ3000_fab7r7mc'},
-            {modelID: 'TS0203', manufacturerName: '_TZ3000_6jeesvrt'}],
+            {modelID: 'TS0203', manufacturerName: '_TZ3000_6jeesvrt'},
+            {modelID: 'TS0203', manufacturerName: '_TZ3000_psqjayrd'}],
         model: 'TS0203',
         vendor: 'TuYa',
         description: 'Door sensor',
