d7c429a6003cf8b5e87dbf1110f587fba4ad0c9d
Robert Alexa <mail@robertalexa.me>
2021-08-05 12:27:20 +0100

    Remove state to pass tests - to be discussed

--- devices/hive.js
+++ devices/hive.js
@@ -295,7 +295,7 @@ module.exports = [
         exposes: [
             exposes.climate().withSetpoint('occupied_heating_setpoint', 5, 32, 0.5).withLocalTemperature()
                 .withSystemMode(['off', 'auto', 'heat']).withRunningState(['idle', 'heat']).withEndpoint('heat'),
-            exposes.climate().withSetpoint('occupied_heating_setpoint', 22, 22, 1, ea.STATE).withLocalTemperature()
+            exposes.climate().withSetpoint('occupied_heating_setpoint', 22, 22, 1).withLocalTemperature()
                 .withSystemMode(['off', 'auto', 'heat']).withRunningState(['idle', 'heat']).withEndpoint('water')],
     },
     {
@@ -334,7 +334,7 @@ module.exports = [
         exposes: [
             exposes.climate().withSetpoint('occupied_heating_setpoint', 5, 32, 0.5).withLocalTemperature()
                 .withSystemMode(['off', 'auto', 'heat']).withRunningState(['idle', 'heat']).withEndpoint('heat'),
-            exposes.climate().withSetpoint('occupied_heating_setpoint', 22, 22, 1, ea.STATE).withLocalTemperature()
+            exposes.climate().withSetpoint('occupied_heating_setpoint', 22, 22, 1).withLocalTemperature()
                 .withSystemMode(['off', 'auto', 'heat']).withRunningState(['idle', 'heat']).withEndpoint('water')],
     },
     {
