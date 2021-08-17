837418d7ea82def48e824522f63acc1f18d71fe8
geduxas <gediminas.vileikis@gmail.com>
2021-08-17 17:56:34 +0300

    Add PF-PM02D-TYZ (#2917)
    * Add PF-PM02D-TYZ smart valve support
    
    * Update iotperfect.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

---
+++ devices/iotperfect.js
@@ -0,0 +1,19 @@
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const extend = require('../lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+module.exports = [
+    {
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_vrjkcam9'}],
+        model: 'PF-PM02D-TYZ',
+        vendor: 'IOTPerfect',
+        description: 'Smart water/gas valve',
+        extend: extend.switch(),
+        exposes: [e.switch().setAccess('state', ea.STATE_SET)],
+        fromZigbee: [fz.tuya_switch, fz.ignore_time_read, fz.ignore_basic_report],
+        toZigbee: [tz.tuya_switch_state],
+    },
+];
