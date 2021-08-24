88777f8634f7df4a80500fc3e9ba877f6e4bdcff
Oste Hovel <OsteHovel@users.noreply.github.com>
2021-08-24 17:01:29 +0200

    Add S4RX-110 (#2947)
    * Support for Onesti Relax Smart Plug
    
    * Update onesti.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/onesti.js
+++ devices/onesti.js
@@ -24,4 +24,29 @@ module.exports = [
         exposes: [e.lock(), e.battery(),
             exposes.enum('sound_volume', ea.ALL, constants.lockSoundVolume).withDescription('Sound volume of the lock')],
     },
+    {
+        zigbeeModel: ['S4RX-110'],
+        model: 'S4RX-110',
+        vendor: 'Onesti Products AS',
+        description: 'Relax smart plug',
+        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.device_temperature, fz.identify],
+        toZigbee: [tz.on_off],
+        exposes: [e.switch(), e.power(), e.current(), e.voltage(), e.energy(), e.device_temperature()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(2);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genIdentify', 'genOnOff', 'genDeviceTempCfg',
+                'haElectricalMeasurement', 'seMetering']);
+            await reporting.onOff(endpoint);
+            await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
+            await reporting.activePower(endpoint);
+            await reporting.rmsCurrent(endpoint);
+            await reporting.rmsVoltage(endpoint);
+            await reporting.readMeteringMultiplierDivisor(endpoint);
+            await reporting.currentSummDelivered(endpoint);
+            await reporting.deviceTemperature(endpoint);
+        },
+        endpoint: (device) => {
+            return {default: 2};
+        },
+    },
 ];
