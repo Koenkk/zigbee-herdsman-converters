564dac96eab2daf069827d25089f0e6a39714277
Tediore <tediore.maliwan@gmail.com>
2021-08-25 15:08:31 -0500

    Remove non-functional battery reporting from Echostar single gang switch

--- devices/echostar.js
+++ devices/echostar.js
@@ -1,6 +1,5 @@
 const exposes = require('../lib/exposes');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
-const reporting = require('../lib/reporting');
 const e = exposes.presets;
 
 module.exports = [
@@ -19,13 +18,8 @@ module.exports = [
         model: 'SAGE206611',
         vendor: 'Echostar',
         description: 'SAGE by Hughes single gang light switch',
-        fromZigbee: [fz.command_on, fz.command_off, fz.battery],
-        exposes: [e.battery(), e.action(['on', 'off'])],
+        fromZigbee: [fz.command_on, fz.command_off],
+        exposes: [e.action(['on', 'off'])],
         toZigbee: [],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(18);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
-            await reporting.batteryPercentageRemaining(endpoint);
-        },
     },
 ];
