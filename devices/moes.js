a763b55ccd67b54a2ef6fc61907f1c2c0f07b669
Koen Kanters <koenkanters94@gmail.com>
2021-09-03 21:10:49 +0200

    Update moes.js

--- devices/moes.js
+++ devices/moes.js
@@ -248,11 +248,11 @@ module.exports = [
         fingerprint: [{modelID: 'TS130F', manufacturerName: '_TZ3000_1dd0d5yi'}],
         model: 'MS-108ZR',
 	vendor: 'Moes',
-        description: 'Zigbee + RF Curtain Switch Module',
+        description: 'Zigbee + RF curtain switch module',
         supports: 'open, close, stop, position',
         fromZigbee: [fz.tuya_cover_options, fz.cover_position_tilt ],
         toZigbee: [tz.cover_state, tz.moes_cover_calibration, tz.cover_position_tilt, tz.tuya_cover_reversal ],
-	exposes: [e.cover_position(), exposes.numeric('calibration_time', ea.STATE_SET), exposes.enum('moving', ea.STATE, ['UP', 'STOP', 'DOWN']),
+	exposes: [e.cover_position(), exposes.numeric('calibration_time', ea.ALL), exposes.enum('moving', ea.STATE, ['UP', 'STOP', 'DOWN']),
             exposes.binary('motor_reversal', ea.ALL, 'ON', 'OFF')], 
     },
 ];
