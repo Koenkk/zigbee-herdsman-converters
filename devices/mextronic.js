a3d0272b301f10c5f860185ee4046c1ac9c73865
jensweimann <jewe1986@hotmail.de>
2021-09-05 11:18:52 +0200

    Add support for Mextronic ZBZIGBEEW12365A LED Controller

---
+++ devices/mextronic.js
@@ -0,0 +1,12 @@
+const extend = require('../lib/extend');
+
+module.exports = [
+    {
+		zigbeeModel: ['iStar DIM Light'],
+		model: 'ZBZIGBEEW12365A',
+		vendor: 'Mextronic',
+		description: 'ZigBee 3.0 LED Controller, dimmable white 12-36V DC max. 5A',
+		extend: extend.light_onoff_brightness(),
+		meta: {turnsOffAtBrightness1: true}
+    },
+];
