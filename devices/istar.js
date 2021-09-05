ee50df6351f05e1b6919c4efd51c360f41919ac0
Koen Kanters <koenkanters94@gmail.com>
2021-09-05 19:57:04 +0200

    Update istar.js

--- devices/istar.js
+++ devices/istar.js
@@ -5,7 +5,7 @@ module.exports = [
         zigbeeModel: ['iStar DIM Light'],
         model: 'SCCV2401-1',
         vendor: 'iStar',
-        description: 'ZigBee 3.0 LED Controller, dimmable white 12-36V DC max. 5A',
+        description: 'Zigbee 3.0 LED controller, dimmable white 12-36V DC max. 5A',
         extend: extend.light_onoff_brightness(),
         meta: {turnsOffAtBrightness1: true}
     },
