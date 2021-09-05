aabf954560b3b4daea6880f062ccbd90bcdb5c97
jensweimann <jewe1986@hotmail.de>
2021-09-05 14:13:55 +0200

    Use iStar model, instead of Mextronic

--- devices/istar.js
+++ devices/istar.js
@@ -3,7 +3,7 @@ const extend = require('../lib/extend');
 module.exports = [
     {
         zigbeeModel: ['iStar DIM Light'],
-        model: 'ZBZIGBEEW12365A',
+        model: 'SCCV2401-1',
         vendor: 'iStar',
         description: 'ZigBee 3.0 LED Controller, dimmable white 12-36V DC max. 5A',
         extend: extend.light_onoff_brightness(),
