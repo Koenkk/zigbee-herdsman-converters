96e693e79651fb559a812b91c8ee4e2b7678408b
airens <45446974+airens@users.noreply.github.com>
2021-09-12 17:50:31 +0300

    Extended support for Legrand 067771
    1. Added support for setting min\max level of brightness 
    2. Added all needed options to `exposes`

--- devices/legrand.js
+++ devices/legrand.js
@@ -158,14 +158,19 @@ module.exports = [
         // led blink RED when battery is low
         description: 'Wired switch without neutral',
         extend: extend.light_onoff_brightness({noConfigure: true}),
-        fromZigbee: [fz.brightness, fz.identify, fz.on_off],
+        fromZigbee: [fz.brightness, fz.identify, fz.on_off, fz.lighting_ballast_configuration],
         toZigbee: [tz.light_onoff_brightness, tz.legrand_settingAlwaysEnableLed, tz.legrand_settingEnableLedIfOn,
-            tz.legrand_settingEnableDimmer, tz.legrand_identify],
-        exposes: [e.light_brightness()],
+            tz.legrand_settingEnableDimmer, tz.legrand_identify, tz.ballast_config],
+        exposes: [e.light_brightness(),
+                  exposes.numeric('ballast_minimum_level', ea.ALL).withValueMin(1).withValueMax(254).withDescription('Specifies the minimum brightness value'),
+                  exposes.numeric('ballast_maximum_level', ea.ALL).withValueMin(1).withValueMax(254).withDescription('Specifies the maximum brightness value'),
+                  exposes.binary('dimmer_enabled', ea.STATE_SET, "ON", "OFF").withDescription('Allow the device to change brightness'), 
+                  exposes.binary('permanent_led', ea.STATE_SET, "ON", "OFF").withDescription('Enable or disable the permanent blue LED'), 
+                  exposes.binary('led_when_on', ea.STATE_SET, "ON", "OFF").withDescription('Enables the LED when the light is on')],
         configure: async (device, coordinatorEndpoint, logger) => {
             await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
             const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genIdentify', 'genOnOff', 'genLevelCtrl', 'genBinaryInput']);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genIdentify', 'genOnOff', 'genLevelCtrl', 'genBinaryInput', 'lightingBallastCfg']);
             await reporting.onOff(endpoint);
             await reporting.brightness(endpoint);
         },
