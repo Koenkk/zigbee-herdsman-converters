07669a72aab9a8cb8eaea2d6c49801369e948e84
danieledwardgeorgehitchcock <daniel.edward.george.hitchcock@gmail.com>
2021-09-24 15:22:05 +0100

    Added support for Philips Hue 1746630P7

--- devices/philips.js
+++ devices/philips.js
@@ -2002,4 +2002,13 @@ module.exports = [
         extend: hueExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
+    {
+        zigbeeModel: ['1746630P7'],
+        model: '1746630P7',
+        vendor: 'Philips',
+        description: 'Hue White and Colour Ambiance Amarant linear outdoor light',
+        meta: { turnsOffAtBrightness1: true },
+        extend: hueExtend.light_onoff_brightness_colortemp_color({ colorTempRange: [153, 500] }),
+        ota: ota.zigbeeOTA,
+    },
 ];
