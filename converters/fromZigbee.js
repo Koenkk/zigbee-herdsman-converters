19ca97da221d6b5a31d36c150c4abb0511a574f5
Ryan Kim(True-World) <nukusinji@naver.com>
2021-09-25 22:44:28 +0900

    ts0201 with illuminace
    https://www.aliexpress.com/item/1005002540793253.html?spm=a2g0s.9042311.0.0.27424c4dX7QekW

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1681,6 +1681,35 @@ const converters = {
             }
         },
     },
+    ts0201_temperature_humidity_alarm: {
+        cluster: 'manuSpecificTuya_2',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty('alarm_temperature_max')) {
+                result.alarm_temperature_max = msg.data['alarm_temperature_max'];
+            }
+            if (msg.data.hasOwnProperty('alarm_temperature_min')) {
+                result.alarm_temperature_min = msg.data['alarm_temperature_min'];
+            }
+            if (msg.data.hasOwnProperty('alarm_humidity_max')) {
+                result.alarm_humidity_max = msg.data['alarm_humidity_max'];
+            }
+            if (msg.data.hasOwnProperty('alarm_humidity_min')) {
+                result.alarm_humidity_min = msg.data['alarm_humidity_min'];
+            }
+            if (msg.data.hasOwnProperty('alarm_humidity')) {
+                const sensorAlarmLookup = {'0': 'MIN_ALARM_ON', '1': 'MAX_ALARM_ON', '2': 'ALARM_OFF'};
+                result.alarm_humidity = sensorAlarmLookup[msg.data['alarm_humidity']];
+            }
+            if (msg.data.hasOwnProperty('alarm_temperature')) {
+                const sensorAlarmLookup = {'0': 'MIN_ALARM_ON', '1': 'MAX_ALARM_ON', '2': 'ALARM_OFF'};
+                result.alarm_temperature = sensorAlarmLookup[msg.data['alarm_temperature']];
+            }	
+			return result;
+        },
+
+    },
     tuya_thermostat_weekly_schedule: {
         cluster: 'manuSpecificTuya',
         type: ['commandGetData', 'commandSetDataResponse'],
