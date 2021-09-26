2795ccbe1b07f2295872289bf85febbcab8a7fb1
bvdberg01 <bas.van.den.berg.van.den.berg@windesheim.nl>
2021-09-27 01:04:23 +0200

    Update centralite.js

--- devices/centralite.js
+++ devices/centralite.js
@@ -205,7 +205,7 @@ module.exports = [
     {
         zigbeeModel: ['3200-fr'],
         model: '3200-fr',
-        vendor: 'CentraLite',
+        vendor: 'Centralite',
         description: 'Smart outlet',
         fromZigbee: [fz.on_off, fz.electrical_measurement],
         toZigbee: [tz.on_off],
