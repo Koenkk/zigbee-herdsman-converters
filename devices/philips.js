099d7a8e9c68a306e31332ef243e058d9e7ea896
danieledwardgeorgehitchcock <daniel.edward.george.hitchcock@gmail.com>
2021-09-27 09:33:41 +0100

    Fixed indent issue

--- devices/philips.js
+++ devices/philips.js
@@ -2019,5 +2019,5 @@ module.exports = [
         meta: {turnsOffAtBrightness1: true},
         extend: hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
         ota: ota.zigbeeOTA,
-   },
+    },
 ];
