aaff98b8a5cdc342870120c66348b3f541fb8bb4
Irimi Tenkan <irimi@gmx.de>
2021-10-08 21:27:03 +0200

    added LED Controller Multi 5 (5110.40)

--- devices/iluminize.js
+++ devices/iluminize.js
@@ -125,6 +125,13 @@ module.exports = [
         extend: extend.light_onoff_brightness_colortemp_color(),
     },
     {
+        zigbeeModel: ['HK-ZD-RGB-A', '5110.40'],
+        model: '5110.40',
+        vendor: 'Iluminize',
+        description: 'Zigbee 3.0 LED Controller Multi 5 - 4A,RGB W/CCT LED',
+        extend: extend.light_onoff_brightness_colortemp_color(),
+    },
+    {
         zigbeeModel: ['HK-ZD-RGBCCT-A', '511.000'],
         model: '511.000',
         vendor: 'Iluminize',
