bea64b1172aff93cffd2a8f903dd35bc26543161
kangaroomadman <6332150+kangaroomadman@users.noreply.github.com>
2021-10-10 11:55:59 +0100

    Add HK-DIM-A

---
+++ devices/candeo.js
@@ -0,0 +1,19 @@
+const reporting = require('../lib/reporting');
+const extend = require('../lib/extend');
+
+module.exports = [
+    {
+        zigbeeModel: ['HK-DIM-A'],
+        model: 'HK-DIM-A',
+        vendor: 'Candeo',
+        description: 'ZigBee LED Dimmer Smart Switch',
+        extend: extend.light_onoff_brightness({noConfigure: true, disableEffect: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+            await reporting.brightness(endpoint);
+        },
+    },
+];
