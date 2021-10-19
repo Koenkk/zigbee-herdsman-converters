ae6bb13ab9da590b83582ea218556d5922949dd7
Aleks <nitro88@yandex.ru>
2021-10-19 20:38:39 +0300

    Add PECLS01 (#3189)
    * Added converter for Perenio device (PECLS01)
    
    * Added new line after line 25
    
    * Update perenio.js
    
    * Update perenio.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

---
+++ devices/perenio.js
@@ -0,0 +1,21 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const e = exposes.presets;
+
+module.exports = [
+    {
+        zigbeeModel: ['PECLS01'],
+        model: 'PECLS01',
+        vendor: 'Perenio',
+        description: 'Flood alarm device',
+        fromZigbee: [fz.ias_water_leak_alarm_1, fz.ignore_basic_report, fz.battery],
+        toZigbee: [],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+        exposes: [e.water_leak(), e.battery_low(), e.tamper(), e.battery()],
+    },
+];
