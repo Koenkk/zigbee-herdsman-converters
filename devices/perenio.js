983dddf189fabd5a85c9d42101ad828b5678ef0a
Koen Kanters <koenkanters94@gmail.com>
2021-10-19 19:36:44 +0200

    Update perenio.js

--- devices/perenio.js
+++ devices/perenio.js
@@ -1,10 +1,7 @@
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
 const e = exposes.presets;
-const ea = exposes.access;
 
 module.exports = [
     {
@@ -14,12 +11,11 @@ module.exports = [
         description: 'Flood alarm device',
         fromZigbee: [fz.ias_water_leak_alarm_1, fz.ignore_basic_report, fz.battery],
         toZigbee: [],
-        configure: async (device, coordinatorEndpoint, logger) => 
-        {
+        configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
             await reporting.batteryPercentageRemaining(endpoint);
         },
         exposes: [e.water_leak(), e.battery_low(), e.tamper(), e.battery()],
     },
-]
+];
