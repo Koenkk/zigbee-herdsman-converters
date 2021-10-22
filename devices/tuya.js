87bcd57fb341901118217e7503c0acdac3d6315c
Koen Kanters <koenkanters94@gmail.com>
2021-10-22 19:19:53 +0200

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -687,7 +687,7 @@ module.exports = [
         zigbeeModel: ['kud7u2l'],
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_ckud7u2l'}, {modelID: 'TS0601', manufacturerName: '_TZE200_ywdxldoj'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_cwnjrr72'}, {modelID: 'TS0601', manufacturerName: '_TZE200_chyvmhay'},
-                      {modelID: 'TS0601', manufacturerName: '_TZE200_a4bpgplm'}],
+            {modelID: 'TS0601', manufacturerName: '_TZE200_a4bpgplm'}],
         model: 'TS0601_thermostat',
         vendor: 'TuYa',
         description: 'Radiator valve with thermostat',
