aab4293c765338f0128e3e8d0ceb59c2c5bb950d
Ryan Kim(True-World) <nukusinji@naver.com>
2021-10-22 21:49:11 +0900

    Sihas multipurpose sensor
    sihas multipurpose sensor
    temperatrue, humidity, illuminance, occupancy
    
    https://smartstore.naver.com/sihas/products/5928899987

--- devices/shinasystem.js
+++ devices/shinasystem.js
@@ -25,4 +25,24 @@ module.exports = [
         exposes: [e.battery(), e.battery_voltage(),
             exposes.numeric('people', ea.ALL).withValueMin(0).withValueMax(50).withDescription('People count')],
     },
+    {
+        zigbeeModel: ['USM-300Z'],
+        model: 'USM-300ZB',
+        vendor: 'ShinaSystem',
+        description: 'Sihas multipurpose sensor',
+        meta: {battery: {voltageToPercentage: '3V_2100'}},
+        fromZigbee: [fz.battery, fz.temperature, fz.humidity, fz.occupancy, fz.illuminance],
+        toZigbee: [],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            const binds = ['genPowerCfg', 'msIlluminanceMeasurement', 'msTemperatureMeasurement', 'msOccupancySensing'];
+            await reporting.bind(endpoint, coordinatorEndpoint, binds);
+            await reporting.batteryVoltage(endpoint, {min: 30, max: 21600, change: 1});
+            await reporting.occupancy(endpoint, {min: 1, max: 600, change: 1});
+            await reporting.temperature(endpoint, {min: 20, max: 300, change: 10});
+            await reporting.humidity(endpoint, {min: 20, max: 300, change: 40});
+            await reporting.illuminance(endpoint, {min: 20, max: 3600, change: 10});
+        },
+        exposes: [e.battery(), e.battery_voltage(), e.temperature(), e.humidity(), e.occupancy(), e.illuminance(), e.illuminance_lux()],
+    },
 ];
