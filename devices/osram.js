3d1135952912eb861a0f0b4ece8148081bad2b19
Danit2 <71522810+Danit2@users.noreply.github.com>
2021-10-30 13:12:37 +0200

    Smal Cosmetics

--- devices/osram.js
+++ devices/osram.js
@@ -382,15 +382,15 @@ module.exports = [
         ota: ota.ledvance,
     },
     {
-        zigbeeModel: ['Zigbee 3.0 DALI CONV LI\u0000'],
+        zigbeeModel: ['Zigbee 3.0 DALI CONV LI'],
         model: '4062172044776',
         vendor: 'OSRAM',
         description: 'OSRAM Zigbee 3.0 DALI CONV LI dimmer for DALI-based luminaires',
         extend: extend.light_onoff_brightness(),
+        exposes: [e.light_brightness().withEndpoint('l1'), e.light_brightness().withEndpoint('l2')],
         endpoint: (device) => {
             return {'l1': 10, 'l2': 11};
         },
-        meta: {multiEndpoint: true},
-        exposes: [e.light_brightness().withEndpoint('l1'), e.light_brightness().withEndpoint('l2')],
+        meta: {multiEndpoint: true},        
     },
 ];
