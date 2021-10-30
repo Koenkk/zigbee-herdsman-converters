70e971d262e4753acb702e813674e7cad6c782bc
Koen Kanters <koenkanters94@gmail.com>
2021-10-30 22:25:09 +0200

    Update osram.js

--- devices/osram.js
+++ devices/osram.js
@@ -385,8 +385,8 @@ module.exports = [
         zigbeeModel: ['Zigbee 3.0 DALI CONV LI'],
         model: '4062172044776',
         vendor: 'OSRAM',
-        description: 'OSRAM Zigbee 3.0 DALI CONV LI dimmer for DALI-based luminaires',
-        extend: extend.light_onoff_brightness(),
+        description: 'Zigbee 3.0 DALI CONV LI dimmer for DALI-based luminaires',
+        extend: extend.ledvance.light_onoff_brightness(),
         exposes: [e.light_brightness().withEndpoint('l1'), e.light_brightness().withEndpoint('l2')],
         endpoint: (device) => {
             return {'l1': 10, 'l2': 11};
