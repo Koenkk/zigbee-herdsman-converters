bd88a0cda075ef5a04ec3acb369e56e018806314
Danit2 <71522810+Danit2@users.noreply.github.com>
2021-10-30 22:30:36 +0200

    Expose 2 endpoints for OSRAM 4062172044776 (#3241)
    * Correct Zigbee DALI Device
    
    - Small correction on the name
    - Make it possible to controll two light when two DALI devices are connectet
    - Delete OTA because there is no Firmware online
    
    * Update osram.js
    
    Sorry i have upload wrong file. :)
    
    * Update osram.js
    
    I have it changed back.
    But in home assistant are only one entitie. 
    and in the state i have tree times the state and brightness.
    '"state_l1": "OFF",
        "state_l2": "OFF",
        "state": "OFF",
        "brightness_l1": 1,
        "brightness_l2": 1,
        "brightness": 23'
    
    * Smal Cosmetics
    
    * Update osram.js
    
    * Update osram.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/osram.js
+++ devices/osram.js
@@ -385,8 +385,12 @@ module.exports = [
         zigbeeModel: ['Zigbee 3.0 DALI CONV LI'],
         model: '4062172044776',
         vendor: 'OSRAM',
-        description: 'OSRAM Zigbee 3.0 DALI CONV LI dimmer for DALI-based luminaires',
+        description: 'Zigbee 3.0 DALI CONV LI dimmer for DALI-based luminaires',
         extend: extend.ledvance.light_onoff_brightness(),
-        ota: ota.ledvance,
+        exposes: [e.light_brightness().withEndpoint('l1'), e.light_brightness().withEndpoint('l2')],
+        endpoint: (device) => {
+            return {'l1': 10, 'l2': 11};
+        },
+        meta: {multiEndpoint: true},
     },
 ];
