e588bd05920e448a9a7d52af467f1c054fb76848
Ilya Kirov <kirovilya@gmail.com>
2021-10-31 21:18:38 +0300

    Perenio Door sensor PECWS01

--- devices/perenio.js
+++ devices/perenio.js
@@ -18,4 +18,18 @@ module.exports = [
         },
         exposes: [e.water_leak(), e.battery_low(), e.tamper(), e.battery()],
     },
+    {
+        zigbeeModel: ['ZHA-DoorLockSensor'],
+        model: 'PECWS01',
+        vendor: 'Perenio',
+        description: 'Door sensor',
+        fromZigbee: [fz.ias_contact_alarm_1, fz.battery, fz.ignore_basic_report, fz.ias_contact_alarm_1_report],
+        toZigbee: [],
+        exposes: [e.contact(), e.battery()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+    },
 ];
