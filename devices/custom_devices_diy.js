b1f56afb54cd72893f2c69bd7e12869647830fdf
Andrew Lamchenko <31867646+smartboxchannel@users.noreply.github.com>
2021-10-31 23:15:46 +0300

    delete tab character

--- devices/custom_devices_diy.js
+++ devices/custom_devices_diy.js
@@ -239,7 +239,7 @@ module.exports = [
         vendor: 'Custom devices (DiY)',
         description: 'DIY outdoor temperature, humidity and atmospheric pressure sensor, long battery life',
         fromZigbee: [fz.temperature, fz.humidity, fz.pressure, fz.battery],
-		toZigbee: [tz.factory_reset],
+	toZigbee: [tz.factory_reset],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, [
@@ -260,7 +260,7 @@ module.exports = [
         vendor: 'Custom devices (DiY)',
         description: '[Mini Plant Wattering Sensor](https://efektalab.com/minipws)',
         fromZigbee: [fz.soil_moisture, fz.battery],
-		toZigbee: [tz.factory_reset],
+	toZigbee: [tz.factory_reset],
         configure: async (device, coordinatorEndpoint, logger) => {
             const firstEndpoint = device.getEndpoint(1);
             await reporting.bind(firstEndpoint, coordinatorEndpoint, ['genPowerCfg', 'msSoilMoisture']);
