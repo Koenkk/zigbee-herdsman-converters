6b5eae6c7372a6d59e95b66076500083675c581d
Thomas Bowman Mørch <thomas.git@bowmo.dk>
2021-11-03 22:41:54 +0100

    add 2ch box relay from envilar

--- devices/envilar.js
+++ devices/envilar.js
@@ -1,5 +1,9 @@
 const reporting = require('../lib/reporting');
 const extend = require('../lib/extend');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const e = exposes.presets;
 
 module.exports = [
     {
@@ -26,4 +30,25 @@ module.exports = [
             await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic', 'genIdentify', 'genOnOff']);
         },
     },
+    {
+        zigbeeModel: ['2CH-ZG-BOX-RELAY'], 
+        model: '2CH-ZG-BOX-RELAY',
+        vendor: 'Envilar',
+        description: '2Ch box relay',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        exposes: [
+            e.switch().withEndpoint('l1'),
+            e.switch().withEndpoint('l2'),
+        ],
+        endpoint: (device) => {
+            return {'l1': 1, 'l2': 2};
+        },
+        meta: {multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint, logger) => {
+                await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
+                await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+        
+        }
+    }
 ];
