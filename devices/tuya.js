db44b2ca4ea89c6646cadf8dfe7638eb1da047c1
Vladimir <45510877+vladi1234@users.noreply.github.com>
2021-11-07 13:18:32 +0100

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -777,33 +777,30 @@ module.exports = [
             e.max_temperature(), e.min_temperature(), e.week(), e.away_preset_temperature()],
     },
     {
-        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_hue3yfsn'}],
+        fingerprint: [
+            {/* model: 'TV02-Zigbee', vendor: 'TuYa',  */modelID: 'TS0601', manufacturerName: '_TZE200_hue3yfsn'}],
         model: 'TV02-Zigbee',
         vendor: 'TuYa',
-        description: 'Thermostat radiator valve ',
+        description: 'Thermostat radiator valve',
         ota: ota.zigbeeOTA,
-        fromZigbee: [fz.ignore_basic_report, fz.ignore_tuya_set_time, fz.tvtwo_thermostat, fz.tuya_data_point_dump],
-        toZigbee: [tz.tvtwo_thermostat, tz.tuya_data_point_test],            
+        fromZigbee: [fz.ignore_basic_report, fz.ignore_tuya_set_time, fz.tvtwo_thermostat],
+        toZigbee: [tz.tvtwo_thermostat],
         onEvent: tuya.onEventSetLocalTime,
         exposes: [
             e.battery_low(), e.window_detection(), e.child_lock(), e.comfort_temperature(), e.eco_temperature(),
             e.open_window_temperature(), e.holiday_temperature(),
-            exposes.binary('frost_protection', ea.STATE_SET, 'ON', 'OFF').withDescription('When Anti-Freezing function '+
-                    'is activated, the temperature in the house is kept at 8 °C ‚the device display "AF".press the pair'+
-                    ' button to cancel.'),
-            exposes.binary('heating_stop', ea.STATE_SET, 'ON', 'OFF').withDescription('Battery life can be prolonged by'+
-                    ' switching the heating off. To achieve this, the valve is closed fully. To activate the heating '+
-                    'stop, the device display "HS" ‚press the pair button to cancel.'),
             exposes.climate().withPreset(['auto', 'manual', 'holiday']).withLocalTemperatureCalibration(ea.STATE_SET)
-                .withLocalTemperature().withSetpoint('current_heating_setpoint', 0, 30, 0.5, ea.STATE_SET),
+                .withLocalTemperature(ea.STATE).withSetpoint('current_heating_setpoint', 0, 30, 0.5, ea.STATE_SET),
             exposes.numeric('boost_timeset_countdown', ea.STATE_SET).withUnit('second').withDescription('Setting '+
                     'minimum 0 - maximum 465 seconds boost time. The boost (♨) function is activated. The remaining '+
                     'time for the function will be counted down in seconds ( 465 to 0 ).'),
-            exposes.numeric('error_status', ea.STATE).withDescription('Error status'),
+            exposes.binary('frost_protection', ea.STATE_SET, 'ON', 'OFF').withDescription('When Anti-Freezing function'+
+                    ' is activated, the temperature in the house is kept at 8 °C ‚the device display "AF".press the '+
+                    'pair button to cancel.'),
+            exposes.binary('heating_stop', ea.STATE_SET, 'ON', 'OFF').withDescription('Battery life can be prolonged'+
+                    ' by switching the heating off. To achieve this, the valve is closed fully. To activate the '+
+                    'heating stop, the device display "HS" ‚press the pair button to cancel.'),
             exposes.binary('online', ea.STATE_SET, 'ON', 'OFF').withDescription('Is the device online'),
-            // exposes.numeric('week_schedule', ea.STATE).withDescription('week_schedule'),
-            // exposes.numeric('working_day', ea.STATE).withDescription('working_day'),
-            // e.week(),
             exposes.numeric('holiday_mode_date', ea.STATE_SET).withDescription('The holiday mode( ⛱ ) will '+
                     'automatically start at the set time starting point and run the holiday temperature.'),
             exposes.composite('programming', 'programming').withDescription('Auto Mode ⏱ - In this mode,'+
@@ -815,6 +812,10 @@ module.exports = [
                 .withFeature(exposes.text('schedule_friday', ea.STATE))
                 .withFeature(exposes.text('schedule_saturday', ea.STATE))
                 .withFeature(exposes.text('schedule_sunday', ea.STATE)),
+            exposes.numeric('error_status', ea.STATE).withDescription('Error status'),
+            // exposes.numeric('week_schedule', ea.STATE).withDescription('week_schedule'),
+            // exposes.numeric('working_day', ea.STATE).withDescription('working_day'),
+            // e.week(),
         ],
     },
     {
