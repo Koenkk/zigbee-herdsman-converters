d5e41a883de731a3897c89329cc899d265aa76b1
Chinh Nguyen <dinhchinh@gmail.com>
2021-11-11 13:45:02 +0700

    format code

--- devices/javis.js
+++ devices/javis.js
@@ -2,8 +2,6 @@ const exposes = require('../lib/exposes');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const tz = require('../converters/toZigbee');
 const e = exposes.presets;
-const reporting = require('../lib/reporting');
-const extend = require('../lib/extend');
 const ea = exposes.access;
 module.exports = [
     {
@@ -28,10 +26,10 @@ module.exports = [
         supports: 'action',
         fromZigbee: [fz.javis_microwave_sensor, fz.ignore_basic_report],
         toZigbee: [tz.javis_microwave_sensor],
-        exposes: [e.occupancy(), e.illuminance_lux(), 
+        exposes: [e.occupancy(), e.illuminance_lux(),
             exposes.binary('led_enable', ea.STATE_SET, true, false).withDescription('Enabled LED'),
-            exposes.enum('keep_time', ea.STATE_SET, ['0', '1', '2', '3', '4', '5', '6', '7']).withDescription('PIR keep \
-            time 0:5s|1:30s|2:60s|3:180s|4:300s|5:600s|6:1200s|7:1800s'),
+            exposes.enum('keep_time', ea.STATE_SET, ['0', '1', '2', '3', '4', '5', '6', '7'])
+                .withDescription('PIR keep time 0:5s|1:30s|2:60s|3:180s|4:300s|5:600s|6:1200s|7:1800s'),
             exposes.enum('sensitivity', ea.STATE_SET, ['25', '50', '75', '100']),
             exposes.numeric('illuminance_calibration', ea.STATE_SET).withDescription('Illuminance calibration')],
     },
