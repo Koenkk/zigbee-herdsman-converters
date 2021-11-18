7a70085b4f6e1e34b0271c414b4647c99e046682
DJTerentjev <DJTerentjev@yandex.ru>
2021-11-18 23:06:14 +0300

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -998,45 +998,6 @@ module.exports = [
                 .withDescription('Recover state after power outage')],
     },
     {
-        fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_w0qqde0g'}, {modelID: 'TS011F', manufacturerName: '_TZ3000_mraovvmm'}],
-        model: 'nas_wr01b',
-        description: 'Smart EU Plug',
-        vendor: 'TuYa',
-        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.ignore_basic_report, fz.tuya_switch_power_outage_memory],
-        toZigbee: [tz.on_off, tz.tuya_switch_power_outage_memory],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement']);
-            endpoint.saveClusterAttributeKeyValue('seMetering', {divisor: 100, multiplier: 1});
-            endpoint.saveClusterAttributeKeyValue('haElectricalMeasurement', {
-                acVoltageMultiplier: 1, acVoltageDivisor: 1, acCurrentMultiplier: 1, acCurrentDivisor: 1000, acPowerMultiplier: 1,
-                acPowerDivisor: 1,
-            });
-        },
-        // This device doesn't support reporting correctly.
-        // https://github.com/Koenkk/zigbee-herdsman-converters/pull/1270
-        exposes: [e.switch(), e.power(), e.current(), e.voltage().withAccess(ea.STATE),
-            e.energy(), exposes.enum('power_outage_memory', ea.STATE_SET, ['on', 'off', 'restore'])
-                .withDescription('Recover state after power outage')],
-        onEvent: (type, data, device, options) => {
-            const endpoint = device.getEndpoint(1);
-
-            if (type === 'stop') {
-                clearInterval(globalStore.getValue(device, 'interval'));
-                globalStore.clearValue(device, 'interval');
-            } else if (!globalStore.hasValue(device, 'interval')) {
-                const seconds = options && options.measurement_poll_interval ? options.measurement_poll_interval : 5;
-                const interval = setInterval(async () => {
-                    try {
-                        await endpoint.read('haElectricalMeasurement', ['rmsVoltage', 'rmsCurrent', 'activePower']);
-                        await endpoint.read('seMetering', ['currentSummDelivered']);
-                    } catch (error) {/* Do nothing*/}
-                }, seconds*1000);
-                globalStore.putValue(device, 'interval', interval);
-            }
-        },
-    },
-    {
         fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_hyfvrar3'}],
         model: 'TS011F_plug_2',
         description: 'Smart plug (without power monitoring)',
@@ -1055,6 +1016,8 @@ module.exports = [
             {modelID: 'TS011F', manufacturerName: '_TZ3000_dpo1ysak'},
             {modelID: 'TS011F', manufacturerName: '_TZ3000_cphmq0q7', applicationVersion: 69},
             {modelID: 'TS011F', manufacturerName: '_TZ3000_5f43h46b'},
+            {modelID: 'TS011F', manufacturerName: '_TZ3000_w0qqde0g'},
+            {modelID: 'TS011F', manufacturerName: '_TZ3000_mraovvmm'},
         ],
         model: 'TS011F_plug_3',
         description: 'Smart plug (with power monitoring by polling)',
