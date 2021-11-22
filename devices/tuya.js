22e9d6b6907aca161e6ab2084b94ed043865c4ee
Koen Kanters <koenkanters94@gmail.com>
2021-11-22 19:54:54 +0100

    Add Lidl 368308_2010 (_TZE200_chyvmhay). https://github.com/Koenkk/zigbee2mqtt/issues/9509

--- devices/tuya.js
+++ devices/tuya.js
@@ -756,7 +756,6 @@ module.exports = [
             {modelID: 'TS0601', manufacturerName: '_TZE200_ckud7u2l'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_ywdxldoj'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_cwnjrr72'},
-            {modelID: 'TS0601', manufacturerName: '_TZE200_chyvmhay'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_pvvbommb'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_2atgpdho'}, // HY367
         ],
