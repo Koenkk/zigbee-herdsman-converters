b42e7b8991b19854aea07e589726f53029cb0d67
airlychee <68546597+airlychee@users.noreply.github.com>
2021-11-22 21:35:11 +0800

    Update xiaomi.js

--- devices/xiaomi.js
+++ devices/xiaomi.js
@@ -1606,7 +1606,7 @@ module.exports = [
         model: 'QBKG41LM',
         vendor: 'Xiaomi',
         description: 'Aqara E1 2 gang switch (with neutral)',
-        fromZigbee: [fz.on_off, fz.xiaomi_multistate_action, fz.xiaomi_multistate_action],
+        fromZigbee: [fz.on_off, fz.xiaomi_multistate_action, fz.xiaomi_switch_opple_basic],
         toZigbee: [tz.on_off, tz.xiaomi_switch_operation_mode_opple, tz.xiaomi_switch_power_outage_memory],
         meta: {multiEndpoint: true},
         endpoint: (device) => {
