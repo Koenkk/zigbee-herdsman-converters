1754d1a7a0542e1c3f2c8f3e971dd684a8fb9d44
Robert Alexa <mail@robertalexa.me>
2021-12-01 09:59:15 +0000

    Fix description

--- devices/hive.js
+++ devices/hive.js
@@ -183,8 +183,8 @@ module.exports = [
             exposes.climate().withSetpoint('occupied_heating_setpoint', 5, 32, 0.5).withLocalTemperature().withPiHeatingDemand()
                 .withSystemMode(['heat']).withRunningState(['idle', 'heat'], ea.STATE),
             exposes.numeric('external_measured_room_sensor', ea.ALL)
-                .withDescription('Set at maximum 3 hours interval but not more often than every 30 minutes at every 100 ' +
-                    'value change. Resets every 3hours to standard. e.g. 21C = 2100 (-8000=undefined).'),
+                .withDescription('Set at maximum 3 hours interval but not more often than every 30 minutes and 0.1 ' +
+                    'degrees difference. Resets every 3hours to standard. e.g. 21C = 2100 (-8000=undefined).'),
             exposes.numeric('window_open_internal', ea.STATE_GET).withValueMin(0).withValueMax(4)
                 .withDescription('0=Quarantine, 1=Windows are closed, 2=Hold - Windows are maybe about to open, ' +
                     '3=Open window detected, 4=In window open state from external but detected closed locally'),
