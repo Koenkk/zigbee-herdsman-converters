199686d63c55fd09b4e5c8f8cd2244ba96f5ffc9
ferrarii1 <90213726+ferrarii1@users.noreply.github.com>
2021-12-02 11:41:18 +0300

    Update tuya.js
    I ask you to add a device, I checked it, it works.

--- devices/tuya.js
+++ devices/tuya.js
@@ -1212,7 +1212,7 @@ module.exports = [
         },
     },
     {
-        fingerprint: [{modelID: 'TS0011', manufacturerName: '_TZ3000_ji4araar'}],
+        fingerprint: [{modelID: 'TS0011', manufacturerName: '_TZ3000_ji4araar'}, {modelID: 'TS0011', manufacturerName: '_TZ3000_qmi1cfuq'}],
         model: 'TS0011_switch_module',
         vendor: 'TuYa',
         description: '1 gang switch module - (without neutral)',
