f3c5a26bb494367c665e528905b2b0316dff50fb
Koen Kanters <koenkanters94@gmail.com>
2021-12-04 15:30:55 +0100

    Update acova.js

--- devices/acova.js
+++ devices/acova.js
@@ -1,28 +1,22 @@
 const exposes = require('../lib/exposes');
-const fz = { ...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee };
+const fz = require('../converters/fromZigbee');
 const tz = require('../converters/toZigbee');
-const ota = require('../lib/ota');
-const constants = require('../lib/constants');
 const reporting = require('../lib/reporting');
-const e = exposes.presets;
 const ea = exposes.access;
 
 module.exports = [{
     zigbeeModel: ['PERCALE2 D1.00P1.01Z1.00'],
     model: 'PERCALE2',
     vendor: 'Acova',
-    description: 'Acova Percale 2 Heater',
-    fromZigbee: [
-        fz.thermostat,
-        fz.hvac_user_interface
-    ],
+    description: 'Percale 2 heater',
+    fromZigbee: [fz.thermostat, fz.hvac_user_interface],
     toZigbee: [
         tz.thermostat_local_temperature,
         tz.thermostat_system_mode,
         tz.thermostat_occupied_heating_setpoint,
         tz.thermostat_unoccupied_heating_setpoint,
         tz.thermostat_occupied_cooling_setpoint,
-        tz.thermostat_running_state
+        tz.thermostat_running_state,
     ],
     exposes: [
         exposes.climate()
@@ -38,4 +32,4 @@ module.exports = [{
         await reporting.thermostatOccupiedHeatingSetpoint(endpoint);
         await reporting.thermostatUnoccupiedHeatingSetpoint(endpoint);
     },
-}]
+}];
