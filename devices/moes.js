b1902945cdc6229aabbe463b39991a44a7c914f8
Vladimir <45510877+vladi1234@users.noreply.github.com>
2021-12-05 19:10:26 +0100

    Update moes.js

--- devices/moes.js
+++ devices/moes.js
@@ -225,8 +225,7 @@ module.exports = [
                 'You can set up to 4 stages of temperature every for WEEKDAY ➀➁➂➃➄,  SATURDAY ➅ and SUNDAY ➆.'),
             exposes.binary('boost_heating', ea.STATE_SET, 'ON', 'OFF').withDescription('Boost Heating: press and hold "+" for 3 seconds, ' +
                 'the device will enter the boost heating mode, and the ▷╵◁ will flash. The countdown will be displayed in the APP'),
-            exposes.numeric('boost_heating_countdown', ea.STATE).withUnit('min').withDescription('Countdown in minutes')
-                .withValueMin(0).withValueMax(15),
+            exposes.text('boost_heating_countdown', ea.STATE).withDescription('Countdown in minutes'),
             exposes.numeric('boost_heating_countdown_time_set', ea.STATE_SET).withUnit('second')
                 .withDescription('Boost Time Setting 100 sec - 900 sec, (default = 300 sec)').withValueMin(100).withValueMax(900)],
     },
