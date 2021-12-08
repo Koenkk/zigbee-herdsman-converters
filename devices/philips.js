b37fda717832ee013052ea52a9e85722e2db1692
peedub <peedub@gmail.com>
2021-12-09 05:35:15 +1300

    Add 1743430P7 (#3475)
    * Update philips.js
    
    Include updated revision of Hue Impress outdoor Pedestal (1743430P7)
    
    * Update philips.js
    
    added color temp range

--- devices/philips.js
+++ devices/philips.js
@@ -405,6 +405,15 @@ module.exports = [
         ota: ota.zigbeeOTA,
     },
     {
+        zigbeeModel: ['1743430P7'],
+        model: '1743430P7',
+        vendor: 'Philips',
+        description: 'Hue Impress outdoor Pedestal',
+        meta: {turnsOffAtBrightness1: true},
+        extend: hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
+        ota: ota.zigbeeOTA,
+    },
+    {
         zigbeeModel: ['1740193P0'],
         model: '1740193P0',
         vendor: 'Philips',
