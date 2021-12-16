43a94b540cdbfcb174b4b3480d03d0bc270512c5
Vladimir <45510877+vladi1234@users.noreply.github.com>
2021-12-16 22:22:36 +0100

    Update fromZigbee.js

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -3571,7 +3571,7 @@ const converters = {
             case tuya.dataPoints.moesSheatingSetpoint:
                 return {current_heating_setpoint: value};
             case tuya.dataPoints.moesSlocalTemp:
-                return {local_temperature: (value / 10)};
+                return {local_temperature: (value / 10).toFixed(1)};
             case tuya.dataPoints.moesSboostHeating:
                 return {boost_heating: value ? 'ON' : 'OFF'};
             case tuya.dataPoints.moesSboostHeatingCountdown:
