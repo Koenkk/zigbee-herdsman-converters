2f0a982162aabf2bf8d7aa2738b33baa7f76fd40
pixel::doc <pixeldoc2000@users.noreply.github.com>
2021-12-18 00:56:10 +0100

    Update ksentry.js
    Link is not working anymore and does blow up zigbee2mqtt_networkmap unnecessarily.

--- devices/ksentry.js
+++ devices/ksentry.js
@@ -6,8 +6,7 @@ module.exports = [
         zigbeeModel: ['Lamp_01'],
         model: 'KS-SM001',
         vendor: 'Ksentry Electronics',
-        description: '[Zigbee OnOff Controller](http://ksentry.manufacturer.globalsources.com/si/6008837134660'+
-                     '/pdtl/ZigBee-module/1162731630/zigbee-on-off-controller-modules.htm)',
+        description: '[Zigbee OnOff Controller]',
         extend: extend.switch(),
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(11);
