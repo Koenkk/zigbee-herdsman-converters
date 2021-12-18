86719623ba2d3feaa582d74ed28fea325435b4d8
DavidFFerreira <32928749+DavidFFerreira@users.noreply.github.com>
2021-12-18 21:07:08 +0000

    Update lellki.js

--- devices/lellki.js
+++ devices/lellki.js
@@ -8,7 +8,8 @@ const ea = exposes.access;
 
 module.exports = [
     {
-        fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_air9m6af'}, {modelID: 'TS011F', manufacturerName: '_TZ3000_9djocypn'}, 
+        fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_air9m6af'}, 
+                      {modelID: 'TS011F', manufacturerName: '_TZ3000_9djocypn'}, 
                       {modelID: 'TS011F', manufacturerName: '_TZ3000_bppxj3sf'}],
         zigbeeModel: ['JZ-ZB-005'],
         model: 'WP33-EU',
