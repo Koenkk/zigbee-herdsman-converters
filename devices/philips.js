5aa94a424d8f15d08663aa841c2b4cb1e9337e80
Burkhard Kneiseler <burkhard@kneiseler.de>
2021-12-23 19:15:30 +0100

    Expose action_duration for Philips 324131092621 (#3557)
    * Update philips.js
    
    Added "duration" to exposes. Need this for using dimming-function in Symcon. Hopefully, it's okay for all.
    
    * Update philips.js
    
    Sorry, line to long.
    
    * Update philips.js
    
    Next try, used to many spaces.
    
    * Update philips.js
    
    * Update philips.js
    
    Wrong unit used, sorry.
    
    * Update philips.js
    
    Change from legacy action to action_duration.
    Thanks koenkk.
    
    * Update philips.js
    
    Sorry, small Error
    
    * Update philips.js
    
    Hi Koenk,
    
    I'm really thankfull for your support. That are mistakes, I do only once a time. But to my apologies, I am really a complete beginner at programming. 
    Hopefully now it's all okay. On my local machine, it's running well.
    
    Many, many thanks.
    
    * Update philips.js
    
    * Update philips.js
    
    * Update philips.js

--- devices/philips.js
+++ devices/philips.js
@@ -1748,7 +1748,8 @@ module.exports = [
         fromZigbee: [fz.ignore_command_on, fz.ignore_command_off, fz.ignore_command_step, fz.ignore_command_stop,
             fz.legacy.hue_dimmer_switch, fz.battery],
         exposes: [e.battery(), e.action(['on-press', 'on-hold', 'on-hold-release', 'up-press', 'up-hold', 'up-hold-release',
-            'down-press', 'down-hold', 'down-hold-release', 'off-press', 'off-hold', 'off-hold-release'])],
+            'down-press', 'down-hold', 'down-hold-release', 'off-press', 'off-hold', 'off-hold-release']),
+        exposes.numeric('action_duration', ea.STATE).withUnit('second')],
         toZigbee: [],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint1 = device.getEndpoint(1);
