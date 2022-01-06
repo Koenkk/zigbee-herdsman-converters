9a0f37bb2a3907d04de37ed6931d34be69c58c63
Eray Çetinay <eraycetinay@gmail.com>
2022-01-06 14:58:19 +0300

    _TZ3000_ji4araar added on TS0011_switch_module
    https://github.com/Koenkk/zigbee-herdsman-converters/pull/3561/files/f4739d4f1db42f8de58611b89c0c0c3339984735
    Explained here.

--- devices/tuya.js
+++ devices/tuya.js
@@ -1293,7 +1293,7 @@ module.exports = [
         },
     },
     {
-        fingerprint: [{modelID: 'TS0011', manufacturerName: '_TZ3000_qmi1cfuq'},
+        fingerprint: [{modelID: 'TS0011', manufacturerName: '_TZ3000_ji4araar'}, {modelID: 'TS0011', manufacturerName: '_TZ3000_qmi1cfuq'},
             {modelID: 'TS0011', manufacturerName: '_TZ3000_txpirhfq'}],
         model: 'TS0011_switch_module',
         vendor: 'TuYa',
