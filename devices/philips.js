30ed5fc8de31e2612bafa135819106c39e292b32
Koen Kanters <koenkanters94@gmail.com>
2022-01-06 21:38:32 +0100

    Support color for 929003047501 https://github.com/Koenkk/zigbee2mqtt/issues/10617

--- devices/philips.js
+++ devices/philips.js
@@ -35,7 +35,7 @@ module.exports = [
         vendor: 'Philips',
         description: 'Centura recessed spotlight',
         meta: {turnsOffAtBrightness1: true},
-        extend: hueExtend.light_onoff_brightness_colortemp({colorTempRange: [153, 500]}),
+        extend: hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
         ota: ota.zigbeeOTA,
     },
     {
