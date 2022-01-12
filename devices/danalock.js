7ccca8cf2d7ec853ac556d16ea9e79c2aa39d981
XLDom <90308906+XLDom@users.noreply.github.com>
2022-01-12 18:49:57 +0100

    Support pin add/delete for Danalock V3-BTZB/V3-BTZBE (#3665)
    When Danapad installed (https://www.danalock.fr/danapad), we can create/update/remove the pincode

--- devices/danalock.js
+++ devices/danalock.js
@@ -10,14 +10,15 @@ module.exports = [
         model: 'V3-BTZB/V3-BTZBE',
         vendor: 'Danalock',
         description: 'BT/ZB smartlock',
-        fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery],
-        toZigbee: [tz.lock],
+        fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery, fz.lock_programming_event, fz.lock_pin_code_response],
+        toZigbee: [tz.lock, tz.pincode_lock, tz.lock_userstatus],
+        meta: {pinCodeCount: 20},
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
             await reporting.lockState(endpoint);
             await reporting.batteryPercentageRemaining(endpoint);
         },
-        exposes: [e.lock(), e.battery()],
+        exposes: [e.lock(), e.battery(), e.pincode()],
     },
 ];
