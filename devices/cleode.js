4d501fecbd7a03dfdef11ceb5a76027c389d28c4
Koen Kanters <koenkanters94@gmail.com>
2022-01-12 19:31:57 +0100

    Add ZPLUG_Boost. https://github.com/Koenkk/zigbee2mqtt/discussions/10693

---
+++ devices/cleode.js
@@ -0,0 +1,23 @@
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const reporting = require('../lib/reporting');
+const e = exposes.presets;
+
+module.exports = [
+    {
+        zigbeeModel: ['ZPLUG'],
+        model: 'ZPLUG_Boost',
+        vendor: 'CLEODE',
+        description: 'ZPlug boost',
+        meta: {multiEndpoint: true},
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        exposes: [e.switch(), e.power()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'seMetering']);
+            await reporting.readMeteringMultiplierDivisor(endpoint);
+        },
+    },
+];
