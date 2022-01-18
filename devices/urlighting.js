2ca408aea89e5e5b54d30a41facb2d9610f520e2
Koen Kanters <koenkanters94@gmail.com>
2022-01-18 16:46:54 +0100

    Update urlighting.js

--- devices/urlighting.js
+++ devices/urlighting.js
@@ -1,14 +1,12 @@
-const exposes = require('../lib/exposes');
-const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const extend = require('../lib/extend');
-const e = exposes.presets;
 
 module.exports = [
     {
         fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_dn5higyl'}],
         model: 'TH008L10RGBCCT',
         vendor: 'UR Lighting',
-        description: 'UR Lighting 10W RGB+CCT downlight',
+        description: '10W RGB+CCT downlight',
+        meta: {applyRedFix: true, enhancedHue: false},
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
 ];
