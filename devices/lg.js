b439fdc4c46d697d8caee3bc27e413cbe5a81568
Koen Kanters <koenkanters94@gmail.com>
2022-01-19 17:34:39 +0100

    Update lg.js

--- devices/lg.js
+++ devices/lg.js
@@ -5,14 +5,14 @@ module.exports = [
         zigbeeModel: ['B1027EB0Z01'],
         model: 'B1027EB0Z01',
         vendor: 'LG Electronics',
-        description: 'LG Smart Bulb 1',
+        description: 'Smart bulb 1',
         extend: extend.light_onoff_brightness(),
     },
     {
         zigbeeModel: ['B1027EB0Z02'],
         model: 'B1027EB0Z02',
         vendor: 'LG Electronics',
-        description: 'LG Smart Bulb 2',
+        description: 'Smart bulb 2',
         extend: extend.light_onoff_brightness(),
     },
 ];
