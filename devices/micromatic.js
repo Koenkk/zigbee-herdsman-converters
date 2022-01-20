863070839cc78b2abecf29c76bb321d711ff257c
geir råness <git@xmod.no>
2022-01-20 14:55:50 +0000

    add support for micro matic zb250 / SZ1000

---
+++ devices/micromatic.js
@@ -0,0 +1,29 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+    zigbeeModel: ['SZ1000'],
+    model: 'ZB250',
+    vendor: 'Micro Matic Norge AS',
+    description: 'Zigbee dimmer for LED',
+    fromZigbee: extend.light_onoff_brightness().fromZigbee.concat([fz.electrical_measurement, fz.metering]),
+    toZigbee: extend.light_onoff_brightness().toZigbee,
+    exposes: [e.light_brightness(), e.power(), e.current(), e.voltage().withAccess(ea.STATE), e.energy()],
+    configure: async (device, coordinatorEndpoint, logger) => {
+        await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+        const endpoint = device.getEndpoint(1);
+        await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl', 'haElectricalMeasurement', 'seMetering']);
+        await reporting.brightness(endpoint);
+        await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
+        await reporting.rmsVoltage(endpoint);
+        await reporting.rmsCurrent(endpoint, {change: 50});
+        await reporting.activePower(endpoint);
+        await reporting.currentSummDelivered(endpoint);
+    },
+};
+
+module.exports = definition;
