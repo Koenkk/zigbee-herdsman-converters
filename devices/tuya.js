c1fec80063a16f369ef5ba2d1440ca289f27c981
Koenkk <koenkanters94@gmail.com>
2022-01-25 16:37:54 +0100

    Add _TZ3000_q6vxaod1 to TS0014. https://github.com/Koenkk/zigbee2mqtt/issues/10918

--- devices/tuya.js
+++ devices/tuya.js
@@ -1435,7 +1435,8 @@ module.exports = [
     {
         fingerprint: [{modelID: 'TS0014', manufacturerName: '_TZ3000_jr2atpww'}, {modelID: 'TS0014', manufacturerName: '_TYZB01_dvakyzhd'},
             {modelID: 'TS0014', manufacturerName: '_TZ3210_w3hl6rao'}, {modelID: 'TS0014', manufacturerName: '_TYZB01_bagt1e4o'},
-            {modelID: 'TS0014', manufacturerName: '_TZ3000_r0pmi2p3'}, {modelID: 'TS0014', manufacturerName: '_TZ3000_fxjdcikv'}],
+            {modelID: 'TS0014', manufacturerName: '_TZ3000_r0pmi2p3'}, {modelID: 'TS0014', manufacturerName: '_TZ3000_fxjdcikv'},
+            {modelID: 'TS0014', manufacturerName: '_TZ3000_q6vxaod1'}],
         model: 'TS0014',
         vendor: 'TuYa',
         description: 'Smart light switch - 4 gang without neutral wire',
