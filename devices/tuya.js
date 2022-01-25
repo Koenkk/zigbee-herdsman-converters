9f2bba35c35f527a1fea2d2c701e9b709d48dada
Jimmypl007 <98302541+Jimmypl007@users.noreply.github.com>
2022-01-25 23:41:01 +0800

    Add TS0501A (_TZ3000_yeg1e5eh) (#3732)
    * Adding Tuya Highbay Light TS0501A _TZ3000_yeg1e5eh
    
    I Apologise if i am doing anything wrong, I am really new to this, after fumbling through the tutorials I managed to get these lights supported my side.
    and now i am struggling to figure out what i am doing here on GitHub, any help appreciated.
    
    I think my next step is to upload a photo somehow over at the  zigbee2mqtt.io GitHub, and probably fail at adding an .md file..
    
    The High-bay light is fairly simple it has some "scenes" option through tuya's app that i would think makes sense that they aren't available through the extension.
    also (I guess) the extend exposes some "effect" option that no matter what you choose just does the same sort of "fading blink" that is the same as when the lights are in pairing mode.
    
    I also apologise if this is not the area to say any of this..
    
    * Update tuya.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/tuya.js
+++ devices/tuya.js
@@ -203,6 +203,13 @@ module.exports = [
         meta: {applyRedFix: true},
     },
     {
+        fingerprint: [{modelID: 'TS0501A', manufacturerName: '_TZ3000_yeg1e5eh'}],
+        model: 'TS0501A',
+        description: 'Zigbee light',
+        vendor: 'TuYa',
+        extend: extend.light_onoff_brightness(),
+    },
+    {
         fingerprint: [{modelID: 'TS0501B', manufacturerName: '_TZ3000_4whigl8i'},
             {modelID: 'TS0501B', manufacturerName: '_TZ3210_4whigl8i'},
             {modelID: 'TS0501B', manufacturerName: '_TZ3210_9q49basr'},
