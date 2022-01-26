83880b4299630a00df902476d9dd901b1dcd79be
Bjarne Riis <bjarne@briis.com>
2022-01-26 12:13:32 +0100

    Adding 545D6306 - LK FUGA Wiser wireless PIR with Relay

--- devices/schneider_electric.js
+++ devices/schneider_electric.js
@@ -642,4 +642,35 @@ module.exports = [
             await reporting.currentSummDelivered(endpoint, {min: 60, change: 1});
         },
     },
+    {
+        zigbeeModel: ['NHMOTION/SWITCH/1'],
+        model: '545D6306',
+        vendor: 'Schneider Electric',
+        description: 'LK FUGA Wiser wireless PIR with Relay',
+        fromZigbee: [fz.on_off, fz.illuminance, fz.occupancy, fz.occupancy_timeout],
+        exposes: [e.switch().withEndpoint('l1'), e.occupancy(), e.illuminance_lux(), e.illuminance(),
+            exposes.numeric('occupancy_timeout', ea.ALL).withUnit('second').withValueMin(0).withValueMax(3600).withDescription('Time in seconds after which occupancy is cleared after detecting it')
+        ],
+        toZigbee: [tz.on_off, tz.occupancy_timeout],
+        endpoint: (device) => {
+            return { 'default': 37, 'l1': 1, 'l2': 37 };
+        },
+        meta: { multiEndpoint: true },
+        configure: async(device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            const binds1 = ['genBasic', 'genIdentify', 'genOnOff'];
+            await reporting.bind(endpoint1, coordinatorEndpoint, binds1);
+            await reporting.onOff(endpoint1);
+            // read switch state
+            await endpoint1.read('genOnOff', ['onOff']);
+
+            const endpoint37 = device.getEndpoint(37);
+            const binds37 = ['msIlluminanceMeasurement', 'msOccupancySensing'];
+            await reporting.bind(endpoint37, coordinatorEndpoint, binds37);
+            await reporting.occupancy(endpoint37);
+            await reporting.illuminance(endpoint37);
+            // read occupancy_timeout
+            await endpoint37.read('msOccupancySensing', ['pirOToUDelay']);
+        },
+    },
 ];
