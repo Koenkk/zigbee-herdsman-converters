88c7c006609689e4b9f0fd009f21c4e853e5377a
xfzha <xfzhao@vip.qq.com>
2022-01-30 20:37:05 +0800

    fix typo

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -2219,7 +2219,7 @@ const converters = {
                 // Turn on dimming channel 1 and channel 2
                 await entity.write('aqaraOpple', {0x050F: {value: 3, type: 0x23}}, manufacturerOptions.xiaomi);
             }
-            return {state: {device_mode: value}};
+            return {state: {dimmer_mode: value}};
         },
         convertGet: async (entity, key, value, meta) => {
             await entity.read('aqaraOpple', [0x0509], manufacturerOptions.xiaomi);
