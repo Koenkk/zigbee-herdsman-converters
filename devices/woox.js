33460ce05b754acdfa3ea9fd6dd02b0e2d19ff66
Koen Kanters <koenkanters94@gmail.com>
2022-02-10 19:34:25 +0100

    Fix imports

--- devices/woox.js
+++ devices/woox.js
@@ -1,5 +1,5 @@
 const exposes = require('../lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const reporting = require('../lib/reporting');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const tz = require('../converters/toZigbee');
 const tuya = require('../lib/tuya');
