5c08c975d483881407b2925018e2a442a3225aa3
Koen Kanters <koenkanters94@gmail.com>
2022-02-13 17:25:31 +0100

    Update profalux.js

--- devices/profalux.js
+++ devices/profalux.js
@@ -1,9 +1,31 @@
 const fz = require('../converters/fromZigbee');
 const tz = require('../converters/toZigbee');
+const utils = require('../lib/utils');
 const exposes = require('../lib/exposes');
 const reporting = require('../lib/reporting');
 const e = exposes.presets;
 const ea = exposes.access;
+
+const tzLocal = {
+    profalux_cover_state: {
+        key: ['state'],
+        convertSet: async (entity, key, value, meta) => {
+            switch (value) {
+            case 'OPEN':
+                await entity.command('genOnOff', 'on', {}, utils.getOptions(meta.mapped, entity));
+                break;
+            case 'CLOSE':
+                await entity.command('genOnOff', 'off', {}, utils.getOptions(meta.mapped, entity));
+                break;
+            case 'STOP':
+                await entity.command('genLevelCtrl', 'stop', {}, utils.getOptions(meta.mapped, entity));
+                break;
+            default:
+                throw new Error(`Value '${value}' is not a valid cover position (must be one of 'OPEN' or 'CLOSE' or 'STOP')`);
+            }
+        },
+    },
+};
 
 module.exports = [
     {
@@ -12,7 +34,7 @@ module.exports = [
         vendor: 'Profalux',
         description: 'Visio cover with position control',
         fromZigbee: [fz.identify, fz.cover_state_via_onoff, fz.cover_position_via_brightness],
-        toZigbee: [tz.profalux_cover_state, tz.cover_via_brightness],
+        toZigbee: [tzLocal.profalux_cover_state, tz.cover_via_brightness],
         exposes: [e.cover_position().setAccess('position', ea.STATE_SET)],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
