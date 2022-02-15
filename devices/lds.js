25b3fb99b2f0604058b615a23109bbcbc990191b
Koen Kanters <koenkanters94@gmail.com>
2022-02-15 17:04:48 +0100

    Update lds.js

--- devices/lds.js
+++ devices/lds.js
@@ -5,7 +5,7 @@ module.exports = [
         zigbeeModel: ['ZBT-RGBWLight-A0000'],
         model: 'ZBT-RGBWLight-A0000',
         vendor: 'LDS',
-        description: 'Ynoa Smart LED E27',
+        description: 'Ynoa smart LED E27',
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 555]}),
     },
 ];
