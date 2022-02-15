82f32eff277a8e9e84c2e4d8b8171dab736032ac
Rene Honig <renehonig@gmail.com>
2022-02-15 11:18:58 +0100

    Update lds.js
    fix type issue

--- devices/lds.js
+++ devices/lds.js
@@ -1,11 +1,11 @@
 const extend = require('../lib/extend');
 
-const definition = {
-    zigbeeModel: ['ZBT-RGBWLight-A0000'],
-    model: 'ZBT-RGBWLight-A0000',
-    vendor: 'LDS',
-    description: 'Ynoa Smart LED E27',
-    extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 555]}),
-};
-
-module.exports = definition;
+module.exports = [
+    {
+        zigbeeModel: ['ZBT-RGBWLight-A0000'],
+        model: 'ZBT-RGBWLight-A0000',
+        vendor: 'LDS',
+        description: 'Ynoa Smart LED E27',
+        extend: extend.light_onoff_brightness_colortemp_color({ colorTempRange: [153, 555] }),
+    },
+];
