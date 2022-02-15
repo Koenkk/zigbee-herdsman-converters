b8beb8ac325c85cf5b5892a4435b52cf7ed3fcf2
Rene Honig <renehonig@gmail.com>
2022-02-15 10:41:46 +0100

    Update lds.js
    fix lint issues

--- devices/lds.js
+++ devices/lds.js
@@ -1,10 +1,4 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const extend = require('zigbee-herdsman-converters/lib/extend');
-const e = exposes.presets;
-const ea = exposes.access;
 
 const definition = {
     zigbeeModel: ['ZBT-RGBWLight-A0000'],
