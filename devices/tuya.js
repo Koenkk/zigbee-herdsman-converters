2ace66f2aafdd63161a76030094a85c543a13965
vladispavlov <vladyslav.pavlov@outlook.com>
2022-02-17 02:37:14 +0200

    Add battery_voltage to TS0203

--- devices/tuya.js
+++ devices/tuya.js
@@ -51,13 +51,14 @@ module.exports = [
         description: 'Door sensor',
         fromZigbee: [fz.ias_contact_alarm_1, fz.battery, fz.ignore_basic_report, fz.ias_contact_alarm_1_report],
         toZigbee: [],
-        exposes: [e.contact(), e.battery_low(), e.tamper(), e.battery()],
+        exposes: [e.contact(), e.battery_low(), e.tamper(), e.battery(), e.battery_voltage()],
         whiteLabel: [{vendor: 'CR Smart Home', model: 'TS0203'}],
         configure: async (device, coordinatorEndpoint, logger) => {
             try {
                 const endpoint = device.getEndpoint(1);
                 await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
                 await reporting.batteryPercentageRemaining(endpoint);
+                await reporting.batteryVoltage(endpoint);
             } catch (error) {/* Fails for some*/}
         },
     },
