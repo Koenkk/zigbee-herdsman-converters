6269d77288655ae715a0444d74fa0dc2f7314fc3
Koen Kanters <koenkanters94@gmail.com>
2022-02-20 14:28:43 +0100

    Update bega.js

--- devices/bega.js
+++ devices/bega.js
@@ -15,7 +15,7 @@ module.exports = [
     {
         zigbeeModel: ['BEGA 13557 bulb E27 RGBW 805lm'],
         model: '13557',
-        vendor: 'BEGA Gantenbrink-Leuchten KG',
+        vendor: 'Bega',
         description: 'LED lamp with adjustable LED color temperature (Tunable White - RGBW) for use in luminaires with E27 lamp base',
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 556]}),
     },
