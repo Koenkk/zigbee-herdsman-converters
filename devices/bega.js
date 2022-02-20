ff26eeb335a139b69985b3b34b52c1e587480295
natenjo <natenjo@users.noreply.github.com>
2022-02-20 12:57:35 +0100

    Added BEGA 13557 bulb E27 RGBW
    not sure about the part "Fingerprint" which I left out, as this is the first time I am adding something to Zigbee2MQTT, following the following documentation https://www.zigbee2mqtt.io/advanced/support-new-devices/01_support_new_devices.html#_3-adding-converter-s-for-your-device

--- devices/bega.js
+++ devices/bega.js
@@ -12,4 +12,11 @@ module.exports = [
         description: 'Zigbee control module DALI',
         extend: extend.light_onoff_brightness(),
     },
+    {
+       zigbeeModel: ['BEGA 13557 bulb E27 RGBW 805lm'], 
+       model: '13557', 
+       vendor: 'BEGA Gantenbrink-Leuchten KG',
+       description: 'LED-Leuchtmittel mit einstellbarer LED Farbtemperatur (Tunable White · RGBW) in funkgesteuerter Ausführung Zigbee 3.0 für den Einsatz in Leuchten mit konventionellem Lampensockel E 27.',
+       extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 556]}),
+    },
 ];
