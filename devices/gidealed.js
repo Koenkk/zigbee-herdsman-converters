1dd21bffb661ffbe3911dbe651430e1161c01867
Koen Kanters <koenkanters94@gmail.com>
2022-02-21 17:22:30 +0100

    Update gidealed.js

--- devices/gidealed.js
+++ devices/gidealed.js
@@ -1,17 +1,11 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const extend = require('zigbee-herdsman-converters/lib/extend');
-const e = exposes.presets;
-const ea = exposes.access;
 
-const definition = {
-    zigbeeModel: ['A11'], 
-    model: 'ZC05M', 
-    vendor: 'GIDEALED', 
-    description: 'Smart Zigbee RGB LED strip controller',
-    extend: extend.light_onoff_brightness_colortemp_color({supportsHS: true, colorTempRange: [153, 500]}), 
-};
-
-module.exports = definition;
+module.exports = [
+    {
+        zigbeeModel: ['A11'],
+        model: 'ZC05M',
+        vendor: 'GIDEALED',
+        description: 'Smart Zigbee RGB LED strip controller',
+        extend: extend.light_onoff_brightness_colortemp_color({supportsHS: true, colorTempRange: [153, 500]}),
+    },
+];
