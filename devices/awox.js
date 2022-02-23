59a3e2e509e65f905a96b2912cba3ec4da45bcad
Koen Kanters <koenkanters94@gmail.com>
2022-02-23 19:23:30 +0100

    Add additional fingerprint for 33957. https://github.com/Koenkk/zigbee2mqtt/issues/11577

--- devices/awox.js
+++ devices/awox.js
@@ -117,6 +117,11 @@ module.exports = [
                 {ID: 1, profileID: 260, deviceID: 268, inputClusters: [0, 3, 4, 5, 6, 8, 768, 4096, 64599], outputClusters: [6]},
                 {ID: 3, profileID: 4751, deviceID: 268, inputClusters: [65360, 65361], outputClusters: [65360, 65361]},
             ]},
+            {type: 'Router', manufacturerName: 'AwoX', modelID: 'TLSR82xx', endpoints: [
+                {ID: 1, profileID: 260, deviceID: 268, inputClusters: [0, 3, 4, 5, 6, 8, 768, 4096, 64599], outputClusters: [6]},
+                {ID: 242, profileID: 41440, deviceID: 97, inputClusters: [], outputClusters: [33]},
+                {ID: 3, profileID: 4751, deviceID: 268, inputClusters: [65360, 65361], outputClusters: [65360, 65361]},
+            ]},
         ],
         model: '33957',
         vendor: 'AwoX',
