92eeccbce376f42cac15c9493c6eb432151800c9
Knodd <trond@rossvoll.net>
2022-03-02 00:43:07 +0100

    Update elko.js
    Add support for EKO05806 (remote control for 316GLEDRF)

--- devices/elko.js
+++ devices/elko.js
@@ -22,6 +22,20 @@ module.exports = [
         },
     },
     {
+        zigbeeModel: ['ElkoDimmerRemoteZHA'],
+        model: 'EKO05806',
+        vendor: 'ELKO',
+        description: 'Elko ESH 316 Endevender RF',
+        fromZigbee: [fz.command_toggle,fz.command_step],
+        toZigbee: [],
+        exposes: [e.action(['toggle', 'brightness_step_up', 'brightness_step_down'])],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff','genLevelCtrl']);
+            await reporting.onOff(endpoint);
+        },
+    },
+    {
         zigbeeModel: ['Super TR'],
         model: '4523430',
         vendor: 'ELKO',
