0c30580371c349547874a4662d6c0d5b3cdcf20c
mephistos <pierre.collin52@gmail.com>
2022-03-03 18:13:50 +0100

    Add R7051 (#3915)
    * Add support to woox R7051
    
    * Fix build error
    
    * Fix lint errors
    
    * Fix PR from Koenkk
    
    * Update fromZigbee.js
    
    * Update woox.js
    
    * Update woox.js
    
    Co-authored-by: Pierre <pierre@asterapp.co>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1018,6 +1018,16 @@ const converters = {
             };
         },
     },
+    ias_alarm_only_alarm_1: {
+        cluster: 'ssIasZone',
+        type: 'attributeReport',
+        convert: (model, msg, publish, options, meta) => {
+            const zoneStatus = msg.data.zoneStatus;
+            return {
+                alarm: (zoneStatus & 1) > 0,
+            };
+        },
+    },
     ias_occupancy_only_alarm_2: {
         cluster: 'ssIasZone',
         type: 'commandStatusChangeNotification',
@@ -2593,6 +2603,16 @@ const converters = {
                 result['alarm'] = (msg.data['61440'] == 0) ? false : true;
             }
             return result;
+        },
+    },
+    ts0219_power_source: {
+        cluster: 'genBasic',
+        type: 'attributeReport',
+        convert: (model, msg, publish, options, meta) => {
+            const powerSource = msg.data.powerSource;
+            return {
+                ac_connected: powerSource === 2 ? true : false,
+            };
         },
     },
     tuya_cover_options: {
