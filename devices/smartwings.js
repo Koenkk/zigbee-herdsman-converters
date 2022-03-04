ce5d66859e76ff2dbe22c22969da3d701b33fbc9
Jeffrey Stone <thejeffreystone@gmail.com>
2022-03-04 09:08:43 -0500

    Fixing lint errors

--- devices/smartwings.js
+++ devices/smartwings.js
@@ -2,15 +2,13 @@ const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
 const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
 const e = exposes.presets;
-const ea = exposes.access;
 
 module.exports = {
-    zigbeeModel: ['WM25/L-Z'], 
-    model: 'WM25/L-Z', 
-    vendor: 'Smartwings', 
-    description: 'Roller Shade', 
+    zigbeeModel: ['WM25WM25\u002FL-Z'],
+    model: 'WM25\u002FL-Z',
+    vendor: 'Smartwings',
+    description: 'Roller Shade',
     fromZigbee: [fz.cover_position_tilt, fz.battery],
     toZigbee: [tz.cover_state, tz.cover_position_tilt],
     meta: {battery: {dontDividePercentage: true}},
