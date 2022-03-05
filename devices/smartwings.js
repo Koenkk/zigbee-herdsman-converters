d55c1d7d1f6fa72782772513f64f17054411796a
Jeffrey Stone <thejeffreystone@gmail.com>
2022-03-04 21:10:03 -0500

    Ok, figured out that error.

--- devices/smartwings.js
+++ devices/smartwings.js
@@ -1,22 +1,24 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const reporting = require('../lib/reporting');
 const e = exposes.presets;
 
-module.exports = {
-    zigbeeModel: ['WM25/L-Z'],
-    model: 'WM25L-Z',
-    vendor: 'Smartwings',
-    description: 'Roller Shade',
-    fromZigbee: [fz.cover_position_tilt, fz.battery],
-    toZigbee: [tz.cover_state, tz.cover_position_tilt],
-    meta: {battery: {dontDividePercentage: true}},
-    configure: async (device, coordinatorEndpoint, logger) => {
-        const endpoint = device.getEndpoint(1);
-        await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
-        await reporting.batteryPercentageRemaining(endpoint);
-        await reporting.currentPositionLiftPercentage(endpoint);
+module.exports = [
+    {
+        zigbeeModel: ['WM25/L-Z'],
+        model: 'WM25L-Z',
+        vendor: 'Smartwings',
+        description: 'Roller Shade',
+        fromZigbee: [fz.cover_position_tilt, fz.battery],
+        toZigbee: [tz.cover_state, tz.cover_position_tilt],
+        meta: {battery: {dontDividePercentage: true}},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.currentPositionLiftPercentage(endpoint);
+        },
+        exposes: [e.cover_position(), e.battery()],
     },
-    exposes: [e.cover_position(), e.battery()],
-};
+];
