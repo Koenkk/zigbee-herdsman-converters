bef0cc99eb4dd5ea1c32d3b132c83b5ba67374b6
Jeffrey Stone <slackerlabsha@gmail.com>
2022-03-05 03:51:41 -0500

    Add WM25L-Z (#3953)
    * Smartwings WM25L-Z Roller Shade Converter
    
    * Update smartwings.js
    
    * Fixing lint errors
    
    * Fixing device naming
    
    * Ok, figured out that error.
    
    * Update smartwings.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

---
+++ devices/smartwings.js
@@ -0,0 +1,24 @@
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const reporting = require('../lib/reporting');
+const e = exposes.presets;
+
+module.exports = [
+    {
+        zigbeeModel: ['WM25/L-Z'],
+        model: 'WM25L-Z',
+        vendor: 'Smartwings',
+        description: 'Roller shade',
+        fromZigbee: [fz.cover_position_tilt, fz.battery],
+        toZigbee: [tz.cover_state, tz.cover_position_tilt],
+        meta: {battery: {dontDividePercentage: true}},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.currentPositionLiftPercentage(endpoint);
+        },
+        exposes: [e.cover_position(), e.battery()],
+    },
+];
