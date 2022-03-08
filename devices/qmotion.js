22c60566e41d04bb86eeb6b31c45e1b62c11e092
James Brown <james@catch22.net>
2022-03-08 18:15:18 +0000

    position & battery for Qmotion HDM40PV620 (#3968)

--- devices/qmotion.js
+++ devices/qmotion.js
@@ -3,6 +3,7 @@ const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/lega
 const tz = require('../converters/toZigbee');
 const e = exposes.presets;
 const ea = exposes.access;
+const reporting = require('../lib/reporting');
 
 module.exports = [
     {
@@ -19,8 +20,14 @@ module.exports = [
         model: 'HDM40PV620',
         vendor: 'Qmotion',
         description: 'Motorized roller blind',
-        fromZigbee: [fz.identify],
+        fromZigbee: [fz.identify, fz.cover_position_tilt, fz.battery],
         toZigbee: [tz.cover_state, tz.cover_position_tilt],
-        exposes: [e.cover_position()],
+        exposes: [e.cover_position(), e.battery()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.currentPositionLiftPercentage(endpoint);
+        },
     },
 ];
