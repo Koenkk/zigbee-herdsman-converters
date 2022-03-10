9eda7c2a3dac9325382b9a5723ce3627e1ad550a
Adam <78630270+adam-moyer@users.noreply.github.com>
2022-03-10 18:51:57 -0500

    Added color temp range

--- devices/philips.js
+++ devices/philips.js
@@ -1187,7 +1187,7 @@ module.exports = [
         vendor: 'Philips',
         description: 'Hue white ambiance 4" retrofit recessed downlight', 
         meta: {turnsOffAtBrightness1: true},
-        extend: hueExtend.light_onoff_brightness_colortemp(),
+        extend: hueExtend.light_onoff_brightness_colortemp({colorTempRange: [153, 454]}),
         ota: ota.zigbeeOTA,
     },
     {
