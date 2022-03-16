027adac28a3536ed4a0f1f76db137c36f18065eb
Koen Kanters <koenkanters94@gmail.com>
2022-03-16 19:14:25 +0100

    Update plugwise.js

--- devices/plugwise.js
+++ devices/plugwise.js
@@ -22,23 +22,23 @@ module.exports = [
         },
         exposes: [e.switch(), e.power(), e.energy()],
     },
-	{
-		zigbeeModel: ['106-03'], 
-		model: '106-03', 
-		vendor: 'Plugwise', 
-		description: 'Tom thermostatic radiator valve', 
-		fromZigbee: [fz.thermostat, fz.temperature, fz.battery], 
-		toZigbee: [tz.thermostat_system_mode, tz.thermostat_occupied_heating_setpoint, tz.thermostat_pi_heating_demand], 
-		configure: async (device, coordinatorEndpoint, logger) => {
-			const endpoint = device.getEndpoint(1);
-			await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic', 'genPowerCfg', 'hvacThermostat']);
-			await reporting.batteryPercentageRemaining(endpoint);
-		},
-		exposes: [e.battery(),
-				exposes.climate()
-					.withSetpoint('occupied_heating_setpoint', 5, 30, 0.5, ea.STATE_SET).withLocalTemperature(ea.STATE)
-					.withSystemMode(['off', 'auto'], ea.STATE_SET)
-					.withPiHeatingDemand(ea.STATE),
-			], 
-	},
+    {
+        zigbeeModel: ['106-03'],
+        model: '106-03',
+        vendor: 'Plugwise',
+        description: 'Tom thermostatic radiator valve',
+        fromZigbee: [fz.thermostat, fz.temperature, fz.battery],
+        toZigbee: [tz.thermostat_system_mode, tz.thermostat_occupied_heating_setpoint, tz.thermostat_pi_heating_demand],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic', 'genPowerCfg', 'hvacThermostat']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+        exposes: [e.battery(),
+            exposes.climate()
+                .withSetpoint('occupied_heating_setpoint', 5, 30, 0.5, ea.STATE_SET).withLocalTemperature(ea.STATE)
+                .withSystemMode(['off', 'auto'], ea.STATE_SET)
+                .withPiHeatingDemand(ea.STATE),
+        ],
+    },
 ];
