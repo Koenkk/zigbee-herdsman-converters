f4af130a49a50d42eff5747715df9e253b49d0f0
dphroon <77174861+dphroon@users.noreply.github.com>
2022-03-19 20:39:45 +1100

    Add  CH/Socket/2 10a double socket

--- devices/schneider_electric.js
+++ devices/schneider_electric.js
@@ -694,4 +694,25 @@ module.exports = [
         },
         exposes: [e.battery(), e.illuminance(), e.illuminance_lux(), e.occupancy()],
     },
+    {
+        zigbeeModel: ['CH/Socket/2'],
+        model: 'Iconic 395',
+        vendor: 'Schneider Electric',
+        description: 'Connected 10a socket',
+        fromZigbee: [fz.ignore_genOta, fz.on_off],
+        toZigbee: [tz.on_off],
+        exposes: [ e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2')],
+        meta: {multiEndpoint: true},
+        endpoint: (device) => {
+            return {'l1': 1, 'l2': 2};
+        },
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint1);
+            const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint2);
+        },
+    },
 ];
