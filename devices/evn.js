240fa1cfa33802d3f261f8a6d348b58369c310cd
aleg47 <96729544+aleg47@users.noreply.github.com>
2022-03-26 14:39:36 +0100

    Add ZB24100VS (#4033)
    * new vendor file evn.js with 4 channel RGBW remote control ZBHS4RGBW added
    
    * Update EVN model ZBHS4RGBW
    
    * Add EVN device ZB24100VS

--- devices/evn.js
+++ devices/evn.js
@@ -1,26 +1,31 @@
 const exposes = require('../lib/exposes');
+const extend = require('../lib/extend');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
-const reporting = require('../lib/reporting');
 const e = exposes.presets;
 
 module.exports = [
     {
-        zigbeeModel: ['EVN ZBHS4RGBW'],
+        zigbeeModel: ['ZBHS4RGBW'],
         model: 'ZBHS4RGBW',
         vendor: 'EVN',
         description: 'Zigbee 4 channel RGBW remote control',
-        fromZigbee: [fz.battery, fz.command_on, fz.command_off, fz.command_step, fz.command_move, fz.command_stop, fz.command_recall,
-            fz.command_move_hue, fz.command_move_to_color, fz.command_move_to_color_temp],
-        exposes: [e.battery(), e.action(['on', 'off', 'brightness_step_up', 'brightness_step_down', 'brightness_move_up',
-            'brightness_move_down', 'brightness_stop', 'recall_1', 'recall_2', 'recall_3', 'hue_move', 'color_temperature_move',
-            'color_move', 'hue_stop'])],
+        fromZigbee: [fz.battery, fz.command_move_to_color, fz.command_move_to_color_temp, fz.command_move_hue,
+            fz.command_step, fz.command_stop, fz.command_move, fz.command_recall, fz.command_on, fz.command_off],
+        exposes: [e.battery(), e.action([
+            'color_move', 'color_temperature_move', 'brightness_step_up', 'brightness_step_down',
+            'brightness_move_up', 'brightness_move_down', 'brightness_stop',
+            'hue_move', 'hue_stop', 'recall_*', 'on', 'off'])],
         toZigbee: [],
         meta: {multiEndpoint: true, battery: {dontDividePercentage: true}},
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genPowerCfg']);
-            await reporting.batteryVoltage(endpoint);
-            await reporting.batteryPercentageRemaining(endpoint);
+        endpoint: (device) => {
+            return {ep1: 1, ep2: 2, ep3: 3, ep4: 4};
         },
+    },
+    {
+        zigbeeModel: ['ZB24100VS'],
+        model: 'ZB24100VS',
+        vendor: 'EVN',
+        description: 'Zigbee multicolor controller with power supply',
+        extend: extend.light_onoff_brightness_colortemp_color({supportsHS: true, colorTempRange: [160, 450]}),
     },
 ];
