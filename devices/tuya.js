2d3b6e24cee8e16b2552db15ad90948b6ff173f4
SignalStealer <mate.sk@gmail.com>
2022-04-03 00:50:24 +0200

    Added configure method for TS0202_2

--- devices/tuya.js
+++ devices/tuya.js
@@ -330,8 +330,14 @@ module.exports = [
         vendor: 'Tesla Smart',
         description: 'Tesla Smart Sensor Motion',
         fromZigbee: [fz.ias_occupancy_alarm_1, fz.battery, fz.ias_occupancy_alarm_1_report],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.batteryVoltage(endpoint);
+        },
         toZigbee: [],
-        exposes: [e.occupancy(), e.battery_low(), e.tamper()],
+        exposes: [e.occupancy(), e.battery_low(), e.tamper(), e.battery(), e.battery_voltage()],
     },
     {
         fingerprint: [{modelID: 'TS0202', manufacturerName: '_TYZB01_jytabjkb'}],
