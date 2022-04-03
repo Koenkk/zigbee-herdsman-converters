3d7bc6960270dea5eccb6ffae421410bd4e4c4d9
mirakels <marcel@mesa.nl>
2022-04-03 21:11:36 +0200

    Add LA-GU10-RGBW and LA-5KEY-RGBW (#4077)
    * Add Ynoa Smart LED Gu10 RGB CCT (LA-GU10-RGBW) and 5 button control for RGBW light (LA-5KEY-RGBW)
    
    * ynoa.js: fix lint errors
    
    * Update ynoa.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/ynoa.js
+++ devices/ynoa.js
@@ -2,6 +2,7 @@ const exposes = require('../lib/exposes');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const reporting = require('../lib/reporting');
 const e = exposes.presets;
+const extend = require('..//lib/extend');
 
 module.exports = [
     {
@@ -11,6 +12,29 @@ module.exports = [
         description: 'Remote control one button dimmer',
         fromZigbee: [fz.command_on, fz.command_off, fz.command_move, fz.command_stop, fz.battery],
         exposes: [e.action(['on', 'off', 'brightness_move_up', 'brightness_move_down', 'brightness_stop']), e.battery()],
+        toZigbee: [],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+    },
+    {
+        zigbeeModel: ['ZBT-RGBWLight-M0000'],
+        model: 'LA-GU10-RGBW',
+        vendor: 'Ynoa',
+        description: 'Smart LED GU10 RGB CCT',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 526], supportsHS: true}),
+    },
+    {
+        zigbeeModel: ['ZBT-RGBWSwitch-D0800'],
+        model: 'LA-5KEY-RGBW',
+        vendor: 'Ynoa',
+        description: '5 key control for RGBW light',
+        fromZigbee: [fz.command_on, fz.command_off, fz.command_move_to_color_temp,
+            fz.command_move_to_color, fz.command_move_to_level, fz.battery],
+        exposes: [e.battery(), e.battery_low(), e.action(['on', 'off', 'brightness_move_to_level',
+            'color_temperature_move', 'color_move'])],
         toZigbee: [],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
