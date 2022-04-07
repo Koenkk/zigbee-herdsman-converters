23622ce4409138aa28cad86bf71d51d76fd9e3e5
Koen Kanters <koenkanters94@gmail.com>
2022-04-07 22:24:37 +0200

    Add _TZ3210_mzdax7ha to TS0505B. https://github.com/Koenkk/zigbee2mqtt/issues/12105

--- devices/tuya.js
+++ devices/tuya.js
@@ -281,6 +281,7 @@ module.exports = [
             {modelID: 'TS0505B', manufacturerName: '_TZ3210_leyz4rju'},
             {modelID: 'TS0505B', manufacturerName: '_TZ3210_jd3z4yig'},
             {modelID: 'TS0505B', manufacturerName: '_TZ3210_dgdjiw1c'},
+            {modelID: 'TS0505B', manufacturerName: '_TZ3210_mzdax7ha'},
             {modelID: 'TS0505B', manufacturerName: '_TZ3210_a4s41wm4'},
             {modelID: 'TS0505B', manufacturerName: '_TZ3210_qxenlrin'}],
         model: 'TS0505B',
