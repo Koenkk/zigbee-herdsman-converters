44c3b326539c9853b6de3473f50be9f4e70e1362
Mikko Puustelli <puustelli@gmail.com>
2022-04-09 22:44:20 +0300

    Added VBLED
    Added VBLED vendor and Zigbee model 43023

---
+++ devices/vbled.js
@@ -0,0 +1,18 @@
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+
+module.exports = [
+    {
+        zigbeeModel: ['43023'],
+        model: '43023',
+        vendor: 'VBLED',
+        description: 'ZigBee AC phase-cut dimmer',
+        extend: extend.light_onoff_brightness({noConfigure: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+        },
+    },
+];
