43bdede173f595658043b1b81538096d135db929
Koen Kanters <koenkanters94@gmail.com>
2022-04-10 09:57:42 +0200

    Update micromatic.js

--- devices/micromatic.js
+++ devices/micromatic.js
@@ -19,10 +19,6 @@ module.exports = [
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl', 'haElectricalMeasurement', 'seMetering']);
             await reporting.brightness(endpoint);
-            // Unit missing acFrequencyDivisor and acFrequencyMultiplier, so we need to target specific units in haElectricalMeasurement
-            await endpoint.read('haElectricalMeasurement', ['acPowerMultiplier', 'acPowerDivisor', 'acCurrentDivisor']);
-            await endpoint.read('haElectricalMeasurement', ['acVoltageMultiplier', 'acVoltageDivisor', 'acCurrentMultiplier']);
-            // end
             await reporting.readMeteringMultiplierDivisor(endpoint);
             await reporting.rmsVoltage(endpoint, {min: 10, change: 20}); // Voltage - Min change of 2V
             await reporting.rmsCurrent(endpoint, {min: 10, change: 10}); // A - z2m displays only the first decimals, change of 10 / 0,01A
