9d0abe00893b054446b6fef1de63c4ddef3f952d
Gimlir <39776670+Gimlir@users.noreply.github.com>
2022-04-15 12:15:03 +0300

    Make model name unique for separated device

--- devices/tuya.js
+++ devices/tuya.js
@@ -1124,7 +1124,7 @@ module.exports = [
         fingerprint: [
             {modelID: 'TS0601', manufacturerName: '_TZE200_fzo2pocs'},
         ],
-        model: 'TS0601_cover',
+        model: 'ZM25TQ_cover',
         vendor: 'TuYa',
         description: 'Tubular motor',
         whiteLabel: [
