3bb60d3b3741b618fb5513f6aeda88614678dac5
AlexKay88 <a.kann@gmx.de>
2022-04-25 11:13:48 +0200

    registered tuya.dataPoints.fantem*

--- converters/toZigbee.js
+++ converters/toZigbee.js
@@ -6495,14 +6495,16 @@ const converters = {
         convertSet: async (entity, key, value, meta) => {
             switch (key) {
             case 'ext_switch_type':
-                await tuya.sendDataPointEnum(entity, 103, {'unknown': 0, 'toggle_sw': 1,
+                await tuya.sendDataPointEnum(entity, tuya.dataPoints.fantemExtSwitchType, {'unknown': 0, 'toggle_sw': 1,
                     'momentary_sw': 2, 'rotary_sw': 3, 'auto_config': 4}[value], 4);
                 break;
             case 'load_detection_mode':
-                await tuya.sendDataPointEnum(entity, 105, {'none': 0, 'first_power_on': 1, 'every_power_on': 2}[value], 4);
+                await tuya.sendDataPointEnum(entity, tuya.dataPoints.fantemLoadDetectionMode, {'none': 0, 'first_power_on': 1,
+                    'every_power_on': 2}[value], 4);
                 break;
             case 'control_mode':
-                await tuya.sendDataPointEnum(entity, 109, {'ext_switch': 0, 'remote': 1, 'both': 2}[value], 4);
+                await tuya.sendDataPointEnum(entity, tuya.dataPoints.fantemControlMode, {'ext_switch': 0, 'remote': 1,
+                    'both': 2}[value], 4);
                 break;
             default: // Unknown key
                 throw new Error(`tz.ZB006X_settings: Unhandled key ${key}`);
