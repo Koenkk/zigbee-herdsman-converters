2cb557d553a86d43c5a1e89c5965efdb44f434d0
Pla5ma <57805183+Pla5ma@users.noreply.github.com>
2022-04-27 17:59:17 +0200

    Update giderwel.js
    Added A11 smart LED strip.

--- devices/giderwel.js
+++ devices/giderwel.js
@@ -8,4 +8,11 @@ module.exports = [
         description: 'Smart Zigbee RGB LED strip controller',
         extend: extend.light_onoff_brightness_color({supportsHS: false}),
     },
+    {
+        zigbeeModel: ['A11'],
+        model: 'ZC05M-5',
+        vendor: 'GIDERWEL',
+        description: 'Smart Zigbee RGB LED strip controller',
+        extend: extend.light_onoff_brightness_color({supportsHS: false}),
+    },
 ];
