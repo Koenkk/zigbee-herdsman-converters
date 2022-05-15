7b0cbc720feebf27d0a1c0cbc8adad1931b345b8
corchat <58449080+corchat@users.noreply.github.com>
2022-05-15 15:27:13 +0200

    Update sunricher.js

--- devices/sunricher.js
+++ devices/sunricher.js
@@ -7,6 +7,25 @@ const e = exposes.presets;
 
 module.exports = [
     {
+        zigbeeModel: ['SR-ZG9023A(EU)'],
+        model: 'SR-ZG9023A(EU)',
+        vendor: 'Sunricher',
+        description: '4 ports switch with 2 usb ports (no metering)',
+        extend: extend.switch(),
+        exposes: [e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2'), e.switch().withEndpoint('l3'), e.switch().withEndpoint('l4'), e.switch().withEndpoint('l5')],
+        endpoint: (device) => {
+            return {'l1': 1, 'l2': 2,'l3': 3,'l4': 4,'l5': 5};
+        },
+        meta: {multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(3), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(4), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(5), coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
         zigbeeModel: ['ON/OFF(2CH)'],
         model: 'UP-SA-9127D',
         vendor: 'Sunricher',
