b1f13b226aa315c885b7393922222e46768336db
Koen Kanters <koenkanters94@gmail.com>
2022-05-16 16:31:27 +0200

    Update rademacher.js

--- devices/rademacher.js
+++ devices/rademacher.js
@@ -5,7 +5,7 @@ module.exports = [
         zigbeeModel: ['RDM-35104001'],
         model: '35104001',
         vendor: 'Rademacher',
-        description: 'addZ White + Colour',
+        description: 'addZ white + colour',
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 555]}),
     },
 ];
