5177b7749a47f448e3c26a7b0bffa51112716cba
Koen Kanters <koenkanters94@gmail.com>
2022-05-17 16:50:48 +0200

    V3-BTZB/V3-BTZBE doesn't support changing pincodes. https://github.com/Koenkk/zigbee-herdsman-converters/pull/3665

--- devices/danalock.js
+++ devices/danalock.js
@@ -10,15 +10,14 @@ module.exports = [
         model: 'V3-BTZB/V3-BTZBE',
         vendor: 'Danalock',
         description: 'BT/ZB smartlock',
-        fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery, fz.lock_programming_event, fz.lock_pin_code_response],
-        toZigbee: [tz.lock, tz.pincode_lock, tz.lock_userstatus],
-        meta: {pinCodeCount: 20},
+        fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery],
+        toZigbee: [tz.lock],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
             await reporting.lockState(endpoint);
             await reporting.batteryPercentageRemaining(endpoint);
         },
-        exposes: [e.lock(), e.battery(), e.pincode(), e.lock_action(), e.lock_action_source_name(), e.lock_action_source_user()],
+        exposes: [e.lock(), e.battery(), e.lock_action(), e.lock_action_source_name(), e.lock_action_source_user()],
     },
 ];
