bac0b39ce21bbeabc4e52da420f6db864306857a
Fco Javier Felix <ffelix@inode64.com>
2022-05-19 07:36:24 +0200

    Fix max line length

--- devices/xiaomi.js
+++ devices/xiaomi.js
@@ -1567,7 +1567,8 @@ module.exports = [
         // Ignore energy metering reports, rely on aqara_opple: https://github.com/Koenkk/zigbee2mqtt/issues/10709
         fromZigbee: [fz.on_off, fz.device_temperature, fz.aqara_opple, fz.ignore_metering, fz.ignore_electrical_measurement,
             fz.xiaomi_power],
-        exposes: [e.switch(), e.energy(), e.power(), e.device_temperature(), e.power_outage_memory(), e.power_outage_count(),
+        exposes: [e.switch(), e.energy(), e.power(), e.device_temperature(),
+            e.power_outage_memory(), e.power_outage_count(),
             e.switch_type(), e.voltage(), e.temperature(), e.current(),
         ],
         toZigbee: [tz.xiaomi_switch_type, tz.on_off, tz.xiaomi_switch_power_outage_memory, tz.xiaomi_led_disabled_night],
