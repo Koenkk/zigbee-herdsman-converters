f60f09cd21cce5ce09cdf9a1a9d353924c0e8614
Evan <17254809+bigun27@users.noreply.github.com>
2022-05-23 09:48:05 -0400

    Remove trailing comma from 3157100 description

--- devices/centralite.js
+++ devices/centralite.js
@@ -164,7 +164,7 @@ module.exports = [
         fingerprint: [{modelID: '3157100', manufacturerName: 'Centralite'}, {modelID: '3157100-E', manufacturerName: 'Centralite'}],
         model: '3157100',
         vendor: 'Centralite',
-        description: '3-Series pearl touch thermostat,',
+        description: '3-Series pearl touch thermostat',
         fromZigbee: [fz.battery, fz.thermostat, fz.fan, fz.ignore_time_read],
         toZigbee: [tz.factory_reset, tz.thermostat_local_temperature, tz.thermostat_local_temperature_calibration,
             tz.thermostat_occupied_heating_setpoint, tz.thermostat_occupied_cooling_setpoint,
