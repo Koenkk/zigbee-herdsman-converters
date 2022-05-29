9c7852bb3a747f3f6046ed4bcf7e5d45c7d8119e
Pferdebockwurst <37925404+Pferdebockwurst@users.noreply.github.com>
2022-05-29 14:01:03 +0200

    Add 404038 (#4298)
    * Update müller_licht.js
    
    Add support for Müller Licht Tint retro filament LED-bulb E27, Globe bulb gold, white+ambiance (1800-6500K), dimmable, 5,5W.
    
    Link to product page: https://www.mueller-licht.de/produktinformationen/artikel/?q=404038
    
    This light bulb is technically similar to the model 404037, the only difference is the shape of the bulb.
    
    * Update müller_licht.js
    
    * Update müller_licht.js
    
    * Update müller_licht.js

--- devices/müller_licht.js
+++ devices/müller_licht.js
@@ -165,6 +165,14 @@ module.exports = [
         toZigbee: extend.light_onoff_brightness_colortemp().toZigbee.concat([tz.tint_scene]),
     },
     {
+        zigbeeModel: ['tint-ColorTemperature2'],
+        model: '404038',
+        vendor: 'Müller Licht',
+        description: 'Tint retro filament LED-bulb E27, Globe bulb gold, white+ambiance (1800-6500K), dimmable, 5,5W',
+        extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 555]}),
+        toZigbee: extend.light_onoff_brightness_colortemp().toZigbee.concat([tz.tint_scene]),
+    },
+    {
         fingerprint: [{
             // Identify through fingerprint as modelID is the same as Sunricher ZG192910-4
             type: 'Router', manufacturerID: 4635, manufacturerName: 'MLI', modelID: 'CCT Lighting',
