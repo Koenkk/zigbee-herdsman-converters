8a5bcd91cc7028824d89fa42e4396d32fc296c97
Phil-ibert <59770946+Phil-ibert@users.noreply.github.com>
2022-06-02 13:34:37 +0200

    Added PC321 into OWON files
    Fitted PC321 device into OWON and FromZigbee files

--- devices/owon.js
+++ devices/owon.js
@@ -119,4 +119,46 @@ module.exports = [
             device.save();
         },
     },
+	{
+        zigbeeModel: ['PC321'], // The model ID from: Device with modelID 'lumi.sens' is not supported.
+        model: 'PC321', // Vendor model number, look on the device for a model number
+        vendor: 'OWON', // Vendor of the device (only used for documentation and startup logging)
+        description: '3-Phase Clamp Power Meter', // Description of the device, copy from vendor site. (only used for documentation and startup logging)
+        fromZigbee: [fz.metering, fz.PC321_metering],
+        toZigbee: [], // Should be empty, unless device can be controlled (e.g. lights, switches).
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['seMetering']);
+            await reporting.readMeteringMultiplierDivisor(endpoint);
+            //const payload = [{
+            //        attribute: 'owonL2Energy',
+            //        minimumReportInterval: 5,
+            //        maximumReportInterval: 3600,
+            //        reportableChange: 0,
+            //    }];
+                await endpoint.configureReporting('haElectricalMeasurement', payload);
+        },
+        exposes: [e.energy(),
+        exposes.numeric('voltage_l1',ea.STATE).withUnit('V').withDescription('Phase 1 Voltage'),
+        exposes.numeric('voltage_l2',ea.STATE).withUnit('V').withDescription('Phase 2 Voltage'),
+        exposes.numeric('voltage_l3',ea.STATE).withUnit('V').withDescription('Phase 3 Voltage'),
+        exposes.numeric('current_l1',ea.STATE).withUnit('A').withDescription('Phase 1 Current'),
+        exposes.numeric('current_l2',ea.STATE).withUnit('A').withDescription('Phase 2 Current'),
+        exposes.numeric('current_l3',ea.STATE).withUnit('A').withDescription('Phase 3 Current'),
+        exposes.numeric('energy_l1',ea.STATE).withUnit('kWh').withDescription('Phase 1 Energy'),
+        exposes.numeric('energy_l2',ea.STATE).withUnit('kWh').withDescription('Phase 2 Energy'),
+        exposes.numeric('energy_l3',ea.STATE).withUnit('kWh').withDescription('Phase 3 Energy'),
+        exposes.numeric('reactive_energy_l1',ea.STATE).withUnit('kVArh').withDescription('Phase 1 Reactive Energy'),
+        exposes.numeric('reactive_energy_l2',ea.STATE).withUnit('kVArh').withDescription('Phase 2 Reactive Energy'),
+        exposes.numeric('reactive_energy_l3',ea.STATE).withUnit('kVArh').withDescription('Phase 3 Reactive Energy'),
+        exposes.numeric('power_l1',ea.STATE).withUnit('W').withDescription('Phase 1 Power'),
+        exposes.numeric('power_l2',ea.STATE).withUnit('W').withDescription('Phase 2 Power'),
+        exposes.numeric('power_l3',ea.STATE).withUnit('W').withDescription('Phase 3 Power'),
+        exposes.numeric('reactive_power_l1',ea.STATE).withUnit('VAr').withDescription('Phase 1 Reactive Power'),
+        exposes.numeric('reactive_power_l2',ea.STATE).withUnit('VAr').withDescription('Phase 2 Reactive Power'),
+        exposes.numeric('reactive_power_l3',ea.STATE).withUnit('VAr').withDescription('Phase 3 Reactive Power'),
+        exposes.numeric('frequency',ea.STATE).withUnit('Hz').withDescription('Frequency'),
+        exposes.numeric('reactive_energy_sum',ea.STATE).withUnit('kVArh').withDescription('Reactive Energy Sum'),
+        ],
+    },
 ];
