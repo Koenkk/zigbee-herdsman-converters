f50ab5fbe192cf806eb57573523efeae0d0cb415
Koen Kanters <koenkanters94@gmail.com>
2022-06-05 21:55:27 +0200

    Update zemismart.js

--- devices/zemismart.js
+++ devices/zemismart.js
@@ -193,7 +193,7 @@ module.exports = [
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_1n2kyphz'}],
         model: 'TB26-4',
         vendor: 'Zemismart',
-        description: '4-Gang Smart Wall Switch',
+        description: '4-gang smart wall switch',
         exposes: [e.switch().withEndpoint('l1').setAccess('state', ea.STATE_SET),
             e.switch().withEndpoint('l2').setAccess('state', ea.STATE_SET),
             e.switch().withEndpoint('l3').setAccess('state', ea.STATE_SET),
