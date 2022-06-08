7068d4f24b952574b94a57ee9af22e1238912a7a
Christian Winther <jippignu@gmail.com>
2022-06-08 16:38:45 +0200

    Fix `color_temperature` not exposed for Philips 929002376801 (#4334)
    Matches the other Iris 4th gen limited editions
    
    See https://github.com/Koenkk/zigbee2mqtt/issues/12749

--- devices/philips.js
+++ devices/philips.js
@@ -1671,9 +1671,9 @@ module.exports = [
         zigbeeModel: ['929002376801'],
         model: '929002376801',
         vendor: 'Philips',
-        description: 'Hue Iris (generation 4)',
+        description: 'Hue Iris kobber limited edition (generation 4)',
         meta: {turnsOffAtBrightness1: true},
-        extend: hueExtend.light_onoff_brightness_color(),
+        extend: hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
         ota: ota.zigbeeOTA,
     },
     {
