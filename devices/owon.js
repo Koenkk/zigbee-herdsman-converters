d128f1cb673b8c27034274b8a401ee4b39d24abc
Phil-ibert <59770946+Phil-ibert@users.noreply.github.com>
2022-06-08 19:54:54 +0200

    migrate fz to fzLocal

--- devices/owon.js
+++ devices/owon.js
@@ -6,6 +6,76 @@ const reporting = require('../lib/reporting');
 const e = exposes.presets;
 const ea = exposes.access;
 
+const fzLocal = {
+    PC321_metering: {
+        cluster: 'seMetering',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const payload = {};
+            if (msg.data.hasOwnProperty('owonL1Energy')) {
+                payload.energy_l1 = msg.data['owonL1Energy'][1] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL2Energy')) {
+                payload.energy_l2 = msg.data['owonL2Energy'][1] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL3Energy')) {
+                payload.energy_l3 = msg.data['owonL3Energy'][1] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL1ReactiveEnergy')) {
+                payload.reactive_energy_l1 = msg.data['owonL1ReactiveEnergy'][1] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL2ReactiveEnergy')) {
+                payload.reactive_energy_l2 = msg.data['owonL2ReactiveEnergy'][1] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL3ReactiveEnergy')) {
+                payload.reactive_energy_l3 = msg.data['owonL3ReactiveEnergy'][1] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL1PhasePower')) {
+                payload.power_l1 = msg.data['owonL1PhasePower'];
+            }
+            if (msg.data.hasOwnProperty('owonL2PhasePower')) {
+                payload.power_l2 = msg.data['owonL2PhasePower'];
+            }
+            if (msg.data.hasOwnProperty('owonL3PhasePower')) {
+                payload.power_l3 = msg.data['owonL3PhasePower'];
+            }
+            if (msg.data.hasOwnProperty('owonL1PhaseReactivePower')) {
+                payload.reactive_power_l1 = msg.data['owonL1PhaseReactivePower'];
+            }
+            if (msg.data.hasOwnProperty('owonL2PhaseReactivePower')) {
+                payload.reactive_power_l2 = msg.data['owonL2PhaseReactivePower'];
+            }
+            if (msg.data.hasOwnProperty('owonL3PhaseReactivePower')) {
+                payload.reactive_power_l3 = msg.data['owonL3PhaseReactivePower'];
+            }
+            if (msg.data.hasOwnProperty('owonL1PhaseVoltage')) {
+                payload.voltage_l1 = msg.data['owonL1PhaseVoltage'] / 10.0;
+            }
+            if (msg.data.hasOwnProperty('owonL2PhaseVoltage')) {
+                payload.voltage_l2 = msg.data['owonL2PhaseVoltage'] / 10.0;
+            }
+            if (msg.data.hasOwnProperty('owonL3PhaseVoltage')) {
+                payload.voltage_l3 = msg.data['owonL3PhaseVoltage'] / 10.0;
+            }
+            if (msg.data.hasOwnProperty('owonL1PhaseCurrent')) {
+                payload.current_l1 = msg.data['owonL1PhaseCurrent'] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL2PhaseCurrent')) {
+                payload.current_l2 = msg.data['owonL2PhaseCurrent'] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonL3PhaseCurrent')) {
+                payload.current_l3 = msg.data['owonL3PhaseCurrent'] / 1000.0;
+            }
+            if (msg.data.hasOwnProperty('owonFrequency')) {
+                payload.frequency = msg.data['owonFrequency'];
+            }
+            if (msg.data.hasOwnProperty('owonReactiveEnergySum')) {
+                payload.reactive_energy_sum = msg.data['owonReactiveEnergySum'];
+            }
+            return payload;
+        },
+    };
+
 module.exports = [
     {
         zigbeeModel: ['WSP404'],
@@ -125,7 +195,7 @@ module.exports = [
         model: 'PC321', // Vendor model number, look on the device for a model number
         vendor: 'OWON', // Vendor of the device (only used for documentation and startup logging)
         description: '3-Phase Clamp Power Meter', // Description of the device, copy from vendor site.
-        fromZigbee: [fz.metering, fz.PC321_metering],
+        fromZigbee: [fz.metering, fzLocal.PC321_metering],
         toZigbee: [], // Should be empty, unless device can be controlled (e.g. lights, switches).
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
@@ -135,13 +205,6 @@ module.exports = [
                 device.powerSource = 'Mains (single phase)';
                 device.save();
             }
-            // const payload = [{
-            //         attribute: 'owonL2Energy',
-            //         minimumReportInterval: 5,
-            //         maximumReportInterval: 3600,
-            //         reportableChange: 0,
-            //     }];
-            //     await endpoint.configureReporting('haElectricalMeasurement', payload);
         },
         exposes: [e.energy(),
             exposes.numeric('voltage_l1', ea.STATE).withUnit('V').withDescription('Phase 1 Voltage'),
@@ -162,8 +225,6 @@ module.exports = [
             exposes.numeric('reactive_power_l1', ea.STATE).withUnit('VAr').withDescription('Phase 1 Reactive Power'),
             exposes.numeric('reactive_power_l2', ea.STATE).withUnit('VAr').withDescription('Phase 2 Reactive Power'),
             exposes.numeric('reactive_power_l3', ea.STATE).withUnit('VAr').withDescription('Phase 3 Reactive Power'),
-            exposes.numeric('frequency', ea.STATE).withUnit('Hz').withDescription('Frequency'),
-            exposes.numeric('reactive_energy_sum', ea.STATE).withUnit('kVArh').withDescription('Reactive Energy Sum'),
         ],
     },
 ];
