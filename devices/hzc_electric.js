275e4f7600ecb61b5121f36570d71302393eb58c
André Dahlqvist <andre.dahlqvist@notified.com>
2022-06-12 15:06:37 +0200

    Add HZC Eletric's dual dimmer D086-ZG

---
+++ devices/hzc_electric.js
@@ -0,0 +1,16 @@
+const reporting = require('../lib/reporting');
+const extend = require('../lib/extend');
+
+module.exports = [
+    zigbeeModel: ['DimmerSwitch-2Gang-ZB3.0'],
+    model: 'D086-ZG',
+    vendor: 'HZC Electric',
+    description: 'Zigbee Dual Dimmer',
+    extend: extend.light_onoff_brightness({noConfigure: true}),
+    configure: async (device, coordinatorEndpoint, logger) => {
+        await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+        const endpoint = device.getEndpoint(1);
+        await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+        await reporting.onOff(endpoint);
+    },
+];
