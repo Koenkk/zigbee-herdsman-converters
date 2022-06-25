40f78e7ff2d5447b41801b7d8f45057b88971e26
mabosch74 <57590926+mabosch74@users.noreply.github.com>
2022-06-25 20:15:19 +0200

    re-added support for PIR Neo Tuya PST-ZMIR01
    after the latest build 1.25.2, the support of the device was broken.
    See https://github.com/Koenkk/zigbee2mqtt/issues/12925

--- devices/tuya.js
+++ devices/tuya.js
@@ -553,6 +553,7 @@ module.exports = [
             {modelID: 'TS0202', manufacturerName: '_TZ3000_tiwq83wk'},
             {modelID: 'TS0202', manufacturerName: '_TZ3000_ykwcwxmz'},
             {modelID: 'TS0202', manufacturerName: '_TZ3000_hgu1dlak'},
+            {modelID: 'TS0202', manufacturerName: '_TZ3000_kmh5qpmb'},
             {modelID: 'WHD02', manufacturerName: '_TZ3000_hktqahrq'}],
         model: 'TS0202',
         vendor: 'TuYa',
