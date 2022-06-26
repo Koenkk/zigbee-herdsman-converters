1fe8483fb8b2b8a6d223e1ccf5a831acfe8e9877
Koen Kanters <koenkanters94@gmail.com>
2022-06-26 15:33:56 +0200

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -2626,14 +2626,12 @@ module.exports = [
         fingerprint: [{modelID: 'TS1201', manufacturerName: '_TZ3290_7v1k4vufotpowp9z'}],
         model: 'ZS06',
         vendor: 'TuYa',
-        description: 'Universal Smart IR Remote Control',
+        description: 'Universal smart IR remote control',
         fromZigbee: [
             fzZosung.zosung_send_ir_code_00, fzZosung.zosung_send_ir_code_01, fzZosung.zosung_send_ir_code_02,
             fzZosung.zosung_send_ir_code_03, fzZosung.zosung_send_ir_code_04, fzZosung.zosung_send_ir_code_05,
         ],
-        toZigbee: [
-            tzZosung.zosung_ir_code_to_send, tzZosung.zosung_learn_ir_code,
-        ],
+        toZigbee: [tzZosung.zosung_ir_code_to_send, tzZosung.zosung_learn_ir_code],
         exposes: [ez.learn_ir_code(), ez.learned_ir_code(), ez.ir_code_to_send()],
     },
 ];
