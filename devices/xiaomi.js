8184a3e3c9734b999b7befd6bec83d2cb2e9fd32
Anton Samal <8128598+anton-samal@users.noreply.github.com>
2022-06-26 13:16:54 +0000

    exposed "side" for MFKZQ01LM (#4369)
    Co-authored-by: Anton Samal <samantond@gmail.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/xiaomi.js
+++ devices/xiaomi.js
@@ -1053,7 +1053,7 @@ module.exports = [
         meta: {battery: {voltageToPercentage: '3V_2850_3000_log'}},
         fromZigbee: [fz.xiaomi_basic, fz.MFKZQ01LM_action_multistate, fz.MFKZQ01LM_action_analog],
         exposes: [e.battery(), e.battery_voltage(), e.angle('action_angle'), e.device_temperature(), e.power_outage_count(false),
-            e.cube_side('action_from_side'), e.cube_side('action_side'), e.cube_side('action_to_side'),
+            e.cube_side('action_from_side'), e.cube_side('action_side'), e.cube_side('action_to_side'), e.cube_side('side'),
             e.action(['shake', 'wakeup', 'fall', 'tap', 'slide', 'flip180', 'flip90', 'rotate_left', 'rotate_right'])],
         toZigbee: [],
     },
