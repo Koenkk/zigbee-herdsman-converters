24ab9937362c18543324b38c95ad1602785191f0
Koen Kanters <koenkanters94@gmail.com>
2022-06-26 16:16:18 +0200

    Update RMC002.js

--- devices/RMC002.js
+++ devices/RMC002.js
@@ -1,17 +1,17 @@
 const reporting = require('../lib/reporting');
 const extend = require('../lib/extend');
 
-const definition = {
-    zigbeeModel: ['Bouffalolab'],
-    model: 'RMC002',
-    vendor: 'Bouffalolab',
-    description: 'US plug smart socket',
-    extend: extend.switch(),
-    configure: async (device, coordinatorEndpoint, logger) => {
-        const endpoint = device.getEndpoint(1);
-        await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
-        await reporting.onOff(endpoint);
+module.exports = [
+    {
+        zigbeeModel: ['Bouffalolab'],
+        model: 'RMC002',
+        vendor: 'Bouffalolab',
+        description: 'US plug smart socket',
+        extend: extend.switch(),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint);
+        },
     },
-};
-
-module.exports = definition;
+];
