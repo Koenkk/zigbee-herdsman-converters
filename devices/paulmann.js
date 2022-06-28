14acfffda411fcdeb698f1d362c1d9e1d00ba098
MarBra <16831559+MarBra@users.noreply.github.com>
2022-06-28 16:50:33 +0200

    Add CCT Light/Paulmann lamp fingerprint to 50064 (#4384)
    * Add "CCT Light" model to paulmann devices
    
    * Update paulmann.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/paulmann.js
+++ devices/paulmann.js
@@ -71,6 +71,7 @@ module.exports = [
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 370]}),
     },
     {
+        fingerprint: [{modelID: 'CCT Light', manufacturerName: 'Paulmann lamp'}],
         zigbeeModel: ['CCT light', 'CCT_light', 'CCT light '],
         model: '50064',
         vendor: 'Paulmann',
