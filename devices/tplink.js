35905821d15871ff2ddc4428f11f20587bc99ec3
Photo64 <photo848@gmail.com>
2022-06-30 11:33:26 -0400

    Update tplink.js

--- devices/tplink.js
+++ devices/tplink.js
@@ -1,6 +1,11 @@
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const fz = {...require('zigbee-herdsman-converters/converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
+=======
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const reporting = require('../lib/reporting');
+
 const e = exposes.presets;
 
 module.exports = [
@@ -19,6 +24,23 @@ module.exports = [
             await reporting.batteryPercentageRemaining(endpoint);
             await reporting.bind(endpoint2, coordinatorEndpoint, ['msIlluminanceMeasurement']);
             await reporting.illuminance(endpoint2);
+            device.powerSource = 'Battery';
+            device.save();
+        },
+    },
+=======
+    {
+        zigbeeModel: ['CS100'],
+        model: 'CS100',
+        vendor: 'TP-Link',
+        description: 'Contact sensor',
+        fromZigbee: [fz.ias_contact_alarm_1, fz.battery],
+        toZigbee: [],
+        exposes: [e.contact(), e.battery_low(), e.battery()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
             device.powerSource = 'Battery';
             device.save();
         },
