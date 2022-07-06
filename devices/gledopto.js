31bc0393d1c6e20926ba91ddf9e2af7e7bad7e86
Andrey Gorbunov <andrey@optimuspro.ru>
2022-07-06 18:20:24 +0300

    Remove unsupported `color_temp_startup` from Gledopto GL-S-004Z (#4423)

--- devices/gledopto.js
+++ devices/gledopto.js
@@ -348,7 +348,7 @@ module.exports = [
         model: 'GL-S-004Z',
         vendor: 'Gledopto',
         description: 'Zigbee 4W MR16 Bulb 30deg RGB+CCT',
-        extend: gledoptoExtend.light_onoff_brightness_colortemp_color(),
+        extend: gledoptoExtend.light_onoff_brightness_colortemp_color({disableColorTempStartup: false}),
     },
     {
         zigbeeModel: ['GL-S-005Z'],
