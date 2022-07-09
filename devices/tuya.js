d1d3751c1b589d036dc776f35f81b85a269df29c
Burkhard Kneiseler <burkhard@kneiseler.de>
2022-07-09 12:36:07 +0200

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -752,7 +752,7 @@ module.exports = [
             await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic']);
         },
         exposes: [e.battery(), e.illuminance_lux(), e.linkquality(),
-            exposes.enum('brightness_state', ea.STATE, ['LOW', 'Medium', 'HIGH'],
+            exposes.enum('brightness_state', ea.STATE, ['LOW', 'Medium', 'HIGH']),
     },
     {
         zigbeeModel: ['TS130F'],
