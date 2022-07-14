2f511df5fe00fe49bfd0e7516c2853926c1ec96c
Andrew Lamchenko <31867646+smartboxchannel@users.noreply.github.com>
2022-07-14 14:01:00 +0300

    EFEKTA_CO2_Smart_Monitor
    add new device EFEKTA_CO2_Smart_Monitor

--- devices/custom_devices_diy.js
+++ devices/custom_devices_diy.js
@@ -80,6 +80,54 @@ const tzLocal = {
             };
         },
     },
+    termostat_config: {
+        key: ['high_temp', 'low_temp', 'enable_temp'],
+        convertSet: async (entity, key, rawValue, meta) => {
+            const lookup = {'OFF': 0x00, 'ON': 0x01};
+            const value = lookup.hasOwnProperty(rawValue) ? lookup[rawValue] : parseInt(rawValue, 10);
+            const payloads = {
+                high_temp: ['msTemperatureMeasurement', {0x0221: {value, type: 0x29}}],
+                low_temp: ['msTemperatureMeasurement', {0x0222: {value, type: 0x29}}],
+				enable_temp: ['msTemperatureMeasurement', {0x0220: {value, type: 0x10}}],
+            };
+            await entity.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
+	hydrostat_config: {
+        key: ['high_hum', 'low_hum', 'enable_hum'],
+        convertSet: async (entity, key, rawValue, meta) => {
+            const lookup = {'OFF': 0x00, 'ON': 0x01};
+            const value = lookup.hasOwnProperty(rawValue) ? lookup[rawValue] : parseInt(rawValue, 10);
+            const payloads = {
+                high_hum: ['msRelativeHumidity', {0x0221: {value, type: 0x21}}],
+                low_hum: ['msRelativeHumidity', {0x0222: {value, type: 0x21}}],
+				enable_hum: ['msRelativeHumidity', {0x0220: {value, type: 0x10}}],
+            };
+            await entity.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
+	co2_gasstat_config: {
+        key: ['high_gas', 'low_gas', 'enable_gas'],
+        convertSet: async (entity, key, rawValue, meta) => {
+            const lookup = {'OFF': 0x00, 'ON': 0x01};
+            const value = lookup.hasOwnProperty(rawValue) ? lookup[rawValue] : parseInt(rawValue, 10);
+            const payloads = {
+                high_gas: ['msCO2', {0x0221: {value, type: 0x21}}],
+                low_gas: ['msCO2', {0x0222: {value, type: 0x21}}],
+				enable_gas: ['msCO2', {0x0220: {value, type: 0x10}}],
+            };
+            await entity.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
 };
 
 const fzLocal = {
@@ -148,6 +196,57 @@ const fzLocal = {
             const result = {};
             if (msg.data.hasOwnProperty(0x0210)) {
                 result.humidity_offset = msg.data[0x0210];
+            }
+            return result;
+        },
+    },
+    termostat_config: {
+        cluster: 'msTemperatureMeasurement',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0221)) {
+                result.high_temp = msg.data[0x0221];
+            }
+			if (msg.data.hasOwnProperty(0x0222)) {
+                result.low_temp = msg.data[0x0222];
+            }
+            if (msg.data.hasOwnProperty(0x0220)) {
+                result.enable_temp = ['OFF', 'ON'][msg.data[0x0220]];
+            }
+            return result;
+        },
+    },
+	hydrostat_config: {
+        cluster: 'msRelativeHumidity',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0221)) {
+                result.high_hum = msg.data[0x0221];
+            }
+			if (msg.data.hasOwnProperty(0x0222)) {
+                result.low_hum = msg.data[0x0222];
+            }
+            if (msg.data.hasOwnProperty(0x0220)) {
+                result.enable_hum = ['OFF', 'ON'][msg.data[0x0220]];
+            }
+            return result;
+        },
+    },
+	co2_gasstat_config: {
+        cluster: 'msCO2',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0221)) {
+                result.high_gas = msg.data[0x0221];
+            }
+			if (msg.data.hasOwnProperty(0x0222)) {
+                result.low_gas = msg.data[0x0222];
+            }
+            if (msg.data.hasOwnProperty(0x0220)) {
+                result.enable_gas = ['OFF', 'ON'][msg.data[0x0220]];
             }
             return result;
         },
@@ -620,5 +719,54 @@ module.exports = [
             exposes.numeric('manual_forced_recalibration', ea.STATE_SET).withUnit('ppm')
                 .withDescription('Start Manual FRC (Perform Forced Recalibration of the CO2 Sensor)')
                 .withValueMin(0).withValueMax(5000)],
+    },
+    {
+        zigbeeModel: ['EFEKTA_CO2_Smart_Monitor'],
+        model: 'EFEKTA_CO2_Smart_Monitor',
+        vendor: 'Custom devices (DiY)',
+        description: '[EFEKTA CO2 Smart Monitor, ws2812b indicator, can control the relay, binding on some other devices](https://efektalab.com/CO2_Monitor)',
+        fromZigbee: [fz.temperature, fz.humidity, fzLocal.co2, fzLocal.co2_config, fzLocal.temperature_config,
+		    fzLocal.humidity_config, fzLocal.termostat_config, fzLocal.hydrostat_config, fzLocal.co2_gasstat_config],
+        toZigbee: [tz.factory_reset, tzLocal.co2_config, tzLocal.temperature_config, tzLocal.humidity_config,
+		    tzLocal.termostat_config, tzLocal.hydrostat_config, tzLocal.co2_gasstat_config],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            const clusters = ['msTemperatureMeasurement', 'msRelativeHumidity', 'msCO2'];
+            await reporting.bind(endpoint, coordinatorEndpoint, clusters);
+            for (const cluster of clusters) {
+                await endpoint.configureReporting(cluster, [
+                    {attribute: 'measuredValue', minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 0},
+                ]);
+            }
+        },
+        exposes: [e.co2(), e.temperature(), e.humidity(),
+            exposes.binary('light_indicator', ea.STATE_SET, 'ON', 'OFF').withDescription('Enable or Disable light_indicator'),
+			exposes.numeric('light_ind_level', ea.STATE_SET).withUnit('%').withDescription('light_indicator_level')
+                .withValueMin(0).withValueMax(100),
+			exposes.numeric('set_altitude', ea.STATE_SET).withUnit('meters').withDescription('Setting the altitude above sea level (for high accuracy of the CO2 sensor)')
+                .withValueMin(0).withValueMax(3000),
+			exposes.numeric('temperature_offset', ea.STATE_SET).withUnit('°C').withDescription('Adjust temperature')
+                .withValueMin(-30).withValueMax(60),
+            exposes.numeric('humidity_offset', ea.STATE_SET).withUnit('%').withDescription('Adjust humidity')
+                .withValueMin(0).withValueMax(99),
+			exposes.binary('forced_recalibration', ea.STATE_SET, 'ON', 'OFF').withDescription('Start FRC (Perform Forced Recalibration of the CO2 Sensor)'),
+			exposes.numeric('manual_forced_recalibration', ea.STATE_SET).withUnit('ppm').withDescription('Start Manual FRC (Perform Forced Recalibration of the CO2 Sensor)')
+                .withValueMin(0).withValueMax(5000),
+			exposes.binary('factory_reset_co2', ea.STATE_SET, 'ON', 'OFF').withDescription('Factory Reset CO2 sensor'),
+			exposes.binary('enable_gas', ea.STATE_SET, 'ON', 'OFF').withDescription('Enable CO2 Gas Control'),
+            exposes.numeric('high_gas', ea.STATE_SET).withUnit('ppm').withDescription('Setting High CO2 Gas Border')
+                .withValueMin(0).withValueMax(99),
+            exposes.numeric('low_gas', ea.STATE_SET).withUnit('ppm').withDescription('Setting Low CO2 Gas Border')
+                .withValueMin(0).withValueMax(99),
+		    exposes.binary('enable_temp', ea.STATE_SET, 'ON', 'OFF').withDescription('Enable Temperature Control'),
+            exposes.numeric('high_temp', ea.STATE_SET).withUnit('C').withDescription('Setting High Temperature Border')
+                .withValueMin(-5).withValueMax(50),
+            exposes.numeric('low_temp', ea.STATE_SET).withUnit('C').withDescription('Setting Low Temperature Border')
+                .withValueMin(-5).withValueMax(50),
+		    exposes.binary('enable_hum', ea.STATE_SET, 'ON', 'OFF').withDescription('Enable Humidity Control'),
+            exposes.numeric('high_hum', ea.STATE_SET).withUnit('C').withDescription('Setting High Humidity Border')
+                .withValueMin(0).withValueMax(99),
+            exposes.numeric('low_hum', ea.STATE_SET).withUnit('C').withDescription('Setting Low Humidity Border')
+                .withValueMin(0).withValueMax(99)],
     },
 ];
