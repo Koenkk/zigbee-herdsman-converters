da09eac8cc183dd5f3dc9fb17d69771d35bb6eb6
5muck1 <33559818+5muck1@users.noreply.github.com>
2022-07-23 21:00:47 +0200

    Update rademacher.js
    added support for 35144001

--- devices/rademacher.js
+++ devices/rademacher.js
@@ -8,4 +8,11 @@ module.exports = [
         description: 'addZ white + colour',
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 555]}),
     },
+    {
+        zigbeeModel: ['RDM-35144001'],
+        model: '35144001',
+        vendor: 'Rademacher',
+        description: 'addZ white + colour',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 555]}),
+    },
 ];
