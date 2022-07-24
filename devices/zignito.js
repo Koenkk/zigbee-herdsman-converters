09c6574ca73424c3b285722d12db4d290eb237b7
Hayden Broadhead <hayden.broadhea@gmail.com>
2022-07-24 21:38:45 +0100

    Update: non-conflicting model name

--- devices/zignito.js
+++ devices/zignito.js
@@ -5,7 +5,7 @@ const e = exposes.presets;
 module.exports = [
     {
         fingerprint: [{ modelID: 'TS0505B', manufacturerName: '_TZ3210_hzy4rjz3' }],
-        model: 'TS0505B',
+        model: 'AJ_RGBCCT_CTRL',
         vendor: 'Zignito',
         description: 'Zignito LED panel RGB+CCT',
         extend: extend.light_onoff_brightness_colortemp_color({ colorTempRange: [153, 500], disableColorTempStartup: true }),
