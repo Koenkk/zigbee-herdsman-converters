86d470e9b25f778e583cd3778432d5ac514fe1bd
Koen Kanters <koenkanters94@gmail.com>
2022-07-25 17:50:08 +0200

    Update zignito.js

--- devices/zignito.js
+++ devices/zignito.js
@@ -1,16 +1,12 @@
-const exposes = require('../lib/exposes');
 const extend = require('../lib/extend');
-const e = exposes.presets;
 
 module.exports = [
     {
-        fingerprint: [{ modelID: 'TS0505B', manufacturerName: '_TZ3210_hzy4rjz3' }],
+        fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_hzy4rjz3'}],
         model: 'AJ_RGBCCT_CTRL',
         vendor: 'Zignito',
         description: 'Zignito LED panel RGB+CCT',
-        extend: extend.light_onoff_brightness_colortemp_color({ colorTempRange: [153, 500], disableColorTempStartup: true }),
-        meta: { applyRedFix: true, enhancedHue: false }
-    }
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500], disableColorTempStartup: true}),
+        meta: {applyRedFix: true, enhancedHue: false},
+    },
 ];
-
-
