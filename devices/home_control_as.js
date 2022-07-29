a9f2adb2d5756de587c3bd848253bdcb361ea50d
David Nagy <danagy@grundfos.com>
2022-07-29 11:26:03 +0200

    add autorelock and sound volume to HC-SLM-1

--- devices/home_control_as.js
+++ devices/home_control_as.js
@@ -11,13 +11,14 @@ module.exports = [
         vendor: 'Home Control AS',
         description: 'Heimgard (Wattle) door lock pro',
         fromZigbee: [fz.lock, fz.battery],
-        toZigbee: [tz.lock],
+        toZigbee: [tz.lock, tz.lock_auto_relock_time, tz.lock_sound_volume],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
+            console.log(endpoint.clusters)
             await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
             await reporting.lockState(endpoint);
             await reporting.batteryPercentageRemaining(endpoint);
         },
-        exposes: [e.lock(), e.battery()],
+        exposes: [e.lock(), e.battery(), e.auto_relock_time(), e.sound_volume()],
     },
 ];
