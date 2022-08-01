c15da4bd02bc4a56e8f24a4c981bc4758aa80a73
Koen Kanters <koenkanters94@gmail.com>
2022-08-01 19:02:18 +0200

    Disable effect for Sengled bulbs since they don't support it. https://github.com/Koenkk/zigbee2mqtt/issues/13348

--- devices/sengled.js
+++ devices/sengled.js
@@ -3,8 +3,17 @@ const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/lega
 const tz = require('../converters/toZigbee');
 const ota = require('../lib/ota');
 const reporting = require('../lib/reporting');
-const extend = require('../lib/extend');
 const e = exposes.presets;
+
+// Make sure extend.ldight_* is not used (sengledExtend should be used instead)
+const extendDontUse = require('../lib/extend');
+const extend = {switch: extendDontUse.switch};
+const sengledExtend = {
+    light_onoff_brightness: (options={}) => extendDontUse.light_onoff_brightness({disableEffect: true, ...options}),
+    light_onoff_brightness_colortemp: (options={}) => extendDontUse.light_onoff_brightness_colortemp({disableEffect: true, ...options}),
+    light_onoff_brightness_colortemp_color: (options={}) =>
+        extendDontUse.light_onoff_brightness_colortemp_color({disableEffect: true, ...options}),
+};
 
 module.exports = [
     {
@@ -12,9 +21,9 @@ module.exports = [
         model: 'E13-N11',
         vendor: 'Sengled',
         description: 'Flood light with motion sensor light outdoor',
-        fromZigbee: extend.light_onoff_brightness().fromZigbee.concat([fz.ias_occupancy_alarm_1]),
-        toZigbee: extend.light_onoff_brightness().toZigbee,
-        exposes: extend.light_onoff_brightness().exposes.concat([e.occupancy()]),
+        fromZigbee: sengledExtend.light_onoff_brightness().fromZigbee.concat([fz.ias_occupancy_alarm_1]),
+        toZigbee: sengledExtend.light_onoff_brightness().toZigbee,
+        exposes: sengledExtend.light_onoff_brightness().exposes.concat([e.occupancy()]),
         ota: ota.zigbeeOTA,
     },
     {
@@ -22,7 +31,7 @@ module.exports = [
         model: 'E21-N13A',
         vendor: 'Sengled',
         description: 'Smart LED (A19)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -30,12 +39,12 @@ module.exports = [
         model: 'E21-N1EA',
         vendor: 'Sengled',
         description: 'Smart LED multicolor A19 bulb',
-        fromZigbee: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).fromZigbee.concat([fz.metering]),
-        toZigbee: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).toZigbee,
-        exposes: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).exposes.concat([e.power(), e.energy()]),
+        fromZigbee: sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).fromZigbee.concat([fz.metering]),
+        toZigbee: sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).toZigbee,
+        exposes: sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).exposes.concat([e.power(), e.energy()]),
         ota: ota.zigbeeOTA,
         configure: async (device, coordinatorEndpoint, logger) => {
-            await extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]})
+            await sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]})
                 .configure(device, coordinatorEndpoint, logger);
             device.powerSource = 'Mains (single phase)';
             device.save();
@@ -52,7 +61,7 @@ module.exports = [
         model: 'E12-N1E',
         vendor: 'Sengled',
         description: 'Smart LED multicolor (BR30)',
-        extend: extend.light_onoff_brightness_colortemp_color(),
+        extend: sengledExtend.light_onoff_brightness_colortemp_color(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -60,7 +69,7 @@ module.exports = [
         model: 'E1G-G8E',
         vendor: 'Sengled',
         description: 'Multicolor light strip (2M)',
-        extend: extend.light_onoff_brightness_colortemp_color(),
+        extend: sengledExtend.light_onoff_brightness_colortemp_color(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -68,7 +77,7 @@ module.exports = [
         model: 'E11-U21U31',
         vendor: 'Sengled',
         description: 'Element touch (A19)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -76,11 +85,11 @@ module.exports = [
         model: 'E11-G13',
         vendor: 'Sengled',
         description: 'Element classic (A19)',
-        fromZigbee: extend.light_onoff_brightness().fromZigbee.concat([fz.metering]),
-        toZigbee: extend.light_onoff_brightness().toZigbee,
+        fromZigbee: sengledExtend.light_onoff_brightness().fromZigbee.concat([fz.metering]),
+        toZigbee: sengledExtend.light_onoff_brightness().toZigbee,
         ota: ota.zigbeeOTA,
         configure: async (device, coordinatorEndpoint, logger) => {
-            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            await sengledExtend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
             device.powerSource = 'Mains (single phase)';
             device.save();
 
@@ -90,14 +99,14 @@ module.exports = [
             await reporting.currentSummDelivered(endpoint);
             await reporting.instantaneousDemand(endpoint);
         },
-        exposes: extend.light_onoff_brightness().exposes.concat([e.power(), e.energy()]),
+        exposes: sengledExtend.light_onoff_brightness().exposes.concat([e.power(), e.energy()]),
     },
     {
         zigbeeModel: ['E11-G23', 'E11-G33'],
         model: 'E11-G23/E11-G33',
         vendor: 'Sengled',
         description: 'Element classic (A60)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -105,7 +114,7 @@ module.exports = [
         model: 'E11-N13/E11-N13A/E11-N14/E11-N14A',
         vendor: 'Sengled',
         description: 'Element extra bright (A19)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -113,7 +122,7 @@ module.exports = [
         model: 'Z01-CIA19NAE26',
         vendor: 'Sengled',
         description: 'Element touch (A19)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -121,7 +130,7 @@ module.exports = [
         model: 'Z01-A19NAE26',
         vendor: 'Sengled',
         description: 'Element plus (A19)',
-        extend: extend.light_onoff_brightness_colortemp(),
+        extend: sengledExtend.light_onoff_brightness_colortemp(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -129,7 +138,7 @@ module.exports = [
         model: 'Z01-A60EAE27',
         vendor: 'Sengled',
         description: 'Element Plus (A60)',
-        extend: extend.light_onoff_brightness_colortemp(),
+        extend: sengledExtend.light_onoff_brightness_colortemp(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -137,11 +146,11 @@ module.exports = [
         model: 'E11-N1EA',
         vendor: 'Sengled',
         description: 'Element plus color (A19)',
-        fromZigbee: extend.light_onoff_brightness().fromZigbee.concat([fz.metering]),
-        toZigbee: extend.light_onoff_brightness().toZigbee,
+        fromZigbee: sengledExtend.light_onoff_brightness().fromZigbee.concat([fz.metering]),
+        toZigbee: sengledExtend.light_onoff_brightness().toZigbee,
         ota: ota.zigbeeOTA,
         configure: async (device, coordinatorEndpoint, logger) => {
-            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            await sengledExtend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
             device.powerSource = 'Mains (single phase)';
             device.save();
 
@@ -151,14 +160,14 @@ module.exports = [
             await reporting.currentSummDelivered(endpoint);
             await reporting.instantaneousDemand(endpoint);
         },
-        exposes: extend.light_onoff_brightness().exposes.concat([e.power(), e.energy()]),
+        exposes: sengledExtend.light_onoff_brightness().exposes.concat([e.power(), e.energy()]),
     },
     {
         zigbeeModel: ['E11-U2E'],
         model: 'E11-U2E',
         vendor: 'Sengled',
         description: 'Element color plus E27',
-        extend: extend.light_onoff_brightness_colortemp_color(),
+        extend: sengledExtend.light_onoff_brightness_colortemp_color(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -166,7 +175,7 @@ module.exports = [
         model: 'E11-U3E',
         vendor: 'Sengled',
         description: 'Element color plus B22',
-        extend: extend.light_onoff_brightness_colortemp_color(),
+        extend: sengledExtend.light_onoff_brightness_colortemp_color(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -174,12 +183,12 @@ module.exports = [
         model: 'E1F-N5E',
         vendor: 'Sengled',
         description: 'Element color plus E12',
-        fromZigbee: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).fromZigbee.concat([fz.metering]),
-        toZigbee: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).toZigbee,
-        exposes: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).exposes.concat([e.power(), e.energy()]),
+        fromZigbee: sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).fromZigbee.concat([fz.metering]),
+        toZigbee: sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).toZigbee,
+        exposes: sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]}).exposes.concat([e.power(), e.energy()]),
         ota: ota.zigbeeOTA,
         configure: async (device, coordinatorEndpoint, logger) => {
-            await extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]})
+            await sengledExtend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 500]})
                 .configure(device, coordinatorEndpoint, logger);
             device.powerSource = 'Mains (single phase)';
             device.save();
@@ -196,7 +205,7 @@ module.exports = [
         model: 'E12-N14',
         vendor: 'Sengled',
         description: 'Element Classic (BR30)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -204,7 +213,7 @@ module.exports = [
         model: 'E1ACA4ABE38A',
         vendor: 'Sengled',
         description: 'Element downlight smart LED bulb',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -261,7 +270,7 @@ module.exports = [
         model: 'E11-N1G',
         vendor: 'Sengled',
         description: 'Vintage LED edison bulb (ST19)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
     {
@@ -269,7 +278,7 @@ module.exports = [
         model: 'E1F-N9G',
         vendor: 'Sengled',
         description: 'Smart LED filament candle (E12)',
-        extend: extend.light_onoff_brightness(),
+        extend: sengledExtend.light_onoff_brightness(),
         ota: ota.zigbeeOTA,
     },
 ];
