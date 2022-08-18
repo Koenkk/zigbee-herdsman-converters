02aa6d9ec4cf6f4eef87753cd3198e9f2b3718fe
Burkhard Kneiseler <burkhard@kneiseler.de>
2022-08-18 06:53:20 +0200

    Update iluminize.js
    add description to some exposes

--- devices/iluminize.js
+++ devices/iluminize.js
@@ -164,10 +164,12 @@ module.exports = [
             'recall_*', 'on', 'off']),
         exposes.composite('action_color', 'action_color')
             .withFeature(exposes.numeric('x', ea.STATE))
-            .withFeature(exposes.numeric('y', ea.STATE)),
+            .withFeature(exposes.numeric('y', ea.STATE))
+            .withDescription('Only shows the transmitted color in X7Y-Mode. Noch changes possible.'),
         exposes.numeric('action_color_temperature', ea.STATE).withUnit('mired')
             .withDescription('color temperature value. Fixed values for each key press: 145, 175, 222, 304, 480 mired'),
-        exposes.numeric('action_group', ea.STATE),
+        exposes.numeric('action_group', ea.STATE)
+        .withDescription('Shows the zigbee2mqtt group bound to the active data point EP(1-4).'),
         exposes.numeric('action_transition_time', ea.STATE),
         exposes.numeric('action_step_size', ea.STATE),
         exposes.numeric('action_rate', ea.STATE)],
