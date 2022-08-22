6926b4ac8e7a86371cfa2483f7ccf4d607a3b939
Koen Kanters <koenkanters94@gmail.com>
2022-08-22 19:41:44 +0200

    Fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -483,7 +483,7 @@ describe('index.js', () => {
         const ZNCLDJ12LM = index.definitions.find((d) => d.model == 'ZNCLDJ12LM');
         expect(ZNCLDJ12LM.options.length).toBe(1);
         const ZNCZ04LM = index.definitions.find((d) => d.model == 'ZNCZ04LM');
-        expect(ZNCZ04LM.options.length).toBe(2);
+        expect(ZNCZ04LM.options.length).toBe(1);
     });
 
     it('Verify imports', () => {
