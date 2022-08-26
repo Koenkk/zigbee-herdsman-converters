2b1e6ca6aa62c49d0097d5f005cf49408789841d
Scales82 <44252921+Scales82@users.noreply.github.com>
2022-08-27 07:44:19 +1000

    Update ozsmartthings.js

--- devices/ozsmartthings.js
+++ devices/ozsmartthings.js
@@ -1,11 +1,11 @@
-const extend = require('../lib/extend');
+const extend = require('zigbee-herdsman-converters/lib/extend');
 
-const definition = {
-    zigbeeModel: ['TS0505B'],
-    model: 'DL41-03-10-R-ZB',
-    vendor: 'OzSmartThings',
-    description: 'OZ SMART RGBW ZIGBEE DOWNLIGHT Smart Led Zigbee Downlight 10W Australian Approved',
-    extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
-};
-
-module.exports = definition;
+module.exports = [
+    {
+        zigbeeModel: ['TS0505B'],
+        model: 'DL41-03-10-R-ZB',
+        vendor: 'OzSmartThings',
+        description: 'Oz Smart RGBW Zigbee Downlight',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
+    },
+];
