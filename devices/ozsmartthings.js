db4c6cfe16b4104b1bdaf48d92b56eadede55f82
Scales82 <44252921+Scales82@users.noreply.github.com>
2022-08-27 08:25:44 +1000

    Update ozsmartthings.js

--- devices/ozsmartthings.js
+++ devices/ozsmartthings.js
@@ -2,6 +2,7 @@ const extend = require('../lib/extend');
 
 module.exports = [
     {
+        fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_mcm6m1ma'}],
         zigbeeModel: ['TS0505B'],
         model: 'DL41-03-10-R-ZB',
         vendor: 'OzSmartThings',
@@ -9,4 +10,3 @@ module.exports = [
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
 ];
-
