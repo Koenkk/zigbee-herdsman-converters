78e128d3564f9e086ba7c6aa9c98b67890b03fe4
Scales82 <44252921+Scales82@users.noreply.github.com>
2022-08-27 07:33:12 +1000

    Update ozsmartthings.js

--- devices/ozsmartthings.js
+++ devices/ozsmartthings.js
@@ -1,26 +1,10 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const extend = require('zigbee-herdsman-converters/lib/extend');
-const ota = require('zigbee-herdsman-converters/lib/ota');
-const tuya = require('zigbee-herdsman-converters/lib/tuya');
-const e = exposes.presets;
-const ea = exposes.access;
 
 const definition = {
     zigbeeModel: ['TS0505B'],
     model: 'DL41-03-10-R-ZB',
     vendor: 'OzSmartThings',
     description: 'OZ SMART RGBW ZIGBEE DOWNLIGHT Smart Led Zigbee Downlight 10W Australian Approved',
-    // Note that fromZigbee, toZigbee and exposes are missing here since we use extend here.
-    // Extend contains a default set of fromZigbee/toZigbee converters and expose for common device types.
-    // The following extends are available:
-    // - extend.switch
-    // - extend.light_onoff_brightness
-    // - extend.light_onoff_brightness_colortemp
-    // - extend.light_onoff_brightness_color
-    // - extend.light_onoff_brightness_colortemp_color
     extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
 };
 
