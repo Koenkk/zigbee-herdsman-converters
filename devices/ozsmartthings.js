f457ad9ae4c0da6c61447edc38b6df9478ad78d5
Scales82 <44252921+Scales82@users.noreply.github.com>
2022-08-27 08:27:30 +1000

    Update ozsmartthings.js

--- devices/ozsmartthings.js
+++ devices/ozsmartthings.js
@@ -5,8 +5,8 @@ module.exports = [
         fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_mcm6m1ma'}],
         zigbeeModel: ['TS0505B'],
         model: 'DL41-03-10-R-ZB',
-        vendor: 'OzSmartThings',
-        description: 'Oz Smart RGBW Zigbee Downlight',
+        vendor: 'Oz Smart Things',
+        description: 'Oz Smart RGBW Zigbee Downlight 10w',
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
 ];
