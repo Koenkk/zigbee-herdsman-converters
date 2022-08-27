1fba21057e02dd1e9239bb404572a0faa1e97d37
Koen Kanters <koenkanters94@gmail.com>
2022-08-27 19:54:25 +0200

    Update garza.js

--- devices/garza.js
+++ devices/garza.js
@@ -3,10 +3,9 @@ const extend = require('../lib/extend');
 module.exports = [
     {
         fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_sln7ah6r'}],
-        zigbeeModel: ['TS0505B'],
         model: 'Garza-Standard-A60',
         vendor: 'Garza Smart',
-        description: 'Garza Smart Zigbee Standard A60',
+        description: 'Standard A60 bulb',
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
 ];
