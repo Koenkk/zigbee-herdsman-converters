5df888fe93ba3d69d310f1c31843be3782b8833d
Pablo Verdu <verduromeropablo@gmail.com>
2022-08-27 12:33:42 +0200

    Fixed module.export funtion schema

--- devices/garza.js
+++ devices/garza.js
@@ -1,17 +1,17 @@
 const extend = require('../lib/extend');
 
-const definition = {
-    fingerprint: [
-        {
-            modelID: 'TS0505B',
-            manufacturerName: '_TZ3210_sln7ah6r'
-        },
-    ],
-    zigbeeModel: ['TS0505B'],
-    model: 'Garza-Standard-A60',
-    vendor: 'Garza Smart',
-    description: 'Garza Smart Zigbee Standard A60',
-    extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
-};
-
-module.exports = definition;
+module.exports = [
+    {
+        fingerprint: [
+            {
+                modelID: 'TS0505B',
+                manufacturerName: '_TZ3210_sln7ah6r'
+            },
+        ],
+        zigbeeModel: ['TS0505B'],
+        model: 'Garza-Standard-A60',
+        vendor: 'Garza Smart',
+        description: 'Garza Smart Zigbee Standard A60',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
+    }
+];
