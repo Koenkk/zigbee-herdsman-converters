be0bde67fac60ec244e0e4d04c34c171c4118514
dsantos21 <44759158+dsantos21@users.noreply.github.com>
2022-08-31 01:14:02 -0300

    Add support for Yale YMC 420 D

--- devices/yale.js
+++ devices/yale.js
@@ -125,6 +125,21 @@ module.exports = [
         extend: lockExtend(),
     },
     {
+        zigbeeModel: ['YMC 420 D'],
+        model: 'YMC 420 D',
+        vendor: 'Yale',
+        description: 'Biometric digital lock',
+        fromZigbee: [
+          fz.yale_ymc_420_d_lock_operation_event,
+          fz.lock_programming_event,
+          fz.lock_pin_code_response,
+          fz.lock_user_status_response,
+          fz.lock,
+          fz.battery
+        ],
+        extend: lockExtend({ timeout: 20000 }), //
+    },
+    {
         zigbeeModel: ['c700000202', '06ffff2029'],
         model: 'YDF40',
         vendor: 'Yale',
