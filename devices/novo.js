fa16c523caa60f61f36d206da4d94975e94415b0
sreisi <98765022+sreisi@users.noreply.github.com>
2022-09-01 12:18:19 +0200

    Add support for novo curtain motor

---
+++ devices/novo.js
@@ -0,0 +1,18 @@
+const exposes = require('../lib/exposes');
+const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const tz = require('../converters/toZigbee');
+const extend = require('../lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+module.exports = [ 
+    {
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_swhwv3k3'}],
+        model: 'C10-3E-1.2',
+        vendor: 'Novo',
+        description: 'Curtain switch',
+        fromZigbee: [fz.tuya_cover, fz.ignore_basic_report],
+        toZigbee: [tz.tuya_cover_control, tz.tuya_cover_options],
+        exposes: [e.cover_position().setAccess('position', ea.STATE_SET)],
+    },
+];
