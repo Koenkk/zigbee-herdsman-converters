db8ef206910a35b0516584d86737e469d8ea99b1
ciedema <ciedema@users.noreply.github.com>
2022-09-02 15:55:48 +1000

    Fix Lint issues

--- devices/mecrator.js
+++ devices/mecrator.js
@@ -6,7 +6,6 @@ const e = exposes.presets;
 const ea = exposes.access;
 
 
-
 module.exports = [
     {
         fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3210_yvxjawlt'}],
@@ -65,18 +64,18 @@ module.exports = [
         },
     },
 	{
-	    fingerprint: [  {modelID: 'TS0501', manufacturerName: '_TZ3210_lzqq3u4r'},
-						{modelID: 'TS0501', manufacturerName: '_TZ3210_4whigl8i'}
-					],
+		fingerprint: [  {modelID: 'TS0501', manufacturerName: '_TZ3210_lzqq3u4r'},
+                        {modelID: 'TS0501', manufacturerName: '_TZ3210_4whigl8i'}
+						],
 		model: 'SSWF01G',
 		description: 'AC Fan Controller',
 		vendor: 'Mercator',
 		fromZigbee: [fz.on_off, fz.fan],
 		toZigbee: [tz.fan_mode, tz.on_off],
-		exposes: [e.switch(), e.fan().withModes(['off','low','medium','high','on'])],
+		exposes: [e.switch(), e.fan().withModes(['off', 'low', 'medium', 'high', 'on'])],
 		configure: async (device, coordinatorEndpoint, logger) => {
 			const endpoint = device.getEndpoint(1);
-			await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic', 'genOta', 'genTime', 'genGroups', 'genScenes', 'genOnOff', 'genIdentify', 'manuSpecificTuya','hvacFanCtrl']);
+			await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic', 'genOta', 'genTime', 'genGroups', 'genScenes', 'genOnOff', 'genIdentify', 'manuSpecificTuya', 'hvacFanCtrl']);
 			await reporting.onOff(endpoint);
 			await reporting.fanMode(endpoint);
 		},
