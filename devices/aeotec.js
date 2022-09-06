d3fb717491369e89a68e296bc0a8c1616f7a7312
Koen Kanters <koenkanters94@gmail.com>
2022-09-06 09:37:23 +0200

    Update aeotec.js

--- devices/aeotec.js
+++ devices/aeotec.js
@@ -5,7 +5,7 @@ module.exports = [
         zigbeeModel: ['WG001-Z01'],
         model: 'WG001',
         vendor: 'Aeotec',
-        description: 'Range Extender Zi',
+        description: 'Range extender Zi',
         fromZigbee: [fz.linkquality_from_basic],
         toZigbee: [],
         exposes: [],
