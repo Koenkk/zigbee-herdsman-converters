70c7bc53bbf43c6a326029a6412b35e25ba4ab0e
sreisi <98765022+sreisi@users.noreply.github.com>
2022-09-06 13:37:03 +0200

    Update zigbeemodel in novo.js

--- devices/novo.js
+++ devices/novo.js
@@ -5,8 +5,8 @@ const e = exposes.presets;
 const ea = exposes.access;
 
 module.exports = [
-    {
-        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_swhwv3k3'}],
+    {        
+        zigbeeModel: ['TS0601'],
         model: 'C10-3E-1.2',
         vendor: 'Novo',
         description: 'Curtain switch',
