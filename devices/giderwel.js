eb2008348efd530eefc077ea1d30c1d4747e41df
Brian M <radiohe4d@live.com>
2022-09-06 19:50:37 +0100

    Add GIDERWEL C05Z RGB+CCT LED Controller

--- devices/giderwel.js
+++ devices/giderwel.js
@@ -8,4 +8,15 @@ module.exports = [
         description: 'Smart Zigbee RGB LED strip controller',
         extend: extend.light_onoff_brightness_color({supportsHS: false}),
     },
+    {
+        fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_ijczzg9h'}],
+        model: 'C05Z',
+        vendor: 'GIDERWEL',
+        description: 'Zigbee RGB+CCT LED strip controller',
+        extend: extend.light_onoff_brightness_colortemp_color({
+            colorTempRange: [153, 500],
+            disableColorTempStartup: true,
+            disableEffect: true,
+        }),
+    },
 ];
