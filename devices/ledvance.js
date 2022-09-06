948ddf8bd438b165b4cb6e0c16cdc44a6731afe3
LEDVANCE Products <109606930+dhanukapulkit@users.noreply.github.com>
2022-09-06 14:04:45 +0200

    Update ledvance.js
    Added the new B40 E14 and PAR16 GU10 dimmable lamps

--- devices/ledvance.js
+++ devices/ledvance.js
@@ -176,5 +176,24 @@ module.exports = [
         ota: ota.ledvance,
         
     };
+{
+        zigbeeModel: ['B40 DIM T'],
+        model: '4058075729063',
+        vendor: 'LEDVANCE',
+        description: 'SMART+ Classic B40 E14 dimmable white',
+	   extend: extend.ledvance.light_onoff_brightness(),
+        ota: ota.ledvance,        
+    };
+{
+        zigbeeModel: ['PAR16 DIM T'],
+        model: '4058075729148',
+        vendor: 'LEDVANCE',
+        description: 'SMART+ Spot PAR16 50 GU10 dimmable white',
+	   extend: extend.ledvance.light_onoff_brightness(),
+        ota: ota.ledvance,
+
+        
+    };
+
 
 ];
