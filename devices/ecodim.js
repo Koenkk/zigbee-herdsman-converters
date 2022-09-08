c923d62c15085db7ec4916e4f4b9fe8c3dc0e6f4
Koen Kanters <koenkanters94@gmail.com>
2022-09-08 09:23:39 +0200

    Add new fingerprint for Eco-Dim.07/Eco-Dim.10. https://github.com/Koenkk/zigbee2mqtt/issues/13876

--- devices/ecodim.js
+++ devices/ecodim.js
@@ -38,6 +38,11 @@ module.exports = [
                 {ID: 1, profileID: 260, deviceID: 257, inputClusters: [0, 3, 4, 5, 6, 8, 2821, 4096], outputClusters: [25]},
                 {ID: 242, profileID: 41440, deviceID: 97, inputClusters: [], outputClusters: [33]},
             ]},
+            {type: 'Router', manufacturerName: 'EcoDim BV', modelID: 'EcoDim-Zigbee 3.0', endpoints: [
+                {ID: 1, profileID: 260, deviceID: 257, inputClusters: [0, 3, 4, 5, 6, 8, 2821, 4096], outputClusters: [25]},
+                {ID: 67, inputClusters: [], outputClusters: []},
+                {ID: 242, profileID: 41440, deviceID: 97, inputClusters: [], outputClusters: [33]},
+            ]},
         ],
         zigbeeModel: ['Dimmer-Switch-ZB3.0'],
         model: 'Eco-Dim.07/Eco-Dim.10',
