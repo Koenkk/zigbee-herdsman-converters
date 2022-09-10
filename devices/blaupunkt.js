234c7035675a519aa057b7fbfbed43a057054116
Pawel Forynski <5372454+jestempablo@users.noreply.github.com>
2022-09-10 10:05:27 +0200

    Added support for Blaupunkt TMST-S1 Thermostat
    This is not full support (missing battery reporting, missing fahrenheit/celsius toggle, possibly more), but at least core functionality is OK.

--- devices/blaupunkt.js
+++ devices/blaupunkt.js
@@ -24,4 +24,40 @@ module.exports = [
         },
         exposes: [e.cover_position().setAccess('state', ea.ALL)],
     },
+    {
+        zigbeeModel: ['TMSTB_00.00.03.07TC'],
+        model: 'TMST-S1',
+        vendor: 'Blaupunkt',
+        description: 'Blaupunkt Smart Thermostat TMST-S1',
+        fromZigbee: [fz.thermostat, fz.hvac_user_interface],
+        toZigbee: [
+            tz.thermostat_local_temperature,
+            tz.thermostat_system_mode,
+            tz.thermostat_occupied_heating_setpoint,
+            tz.thermostat_unoccupied_heating_setpoint,
+            tz.thermostat_occupied_cooling_setpoint,
+            tz.thermostat_unoccupied_cooling_setpoint,
+            tz.thermostat_programming_operation_mode,
+        ],
+        exposes: [
+            e.programming_operation_mode(),
+            exposes.climate()
+                .withSetpoint('occupied_heating_setpoint', 9, 30, 1)
+                .withSetpoint('occupied_cooling_setpoint', 11, 32, 1)
+                .withSetpoint('unoccupied_heating_setpoint', 9, 30, 1)
+                .withSetpoint('unoccupied_cooling_setpoint', 11, 32, 1)
+                .withLocalTemperature()
+                .withSystemMode(['off', 'heat', 'cool', 'auto'])
+        ],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genBasic', 'genTime', 'genPowerCfg', 'genIdentify', 'hvacThermostat', 'hvacFanCtrl']);
+            await reporting.thermostatTemperature(endpoint);
+            await reporting.thermostatRunningState(endpoint);
+            await reporting.thermostatOccupiedHeatingSetpoint(endpoint);
+            await reporting.thermostatUnoccupiedHeatingSetpoint(endpoint);
+            await reporting.thermostatOccupiedCoolingSetpoint(endpoint);
+            await reporting.thermostatUnoccupiedCoolingSetpoint(endpoint);
+        },
+    },
 ];
