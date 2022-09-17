548be029a01680ab8f7d78a71145c8f57a4cde44
Brad <wombo1@gmail.com>
2022-09-17 20:25:29 +0300

    Fixed tabs

--- devices/tuya.js
+++ devices/tuya.js
@@ -3303,14 +3303,14 @@ module.exports = [
         },
         exposes: [
             e.occupancy(),
-		    exposes.numeric('states', ea.STATE).withDescription('Motion State')
+            exposes.numeric('states', ea.STATE).withDescription('Motion State')
                 .withDescription('No motion, Big motion, Minor motion, Breathing'),
-		    e.illuminance_lux(),
-		    exposes.binary('led_enable', ea.STATE_SET, true, false).withDescription('Enabled LED'),
-		    exposes.enum('keep_time', ea.STATE_SET, ['0', '1', '2', '3', '4', '5', '6', '7'])
+            e.illuminance_lux(),
+            exposes.binary('led_enable', ea.STATE_SET, true, false).withDescription('Enabled LED'),
+            exposes.enum('keep_time', ea.STATE_SET, ['0', '1', '2', '3', '4', '5', '6', '7'])
                 .withDescription('PIR keep time 0:5s|1:30s|2:60s|3:180s|4:300s|5:600s|6:1200s|7:1800s'),
-		    exposes.enum('sensitivity', ea.STATE_SET, ['25', '50', '75', '100']),
-		    exposes.numeric('illuminance_calibration', ea.STATE_SET).withDescription('Illuminance calibration')
+            exposes.enum('sensitivity', ea.STATE_SET, ['25', '50', '75', '100']),
+            exposes.numeric('illuminance_calibration', ea.STATE_SET).withDescription('Illuminance calibration')
                 .withValueMin(-10000).withValueMax(10000),
         ],
     },
