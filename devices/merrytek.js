ceb25023052d07b3ff0ebc1e914fcafb1438d4f8
Brad <wombo1@gmail.com>
2022-09-19 20:42:08 +0300

    Add reporting

--- devices/merrytek.js
+++ devices/merrytek.js
@@ -1,6 +1,7 @@
 const exposes = require('../lib/exposes');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
 const tz = require('../converters/toZigbee');
+const reporting = require('../lib/reporting');
 const e = exposes.presets;
 const ea = exposes.access;
 
