5a296f8fe3f5e4c315aecaaaa377890dea882561
d51x <dimonich110@gmail.com>
2022-09-20 17:58:53 +0300

    Expose energy for LifeControl MCLH-03 (#4676)

--- devices/lifecontrol.js
+++ devices/lifecontrol.js
@@ -38,13 +38,15 @@ module.exports = [
         model: 'MCLH-03',
         vendor: 'LifeControl',
         description: 'Power plug',
-        fromZigbee: [fz.on_off, fz.electrical_measurement],
+        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering],
         toZigbee: [tz.on_off],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement']);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement', 'seMetering']);
             await reporting.onOff(endpoint);
             await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
+            await reporting.readMeteringMultiplierDivisor(endpoint);
+            await reporting.currentSummDelivered(endpoint);
         },
         onEvent: async (type, data, device) => {
             // This device doesn't support reporting correctly.
@@ -57,6 +59,7 @@ module.exports = [
                 const interval = setInterval(async () => {
                     try {
                         await endpoint.read('haElectricalMeasurement', ['rmsVoltage', 'rmsCurrent', 'activePower']);
+                        await endpoint.read('seMetering', ['currentSummDelivered', 'multiplier', 'divisor']);
                     } catch (error) {
                         // Do nothing
                     }
@@ -64,7 +67,7 @@ module.exports = [
                 globalStore.putValue(device, 'interval', interval);
             }
         },
-        exposes: [e.switch(), e.power(), e.current(), e.voltage()],
+        exposes: [e.switch(), e.power(), e.current(), e.voltage(), e.energy()],
     },
     {
         zigbeeModel: ['Motion_Sensor'],
