d4e3db59c195b148d1ad4058fa0b3df6d0ada326
Koen Kanters <koenkanters94@gmail.com>
2022-09-22 20:02:50 +0200

    Add more actions to 929003017102. https://github.com/Koenkk/zigbee2mqtt.io/pull/1596

--- devices/philips.js
+++ devices/philips.js
@@ -1822,7 +1822,9 @@ module.exports = [
         vendor: 'Philips',
         description: 'Hue wall switch module',
         fromZigbee: [fz.battery, fz.hue_wall_switch_device_mode, fz.hue_wall_switch],
-        exposes: [e.battery(), e.action(['left_press', 'left_press_release', 'right_press', 'right_press_release']),
+        exposes: [
+            e.battery(), e.action(['left_press', 'left_press_release', 'right_press', 'right_press_release',
+                'left_hold', 'left_hold_release', 'right_hold', 'right_hold_release']),
             exposes.enum('device_mode', ea.ALL, ['single_rocker', 'single_push_button', 'dual_rocker', 'dual_push_button'])],
         toZigbee: [tz.hue_wall_switch_device_mode],
         configure: async (device, coordinatorEndpoint, logger) => {
