12e8b8c28873c29d5131f81a50134fbc6b0af1d7
Koen Kanters <koenkanters94@gmail.com>
2022-10-06 17:23:26 +0200

    Add _TZE200_jva8ink8 to TS0601_smart_human_presense_sensor. https://github.com/Koenkk/zigbee2mqtt/issues/14349

--- devices/tuya.js
+++ devices/tuya.js
@@ -3182,6 +3182,7 @@ module.exports = [
     {
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_ikvncluo'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_lyetpprm'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_jva8ink8'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_wukb7rhc'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_ztc6ggyl'}],
         model: 'TS0601_smart_human_presense_sensor',
