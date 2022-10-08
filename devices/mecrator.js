70edd6e05894d1dd7f7cec0fdaffffb2593ca98e
Alex Retzlaff <AlexRetzlaff@users.noreply.github.com>
2022-10-08 19:39:10 +1100

    Add SPUSB02 (#4686)
    * Added support for SPUSB02
    
    * Fixed lint
    
    * Fixed lint
    
    * Remove duplicate 'await' lines
    
    * Update mecrator.js
    
    Co-authored-by: pyscripter99 <78647110+pyscripter99@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/mecrator.js
+++ devices/mecrator.js
@@ -79,4 +79,41 @@ module.exports = [
             await reporting.fanMode(endpoint);
         },
     },
+    {
+        fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3210_pfbzs1an'}],
+        model: 'SPUSB02',
+        vendor: 'Mercator',
+        description: 'Ikuü double power point with USB',
+        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.ignore_basic_report, fz.tuya_switch_power_outage_memory],
+        toZigbee: [tz.on_off],
+        exposes: [
+            e.switch().withEndpoint('left'),
+            e.switch().withEndpoint('right'),
+            e.power().withEndpoint('left'),
+            e.current().withEndpoint('left'),
+            e.voltage().withEndpoint('left').withAccess(ea.STATE),
+            e.energy(),
+        ],
+        endpoint: (device) => {
+            return {left: 1, right: 2};
+        },
+        // The configure method below is needed to make the device reports on/off state changes
+        // when the device is controlled manually through the button on it.
+        meta: {multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['genBasic', 'genOnOff', 'haElectricalMeasurement', 'seMetering']);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint1);
+            await reporting.rmsVoltage(endpoint1, {change: 5});
+            await reporting.rmsCurrent(endpoint1, {change: 50});
+            await reporting.activePower(endpoint1, {change: 1});
+            await reporting.onOff(endpoint1);
+            await reporting.onOff(endpoint2);
+            endpoint1.saveClusterAttributeKeyValue('haElectricalMeasurement', {acCurrentDivisor: 1000, acCurrentMultiplier: 1});
+            endpoint1.saveClusterAttributeKeyValue('seMetering', {divisor: 100, multiplier: 1});
+            device.save();
+        },
+    },
 ];
