d625c87162b40102c47fb8d106f53d67c41fcfb2
Koen Kanters <koenkanters94@gmail.com>
2022-10-12 16:14:31 +0200

    Add _TZE200_holel4dk to TS0601_smart_human_presense_sensor. https://github.com/Koenkk/zigbee2mqtt/issues/14449

--- devices/tuya.js
+++ devices/tuya.js
@@ -3222,6 +3222,7 @@ module.exports = [
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_ikvncluo'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_lyetpprm'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_jva8ink8'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_holel4dk'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_wukb7rhc'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_ztc6ggyl'}],
         model: 'TS0601_smart_human_presense_sensor',
