a9b9d89b8ce1d0173731b35ed979068f97b05489
unknown <daniel.edward.george.hitchcock@gmail.com>
2022-10-14 13:19:22 +0100

    Removed wording from description to make converter universal

--- devices/fantem.js
+++ devices/fantem.js
@@ -13,7 +13,7 @@ module.exports = [
             {modelID: 'TS110F', manufacturerName: '_TZ3210_ebbfkvoy'}],
         model: 'ZB006-X',
         vendor: 'Fantem',
-        description: 'Smart dimmer module (optional neutral)',
+        description: 'Smart dimmer module',
         extend: extend.light_onoff_brightness({noConfigure: true}),
         fromZigbee: [...extend.light_onoff_brightness({noConfigure: true}).fromZigbee,
             fz.command_on, fz.command_off, fz.command_move, fz.command_stop, fz.ZB006X_settings],
