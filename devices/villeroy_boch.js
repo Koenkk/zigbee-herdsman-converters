e32178c10a0b8ced821053b802d6f02ddb8e6367
Koen Kanters <koenkanters94@gmail.com>
2022-10-18 17:47:19 +0200

    Add C0040000. https://github.com/Koenkk/zigbee2mqtt/issues/14515

--- devices/villeroy_boch.js
+++ devices/villeroy_boch.js
@@ -5,7 +5,14 @@ module.exports = [
         zigbeeModel: ['5991711'],
         model: 'C5850000',
         vendor: 'Villeroy & Boch',
-        description: 'Subway 3.0 Zigbee home Aautomation kit ',
+        description: 'Subway 3.0 Zigbee home automation kit',
+        extend: extend.light_onoff_brightness_colortemp({colorTempRange: [160, 450]}),
+    },
+    {
+        zigbeeModel: ['EC1300'],
+        model: 'C0040000',
+        vendor: 'Villeroy & Boch',
+        description: 'Zigbee home automation kit for mirror',
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [160, 450]}),
     },
 ];
