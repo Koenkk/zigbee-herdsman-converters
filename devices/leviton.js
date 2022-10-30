c5cf4b0f0f00f42d10aebf1d13062932db355aee
Kevin David <kevin-david@github.com>
2022-10-30 15:46:56 +0000

    Add `meta: { disableDefaultResponse: true }`, remove converters that caused bad behavior
    In particular, `*.level_config` was causing the lights to turn off then back on.

--- devices/leviton.js
+++ devices/leviton.js
@@ -93,10 +93,11 @@ module.exports = [
         description: 'Wall switch, 0-10V dimmer, 120-277V, Lumina™ RF',
         // Do not use extend.light_onoff_brightness because that has fz.ignore_basic_report,
         // which is what tells us when the physical switch is used
-        fromZigbee: [fz.on_off, fz.on_off_via_brightness, fz.brightness, fz.level_config, fz.identify,
+        meta: { disableDefaultResponse: true },
+        fromZigbee: [fz.on_off, fz.brightness, fz.on_off_via_brightness, fz.identify,
             fz.lighting_ballast_configuration],
-        toZigbee: [tz.on_off, tz.light_onoff_brightness, tz.ballast_config, tz.level_config],
-        exposes: [e.light_brightness().withLevelConfig(),
+        toZigbee: [tz.light_onoff_brightness, tz.ballast_config],
+        exposes: [e.light_brightness(),
             exposes.numeric('ballast_minimum_level', ea.ALL).withValueMin(1).withValueMax(254)
                 .withDescription('Specifies the minimum brightness value'),
             exposes.numeric('ballast_maximum_level', ea.ALL).withValueMin(1).withValueMax(254)
