Koenkk/zigbee-herdsman-converters

commit 4d2a08b85725ed078d5b8bf0ebf940df86a9dc7c

Remove `ballast_physical_*_level` (#4813)
* Fix ZS057 `ballast_physical_*_level`

This is not settable, and the range is 0-255 not 1-254

```
Read result of 'lightingBallastCfg': {"physicalMinLevel":0,"physicalMaxLevel":255}
```

and
```
First occurred: October 16, 2022 at 11:22:54 PM (2670 occurrences)
Last logged: 2:44:12 PM

    Invalid value for number.0x25c823ffffa60700_ballast_physical_minimum_level: 0 (range 1.0 - 254.0)
    Invalid value for number.0x25c823ffffa60700_ballast_physical_maximum_level: 255 (range 1.0 - 254.0)
```

and

```
Publish 'set' 'write' to '0x25c823ffffa60700' failed: 'Error: Write 0x25c823ffffa60700/1 lightingBallastCfg({"physicalMinLevel":6}, {"sendWhen":"immediate","timeout":10000,"disableResponse":false,"disableRecovery":false,"disableDefaultResponse":true,"direction":0,"srcEndpoint":null,"reservedBits":0,"manufacturerCode":null,"transactionSequenceNumber":null,"writeUndiv":false}) failed (Status 'READ_ONLY')'
```

* Update toZigbee.js

* Completely remove `ballast_physical_*_level`

As discussed in https://github.com/Koenkk/zigbee-herdsman-converters/pull/4813#issuecomment-1288036529

Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

converters/fromZigbee.js
@@ -1588,12 +1588,6 @@ const converters = {
         type: ['attributeReport', 'readResponse'],
         convert: (model, msg, publish, options, meta) => {
             const result = {};
-            if (msg.data.hasOwnProperty('physicalMinLevel')) {
-                result['ballast_physical_minimum_level'] = msg.data.physicalMinLevel;
-            }
-            if (msg.data.hasOwnProperty('physicalMaxLevel')) {
-                result['ballast_physical_maximum_level'] = msg.data.physicalMaxLevel;
-            }
             if (msg.data.hasOwnProperty('ballastStatus')) {
                 const ballastStatus = msg.data.ballastStatus;
                 result['ballast_status_non_operational'] = ballastStatus & 1 ? true : false;

devices/leviton.js
@@ -100,11 +100,7 @@ module.exports = [
             exposes.numeric('ballast_maximum_level', ea.ALL).withValueMin(1).withValueMax(254)
                 .withDescription('Specifies the maximum brightness value'),
             exposes.numeric('ballast_power_on_level', ea.ALL).withValueMin(1).withValueMax(254)
-                .withDescription('Specifies the initialisation light level. Can not be set lower than "ballast_minimum_level"'),
-            exposes.numeric('ballast_physical_minimum_level', ea.ALL).withValueMin(1).withValueMax(254)
-                .withDescription('Specifies the minimum light output the ballast can achieve.'),
-            exposes.numeric('ballast_physical_maximum_level', ea.ALL).withValueMin(1).withValueMax(254)
-                .withDescription('Specifies the maximum light output the ballast can achieve.')],
+                .withDescription('Specifies the initialisation light level. Can not be set lower than "ballast_minimum_level"')],
         configure: async (device, coordinatorEndpoint, logger) => {
             await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
             const endpoint = device.getEndpoint(1);

converters/toZigbee.js
@@ -578,8 +578,6 @@ const converters = {
     },
     ballast_config: {
         key: ['ballast_config',
-            'ballast_physical_minimum_level',
-            'ballast_physical_maximum_level',
             'ballast_minimum_level',
             'ballast_maximum_level',
             'ballast_power_on_level'],
@@ -607,8 +605,6 @@ const converters = {
         convertGet: async (entity, key, meta) => {
             let result = {};
             for (const attrName of [
-                'physical_min_level',
-                'physical_max_level',
                 'ballast_status',
                 'min_level',
                 'max_level',

devices/ubisys.js
@@ -671,10 +671,6 @@ module.exports = [
             ubisys.tz.dimmer_setup_genLevelCtrl, ubisys.tz.configure_device_setup, tz.ignore_transition, tz.light_brightness_move,
             tz.light_brightness_step],
         exposes: [e.light_brightness().withLevelConfig(), e.power(),
-            exposes.numeric('ballast_physical_minimum_level', ea.ALL).withValueMin(1).withValueMax(254)
-                .withDescription('Specifies the minimum light output the ballast can achieve.'),
-            exposes.numeric('ballast_physical_maximum_level', ea.ALL).withValueMin(1).withValueMax(254)
-                .withDescription('Specifies the maximum light output the ballast can achieve.'),
             exposes.numeric('ballast_minimum_level', ea.ALL).withValueMin(1).withValueMax(254)
                 .withDescription('Specifies the minimum light output of the ballast'),
             exposes.numeric('ballast_maximum_level', ea.ALL).withValueMin(1).withValueMax(254)

commit 96c1bb6d4717d9e49cd2f810360dcfc9f270ba89

Improve Leviton ZS057 reporting behavior
I broke this out from #4813 because it really is a different problem entirely...

I figured out the problem I mentioned here: https://github.com/Koenkk/zigbee-herdsman-converters/pull/4813#issuecomment-1288254692 - see the added comment

I'm still having a big problem with timeouts on this device despite what I _think_ is acceptable LQI (50+), getting errors like this:

```
error 2022-10-25 23:38:15Publish 'set' 'state' to '0x25c823ffffa60700' failed: 'Error: Command 0x25c823ffffa60700/1 genLevelCtrl.moveToLevelWithOnOff({"level":0,"transtime":0}, {"sendWhen":"immediate","timeout":10000,"disableResponse":false,"disableRecovery":false,"disableDefaultResponse":false,"direction":0,"srcEndpoint":null,"reservedBits":0,"manufacturerCode":null,"transactionSequenceNumber":null,"writeUndiv":false}) failed (Timeout - 29522 - 1 - 48 - 8 - 11 after 10000ms)'
```

It seems _partially_ working because the brightness change takes effect, but it gives that error / times out.

Strangely, using `moveToLevel` instead doesn't seem to work well (makes the lights flash)... still digging in, any advice apprechiated!

converters/fromZigbee.js
@@ -788,6 +788,17 @@ const converters = {
             }
         },
     },
+    on_off_via_brightness: {
+        cluster: 'genLevelCtrl',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            if (msg.data.hasOwnProperty('currentLevel')) {
+                const currentLevel = Number(msg.data['currentLevel']);
+                const property = postfixWithEndpointName('state', msg, model, meta);
+                return {[property]: currentLevel > 0 ? 'ON' : 'OFF'};
+            }
+        },
+    },
     power_on_behavior: {
         cluster: 'genOnOff',
         type: ['attributeReport', 'readResponse'],

devices/leviton.js
@@ -91,10 +91,12 @@ module.exports = [
         model: 'ZS057-D0Z',
         vendor: 'Leviton',
         description: 'Wall switch, 0-10V dimmer, 120-277V, Lumina™ RF',
-        extend: extend.light_onoff_brightness({disableEffect: true, noConfigure: true}),
-        fromZigbee: [fz.brightness, fz.identify, fz.lighting_ballast_configuration, fz.level_config],
-        toZigbee: [tz.light_onoff_brightness, tz.ballast_config, tz.level_config],
-        exposes: [e.light_brightness(),
+        // Do not use extend.light_onoff_brightness because that has fz.ignore_basic_report,
+        // which is what tells us when the physical switch is used
+        fromZigbee: [fz.on_off, fz.on_off_via_brightness, fz.brightness, fz.level_config, fz.identify,
+            fz.lighting_ballast_configuration],
+        toZigbee: [tz.on_off, tz.light_onoff_brightness, tz.ballast_config, tz.level_config],
+        exposes: [e.light_brightness().withLevelConfig(),
             exposes.numeric('ballast_minimum_level', ea.ALL).withValueMin(1).withValueMax(254)
                 .withDescription('Specifies the minimum brightness value'),
             exposes.numeric('ballast_maximum_level', ea.ALL).withValueMin(1).withValueMax(254)
@@ -102,10 +104,10 @@ module.exports = [
             exposes.numeric('ballast_power_on_level', ea.ALL).withValueMin(1).withValueMax(254)
                 .withDescription('Specifies the initialisation light level. Can not be set lower than "ballast_minimum_level"')],
         configure: async (device, coordinatorEndpoint, logger) => {
-            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl', 'lightingBallastCfg']);
             await reporting.onOff(endpoint);
+            await reporting.brightness(endpoint);
         },
     },
 ];