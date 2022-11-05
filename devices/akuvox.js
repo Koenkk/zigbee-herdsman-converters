975cc35ed32e7ebf204b9c767b52d6a127c31f5d
Burkhard Kneiseler <burkhard@kneiseler.de>
2022-11-05 05:40:59 +0100

    Update akuvox.js

--- devices/akuvox.js
+++ devices/akuvox.js
@@ -1,34 +1,29 @@
 const exposes = require('../lib/exposes');
 const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
-const tz = require('../converters/toZigbee');
-const constants = require('../lib/constants');
 const reporting = require('../lib/reporting');
-const extend = require('../lib/extend');
 const e = exposes.presets;
-const ea = exposes.access;
-const tuya = require('../lib/tuya');
 
 module.exports = [
-{
-    fingerprint: [{modelID: 'TS0201', manufacturerName: '_TYZB01_ujfk3xd9'}],
-    zigbeeModel: ['M423-9E'],
-    model: 'M423-9E',
-    vendor: 'Akuvox',
-    description: 'Smart temperature & humidity Sensor',
-    exposes: [e.battery(), e.temperature(), e.humidity()],
-    fromZigbee: [fz.temperature, fz.humidity, fz.battery],
-    toZigbee: [],
-    meta: {battery: {voltageToPercentage: '3V_2500'}},
-    configure: async (device, coordinatorEndpoint, logger) => {
-        const endpoint1 = device.getEndpoint(1);
-        await reporting.bind(endpoint1, coordinatorEndpoint, ['msTemperatureMeasurement', 'genPowerCfg']);
-        await endpoint1.read('genPowerCfg', ['batteryPercentageRemaining']);
-        const endpoint2 = device.getEndpoint(2);
-        await reporting.bind(endpoint2, coordinatorEndpoint, ['msRelativeHumidity']);
-        await reporting.temperature(endpoint1);
-        await reporting.humidity(endpoint2);
-        await reporting.batteryVoltage(endpoint1);
-        await reporting.batteryPercentageRemaining(endpoint1);
+    {
+        fingerprint: [{modelID: 'TS0201', manufacturerName: '_TYZB01_ujfk3xd9'}],
+        zigbeeModel: ['M423-9E'],
+        model: 'M423-9E',
+        vendor: 'Akuvox',
+        description: 'Smart temperature & humidity Sensor',
+        exposes: [e.battery(), e.temperature(), e.humidity()],
+        fromZigbee: [fz.temperature, fz.humidity, fz.battery],
+        toZigbee: [],
+        meta: {battery: {voltageToPercentage: '3V_2500'}},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['msTemperatureMeasurement', 'genPowerCfg']);
+            await endpoint1.read('genPowerCfg', ['batteryPercentageRemaining']);
+            const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['msRelativeHumidity']);
+            await reporting.temperature(endpoint1);
+            await reporting.humidity(endpoint2);
+            await reporting.batteryVoltage(endpoint1);
+            await reporting.batteryPercentageRemaining(endpoint1);
+        },
     },
-},
 ];
