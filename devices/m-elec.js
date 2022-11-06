8597b8699811feb30fcf9101144b1add930c2f04
gid204 <gideon.kanikevich@me.com>
2022-11-06 22:01:42 +1100

    add support for ML-ST-BP-DIM

--- devices/m-elec.js
+++ devices/m-elec.js
@@ -8,4 +8,11 @@ module.exports = [
         description: 'Stitchy Dim switchable wall module',
         extend: extend.light_onoff_brightness(),
     },
+    {
+        zigbeeModel: ['ML-ST-BP-DIM'],
+        model: 'ML-ST-BP-DIM',
+        vendor: 'M-ELEC',
+        description: 'Stitchy Dim Mechanism',
+        extend: extend.light_onoff_brightness({disableEffect: true}),
+    },
 ];
