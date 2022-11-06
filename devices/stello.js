a776af2ceeb298b4f763cd2e3bdd65ac651f3c63
FrancoLoco <37370192+FrancoLoco@users.noreply.github.com>
2022-11-06 15:50:49 -0500

    Add files via upload

--- devices/stello.js
+++ devices/stello.js
@@ -8,24 +8,24 @@ const ea = exposes.access;
 
 const stello = {
     fz: {
-          stello_power: {
+          power: {
             cluster: 'hvacThermostat',
             type: ['attributeReport', 'readResponse'],
             convert: (model, msg, publish, options, meta) => {
                 if (msg.data.hasOwnProperty('16392')) {
-                    return {stello_power: msg.data["16392"]};
+                    return {power: msg.data["16392"]};
                 }
 				else {
 					return 0;
 				}					
             },
 		  },		  
-		  stello_energy: {
+		  energy: {
             cluster: 'hvacThermostat',
             type: ['attributeReport', 'readResponse'],
             convert: (model, msg, publish, options, meta) => {
                 if (msg.data.hasOwnProperty('16393')) {
-                    return {stello_energy: parseFloat(msg.data["16393"]) / 1000};
+                    return {energy: parseFloat(msg.data["16393"]) / 1000};
                 }
             },
         },
@@ -37,11 +37,11 @@ const definition = {
 	model: 'HT402', 
 	vendor: 'Stello', 
 	description: 'Hilo Stelpro thermostat', 
-	fromZigbee: [fz.legacy.stelpro_thermostat, fz.legacy.hvac_user_interface, stello.fz.stello_power, stello.fz.stello_energy],  
+	fromZigbee: [fz.legacy.stelpro_thermostat, fz.legacy.hvac_user_interface, stello.fz.power, stello.fz.energy],  
 	toZigbee: [tz.thermostat_local_temperature, tz.thermostat_occupancy, tz.thermostat_occupied_heating_setpoint,
 		tz.thermostat_temperature_display_mode, tz.thermostat_keypad_lockout, tz.thermostat_system_mode,
 		tz.thermostat_running_state, tz.stelpro_thermostat_outdoor_temperature],
-	exposes: [e.local_temperature(), e.keypad_lockout(), e.power().withAccess(ea.STATE_GET).withProperty('stello_power'), e.energy().withAccess(ea.STATE_GET).withProperty('stello_energy'), 
+	exposes: [e.local_temperature(), e.keypad_lockout(), e.power().withAccess(ea.STATE_GET).withProperty('power'), e.energy().withAccess(ea.STATE_GET).withProperty('energy'), 
 		exposes.climate().withSetpoint('occupied_heating_setpoint', 5, 30, 0.5).withLocalTemperature()
 			.withSystemMode(['heat']).withRunningState(['idle', 'heat'])],
 	configure: async (device, coordinatorEndpoint, logger) => {
