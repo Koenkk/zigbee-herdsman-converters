ed658884d5d0d3f9bd920ce93388c5ddd6ebea43
Koen Kanters <koenkanters94@gmail.com>
2022-11-07 17:35:10 +0100

    Update akuvox.js

--- devices/akuvox.js
+++ devices/akuvox.js
@@ -1,12 +1,11 @@
 const exposes = require('../lib/exposes');
-const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
+const fz = require('../converters/fromZigbee');
 const reporting = require('../lib/reporting');
 const e = exposes.presets;
 
 module.exports = [
     {
         fingerprint: [{modelID: 'TS0201', manufacturerName: '_TYZB01_ujfk3xd9'}],
-        zigbeeModel: ['M423-9E'],
         model: 'M423-9E',
         vendor: 'Akuvox',
         description: 'Smart temperature & humidity Sensor',
