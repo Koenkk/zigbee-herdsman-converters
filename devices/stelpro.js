d00369d7d3a2f080aec919ed602161372112bf83
Koen Kanters <koenkanters94@gmail.com>
2022-11-08 16:52:54 +0100

    Update stelpro.js

--- devices/stelpro.js
+++ devices/stelpro.js
@@ -3,46 +3,40 @@ const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/lega
 const tz = require('../converters/toZigbee');
 const reporting = require('../lib/reporting');
 const e = exposes.presets;
-const ea = exposes.access;
 const constants = require('../lib/constants');
 
-const stello = {
-    fz: {
-          power: {
-            cluster: 'hvacThermostat',
-            type: ['attributeReport', 'readResponse'],
-            convert: (model, msg, publish, options, meta) => {
-                if (msg.data.hasOwnProperty('16392')) {
-                    return {power: msg.data["16392"]};
-                }
-				else {
-					return 0;
-				}					
-            },
-		  },		  
-		  energy: {
-            cluster: 'hvacThermostat',
-            type: ['attributeReport', 'readResponse'],
-            convert: (model, msg, publish, options, meta) => {
-                if (msg.data.hasOwnProperty('16393')) {
-                    return {energy: parseFloat(msg.data["16393"]) / 1000};
-                }
-            },
+const fzLocal = {
+    power: {
+        cluster: 'hvacThermostat',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            if (msg.data.hasOwnProperty('16392')) {
+                return {power: msg.data['16392']};
+            }
+        },
+    },
+    energy: {
+        cluster: 'hvacThermostat',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            if (msg.data.hasOwnProperty('16393')) {
+                return {energy: parseFloat(msg.data['16393']) / 1000};
+            }
         },
     },
 };
 
 module.exports = [
     {
-        zigbeeModel: ['HT402'], 
-        model: 'HT402', 
-        vendor: 'Stelpro', 
-        description: 'Hilo thermostat', 
-        fromZigbee: [fz.legacy.stelpro_thermostat, fz.legacy.hvac_user_interface, stello.fz.power, stello.fz.energy],  
+        zigbeeModel: ['HT402'],
+        model: 'HT402',
+        vendor: 'Stelpro',
+        description: 'Hilo thermostat',
+        fromZigbee: [fz.stelpro_thermostat, fz.hvac_user_interface, fzLocal.power, fzLocal.energy],
         toZigbee: [tz.thermostat_local_temperature, tz.thermostat_occupancy, tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_temperature_display_mode, tz.thermostat_keypad_lockout, tz.thermostat_system_mode,
             tz.thermostat_running_state, tz.stelpro_thermostat_outdoor_temperature],
-        exposes: [e.local_temperature(), e.keypad_lockout(), e.power().withAccess(ea.STATE_GET).withProperty('power'), e.energy().withAccess(ea.STATE_GET).withProperty('energy'), 
+        exposes: [e.local_temperature(), e.keypad_lockout(), e.power(), e.energy(),
             exposes.climate().withSetpoint('occupied_heating_setpoint', 5, 30, 0.5).withLocalTemperature()
                 .withSystemMode(['heat']).withRunningState(['idle', 'heat'])],
         configure: async (device, coordinatorEndpoint, logger) => {
