1db58b106a28818364f6f3d50adbd41a60286b41
danieledwardgeorgehitchcock <daniel.edward.george.hitchcock@gmail.com>
2022-11-14 20:02:22 +0000

    Add LED2006R9 (#4952)
    * Added support for Tradfri PAR38 bulb
    
    * Update ikea.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/ikea.js
+++ devices/ikea.js
@@ -999,4 +999,11 @@ module.exports = [
         description: 'STOFTMOLN ceiling/wall lamp 24 warm light dimmable',
         extend: tradfriExtend.light_onoff_brightness(),
     },
+    {
+        zigbeeModel: ['TTRADFRIbulbPAR38WS900lm'],
+        model: 'LED2006R9',
+        vendor: 'IKEA',
+        description: 'TRADFRI E26 PAR38 LED bulb 900 lumen, dimmable, white spectrum, downlight',
+        extend: tradfriExtend.light_onoff_brightness_colortemp(),
+    },
 ];
