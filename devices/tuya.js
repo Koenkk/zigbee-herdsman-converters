9d75e24c7d7654beeb20aaa55fe18c6fc0562ad0
Koen Kanters <koenkanters94@gmail.com>
2022-11-15 19:40:31 +0100

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -2479,7 +2479,7 @@ module.exports = [
         meta: {
             tuyaDatapoints: [
                 [1, 'energy', tuya.valueConverter.divideBy100],
-                [6, null, tuya.valueConverter.phaseA], // voltage and current
+                [6, null, tuya.valueConverter.phaseVariant1], // voltage and current
                 [16, 'state', tuya.valueConverter.onOff],
                 [102, 'produced_energy', tuya.valueConverter.divideBy100],
                 [103, 'power', tuya.valueConverter.raw],
@@ -2507,14 +2507,16 @@ module.exports = [
         exposes: [tuya.exposes.switch(), e.energy(), e.power(), e.voltage(), e.current(),
             exposes.enum('fault', ea.STATE, ['clear', 'over_current_threshold', 'over_power_threshold',
                 'over_voltage threshold', 'wrong_frequency_threshold']).withDescription('Fault status of the device (clear = nothing)'),
-            exposes.binary('threshold_1_state', ea.STATE, 'ON', 'OFF')
+            exposes.enum('threshold_1', ea.STATE, ['not_set', 'over_current_threshold', 'over_voltage_threshold'])
+                .withDescription('State of threshold_1'),
+            exposes.binary('threshold_1_protection', ea.STATE, 'ON', 'OFF')
                 .withDescription('OFF - alarm only, ON - relay will be off when threshold reached'),
-            exposes.text('threshold_1_description', ea.STATE), // TODO: what is this?
             exposes.numeric('threshold_1_value', ea.STATE)
                 .withDescription('Can be in Volt or Ampere depending on threshold setting. Setup the value on the device'),
-            exposes.binary('threshold_2_state', ea.STATE, 'ON', 'OFF')
+            exposes.enum('threshold_2', ea.STATE, ['not_set', 'over_current_threshold', 'over_voltage_threshold'])
+                .withDescription('State of threshold_2'),
+            exposes.binary('threshold_2_protection', ea.STATE, 'ON', 'OFF')
                 .withDescription('OFF - alarm only, ON - relay will be off when threshold reached'),
-            exposes.text('threshold_2_description', ea.STATE),
             exposes.numeric('threshold_2_value', ea.STATE)
                 .withDescription('Setup value on the device'),
             exposes.binary('clear_fault', ea.STATE_SET, 'ON', 'OFF')
@@ -2526,12 +2528,12 @@ module.exports = [
                 [1, 'energy', tuya.valueConverter.divideBy100],
                 [3, null, null], // Monthly, but sends data only after request
                 [4, null, null], // Dayly, but sends data only after request
-                [6, null, tuya.valueConverter.phaseB], // voltage and current
+                [6, null, tuya.valueConverter.phaseVariant2], // voltage and current
                 [10, 'fault', tuya.valueConverterBasic.lookup({'clear': 0, 'over_current_threshold': 1,
                     'over_power_threshold': 2, 'over_voltage_threshold': 4, 'wrong_frequency_threshold': 8})],
                 [11, null, null], // Frozen - strange function, in native app - nothing is clear
                 [16, 'state', tuya.valueConverter.onOff],
-                [17, null, tuya.valueConverter.alarm], // It's settable, but can't write converter
+                [17, null, tuya.valueConverter.threshold], // It's settable, but can't write converter
                 [18, 'meterid', tuya.valueConverter.raw],
                 [20, 'clear_fault', tuya.valueConverter.onOff], // Clear fault
                 [21, null, null], // Forward Energy T1 - don't know what this
