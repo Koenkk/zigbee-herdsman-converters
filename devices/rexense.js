87ffb51151b447cbb8f09eb6630f730a03489479
cxf <chengxunfei@126.com>
2022-11-16 11:54:38 +0800

    add colorTempRange for zigbeeModel RH0030

--- devices/rexense.js
+++ devices/rexense.js
@@ -6,6 +6,6 @@ module.exports = [
         model: 'RH0030',
         vendor: 'Rexense',
         description: 'Rexense Color bulb lamp',
-        extend: extend.light_onoff_brightness_colortemp_color(),
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
 ];
