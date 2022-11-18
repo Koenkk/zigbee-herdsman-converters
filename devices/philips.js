609c78ac4da402d7eb9ac60468dfa08fbfd50f52
synsi23b <80886627+synsi23b@users.noreply.github.com>
2022-11-18 22:51:36 +0900

    Add Phillips Hue Go with Bluetooth
    japanese model number

--- devices/philips.js
+++ devices/philips.js
@@ -561,7 +561,7 @@ module.exports = [
         extend: hueExtend.light_onoff_brightness(),
     },
     {
-        zigbeeModel: ['LCT026', '7602031P7', '7602031U7', '7602031PU'],
+        zigbeeModel: ['LCT026', '7602031P7', '7602031U7', '7602031PU', '7602031J6'],
         model: '7602031P7',
         vendor: 'Philips',
         description: 'Hue Go with Bluetooth',
