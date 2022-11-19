9b7cfcde31a187c742dd1b6fc4949668dcab31fe
Alexey Bolshov <Alex_Bolshov@bitbucket.org>
2022-11-19 16:20:10 +0200

    Update kdesv.js

--- devices/kdesv.js
+++ devices/kdesv.js
@@ -2,26 +2,24 @@ const fz = require('../converters/fromZigbee');
 const tz = require('../converters/toZigbee');
 const exposes = require('../lib/exposes');
 const reporting = require('../lib/reporting');
-const extend = require('../lib/extend');
 const ota = require('../lib/ota');
 const e = exposes.presets;
-const ea = exposes.access;
 
 module.exports = [
-	{
-		zigbeeModel: ['TWV'],
-		model: 'TWV',
-		vendor: 'KDE',
-		description: 'UHome Smart Valve',
-		ota: ota.zigbeeOTA,
-		fromZigbee: [fz.on_off, fz.battery],
-		toZigbee: [tz.on_off],
-		exposes: [e.battery(), e.switch()],
-			configure: async (device, coordinatorEndpoint) => {
-			const endpoint = device.getEndpoint(1);
-			await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg','genOnOff']);
-			await reporting.batteryPercentageRemaining(endpoint);
-			await reporting.onOff(endpoint);
-		},
-	},
+    {
+     zigbeeModel: ['TWV'],
+     model: 'TWV',
+     vendor: 'KDE',
+     description: 'UHome Smart Valve',
+     ota: ota.zigbeeOTA,
+     fromZigbee: [fz.on_off, fz.battery],
+     toZigbee: [tz.on_off],
+     exposes: [e.battery(), e.switch()],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'genOnOff']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.onOff(endpoint);
+        },
+    },
 ];
