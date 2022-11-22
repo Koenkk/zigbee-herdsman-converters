3904127931ef780f1aa2f6474ccf23d69ef2c6f2
floxboy <floxxx@gmail.com>
2022-11-22 16:29:02 +0500

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -1504,7 +1504,7 @@ module.exports = [
             {modelID: 'TS0201', manufacturerName: '_TZ3000_fllyghyj'},
             {modelID: 'TS0201', manufacturerName: '_TZ3000_yd2e749y'},
             {modelID: 'TS0201', manufacturerName: '_TZ3000_6uzkisv2'},
-            {modelID: 'TS0601', manufacturerName: '__TZE200_9yapgbuv'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_9yapgbuv'},
         ],
         model: 'WSD500A',
         vendor: 'TuYa',
