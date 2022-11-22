f7a62e5426f92d26734de3a4123c024a4599a806
André Dahlqvist <andre.dahlqvist@notified.com>
2022-11-23 00:03:41 +0100

    Fix D086-ZG so it reports changes to Zigbee

--- devices/hzc_electric.js
+++ devices/hzc_electric.js
@@ -4,28 +4,19 @@ const exposes = require('../lib/exposes');
 const fz = require('../converters/fromZigbee');
 const e = exposes.presets;
 
+
 module.exports = [
     {
         zigbeeModel: ['DimmerSwitch-2Gang-ZB3.0'],
         model: 'D086-ZG',
         vendor: 'HZC Electric',
         description: 'Zigbee dual dimmer',
-        extend: extend.light_onoff_brightness({noConfigure: true}),
+        extend: extend.light_onoff_brightness(),
         exposes: [e.light_brightness().withEndpoint('l1'), e.light_brightness().withEndpoint('l2')],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
-            const endpoint1 = device.getEndpoint(1);
-            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-            await reporting.onOff(endpoint1);
-
-            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
-            const endpoint2 = device.getEndpoint(2);
-            await reporting.bind(endpoint2, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-            await reporting.onOff(endpoint2);
-        },
         endpoint: () => {
             return {l1: 1, l2: 2};
         },
+        meta: {multiEndpoint: true, disableDefaultResponse: true},
     },
     {
         zigbeeModel: ['TempAndHumSensor-ZB3.0'],
