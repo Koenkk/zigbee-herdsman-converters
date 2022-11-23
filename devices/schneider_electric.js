c48927eec63b7ac629ac261bbc9fc7a4d809ec97
dotnetpro100888 <118766466+dotnetpro100888@users.noreply.github.com>
2022-11-23 17:16:49 +0100

    Add MEG5161-0000 (#5002)
    * support for merten 1GANG/SWITCH/1 added
    
    * Update schneider_electric.js
    
    * Update schneider_electric.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/schneider_electric.js
+++ devices/schneider_electric.js
@@ -465,6 +465,18 @@ module.exports = [
         },
     },
     {
+        zigbeeModel: ['1GANG/SWITCH/1'],
+        model: 'MEG5161-0000',
+        vendor: 'Schneider Electric',
+        description: 'Merten PlusLink relay insert with Merten Wiser system M push button (1fold)',
+        extend: extend.switch(),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint);
+        },
+    },
+    {
         zigbeeModel: ['LK Switch'],
         model: '545D6514',
         vendor: 'Schneider Electric',
