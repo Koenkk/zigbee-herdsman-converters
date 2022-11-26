43ef88657c63099d3031b1edd44df6a8dbbb9681
Bart Gysens <bart.gysens@gmail.com>
2022-11-26 09:44:47 +0100

    Add PIR323-PTH (#5019)
    * Add device PIR323-PTH - Multi-sensor
    
    Productpage: https://www.owon-smart.com/wireless-zone-sensor-for-smart-home-sensor-323-2-product/
    Datasheet device: https://www.owon-smart.com/uploads/Datasheet-PIR323-ZigBee-Multi-Sensor.pdf
    
    * Add device PIR323-PTH - Multi-sensor
    
    Productpage: https://www.owon-smart.com/wireless-zone-sensor-for-smart-home-sensor-323-2-product/
    Datasheet: https://www.owon-smart.com/uploads/Datasheet-PIR323-ZigBee-Multi-Sensor.pdf
    
    * Update owon.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/owon.js
+++ devices/owon.js
@@ -290,4 +290,19 @@ module.exports = [
             await endpoint2.read('msOccupancySensing', ['occupancy']);
         },
     },
+    {
+        zigbeeModel: ['PIR323-PTH'],
+        model: 'PIR323-PTH',
+        vendor: 'OWON',
+        description: 'Multi-sensor',
+        fromZigbee: [fz.battery, fz.ignore_basic_report, fz.ias_occupancy_alarm_1, fz.temperature, fz.humidity, fz.occupancy_timeout],
+        toZigbee: [],
+        exposes: [e.occupancy(), e.battery_low(), e.temperature(), e.humidity()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(2);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['msTemperatureMeasurement', 'msRelativeHumidity']);
+            device.powerSource = 'Battery';
+            device.save();
+        },
+    },
 ];
