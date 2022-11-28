2392bb503a6d821da58e5be96fb678e9109b0aa8
alesoft73 <46445679+alesoft73@users.noreply.github.com>
2022-11-28 18:09:54 +0100

    Add _TZE200_atpwqgml to TS0601_switch_3_gang (#5036)
    * Add new model tuya TA0601 "_TZE200_atpwqgml"
    
    Add a new model manufacturer 3 gang swicth.
    
    * Correct the error
    
    Sorry for erro, many thanks

--- devices/tuya.js
+++ devices/tuya.js
@@ -1422,7 +1422,8 @@ module.exports = [
     {
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_kyfqmmyl'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_2hf7x9n3'},
-            {modelID: 'TS0601', manufacturerName: '_TZE200_bynnczcb'}],
+            {modelID: 'TS0601', manufacturerName: '_TZE200_bynnczcb'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_atpwqgml'}],
         model: 'TS0601_switch_3_gang',
         vendor: 'TuYa',
         description: '3 gang switch',
