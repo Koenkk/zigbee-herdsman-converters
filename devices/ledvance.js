8ef420c5e5de5f808c1b27a88b4110a146364caf
LEDVANCE Products <109606930+dhanukapulkit@users.noreply.github.com>
2022-11-29 15:26:17 +0100

    Update ledvance.js

--- devices/ledvance.js
+++ devices/ledvance.js
@@ -286,6 +286,19 @@ module.exports = [
         },
     },
     {
+        zigbeeModel: ['PLUG EU T'],
+        model: '4058075729261',
+        vendor: 'LEDVANCE',
+        description: 'SMART+ Plug EU',
+        extend: extend.switch(),
+        ota: ota.ledvance,
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint);
+        },
+    },
+    {
         zigbeeModel: ['PLUG OUTDOOR EU T'],
         model: '4058075729308',
         vendor: 'LEDVANCE',
