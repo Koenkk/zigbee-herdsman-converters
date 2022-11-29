cbbab69109f092186c24daaade47764dc0529779
MattL0 <matthieulauzon@live.ca>
2022-11-28 22:41:34 -0500

    Update sinope.js

--- devices/sinope.js
+++ devices/sinope.js
@@ -30,9 +30,9 @@ module.exports = [
                 .withSystemMode(['off', 'heat'], ea.ALL, 'Mode of the thermostat')
                 .withRunningState(['idle', 'heat'])
                 .withPiHeatingDemand(ea.STATE_GET),
-            exposes.enum('thermostat_occupancy', ea.SET, ['unoccupied', 'occupied'])
+            exposes.enum('thermostat_occupancy', ea.ALL, ['unoccupied', 'occupied'])
                 .withDescription('Occupancy state of the thermostat'),
-            exposes.enum('backlight_auto_dim', ea.SET, ['on demand', 'sensing'])
+            exposes.enum('backlight_auto_dim', ea.ALL, ['on demand', 'sensing'])
                 .withDescription('Control backlight dimming behavior'),
             exposes.enum('keypad_lockout', ea.ALL, ['unlock', 'lock1'])
                 .withDescription('Enables or disables the device’s buttons'),
