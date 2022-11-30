b809a01ab23aef500b0fb4de02bd814760e667dd
Gustav Westling <gustav@westling.dev>
2022-11-30 20:49:10 +0100

    philips/gradient: decode and export current state, use x/y color space instead of hex, support up to 9 input colors

--- devices/philips.js
+++ devices/philips.js
@@ -5,7 +5,7 @@ const ota = require('../lib/ota');
 const reporting = require('../lib/reporting');
 const globalStore = require('../lib/store');
 const utils = require('../lib/utils');
-const ColorRGB = require('../lib/color').ColorRGB;
+const ColorXY = require('../lib/color').ColorXY;
 const e = exposes.presets;
 const ea = exposes.access;
 
@@ -82,6 +82,32 @@ const fzLocal = {
             }
             return payload;
         },
+    },
+
+    philips_hue_multicolor: (opts = {reverse: false}) => {
+        return {
+            cluster: 'manuSpecificPhilips2',
+            type: ['attributeReport', 'readResponse'],
+            convert: (model, msg, publish, options, meta) => {
+                if (msg.data.hasOwnProperty('2')) {
+                    const input = msg.data['2'].toString('hex');
+                    // Example set:         500104001350000000f3297ff3bd52f3bd52f3297ff3bd522800
+                    // Example get  4b010164fb74346b1350000000f3297fda7d55da7d55f3297fda7d552800
+                    const offset = input.indexOf('1350000000') + 10;
+                    const points = input.slice(offset, - 4);
+                    const pairs = points.match(/.{6}/g);
+                    const colors = pairs.map(decodeScaledGradientToXY);
+
+                    if (opts.reverse) {
+                        colors.reverse();
+                    }
+
+                    return {colors};
+                }
+
+                return {};
+            },
+        };
     },
 };
 
@@ -163,11 +189,10 @@ const gradientScenes = {
     'crystalline': '5001040013500000006ea96a92a85e58074e18543d9cf3332800',
 };
 
-const encodeHexToPoint = (hex) => {
-    const rgb = ColorRGB.fromHex(hex);
-    const xy = rgb.toXY();
-    const x = xy.x * 4095 / 0.7347;
-    const y = xy.y * 4095 / 0.8413;
+
+const encodePoint = (point) => {
+    const x = point['x'] * 4095 / 0.7347;
+    const y = point['y'] * 4095 / 0.8413;
     const xx = Math.round(x).toString(16);
     const yy = Math.round(y).toString(16);
 
@@ -176,6 +201,16 @@ const encodeHexToPoint = (hex) => {
         yy[2], xx[0],
         yy[0], yy[1],
     ].join('');
+};
+
+const decodeScaledGradientToXY = (p) => {
+    const x = p[3] + p[0] + p[1];
+    const y = p[4] + p[5] + p[2];
+
+    const xx = (parseInt(x, 16) * 0.7347 / 4095).toFixed(4);
+    const yy = (parseInt(y, 16) * 0.8413 / 4095).toFixed(4);
+
+    return new ColorXY(xx, yy);
 };
 
 const tzLocal = {
@@ -192,8 +227,11 @@ const tzLocal = {
         return {
             key: ['colors'],
             convertSet: async (entity, key, value, meta) => {
-                if (value.length !== 5) {
-                    throw new Error(`Expected 5 colors, got ${value.length}`);
+                if (value.length > 9) {
+                    throw new Error(`Expected up to 9 colors, got ${value.length}`);
+                }
+                if (value.length < 1) {
+                    throw new Error(`Expected at least 1 color, got 0`);
                 }
 
                 // For devices where it makes more sense to specify the colors in reverse
@@ -202,12 +240,35 @@ const tzLocal = {
                     value.reverse();
                 }
 
-                const scene = '500104001350000000' + value.map(encodeHexToPoint).join('') + '2800';
+                // The number of colors and segments can technically differ. Here they are always the same, but we could
+                // support it by extending the API.
+                // If number of colors is less than the number of segments, the colors will repeat.
+                // It seems like the maximum number of colors is 9, and the maximum number of segments is 31.
+                const colors = (value.length << 4).toString(16);
+                const segments = (value.length << 3).toString(16);
+
+                // Encode the colors
+                const colorsPayload = value.map(encodePoint).join('');
+
+                // Offset of the first color, left shifted 3 bits. 0 means the first segment uses the first color.
+                const offset = '00';
+
+                // Payload length
+                const length = (1 + 3 * (value.length + 1)).toString(16);
+
+                const scene = `50010400${length}${colors}000000${colorsPayload}${segments}${offset}`;
                 const payload = {data: Buffer.from(scene, 'hex')};
                 await entity.command('manuSpecificPhilips2', 'multiColor', payload);
             },
         };
     },
+};
+
+
+const bind = async (endpoint, target, clusters) => {
+    for (const cluster of clusters) {
+        await endpoint.bind(cluster, target);
+    }
 };
 
 module.exports = [
@@ -1677,12 +1738,27 @@ module.exports = [
             tzLocal.philips_hue_multicolor({reverse: true}),
             ...hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}).toZigbee,
         ],
+        fromZigbee: [
+            fzLocal.philips_hue_multicolor({reverse: true}),
+            ...hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}).fromZigbee,
+        ],
         exposes: [
+            exposes.list('colors', ea.ALL, exposes.composite('color_xy', 'color')
+                .withFeature(exposes.numeric('x', ea.ALL))
+                .withFeature(exposes.numeric('y', ea.ALL))
+                .withDescription('Color of this point in the CIE 1931 color space (x/y)'))
+                .withDescription('List of color points in the CIE 1931 color space (x/y)'),
             exposes.enum('gradient_scene', ea.SET, Object.keys(gradientScenes)),
-            exposes.list('colors', ea.SET, exposes.text('color', ea.SET)),
             ...hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}).exposes,
         ],
-        extend: hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await hueExtend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]})
+                .configure(device, coordinatorEndpoint, logger);
+
+            device.endpoints.forEach(async (ep) => {
+                await bind(ep, coordinatorEndpoint, ['manuSpecificPhilips2']);
+            });
+        },
     },
     {
         zigbeeModel: ['915005987601'],
