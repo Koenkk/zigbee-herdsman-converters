Koenkk/zigbee-herdsman-converters

commit 95da3769713b473d455d7b004366b1648b0e2667

Fix devices incorrectly detected as TuYa TS011F_plug_3. https://github.com/Koenkk/zigbee2mqtt/issues/15282

devices/tuya.js
@@ -2346,7 +2346,7 @@ module.exports = [
     },
     {
         fingerprint: [160, 69, 68, 65, 64].map((applicationVersion) => {
-            return {modelID: 'TS011F', applicationVersion};
+            return {modelID: 'TS011F', applicationVersion, priority: -1};
         }),
         model: 'TS011F_plug_3',
         description: 'Smart plug (with power monitoring by polling)',

index.js
@@ -142,14 +142,21 @@ function findByDevice(device) {
         return candidates[0];
     } else {
         // First try to match based on fingerprint, return the first matching one.
+        const fingerprintMatch = {priority: null, definition: null};
         for (const candidate of candidates) {
             if (candidate.hasOwnProperty('fingerprint')) {
                 for (const fingerprint of candidate.fingerprint) {
-                    if (fingerprintMatch(fingerprint, device)) {
-                        return candidate;
+                    const priority = fingerprint.hasOwnProperty('priority') ? fingerprint.priority : 0;
+                    if (isFingerprintMatch(fingerprint, device) && (!fingerprintMatch.definition || priority > fingerprintMatch.priority)) {
+                        fingerprintMatch.definition = candidate;
+                        fingerprintMatch.priority = priority;
                     }
                 }
             }
+        }
+
+        if (fingerprintMatch.definition) {
+            return fingerprintMatch.definition;
         }
 
         // Match based on fingerprint failed, return first matching definition based on zigbeeModel
@@ -163,7 +170,7 @@ function findByDevice(device) {
     return null;
 }
 
-function fingerprintMatch(fingerprint, device) {
+function isFingerprintMatch(fingerprint, device) {
     let match =
         (!fingerprint.applicationVersion || device.applicationVersion === fingerprint.applicationVersion) &&
         (!fingerprint.manufacturerID || device.manufacturerID === fingerprint.manufacturerID) &&

test/index.test.js
@@ -96,6 +96,30 @@ describe('index.js', () => {
         expect(definition.model).toBe("RTCGQ01LM");
     });
 
+    it('Find by fingerprint with priority', () => {
+        const HG06338 = {
+            type: 'Router',
+            manufacturerName: '_TZ3000_vzopcetz',
+            modelID: 'TS011F',
+            applicationVersion: 69,
+        };
+        const TS011F_plug_3 = {
+            type: 'Router',
+            manufacturerName: '_TZ3000_vzopcetz_random',
+            modelID: 'TS011F',
+            applicationVersion: 69,
+        };
+        const TS011F_plug_1 = {
+            type: 'Router',
+            manufacturerName: '_TZ3000_vzopcetz_random',
+            modelID: 'TS011F',
+            applicationVersion: 1,
+        };
+        expect(index.findByDevice(HG06338).model).toBe('HG06338');
+        expect(index.findByDevice(TS011F_plug_3).model).toBe('TS011F_plug_3');
+        expect(index.findByDevice(TS011F_plug_1).model).toBe('TS011F_plug_1');
+    });
+
     it('Find by device should prefer fingerprint match over zigbeeModel', () => {
         const mullerEndpoints = [
             {ID: 1, profileID: 49246, deviceID: 544, inputClusters: [0, 3, 4, 5, 6, 8, 768, 2821, 4096], outputClusters: [25]},

commit 2ada42213f3d32a24dcf670053d29426894e4c0c

Fix for _TZ3000_qeuvnohg incorrectly detected as TuYa TS011F_plug_3
Hello,
I think we should be making sure : _TZ3000_qeuvnohg
is detected as https://www.zigbee2mqtt.io/devices/TS011F_din_smart_relay.html and not TS011F_plug_3 as it supports energy monitoring without polling

This was working well until 2 days ago and I think the refactoring in commit 95da3769713b473d455d7b004366b1648b0e2667 caused it

devices/tuya.js
@@ -3006,7 +3006,7 @@ module.exports = [
     },
     {
         fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_8bxrzyxz'},
-            {modelID: 'TS011F', manufacturerName: '_TZ3000_ky0fq4ho'}],
+            {modelID: 'TS011F', manufacturerName: '_TZ3000_ky0fq4ho'}, {modelID: 'TS011F', manufacturerName: '_TZ3000_qeuvnohg'}],
         model: 'TS011F_din_smart_relay',
         description: 'Din smart relay (with power monitoring)',
         vendor: 'TuYa',