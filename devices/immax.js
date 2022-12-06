eaf7e65104ce762f3d6233f4bb7db286f6751fc3
Jiří Balcar <JiriBalcar@users.noreply.github.com>
2022-12-06 17:47:00 +0100

    Add 07505L (#5045)
    * #3279 - Support for Immax NEO 07505L
    
    * fix formatting
    
    * formatting fix
    
    * final fix of formatting
    
    * refactor to new TuYa format
    
    * fix lint issues
    
    * rename action name property
    
    * Update tuya.js
    
    * Update tuya.js
    
    * Update immax.js
    
    Co-authored-by: Jiri Balcar <jiri.balcar@gmail.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- devices/immax.js
+++ devices/immax.js
@@ -182,4 +182,22 @@ module.exports = [
             // eslint-disable-next-line
             exposes.enum('keep_time', ea.STATE_SET, ['0', '30', '60', '120', '240']).withDescription('PIR keep time in seconds')],
     },
+    {
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_n9clpsht']),
+        model: '07505L',
+        vendor: 'Immax',
+        description: 'Neo smart keypad',
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [],
+        exposes: [e.action(['disarm', 'arm_home', 'arm_away', 'sos']), e.tamper()],
+        meta: {
+            tuyaDatapoints: [
+                [24, 'tamper', tuya.valueConverter.trueFalse],
+                [26, 'action', tuya.valueConverter.static('disarm')],
+                [27, 'action', tuya.valueConverter.static('arm_away')],
+                [28, 'action', tuya.valueConverter.static('arm_home')],
+                [29, 'action', tuya.valueConverter.static('sos')],
+            ],
+        },
+    },
 ];
