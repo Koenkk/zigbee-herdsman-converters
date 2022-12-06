d937c897fd87c7ce2a057024f9c0d924cb997ebc
Koen Kanters <koenkanters94@gmail.com>
2022-12-06 16:57:46 +0100

    Update tuya.js

--- devices/tuya.js
+++ devices/tuya.js
@@ -2201,6 +2201,49 @@ module.exports = [
         ],
     },
     {
+        fingerprint: tuya.fingerprint('TS0601', [
+            '_TZE200_bvu2wnxz', /* model: 'ME167', vendor: 'Avatto' */
+        ]),
+        model: 'TS0601_thermostat_3',
+        vendor: 'TuYa',
+        description: 'Thermostatic radiator valve',
+        fromZigbee: [tuya.fzDataPoints],
+        toZigbee: [tuya.tzDataPoints],
+        whiteLabel: [{vendor: 'Avatto', model: 'ME167'}],
+        onEvent: tuya.onEventSetTime,
+        configure: tuya.configureMagicPacket,
+        exposes: [
+            e.child_lock(), e.battery_low(),
+            exposes.climate()
+                .withSetpoint('current_heating_setpoint', 5, 35, 1, ea.STATE_SET)
+                .withLocalTemperature(ea.STATE)
+                .withSystemMode(['auto', 'heat', 'off'], ea.STATE_SET)
+                .withRunningState(['idle', 'heat'], ea.STATE)
+                .withLocalTemperatureCalibration(-3, 3, 1, ea.STATE_SET),
+            exposes.binary('scale_protection', ea.STATE_SET, 'ON', 'OFF').withDescription('If the heat sink is not fully opened within ' +
+                'two weeks or is not used for a long time, the valve will be blocked due to silting up and the heat sink will not be ' +
+                'able to be used. To ensure normal use of the heat sink, the controller will automatically open the valve fully every ' +
+                'two weeks. It will run for 30 seconds per time with the screen displaying "Ad", then return to its normal working state ' +
+                'again.'),
+            exposes.binary('frost_protection', ea.STATE_SET, 'ON', 'OFF').withDescription('When the room temperature is lower than ' +
+                '5 °C, the valve opens; when the temperature rises to 8 °C, the valve closes.'),
+            exposes.numeric('error', ea.STATE).withDescription('If NTC is damaged, "Er" will be on the TRV display.'),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [2, 'system_mode', tuya.valueConverterBasic.lookup({'auto': tuya.enum(0), 'heat': tuya.enum(1), 'off': tuya.enum(2)})],
+                [3, 'running_state', tuya.valueConverterBasic.lookup({'heat': tuya.enum(0), 'idle': tuya.enum(1)})],
+                [4, 'current_heating_setpoint', tuya.valueConverter.divideBy10],
+                [5, 'local_temperature', tuya.valueConverter.divideBy10],
+                [7, 'child_lock', tuya.valueConverter.lockUnlock],
+                [35, null, tuya.valueConverter.errorBatteryLow],
+                [36, 'frost_protection', tuya.valueConverter.onOff],
+                [39, 'scale_protection', tuya.valueConverter.onOff],
+                [47, 'local_temperature_calibration', tuya.valueConverter.localTempCalibration2],
+            ],
+        },
+    },
+    {
         fingerprint: [
             {modelID: 'v90ladg\u0000', manufacturerName: '_TYST11_wv90ladg'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_wv90ladg'},
@@ -3622,47 +3665,6 @@ module.exports = [
                 [7, null, tuya.valueConverter.phaseVariant2WithPhase('b')],
                 [8, null, tuya.valueConverter.phaseVariant2WithPhase('c')],
                 [134, 'device_status', tuya.valueConverter.raw],
-            ],
-        },
-    },
-    {
-        fingerprint: tuya.fingerprint('TS0601', [
-            '_TZE200_bvu2wnxz', /* model: 'ME167', vendor: 'Avatto' */
-        ]),
-        model: 'TS0601_thermostat_3',
-        vendor: 'TuYa',
-        description: 'Thermostatic radiator valve',
-        fromZigbee: [tuya.fzDataPoints],
-        toZigbee: [tuya.tzDataPoints],
-        whiteLabel: [{vendor: 'Avatto', model: 'ME167'}],
-        onEvent: tuya.onEventSetTime,
-        configure: tuya.configureMagicPacket,
-        exposes: [
-            exposes.climate().withSetpoint('current_heating_setpoint', 5, 35, 1, ea.STATE_SET).withLocalTemperature(ea.STATE)
-                .withSystemMode(['auto', 'heat', 'off'], ea.STATE_SET).withRunningState(['idle', 'heat'], ea.STATE)
-                .withLocalTemperatureCalibration(-3, 3, 1, ea.STATE_SET),
-            e.child_lock(),
-            e.battery_low(),
-            exposes.binary('scale_protection', ea.STATE_SET, 'ON', 'OFF').withDescription('If the heat sink is not fully opened within ' +
-                'two weeks or is not used for a long time, the valve will be blocked due to silting up and the heat sink will not be ' +
-                'able to be used. To ensure normal use of the heat sink, the controller will automatically open the valve fully every ' +
-                'two weeks. It will run for 30 seconds per time with the screen displaying "Ad", then return to its normal working state ' +
-                'again.'),
-            exposes.binary('frost_protection', ea.STATE_SET, 'ON', 'OFF').withDescription('When the room temperature is lower than ' +
-                '5 °C, the valve opens; when the temperature rises to 8 °C, the valve closes.'),
-            exposes.numeric('error', ea.STATE).withDescription('If NTC is damaged, "Er" will be on the TRV display.'),
-        ],
-        meta: {
-            tuyaDatapoints: [
-                [2, 'system_mode', tuya.valueConverterBasic.lookup({'auto': tuya.enum(0), 'heat': tuya.enum(1), 'off': tuya.enum(2)})],
-                [3, 'running_state', tuya.valueConverterBasic.lookup({'heat': tuya.enum(0), 'idle': tuya.enum(1)})],
-                [4, 'current_heating_setpoint', tuya.valueConverter.divideBy10],
-                [5, 'local_temperature', tuya.valueConverter.divideBy10],
-                [7, 'child_lock', tuya.valueConverter.lockUnlock],
-                [35, null, tuya.valueConverter.errorBatteryLow],
-                [36, 'frost_protection', tuya.valueConverter.onOff],
-                [39, 'scale_protection', tuya.valueConverter.onOff],
-                [47, 'local_temperature_calibration', tuya.valueConverter.localTempCalibration2],
             ],
         },
     },
