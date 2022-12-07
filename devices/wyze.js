90ee95627997b67ae7b4aad663652916527d40f9
Koen Kanters <koenkanters94@gmail.com>
2022-12-07 18:07:20 +0100

    Update wyze.js

--- devices/wyze.js
+++ devices/wyze.js
@@ -1,24 +1,24 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const exposes = require('../lib/exposes');
+const fz = require('../converters/fromZigbee');
+const tz = require('../converters/toZigbee');
+const reporting = require('../lib/reporting');
 const e = exposes.presets;
 
-const definition = {
-    zigbeeModel: ['Ford'],
-    model: 'Ford',
-    vendor: 'Yunding',
-    description: 'Wyze Lock',
-    fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery],
-    toZigbee: [tz.lock],
-    meta: {configureKey: 1, options: {disableDefaultResponse: true}},
-    configure: async (device, coordinatorEndpoint, logger) => {
-        const endpoint = device.endpoints[0];
-        await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
-        await reporting.lockState(endpoint);
-        await reporting.batteryPercentageRemaining(endpoint);
+module.exports = [
+    {
+        zigbeeModel: ['Ford'],
+        model: 'WLCKG1',
+        vendor: 'Wyze',
+        description: 'Lock',
+        fromZigbee: [fz.lock, fz.lock_operation_event, fz.battery],
+        toZigbee: [tz.lock],
+        meta: {configureKey: 1, options: {disableDefaultResponse: true}},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.endpoints[0];
+            await reporting.bind(endpoint, coordinatorEndpoint, ['closuresDoorLock', 'genPowerCfg']);
+            await reporting.lockState(endpoint);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+        exposes: [e.lock(), e.battery()],
     },
-    exposes: [e.lock(), e.battery()],
-};
-
-module.exports = definition;
+];
