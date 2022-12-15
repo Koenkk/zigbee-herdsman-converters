bbd54c63ba8da239a2228be566d2b80725e12b8c
Koen Kanters <koenkanters94@gmail.com>
2022-12-15 20:41:34 +0100

    Add 4512735. https://github.com/Koenkk/zigbee2mqtt/issues/15572

--- devices/namron.js
+++ devices/namron.js
@@ -591,6 +591,28 @@ module.exports = [
         },
     },
     {
+        zigbeeModel: ['4512735'],
+        model: '4512735',
+        vendor: 'Namron',
+        description: 'Multiprise with 4 AC outlets and 2 USB super charging ports (16A)',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        exposes: [e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2'), e.switch().withEndpoint('l3'),
+            e.switch().withEndpoint('l4'), e.switch().withEndpoint('l5')],
+        endpoint: (device) => {
+            return {'l1': 1, 'l2': 2, 'l3': 3, 'l4': 4, 'l5': 5};
+        },
+        meta: {multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            for (const ID of [1, 2, 3, 4, 5]) {
+                const endpoint = device.getEndpoint(ID);
+                await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            }
+            device.powerSource = 'Mains (single phase)';
+            device.save();
+        },
+    },
+    {
         zigbeeModel: ['5401392', '5401396', '5401393', '5401397', '5401394', '5401398', '5401395', '5401399', '5401395'],
         model: '540139X',
         vendor: 'Namron',
