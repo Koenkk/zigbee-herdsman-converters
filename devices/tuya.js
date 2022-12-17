368e321c32a6d896b0bda00c5e0fc21af2c0177e
Burkhard Kneiseler <burkhard@kneiseler.de>
2022-12-17 16:18:55 +0100

    add _TZE200_yjjdcqsq

--- devices/tuya.js
+++ devices/tuya.js
@@ -1632,6 +1632,7 @@ module.exports = [
             {modelID: 'TS0201', manufacturerName: '_TZ3000_6uzkisv2'},
             {modelID: 'TS0201', manufacturerName: '_TZ3000_xr3htd96'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_9yapgbuv'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_yjjdcqsq'},
         ],
         model: 'WSD500A',
         vendor: 'TuYa',
