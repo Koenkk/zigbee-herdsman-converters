a343d23c96e75be3d3a9d76c1786a7a55bed4041
SjonHortensius <SjonHortensius@users.noreply.github.com>
2022-12-19 16:08:16 +0100

    Add wisdom.js for dmz250 dimmer

---
+++ devices/wisdom.js
@@ -0,0 +1,21 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const ota = require('zigbee-herdsman-converters/lib/ota');
+const tuya = require('zigbee-herdsman-converters/lib/tuya');
+const utils = require('zigbee-herdsman-converters/lib/utils');
+const globalStore = require('zigbee-herdsman-converters/lib/store');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definition = {
+    zigbeeModel: ['HK-DIM-SW'],
+    model: 'dmz250',
+    vendor: 'Wisdom',
+    description: 'Zigbee Wifi Led Dimmer 5-250 Watt',
+    extend: extend.light_onoff_brightness(),
+};
+
+module.exports = definition;
