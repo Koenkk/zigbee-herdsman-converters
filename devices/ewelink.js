2d82d80e12a9f0c0bd3e6e6ea7b1db5f3903a9f3
Koen Kanters <koenkanters94@gmail.com>
2022-12-19 21:01:36 +0100

    Revert "Add SWITCH-ZR03-1 to SA-003-Zigbee. https://github.com/Koenkk/zigbee2mqtt/discussions/15647"
    This reverts commit fdfc1cc7d9b573ed2cafde15701ada6a1cd079e7.

--- devices/ewelink.js
+++ devices/ewelink.js
@@ -6,7 +6,7 @@ const e = exposes.presets;
 
 module.exports = [
     {
-        zigbeeModel: ['SA-003-Zigbee', 'SWITCH-ZR03-1'],
+        zigbeeModel: ['SA-003-Zigbee'],
         model: 'SA-003-Zigbee',
         vendor: 'eWeLink',
         description: 'Zigbee smart plug',
