8b4d9d2c2866da541e288c706e5e9b955e69f153
wifiuk <wifiuk@users.noreply.github.com>
2022-12-19 19:10:21 +0000

    Update woolley.js

--- devices/woolley.js
+++ devices/woolley.js
@@ -16,6 +16,17 @@ module.exports = [
     },
     {
         zigbeeModel: ['SA-030-1'],
+        model: 'SA-030-1',
+        vendor: 'Woolley',
+        description: 'Smart plug',
+        extend: extend.switch(),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint);
+        },
+    {
+        zigbeeModel: ['SA-030-1'],
         model: 'SA-030',
         vendor: 'Woolley',
         description: 'Smart plug',
