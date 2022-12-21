bc7d7883b2f365652a97db78bf4855cf6b34d99e
Koen Kanters <koenkanters94@gmail.com>
2022-12-21 19:30:07 +0100

    Update wisdom.js

--- devices/wisdom.js
+++ devices/wisdom.js
@@ -1,21 +1,11 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
-const ota = require('zigbee-herdsman-converters/lib/ota');
-const tuya = require('zigbee-herdsman-converters/lib/tuya');
-const utils = require('zigbee-herdsman-converters/lib/utils');
-const globalStore = require('zigbee-herdsman-converters/lib/store');
-const e = exposes.presets;
-const ea = exposes.access;
+const extend = require('../lib/extend');
 
-const definition = {
-    zigbeeModel: ['HK-DIM-SW'],
-    model: 'DMS250',
-    vendor: 'Wisdom',
-    description: 'Zigbee led dimmer 5-250 Watt',
-    extend: extend.light_onoff_brightness(),
-};
-
-module.exports = definition;
+module.exports = [
+    {
+        zigbeeModel: ['HK-DIM-SW'],
+        model: 'DMS250',
+        vendor: 'Wisdom',
+        description: 'Zigbee led dimmer 5-250 Watt',
+        extend: extend.light_onoff_brightness(),
+    },
+];
