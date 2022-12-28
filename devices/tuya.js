fd1ab55fa961fa442b2ef1e542e419e24f419f80
Koen Kanters <koenkanters94@gmail.com>
2022-12-28 08:33:30 +0100

    Add _TZ3000_6s5dc9lx to TS011F_2_gang_wall. https://github.com/Koenkk/zigbee2mqtt/issues/15720

--- devices/tuya.js
+++ devices/tuya.js
@@ -927,7 +927,7 @@ module.exports = [
         fingerprint: [{modelID: 'TS011F', manufacturerName: '_TZ3000_mvn6jl7x'},
             {modelID: 'TS011F', manufacturerName: '_TZ3000_raviyuvk'}, {modelID: 'TS011F', manufacturerName: '_TYZB01_hlla45kx'},
             {modelID: 'TS011F', manufacturerName: '_TZ3000_92qd4sqa'}, {modelID: 'TS011F', manufacturerName: '_TZ3000_zwaadvus'},
-            {modelID: 'TS011F', manufacturerName: '_TZ3000_k6fvknrr'}],
+            {modelID: 'TS011F', manufacturerName: '_TZ3000_k6fvknrr'}, {modelID: 'TS011F', manufacturerName: '_TZ3000_6s5dc9lx'}],
         model: 'TS011F_2_gang_wall',
         vendor: 'TuYa',
         description: '2 gang wall outlet',
