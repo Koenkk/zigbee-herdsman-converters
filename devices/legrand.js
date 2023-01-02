ab3047d7c553b6437e0fdf90dd33bea66ed9d655
Antor <contact@antor.fr>
2023-01-02 03:39:21 -0500

    Expose `led_in_dark` setting for Legrand 067776 (#5243)
    * Expose led_in_dark setting for Legrand 067776
    
    Expose led_in_dark setting (same as Legrand 067775/741811)
    
    * Add support for HEIMAN custom HS1CG-E
    
    * Revert "Add support for HEIMAN custom HS1CG-E"
    
    This reverts commit 7357a7b7aba018d006de0b08a7ea6e23d008ae4b.

--- devices/legrand.js
+++ devices/legrand.js
@@ -130,7 +130,9 @@ module.exports = [
             await reporting.bind(endpoint, coordinatorEndpoint, ['genBinaryInput', 'closuresWindowCovering', 'genIdentify']);
             await reporting.currentPositionLiftPercentage(endpoint);
         },
-        exposes: [e.cover_position()],
+        exposes: [e.cover_position(),
+            exposes.binary('led_in_dark', ea.ALL, 'ON', 'OFF').withDescription(`Enables the LED when the power socket is turned off,
+            allowing to see it in the dark`)],
     },
     {
         // swbuildid 001a requires coverInverted: https://github.com/Koenkk/zigbee2mqtt/issues/15101#issuecomment-1356787490
