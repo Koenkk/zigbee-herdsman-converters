75c618d886ac36f6ab8147ac59f2f8b7e851de82
Simon Goudie <simon@simongoudie.com>
2023-01-04 00:37:02 +1100

    Added D-Link DCH-B112 Door/Window Sensor
    Not my work, but adapted from unempstudent and SimonNQ in this post: https://www.reddit.com/r/homeassistant/comments/x63d47/zigbee2mqtt_adding_support_for_dlink_dchb112/
    
    Item available from here: https://www.amazon.com.au/D-Link-mydlink-Window-Sensor-DCH-B112/dp/B082VHN4N4

---
+++ devices/dlink.js
@@ -0,0 +1,41 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const fzLocal = {
+    DCH_B112: {
+        cluster: 'ssIasZone',
+        type: 'commandStatusChangeNotification',
+        convert: (model, msg, publish, options, meta) => {
+            const zoneStatus = msg.data.zonestatus;
+            return {
+                contact: !((zoneStatus & 1) > 0),
+                vibration: (zoneStatus & 1<<1) > 0,
+                tamper: (zoneStatus & 1<<2) > 0,
+                battery_low: (zoneStatus & 1<<3) > 0,
+            };
+        },
+    },
+};
+
+
+module.exports = [
+    {
+        zigbeeModel: ['DCH-B112'],
+        model: 'DCH-B112',
+        vendor: 'D-Link',
+        description: 'D-Link wireless smart door window sensor with vibration',
+        fromZigbee: [fzLocal.DCH_B112, fz.battery],
+        toZigbee: [],
+        exposes: [e.battery_low(), e.contact(), e.vibration(), e.tamper(), e.battery()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+    },
+];
