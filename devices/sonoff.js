ff9daeb1c7b534e2478d954202d65b81b1a3f02f
Koen Kanters <koenkanters94@gmail.com>
2023-01-03 10:46:41 +0100

    Fix ce69c2a8902a00cd1d5840df70b2e6cac4e93d20

--- devices/sonoff.js
+++ devices/sonoff.js
@@ -1,6 +1,5 @@
 const exposes = require('../lib/exposes');
-const fz = {...require('../converters/fromZigbee'), legacy: require('../lib/legacy').fromZigbee};
-const tz = require('../converters/toZigbee');
+const fz = require('../converters/fromZigbee');
 const constants = require('../lib/constants');
 const reporting = require('../lib/reporting');
 const extend = require('../lib/extend');
