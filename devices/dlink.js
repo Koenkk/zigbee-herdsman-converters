740c0a1ae177a44e6cc48fffe87129b8bd4874e5
Simon Goudie <simon@simongoudie.com>
2023-01-04 01:05:10 +1100

    Update dlink.js
    Updated require lines

--- devices/dlink.js
+++ devices/dlink.js
@@ -1,8 +1,8 @@
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
+const fz = require('.../converters/fromZigbee');
+const tz = require('.../converters/toZigbee');
+const exposes = require('.../lib/exposes');
+const reporting = require('.../lib/reporting');
+const extend = require('.../lib/extend');
 const e = exposes.presets;
 const ea = exposes.access;
 
