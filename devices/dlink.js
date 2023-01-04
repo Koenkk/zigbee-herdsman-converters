bac2d1170710e4a9d3639061e93318834664ddee
Simon Goudie <simon@simongoudie.com>
2023-01-05 04:52:45 +1100

    Add DCH-B112 (#5259)
    * Added D-Link DCH-B112 Door/Window Sensor
    
    Not my work, but adapted from unempstudent and SimonNQ in this post: https://www.reddit.com/r/homeassistant/comments/x63d47/zigbee2mqtt_adding_support_for_dlink_dchb112/
    
    Item available from here: https://www.amazon.com.au/D-Link-mydlink-Window-Sensor-DCH-B112/dp/B082VHN4N4
    
    * Update dlink.js
    
    Updated require lines
    
    * Update dlink.js
    
    Update require lines
    
    * Update dlink.js
    
    remove unused variables
    
    * Update CRLF to LF
    
    * Update dlink.js
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

---
+++ devices/dlink.js
@@ -0,0 +1,37 @@
+const fz = require('../converters/fromZigbee');
+const exposes = require('../lib/exposes');
+const reporting = require('../lib/reporting');
+const e = exposes.presets;
+
+const fzLocal = {
+    DCH_B112: {
+        cluster: 'ssIasZone',
+        type: 'commandStatusChangeNotification',
+        convert: (model, msg, publish, options, meta) => {
+            const zoneStatus = msg.data.zonestatus;
+            return {
+                contact: !((zoneStatus & 1) > 0),
+                vibration: (zoneStatus & 1<<1) > 0,
+                tamper: (zoneStatus & 1<<2) > 0,
+                battery_low: (zoneStatus & 1<<3) > 0,
+            };
+        },
+    },
+};
+
+module.exports = [
+    {
+        zigbeeModel: ['DCH-B112'],
+        model: 'DCH-B112',
+        vendor: 'D-Link',
+        description: 'Wireless smart door window sensor with vibration',
+        fromZigbee: [fzLocal.DCH_B112, fz.battery],
+        toZigbee: [],
+        exposes: [e.battery_low(), e.contact(), e.vibration(), e.tamper(), e.battery()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+    },
+];
