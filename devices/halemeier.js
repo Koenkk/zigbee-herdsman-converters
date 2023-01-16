043c4b5266991970f502bf179e514b544989acc3
Koen Kanters <koenkanters94@gmail.com>
2023-01-16 19:05:16 +0100

    Update halemeier.js

--- devices/halemeier.js
+++ devices/halemeier.js
@@ -12,7 +12,7 @@ module.exports = [
         zigbeeModel: ['HA-ZM12/24-mw2'],
         model: 'HA-ZM12/24-mw2',
         vendor: 'Halemeier',
-        description: 'MultiWhite 1-channel Smart Receiver 12V',
+        description: 'MultiWhite 1-channel smart receiver 12V',
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [160, 450]}),
     },
 ];
