f2aab1fad271cb174afa9f6710df10d6a5570c30
REN3 <REN3@3ds.com>
2023-01-20 13:37:52 +0100

    Support for SEHAZB-DR-SWITCH-2

---
+++ devices/solaredge.js
@@ -0,0 +1,18 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+
+const e = exposes.presets;
+
+
+const definition = {
+    zigbeeModel: ['SE-SW'], //
+    model: 'SEHAZB-DR-SWITCH-2', //
+    vendor: 'SolarEdge', //
+    description: 'SolarEdge SmartEnergy Switch', //
+    fromZigbee: [fz.on_off], //
+    toZigbee: [tz.on_off], //
+    exposes: [e.switch()], //
+};
+
+module.exports = definition;
