d2d90339bf0321f4d7ef0040606d1774e4fc138b
Koen Kanters <koenkanters94@gmail.com>
2023-01-21 10:02:18 +0100

    Update solaredge.js

--- devices/solaredge.js
+++ devices/solaredge.js
@@ -1,17 +1,11 @@
-const fz = require('../converters/fromZigbee');
-const tz = require('../converters/toZigbee');
-const exposes = require('../lib/exposes');
+const extend = require('../lib/extend');
 
-const e = exposes.presets;
-
-
-module.exports = [{
-    zigbeeModel: ['SE-SW'], //
-    model: 'SEHAZB-DR-SWITCH-2', //
-    vendor: 'SolarEdge', //
-    description: 'SolarEdge SmartEnergy Switch', //
-    fromZigbee: [fz.on_off], //
-    toZigbee: [tz.on_off], //
-    exposes: [e.switch()], //
-}];
-
+module.exports = [
+    {
+        zigbeeModel: ['SE-SW'],
+        model: 'SEHAZB-DR-SWITCH-2',
+        vendor: 'SolarEdge',
+        description: 'Smart energy switch',
+        extend: extend.switch(),
+    },
+];
