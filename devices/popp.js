d4e144975a58059cafa434a59d3e94eb8f24583b
olzibambolzi <36680813+olzibambolzi@users.noreply.github.com>
2023-01-24 13:24:03 +0100

    Update popp.js

--- devices/popp.js
+++ devices/popp.js
@@ -2,13 +2,7 @@ const {extendDevice} = require('../lib/utils');
 
 module.exports = [
     extendDevice(require('./danfoss'), '014G2461', {
-        zigbeeModel: ['eT093WRO'],
-        model: '701721',
-        vendor: 'Popp',
-        description: 'Smart thermostat based on Danfoss Ally (014G2461)',
-    }),
-    extendDevice(require('./danfoss'), '014G2461', {
-        zigbeeModel: ['eT093WRG'],
+        zigbeeModel: ['eT093WRO', 'eT093WRG'],
         model: '701721',
         vendor: 'Popp',
         description: 'Smart thermostat based on Danfoss Ally (014G2461)',
