4589be47dfa45d721b148055f7970ad2787e8d5e
Chartreusito <antigrv@gmail.com>
2023-02-01 19:40:23 +0100

    Add support for EB-E27-ST64-CCT-FV light bulb

--- devices/essentialb.js
+++ devices/essentialb.js
@@ -8,4 +8,11 @@ module.exports = [
         description: 'Smart LED bulb',
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 370]}),
     },
+    {
+        zigbeeModel: ['EB-E27-ST64-CCT-FV'],
+        model: 'EB-E27-ST64-CCT-FV',
+        vendor: 'EssentielB',
+        description: 'Filament Vintage light bulb',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 454]}),
+    },
 ];
