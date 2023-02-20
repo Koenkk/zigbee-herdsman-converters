1f7963239df768173422d61ba71643ea168088dc
Ovidiu Nitan <nitanovidiu@gmail.com>
2023-02-20 14:36:25 +0200

    Add support for _TZE200_zl1kmjqx temp and humidity sensor

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -1980,7 +1980,9 @@ const converters = {
                 return {temperature: calibrateAndPrecisionRoundOptions(value / 10, options, 'temperature')};
             case tuya.dataPoints.tthHumidity:
                 return {humidity: calibrateAndPrecisionRoundOptions(
-                    (value / (meta.device.manufacturerName === '_TZE200_bjawzodf' ? 10 : 1)),
+                    (value / (
+                        meta.device.manufacturerName === '_TZE200_bjawzodf' ||
+                        meta.device.manufacturerName === '_TZE200_zl1kmjqx' ? 10 : 1)),
                     options, 'humidity')};
             case tuya.dataPoints.tthBatteryLevel:
                 return {
