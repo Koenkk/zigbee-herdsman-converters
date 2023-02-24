66b4347fae1d47854c7aaaf585e5be6ed08fcd4c
Ilya Sakharov <44649911+isakharov@users.noreply.github.com>
2023-02-24 21:49:39 +0300

    Update tuya.js
    no more tabs and trailing spaces

--- devices/tuya.js
+++ devices/tuya.js
@@ -4124,18 +4124,18 @@ module.exports = [
         toZigbee: [tuya.tz.datapoints],
         configure: tuya.configureMagicPacket,
         exposes: [
-            tuya.exposes.switch(), 
+            tuya.exposes.switch(),
             tuya.exposes.countdown().withValueMin(0).withValueMax(43200).withUnit('seconds').withDescription('Max on time in seconds'),
             exposes.numeric('fan_speed', ea.STATE_SET).withValueMin(1).withValueMax(5).withValueStep(1).withDescription('Fan speed'),
             exposes.enum('power_on_behavior', ea.STATE_SET, ['off', 'on']).withDescription('Relay state'),
         ],
         meta: {
-            tuyaDatapoints: [                
+            tuyaDatapoints: [
                 [1, 'state', tuya.valueConverter.onOff],
                 [2, 'countdown', tuya.valueConverter.countdown],
                 [3, 'fan_speed', tuya.valueConverterBasic.lookup(
                     {'1': tuya.enum(0), '2': tuya.enum(1), '3': tuya.enum(2), '4': tuya.enum(3), '5': tuya.enum(4)}
-                )],			
+                )],
                 [11, 'power_on_behavior', tuya.valueConverterBasic.lookup({'off': tuya.enum(0), 'on': tuya.enum(1)})],
             ],
         },
