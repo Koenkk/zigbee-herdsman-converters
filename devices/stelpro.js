1466727a283785e07169e783b99867a7b23ed8b7
Thibault Cohen <47721+titilambert@users.noreply.github.com>
2023-03-02 20:08:58 -0500

    Add missing piheatingdemand to stelpro st218

--- devices/stelpro.js
+++ devices/stelpro.js
@@ -64,7 +64,7 @@ module.exports = [
             tz.stelpro_thermostat_outdoor_temperature],
         exposes: [e.local_temperature(), e.keypad_lockout(),
             exposes.climate().withSetpoint('occupied_heating_setpoint', 5, 30, 0.5).withLocalTemperature()
-                .withSystemMode(['off', 'auto', 'heat']).withRunningState(['idle', 'heat'])],
+                .withSystemMode(['off', 'auto', 'heat']).withRunningState(['idle', 'heat']).withPiHeatingDemand()],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(25);
             const binds = ['genBasic', 'genIdentify', 'genGroups', 'hvacThermostat', 'hvacUserInterfaceCfg', 'msTemperatureMeasurement'];
