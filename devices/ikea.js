920690c6d79dab7f8d24737427c240ab5ffb4968
Koen Kanters <koenkanters94@gmail.com>
2023-03-04 16:36:08 +0100

    Update ikea.js

--- devices/ikea.js
+++ devices/ikea.js
@@ -1043,7 +1043,7 @@ module.exports = [
         description: 'Vindstyrka air quality and humidity sensor',
         fromZigbee: [fz.temperature, fz.humidity, fz.pm25, ikea.fz.ikea_voc_index],
         toZigbee: [],
-        exposes: [e.temperature(), e.humidity(), e.pm25(), e.voc_index()],
+        exposes: [e.temperature(), e.humidity(), e.pm25(), e.voc_index().withDescription('Sensirion VOC index')],
         configure: async (device, coordinatorEndpoint, logger) => {
             const ep = device.getEndpoint(1);
             await reporting.bind(ep, coordinatorEndpoint,
