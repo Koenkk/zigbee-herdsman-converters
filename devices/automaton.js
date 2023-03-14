dc2b8268dc1f637095b2023301638d1f4c0f82d8
Krzysztof Dziadowiec <krzysztof.dziadowiec@gmail.com>
2023-03-15 00:29:59 +0100

    fix undefined fzLocal and tzLocal

--- devices/automaton.js
+++ devices/automaton.js
@@ -3,9 +3,57 @@ const tz = require('../converters/toZigbee');
 const exposes = require('../lib/exposes');
 const reporting = require('../lib/reporting');
 const tuya = require('../lib/tuya');
+const utils = require('../lib/utils');
 const e = exposes.presets;
-const fzLocal = tuya.fzLocal;
-const tzLocal = tuya.tzLocal;
+
+const fzLocal = {
+  power_on_behavior: {
+      cluster: 'manuSpecificTuya_3',
+      type: ['attributeReport', 'readResponse'],
+      convert: (model, msg, publish, options, meta) => {
+          const attribute = 'powerOnBehavior';
+          const lookup = { 0: 'off', 1: 'on', 2: 'previous' };
+
+          if (msg.data.hasOwnProperty(attribute)) {
+              const property = utils.postfixWithEndpointName('power_on_behavior', msg, model, meta);
+              return { [property]: lookup[msg.data[attribute]] };
+          }
+      },
+  },
+  child_lock: {
+      cluster: 'genOnOff',
+      type: ['attributeReport', 'readResponse'],
+      convert: (model, msg, publish, options, meta) => {
+          if (msg.data.hasOwnProperty('32768')) {
+              const value = msg.data['32768'];
+              return { child_lock: value ? 'LOCK' : 'UNLOCK' };
+          }
+      },
+  }
+}
+
+const tzLocal = {
+  power_on_behavior: {
+      key: ['power_on_behavior'],
+      convertSet: async (entity, key, value, meta) => {
+          value = value.toLowerCase();
+          const lookup = { 'off': 0, 'on': 1, 'previous': 2 };
+          utils.validateValue(value, Object.keys(lookup));
+          const pState = lookup[value];
+          await entity.write('manuSpecificTuya_3', { 'powerOnBehavior': pState }, { disableDefaultResponse: true });
+          return { state: { power_on_behavior: value } };
+      },
+      convertGet: async (entity, key, meta) => {
+          await entity.read('manuSpecificTuya_3', ['powerOnBehavior']);
+      },
+  },
+  child_lock: {
+      key: ['child_lock'],
+      convertSet: async (entity, key, value, meta) => {
+          await entity.write('genOnOff', { 0x8000: { value: value === 'LOCK', type: 0x10 } });
+      },
+  }
+}
 
 const definition = {
     fingerprint: tuya.fingerprint('TS011F', ['_TZ3000_j0ktmul1']),
