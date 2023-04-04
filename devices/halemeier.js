9eb4aae8242178e6fd599bef595802ab0f00446d
ochstobi <ochs.tobi@gmail.com>
2023-04-04 13:06:22 +0200

    Update halemeier.js
    Add support for HA-ZX1 X-Mitter

--- devices/halemeier.js
+++ devices/halemeier.js
@@ -15,4 +15,13 @@ module.exports = [
         description: 'MultiWhite 1-channel smart receiver 12V',
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [160, 450]}),
     },
+    {
+        zigbeeModel: ['HA-ZX1'],
+        model: 'HA-ZX1',
+        vendor: 'Halemeier',
+        description: 'X-Mitter Smart Remote Control',
+        fromZigbee: [fz.command_on, fz.command_off, fz.command_move, fz.command_stop],
+        toZigbee: [],
+        exposes: [],
+    },
 ];
