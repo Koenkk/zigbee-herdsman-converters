7bc1ffcf01bf88748f16c976fcc753f55c215c16
Koen Kanters <koenkanters94@gmail.com>
2023-04-08 18:07:27 +0200

    Add _TZE200_xpq2rzhq to https://github.com/Koenkk/zigbee2mqtt/issues/17274TS0601_smart_human_presence_sensor.

--- devices/tuya.js
+++ devices/tuya.js
@@ -3775,6 +3775,7 @@ module.exports = [
             {modelID: 'TS0601', manufacturerName: '_TZE200_lyetpprm'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_jva8ink8'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_holel4dk'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_xpq2rzhq'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_wukb7rhc'},
             {modelID: 'TS0601', manufacturerName: '_TZE204_ztc6ggyl'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_ztc6ggyl'}],
