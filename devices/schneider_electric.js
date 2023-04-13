11d5c5e005f70a4db305578a5961fcdc7996eb38
Eilivers <99320797+Eilivers@users.noreply.github.com>
2023-04-13 09:48:47 +0200

    Update schneider_electric.js
    indentation fixes

--- devices/schneider_electric.js
+++ devices/schneider_electric.js
@@ -1089,8 +1089,8 @@ module.exports = [
             tz.schneider_temperature_measured_value, tz.schneider_thermostat_keypad_lockout],
         exposes: [exposes.binary('keypad_lockout', ea.STATE_SET, 'lock1', 'unlock')
             .withDescription('Enables/disables physical input on the device'),
-            exposes.enum('schneider_pilot_mode', ea.ALL, ['contactor', 'pilot']).withDescription('Controls piloting mode'),
-            exposes.climate().withSetpoint('occupied_heating_setpoint', 4, 30, 0.5).withLocalTemperature()
+        exposes.enum('schneider_pilot_mode', ea.ALL, ['contactor', 'pilot']).withDescription('Controls piloting mode'),
+        exposes.climate().withSetpoint('occupied_heating_setpoint', 4, 30, 0.5).withLocalTemperature()
             .withSystemMode(['off', 'heat']).withRunningState(['idle', 'heat']).withPiHeatingDemand()],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint1 = device.getEndpoint(1);
