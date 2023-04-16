bb61c0ac8e30e1f84887cfbbe682b503b62793d5
smarts4home <93162405+smarts4home@users.noreply.github.com>
2023-04-17 00:41:16 +0200

    Update bosch.js

--- devices/bosch.js
+++ devices/bosch.js
@@ -124,6 +124,9 @@ const tzLocal = {
         },
         convertGet: async (entity, key, meta) => {
             switch (key) {
+            case 'state':
+                await entity.read('genOnOff', ['onOff'], boschManufacturer);
+                break;
             case 'device_type':
                 await entity.read(0xFCA0, [0x0000], boschManufacturer);
                 break;
@@ -854,7 +857,7 @@ const definition = [
         vendor: 'Bosch',
         description: 'Bosch Light/shutter control unit II',
         fromZigbee: [ fzLocal.bmct, fz.cover_position_tilt, fz.power_on_behavior, fz.on_off ],
-        toZigbee: [ tzLocal.bmct, tz.cover_position_tilt, tz.power_on_behavior, tz.on_off ],
+        toZigbee: [ tzLocal.bmct, tz.cover_position_tilt, tz.cover_state, tz.power_on_behavior],
         meta: {multiEndpoint: true},
         endpoint: (device) => {
             return {'left': 2, 'right': 3};
@@ -882,6 +885,9 @@ const definition = [
             exposes.binary('child_lock', ea.ALL, 'ON', 'OFF').withDescription('Enable/Disable child lock'),
             exposes.binary('child_lock', ea.ALL, 'ON', 'OFF').withEndpoint('left').withDescription('Enable/Disable child lock'),
             exposes.binary('child_lock', ea.ALL, 'ON', 'OFF').withEndpoint('right').withDescription('Enable/Disable child lock'),
+            exposes.enum('state', ea.ALL, ['Open', 'Close', 'Stop']).withDescription('Shutter state'),
+            exposes.binary('state', ea.ALL, 'ON', 'OFF').withEndpoint('left').withDescription('On/off state of the switch'),
+            exposes.binary('state', ea.ALL, 'ON', 'OFF').withEndpoint('right').withDescription('On/off state of the switch'),
             e.power_on_behavior().withEndpoint('right'),
             e.power_on_behavior().withEndpoint('left'),
 // cover
