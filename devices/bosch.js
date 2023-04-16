9c1b75657f22f13fa417aa8f12c2ee559fdae8e6
smarts4home <93162405+smarts4home@users.noreply.github.com>
2023-04-17 00:17:46 +0200

    Update bosch.js

--- devices/bosch.js
+++ devices/bosch.js
@@ -889,8 +889,8 @@ const definition = [
             e.power_on_behavior().withEndpoint('left'),
 // cover
             exposes.enum('motor_state', ea.STATE, Object.keys(stateMotor)).withDescription('Shutter motor actual state '),
-            exposes.numeric('calibration_closing_time', ea.SET_STATE).withUnit('S').withDescription('Calibration opening time').withValueMin(1).withValueMax(90),
-            exposes.numeric('calibration_opening_time', ea.SET_STATE).withUnit('S').withDescription('Calibration closing time').withValueMin(1).withValueMax(90),
+            exposes.numeric('calibration_closing_time', ea.ALL).withUnit('S').withDescription('Calibration opening time').withValueMin(1).withValueMax(90),
+            exposes.numeric('calibration_opening_time', ea.ALL).withUnit('S').withDescription('Calibration closing time').withValueMin(1).withValueMax(90),
             e.cover_position(),
         ],
     },
