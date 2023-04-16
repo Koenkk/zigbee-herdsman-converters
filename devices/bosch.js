e349c0857e82ea6608c616fe0b9a5bf9b1ea1dae
smarts4home <93162405+smarts4home@users.noreply.github.com>
2023-04-17 01:04:10 +0200

    Update bosch.js

--- devices/bosch.js
+++ devices/bosch.js
@@ -885,9 +885,9 @@ const definition = [
             exposes.binary('child_lock', ea.ALL, 'ON', 'OFF').withDescription('Enable/Disable child lock'),
             exposes.binary('child_lock', ea.ALL, 'ON', 'OFF').withEndpoint('left').withDescription('Enable/Disable child lock'),
             exposes.binary('child_lock', ea.ALL, 'ON', 'OFF').withEndpoint('right').withDescription('Enable/Disable child lock'),
-            //exposes.enum('state', ea.ALL, ['Open', 'Close', 'Stop']).withDescription('Shutter state'),
-            //exposes.binary('state', ea.ALL, 'ON', 'OFF').withEndpoint('left').withDescription('On/off state of the switch'),
-            //exposes.binary('state', ea.ALL, 'ON', 'OFF').withEndpoint('right').withDescription('On/off state of the switch'),
+            exposes.enum('state', ea.ALL, ['Open', 'Close', 'Stop']).withDescription('Shutter state'),
+            exposes.binary('state', ea.ALL, 'ON', 'OFF').withEndpoint('left').withDescription('On/off state of the switch'),
+            exposes.binary('state', ea.ALL, 'ON', 'OFF').withEndpoint('right').withDescription('On/off state of the switch'),
             e.power_on_behavior().withEndpoint('right'),
             e.power_on_behavior().withEndpoint('left'),
 // cover
