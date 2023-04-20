35d452c9d240dfb439f890ef130cd42be9f2ce78
k2iuh <107772989+k2iuh@users.noreply.github.com>
2023-04-20 21:09:56 +0200

    Add 929003526101 to 929001953101 (#5686)
    The device added is a single packaged device (Philips Hue - Xamento Hue recessed black) which is the same as the one from the package with three lights ('929003526202_01', '929003526202_02', '929003526202_03').

--- devices/philips.js
+++ devices/philips.js
@@ -620,7 +620,7 @@ module.exports = [
         extend: philips.extend.light_onoff_brightness_colortemp_color_gradient({colorTempRange: [153, 500]}),
     },
     {
-        zigbeeModel: ['LCG002', '929003047701', '929003047701', '929003526202_01', '929003526202_02', '929003526202_03'],
+        zigbeeModel: ['LCG002', '929003047701', '929003047701', '929003526202_01', '929003526202_02', '929003526202_03', '929003526101'],
         model: '929001953101',
         vendor: 'Philips',
         description: 'Hue White and Color Ambiance GU10',
