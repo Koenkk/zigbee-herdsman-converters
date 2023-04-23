adeb8bd0b7f86da2e010133e11d44d084e489174
Koen Kanters <koenkanters94@gmail.com>
2023-04-23 09:40:09 +0200

    Merge some whitelabel devices (#5691)
    * Merge some whitelabel devices
    
    * Fix

--- devices/miboxer.js
+++ devices/miboxer.js
@@ -5,40 +5,6 @@ const tuya = require('../lib/tuya');
 
 module.exports = [
     {
-        fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_zrvxvydd'}],
-        model: 'FUT066Z',
-        vendor: 'MiBoxer',
-        description: 'RGB+CCT LED Downlight',
-        extend: tuya.extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
-    },
-    {
-        fingerprint: [{modelID: 'TS0505B', manufacturerName: '_TZ3210_jicmoite'}],
-        model: 'FUT039Z',
-        vendor: 'Miboxer',
-        description: 'RGB+CCT LED controller',
-        extend: tuya.extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
-    },
-    {
-        fingerprint: [{modelID: 'TS0501B', manufacturerName: '_TZ3210_dxroobu3'},
-            {modelID: 'TS0501B', manufacturerName: '_TZ3210_dbilpfqk'}],
-        model: 'FUT036Z',
-        description: 'Single color LED controller',
-        vendor: 'Miboxer',
-        extend: tuya.extend.light_onoff_brightness(),
-        onEvent: tuya.onEventSetTime,
-    },
-    {
-        fingerprint: [
-            {modelID: 'TS0502B', manufacturerName: '_TZ3210_frm6149r'},
-            {modelID: 'TS0502B', manufacturerName: '_TZ3210_jtifm80b'},
-            {modelID: 'TS0502B', manufacturerName: '_TZ3210_xwqng7ol'},
-        ],
-        model: 'FUT035Z',
-        description: 'Dual white LED controller',
-        vendor: 'Miboxer',
-        extend: tuya.extend.light_onoff_brightness_colortemp({colorTempRange: [153, 500], disablePowerOnBehavior: true}),
-    },
-    {
         fingerprint: [{modelID: 'TS0504B', manufacturerName: '_TZ3210_ttkgurpb'}],
         model: 'FUT038Z',
         description: 'RGBW LED controller',
