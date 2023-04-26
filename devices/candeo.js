f9cebb53f576d4ebebb80a7cdef6ebcc6af7fa69
t3ch-fr3ak <73948167+t3ch-fr3ak@users.noreply.github.com>
2023-04-26 07:48:29 +0200

    Add diverging model name HK_DIM_A
    A few models report the name HK_DIM_A instead of HK-DIM-A. Additionally, the power supply is not recognized.

--- devices/candeo.js
+++ devices/candeo.js
@@ -25,6 +25,29 @@ module.exports = [
         },
     },
     {
+        zigbeeModel: ['HK_DIM_A', 'Candeo Zigbee Dimmer'],
+        model: 'HK_DIM_A',
+        vendor: 'Candeo',
+        description: 'Zigbee LED dimmer smart switch',
+        extend: extend.light_onoff_brightness({noConfigure: true, disableEffect: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+
+            // The default reporting from the Candeo dimmer can result in a lot of Zigbee traffic
+            // to the extent that reported brighness values can arrive at the endpoint out of order
+            // giving the appearance that the values are jumping around.
+            // Limit the reporting to once a second to give a smoother reporting of brightness.
+            await reporting.brightness(endpoint, {min: 1});
+
+	    // Has Unknown power source, force it here.
+            device.powerSource = 'Mains (single phase)';
+            device.save();
+        },
+    },
+    {
         zigbeeModel: ['C204'],
         model: 'C204',
         vendor: 'Candeo',
