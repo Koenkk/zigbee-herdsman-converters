3c985dcca816ef23c22186d28cff440fbf459bdb
Koen Kanters <koenkanters94@gmail.com>
2023-05-09 19:36:21 +0200

    Add TS0601_smart_human_presence_sensor_2. https://github.com/Koenkk/zigbee2mqtt/issues/17483

--- devices/tuya.js
+++ devices/tuya.js
@@ -4317,7 +4317,7 @@ module.exports = [
             {modelID: 'TS0601', manufacturerName: '_TZE204_xsm7l9xa'},
             {modelID: 'TS0601', manufacturerName: '_TZE204_ztc6ggyl'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_ztc6ggyl'}],
-        model: 'TS0601_smart_human_presence_sensor',
+        model: 'TS0601_smart_human_presence_sensor_1',
         vendor: 'TuYa',
         description: 'Smart Human presence sensor',
         fromZigbee: [fz.tuya_smart_human_presense_sensor],
@@ -4339,6 +4339,40 @@ module.exports = [
             exposes.enum('self_test', ea.STATE, Object.values(tuya.tuyaHPSCheckingResult))
                 .withDescription('Self_test, possible resuts: checking, check_success, check_failure, others, comm_fault, radar_fault.'),
         ],
+    },
+    {
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_sxm7l9xa']),
+        model: 'TS0601_smart_human_presence_sensor_2',
+        vendor: 'TuYa',
+        description: 'Smart Human presence sensor',
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        exposes: [
+            e.illuminance_lux(), e.presence(),
+            exposes.numeric('target_distance', ea.STATE).withDescription('Distance to target').withUnit('m'),
+            exposes.numeric('radar_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(9).withValueStep(1)
+                .withDescription('sensitivity of the radar'),
+            exposes.numeric('minimum_range', ea.STATE_SET).withValueMin(0).withValueMax(9.5).withValueStep(0.15)
+                .withDescription('Minimum range').withUnit('m'),
+            exposes.numeric('maximum_range', ea.STATE_SET).withValueMin(0).withValueMax(9.5).withValueStep(0.15)
+                .withDescription('Maximum range').withUnit('m'),
+            exposes.numeric('detection_delay', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(0.1)
+                .withDescription('Detection delay').withUnit('s'),
+            exposes.numeric('fading_time', ea.STATE_SET).withValueMin(0.5).withValueMax(1500).withValueStep(1)
+                .withDescription('Fading time').withUnit('s'),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [104, 'illuminance_lux', tuya.valueConverter.raw],
+                [105, 'presence', tuya.valueConverter.trueFalse1],
+                [106, 'radar_sensitivity', tuya.valueConverter.raw],
+                [107, 'maximum_range', tuya.valueConverter.divideBy100],
+                [108, 'minimum_range', tuya.valueConverter.divideBy100],
+                [109, 'target_distance', tuya.valueConverter.divideBy100],
+                [110, 'fading_time', tuya.valueConverter.divideBy10],
+                [111, 'detection_delay', tuya.valueConverter.divideBy10],
+            ],
+        },
     },
     {
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_whkgqxse'}],
