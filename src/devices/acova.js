965eb3c2edeb0427f1de788932971dc95ac7eda3
molusk <molusk@gmail.com>
2023-05-22 13:29:51 +0200

    feat: Support more features for Acova TAFFETAS2 (#5773)
    * Update TAFFETAS2 features
    
    added some features to Taffetas 2 heaters :
     - occupancy reporting using embedded motion sensor
     - unoccupied set-point control
     - temperature calibration control
    
    * Update acova.js
    
    Renamed Taffetas2 model
    
    * Update acova.js
    
    missing 'e' constant
    
    * Update acova.js
    
    missing semicolon

--- src/devices/acova.js
+++ src/devices/acova.js
@@ -2,6 +2,7 @@ const exposes = require('../lib/exposes');
 const fz = require('../converters/fromZigbee');
 const tz = require('../converters/toZigbee');
 const reporting = require('../lib/reporting');
+const e = exposes.presets;
 
 module.exports = [
     {
@@ -67,19 +68,18 @@ module.exports = [
         },
     },
     {
-        zigbeeModel: ['TAFFETAS2 D1.00P1.02Z1.00\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
-            'TAFFETAS2 D1.00P1.01Z1.00\u0000\u0000\u0000\u0000\u0000\u0000\u0000'],
+        zigbeeModel: ['TAFFETAS2 D1.00P1.02Z1.00', 'TAFFETAS2 D1.00P1.01Z1.00'],
         model: 'TAFFETAS2',
         vendor: 'Acova',
         description: 'Taffetas 2 heater',
-        fromZigbee: [fz.thermostat, fz.hvac_user_interface],
+        fromZigbee: [fz.thermostat, fz.hvac_user_interface, fz.occupancy],
         toZigbee: [
             tz.thermostat_local_temperature,
             tz.thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_unoccupied_heating_setpoint,
-            tz.thermostat_occupied_cooling_setpoint,
             tz.thermostat_running_state,
+            tz.thermostat_local_temperature_calibration,
         ],
         exposes: [
             exposes.climate()
@@ -87,15 +87,21 @@ module.exports = [
                 .withSetpoint('unoccupied_heating_setpoint', 7, 28, 0.5)
                 .withLocalTemperature()
                 .withSystemMode(['off', 'heat', 'auto'])
-                .withRunningState(['idle', 'heat']),
+                .withRunningState(['idle', 'heat'])
+                .withLocalTemperatureCalibration('local_temperature_calibration'),
+            e.occupancy(),
         ],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
+            const endpoint2 = device.getEndpoint(2);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'hvacThermostat']);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['msOccupancySensing']);
             await reporting.thermostatTemperature(endpoint);
             await reporting.thermostatRunningState(endpoint);
             await reporting.thermostatOccupiedHeatingSetpoint(endpoint);
             await reporting.thermostatUnoccupiedHeatingSetpoint(endpoint);
+            await reporting.thermostatTemperatureCalibration(endpoint);
+            await reporting.occupancy(endpoint2);
         },
     },
 ];
