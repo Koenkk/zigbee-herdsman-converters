8d6895fcf1903063653d1be0fe4c204990b2a118
Sven337 <sven337@aquilenet.fr>
2023-05-23 22:19:27 +0200

    Profalux covers: switch recent ones to using cluster 0x102, add MAI-ZTP20C remote
    This change references remote model MAI-ZTP20C.
    More importantly, it switches the control of covers to using the dedicated ClosuresWindowCovering (0x102) cluster for the covers that support it.
    Sniffing Zigbee traffic of my remotes show that the remotes use this cluster to control the covers, and not 0x8 as currently implemented.
    The compliance document for the Profalux motors at https://csa-iot.org/csa_product/ac-motor-for-window-coverings/ also lists that cluster and does NOT list 0x6/0x8.
    
    So I think the correct way to control these covers is 0x102, and certainly it's the way that seems to work with my shutters (all bought in 2023).
    
    Older revisions of these motors, which did not expose a zigbeeModel field, did not expose 0x102 either and so these use the pre-existing fingerprint to keep using cluster 0x8.

--- src/devices/profalux.ts
+++ src/devices/profalux.ts
@@ -18,28 +18,51 @@ const definitions: Definition[] = [
         exposes: [],
     },
     {
+        // Recent covers are matched by Zigbee model. Their remotes communicate using cluster 0x102 "closuresWindowCovering", so use that.
         zigbeeModel: [
             'MOT-C1Z06C\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
             'MOT-C1Z10F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
             'MOT-C1Z06F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
         ],
-        fingerprint: [{manufacturerID: 4368, endpoints: [{ID: 1, profileID: 260, deviceID: 512,
-            inputClusters: [0, 3, 4, 5, 6, 8, 10, 21, 256, 64544, 64545], outputClusters: [3, 64544]}]}],
         model: 'NSAV061',
         vendor: 'Profalux',
         description: 'Cover',
-        fromZigbee: [fz.cover_position_via_brightness, fz.cover_state_via_onoff],
-        toZigbee: [tz.cover_via_brightness],
+        fromZigbee: [fz.command_cover_close, fz.command_cover_open, fz.cover_position_tilt ],
+        toZigbee: [tz.cover_state, tz.cover_position_tilt],
         exposes: [e.cover_position().setAccess('state', ea.ALL)],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(2);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['closuresWindowCovering']);
+            await reporting.currentPositionLiftPercentage(endpoint);
+        },  
+            endpoint: (device) => {
+                    return { default: 2};
+            },
+    },
+    {
+        // Identify older covers based on their fingerprint. These do not
+        // expose closuresWindowCovering and need to use genLevelCtrl
+        // instead. Sniffing a remote would be welcome to confirm that this
+        // is the right thing to do.
+        fingerprint: [{ manufacturerID: 4368, endpoints: [{ ID: 1, profileID: 260, deviceID: 512,
+                        inputClusters: [0, 3, 4, 5, 6, 8, 10, 21, 256, 64544, 64545], outputClusters: [3, 64544] }] }],
+        model: 'NSAV061 (old)',
+        vendor: 'Profalux',
+        description: 'Cover',
+        fromZigbee: [fz.cover_position_via_brightness, fz.cover_state_via_onoff ],
+        toZigbee: [tz.cover_via_brightness ],
+        exposes: [e.cover_position().setAccess('state', ea.ALL)], 
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genLevelCtrl']);
-            await reporting.brightness(endpoint);
+            await reporting.currentPositionLiftPercentage(endpoint);
         },
     },
     {
-        zigbeeModel: ['MAI-ZTP20F'],
-        model: 'MAI-ZTP20F',
+        // Newer remotes. These expose a bunch of things but they are bound to                                                                                // the cover and don't seem to communicate with the coordinator, so
+        // nothing is likely to be doable in Z2M.
+        zigbeeModel: ['MAI-ZTP20F', 'MAI-ZTP20C'],
+        model: 'MAI-ZTP20',
         vendor: 'Profalux',
         description: 'Cover remote',
         fromZigbee: [],
