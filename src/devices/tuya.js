ab00fbb43b35a7d51f810bc235ed00de380c96d2
koenkk <koenkanters94@gmail.com>
2023-05-23 13:19:01 +0200

    fix(detect): Detect `_TZ3000_ocjlo4ea` as TuYa TS0207_water_leak_detector_1. https://github.com/Koenkk/zigbee2mqtt/issues/17761

--- src/devices/tuya.js
+++ src/devices/tuya.js
@@ -1515,6 +1515,7 @@ module.exports = [
             {vendor: 'CR Smart Home', model: 'TS0207'},
             tuya.whitelabel('Meian', 'SW02', 'Water leak sensor', ['_TZ3000_kyb656no']),
             tuya.whitelabel('Aubess', 'IH-K665', 'Water leak sensor', ['_TZ3000_k4ej3ww2']),
+            tuya.whitelabel('TuYa', 'TS0207_water_leak_detector_1', 'Zigbee water flood sensor + 1m probe cable', ['_TZ3000_ocjlo4ea']),
         ],
         toZigbee: [],
         configure: async (device, coordinatorEndpoint, logger) => {
