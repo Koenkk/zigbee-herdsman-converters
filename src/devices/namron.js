5735246aea06e8266e7bc491ca30ad02ab73fdad
koenkk <koenkanters94@gmail.com>
2023-05-26 17:20:12 +0200

    feat(add): 1402769 https://github.com/Koenkk/zigbee-herdsman-converters/issues/5766

--- src/devices/namron.js
+++ src/devices/namron.js
@@ -232,6 +232,19 @@ module.exports = [
         toZigbee: [],
     },
     {
+        zigbeeModel: ['1402769'],
+        model: '1402769',
+        vendor: 'Namron',
+        description: 'ZigBee LED dimmer',
+        extend: extend.light_onoff_brightness({noConfigure: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+        },
+    },
+    {
         zigbeeModel: ['4512702'],
         model: '4512702',
         vendor: 'Namron',
