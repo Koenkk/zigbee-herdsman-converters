87b12172e769cfe605a3259eb4e31b4cfeb5233c
koenkk <koenkanters94@gmail.com>
2023-06-19 19:09:58 +0200

    feat(detect): Detect `_TZE200_44af8vyi` as TuYa TS0601_temperature_humidity_sensor_1. https://github.com/Koenkk/zigbee2mqtt/issues/18057

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -560,6 +560,7 @@ const definitions: Definition[] = [
             {modelID: 'TS0601', manufacturerName: '_TZE200_bjawzodf'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_qyflbnbj'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_vs0skpuc'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_44af8vyi'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_zl1kmjqx'}],
         model: 'TS0601_temperature_humidity_sensor_1',
         vendor: 'TuYa',
