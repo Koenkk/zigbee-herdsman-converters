e8609650ac272901187719038bfea51ba8a87668
nordish1 <48866793+nordish1@users.noreply.github.com>
2023-07-01 17:41:54 +0200

    Update ls.js

--- src/devices/ls.js
+++ src/devices/ls.js
@@ -1,20 +1,14 @@
-import * as exposes from '../lib/exposes';
-import fz from '../converters/fromZigbee';
-import tz from '../converters/toZigbee';
-import * as reporting from '../lib/reporting';
+import {Definition} from '../lib/types';
 import extend from '../lib/extend';
-import * as ota from '../lib/ota';
-import {Definition, Fz, KeyValue} from '../lib/types';
-const e = exposes.presets;
-const ea = exposes.access;
 
+const definitions: Definition[] = [
+    {
+        zigbeeModel: ['Emotion'],
+        model: 'A319463',
+        vendor: 'LS Deutschland GmbH',
+        description: 'Home base',
+        extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 454]}),
+    },
+];
 
-const definition = {
-    zigbeeModel: ['Emotion'],
-    model: 'Emotion',
-    vendor: 'LS Deutschland GmbH',
-    description: 'L&S Emotion Home Base',
-    extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 454]}),
-};
-
-module.exports = definition;
+module.exports = definitions;
