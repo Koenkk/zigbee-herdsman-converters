00ce85823b71e5040b91696b96883f831dbce4da
koenkk <koenkanters94@gmail.com>
2023-07-04 16:45:44 +0200

    feat(add): PEZ1-042-1020-C1D1 @tetienne https://github.com/Koenkk/zigbee2mqtt/issues/18195

--- src/devices/adeo.ts
+++ src/devices/adeo.ts
@@ -131,6 +131,13 @@ const definitions: Definition[] = [
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 370]}),
     },
     {
+        zigbeeModel: ['ZBEK-28'],
+        model: 'PEZ1-042-1020-C1D1',
+        vendor: 'ADEO',
+        description: 'Gdansk ENKI',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 370]}),
+    },
+    {
         zigbeeModel: ['ZBEK-5'],
         model: 'IST-CDZFB2AS007NA-MZN-01',
         vendor: 'ADEO',
