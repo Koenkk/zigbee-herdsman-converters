ac852a866b03870e7ae07e8b0679f42852ed28db
KDanielK <daniel@karlsson.com>
2023-07-05 19:10:09 +0200

    fix: Fix various Eco-Dim.07/Eco-Dim.10 variants not recognised (#5946)
    * Update ecodim.ts
    
    Added support for all white label versions of the Dimmer-Switch-ZB3.0 since they all have manufacturerID 4714
    For example MoooN AB that is missing now
    
    NOTE: most likely the other versions such as Namron can be removed now since they all have 4714 as manufacturerID
    
    * Update ecodim.ts
    
    * Update ecodim.ts

--- src/devices/ecodim.ts
+++ src/devices/ecodim.ts
@@ -37,10 +37,7 @@ const definitions: Definition[] = [
     },
     {
         fingerprint: [
-            {type: 'Router', manufacturerName: 'EcoDim BV', modelID: 'Dimmer-Switch-ZB3.0'},
-            {type: 'Router', manufacturerName: 'EcoDim B.V', modelID: 'Dimmer-Switch-ZB3.0'},
-            {type: 'Router', manufacturerName: 'Lightsolutions', modelID: 'Dimmer-Switch-ZB3.0'},
-            {type: 'Router', manufacturerName: 'Namron AS', modelID: 'Dimmer-Switch-ZB3.0'},
+            {type: 'Router', manufacturerID: 4714, modelID: 'Dimmer-Switch-ZB3.0'},
             {type: 'Router', manufacturerName: 'EcoDim BV', modelID: 'EcoDim-Zigbee 3.0', endpoints: [
                 {ID: 1, profileID: 260, deviceID: 257, inputClusters: [0, 3, 4, 5, 6, 8, 2821, 4096], outputClusters: [25]},
                 {ID: 242, profileID: 41440, deviceID: 97, inputClusters: [], outputClusters: [33]},
