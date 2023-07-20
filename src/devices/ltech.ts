b6c2a1893a3141425014cd75815e3c158ca76654
Testingfor101 <115428566+Testingfor101@users.noreply.github.com>
2023-07-20 10:21:36 +0300

    Update ltech.ts

--- src/devices/ltech.ts
+++ src/devices/ltech.ts
@@ -42,29 +42,7 @@ const definition = {
 			[3, 'brightness', tuya.valueConverter.scale0_254to0_1000],
 		]
 	},
-},
-
-
-const legacy = require('zigbee-herdsman-converters/lib/legacy');
-const fz = {...require('zigbee-herdsman-converters/converters/fromZigbee'), legacy: require('zigbee-herdsman-converters/lib/legacy').fromZigbee};
-const tz = {...require('zigbee-herdsman-converters/converters/toZigbee'), legacy: require('zigbee-herdsman-converters/lib/legacy').toZigbee};
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const extend = require('zigbee-herdsman-converters/lib/extend');
-const e = exposes.presets;
-const ea = exposes.access;
-const tuya = require('zigbee-herdsman-converters/lib/tuya');
-const color = require('zigbee-herdsman-converters/lib/color');
-const light = require('zigbee-herdsman-converters/lib/light');
-const ota = require('zigbee-herdsman-converters/lib/ota');
-const skip = {
-    // Prevent state from being published when already ON and brightness is also published. 
-    // This prevents 100% -> X% brightness jumps when the switch is already on
-    // https://github.com/Koenkk/zigbee2mqtt/issues/13800#issuecomment-1263592783
-    stateOnAndBrightnessPresent: (meta) => meta.message.hasOwnProperty('brightness') && meta.state.state === 'ON',
-}
-
-const definition = {
+	
 	fingerprint: [
 		{
 			// The model ID from: Device with modelID 'TS0601' is not supported
@@ -90,3 +68,4 @@ const definition = {
 		]
 	},
 };
+module.exports = definitions;
