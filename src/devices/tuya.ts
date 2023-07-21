0f94989aaa42313ef1e3a9930e27c88549db9b5f
arteck <6681528+arteck@users.noreply.github.com>
2023-07-21 08:08:54 +0200

    new fingerprint forZY-M100-S
     fingerprint: tuya.fingerprint('TS0601', ['_TZE204_sxm7l9xa', '_TZE204_qasjif9e']),

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -4175,7 +4175,7 @@ const definitions: Definition[] = [
         ],
     },
     {
-        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_sxm7l9xa']),
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_sxm7l9xa', '_TZE204_qasjif9e']),
         model: 'ZY-M100-S',
         vendor: 'TuYa',
         description: 'Mini human breathe sensor',
