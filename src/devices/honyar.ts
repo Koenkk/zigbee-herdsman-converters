87653abe1e0b284e11c6e6118ec948619b679afb
65698888 <98879715+65698888@users.noreply.github.com>
2023-07-25 18:42:50 +0800

    Update honyar.ts

--- src/devices/honyar.ts
+++ src/devices/honyar.ts
@@ -1,4 +1,4 @@
-import {Definition, Fz} from '../lib/types';
+import {Definition} from '../lib/types';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
 import tz from '../converters/toZigbee';
