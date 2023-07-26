2b93f5b868e9cd36d6e86484fd4a634fdae52a7d
Geir Råness <git@xmod.no>
2023-07-26 23:02:47 +0200

    more fixes

--- src/devices/heatit.ts
+++ src/devices/heatit.ts
@@ -1,8 +1,5 @@
 import {Definition} from '../lib/types';
-import * as exposes from '../lib/exposes';
-import * as reporting from '../lib/reporting';
 import extend from '../lib/extend';
-
 
 const definitions: Definition[] = [
     {
