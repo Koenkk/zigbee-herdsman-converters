594384e13eaa3116be50179950d2ab11a8b30e22
Geir Råness <git@xmod.no>
2023-07-26 22:56:07 +0200

    ci fixes

--- src/devices/heatit.ts
+++ src/devices/heatit.ts
@@ -1,9 +1,8 @@
 import {Definition} from '../lib/types';
-import fz from '../converters/fromZigbee';
 import * as exposes from '../lib/exposes';
 import * as reporting from '../lib/reporting';
 import extend from '../lib/extend';
-const e = exposes.presets;
+
 
 const definitions: Definition[] = [
     {
