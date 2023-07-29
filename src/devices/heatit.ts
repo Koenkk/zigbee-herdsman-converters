8df878f7215ed7e554c1ae3f43dea131485ab55a
Geir Råness <git@xmod.no>
2023-07-29 17:03:55 +0200

    tweak description

--- src/devices/heatit.ts
+++ src/devices/heatit.ts
@@ -6,7 +6,7 @@ const definitions: Definition[] = [
         fingerprint: [{type: 'Router', manufacturerName: 'Heatit Controls AB', modelID: 'Dimmer-Switch-ZB3.0'}],
         model: '1444420',
         vendor: 'Heatit',
-        description: 'Zigbee dimmer 250W',
+        description: 'Zig Dim 250W',
         extend: extend.light_onoff_brightness({disablePowerOnBehavior: true}),
     },
 ];
