cda9f3a60b2e4624cb9bf1bdc931dd9ce62371d2
Geir Råness <git@xmod.no>
2023-07-29 16:58:24 +0200

    use elnummer instead of name, to generic for z2m

--- src/devices/heatit.ts
+++ src/devices/heatit.ts
@@ -4,7 +4,7 @@ import extend from '../lib/extend';
 const definitions: Definition[] = [
     {
         fingerprint: [{type: 'Router', manufacturerName: 'Heatit Controls AB', modelID: 'Dimmer-Switch-ZB3.0'}],
-        model: 'Zig Dim 250W',
+        model: '1444420',
         vendor: 'Heatit',
         description: 'Zigbee dimmer 250W',
         extend: extend.light_onoff_brightness({disablePowerOnBehavior: true}),
