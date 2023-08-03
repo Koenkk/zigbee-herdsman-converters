4b94f1be8efc24d847ff3aeffa8e9e7886867818
Graham Knights <baudfather@users.noreply.github.com>
2023-08-03 10:18:11 -0700

    feat(add): GW-Z-0010 (#6037)
    * Create vendor CEL
    
    * Rename CEL to CEL.ts
    
    * Rename CEL.ts to cel.ts
    
    * Update cel.ts
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

---
+++ src/devices/cel.ts
@@ -0,0 +1,16 @@
+import {Definition} from '../lib/types';
+import fz from '../converters/fromZigbee';
+
+const definitions: Definition[] = [
+    {
+        zigbeeModel: ['Z10'],
+        model: 'CGW-Z-0010',
+        vendor: 'CEL',
+        description: 'Cortet range extender',
+        fromZigbee: [fz.linkquality_from_basic],
+        toZigbee: [],
+        exposes: [],
+    },
+];
+
+module.exports = definitions;
