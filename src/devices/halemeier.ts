21dd5656df1cf3685723971dd133fac5f7d94a01
Sebastian Mueller <sebastian.mueller@yworks.com>
2023-08-06 18:16:39 +0200

    Add support for Halemeier S-Mitter MultiWhite2 Smart Remote Control

--- src/devices/halemeier.ts
+++ src/devices/halemeier.ts
@@ -1,5 +1,9 @@
 import {Definition} from '../lib/types';
+import fz from '../converters/fromZigbee';
+import tz from '../converters/toZigbee';
 import extend from '../lib/extend';
+import * as exposes from '../lib/exposes';
+const e = exposes.presets;
 
 const definitions: Definition[] = [
     {
@@ -22,6 +26,18 @@ const definitions: Definition[] = [
         vendor: 'Halemeier',
         description: 'LED driver',
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [160, 450]}),
+    },
+    {
+        zigbeeModel: ['HA-ZSM-MW2'],
+        model: 'HA-ZSM-MW2',
+        vendor: 'Halemeier',
+        description: 'Halemeier S-Mitter MultiWhite2 Smart Remote Control',
+        fromZigbee: [fz.battery, fz.command_step, fz.command_step_color_temperature,
+            fz.command_recall, fz.command_off, fz.command_on],
+        toZigbee: [tz.battery_percentage_remaining],
+        exposes: [e.action_group(), e.battery(), e.action(['recall_*', 'on', 'off',
+            'color_temperature_step_up', 'color_temperature_step_down', 'brightness_step_up', 'brightness_step_down']),
+        ],
     },
 ];
 
