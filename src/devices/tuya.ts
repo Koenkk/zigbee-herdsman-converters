bd6e04395782ccf634bd0211d16656a902dc1af6
Domagoj Polović <Vorta@users.noreply.github.com>
2023-08-17 20:51:34 +0200

    fix(detect): Detect `_TZ3000_qystbcjg` as UNSH SMKG-1KNL-EU-Z (#6081)

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -4802,7 +4802,7 @@ const definitions: Definition[] = [
         configure: tuya.configureMagicPacket,
     },
     {
-        fingerprint: tuya.fingerprint('TS011F', ['_TZ3000_cayepv1a', '_TZ3000_lepzuhto']),
+        fingerprint: tuya.fingerprint('TS011F', ['_TZ3000_cayepv1a', '_TZ3000_lepzuhto', '_TZ3000_qystbcjg']),
         model: 'TS011F_with_threshold',
         description: 'Din rail switch with power monitoring and threshold settings',
         vendor: 'TuYa',
@@ -4853,6 +4853,7 @@ const definitions: Definition[] = [
         whiteLabel: [
             tuya.whitelabel('TONGOU', 'TO-Q-SY2-163JZT', 'Smart circuit breaker', ['_TZ3000_cayepv1a']),
             tuya.whitelabel('EARU', 'EAKCB-T-M-Z', 'Smart circuit breaker', ['_TZ3000_lepzuhto']),
+            tuya.whitelabel('UNSH', 'SMKG-1KNL-EU-Z', 'Smart Circuit Breaker', ['_TZ3000_qystbcjg']),
         ],
     },
     {
