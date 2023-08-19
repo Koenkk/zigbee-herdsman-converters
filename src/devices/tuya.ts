8ad4808efeebe90f6ca8910fe7e854c4179759e9
Koen Kanters <koenkanters94@gmail.com>
2023-08-19 09:07:06 +0200

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -4319,7 +4319,7 @@ const definitions: Definition[] = [
                 .withDescription('Static detection distance'),
             e.numeric('static_detection_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(1).withUnit('x')
                 .withDescription('Static detection sensitivity'),
-            e.enum('mode', ea.STATE_SET, ['off', 'arm', 'alarm','doorbell']).withDescription('Working mode'),
+            e.enum('mode', ea.STATE_SET, ['off', 'arm', 'alarm', 'doorbell']).withDescription('Working mode'),
             e.enum('alarm_volume', ea.STATE_SET, ['mute', 'low', 'medium', 'high']).withDescription('Alarm volume'),
             e.numeric('alarm_time', ea.STATE_SET).withValueMin(1).withValueMax(60).withValueStep(1).withUnit('m').withDescription('Alarm time'),
             e.binary('light_mode', ea.STATE_SET, 'ON', 'OFF').withDescription('LED indicator mode'),
@@ -4338,7 +4338,8 @@ const definitions: Definition[] = [
                 [16, 'small_motion_detection_sensitivity', tuya.valueConverter.raw],
                 [103, 'static_detection_distance', tuya.valueConverter.divideBy100],
                 [104, 'static_detection_sensitivity', tuya.valueConverter.raw],
-                [105, 'mode', tuya.valueConverterBasic.lookup({'arm': tuya.enum(0), 'off': tuya.enum(1), 'alarm': tuya.enum(2),'doorbell': tuya.enum(3)})],
+                [105, 'mode', tuya.valueConverterBasic.lookup(
+                    {'arm': tuya.enum(0), 'off': tuya.enum(1), 'alarm': tuya.enum(2), 'doorbell': tuya.enum(3)})],
                 [102, 'alarm_volume', tuya.valueConverterBasic.lookup({
                     'low': tuya.enum(0), 'medium': tuya.enum(1), 'high': tuya.enum(2), 'mute': tuya.enum(3),
                 })],
@@ -4347,7 +4348,6 @@ const definitions: Definition[] = [
             ],
         },
     },
-     
     {
         fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_whkgqxse'}],
         model: 'JM-TRH-ZGB-V1',
@@ -4520,7 +4520,7 @@ const definitions: Definition[] = [
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
         configure: tuya.configureMagicPacket,
-        exposes: [e.contact(), e.illuminance().withUnit('lx'), e.battery(), 
+        exposes: [e.contact(), e.illuminance().withUnit('lx'), e.battery(),
             e.numeric('illuminance_interval', ea.STATE_SET).withValueMin(1).withValueMax(720).withValueStep(1).withUnit('minutes')
                 .withDescription('Brightness acquisition interval (refresh and update only while active)')],
         meta: {
@@ -4548,8 +4548,7 @@ const definitions: Definition[] = [
             ],
         },
     },
-
-      {
+    {
         fingerprint: tuya.fingerprint('TS0601', ['_TZE200_3towulqd', '_TZE200_1ibpyhdc', '_TZE200_bh3n6gk8']),
         model: 'ZG-204ZL',
         vendor: 'TuYa',
@@ -4558,20 +4557,20 @@ const definitions: Definition[] = [
         toZigbee: [tuya.tz.datapoints],
         exposes: [
             e.occupancy(), e.illuminance().withUnit('lx'), e.battery(),
-            e.enum('pir_sensitivity', ea.STATE_SET, ['low', 'medium', 'high'])
+            e.enum('sensitivity', ea.STATE_SET, ['low', 'medium', 'high'])
                 .withDescription('PIR sensor sensitivity (refresh and update only while active)'),
-            e.enum('pir_keep_time', ea.STATE_SET, ['10', '30', '60', '120'])
+            e.enum('keep_time', ea.STATE_SET, ['10', '30', '60', '120'])
                 .withDescription('PIR keep time in seconds (refresh and update only while active)'),
             e.numeric('illuminance_interval', ea.STATE_SET).withValueMin(1).withValueMax(720).withValueStep(1).withUnit('minutes')
-                .withDescription('Brightness acquisition interval (refresh and update only while active)')
-
+                .withDescription('Brightness acquisition interval (refresh and update only while active)'),
         ],
-         meta: {
+        meta: {
             tuyaDatapoints: [
                 [1, 'occupancy', tuya.valueConverter.trueFalse0],
                 [4, 'battery', tuya.valueConverter.raw],
-                [9, 'pir_sensitivity',tuya.valueConverterBasic.lookup({'low': tuya.enum(0), 'medium': tuya.enum(1), 'high': tuya.enum(2)})],
-                [10, 'pir_keep_time', tuya.valueConverterBasic.lookup({'10': tuya.enum(0), '30': tuya.enum(1), '60': tuya.enum(2),'120': tuya.enum(3)})],
+                [9, 'sensitivity', tuya.valueConverterBasic.lookup({'low': tuya.enum(0), 'medium': tuya.enum(1), 'high': tuya.enum(2)})],
+                [10, 'keep_time', tuya.valueConverterBasic.lookup(
+                    {'10': tuya.enum(0), '30': tuya.enum(1), '60': tuya.enum(2), '120': tuya.enum(3)})],
                 [12, 'illuminance', tuya.valueConverter.raw],
                 [102, 'illuminance_interval', tuya.valueConverter.raw],
 
@@ -4580,50 +4579,63 @@ const definitions: Definition[] = [
     },
     {
         fingerprint: tuya.fingerprint('TS0225', ['_TZE200_2aaelwxk']),
-        model: 'ZG-205Z',
+        model: 'ZG-205Z/A',
         vendor: 'TuYa',
-        description: 'Mini 24Ghz human presence sensor',
+        description: '24Ghz Human presence sensor',
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
         exposes: [
-            e.presence(),
-            e.enum('motion_state', ea.STATE, ['none', 'large', 'small', 'static']).withDescription('Motion state'),
-            e.illuminance_lux(),
-            e.numeric('fading_time', ea.STATE_SET).withValueMin(0).withValueMax(600).withValueStep(1).withUnit('s')
-                .withDescription('Presence keep time'),
-            e.numeric('large_motion_detection_distance', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(0.01).withUnit('m')
-                .withDescription('Large motion detection distance'),
+            e.presence(), e.illuminance().withUnit('lx'),
             e.numeric('large_motion_detection_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(1).withUnit('x')
-                .withDescription('Large motion detection sensitivity'),
-            e.numeric('small_motion_detection_distance', ea.STATE_SET).withValueMin(0).withValueMax(6).withValueStep(0.01).withUnit('m')
-                .withDescription('Small motion detection distance'),
-            e.numeric('small_motion_detection_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(1).withUnit('x')
-                .withDescription('Small motion detection sensitivity'),
-            e.numeric('static_detection_distance', ea.STATE_SET).withValueMin(0).withValueMax(6).withValueStep(0.01).withUnit('m')
-                .withDescription('Static detection distance'),
-            e.numeric('static_detection_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(1).withUnit('x')
-                .withDescription('Static detection sensitivity'),
-            e.binary('light_mode', ea.STATE_SET, 'ON', 'OFF').withDescription('LED indicator mode'),
+                .withDescription('Motion detection sensitivity'),
+            e.numeric('large_motion_detection_distance', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(0.01).withUnit('m')
+                .withDescription('Motion detection distance'),
+            e.enum('motion_state', ea.STATE, ['none', 'small', 'medium', 'large']).withDescription('State of the motion'),
+            e.numeric('fading_time', ea.STATE_SET).withValueMin(0).withValueMax(28800).withValueStep(1).withUnit('s')
+                .withDescription('For how much time presence should stay true after detecting it'),
+            e.numeric('medium_motion_detection_distance', ea.STATE_SET).withValueMin(0).withValueMax(6).withValueStep(0.01).withUnit('m')
+                .withDescription('Medium motion detection distance'),
+            e.numeric('medium_motion_detection_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(1).withUnit('x')
+                .withDescription('Medium motion detection sensitivity'),
+            e.binary('indicator', ea.STATE_SET, 'ON', 'OFF').withDescription('LED Indicator'),
+            e.numeric('small_detection_distance', ea.STATE_SET).withValueMin(0).withValueMax(6).withValueStep(0.01).withUnit('m')
+                .withDescription('Small detection distance'),
+            e.numeric('small_detection_sensitivity', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(1).withUnit('x')
+                .withDescription('Small detection sensitivity'),
         ],
         meta: {
             tuyaDatapoints: [
                 [1, 'presence', tuya.valueConverter.trueFalse1],
-                [106, 'illuminance_lux', tuya.valueConverter.raw],
-                [101, 'motion_state', tuya.valueConverterBasic.lookup({
-                    'none': tuya.enum(0), 'large': tuya.enum(1), 'small': tuya.enum(2), 'static': tuya.enum(3),
-                })],
-                [102, 'fading_time', tuya.valueConverter.raw],
-                [4, 'large_motion_detection_distance', tuya.valueConverter.divideBy100],
                 [2, 'large_motion_detection_sensitivity', tuya.valueConverter.raw],
-                [104, 'small_motion_detection_distance', tuya.valueConverter.divideBy100],
-                [105, 'small_motion_detection_sensitivity', tuya.valueConverter.raw],
-                [108, 'static_detection_distance', tuya.valueConverter.divideBy100],
-                [109, 'static_detection_sensitivity', tuya.valueConverter.raw],
-                [107, 'light_mode', tuya.valueConverter.onOff],
+                [4, 'large_motion_detection_distance', tuya.valueConverter.divideBy100],
+                [101, 'motion_state', tuya.valueConverterBasic.lookup(
+                    {'none': tuya.enum(0), 'large': tuya.enum(1), 'medium': tuya.enum(2), 'small': tuya.enum(3)})],
+                [102, 'fading_time', tuya.valueConverter.raw],
+                [104, 'medium_motion_detection_distance', tuya.valueConverter.divideBy100],
+                [105, 'medium_motion_detection_sensitivity', tuya.valueConverter.raw],
+                [106, 'illuminance', tuya.valueConverter.raw],
+                [107, 'indicator', tuya.valueConverter.onOff],
+                [108, 'small_detection_distance', tuya.valueConverter.divideBy100],
+                [109, 'small_detection_sensitivity', tuya.valueConverter.raw],
+                // Not exposed DPs/untested
+                // [103, 'motion_false_detection', tuya.valueConverter.raw],
+                // [113, 'breathe_false_detection', tuya.valueConverter.raw],
+                // [3, 'mov_minimum_distance', tuya.valueConverter.raw],
+                // [110, 'micro_minimum_distance', tuya.valueConverter.raw],
+                // [111, 'motionless_minimum_distance', tuya.valueConverter.raw],
+                // [112, 'reset_setting', tuya.valueConverter.raw],
+                // [114, 'time', tuya.valueConverter.raw],
+                // [115, 'alarm_time', tuya.valueConverter.raw],
+                // [116, 'alarm_volume', tuya.valueConverterBasic.lookup(
+                //  {'low': tuya.enum(0), 'medium': tuya.enum(1), 'high': tuya.enum(2), 'mute': tuya.enum(3)})],
+                // [117, 'working_mode', tuya.valueConverterBasic.lookup(
+                // {'arm': tuya.enum(0), 'off': tuya.enum(1), 'alarm': tuya.enum(2),  'doorbell': tuya.enum(3)})],
+                // [118, 'auto1', tuya.valueConverter.raw],
+                // [119, 'auto2', tuya.valueConverter.raw],
+                // [120, 'auto3', tuya.valueConverter.raw],
             ],
         },
     },
-
     {
         fingerprint: tuya.fingerprint('TS110E', ['_TZ3210_zxbtub8r', '_TZ3210_k1msuvg6']),
         model: 'TS110E_1gang_1',
