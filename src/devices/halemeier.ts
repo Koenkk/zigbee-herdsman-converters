25d79fd42a8db33b1be8ff8bf9576d8af09b08fa
Philip Gillißen <guerda@freenet.de>
2023-08-21 11:22:50 +0200

    Add converter for Halemeier HA-ZBM-MW2
    Added first version for remote HA-ZBM-MW2
    Open yet: Battery

--- src/devices/halemeier.ts
+++ src/devices/halemeier.ts
@@ -39,6 +39,20 @@ const definitions: Definition[] = [
             'color_temperature_step_up', 'color_temperature_step_down', 'brightness_step_up', 'brightness_step_down']),
         ],
     },
+    {
+        zigbeeModel: ['HA-ZBM-MW2'],
+        vendor: 'Halemeier',
+        description: 'S-Mitter Basic MultiWhite² 1-channel sender Zigbee ',
+        fromZigbee: [fz.command_recall, fz.command_off, fz.command_on, fz.command_step_color_temperature, fz.command_step],
+        toZigbee: [tz.battery_percentage_remaining],
+        exposes: [e.battery().withAccess(ea.STATE_GET), e.action(['on', 'off']), e.light_brightness_colortemp([0, 254])],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+        },
+    },
+}
 ];
 
 module.exports = definitions;
