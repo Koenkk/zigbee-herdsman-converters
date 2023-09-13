ec2a5088d2e0e7b6b9bfd2ba944007514db1614b
peetipablo <61809759+peetipablo@users.noreply.github.com>
2023-09-13 13:42:19 -0400

    feat(add): RB56SC (#6156)
    * Update acuity_brands_lighting.ts
    
    Add ABL-LIGHT-Z-201 zigbee model
    
    * Update acuity_brands_lighting.ts

--- src/devices/acuity_brands_lighting.ts
+++ src/devices/acuity_brands_lighting.ts
@@ -9,6 +9,13 @@ const definitions: Definition[] = [
         description: 'Juno 4" and 6" LED smart wafer downlight',
         extend: extend.light_onoff_brightness_colortemp({colorTempRange: [200, 370], disableColorTempStartup: true}),
     },
+    {
+        zigbeeModel: ['ABL-LIGHT-Z-201'],
+        model: 'RB56SC',
+        vendor: 'Acuity Brands Lighting (ABL)',
+        description: 'Juno Retrobasics 4" and 6" LED smart downlight',
+        extend: extend.light_onoff_brightness_colortemp({colorTempRange: [200, 370], disableColorTempStartup: true}),
+    },
 ];
 
 module.exports = definitions;
