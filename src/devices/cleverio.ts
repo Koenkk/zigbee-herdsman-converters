ca28fab0c02a05a45c3c47f8bffa23659869485a
Koen Kanters <koenkanters94@gmail.com>
2023-09-18 19:06:53 +0200

    Update cleverio.ts

--- src/devices/cleverio.ts
+++ src/devices/cleverio.ts
@@ -38,7 +38,7 @@ const definitions: Definition[] = [
         fingerprint: [{modelID: 'SM0201', manufacturerName: '_TYZB01_lzrhtcxu'}],
         model: 'SS300',
         vendor: 'Cleverio',
-        description: 'Cleverio RH/Temp Sensor SS300',
+        description: 'Temperature/humdity sensor',
         exposes: [e.battery().withAccess(ea.STATE_GET), e.temperature(), e.humidity()],
         fromZigbee: [fz.temperature, fz.humidity, fz.battery],
         toZigbee: [tz.battery_percentage_remaining],
