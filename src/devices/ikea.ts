355333ad34f5cf2349de91faa5e10551e9513793
koenkk <koenkanters94@gmail.com>
2023-09-19 19:48:24 +0200

    fix: Rename LED2103GS to LED2103G5 https://github.com/Koenkk/zigbee2mqtt/discussions/18987

--- src/devices/ikea.ts
+++ src/devices/ikea.ts
@@ -443,7 +443,7 @@ const definitions: Definition[] = [
     },
     {
         zigbeeModel: ['TRADFRI bulb E27 WW globe 806lm'],
-        model: 'LED2103GS',
+        model: 'LED2103G5',
         vendor: 'IKEA',
         description: 'TRADFRI LED bulb E27 806 lumen, wireless dimmable warm white',
         extend: tradfriExtend.light_onoff_brightness(),
