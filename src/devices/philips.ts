49c4453fe99de23c0971534d4368ab456c20c3d3
koenkk <koenkanters94@gmail.com>
2023-09-19 19:51:07 +0200

    feat(add): 8718696126523 https://github.com/Koenkk/zigbee2mqtt/issues/19020

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -12,6 +12,13 @@ const ea = exposes.access;
 
 const definitions: Definition[] = [
     {
+        zigbeeModel: ['LLM010', 'LLM011', 'LLM012'],
+        model: '8718696126523',
+        vendor: 'Philips',
+        description: 'Hue Phoenix ceiling light',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [154, 455]}),
+    },
+    {
         zigbeeModel: ['LWA023'],
         model: '9290030516',
         vendor: 'Philips',
