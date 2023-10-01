c4fde67be63a9e563f14aa5b964ec70c412818de
Michael Barlock <barlockm@gmail.com>
2023-10-01 19:47:54 -0400

    fix: yookie cover position

--- src/devices/yookee.ts
+++ src/devices/yookee.ts
@@ -12,7 +12,8 @@ const definitions: Definition[] = [
         vendor: 'Yookee',
         description: 'Smart blind controller',
         fromZigbee: [fz.cover_position_tilt, fz.battery],
-        toZigbee: [tz.cover_state, tz.cover_position_tilt],
+        toZigbee: [tz.cover_state, tz.cover_position_invert],
+        meta: {coverInverted: true},
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg', 'closuresWindowCovering']);
