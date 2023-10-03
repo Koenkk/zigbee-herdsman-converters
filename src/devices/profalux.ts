893fa578581d1303741b9e3194377af4a0441a65
Sven337 <sven337@users.noreply.github.com>
2023-10-03 10:10:46 +0200

    feat(add): MOT-C1ZxxC/F (#5788)
    * Profalux covers: switch recent ones to using cluster 0x102, add MAI-ZTP20C remote
    
    This change references remote model MAI-ZTP20C.
    More importantly, it switches the control of covers to using the dedicated ClosuresWindowCovering (0x102) cluster for the covers that support it.
    Sniffing Zigbee traffic of my remotes show that the remotes use this cluster to control the covers, and not 0x8 as currently implemented.
    The compliance document for the Profalux motors at https://csa-iot.org/csa_product/ac-motor-for-window-coverings/ also lists that cluster and does NOT list 0x6/0x8.
    
    So I think the correct way to control these covers is 0x102, and certainly it's the way that seems to work with my shutters (all bought in 2023).
    
    Older revisions of these motors, which did not expose a zigbeeModel field, did not expose 0x102 either and so these use the pre-existing fingerprint to keep using cluster 0x8.
    
    * Profalux: fix state reporting for newer covers
    
    * Profalux: lint
    
    * Profalux: lint more
    
    * Profalux: NSAV061_old
    
    * Profalux: s/NSAV061_old/NSAV061_v1/
    
    * Profalux covers: improve model names
    
    The recent models we know are MOT-C1Zxxy, where "xx" is the torque (06 10 20 30Nm) and y appears to be a version (C or F seen in the wild).
    Update the model number accordingly, as "NSAV061" is the name of the after sales documentation file, not the proper model number.
    
    Keep using NSAV061_v1 for the older version, since we have no confirmation of its actual model number and it doesn't appear to even report it over Zigbee (to be confirmed).
    I suspect it is MOT-C1ZxxA.
    
    * Profalux: rename older model back to "NSAV061"
    
    I hope one day to prove that its model name is MOT-C1Z06A

--- src/devices/profalux.ts
+++ src/devices/profalux.ts
@@ -18,11 +18,42 @@ const definitions: Definition[] = [
         exposes: [],
     },
     {
+        // Recent covers are matched by Zigbee model. Their remotes communicate
+        // using cluster 0x102 "closuresWindowCovering", so use that.
+        // 06/10/20/30 is the torque in Nm. 20/30 have not been seen but
+        // extracted from Profalux documentation. C/F seems to be a version. D
+        // and E have not been seen in the while. I suspect A is the earlier
+        // model covered below, NSAV061.
         zigbeeModel: [
             'MOT-C1Z06C\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
-            'MOT-C1Z10F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
+            'MOT-C1Z10C\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
+            'MOT-C1Z20C\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
+            'MOT-C1Z30C\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
             'MOT-C1Z06F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
+            'MOT-C1Z10F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
+            'MOT-C1Z20F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
+            'MOT-C1Z30F\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000',
         ],
+        model: 'MOT-C1ZxxC/F',
+        vendor: 'Profalux',
+        description: 'Cover',
+        fromZigbee: [fz.command_cover_close, fz.command_cover_open, fz.cover_position_tilt],
+        toZigbee: [tz.cover_state, tz.cover_position_tilt],
+        exposes: [e.cover_position()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(2);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['closuresWindowCovering']);
+            await reporting.currentPositionLiftPercentage(endpoint);
+        },
+        endpoint: (device) => {
+            return {default: 2};
+        },
+    },
+    {
+        // Identify older covers based on their fingerprint. These do not
+        // expose closuresWindowCovering and need to use genLevelCtrl
+        // instead. Sniffing a remote would be welcome to confirm that this
+        // is the right thing to do.
         fingerprint: [{manufacturerID: 4368, endpoints: [{ID: 1, profileID: 260, deviceID: 512,
             inputClusters: [0, 3, 4, 5, 6, 8, 10, 21, 256, 64544, 64545], outputClusters: [3, 64544]}]}],
         model: 'NSAV061',
@@ -34,12 +65,15 @@ const definitions: Definition[] = [
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genLevelCtrl']);
-            await reporting.brightness(endpoint);
+            await reporting.currentPositionLiftPercentage(endpoint);
         },
     },
     {
-        zigbeeModel: ['MAI-ZTP20F'],
-        model: 'MAI-ZTP20F',
+        // Newer remotes. These expose a bunch of things but they are bound to
+        // the cover and don't seem to communicate with the coordinator, so
+        // nothing is likely to be doable in Z2M.
+        zigbeeModel: ['MAI-ZTP20F', 'MAI-ZTP20C'],
+        model: 'MAI-ZTP20',
         vendor: 'Profalux',
         description: 'Cover remote',
         fromZigbee: [],
