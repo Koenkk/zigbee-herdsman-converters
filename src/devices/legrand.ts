4b50e3e089e6c6e103a07e9b164f5b56d8b20c68
apages2 <apages2@free.fr>
2023-10-03 22:06:50 +0200

    Add support for 067776A Shutter sw with level control

--- src/devices/legrand.ts
+++ src/devices/legrand.ts
@@ -639,15 +639,14 @@ const definitions: Definition[] = [
         fromZigbee: [fz.identify, fz.ignore_basic_report, fz.legrand_binary_input_moving, fz.cover_position_tilt, fz.legrand_led_in_dark],
         toZigbee: [tz.cover_state, tz.cover_position_tilt, tz.legrand_identify, tz.legrand_settingEnableLedInDark],
         ota: ota.zigbeeOTA,
-		configure: async (device, coordinatorEndpoint, logger) => {
+	configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
             await reporting.bind(endpoint, coordinatorEndpoint, ['genBinaryInput', 'closuresWindowCovering', 'genIdentify']);
             await reporting.currentPositionLiftPercentage(endpoint);
         },
-		exposes: [e.cover_position(), 
-			e.action(['moving', 'identify', '']),
-            e.binary('led_in_dark', ea.ALL, 'ON', 'OFF').withDescription(`Enables the LED when the power socket is turned off, allowing
-			    to see the switch in the dark`),
+	exposes: [e.cover_position(), 
+	    e.action(['moving', 'identify', '']),
+            e.binary('led_in_dark', ea.ALL, 'ON', 'OFF').withDescription(`Enables the LED when the power socket is turned off, allowing to see the switch in the dark`),
             e.enum('identify', ea.SET, ['blink']).withDescription(`Blinks the built-in LED to make it easier to find the device`)],
     },
 ];
