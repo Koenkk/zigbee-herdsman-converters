9e77ae4dd879df7321bee58d87bd1451e6636eb3
yuanxin <200831913@qq.com>
2023-10-09 23:59:03 +0800

    Repair: eslint error

--- src/lib/tuya.ts
+++ src/lib/tuya.ts
@@ -864,7 +864,7 @@ const tuyaTz = {
             'medium_motion_detection_sensitivity', 'small_detection_distance', 'small_detection_sensitivity', 'fan_mode', 'deadzone_temperature',
             'eco_mode', 'max_temperature_limit', 'min_temperature_limits', 'manual_modes',
             'medium_motion_detection_sensitivity', 'small_detection_distance', 'small_detection_sensitivity', 'switch_type',
-            'ph_max', 'ph_min', 'ec_max', 'ec_min', 'orp_max', 'orp_min', 'free_chlorine_max', 'free_chlorine_min','target_distance',
+            'ph_max', 'ph_min', 'ec_max', 'ec_min', 'orp_max', 'orp_min', 'free_chlorine_max', 'free_chlorine_min','target_distance'
         ],
         convertSet: async (entity, key, value, meta) => {
             // A set converter is only called once; therefore we need to loop
