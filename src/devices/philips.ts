36b9c51e3f9ca4c1eeebcd8d461cf8f6115d5944
Koen Kanters <koenkanters94@gmail.com>
2023-10-09 19:58:26 +0200

    Update philips.ts

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -2510,7 +2510,7 @@ const definitions: Definition[] = [
         zigbeeModel: ['LCS002'],
         model: '577262',
         vendor: 'Philips',
-        description: 'Hue White and Color Ambiance PAR38 Outdoor Spotlight',
+        description: 'Hue White and Color Ambiance PAR38 outdoor spotlight',
         extend: philips.extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
     {
