b3f1b2af2c8fdb20f7bbb04320d018a0464a0757
Ryan Gregg <rgregg@users.noreply.github.com>
2023-10-08 21:12:46 -0700

    Add Philips Hue PAR38 Outdoor Spotlight bulb support

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -2507,6 +2507,13 @@ const definitions: Definition[] = [
         extend: philips.extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
     },
     {
+        zigbeeModel: ['LCS002'],
+        model: '577262',
+        vendor: 'Philips',
+        description: 'Hue White and Color Ambiance PAR38 Outdoor Spotlight',
+        extend: philips.extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500]}),
+    },
+    {
         zigbeeModel: ['1742730P7', '1742830P7'],
         model: '1742830P7',
         vendor: 'Philips',
