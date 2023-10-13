623045479aa62c0ca08ad88c1b26e2b086336c3a
sj8023ld <sunjian62@gmail.com>
2023-10-14 02:16:32 +0800

    feat(add): KK-LP-Q02D, KK-LP-Q03D, KK-QD-Y01w (#6278)
    * Add Konke Light Years Switch
    
    Device Link
    https://www.ikonke.com/parameter/lp_q01w/
    https://www.ikonke.com/parameter/lp_q02w/
    https://www.ikonke.com/parameter/lp_q03w/
    
    * Update konke.ts
    
    * Update konke.ts
    
    * Add Y01W spotlight driver
    
    * Update konke.ts
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/konke.ts
+++ src/devices/konke.ts
@@ -25,17 +25,6 @@ const fzLocal = {
 
 const definitions: Definition[] = [
     {
-        zigbeeModel: ['3AFE292000068621'],
-        model: 'KK-LP-Q01D',
-        vendor: 'Konke',
-        description: '1 gang switch',
-        extend: extend.switch(),
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint1 = device.getEndpoint(1);
-            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff']);
-        },
-    },
-    {
         zigbeeModel: ['3AFE170100510001', '3AFE280100510001'],
         model: '2AJZ4KPKEY',
         vendor: 'Konke',
@@ -152,6 +141,62 @@ const definitions: Definition[] = [
         fromZigbee: [fz.ias_contact_alarm_1, fz.battery],
         toZigbee: [],
         exposes: [e.contact(), e.battery_low(), e.tamper(), e.battery(), e.battery_voltage()],
+    },
+    {
+        zigbeeModel: ['3AFE292000068621'],
+        model: 'KK-LP-Q01D',
+        vendor: 'Konke',
+        description: 'Light years switch 1 gang',
+        extend: extend.switch(),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
+        zigbeeModel: ['3AFE292000068622'],
+        model: 'KK-LP-Q02D',
+        vendor: 'Konke',
+        description: 'Light years switch 2 gangs',
+        extend: extend.switch(),
+        exposes: [e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2')],
+        endpoint: (device) => {
+            return {l1: 1, l2: 2};
+        },
+        meta: {multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff']);
+            const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
+        zigbeeModel: ['3AFE292000068623'],
+        model: 'KK-LP-Q03D',
+        vendor: 'Konke',
+        description: 'Light years switch 3 gangs',
+        extend: extend.switch(),
+        exposes: [e.switch().withEndpoint('l1'), e.switch().withEndpoint('l2'), e.switch().withEndpoint('l3')],
+        endpoint: (device) => {
+            return {l1: 1, l2: 2, l3: 3};
+        },
+        meta: {multiEndpoint: true},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff']);
+            const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['genOnOff']);
+            const endpoint3 = device.getEndpoint(3);
+            await reporting.bind(endpoint3, coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
+        zigbeeModel: ['3AFE2610010C0021'],
+        model: 'KK-QD-Y01w',
+        vendor: 'Konke',
+        description: 'Spotlight driver (cw mode)',
+        extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 370]}),
     },
 ];
 
