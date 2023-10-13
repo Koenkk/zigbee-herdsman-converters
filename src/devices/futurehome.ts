5dfc16915bcdbe801c15733686c785cd75834a61
Erlend Torsvik <erlend.torsvik@gmail.com>
2023-10-13 17:11:42 +0200

    strict Lint correction

--- src/devices/futurehome.ts
+++ src/devices/futurehome.ts
@@ -19,29 +19,29 @@ const definition: Definition[] = [
             // Here you should put all functionality that your device exposes
             e.binary('state', ea.STATE_SET, 'ON', 'OFF')
                     .withDescription('On/off state of the switch'),
-                e.child_lock(),
-                e.window_detection(),
-                e.away_mode(),
-                e.climate()
-                    .withSetpoint('current_heating_setpoint', 5, 35, 1, ea.STATE_SET)
-                    .withLocalTemperature(ea.STATE)
-                    .withLocalTemperatureCalibration(-9, 9, 1, ea.STATE_SET)
-                    .withSystemMode(['heat', 'auto'], ea.STATE_SET)
-                    .withRunningState(['idle', 'heat'], ea.STATE),
-                e.temperature_sensor_select(['internal', 'external', 'both']),
-                e.numeric('external_temperature', ea.STATE)
-                    .withUnit('°C')
-                    .withDescription('Current temperature measured on the external sensor (floor)'),
-                e.numeric('hysteresis', ea.STATE_SET)
-                    .withDescription('The difference between the temperature at which the thermostat switches off, ' +
-                    'and the temperature at which it switches on again.')
-                    .withValueMin(1)
-                    .withValueMax(9),
-                e.numeric('max_temperature_protection', ea.STATE_SET)
-                    .withUnit('°C')
-                    .withDescription('Max guarding temperature')
-                    .withValueMin(20)
-                    .withValueMax(95),
+            e.child_lock(),
+            e.window_detection(),
+            e.away_mode(),
+            e.climate()
+                .withSetpoint('current_heating_setpoint', 5, 35, 1, ea.STATE_SET)
+                .withLocalTemperature(ea.STATE)
+                .withLocalTemperatureCalibration(-9, 9, 1, ea.STATE_SET)
+                .withSystemMode(['heat', 'auto'], ea.STATE_SET)
+                .withRunningState(['idle', 'heat'], ea.STATE),
+            e.temperature_sensor_select(['internal', 'external', 'both']),
+            e.numeric('external_temperature', ea.STATE)
+                .withUnit('°C')
+                .withDescription('Current temperature measured on the external sensor (floor)'),
+            e.numeric('hysteresis', ea.STATE_SET)
+                .withDescription('The difference between the temperature at which the thermostat switches off, ' +
+                'and the temperature at which it switches on again.')
+                .withValueMin(1)
+                .withValueMax(9),
+            e.numeric('max_temperature_protection', ea.STATE_SET)
+                .withUnit('°C')
+                .withDescription('Max guarding temperature')
+                .withValueMin(20)
+                .withValueMax(95),
         ],
     },
 ];
