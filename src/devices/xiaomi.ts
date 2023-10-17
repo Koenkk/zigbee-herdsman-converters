2a58fbef89b13d28334c49ef6fa60f4562646b64
koenkk <koenkanters94@gmail.com>
2023-10-17 20:33:42 +0200

    feat: Expose calibrate option for Xiaomi SRTS-A01 https://github.com/Koenkk/zigbee2mqtt.io/pull/2274

--- src/devices/xiaomi.ts
+++ src/devices/xiaomi.ts
@@ -3199,7 +3199,8 @@ const definitions: Definition[] = [
                 .withPreset(['manual', 'away', 'auto']).setAccess('preset', ea.ALL),
             e.temperature_sensor_select(['internal', 'external']).withAccess(ea.ALL),
             e.binary('calibrated', ea.STATE, true, false)
-                .withDescription('Is the valve calibrated'),
+                .withDescription('Indicates if this valve is calibrated, use the calibrate option to calibrate'),
+            e.enum('calibrate', ea.STATE_SET, ['calibrate']).withDescription('Calibrates the valve'),
             e.child_lock().setAccess('state', ea.ALL),
             e.window_detection().setAccess('state', ea.ALL),
             e.binary('window_open', ea.STATE, true, false),
