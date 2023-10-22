aff00c2a7e4c0b57500f5494d202e17b5ab9077a
Fabian Mangold <fabian.mangold@fajode.net>
2023-10-22 14:57:31 +0200

    feat(add): Added 067776(A) specifics to library
    * Added array of supported calibration modes
    * Added FZ/TZ to support calibration modes
    * Added 067776(A) specific helper methods

--- src/lib/legrand.ts
+++ src/lib/legrand.ts
@@ -1,5 +1,22 @@
-import {Fz, OnEvent, Tz} from '../lib/types';
+import {Fz, Tz, OnEvent} from '../lib/types';
+import * as exposes from './exposes';
 import * as utils from '../lib/utils';
+const e = exposes.presets;
+const ea = exposes.access;
+
+const shutterCalibrationModes = {
+    'Classic (NLLV)': {ID: 0, onlyNLLV: true},
+    'Specific (NLLV)': {ID: 1, onlyNLLV: true},
+    'Up/Down/Stop': {ID: 2, onlyNLLV: false},
+    'Temporal': {ID: 3, onlyNLLV: false},
+    'Venetian (BSO)': {ID: 4, onlyNLLV: false},
+};
+
+const getApplicableCalibrationModes = (isNLLVSwitch: boolean) => {
+    return Object.fromEntries(Object.entries(shutterCalibrationModes)
+        .filter((e) => isNLLVSwitch ? true : e[1].onlyNLLV === false)
+        .map((e) => [e[0], e[1].ID]));
+};
 
 export const readInitialBatteryState: OnEvent = async (type, data, device, options) => {
     if (['deviceAnnounce'].includes(type)) {
@@ -19,6 +36,20 @@ export const tzLegrand = {
             return {state: {'auto_mode': value}};
         },
     } as Tz.Converter,
+    calibration_mode: (isNLLVSwitch: boolean) => {
+        return {
+            key: ['calibration_mode'],
+            convertSet: async (entity, key, value, meta) => {
+                const applicableModes = getApplicableCalibrationModes(isNLLVSwitch);
+                utils.validateValue(value, Object.keys(applicableModes));
+                const idx = applicableModes[value as string];
+                await entity.write('closuresWindowCovering', {'tuyaMotorReversal': idx});
+            },
+            convertGet: async (entity, key, meta) => {
+                await entity.read('closuresWindowCovering', [0xf002]);
+            },
+        } as Tz.Converter;
+    },
 };
 
 export const fzLegrand = {
@@ -36,4 +67,44 @@ export const fzLegrand = {
             }
         },
     } as Fz.Converter,
+    calibration_mode: (isNLLVSwitch: boolean) => {
+        return {
+            cluster: 'closuresWindowCovering',
+            type: ['attributeReport', 'readResponse'],
+            convert: (model, msg, publish, options, meta) => {
+                const attr = 'tuyaMotorReversal';
+                if (msg.data.hasOwnProperty(attr)) {
+                    const idx = msg.data[attr];
+                    const applicableModes = getApplicableCalibrationModes(isNLLVSwitch);
+                    utils.validateValue(idx, Object.values(applicableModes));
+                    const calMode = utils.getKey(applicableModes, idx);
+                    return {calibration_mode: calMode};
+                }
+            },
+        } as Fz.Converter;
+    },
+};
+
+export const _067776 = {
+    getCover: () => {
+        const c = e.cover_position();
+        if (c.hasOwnProperty('features')) {
+            c.features.push(new exposes.Numeric('tilt', ea.ALL)
+                .withValueMin(0).withValueMax(100)
+                .withValueStep(25)
+                .withPreset('Closed', 0, 'Vertical')
+                .withPreset('25 %', 25, '25%')
+                .withPreset('50 %', 50, '50%')
+                .withPreset('75 %', 75, '75%')
+                .withPreset('Open', 100, 'Horizontal')
+                .withUnit('%')
+                .withDescription('Tilt percentage of that cover'));
+        }
+        return c;
+    },
+    getCalibrationModes: (isNLLVSwitch: boolean) => {
+        const modes = getApplicableCalibrationModes(isNLLVSwitch);
+        return e.enum('calibration_mode', ea.ALL, Object.keys(modes))
+            .withDescription('Defines the calibration mode of the switch. (Caution: Changing modes requires a recalibration of the shutter switch!)');
+    },
 };
