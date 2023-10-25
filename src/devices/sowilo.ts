3fae160274eb5b8bbdd37462f7da209c3dfe430a
Koen Kanters <koenkanters94@gmail.com>
2023-10-25 20:07:43 +0200

    Update sowilo.ts

--- src/devices/sowilo.ts
+++ src/devices/sowilo.ts
@@ -6,7 +6,7 @@ const definition: Definition[] = [
         zigbeeModel: ['L258'],
         model: 'L258',
         vendor: 'Sowilo DS',
-        description: 'Heimdall Pro. 5 channel RGBWW controller.',
+        description: 'Heimdall Pro 5 channel RGBWW controller',
         extend: extend.light_onoff_brightness_colortemp_color({supportsHueAndSaturation: true, colorTempRange: [150, 575]}),
         meta: {turnsOffAtBrightness1: true},
     },
