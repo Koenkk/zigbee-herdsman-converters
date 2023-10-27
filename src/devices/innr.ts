1c78360dedd727f6287e2da6eadd52c3681e445e
koenkk <koenkanters94@gmail.com>
2023-10-27 20:38:58 +0200

    feat(add): FL 122 C https://github.com/Koenkk/zigbee2mqtt/issues/19440

--- src/devices/innr.ts
+++ src/devices/innr.ts
@@ -723,6 +723,14 @@ const definitions: Definition[] = [
         meta: {applyRedFix: true, turnsOffAtBrightness1: true},
     },
     {
+        zigbeeModel: ['FL 122 C'],
+        model: 'FL 122 C',
+        vendor: 'Innr',
+        description: 'Flex light colour LED strip 2m, 1440lm, RGBW',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [100, 350], supportsHueAndSaturation: true}),
+        meta: {applyRedFix: true, turnsOffAtBrightness1: true},
+    },
+    {
         zigbeeModel: ['OSL 130 C'],
         model: 'OSL 130 C',
         vendor: 'Innr',
