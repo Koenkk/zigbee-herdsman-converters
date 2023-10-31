e6b42355c07abd23f294d1ca0bc4e148c344b9f8
André Santana Ferreira <andre.991@hotmail.com>
2023-10-31 09:05:51 -0300

    Fix TS0004 Model with backlight

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -3744,7 +3744,7 @@ const definitions: Definition[] = [
         fingerprint: [
             {modelID: 'TS0004', manufacturerName: '_TZ3000_nivavasg'},
         ],
-        model: 'TS0004',
+        model: 'TS0004_switch_4_gang_with_backlight',
         vendor: 'TuYa',
         description: 'Smart light switch - 4 gang with backlight and neutral wire',
         extend: tuya.extend.switch({powerOnBehavior2: true, backlightModeOffOn: true, endpoints: ['l1', 'l2', 'l3', 'l4']}),
