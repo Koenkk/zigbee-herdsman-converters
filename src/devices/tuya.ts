2f67d2bcbd5deb64d5030e3d117e0d3b414352c5
Koen Kanters <koenkanters94@gmail.com>
2023-11-03 16:42:15 +0100

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -913,7 +913,6 @@ const definitions: Definition[] = [
             tuya.whitelabel('Moes', 'ZLD-RCW_1', 'RGB+CCT Zigbee LED controller', ['_TZ3000_7hcgjxpc']),
             tuya.whitelabel('Moes', 'ZLD-RCW_2', 'RGB+CCT LED bulb', ['_TZ3210_rcggc0ys']),
             tuya.whitelabel('Moes', 'ZB-LZD10-RCW', '10W RGB+CCT Smart Downlight', ['_TZ3210_s9lumfhn']),
-
         ],
         extend: tuya.extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 500], noConfigure: true}),
         configure: async (device, coordinatorEndpoint, logger) => {
