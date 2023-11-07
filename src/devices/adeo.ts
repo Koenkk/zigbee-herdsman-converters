ac341cf9a7cf33006f4e360a647e0dbbc79f78f1
koenkk <koenkanters94@gmail.com>
2023-11-07 21:51:26 +0100

    feat(add): 84845509 https://github.com/Koenkk/zigbee2mqtt/issues/19610

--- src/devices/adeo.ts
+++ src/devices/adeo.ts
@@ -180,6 +180,13 @@ const definitions: Definition[] = [
         extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 370]}),
     },
     {
+        zigbeeModel: ['ZBEK-29'],
+        model: '84845509',
+        vendor: 'ADEO',
+        description: 'ENKI LEXMAN Gdansk LED panel',
+        extend: extend.light_onoff_brightness_colortemp_color({colorTempRange: [153, 370]}),
+    },
+    {
         zigbeeModel: ['ZBEK-28'],
         model: 'PEZ1-042-1020-C1D1',
         vendor: 'ADEO',
