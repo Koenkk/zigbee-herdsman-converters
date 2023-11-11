2fa877f7b3f4f166d5310d5496d7b3bd14895d9e
wkwhat <51859454+wkwhat@users.noreply.github.com>
2023-11-10 21:29:40 -0500

    Update adurosmart.ts

--- src/devices/adurosmart.ts
+++ src/devices/adurosmart.ts
@@ -136,6 +136,13 @@ const definitions: Definition[] = [
         description: 'Eria tunable white A19 smart bulb',
         extend: extend.light_onoff_brightness_colortemp_color({supportsHueAndSaturation: true, colorTempRange: [153, 500]}),
     },
+    {
+        zigbeeModel: ['AD-ColorTemperature3001'],
+        model: '81814',
+        vendor: 'AduroSmart',
+        description: 'ERIA Tunable White BR30 light bulb',
+        extend: extend.light_onoff_brightness_colortemp({colorTempRange: [153, 500]}),
+    },
 ];
 
 module.exports = definitions;
