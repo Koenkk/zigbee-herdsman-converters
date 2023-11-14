337faddf78eaaaf34297f9718463112739edc03f
soversk <32395626+soversk@users.noreply.github.com>
2023-11-14 13:42:32 +0100

    Fix indent

--- src/devices/ctm.ts
+++ src/devices/ctm.ts
@@ -133,12 +133,12 @@ const fzLocal = {
             if (data.hasOwnProperty(0x0403)) { // Sensor
                 const sensorModeLookup = {
                     0: 'air', 1: 'floor', 2: 'external', 3: 'regulator', 4: 'mv_air', 5: 'mv_external', 6: 'mv_regulator'};
-		result.sensor = utils.getFromLookup(data[0x0403], sensorModeLookup);
+                    result.sensor = utils.getFromLookup(data[0x0403], sensorModeLookup);
             }
-			if (data.hasOwnProperty('elkoSensor')) { // Sensor
+            if (data.hasOwnProperty('elkoSensor')) { // Sensor
                 const sensorModeLookup = {
                     0: 'air', 1: 'floor', 2: 'external', 3: 'regulator', 4: 'mv_air', 5: 'mv_external', 6: 'mv_regulator'};
-		result.sensor = utils.getFromLookup(data['elkoSensor'], sensorModeLookup);
+                result.sensor = utils.getFromLookup(data['elkoSensor'], sensorModeLookup);
             }
             if (data.hasOwnProperty(0x0405)) { // Regulator mode
                 result.regulator_mode = data[0x0405] ? 'regulator' : 'thermostat';
