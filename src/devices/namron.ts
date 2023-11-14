584117bb01656b4dbfcc6a7194e52213cab99825
xses79 <76107252+xses79@users.noreply.github.com>
2023-11-14 09:40:20 +0100

    Update namron.ts

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -886,31 +886,32 @@ const definitions: Definition[] = [
     },
     {
         zigbeeModel: ['4512766', '4512767'],
-        model: '4512766 / 4512767',
-        vendor: 'NAMRON',
-        description: 'Namron Zigbee smart plug',
-        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.ignore_basic_report, fz.device_temperature],
-        toZigbee: [tz.on_off],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement', 'seMetering', 'genDeviceTempCfg']);
-            endpoint.saveClusterAttributeKeyValue('seMetering', {divisor: 100000, multiplier: 1});
-            endpoint.saveClusterAttributeKeyValue('haElectricalMeasurement', {
-                acVoltageMultiplier: 1, acVoltageDivisor: 1, acCurrentMultiplier: 1, acCurrentDivisor: 1000, acPowerMultiplier: 1,
-                acPowerDivisor: 1,
-            });
-            try {
-                await reporting.deviceTemperature(endpoint);
-                await reporting.currentSummDelivered(endpoint);
-                await reporting.rmsVoltage(endpoint, {change: 5});
-                await reporting.rmsCurrent(endpoint, {change: 50});
-                await reporting.activePower(endpoint, {change: 10});
-                await endpoint.read('genOnOff', 'onOff');
+    model: '4512766 / 4512767',
+    vendor: 'NAMRON',
+    description: 'Namron Zigbee smart plug',
+    fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.ignore_basic_report, fz.device_temperature],
+    toZigbee: [tz.on_off],
+    configure: async (device, coordinatorEndpoint, logger) => {
+        const endpoint = device.getEndpoint(1);
+        await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement', 'seMetering', 'genDeviceTempCfg']);
+        endpoint.saveClusterAttributeKeyValue('seMetering', {divisor: 100000, multiplier: 1});
+        endpoint.saveClusterAttributeKeyValue('haElectricalMeasurement', {
+            acVoltageMultiplier: 1, acVoltageDivisor: 1, acCurrentMultiplier: 1, acCurrentDivisor: 1000, acPowerMultiplier: 1,
+            acPowerDivisor: 1,
+        });
+        try {
+            await reporting.deviceTemperature(endpoint);
+            await reporting.currentSummDelivered(endpoint);
+            await reporting.rmsVoltage(endpoint, {change: 5});
+            await reporting.rmsCurrent(endpoint, {change: 50});
+            await reporting.activePower(endpoint, {change: 10});
+        } catch (error) {}
+        await endpoint.read('genOnOff', ['onOff']);
         
-        },
-        options: [exposes.options.measurement_poll_interval()],
-        exposes: [e.switch(), e.power(), e.current(), e.voltage(),
-            e.energy(), e.device_temperature().withDescription('Device temperature'),],
+    },
+    options: [exposes.options.measurement_poll_interval()],
+    exposes: [e.switch(), e.power(), e.current(), e.voltage(),
+        e.energy(), e.device_temperature().withDescription('Device temperature'),],
     },
     {
         zigbeeModel: ['4512750'],
