3e059882434ea70b6c9d0f20c858f93df56582d0
gdybvik <82578873+gdybvik@users.noreply.github.com>
2023-11-17 11:55:50 +0100

    Changed incorrect references in definitions for new device Smart Plug 16A

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -157,9 +157,9 @@ const definitions: Definition[] = [
         model: '4512767',
         vendor: 'Namron',
         description: 'Zigbee Smart Plug 16A',
-        fromZigbee: [fromZigbee_1.default.metering, fromZigbee_1.default.electrical_measurement],
+        fromZigbee: [fz.default.metering, fz.default.electrical_measurement],
         exposes: [e.power(), e.current(), e.voltage(), e.energy()],
-        extend: extend_1.default.switch(),
+        extend: extend.default.switch(),
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1) || device.getEndpoint(3);
             const binds = [
