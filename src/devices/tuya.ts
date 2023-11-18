a908724b0b28268fef4a3f68f85bbcb22cc2d69f
Jordan H <jordan@hrvy.uk>
2023-11-18 13:26:47 +0000

    Add support for _TZE204_e5m9c5hl

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6670,6 +6670,42 @@ const definitions: Definition[] = [
             ],
         },
     },
+    {
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_e5m9c5hl']),
+        model: 'WZ-M100-Z',
+        vendor: 'Wenzhi',
+        description: 'Human presence sensor',
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        onEvent: tuya.onEventSetTime,
+        configure: tuya.configureMagicPacket,
+        exposes: [
+            e.illuminance_lux(), e.presence(),
+            e.numeric('radar_sensitivity', ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1),
+            e.numeric('distance', ea.STATE).withValueMin(0).withValueMax(10)
+                .withDescription('target distance').withValueStep(0.01).withUnit('m'),
+            e.numeric('fading_time', ea.STATE_SET).withValueMax(1500).withValueMin(5).withValueStep(5).withUnit('s')
+                .withDescription('presence timeout'),
+            e.numeric('far_detection', ea.SET).withValueMin(0).withValueMax(9.5)
+                .withDescription('maximum detection range').withValueStep(0.1).withUnit('m'),
+            e.numeric('near_detection', ea.SET).withValueMin(0).withValueMax(9.5)
+                .withDescription('minimum detection range').withValueStep(0.1).withUnit('m'),
+            e.numeric('detection_delay', ea.STATE_SET).withValueMin(0).withValueMax(10).withValueStep(0.1).withUnit('s')
+                .withDescription('detection delay'),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [105, 'presence', tuya.valueConverter.trueFalse1],
+                [104, 'illuminance_lux', tuya.valueConverter.raw],
+                [106, 'radar_sensitivity', tuya.valueConverter.raw],
+                [109, 'distance', tuya.valueConverter.divideBy100],
+                [110, 'fading_time', tuya.valueConverter.divideBy10],
+                [107, 'far_detection', tuya.valueConverter.divideBy100],
+                [108, 'near_detection', tuya.valueConverter.divideBy100],
+                [111, 'detection_delay', tuya.valueConverter.divideBy10],
+            ]
+        }
+    },
 ];
 
 module.exports = definitions;
