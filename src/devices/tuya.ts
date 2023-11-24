189c661d7837210743de3e649374cbba20b25d48
Sébastien Luttringer <seblu@seblu.net>
2023-11-24 01:15:26 +0100

    feat(add): Detect `_TZE204_iaeejhvf` as TuYa MTG275-ZB-RL

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6449,7 +6449,7 @@ const definitions: Definition[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_sbyx0lm6', '_TZE204_clrdrnya', '_TZE204_dtzziy1e']),
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_sbyx0lm6', '_TZE204_clrdrnya', '_TZE204_dtzziy1e', '_TZE204_iaeejhvf']),
         model: 'MTG075-ZB-RL',
         vendor: 'TuYa',
         description: '5.8G human presence sensor with relay',
