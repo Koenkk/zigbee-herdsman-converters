1f6066db2c90a2f95401ffef4dbfeb490f8f8b47
koenkk <koenkanters94@gmail.com>
2023-11-26 21:24:43 +0100

    Add light

--- src/lib/modernExtend.ts
+++ src/lib/modernExtend.ts
@@ -1,29 +1,88 @@
-import * as exposes from './exposes';
-import legacyExtend from './extend';
+import tz from '../converters/toZigbee';
+import fz from '../converters/fromZigbee';
 import {Fz, Tz, ModernExtend, Range} from './types';
-import {Enum, Numeric, access} from './exposes';
-import {KeyValue} from './types';
-import {getFromLookupByValue, isString, getFromLookup, getEndpointName, assertNumber, postfixWithEndpointName} from './utils';
+import {Enum, Numeric, access, Light, Binary, presets as exposePresets} from './exposes';
+import {KeyValue, Configure, Expose, DefinitionMeta} from './types';
+import {configure as lightConfigure} from './light';
+import {getFromLookupByValue, isString, getFromLookup, getEndpointName, assertNumber, postfixWithEndpointName, isObject} from './utils';
 
-interface SwitchArgs {disablePowerOnBehavior?: boolean}
+interface SwitchArgs {powerOnBehavior?: boolean}
 function switch_(args?: SwitchArgs): ModernExtend {
-    args = {disablePowerOnBehavior: false, ...args};
-    const result = legacyExtend.switch(args);
-    return {...result, isModernExtend: true};
+    args = {powerOnBehavior: true, ...args};
+
+    const exposes: Expose[] = [exposePresets.switch()];
+    const fromZigbee: Fz.Converter[] = [fz.on_off, fz.ignore_basic_report];
+    const toZigbee: Tz.Converter[] = [tz.on_off];
+
+    if (args.powerOnBehavior) {
+        exposes.push(exposePresets.power_on_behavior(['off', 'on', 'toggle', 'previous']));
+        fromZigbee.push(fz.power_on_behavior);
+        toZigbee.push(tz.power_on_behavior);
+    }
+
+    return {exposes, fromZigbee, toZigbee, isModernExtend: true};
 }
 export {switch_ as switch};
 
-interface LightOnOffBrightnessColortempColorArgs {
-    disableEffect?: boolean, supportsHueAndSaturation?: boolean, disableColorTempStartup?: boolean, preferHueAndSaturation?: boolean,
-    disablePowerOnBehavior?: boolean, colorTempRange?: Range,
+interface LightArgs {
+    effect?: boolean, powerOnBehaviour?: boolean, colorTemp?: {startup?: boolean, range: Range},
+    color?: boolean | {modes: ('xy' | 'hs')[]}
 }
-export function lightOnOffBrightnessColortempColor(args?: LightOnOffBrightnessColortempColorArgs): ModernExtend {
-    args = {
-        disableEffect: false, supportsHueAndSaturation: false, disableColorTempStartup: false, preferHueAndSaturation: false,
-        disablePowerOnBehavior: false, ...args,
+export function light(args?: LightArgs): ModernExtend {
+    args = {effect: true, powerOnBehaviour: true, ...args};
+    if (args.colorTemp) {
+        args.colorTemp = {startup: true, ...args.colorTemp};
+    }
+    const argsColor = args.color ? false : {modes: ['xy'] satisfies ('xy' | 'hs')[], ...(isObject(args.color) ? args.color : {})};
+
+    let lightExpose = new Light().withBrightness();
+
+    const fromZigbee: Fz.Converter[] = [fz.on_off, fz.brightness, fz.ignore_basic_report, fz.level_config];
+    const toZigbee: Tz.Converter[] = [
+        tz.light_onoff_brightness, tz.ignore_transition, tz.level_config, tz.ignore_rate, tz.light_brightness_move, tz.light_brightness_step,
+    ];
+    const meta: DefinitionMeta = {};
+
+    if (args.colorTemp || argsColor) {
+        fromZigbee.push(fz.color_colortemp);
+        toZigbee.push(tz.light_color_colortemp, tz.light_color_mode, tz.light_color_options);
+    }
+
+    if (args.colorTemp) {
+        lightExpose = lightExpose.withColorTemp(args.colorTemp.range);
+        toZigbee.push(tz.light_colortemp_move, tz.light_colortemp_step);
+        if (args.colorTemp.startup) {
+            toZigbee.push(tz.light_colortemp_startup);
+            lightExpose = lightExpose.withColorTempStartup(args.colorTemp.range);
+        }
+    }
+
+    if (argsColor) {
+        lightExpose = lightExpose.withColor(argsColor.modes);
+        toZigbee.push(tz.light_hue_saturation_move, tz.light_hue_saturation_step);
+        if (argsColor.modes.includes('hs')) {
+            meta.supportsHueAndSaturation = true;
+        }
+    }
+
+    const exposes: Expose[] = [lightExpose];
+
+    if (args.effect) {
+        exposes.push(exposePresets.effect());
+        toZigbee.push(tz.effect);
+    }
+
+    if (args.powerOnBehaviour) {
+        exposes.push(exposePresets.power_on_behavior(['off', 'on', 'toggle', 'previous']));
+        fromZigbee.push(fz.power_on_behavior);
+        toZigbee.push(tz.power_on_behavior);
+    }
+
+    const configure: Configure = async (device, coordinatorEndpoint, logger) => {
+        await lightConfigure(device, coordinatorEndpoint, logger, true);
     };
-    const result = legacyExtend.light_onoff_brightness_colortemp_color(args);
-    return {...result, isModernExtend: true};
+
+    return {exposes, fromZigbee, toZigbee, configure, isModernExtend: true};
 }
 
 interface EnumLookupArgs {
@@ -120,7 +179,7 @@ export function binary(args: BinaryArgs): ModernExtend {
     const {name, valueOn, valueOff, cluster, attribute, description, zigbeeCommandOptions, readOnly, endpoint} = args;
     const attributeKey = isString(attribute) ? attribute : attribute.id;
 
-    let expose = new exposes.Binary(name, readOnly ? access.STATE_GET : access.ALL, valueOn[0], valueOff[0]).withDescription(description);
+    let expose = new Binary(name, readOnly ? access.STATE_GET : access.ALL, valueOn[0], valueOff[0]).withDescription(description);
     if (endpoint) expose = expose.withEndpoint(endpoint);
 
     const fromZigbee: Fz.Converter[] = [{
