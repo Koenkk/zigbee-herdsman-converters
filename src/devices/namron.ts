adc02346a0b8d2af98f4c52db67a2314f05f866b
Geir Råness <geirra@users.noreply.github.com>
2023-11-27 19:34:00 +0100

    feat(add): 4512773 (#6568)
    * Add support for Namron kanalbryter 4512772/4512773
    
    * add usage of whitelabel
    
    * use english desc instead of product name
    
    ---------
    
    Co-authored-by: Geir Råness <git@winning.no>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -943,6 +943,25 @@ const definitions: Definition[] = [
         },
     },
     {
+        zigbeeModel: ['4512772', '4512773'],
+        model: '4512773',
+        vendor: 'Namron',
+        description: 'Zigbee 8 channel switch black',
+        whiteLabel: [{vendor: 'Namron', model: '4512772', description: 'Zigbee 8 channel switch white', fingerprint: [{modelID: '4512772'}]}],
+        fromZigbee: [fz.battery, fz.command_on, fz.command_off, fz.command_move, fz.command_stop],
+        toZigbee: [],
+        meta: {multiEndpoint: true},
+        exposes: [e.battery(), e.action([
+            'on_l1', 'off_l1', 'brightness_move_up_l1', 'brightness_move_down_l1', 'brightness_stop_l1',
+            'on_l2', 'off_l2', 'brightness_move_up_l2', 'brightness_move_down_l2', 'brightness_stop_l2',
+            'on_l3', 'off_l3', 'brightness_move_up_l3', 'brightness_move_down_l3', 'brightness_stop_l3',
+            'on_l4', 'off_l4', 'brightness_move_up_l4', 'brightness_move_down_l4', 'brightness_stop_l4'])],
+        endpoint: (device) => {
+            return {l1: 1, l2: 2, l3: 3, l4: 4};
+        },
+        ota: ota.zigbeeOTA,
+    },
+    {
         zigbeeModel: ['4512768'],
         model: '4512768',
         vendor: 'Namron',
