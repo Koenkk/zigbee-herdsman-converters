bbc21c2f880407005d26d9e0bad650dad1845aaa
Marco Starker <marco.starker@gmx.net>
2023-11-28 19:30:19 +0100

    fix: Fixes for TuYa `_TZE204_5toc8efa` (#6581)
    * fix for _TZE204_5toc8efa
    
    * fix for _TZE204_5toc8efa

--- src/devices/moes.ts
+++ src/devices/moes.ts
@@ -117,7 +117,7 @@ const definitions: Definition[] = [
             tuya.whitelabel('Moes', 'BHT-002/BHT-006', 'Smart heating thermostat', ['_TZE204_aoclfnxz']),
         ],
         exposes: [e.child_lock(), e.deadzone_temperature(), e.max_temperature_limit(), e.min_temperature_limit(),
-            e.climate().withSetpoint('current_heating_setpoint', 5, 35, 1, ea.STATE_SET)
+            e.climate().withSetpoint('current_heating_setpoint', 5, 35, 0.5, ea.STATE_SET)
                 .withLocalTemperature(ea.STATE).withLocalTemperatureCalibration(-30, 30, 0.1, ea.STATE_SET)
                 .withSystemMode(['off', 'heat'], ea.STATE_SET).withRunningState(['idle', 'heat', 'cool'], ea.STATE)
                 .withPreset(['hold', 'program']),
