38f71e292a9cb11f9e3a606183abd9d219235924
root <root@home.local>
2023-12-06 22:22:26 +0200

    FIX: linting

--- src/lib/legacy.ts
+++ src/lib/legacy.ts
@@ -3832,7 +3832,7 @@ const fromZigbee1 = {
                 };
             case dataPoints.state: // Thermostat on standby = OFF, running = ON
                 if (model.model === 'BAC-002-ALZB') {
-                    const stateLookup = {0: 'cool', 1: 'heat', 2: 'fan_only'};
+                    const stateLookup: KeyValueAny = {'0': 'cool', '1': 'heat', '2': 'fan_only'};
                     return {system_mode: stateLookup[value]};
                 } else {
                     return {system_mode: value ? 'heat' : 'off'};
@@ -6786,35 +6786,35 @@ const toZigbee2 = {
         convertSet: async (entity, key, value, meta) => {
             const hold = value === 'hold' ? 0 : 1;
             const schedule = value === 'program' ? 0 : 1;
-            
-            const stateLookup = {0: 'cool', 1: 'heat', 2: 'fan_only'};
-            
+
+            // const stateLookup: KeyValueAny = {'0': 'cool', '1': 'heat', '2': 'fan_only'};
+
             await sendDataPointEnum(entity, dataPoints.moesHold, hold);
             await sendDataPointEnum(entity, dataPoints.moesScheduleEnable, schedule);
 
-             switch (value) {
-                case 'off':
-                     await sendDataPointBool(entity, tuya.dataPoints.moesSsystemMode, 0);
-                    break;
-                case 'cool':
-                    // turn on
-                    await sendDataPointBool(entity, tuya.dataPoints.moesSsystemMode, 1);
-    
-                    await sendDataPointEnum(entity, tuya.dataPoints.tvMode, 0);
-                    break;
-                case 'heat':
-                    // turn on
-                    await sendDataPointBool(entity, tuya.dataPoints.moesSsystemMode, 1);
-    
-                    await sendDataPointEnum(entity, tuya.dataPoints.tvMode, 1);
-                    break;
-                case 'fan_only':
-                    // turn on
-                    await sendDataPointBool(entity, tuya.dataPoints.moesSsystemMode, 1);
-    
-                    await sendDataPointEnum(entity, tuya.dataPoints.tvMode, 2);
-                    //await sendDataPointEnum(entity, tuya.dataPoints.moesScheduleEnable, 0);
-                    break;
+            switch (value) {
+            case 'off':
+                await sendDataPointBool(entity, dataPoints.moesSsystemMode, 0);
+                break;
+            case 'cool':
+                // turn on
+                await sendDataPointBool(entity, dataPoints.moesSsystemMode, 1);
+
+                await sendDataPointEnum(entity, dataPoints.tvMode, 0);
+                break;
+            case 'heat':
+                // turn on
+                await sendDataPointBool(entity, dataPoints.moesSsystemMode, 1);
+
+                await sendDataPointEnum(entity, dataPoints.tvMode, 1);
+                break;
+            case 'fan_only':
+                // turn on
+                await sendDataPointBool(entity, dataPoints.moesSsystemMode, 1);
+
+                await sendDataPointEnum(entity, dataPoints.tvMode, 2);
+                // await sendDataPointEnum(entity, dataPoints.moesScheduleEnable, 0);
+                break;
             }
         },
     } satisfies Tz.Converter,
