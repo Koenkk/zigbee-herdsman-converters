4eca004a86bd62cc630b3647d6590f3eee0013cf
Koen Kanters <koenkanters94@gmail.com>
2023-12-07 19:40:42 +0100

    Update ikea.ts

--- src/devices/ikea.ts
+++ src/devices/ikea.ts
@@ -1066,7 +1066,7 @@ const definitions: Definition[] = [
         extend: tradfriExtend.light_onoff_brightness_colortemp(),
     },
     {
-        zigbeeModel: ['TRADFRI_bulb_GU10_WS_345lm', 'TRADFRI bulb GU10 WW 345lm'],
+        zigbeeModel: ['TRADFRI_bulb_GU10_WS_345lm'],
         model: 'LED2106R3',
         vendor: 'IKEA',
         description: 'TRADFRI LED bulb GU10 345 lumen, dimmable, white spectrum',
