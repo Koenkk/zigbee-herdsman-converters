581a1f4357134f304aac4f3aa39a9933355c12d1
koenkk <koenkanters94@gmail.com>
2023-12-08 20:16:40 +0100

    fix(ignore): Refactor Philips lights to modernExtend

--- scripts/modernExtendRefactor.ts
+++ scripts/modernExtendRefactor.ts
@@ -17,7 +17,7 @@ project.getSourceFiles().forEach((sourceFile) => {
 
     let changed = true;
     let save = false;
-    const type = 'light';
+    const type = 'philipsLight';
     while (changed) {
         changed = false;
         const definitions = sourceFile.getVariableStatementOrThrow('definitions').getDescendantsOfKind(SyntaxKind.ObjectLiteralExpression);
@@ -33,17 +33,22 @@ project.getSourceFiles().forEach((sourceFile) => {
             const toZigbee = childs.find((c) => c.getFirstChildByKind(SyntaxKind.Identifier)?.getText() === 'toZigbee');
             const meta = childs.find((c) => c.getFirstChildByKind(SyntaxKind.Identifier)?.getText() === 'meta');
 
-            if ((extend?.getFullText().includes('extend: extend.light_onoff_brightness_colortemp(') ||
-                extend?.getFullText().includes('extend: extend.light_onoff_brightness_colortemp_color(') ||
-                extend?.getFullText().includes('extend: extend.light_onoff_brightness_color(')) &&
+            if ((extend?.getFullText().includes('extend: philips.extend.light_onoff_brightness_colortemp(') ||
+                extend?.getFullText().includes('extend: philips.extend.light_onoff_brightness_colortemp_color(') ||
+                extend?.getFullText().includes('extend: philips.extend.light_onoff_brightness(') ||
+                extend?.getFullText().includes('extend: philips.extend.light_onoff_brightness_colortemp_color_gradient(') ||
+                extend?.getFullText().includes('extend: philips.extend.light_onoff_brightness_color(')) &&
                 !fromZigbee && !toZigbee && !configure && !exposes) {
                 console.log(`Handling ${model?.getFullText().trim()}`);
                 const newOpts: {[s: string]: unknown} = {};
                 if (extend.getFullText().includes('_colortemp(')) {
                     newOpts.colorTemp = {range: null};
                 }
-                if (extend.getFullText().includes('_color(')) {
+                if (extend.getFullText().includes('_color(') || extend.getFullText().includes('_color_')) {
                     newOpts.color = true;
+                }
+                if (extend.getFullText().includes('_gradient(')) {
+                    newOpts.gradient = true;
                 }
                 let opts = extend?.getFullText().split('(')[1].slice(0, -1).trim();
                 if (opts) {
@@ -61,9 +66,14 @@ project.getSourceFiles().forEach((sourceFile) => {
                             newOpts.powerOnBehaviour = !value;
                         } else if (key === 'disableEffect') {
                             newOpts.effect = !value;
+                        } else if (key === 'disableHueEffects') {
+                            newOpts.hueEffect = !value;
                         } else if (key === 'supportsHueAndSaturation') {
                             // @ts-expect-error
                             newOpts.color = {...newOpts.color, modes: ['xy', 'hs']};
+                        } else if (key === 'extraEffects') {
+                            // @ts-expect-error
+                            newOpts.gradient = {...newOpts.gradient, extraEffects: value};
                         } else {
                             throw new Error(`Unsupported ${key} - ${value}`);
                         }
