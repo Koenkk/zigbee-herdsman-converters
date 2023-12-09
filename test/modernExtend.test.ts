e7de50bb6ba1580df820f342deb634609a98ea8d
koenkk <koenkanters94@gmail.com>
2023-12-09 13:09:41 +0100

    fix test

--- test/modernExtend.test.ts
+++ test/modernExtend.test.ts
@@ -206,7 +206,7 @@ describe('ModernExtend', () => {
                 'color_temp_move', 'color_temp_step', 'color_temp_startup', 'hue_move', 'saturation_move', 'hue_step', 'saturation_step', 'power_on_behavior',
                 'hue_power_on_behavior', 'hue_power_on_brightness', 'hue_power_on_color_temperature', 'hue_power_on_color', 'effect', 'gradient_scene', 'gradient'
             ],
-            exposes: ['effect', 'effect', 'gradient', 'gradient_scene', 'light(state,brightness,color_temp,color_temp_startup,color_xy,color_hs)', 'linkquality', 'power_on_behavior'],
+            exposes: ['effect', 'gradient', 'gradient_scene', 'light(state,brightness,color_temp,color_temp_startup,color_xy,color_hs)', 'linkquality', 'power_on_behavior'],
             bind: ['manuSpecificPhilips2'],
             read: [
                 ['lightingColorCtrl', ['colorCapabilities']],
