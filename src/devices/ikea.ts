abe682d1422276918ad069671c3ea6e134772994
thatslolo <73950246+thatslolo@users.noreply.github.com>
2023-12-10 20:36:58 +0100

    feat(add): E2213 (#6574)
    * initial commit of e2213
    
    * added missing trailing comma
    
    * heimanSpecificScenes
    based on symfonisk remote
    
    * corrected indentation of comment
    
    * some more cosmetics
    
    * when in comma-dangling-town…
    
    * changed to tradfriButton cluster
    
    * trailing space removed
    indentation cleaned up
    
    * cleaned up the cleanup
    
    * changed cluster of Symfonisk to tradfriButton
    
    * Update ikea.ts
    
    * removed buttons prefix
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/ikea.ts
+++ src/devices/ikea.ts
@@ -297,6 +297,22 @@ const fzLocal = {
             return {action: `dots_${button}_${action}`};
         },
     } satisfies Fz.Converter,
+    ikea_dots_click_v2_somrig: {
+        cluster: 'tradfriButton',
+        type: ['commandAction1', 'commandAction2', 'commandAction3', 'commandAction4', 'commandAction6'],
+        convert: (model, msg, publish, options, meta) => {
+            const button = utils.getFromLookup(msg.endpoint.ID, {1: '1', 2: '2'});
+            const lookup = {
+                commandAction1: 'initial_press',
+                commandAction2: 'long_press',
+                commandAction3: 'short_release',
+                commandAction4: 'long_release',
+                commandAction6: 'double_press',
+            };
+            const action = utils.getFromLookup(msg.type, lookup);
+            return {action: `${button}_${action}`};
+        },
+    } satisfies Fz.Converter,
     ikea_volume_click: {
         cluster: 'genLevelCtrl',
         type: 'commandMoveWithOnOff',
@@ -1204,10 +1220,10 @@ const definitions: Definition[] = [
             const endpoint3 = device.getEndpoint(3);
             await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl', 'genPollCtrl']);
             if (endpoint2) {
-                await reporting.bind(endpoint2, coordinatorEndpoint, ['heimanSpecificScenes']);
+                await reporting.bind(endpoint2, coordinatorEndpoint, ['tradfriButton']);
             }
             if (endpoint3) {
-                await reporting.bind(endpoint3, coordinatorEndpoint, ['heimanSpecificScenes']);
+                await reporting.bind(endpoint3, coordinatorEndpoint, ['tradfriButton']);
             }
             await reporting.batteryVoltage(endpoint1);
         },
@@ -1256,6 +1272,27 @@ const definitions: Definition[] = [
             await reporting.occupancy(endpoint2);
             await reporting.bind(endpoint3, cordinatorEndpoint, ['msIlluminanceMeasurement']);
             await reporting.illuminance(endpoint3);
+        },
+    },
+    {
+        zigbeeModel: ['SOMRIG shortcut button'],
+        model: 'E2213',
+        vendor: 'IKEA',
+        description: 'SOMRIG shortcut button',
+        fromZigbee: [fz.battery, fzLocal.ikea_dots_click_v2_somrig],
+        toZigbee: [tz.battery_percentage_remaining],
+        exposes: [
+            e.battery().withAccess(ea.STATE_GET), e.action(['dots_1_initial_press',
+                'dots_2_initial_press', 'dots_1_long_press', 'dots_2_long_press',
+                'dots_1_short_release', 'dots_2_short_release', 'dots_1_long_release']),
+        ],
+        ota: ota.tradfri,
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['tradfriButton', 'genPollCtrl']);
+            await reporting.bind(endpoint2, coordinatorEndpoint, ['tradfriButton']);
+            await reporting.batteryVoltage(endpoint1);
         },
     },
     {
