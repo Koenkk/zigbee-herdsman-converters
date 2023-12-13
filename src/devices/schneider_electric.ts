9513f4b638b15b6bbff5f58dafc5f3c80dd8bc07
koenkk <koenkanters94@gmail.com>
2023-12-13 19:58:02 +0100

    fix: Fix `Cannot read properties of undefined` for Schneider Electric A9MEM1570 https://github.com/Koenkk/zigbee2mqtt/issues/20193

--- src/devices/schneider_electric.ts
+++ src/devices/schneider_electric.ts
@@ -173,7 +173,7 @@ const fzLocal = {
             if (rxAfterTx) {
                 // Send Schneider specific ACK to make PowerTag happy
                 // @ts-expect-error
-                const networkParameters = await msg.device.zh.constructor.adapter.getNetworkParameters();
+                const networkParameters = await msg.device.constructor.adapter.getNetworkParameters();
                 const payload = {
                     options: 0b000,
                     tempMaster: msg.data.gppNwkAddr,
