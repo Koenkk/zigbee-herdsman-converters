d30beab239d3f5882ecf50b1acd79bfa9de00484
XS400DOHC <63352197+xs400dohc@users.noreply.github.com>
2023-12-14 15:13:11 +0100

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -4929,7 +4929,7 @@ const definitions: Definition[] = [
                 .withDescription('Alarm humidity min'),
         ],
     },
-    {fingerprint: [{ modelID: 'TS0601', manufacturerName: '_TZE200_vvmbj46n' }],
+    {fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_vvmbj46n'}],
         model: 'TH05Z',
         vendor: 'TuYa',
         description: 'Zigbee Temperature & Humidity Sensor with clock',
@@ -4956,8 +4956,7 @@ const definitions: Definition[] = [
             e.numeric('max_humidity', ea.STATE_SET).withUnit('%').withValueMin(0).withValueMax(100)
                 .withDescription('Alarm humidity max'),
             e.numeric('min_humidity', ea.STATE_SET).withUnit('%').withValueMin(0).withValueMax(100)
-                .withDescription('Alarm humidity min'),
-        ],},
+                .withDescription('Alarm humidity min'),],},
     {
         fingerprint: [{modelID: 'TS004F', manufacturerName: '_TZ3000_kjfzuycl'},
             {modelID: 'TS004F', manufacturerName: '_TZ3000_ja5osu5g'}],
