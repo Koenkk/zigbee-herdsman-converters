2c43c5a308da796d7f50631fb253a9a748f09292
Geir Råness <git@winning.no>
2023-12-15 22:06:17 +0100

    fix reporting and binding for heatit zig dim 250

--- src/devices/heatit.ts
+++ src/devices/heatit.ts
@@ -7,7 +7,14 @@ const definitions: Definition[] = [
         model: '1444420',
         vendor: 'Heatit',
         description: 'Zig Dim 250W',
-        extend: extend.light_onoff_brightness({disablePowerOnBehavior: true}),
+        extend: extend.light_onoff_brightness({noConfigure: true, disablePowerOnBehavior: true}),
+        configure: async (device, coordinatorEndpoint, logger) => {
+            await extend.light_onoff_brightness().configure(device, coordinatorEndpoint, logger);
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint);
+            await reporting.brightness(endpoint);
+        },
     },
 ];
 
