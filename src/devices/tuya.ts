316515f68613ff738de926e7ba05e5c005a0aa5f
pixel::doc <pixeldoc2000@users.noreply.github.com>
2023-12-16 23:14:30 +0100

    Tuya TS0205 - fix Battery Status Report
    Added bind of genPowerCfg and reporting for batteryPercentageRemaining.

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -535,13 +535,24 @@ const definitions: Definition[] = [
         exposes: [e.gas(), e.tamper()],
     },
     {
+        fingerprint: [{modelID: 'TS0205', manufacturerName: '_TZ3210_up3pngle'}],
         zigbeeModel: ['TS0205'],
         model: 'TS0205',
         vendor: 'TuYa',
         description: 'Smoke sensor',
-        whiteLabel: [{vendor: 'Tesla Smart', model: 'TSL-SEN-SMOKE'}],
+        whiteLabel: [
+            {vendor: 'Tesla Smart', model: 'TSL-SEN-SMOKE'},
+            {vendor: 'Dongguan Daying Electornics Technology', model: 'YG400A (Zigbee)'},
+        ],
         fromZigbee: [fz.ias_smoke_alarm_1, fz.battery, fz.ignore_basic_report],
         toZigbee: [],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            try {
+                const endpoint = device.getEndpoint(1);
+                await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+                await reporting.batteryPercentageRemaining(endpoint);
+            } catch (error) {/* Fails for some*/}
+        },
         exposes: [e.smoke(), e.battery_low(), e.tamper(), e.battery()],
     },
     {
