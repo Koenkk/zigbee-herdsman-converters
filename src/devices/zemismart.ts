f3e2492b49e6145db50bfabe340f5ac00a70f1bf
Koen Kanters <koenkanters94@gmail.com>
2023-12-21 07:56:25 +0100

    Update zemismart.ts

--- src/devices/zemismart.ts
+++ src/devices/zemismart.ts
@@ -110,6 +110,8 @@ const definitions: Definition[] = [
         model: 'ZM25RX-08/30',
         vendor: 'Zemismart',
         description: 'Tubular motor',
+        // mcuVersionResponse spsams: https://github.com/Koenkk/zigbee2mqtt/issues/19817
+        onEvent: tuya.onEvent({respondToMcuVersionResponse: false}),
         configure: tuya.configureMagicPacket,
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
