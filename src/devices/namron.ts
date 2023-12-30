2b40227d0e11ce557a657328c7c76215029633ab
Einar Jørgen Haraldseid <einar@haraldseid.net>
2023-12-30 19:57:51 +0100

    Add support for Namron Door Sensor 4512762

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -833,6 +833,20 @@ const definitions: Definition[] = [
         },
     },
     {
+        zigbeeModel: ['4512762'],
+        model: '4512762',
+        vendor: 'Namron AS',
+        description: 'Zigbee Door Sensor',
+        fromZigbee: [fz.ias_contact_alarm_1, fz.battery, fz.ias_contact_alarm_1_report],
+        toZigbee: [],
+        exposes: [e.contact(), e.battery(), e.battery_voltage()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint);
+            await reporting.batteryVoltage(endpoint);
+    },
+    {
         zigbeeModel: ['4512763'],
         model: '4512763',
         vendor: 'Namron',
