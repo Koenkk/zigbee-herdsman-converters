cfc2937f612b687080b4b2557a167bbb02a78c46
Juan Jose Chong <juchong@users.noreply.github.com>
2024-01-15 10:53:31 -0800

    fix(detect): Detect `_TZE200_9p5xmj5r` as Hiladuo B09M3R35GC (#6893)
    * Added support for Hiladuo blinds sold on Amazon
    
    * Added ASIN (Amazon Standard Identification Number)
    
    * Update tuya.ts
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -2804,7 +2804,7 @@ const definitions: Definition[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_68nvbio9', '_TZE200_pw7mji0l', '_TZE200_cf1sl3tj', '_TZE200_nw1r9hp6']),
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_68nvbio9', '_TZE200_pw7mji0l', '_TZE200_cf1sl3tj', '_TZE200_nw1r9hp6', '_TZE200_9p5xmj5r']),
         model: 'TS0601_cover_3',
         vendor: 'TuYa',
         description: 'Cover motor',
@@ -2824,6 +2824,7 @@ const definitions: Definition[] = [
             tuya.whitelabel('Zemismart', 'ZM16EL-03/33', 'Cover motor', ['_TZE200_68nvbio9']),
             tuya.whitelabel('Zemismart', 'ZM25EL', 'Cover motor', ['_TZE200_pw7mji0l']),
             tuya.whitelabel('Zemismart', 'ZM85EL-2Z', 'Roman Rod I type U curtains track', ['_TZE200_cf1sl3tj', '_TZE200_nw1r9hp6']),
+            tuya.whitelabel('Hiladuo', 'B09M3R35GC', 'Motorized roller shade', ['_TZE200_9p5xmj5r']),
         ],
         meta: {
             // All datapoints go in here
