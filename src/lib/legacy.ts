3840ea49154cc3585b28bc2fcd60213e07247688
koenkk <koenkanters94@gmail.com>
2024-01-18 22:04:57 +0100

    fix: Fix negative temperature values for TuYa TS0601_air_quality_sensor incorrect https://github.com/Koenkk/zigbee2mqtt/issues/20774

--- src/lib/legacy.ts
+++ src/lib/legacy.ts
@@ -3999,7 +3999,7 @@ const fromZigbee1 = {
             const value = getDataValue(dpValue);
             switch (dp) {
             case dataPoints.tuyaSabTemp:
-                return {temperature: value / 10};
+                return {temperature: (value > 0x2000 ? value - 0xFFFF : value) / 10};
             case dataPoints.tuyaSabHumidity:
                 return {humidity: value / 10};
                 // DP22: Smart Air Box: Formaldehyd, Smart Air Housekeeper: co2
