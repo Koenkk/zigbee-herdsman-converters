70aaa054c9762bb334cc90233f7a8ce8349c8e8d
Ben <44917135+BenJamesAndo@users.noreply.github.com>
2024-01-24 15:55:09 +1000

    Update tuya.ts
    Add buzzer_feedback and rf_pairing as datapoints

--- src/lib/tuya.ts
+++ src/lib/tuya.ts
@@ -1120,7 +1120,7 @@ const tuyaTz = {
             'ph_max', 'ph_min', 'ec_max', 'ec_min', 'orp_max', 'orp_min', 'free_chlorine_max', 'free_chlorine_min', 'target_distance',
             'illuminance_treshold_max', 'illuminance_treshold_min', 'presence_illuminance_switch', 'light_switch', 'light_linkage',
             'indicator_light', 'find_switch', 'detection_method', 'sensor', 'hysteresis', 'max_temperature_protection', 'display_brightness',
-            'screen_orientation', 'regulator_period', 'regulator_set_point', 'upper_stroke_limit', 'middle_stroke_limit', 'lower_stroke_limit',
+            'screen_orientation', 'regulator_period', 'regulator_set_point', 'upper_stroke_limit', 'middle_stroke_limit', 'lower_stroke_limit', 'buzzer_feedback', 'rf_pairing',
         ],
         convertSet: async (entity, key, value, meta) => {
             // A set converter is only called once; therefore we need to loop
