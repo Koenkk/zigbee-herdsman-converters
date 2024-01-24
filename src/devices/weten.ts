6f3c3a05ce9ec4b07cbbca5fa6a52848e8e37cce
Ben <44917135+BenJamesAndo@users.noreply.github.com>
2024-01-24 16:02:44 +1000

    Update weten.ts
    Remove non-working datapoints. Improve names and descriptions.

--- src/devices/weten.ts
+++ src/devices/weten.ts
@@ -16,33 +16,25 @@ const definitions: Definition[] = [
         extend: extend.switch(),
         fromZigbee: [fz.on_off, fz.ignore_basic_report, fz.ignore_time_read],
     },
-const tzDatapoints = {
-    ...tuya.tz.datapoints,
-        key: ['PC_Power', 'shutdown_reset', 'buzzer', 'child_lock', 'RF', 'pair_RF_remote']
-}
     {
         fingerprint: tuya.fingerprint('TS0601', ['_TZE204_6fk3gewc']),
         model: 'PCI E',
         vendor: 'WETEN',
         description: 'PC switch',
         fromZigbee: [tuya.fz.datapoints],
-        toZigbee: [tzDatapoints],
+        toZigbee: [tuya.tz.datapoints],
         exposes: [
-            e.binary('PC_Power', ea.STATE_SET, 'ON', 'OFF').withDescription('PC Power'),		
-	    e.binary('shutdown_reset', ea.STATE_SET, 'ON', 'OFF').withDescription('Shutdown or Reset? Does not seem to actually do anything'),
-            e.binary('buzzer', ea.STATE_SET, 'ON', 'OFF').withDescription('Buzzer on means no buzzer noise'),
-	    e.binary('child_lock', ea.STATE_SET, 'LOCK', 'UNLOCK').withDescription('Child safety lock'),
-	    e.binary('RF', ea.STATE_SET, 'ON', 'OFF').withDescription('To enable or disable the use of RF remote control, does not seem to actually work'),
-	    e.binary('pair_RF_remote', ea.STATE_SET, 'ON', 'OFF').withDescription('To pair a RF 433 remote, such as the one supplied')
+            e.binary('state', ea.STATE_SET, 'ON', 'OFF').withDescription('PC Power'),		
+            e.binary('buzzer_feedback', ea.STATE_SET, 'ON', 'OFF').withDescription('ON means no buzzer noise'),
+	    e.child_lock('child_lock', ea.STATE_SET, 'LOCK', 'UNLOCK'),
+	    e.binary('rf_pairing', ea.STATE_SET, 'ON', 'OFF').withDescription('Enables/disables RF 433 remote pairing mode')
         ],
         meta: {
             tuyaDatapoints: [
-                [1, 'PC_Power', tuya.valueConverter.onOff],
-            	[105, 'shutdown_reset', tuya.valueConverter.onOff],
-            	[104, 'buzzer', tuya.valueConverter.onOff],
+                [1, 'state', tuya.valueConverter.onOff],
+            	[104, 'buzzer_feedback', tuya.valueConverter.onOff],
             	[106, 'child_lock', tuya.valueConverter.lockUnlock],
-            	[102, 'RF', tuya.valueConverter.onOff],
-            	[103, 'pair_RF_remote', tuya.valueConverter.onOff],
+            	[103, 'rf_pairing', tuya.valueConverter.onOff],
             ],
         },
     },
