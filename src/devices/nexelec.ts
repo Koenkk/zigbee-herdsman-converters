e659e859311ab853ffa8a875a0534886c37a7ba4
ElouanHocineNexelec <elouan.hocine@nexelec.fr>
2024-01-26 11:46:07 +0100

    nexelec.ts forgot curly braces

--- src/devices/nexelec.ts
+++ src/devices/nexelec.ts
@@ -2,11 +2,13 @@ import {Definition} from '../lib/types';
 import {temperature, humidity, co2, batteryPercentage, identify} from '../lib/modernExtend';
 
 const definitions: Definition[] = [
+    {
     zigbeeModel: ['Air Quality Sensor Nexelec'],
     model: 'Open\'R',
     vendor: 'Nexelec',
     description: 'Measure CO2, Temperature and Relative Humidity',
     extend: [temperature(), humidity(), co2(), batteryPercentage(), identify()],
+    },
 ];
 
 export default definitions;
