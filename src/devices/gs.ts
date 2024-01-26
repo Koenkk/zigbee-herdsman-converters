f58f1bd35b2775353194b6bd4e281c7071bd04ff
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-01-26 23:06:26 +0300

    Convert to modernExtend

--- src/devices/gs.ts
+++ src/devices/gs.ts
@@ -1,9 +1,8 @@
 import {Definition} from '../lib/types';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
-import tz from '../converters/toZigbee';
 import * as reporting from '../lib/reporting';
-import {light} from '../lib/modernExtend';
+import {light, onOff, electricityMeter} from '../lib/modernExtend';
 
 const e = exposes.presets;
 
@@ -34,24 +33,7 @@ const definitions: Definition[] = [
         model: 'SKHMP30-I1',
         description: 'Smart metering plug',
         vendor: 'GS',
-        fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering],
-        exposes: [e.switch(), e.power(), e.current(), e.voltage(), e.energy()],
-        toZigbee: [tz.on_off],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'haElectricalMeasurement', 'seMetering']);
-            await reporting.onOff(endpoint);
-            await reporting.rmsVoltage(endpoint);
-            await reporting.rmsCurrent(endpoint);
-            await reporting.activePower(endpoint);
-            await reporting.readMeteringMultiplierDivisor(endpoint);
-            await reporting.currentSummDelivered(endpoint);
-            endpoint.saveClusterAttributeKeyValue('haElectricalMeasurement', {
-                acVoltageMultiplier: 1, acVoltageDivisor: 100,
-                acCurrentMultiplier: 1, acCurrentDivisor: 100,
-                acPowerMultiplier: 1, acPowerDivisor: 10,
-            });
-        },
+        extend: [onOff({powerOnBehavior: false}), electricityMeter()],
     },
 ];
 
