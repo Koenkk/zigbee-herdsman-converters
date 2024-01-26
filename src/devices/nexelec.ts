8f0265deb9626593ef5745b34a3d02db13e04ba7
ElouanHocineNexelec <elouan.hocine@nexelec.fr>
2024-01-26 11:43:09 +0100

    Update nexelec.ts to comply with modernExtend

--- src/devices/nexelec.ts
+++ src/devices/nexelec.ts
@@ -1,35 +1,12 @@
 import {Definition} from '../lib/types';
-import * as exposes from '../lib/exposes';
-import fz from '../converters/fromZigbee';
-import tz from '../converters/toZigbee';
-import * as reporting from '../lib/reporting';
-const e = exposes.presets;
+import {temperature, humidity, co2, batteryPercentage, identify} from '../lib/modernExtend';
+
 const definitions: Definition[] = [
-    zigbeeModel: ['Air Quality Sensor Nexelec'], 
-        model: 'Open\'R', 
-        vendor: 'Nexelec', 
-        description: 'Measures CO2, Temperature and Relative Humidity', 
-        fromZigbee: [fz.battery, fz.temperature, fz.humidity, fz.co2], 
-        toZigbee: [], 
-        endpoint: (device) => {
-            const endpoint1 = device.getEndpoint(1);
-            const endpoint2 = device.getEndpoint(2);
-            const endpoint3 = device.getEndpoint(3);
-            return {'e1': 1, 'e2': 2, 'e3': 3};
-        },
-        exposes: [e.battery(), 
-                e.temperature().withEndpoint('e1'), 
-                e.humidity().withEndpoint('e1'), 
-                e.co2(),            
-                ],
-        meta: {multiEndpoint: true, battery: {dontDividePercentage: false}},
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint1 = device.getEndpoint(1);
-            const endpoint2 = device.getEndpoint(2);
-            const endpoint3 = device.getEndpoint(3);
-            await reporting.bind(endpoint1, coordinatorEndpoint, ['msTemperatureMeasurement', 'msRelativeHumidity', 'msCO2']);
-            await reporting.batteryPercentageRemaining(endpoint3);
-        },
+    zigbeeModel: ['Air Quality Sensor Nexelec'],
+    model: 'Open\'R',
+    vendor: 'Nexelec',
+    description: 'Measure CO2, Temperature and Relative Humidity',
+    extend: [temperature(), humidity(), co2(), batteryPercentage(), identify()],
 ];
 
 export default definitions;
