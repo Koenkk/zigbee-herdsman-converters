e122451393c87fffd63c1afd34b4bfe3b167ad68
koenkk <koenkanters94@gmail.com>
2024-02-01 21:10:36 +0100

    fix: Fix `battery` not exposed for Aqara MCCGQ12LM https://github.com/Koenkk/zigbee2mqtt/issues/19559

--- src/devices/lumi.ts
+++ src/devices/lumi.ts
@@ -3045,7 +3045,8 @@ const definitions: Definition[] = [
         description: 'Door and window sensor T1',
         fromZigbee: [fz.lumi_contact, lumi.fromZigbee.lumi_specific, fz.ias_contact_alarm_1],
         toZigbee: [],
-        exposes: [e.contact(), e.battery()],
+        exposes: [e.contact(), e.battery(), e.battery_voltage()],
+        meta: {battery: {voltageToPercentage: '3V_2850_3000'}},
         extend: [lumiZigbeeOTA()],
     },
     {
@@ -3072,7 +3073,6 @@ const definitions: Definition[] = [
         exposes: [e.battery(), e.battery_voltage(),
             e.action(['single', 'double', 'hold', 'release', 'start_rotating', 'rotation', 'stop_rotating']),
             e.enum('operation_mode', ea.ALL, ['event', 'command']).withDescription('Button mode'),
-            e.enum('action_rotation_button_state', ea.STATE, ['released', 'pressed']).withDescription('Button state during rotation'),
             e.numeric('action_rotation_angle', ea.STATE).withUnit('*').withDescription('Rotation angle'),
             e.numeric('action_rotation_angle_speed', ea.STATE).withUnit('*').withDescription('Rotation angle speed'),
             e.numeric('action_rotation_percent', ea.STATE).withUnit('%').withDescription('Rotation percent'),
