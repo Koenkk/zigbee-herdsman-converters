4d9df263b2abead775b8c69325704aeef691ddb6
koenkk <koenkanters94@gmail.com>
2024-02-03 12:20:07 +0100

    fix: Fix Light Solution 3137308/3137309 not exposing dimming functionality https://github.com/Koenkk/zigbee2mqtt/issues/21183

--- src/devices/lightsolutions.ts
+++ src/devices/lightsolutions.ts
@@ -25,10 +25,10 @@ const definitions: Definition[] = [
     },
     {
         fingerprint: [{modelID: 'Dimmer-Switch-ZB3.0', manufacturerName: 'Light Solutions'}],
-        model: '3137309',
+        model: '3137308/3137309',
         vendor: 'Light Solutions',
         description: 'Zigbee dimmer for wire',
-        extend: [onOff({powerOnBehavior: false})],
+        extend: [light({configureReporting: true})],
     },
 ];
 
