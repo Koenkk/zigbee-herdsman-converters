2310100e95abae5e6de3c9fc45e78e901bf0d877
Ivan Belokobylskiy <belokobylskij@gmail.com>
2024-02-06 23:31:53 +0400

    fix: Fix Xiaomi LYWSD03MMC integration (#7015)

--- src/devices/custom_devices_diy.ts
+++ src/devices/custom_devices_diy.ts
@@ -1,3 +1,4 @@
+import dataType from 'zigbee-herdsman/dist/zcl/definition/dataType';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
 import * as legacy from '../lib/legacy';
@@ -1282,6 +1283,7 @@ const definitions: Definition[] = [
         },
     },
     {
+        // https://github.com/devbis/z03mmc/
         zigbeeModel: ['LYWSD03MMC'],
         model: 'LYWSD03MMC',
         vendor: 'Custom devices (DiY)',
@@ -1289,9 +1291,7 @@ const definitions: Definition[] = [
         extend: [
             quirkAddEndpointCluster({
                 endpointID: 1,
-                outputClusters: [
-                    'hvacUserInterfaceCfg',
-                ],
+                outputClusters: [],
                 inputClusters: [
                     'genPowerCfg',
                     'msTemperatureMeasurement',
@@ -1302,8 +1302,6 @@ const definitions: Definition[] = [
             batteryPercentage(),
             temperature({reporting: {min: 10, max: 300, change: 10}}),
             humidity({reporting: {min: 10, max: 300, change: 50}}),
-            // Temperature display and show smile.
-            // For details, see: https://github.com/pvvx/ZigbeeTLc/issues/28#issue-2033984519
             enumLookup({
                 name: 'temperature_display_mode',
                 lookup: {'celsius': 0, 'fahrenheit': 1},
@@ -1312,77 +1310,100 @@ const definitions: Definition[] = [
                 description: 'The units of the temperature displayed on the device screen.',
             }),
             binary({
-                name: 'show_smile',
-                valueOn: ['HIDE', 1],
-                valueOff: ['SHOW', 0],
+                name: 'show_smiley',
+                valueOn: ['SHOW', 1],
+                valueOff: ['HIDE', 0],
                 cluster: 'hvacUserInterfaceCfg',
-                attribute: 'programmingVisibility',
-                description: 'Whether to show a smile on the device screen.',
+                attribute: {ID: 0x0010, type: dataType.boolean},
+                description: 'Whether to show a smiley on the device screen.',
             }),
-            // Setting offsets for temperature and humidity.
-            // For details, see: https://github.com/pvvx/ZigbeeTLc/issues/30
+            binary({
+                name: 'enable_display',
+                valueOn: ['ON', 1],
+                valueOff: ['OFF', 0],
+                cluster: 'hvacUserInterfaceCfg',
+                attribute: {ID: 0x0011, type: dataType.boolean},
+                description: 'Whether to turn display on/off.',
+            }),
             numeric({
                 name: 'temperature_calibration',
-                unit: 'C',
-                cluster: 'hvacUserInterfaceCfg',
-                attribute: {ID: 0x0100, type: 40},
-                valueMin: -12.7,
-                valueMax: 12.7,
-                valueStep: 0.1,
-                scale: 10,
-                description: 'The temperature calibration, in 0.1° steps. Requires v0.1.1.6 or newer.',
+                unit: '°C',
+                cluster: 'msTemperatureMeasurement',
+                attribute: {ID: 0x0010, type: dataType.int16},
+                valueMin: -100.0,
+                valueMax: 100.0,
+                valueStep: 0.01,
+                scale: 100,
+                description: 'The temperature calibration offset is set in 0.01° steps.',
             }),
             numeric({
                 name: 'humidity_calibration',
                 unit: '%',
-                cluster: 'hvacUserInterfaceCfg',
-                attribute: {ID: 0x0101, type: 40},
-                valueMin: -12.7,
-                valueMax: 12.7,
-                valueStep: 0.1,
-                scale: 10,
-                description: 'The humidity offset is set in 0.1 % steps. Requires v0.1.1.6 or newer.',
+                cluster: 'msRelativeHumidity',
+                attribute: {ID: 0x0010, type: dataType.int16},
+                valueMin: -100.0,
+                valueMax: 100.0,
+                valueStep: 0.01,
+                scale: 100,
+                description: 'The humidity calibration offset is set in 0.01 % steps.',
             }),
-            // Comfort parameters.
-            // For details, see: https://github.com/pvvx/ZigbeeTLc/issues/28#issuecomment-1855763432
             numeric({
                 name: 'comfort_temperature_min',
-                unit: 'C',
+                unit: '°C',
                 cluster: 'hvacUserInterfaceCfg',
-                attribute: {ID: 0x0102, type: 40},
-                valueMin: -127,
-                valueMax: 127,
-                description: 'Comfort parameters/Temperature minimum, in 1° steps. Requires v0.1.1.7 or newer.',
+                attribute: {ID: 0x0102, type: dataType.int16},
+                valueMin: -100.0,
+                valueMax: 100.0,
+                scale: 100,
+                description: 'Comfort parameters/Temperature minimum, in 0.01°C steps.',
             }),
             numeric({
                 name: 'comfort_temperature_max',
-                unit: 'C',
+                unit: '°C',
                 cluster: 'hvacUserInterfaceCfg',
-                attribute: {ID: 0x0103, type: 40},
-                valueMin: -127,
-                valueMax: 127,
-                description: 'Comfort parameters/Temperature maximum, in 1° steps. Requires v0.1.1.7 or newer.',
+                attribute: {ID: 0x0103, type: dataType.int16},
+                valueMin: -100.0,
+                valueMax: 100.0,
+                scale: 100,
+                description: 'Comfort parameters/Temperature maximum, in 0.01°C steps.',
             }),
             numeric({
                 name: 'comfort_humidity_min',
                 unit: '%',
                 cluster: 'hvacUserInterfaceCfg',
-                attribute: {ID: 0x0104, type: 32},
-                valueMin: 0,
-                valueMax: 100,
-                description: 'Comfort parameters/Humidity minimum, in 1% steps. Requires v0.1.1.7 or newer.',
+                attribute: {ID: 0x0104, type: dataType.uint16},
+                valueMin: 0.0,
+                valueMax: 100.0,
+                scale: 100,
+                description: 'Comfort parameters/Humidity minimum, in 0.01% steps.',
             }),
             numeric({
                 name: 'comfort_humidity_max',
                 unit: '%',
                 cluster: 'hvacUserInterfaceCfg',
-                attribute: {ID: 0x0105, type: 32},
-                valueMin: 0,
-                valueMax: 100,
-                description: 'Comfort parameters/Humidity maximum, in 1% steps. Requires v0.1.1.7 or newer.',
+                attribute: {ID: 0x0105, type: dataType.uint16},
+                valueMin: 0.0,
+                valueMax: 100.0,
+                scale: 100,
+                description: 'Comfort parameters/Humidity maximum, in 0.01% steps.',
             }),
         ],
         ota: ota.zigbeeOTA,
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            const bindClusters = ['msTemperatureMeasurement', 'msRelativeHumidity', 'genPowerCfg'];
+            await reporting.bind(endpoint, coordinatorEndpoint, bindClusters);
+            await reporting.temperature(endpoint, {min: 10, max: 300, change: 10});
+            await reporting.humidity(endpoint, {min: 10, max: 300, change: 50});
+            await reporting.batteryPercentageRemaining(endpoint);
+            try {
+                await endpoint.read('hvacThermostat', [0x0010, 0x0011, 0x0102, 0x0103, 0x0104, 0x0105]);
+                await endpoint.read('msTemperatureMeasurement', [0x0010]);
+                await endpoint.read('msRelativeHumidity', [0x0010]);
+            } catch (e) {
+                /* backward compatibility */
+            }
+        },
     },
     {
         zigbeeModel: ['MHO-C401N'],
