2da305f16d438bee4b325516d439c7c1ca9f23e7
Andrey Chernopyatov <dronablo@yandex.ru>
2024-02-08 18:45:06 +0300

    Add ROXIMO SZTH02 as TS0601_temperature_humidity_sensor_2

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -691,7 +691,7 @@ const definitions: Definition[] = [
     },
     {
         fingerprint: tuya.fingerprint('TS0601', ['_TZE200_yjjdcqsq', '_TZE200_9yapgbuv', '_TZE204_9yapgbuv',
-            '_TZE204_upagmta9', '_TZE200_cirvgep4', '_TZE200_upagmta9', '_TZE204_yjjdcqsq']),
+            '_TZE204_upagmta9', '_TZE200_cirvgep4', '_TZE200_upagmta9', '_TZE204_yjjdcqsq', '_TZE204_utkemkbs']),
         model: 'TS0601_temperature_humidity_sensor_2',
         vendor: 'TuYa',
         description: 'Temperature and humidity sensor',
@@ -717,6 +717,7 @@ const definitions: Definition[] = [
             tuya.whitelabel('TuYa', 'ZTH02', 'Temperature and humidity sensor', ['_TZE200_9yapgbuv', '_TZE204_9yapgbuv']),
             tuya.whitelabel('TuYa', 'ZTH05', 'Temperature and humidity sensor', ['_TZE204_upagmta9', '_TZE200_upagmta9']),
             tuya.whitelabel('TuYa', 'ZTH08-E', 'Temperature and humidity sensor', ['_TZE200_cirvgep4']),
+            tuya.whitelabel('ROXIMO', 'SZTH02', 'Temperature and humidity sensor', ['_TZE204_utkemkbs']),
         ],
     },
     {
