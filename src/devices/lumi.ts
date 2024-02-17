51b539b104ec06024f681558bbd50ee13d91e70f
Water Lou <water@waterworld.com.hk>
2024-02-17 13:17:00 +0800

    fix bug on aqara z1 1 gang switch

--- src/devices/lumi.ts
+++ src/devices/lumi.ts
@@ -3303,17 +3303,17 @@ const definitions: Definition[] = [
         },
         exposes: [
             e.power(), e.voltage(), e.device_temperature(),
-            e.switch().withEndpoint('button'),
+            e.switch().withEndpoint('button_1'),
 
             e.enum('power_outage_memory', ea.ALL, ['on', 'electric_appliances_on', 'electric_appliances_off', 'inverted'])
-                .withDescription('Power Outage Memory').withEndpoint('button'),
+                .withDescription('Power Outage Memory').withEndpoint('button_1'),
             e.led_disabled_night(),
 
             e.enum('operation_mode', ea.ALL, ['control_relay', 'decoupled'])
-                .withDescription('Decoupled mode').withEndpoint('button'),
+                .withDescription('Decoupled mode').withEndpoint('button_1'),
 
             e.binary('lock_relay', ea.ALL, true, false)
-                .withDescription('Lock relay mode').withEndpoint('button'),
+                .withDescription('Lock relay mode').withEndpoint('button_1'),
 
             e.action(['single']),
         ],
