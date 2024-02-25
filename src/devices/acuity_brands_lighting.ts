0528bacf8c090e50b2b32f644503261165584d22
Frank Qin <qinzhiheng24@gmail.com>
2024-02-25 01:00:59 -0800

    feat: Add color support to Acuity Brands Lighting RB56SC and RB56AC (#7113)
    Adding color support to RB56SC and RB56AC.
    
    Co-authored-by: Jolin Zhou <jolinzhou90@gmail.com>

--- src/devices/acuity_brands_lighting.ts
+++ src/devices/acuity_brands_lighting.ts
@@ -14,14 +14,14 @@ const definitions: Definition[] = [
         model: 'RB56SC',
         vendor: 'Acuity Brands Lighting (ABL)',
         description: 'Juno Retrobasics 4" and 6" LED smart downlight',
-        extend: [light({colorTemp: {range: [200, 370], startup: false}})],
+        extend: [light({colorTemp: {range: [200, 370], startup: false}, color: true})],
     },
     {
         zigbeeModel: ['ABL-LIGHT-Z-202'],
         model: 'RB56AC',
         vendor: 'Acuity Brands Lighting (ABL)',
         description: 'Juno Retrobasics 4" and 6" LED smart adjustable downlight',
-        extend: [light({colorTemp: {range: [200, 370], startup: false}})],
+        extend: [light({colorTemp: {range: [200, 370], startup: false}, color: true})],
     },
 ];
 
