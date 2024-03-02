a936caaa4538a8379704b457db4bfc05c8db783f
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-03-02 13:58:04 +0300

    Use battery extend

--- src/devices/gs.ts
+++ src/devices/gs.ts
@@ -3,7 +3,7 @@ import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
 import tz from '../converters/toZigbee';
 import * as reporting from '../lib/reporting';
-import {light, onOff, electricityMeter, iasZoneAlarm, temperature, humidity} from '../lib/modernExtend';
+import {light, onOff, electricityMeter, iasZoneAlarm, temperature, humidity, battery, ignoreClusterReport} from '../lib/modernExtend';
 
 const e = exposes.presets;
 
@@ -48,17 +48,10 @@ const definitions: Definition[] = [
         model: 'SMHM-I1',
         vendor: 'GS',
         description: 'Motion sensor',
-        fromZigbee: [fz.ias_occupancy_alarm_1, fz.battery],
+        fromZigbee: [fz.ias_occupancy_alarm_1],
         toZigbee: [],
-        exposes: [e.occupancy(), e.battery_low(), e.battery(), e.battery_voltage(), e.tamper()],
-        meta: {battery: {voltageToPercentage: '3V_2500'}},
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            const bindClusters = ['genPowerCfg'];
-            await reporting.bind(endpoint, coordinatorEndpoint, bindClusters);
-            await reporting.batteryPercentageRemaining(endpoint);
-            await reporting.batteryVoltage(endpoint);
-        },
+        exposes: [e.occupancy(), e.battery_low(), e.tamper()],
+        extend: [battery({voltageToPercentage: '3V_2500', batteryAlarm: false})],
     },
     {
         zigbeeModel: ['SOHM-I1'],
@@ -68,51 +61,40 @@ const definitions: Definition[] = [
         fromZigbee: [fz.ias_contact_alarm_1],
         toZigbee: [],
         exposes: [e.contact(), e.battery_low(), e.tamper()],
+        extend: [battery({batteryAlarm: false})],
     },
     {
         zigbeeModel: ['SRHMP-I1'],
         model: 'SRHMP-I1',
         vendor: 'GS',
         description: 'Siren',
-        fromZigbee: [fz.battery, fz.ignore_basic_report],
         toZigbee: [tz.warning],
         meta: {disableDefaultResponse: true},
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
-            await reporting.batteryPercentageRemaining(endpoint);
-        },
-        exposes: [e.battery(), e.warning()],
+        exposes: [e.warning()],
+        extend: [
+            battery({batteryAlarm: false}),
+            ignoreClusterReport({cluster: 'genBasic'}),
+        ],
     },
     {
         zigbeeModel: ['SSHM-I1'],
         model: 'SSHM-I1',
         vendor: 'GS',
         description: 'Smoke detector',
-        fromZigbee: [fz.ias_smoke_alarm_1, fz.battery],
-        toZigbee: [],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
-            await reporting.batteryPercentageRemaining(endpoint);
-        },
-        exposes: [e.smoke(), e.battery_low(), e.tamper(), e.battery()],
+        fromZigbee: [fz.ias_smoke_alarm_1],
+        exposes: [e.smoke(), e.battery_low(), e.tamper()],
+        extend: [battery({batteryAlarm: false})],
     },
     {
         zigbeeModel: ['STHM-I1H'],
         model: 'STHM-I1H',
         vendor: 'GS',
         description: 'Temperature and humidity sensor',
-        fromZigbee: [fz.battery],
-        meta: {battery: {voltageToPercentage: '3V_2500'}},
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            const bindClusters = ['genPowerCfg'];
-            await reporting.bind(endpoint, coordinatorEndpoint, bindClusters);
-            await reporting.batteryVoltage(endpoint);
-        },
-        exposes: [e.battery()],
-        extend: [temperature(), humidity()],
+        extend: [
+            temperature(),
+            humidity(),
+            battery({voltageToPercentage: '3V_2500', batteryAlarm: false}),
+        ],
     },
     {
         zigbeeModel: ['SWHM-I1'],
@@ -120,8 +102,8 @@ const definitions: Definition[] = [
         vendor: 'GS',
         description: 'Water leakage sensor',
         fromZigbee: [fz.ias_water_leak_alarm_1],
-        toZigbee: [],
         exposes: [e.water_leak(), e.battery_low(), e.tamper()],
+        extend: [battery({batteryAlarm: false})],
     },
 ];
 
