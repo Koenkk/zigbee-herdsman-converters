d14609788bd59c6b15b32b77578608b1140e9ae8
Koen Kanters <koenkanters94@gmail.com>
2024-03-09 15:56:03 +0100

    Update imou.ts

--- src/devices/imou.ts
+++ src/devices/imou.ts
@@ -1,43 +1,28 @@
 import {Definition} from '../lib/types';
-import fz from '../converters/fromZigbee';
-import tz from '../converters/toZigbee';
-import * as exposes from '../lib/exposes';
-import * as reporting from '../lib/reporting';
-
-const e = exposes.presets;
+import {battery, forceDeviceType, iasWarning, iasZoneAlarm} from '../lib/modernExtend';
 
 const definitions: Definition[] = [
     {
         zigbeeModel: ['ZP1-EN'],
         model: 'ZP1-EN',
         vendor: 'IMOU',
-        description: 'IMOU ZigBee ZP1 PIR Motion Sensor',
-        fromZigbee: [fz.ias_occupancy_alarm_1_with_timeout, fz.battery, fz.ignore_basic_report],
-        toZigbee: [],
-        exposes: [e.occupancy(), e.battery_low(), e.linkquality(), e.battery(), e.battery_voltage()],
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
-            await reporting.batteryPercentageRemaining(endpoint);
-        },
+        description: 'Zigbee ZP1 PIR motion sensor',
+        extend: [
+            battery(),
+            iasZoneAlarm({zoneType: 'occupancy', zoneAttributes: ['alarm_1', 'tamper', 'battery_low'], alarmTimeout: true}),
+        ],
     },
     {
         zigbeeModel: ['ZR1-EN'],
         model: 'ZR1-EN',
         vendor: 'IMOU',
-        description: 'IMOU ZigBee ZR1 Siren',
-        fromZigbee: [fz.ignore_basic_report, fz.ias_siren],
-        toZigbee: [tz.warning],
-        exposes: [e.tamper(), e.warning()],
+        description: 'Zigbee ZR1 siren',
+        extend: [
+            battery(),
+            forceDeviceType({type: 'EndDevice'}), iasWarning(),
+            iasZoneAlarm({zoneType: 'alarm', zoneAttributes: ['alarm_1', 'tamper', 'battery_low']}),
+        ],
         meta: {disableDefaultResponse: true},
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
-            await reporting.batteryPercentageRemaining(endpoint);
-            // Device advertises itself as Router but is an EndDevice
-            device.type = 'EndDevice';
-            device.save();
-        },
     },
 ];
 
