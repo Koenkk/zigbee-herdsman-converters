3edfa24bd7583557f7adc9b4569f5fb041fe979a
m00ny <TSCxxXMONSTAXxx@gmail.com>
2024-03-09 07:50:40 +0100

    fix: Fix 8719514440937/8719514440999 description (#7183)

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -2891,9 +2891,9 @@ const definitions: Definition[] = [
             e.enum('action_type', ea.STATE, ['step', 'rotate'])
                 .withDescription('Type of the rotation, value in the first message is `step` and in the next messages value is `rotate`'),
             e.numeric('action_time', ea.STATE)
-                .withDescription('Raw value that represents the amount the dial was turned').withValueMin(0).withValueMax(255),
+                .withDescription('value in seconds representing the amount of time the last action took').withValueMin(0).withValueMax(255),
             e.numeric('brightness', ea.STATE)
-                .withDescription('Raw rotation state value of the dial that').withValueMin(0).withValueMax(255),
+                .withDescription('Raw rotation state value of the dial which represents brightness from 0-255').withValueMin(0).withValueMax(255),
             e.numeric('action_step_size', ea.STATE)
                 .withDescription('amount of steps the last action took on the dial exposed as a posive value from 0-255')
                 .withValueMin(0).withValueMax(255),
