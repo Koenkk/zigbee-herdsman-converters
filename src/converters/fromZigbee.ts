9e4e1fd8b65a8acb5f5efc460f44414f5e83dc88
Koen Kanters <koenkanters94@gmail.com>
2024-03-14 20:33:37 +0100

    fix(ignore): Fix no action for TuYa TS004X devices https://github.com/Koenkk/zigbee2mqtt/issues/21784

--- src/converters/fromZigbee.ts
+++ src/converters/fromZigbee.ts
@@ -2166,31 +2166,26 @@ const converters1 = {
     } satisfies Fz.Converter,
     tuya_on_off_action: {
         cluster: 'genOnOff',
-        type: 'raw',
+        type: 'commandTuyaAction',
         convert: (model, msg, publish, options, meta) => {
-            if (hasAlreadyProcessedMessage(msg, model, msg.data[1])) return;
+            if (hasAlreadyProcessedMessage(msg, model)) return;
             const clickMapping: KeyValueNumberString = {0: 'single', 1: 'double', 2: 'hold'};
             const buttonMapping: KeyValueNumberString = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8'};
             // TS004F has single endpoint, TS0041A/TS0041 can have multiple but have just one button
             const button = msg.device.endpoints.length == 1 || ['TS0041A', 'TS0041'].includes(msg.device.modelID) ?
                 '' : `${buttonMapping[msg.endpoint.ID]}_`;
-            // Since it is a non standard ZCL command, no default response is send from zigbee-herdsman
-            // Send the defaultResponse here, otherwise the second button click delays.
-            // https://github.com/Koenkk/zigbee2mqtt/issues/8149
-            msg.endpoint.defaultResponse(0xfd, 0, 6, msg.data[1]).catch((error) => {});
-            return {action: `${button}${clickMapping[msg.data[3]]}`};
+            return {action: `${button}${clickMapping[msg.data.value]}`};
         },
     } satisfies Fz.Converter,
     tuya_switch_scene: {
         cluster: 'genOnOff',
-        type: 'raw',
+        type: 'commandTuyaAction',
         convert: (model, msg, publish, options, meta) => {
-            if (hasAlreadyProcessedMessage(msg, model, msg.data[1])) return;
+            if (hasAlreadyProcessedMessage(msg, model)) return;
             // Since it is a non standard ZCL command, no default response is send from zigbee-herdsman
             // Send the defaultResponse here, otherwise the second button click delays.
             // https://github.com/Koenkk/zigbee2mqtt/issues/8149
-            msg.endpoint.defaultResponse(0xfd, 0, 6, msg.data[1]).catch((error) => {});
-            return {action: 'switch_scene', action_scene: msg.data[3]};
+            return {action: 'switch_scene', action_scene: msg.data.value};
         },
     } satisfies Fz.Converter,
     livolo_switch_state: {
