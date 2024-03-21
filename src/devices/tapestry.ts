31c64bc1efb5d2e890bae9e74d695bf9fc9806b5
Elaine <pyoon99@gmail.com>
2024-03-21 15:40:36 -0700

    add Tapestry vendor file

---
+++ src/devices/tapestry.ts
@@ -0,0 +1,21 @@
+import { Definition } from '../lib/types';
+import * as exposes from '../lib/exposes';
+import fz from '../converters/fromZigbee';
+const e = exposes.presets;
+
+const definitions: Definition[] = [
+    {
+        zigbeeModel: ['Presence Z1'],
+        model: 'THPZ1',
+        vendor: 'Tapestry',
+        description: 'Tapestry Presence Sensor Z1 Occupancy and Temperature/Humidity Sensor',
+        extend: [],
+        fromZigbee: [fz.temperature, fz.humidity, fz.occupancy],
+        toZigbee: [],
+        exposes: [e.occupancy(), e.temperature(), e.humidity()],
+        meta: {},
+    },
+];
+
+export default definitions;
+module.exports = definitions;
