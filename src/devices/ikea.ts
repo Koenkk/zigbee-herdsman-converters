deef79853bbf3f4c1b94c18becb16840734e17ce
jameson_uk <1040621+jamesonuk@users.noreply.github.com>
2024-03-21 18:39:17 +0000

    Add support for IKEA Jetstrom 3030
    This supports colour unlike the other two Jetstrom lights which have already been added.

--- src/devices/ikea.ts
+++ src/devices/ikea.ts
@@ -1139,6 +1139,13 @@ const definitions: Definition[] = [
         extend: [tradfriLight()],
     },
     {
+        zigbeeModel: ['JETSTROM 3030 ceiling'],
+        model: 'L2205, L2206 JETSTRÖM',
+        vendor: 'IKEA',
+        description: 'JETSTRÖM LED wall light panel, smart dimmable/wired-in colour and white spectrum, 30x30cm',
+        extend: [tradfriLight({colorTemp: true, color: true})],
+    },    
+    {
         zigbeeModel: ['JETSTROM 40100'],
         model: 'L2208',
         vendor: 'IKEA',
