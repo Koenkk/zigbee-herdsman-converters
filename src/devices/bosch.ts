6ebd353ccb37e5b6bc820078ed416e226f506b94
ftsz <fabian@tinsz.com>
2024-04-01 20:54:25 +0200

    Import ota from modernExtend

--- src/devices/bosch.ts
+++ src/devices/bosch.ts
@@ -1,4 +1,4 @@
-import {identify, light, onOff, quirkCheckinInterval} from '../lib/modernExtend';
+import {identify, light, onOff, ota, quirkCheckinInterval} from '../lib/modernExtend';
 import {Zcl} from 'zigbee-herdsman';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
