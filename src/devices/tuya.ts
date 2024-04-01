c4a584cf3cd31aa10ab2dc85e95483970c171042
dinamik <dinamikoff1488@gmail.com>
2024-04-01 16:14:55 +0300

    ledron1

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -7687,6 +7687,30 @@ const definitions: Definition[] = [
         ],
     },
     {
+        fingerprint: [{modelID: 'TS1002', manufacturerName: '_TZ3000_zwszqdpy'}],
+            model: 'YK-16',
+            vendor: 'Ledron',
+            description: 'RGB+CCT Remote',
+            fromZigbee: [fz.battery, fz.command_on, fz.command_off, fz.command_move_to_level, fz.command_move_to_color_temp, fz.command_step_color_temperature, fz.command_step],
+            toZigbee: [],
+            exposes: [e.battery(), e.battery_voltage(), e.action(['on', 'off', 'brightness_move_to_level', 'color_temperature_move'])],
+            configure: async (device, coordinatorEndpoint, logger) => {
+                const endpoint = device.getEndpoint(1);
+                await tuya.configureMagicPacket(device, coordinatorEndpoint, logger);
+                await endpoint.command('genGroups', 'miboxerSetZones', {zones: [
+                    {zoneNum: 1, groupId: 101},
+                    {zoneNum: 2, groupId: 102},
+                    {zoneNum: 3, groupId: 103},
+                    {zoneNum: 4, groupId: 104},
+                    {zoneNum: 5, groupId: 105},
+                    {zoneNum: 6, groupId: 106},
+                    {zoneNum: 7, groupId: 107},
+                    {zoneNum: 8, groupId: 108},
+                ]});
+                await endpoint.command('genBasic', 'tuyaSetup', {}, {disableDefaultResponse: true});
+            },
+        },
+    {
         fingerprint: tuya.fingerprint('TS0601', ['_TZE204_l6llgoxq']),
         model: 'EA4161C-BI',
         vendor: 'TuYa',
