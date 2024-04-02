d67a32b7e3c565126ec073a3990881bb63788325
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-04-02 23:54:38 +0300

    Use ota modern extend

--- src/devices/sengled.ts
+++ src/devices/sengled.ts
@@ -2,9 +2,8 @@ import {Definition} from '../lib/types';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
 import tz from '../converters/toZigbee';
-import * as ota from '../lib/ota';
 import * as reporting from '../lib/reporting';
-import {onOff, LightArgs, light as lightDontUse, electricityMeter, forcePowerSource, light} from '../lib/modernExtend';
+import {onOff, LightArgs, light as lightDontUse, electricityMeter, forcePowerSource, light, ota} from '../lib/modernExtend';
 
 const e = exposes.presets;
 
@@ -20,16 +19,20 @@ const definitions: Definition[] = [
         description: 'Flood light with motion sensor light outdoor',
         fromZigbee: [fz.ias_occupancy_alarm_1],
         exposes: [e.occupancy()],
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E21-N13A'],
         model: 'E21-N13A',
         vendor: 'Sengled',
         description: 'Smart LED (A19)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E21-N1EA'],
@@ -37,11 +40,11 @@ const definitions: Definition[] = [
         vendor: 'Sengled',
         description: 'Smart LED multicolor A19 bulb',
         extend: [
-            sengledLight({colorTemp: {range: [154, 500]}}),
+            sengledLight({colorTemp: {range: [154, 500]}, color: {modes: ['xy']}}),
             electricityMeter({cluster: 'metering'}),
             forcePowerSource({powerSource: 'Mains (single phase)'}),
+            ota(),
         ],
-        ota: ota.zigbeeOTA,
     },
     {
         zigbeeModel: ['E12-N1E'],
@@ -52,24 +55,28 @@ const definitions: Definition[] = [
             sengledLight({colorTemp: {range: [154, 500]}}),
             electricityMeter({cluster: 'metering'}),
             forcePowerSource({powerSource: 'Mains (single phase)'}),
+            ota(),
         ],
-        ota: ota.zigbeeOTA,
     },
     {
         zigbeeModel: ['E1G-G8E'],
         model: 'E1G-G8E',
         vendor: 'Sengled',
         description: 'Multicolor light strip (2M)',
-        extend: [light({colorTemp: {range: undefined}, color: true})],
-        ota: ota.zigbeeOTA,
+        extend: [
+            light({colorTemp: {range: undefined}, color: true}),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E11-U21U31'],
         model: 'E11-U21U31',
         vendor: 'Sengled',
         description: 'Element touch (A19)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E11-G13'],
@@ -80,32 +87,38 @@ const definitions: Definition[] = [
             sengledLight({colorTemp: {range: [154, 500]}}),
             electricityMeter({cluster: 'metering'}),
             forcePowerSource({powerSource: 'Mains (single phase)'}),
+            ota(),
         ],
-        ota: ota.zigbeeOTA,
     },
     {
         zigbeeModel: ['E11-G23', 'E11-G33'],
         model: 'E11-G23/E11-G33',
         vendor: 'Sengled',
         description: 'Element classic (A60)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E11-N13', 'E11-N13A', 'E11-N14', 'E11-N14A'],
         model: 'E11-N13/E11-N13A/E11-N14/E11-N14A',
         vendor: 'Sengled',
         description: 'Element extra bright (A19)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['Z01-CIA19NAE26'],
         model: 'Z01-CIA19NAE26',
         vendor: 'Sengled',
         description: 'Element touch (A19)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['Z01-A19NAE26'],
@@ -115,16 +128,18 @@ const definitions: Definition[] = [
         extend: [
             sengledLight({colorTemp: {range: [154, 500]}}),
             electricityMeter({cluster: 'metering'}),
+            ota(),
         ],
-        ota: ota.zigbeeOTA,
     },
     {
         zigbeeModel: ['Z01-A60EAE27'],
         model: 'Z01-A60EAE27',
         vendor: 'Sengled',
         description: 'Element Plus (A60)',
-        extend: [light({colorTemp: {range: undefined}})],
-        ota: ota.zigbeeOTA,
+        extend: [
+            light({colorTemp: {range: undefined}}),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E11-N1EA'],
@@ -135,24 +150,28 @@ const definitions: Definition[] = [
             sengledLight({colorTemp: {range: [154, 500]}}),
             electricityMeter({cluster: 'metering'}),
             forcePowerSource({powerSource: 'Mains (single phase)'}),
+            ota(),
         ],
-        ota: ota.zigbeeOTA,
     },
     {
         zigbeeModel: ['E11-U2E'],
         model: 'E11-U2E',
         vendor: 'Sengled',
         description: 'Element color plus E27',
-        extend: [light({colorTemp: {range: undefined}, color: true})],
-        ota: ota.zigbeeOTA,
+        extend: [
+            light({colorTemp: {range: undefined}, color: true}),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E11-U3E'],
         model: 'E11-U3E',
         vendor: 'Sengled',
         description: 'Element color plus B22',
-        extend: [light({colorTemp: {range: undefined}, color: true})],
-        ota: ota.zigbeeOTA,
+        extend: [
+            light({colorTemp: {range: undefined}, color: true}),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E1F-N5E'],
@@ -163,24 +182,28 @@ const definitions: Definition[] = [
             sengledLight({colorTemp: {range: [154, 500]}}),
             electricityMeter({cluster: 'metering'}),
             forcePowerSource({powerSource: 'Mains (single phase)'}),
+            ota(),
         ],
-        ota: ota.zigbeeOTA,
     },
     {
         zigbeeModel: ['E12-N14'],
         model: 'E12-N14',
         vendor: 'Sengled',
         description: 'Element Classic (BR30)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E1A-AC2'],
         model: 'E1ACA4ABE38A',
         vendor: 'Sengled',
         description: 'Element downlight smart LED bulb',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E1D-G73'],
@@ -188,8 +211,6 @@ const definitions: Definition[] = [
         vendor: 'Sengled',
         description: 'Smart window and door sensor',
         fromZigbee: [fz.ias_contact_alarm_1, fz.battery],
-        toZigbee: [],
-        ota: ota.zigbeeOTA,
         exposes: [e.contact(), e.battery_low(), e.battery(), e.battery_voltage(), e.tamper()],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
@@ -197,6 +218,9 @@ const definitions: Definition[] = [
             await reporting.batteryVoltage(endpoint);
             await reporting.batteryPercentageRemaining(endpoint);
         },
+        extend: [
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E2D-G73'],
@@ -204,8 +228,6 @@ const definitions: Definition[] = [
         vendor: 'Sengled',
         description: 'Smart window and door sensor G2',
         fromZigbee: [fz.ias_contact_alarm_1, fz.battery],
-        toZigbee: [],
-        ota: ota.zigbeeOTA,
         exposes: [e.contact(), e.battery_low(), e.battery(), e.battery_voltage(), e.tamper()],
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(1);
@@ -213,14 +235,19 @@ const definitions: Definition[] = [
             await reporting.batteryVoltage(endpoint);
             await reporting.batteryPercentageRemaining(endpoint);
         },
+        extend: [
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E1C-NB6'],
         model: 'E1C-NB6',
         vendor: 'Sengled',
         description: 'Smart plug',
-        extend: [onOff()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            onOff(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E1C-NB7'],
@@ -245,24 +272,29 @@ const definitions: Definition[] = [
         description: 'Smart switch',
         fromZigbee: [fz.E1E_G7F_action],
         exposes: [e.action(['on', 'up', 'down', 'off', 'on_double', 'on_long', 'off_double', 'off_long'])],
-        toZigbee: [],
-        ota: ota.zigbeeOTA,
+        extend: [
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E11-N1G'],
         model: 'E11-N1G',
         vendor: 'Sengled',
         description: 'Vintage LED edison bulb (ST19)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
     {
         zigbeeModel: ['E1F-N9G'],
         model: 'E1F-N9G',
         vendor: 'Sengled',
         description: 'Smart LED filament candle (E12)',
-        extend: [sengledLight()],
-        ota: ota.zigbeeOTA,
+        extend: [
+            sengledLight(),
+            ota(),
+        ],
     },
 ];
 
