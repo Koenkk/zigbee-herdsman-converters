ec8acc9e5369087075869683161a57a5c2574367
dinamik <76748623+dinamik1488@users.noreply.github.com>
2024-04-02 21:16:07 +0300

    feat(add): F00XN00-04-1 (#7310)
    * dimmer-1
    
    * dimmer-2
    
    * dimmer-3
    
    * dimmer-4
    
    * dimmer-5
    
    * dimmer-6
    
    * dimmer-7
    
    * Dimmer-8

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -7730,6 +7730,27 @@ const definitions: Definition[] = [
         ],
     },
     {
+        fingerprint: [{modelID: 'TS1002', manufacturerName: '_TZ3000_etufnltx'}],
+        model: 'F00XN00-04-1',
+        vendor: 'FORIA',
+        description: 'Dimmer 4 scenes',
+        fromZigbee: [fz.battery, fz.command_on, fz.command_off, fz.command_move_to_level, fz.command_move_to_color_temp,
+            fz.command_step_color_temperature, fz.command_step],
+        toZigbee: [],
+        exposes: [e.battery(), e.battery_voltage(), e.action(['on', 'off', 'brightness_move_to_level', 'color_temperature_move'])],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await tuya.configureMagicPacket(device, coordinatorEndpoint, logger);
+            await endpoint.command('genGroups', 'miboxerSetZones', {zones: [
+                {zoneNum: 1, groupId: 101},
+                {zoneNum: 2, groupId: 102},
+                {zoneNum: 3, groupId: 103},
+                {zoneNum: 4, groupId: 104},
+            ]});
+            await endpoint.command('genBasic', 'tuyaSetup', {}, {disableDefaultResponse: true});
+        },
+    },
+    {
         fingerprint: tuya.fingerprint('TS0601', ['_TZE204_l6llgoxq']),
         model: 'EA4161C-BI',
         vendor: 'TuYa',
