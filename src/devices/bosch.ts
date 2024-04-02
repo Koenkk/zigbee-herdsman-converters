c6f18b40229bb5d9f080b1836074cd8d7cdad8ca
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2024-04-02 20:38:55 +0200

    fix: Change AQI scale for Bosch 8750001213 Twinguard  (#7313)
    * feat: Expose raw IAQ value
    
    Expose raw IAQ (indoor air quality) levels as an AQI (air quality index) sensor. Might be useful for downstream applications doing their own calculations.
    
    * feat: Handle non-linear IAQ scale (#7274)
    
    Add basic handling for the non-linear nature of the IAQ scale used by the BME680 gas sensor.
    
    * fix: Minor changes to IAQ calculations
    
    * fix: Ensure Lint is happy

--- src/devices/bosch.ts
+++ src/devices/bosch.ts
@@ -790,8 +790,22 @@ const fzLocal = {
                 }
             }
             if (msg.data.hasOwnProperty('airpurity')) {
-                result.co2 = utils.precisionRound((msg.data['airpurity'] * 10.0 + 500.0), 2);
-                result.voc = utils.precisionRound((msg.data['airpurity'] * 71.22), 2);
+                const iaq = parseInt(msg.data['airpurity']);
+                result.aqi = iaq;
+                result.co2 = ((iaq * 10) + 500);
+                let factor = 6;
+                if ((iaq >= 51) && (iaq <= 100)) {
+                    factor = 10;
+                } else if ((iaq >= 101) && (iaq <= 150)) {
+                    factor = 20;
+                } else if ((iaq >= 151) && (iaq <= 200)) {
+                    factor = 50;
+                } else if ((iaq >= 201) && (iaq <= 250)) {
+                    factor = 100;
+                } else if (iaq >= 251) {
+                    factor = 100;
+                }
+                result.voc = (iaq * factor);
             }
             if (msg.data.hasOwnProperty('temperature')) {
                 result.temperature = parseFloat(msg.data['temperature']) / 100.0;
@@ -1333,6 +1347,7 @@ const definitions: Definition[] = [
             e.humidity().withValueMin(0).withValueMax(100).withValueStep(0.1),
             e.voc().withValueMin(0).withValueMax(35610).withValueStep(1),
             e.co2().withValueMin(500).withValueMax(5500).withValueStep(1),
+            e.aqi().withValueMin(0).withValueMax(500).withValueStep(1),
             e.illuminance_lux(), e.battery(),
             e.enum('alarm', ea.ALL, Object.keys(sirenState)).withDescription('Mode of the alarm (sound effect)'),
             e.text('siren_state', ea.STATE).withDescription('Siren state'),
