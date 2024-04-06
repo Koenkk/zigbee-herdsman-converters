bfb8546583bdbd529a6fdbfc8861d25af78e32ca
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2024-04-06 22:47:16 +0300

    Draft modern extends for is-sr-sb

--- src/devices/ekf.ts
+++ src/devices/ekf.ts
@@ -1,8 +1,10 @@
-import {Definition, Tz} from '../lib/types';
-import { battery, humidity, iasZoneAlarm, identify, ignoreClusterReport, illuminance, temperature } from '../lib/modernExtend';
+import {Definition} from '../lib/types';
+import {
+    battery, forcePowerSource, humidity, iasWarning, iasZoneAlarm,
+    identify, ignoreClusterReport, illuminance, temperature,
+} from '../lib/modernExtend';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
-import tz from '../converters/toZigbee';
 import * as legacy from '../lib/legacy';
 const e = exposes.presets;
 const ea = exposes.access;
@@ -10,59 +12,59 @@ import * as tuya from '../lib/tuya';
 import {modernExtend as tuyaModernExtend} from '../lib/tuya';
 const {tuyaMagicPacket, dpGas, dpEnumLookup, dpBinary} = tuyaModernExtend;
 
-const tzLocal = {
-    TS0219: {
-        key: ['light', 'alarm_duration'],
-        convertSet: async (entity, key, value, meta) => {
-            if (key === 'light') {
-                await entity.command('genOnOff', value.toLowerCase() === 'on' ? 'on' : 'off', {}, utils.getOptions(meta.mapped, entity));
-            } else if (key === 'duration') {
-                await entity.write('ssIasWd', {'maxDuration': value}, utils.getOptions(meta.mapped, entity));
-            } else if (key === 'volume') {
-                const lookup = {'mute': 0, 'low': 10, 'medium': 30, 'high': 50};
-                value = value.toLowerCase();
-                utils.validateValue(value, Object.keys(lookup));
-                await entity.write('ssIasWd', {0x0002: {value: lookup[value], type: 0x0a}}, utils.getOptions(meta.mapped, entity));
-            }
-            return {state: {[key]: value}};
-        },
-    } satisfies Tz.Converter,
-};
+// const tzLocal = {
+//     TS0219: {
+//         key: ['light', 'alarm_duration'],
+//         convertSet: async (entity, key, value, meta) => {
+//             if (key === 'light') {
+//                 await entity.command('genOnOff', value.toLowerCase() === 'on' ? 'on' : 'off', {}, utils.getOptions(meta.mapped, entity));
+//             } else if (key === 'duration') {
+//                 await entity.write('ssIasWd', {'maxDuration': value}, utils.getOptions(meta.mapped, entity));
+//             } else if (key === 'volume') {
+//                 const lookup = {'mute': 0, 'low': 10, 'medium': 30, 'high': 50};
+//                 value = value.toLowerCase();
+//                 utils.validateValue(value, Object.keys(lookup));
+//                 await entity.write('ssIasWd', {0x0002: {value: lookup[value], type: 0x0a}}, utils.getOptions(meta.mapped, entity));
+//             }
+//             return {state: {[key]: value}};
+//         },
+//     } satisfies Tz.Converter,
+// };
 
-const alarmConverter = {
-    alarm: {
-        key: ['alarm'],
-        convertSet: async (entity, key, value, meta) => {
-            const mode = {'stop': 0, 'burglar': 1, 'fire': 2, 'emergency': 3, 'police_panic': 4, 'fire_panic': 5, 'emergency_panic': 6};
-            const level = {'low': 0, 'medium': 1, 'high': 2, 'very_high': 3};
-            const strobeLevel = {'low': 0, 'medium': 1, 'high': 2, 'very_high': 3};
+// const alarmConverter = {
+//     alarm: {
+//         key: ['alarm'],
+//         convertSet: async (entity, key, value, meta) => {
+//             const mode = {'stop': 0, 'burglar': 1, 'fire': 2, 'emergency': 3, 'police_panic': 4, 'fire_panic': 5, 'emergency_panic': 6};
+//             const level = {'low': 0, 'medium': 1, 'high': 2, 'very_high': 3};
+//             const strobeLevel = {'low': 0, 'medium': 1, 'high': 2, 'very_high': 3};
 
-            const values = {
-                mode: value.toLowerCase() === 'on' ? 'emergency' : 'stop',
-                level: 'medium',
-                strobe: true,
-                duration: 3600,
-                strobeDutyCycle: 10,
-                strobeLevel: 3,
-            };
+//             const values = {
+//                 mode: value.toLowerCase() === 'on' ? 'emergency' : 'stop',
+//                 level: 'medium',
+//                 strobe: true,
+//                 duration: 3600,
+//                 strobeDutyCycle: 10,
+//                 strobeLevel: 3,
+//             };
 
-            let info;
-            // https://github.com/Koenkk/zigbee2mqtt/issues/8310 some devices require the info to be reversed.
-            if (['SIRZB-110', 'SRAC-23B-ZBSR', 'AV2010/29A', 'AV2010/24A'].includes(meta.mapped.model)) {
-                info = (mode[values.mode]) + ((values.strobe ? 1 : 0) << 4) + (level[values.level] << 6);
-            } else {
-                info = (mode[values.mode] << 4) + ((values.strobe ? 1 : 0) << 2) + (level[values.level]);
-            }
+//             let info;
+//             // https://github.com/Koenkk/zigbee2mqtt/issues/8310 some devices require the info to be reversed.
+//             if (['SIRZB-110', 'SRAC-23B-ZBSR', 'AV2010/29A', 'AV2010/24A'].includes(meta.mapped.model)) {
+//                 info = (mode[values.mode]) + ((values.strobe ? 1 : 0) << 4) + (level[values.level] << 6);
+//             } else {
+//                 info = (mode[values.mode] << 4) + ((values.strobe ? 1 : 0) << 2) + (level[values.level]);
+//             }
 
-            await entity.command(
-                'ssIasWd',
-                'startWarning',
-                {startwarninginfo: info, warningduration: values.duration,
-                    strobedutycycle: values.strobeDutyCycle, strobelevel: values.strobeLevel},
-                utils.getOptions(meta.mapped, entity));
-        },
-    } satisfies Tz.Converter,
-};
+//             await entity.command(
+//                 'ssIasWd',
+//                 'startWarning',
+//                 {startwarninginfo: info, warningduration: values.duration,
+//                     strobedutycycle: values.strobeDutyCycle, strobelevel: values.strobeLevel},
+//                 utils.getOptions(meta.mapped, entity));
+//         },
+//     } satisfies Tz.Converter,
+// };
 
 const definitions: Definition[] = [
     {
@@ -127,15 +129,20 @@ const definitions: Definition[] = [
         model: 'is-sr-sb',
         vendor: 'EKF',
         description: 'Smart siren',
-        fromZigbee: [],
-        toZigbee: [tz.warning, tzLocal.TS0219, alarmConverter.alarm],
-        exposes: [e.warning(),
-            e.binary('light', ea.ALL, 'ON', 'OFF').withDescription('Turn the light ON/OFF'),
-            e.binary('alarm', ea.ALL, 'ON', 'OFF').withDescription('Turn the alarm ON/OFF'),
-            e.numeric('duration', ea.STATE_SET).withValueMin(60).withValueMax(3600).withValueStep(1).withUnit('s')
-                .withDescription('Duration of the alarm'),
-            e.enum('volume', ea.STATE_SET, ['mute', 'low', 'medium', 'high'])
-                .withDescription('Volume of the alarm'),
+        // toZigbee: [tz.warning, tzLocal.TS0219, alarmConverter.alarm],
+        // exposes: [e.warning(),
+        //     e.binary('light', ea.ALL, 'ON', 'OFF').withDescription('Turn the light ON/OFF'),
+        //     e.binary('alarm', ea.ALL, 'ON', 'OFF').withDescription('Turn the alarm ON/OFF'),
+        //     e.numeric('duration', ea.STATE_SET).withValueMin(60).withValueMax(3600).withValueStep(1).withUnit('s')
+        //         .withDescription('Duration of the alarm'),
+        //     e.enum('volume', ea.STATE_SET, ['mute', 'low', 'medium', 'high'])
+        //         .withDescription('Volume of the alarm'),
+        // ],
+        extend: [
+            forcePowerSource({powerSource: 'Mains (single phase)'}),
+            iasWarning(),
+            iasZoneAlarm({zoneType: 'generic', zoneAttributes: []}),
+            battery({voltage: true}),
         ],
     },
     {
