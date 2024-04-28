4e98b133c3f4c9fea34f96ca4e5b387494831af7
Dorian Benech <dorian.benech007@gmail.com>
2024-04-28 12:21:52 +0200

    new cluster definition

--- src/devices/gmmts.ts
+++ src/devices/gmmts.ts
@@ -14,6 +14,8 @@ import * as ota from '../lib/ota';
 import {Buffer} from 'buffer';
 import {Device} from 'zigbee-herdsman/dist/controller/model';
 import {logger} from '../lib/logger';
+import { deviceAddCustomCluster } from '../lib/modernExtend';
+
 const ea = exposes.access;
 const e = exposes.presets;
 
@@ -90,7 +92,7 @@ const ticmeter_data = [
     {name: 'Index 8',                       desc: 'Index 8',                                             cluster: METERING_CLUSTER,   attribute: 'currentTier8SummDelivered',          type: NUMBER, unit: 'Wh',   poll: false,  tic: T.STD,  contract: C.ANY,   elec: E.ANY,  producer: false                         },
     {name: 'Index 9',                       desc: 'Index 9',                                             cluster: METERING_CLUSTER,   attribute: 'currentTier9SummDelivered',          type: NUMBER, unit: 'Wh',   poll: false,  tic: T.STD,  contract: C.ANY,   elec: E.ANY,  producer: false                         },
     {name: 'Index 10',                      desc: 'Index 10',                                            cluster: METERING_CLUSTER,   attribute: 'currentTier10SummDelivered',         type: NUMBER, unit: 'Wh',   poll: false,  tic: T.STD,  contract: C.ANY,   elec: E.ANY,  producer: false                         },
-    {name: 'Tarif en cours',                desc: 'Option tarifaire en cours',                           cluster: TICMETER_CLUSTER,   attribute: 'activeRegisterTierDelivered',        type: STRING, unit: '',     poll: false,  tic: T.ANY,  contract: C.ANY,   elec: E.ANY,  producer: false                         },
+    {name: 'Tarif en cours',                desc: 'Option tarifaire en cours',                           cluster: TICMETER_CLUSTER,   attribute: 'currentTarif',                       type: STRING, unit: '',     poll: false,  tic: T.ANY,  contract: C.ANY,   elec: E.ANY,  producer: false                         },
     {name: 'Couleur demain',                desc: 'Couleur demain',                                      cluster: TICMETER_CLUSTER,   attribute: 'tomorowColor',                       type: STRING, unit: '',     poll: false,  tic: T.ANY,  contract: C.TEMPO, elec: E.ANY,  producer: false                         },
     {name: 'Intensité instantanée',         desc: 'Intensité instantanée',                               cluster: ELEC_MES_CLUSTER,   attribute: 'rmsCurrent',                         type: NUMBER, unit: 'A',    poll: false,  tic: T.ANY,  contract: C.ANY,   elec: E.MONO, producer: false                         },
     {name: 'Intensité instantanée Ph A',    desc: 'Intensité instantanée Phase A',                       cluster: ELEC_MES_CLUSTER,   attribute: 'rmsCurrent',                         type: NUMBER, unit: 'A',    poll: false,  tic: T.ANY,  contract: C.ANY,   elec: E.TRI,  producer: false                         },
@@ -427,6 +429,109 @@ const definitions: Definition[] = [
         description: 'TICMeter pour Linky',
         fromZigbee: [fz.meter_identification, fzLocal.ticmeter_cluster_fz, fzLocal.ticmeter_ha_electrical_measurement, fzLocal.ticmeter_metering],
         toZigbee: tzLocal,
+
+        extend: [
+            deviceAddCustomCluster('manuSpecificGmmts',
+                {
+                    ID: 0xff42,
+                    attributes: {
+                        contractType: { ID: 0x0000, type: Zcl.DataType.charStr },
+                        startEJP: { ID: 0x0001, type: Zcl.DataType.uint16 },
+                        refreshRate: { ID: 0x0002, type: Zcl.DataType.uint16 },
+                        tomorowColor: { ID: 0x0003, type: Zcl.DataType.charStr },
+                        //
+                        powerOverrun: { ID: 0x0004, type: Zcl.DataType.uint16 },
+                        powerOverrunA: { ID: 0x0005, type: Zcl.DataType.uint16 },
+                        powerOverrunB: { ID: 0x0006, type: Zcl.DataType.uint16 },
+                        powerOverrunC: { ID: 0x0007, type: Zcl.DataType.uint16 },
+                        //
+                        potentialPresence: { ID: 0x0008, type: Zcl.DataType.uint32 },
+                        //
+                        hcHours: { ID: 0x0009, type: Zcl.DataType.charStr },
+                        motdetat: { ID: 0x000a, type: Zcl.DataType.charStr },
+                        //
+                        date: { ID: 0x000b, type: Zcl.DataType.uint64 },
+                        //
+                        index1Dist: { ID: 0x000e, type: Zcl.DataType.uint48 },
+                        index2Dist: { ID: 0x000f, type: Zcl.DataType.uint48 },
+                        index3Dist: { ID: 0x0010, type: Zcl.DataType.uint48 },
+                        index4Dist: { ID: 0x0011, type: Zcl.DataType.uint48 },
+                        //
+                        powerMaxYesterday: { ID: 0x0012, type: Zcl.DataType.uint16 },
+                        powerMaxYesterday1: { ID: 0x0013, type: Zcl.DataType.uint16 },
+                        powerMaxYesterday2: { ID: 0x0014, type: Zcl.DataType.uint16 },
+                        powerMaxYesterday3: { ID: 0x0015, type: Zcl.DataType.uint16 },
+                        //
+                        powerInjected: { ID: 0x0016, type: Zcl.DataType.uint32 },
+                        powerMaxInjected: { ID: 0x0017, type: Zcl.DataType.uint32 },
+                        powerMaxInjectedYesterday: { ID: 0x0018, type: Zcl.DataType.uint32 },
+                        //
+                        injectedLoadN: { ID: 0x0019, type: Zcl.DataType.uint16 },
+                        injectedLoadN_1: { ID: 0x001a, type: Zcl.DataType.uint16 },
+                        //
+                        startEJP1: { ID: 0x001c, type: Zcl.DataType.uint64 },
+                        stopEJP1: { ID: 0x001d, type: Zcl.DataType.uint64 },
+                        startEJP2: { ID: 0x001e, type: Zcl.DataType.uint64 },
+                        stopEJP2: { ID: 0x001f, type: Zcl.DataType.uint64 },
+                        startEJP3: { ID: 0x0020, type: Zcl.DataType.uint64 },
+                        stopEJP3: { ID: 0x0021, type: Zcl.DataType.uint64 },
+                        //
+                        shortMsg: { ID: 0x0022, type: Zcl.DataType.charStr },
+                        ultraShortMsg: { ID: 0x0023, type: Zcl.DataType.charStr },
+                        //
+                        relays: { ID: 0x0024, type: Zcl.DataType.charStr },
+                        //
+                        currentIndex: { ID: 0x0025, type: Zcl.DataType.uint8 },
+                        //
+                        currentTarif: { ID: 0x0039, type: Zcl.DataType.charStr },
+                        calendarSupplierDay: { ID: 0x0026, type: Zcl.DataType.uint16 },
+                        nextSupplierCalendarDay: { ID: 0x0027, type: Zcl.DataType.uint16 },
+                        calendarDay: { ID: 0x0028, type: Zcl.DataType.charStr },
+                        calendarDayPointe: { ID: 0x0029, type: Zcl.DataType.charStr },
+                        //
+                        elecMode: { ID: 0x002a, type: Zcl.DataType.uint8 },
+                        maxContractPower: { ID: 0x002b, type: Zcl.DataType.uint16 },
+                        ticMode: { ID: 0x002c, type: Zcl.DataType.uint8 },
+                        uptime: { ID: 0x002d, type: Zcl.DataType.uint48 },
+                        ticVersion: { ID: 0x002e, type: Zcl.DataType.charStr },
+                        //
+                        powerMaxTodayTime: { ID: 0x002f, type: Zcl.DataType.uint64 },
+                        powerMaxToday1Time: { ID: 0x0030, type: Zcl.DataType.uint64 },
+                        powerMaxToday2Time: { ID: 0x0031, type: Zcl.DataType.uint64 },
+                        powerMaxToday3Time: { ID: 0x0032, type: Zcl.DataType.uint64 },
+                        //
+                        powerMaxYesterdayTime: { ID: 0x0033, type: Zcl.DataType.uint64 },
+                        powerMaxYesterday1Time: { ID: 0x0034, type: Zcl.DataType.uint64 },
+                        powerMaxYesterday2Time: { ID: 0x0035, type: Zcl.DataType.uint64 },
+                        powerMaxYesterday3Time: { ID: 0x0036, type: Zcl.DataType.uint64 },
+                        //
+                        powerMaxInjectedTime: { ID: 0x0037, type: Zcl.DataType.uint64 },
+                        powerMaxInjectedYesterdayTime: { ID: 0x0038, type: Zcl.DataType.uint64 },
+                    },
+                    commands: {
+                        refreshRate: {
+                            ID: 0,
+                            parameters: [
+                                { name: 'refreshRate', type: Zcl.DataType.uint16 },
+                            ],
+                        },
+                        reboot: {
+                            ID: 1,
+                            parameters: [],
+                        },
+                    },
+                    commandsResponse: {
+                        refreshRate: {
+                            ID: 1,
+                            parameters: [
+                                { name: 'seq', type: Zcl.DataType.uint16 },
+                            ],
+                        },
+                    },
+                }
+            )
+        ],
+
         exposes: (device, options) => {
             let endpoint: Zh.Endpoint;
             const exposes: any[] = [];
