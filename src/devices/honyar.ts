fb4a5e314888be47e085e5967422ebbc6bef0fa0
65698888 <98879715+65698888@users.noreply.github.com>
2024-05-07 18:39:39 +0800

    Update honyar.ts

--- src/devices/honyar.ts
+++ src/devices/honyar.ts
@@ -154,7 +154,7 @@ const definitions: Definition[] = [
                     } catch (error) {
                         // Do nothing
                     }
-                }, 5*1000);
+                }, 5*1000);// Every 5 seconds
                 globalStore.putValue(device, 'interval', interval);
             }
         },
