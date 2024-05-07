f7ae3b8d6e5b6ffd0fca612e472b2369bae9096c
65698888 <98879715+65698888@users.noreply.github.com>
2024-05-07 16:25:30 +0800

    Update honyar.ts

--- src/devices/honyar.ts
+++ src/devices/honyar.ts
@@ -15,7 +15,7 @@ const fzLocal = {
         type: ['attributeReport', 'readResponse'],
         convert: (model : any, msg : any, publish : any, options : any, meta : any) => {
             if (meta.device.dateCode === '20170621') {
-                const result = {};
+                const result  = (e:any) => {};
                 if (msg.data.hasOwnProperty('currentSummDelivered')) {
                     const data = msg.data['currentSummDelivered'];
                     const value = (parseInt(data[0]) << 32) + parseInt(data[1]);
@@ -37,7 +37,7 @@ const fzLocal = {
         ],
         convert: (model : any, msg : any, publish : any, options : any, meta : any) => {
             if (meta.device.dateCode === '20170621') {
-                const payload = {};
+                const payload = (e:any) => {};
                 if (msg.data.hasOwnProperty('rmsCurrent')) {
                     const current = msg.data['rmsCurrent'];
                     payload.current = current / 1000.0;
