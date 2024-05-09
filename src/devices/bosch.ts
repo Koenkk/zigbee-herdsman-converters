8f7b9fb4f2c5e06b22e06034db7b08b83c21793e
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2024-05-09 03:11:55 +0200

    Update bosch.ts

--- src/devices/bosch.ts
+++ src/devices/bosch.ts
@@ -1005,7 +1005,7 @@ const definitions: Definition[] = [
             fzLocal.bosch_thermostat,
         ],
         toZigbee: [
-            tz.tz.thermostat_running_state,
+            tz.thermostat_running_state,
             tz.thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_local_temperature_calibration,
