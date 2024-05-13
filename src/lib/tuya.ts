6e5361164b11432f980fa7411d6e7fd0a9daba0c
Urusus2 <166756444+Urusus2@users.noreply.github.com>
2024-05-13 23:12:47 +0300

    feat: Support more features for Zemismart ZM25R1 (#7516)
    * Added motor_direction, remote_pair and motor_working_mode for ZM25R1
    
    * Added datapoint remote_pair and motor_working_mode
    
    * fix line length
    
    * fix line spaces

--- src/lib/tuya.ts
+++ src/lib/tuya.ts
@@ -1136,7 +1136,7 @@ const tuyaTz = {
             'screen_orientation', 'regulator_period', 'regulator_set_point', 'upper_stroke_limit', 'middle_stroke_limit', 'lower_stroke_limit',
             'buzzer_feedback', 'rf_pairing', 'max_temperature_alarm', 'min_temperature_alarm', 'max_humidity_alarm', 'min_humidity_alarm',
             'temperature_periodic_report', 'humidity_periodic_report', 'temperature_sensitivity', 'humidity_sensitivity', 'temperature_alarm',
-            'humidity_alarm', 'move_sensitivity', 'radar_range', 'presence_timeout', 'update_frequency',
+            'humidity_alarm', 'move_sensitivity', 'radar_range', 'presence_timeout', 'update_frequency', 'remote_pair', 'motor_working_mode',
         ],
         convertSet: async (entity, key, value, meta) => {
             // A set converter is only called once; therefore we need to loop
