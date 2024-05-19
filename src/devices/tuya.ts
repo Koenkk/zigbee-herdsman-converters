7e347fa6f9a2fbb68aad7a27d9ee41f79a73d33d
Koen Kanters <koenkanters94@gmail.com>
2024-05-19 16:15:51 +0200

    fix(detect): Detect `_TZ3000_j61x9rxn` as TuYa TS0044_1 @ferarias https://github.com/Koenkk/zigbee2mqtt/issues/22669

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -2722,7 +2722,7 @@ const definitions: Definition[] = [
             {vendor: 'LoraTap', model: 'SS6400ZB'}, {vendor: 'Moes', model: 'ZT-SY-EU-G-4S-WH-MS'},
             tuya.whitelabel('Moes', 'ZT-SR-EU4', 'Star Ring 4 Gang Scene Switch', ['_TZ3000_a4xycprs']),
             tuya.whitelabel('TuYa', 'TS0044_1', 'Zigbee 4 button remote - 12 scene',
-                ['_TZ3000_dziaict4', '_TZ3000_mh9px7cq']),
+                ['_TZ3000_dziaict4', '_TZ3000_mh9px7cq', '_TZ3000_j61x9rxn']),
             tuya.whitelabel('TuYa', 'TM-YKQ004', 'Zigbee 4 button remote - 12 scene', ['_TZ3000_u3nv1jwk']),
         ],
         fromZigbee: [tuya.fz.on_off_action, fz.battery],
