b41636e651279459883ed9204ffedcdf8dca021b
Koen Kanters <koenkanters94@gmail.com>
2024-05-28 20:31:02 +0200

    fix(detect): Detect `_TZE200_2odrmqwq` as TuYa TS0601_cover_1 @mrespin https://github.com/Koenkk/zigbee2mqtt/issues/22795

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -3329,6 +3329,7 @@ const definitions: Definition[] = [
             {modelID: 'TS0601', manufacturerName: '_TZE200_yia0p3tr'},
             {modelID: 'TS0601', manufacturerName: '_TZE200_rsj5pu8y'},
             {modelID: 'TS0601', manufacturerName: '_TZE204_xu4a5rhj'},
+            {modelID: 'TS0601', manufacturerName: '_TZE200_2odrmqwq'},
         ],
         model: 'TS0601_cover_1',
         vendor: 'TuYa',
