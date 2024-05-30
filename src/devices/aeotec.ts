fff4ffb2d249b74e2b7804ef2c0f565f9b752cc8
iot-holding <162294299+iot-holding@users.noreply.github.com>
2024-05-30 21:09:11 +0200

    feat(add): ZGA002, ZGA003, ZGA004 (#7579)
    * add Aeotec Pico Series
    
    - Pico Switch (ZGA002)
    - Pico Switch Duo (ZGA003)
    - Pico Shutter (ZGA004)
    
    * ZGA004 update
    
    * Update aeotec.ts
    
    ---------
    
    Co-authored-by: mh-zwave <michael.hensche@aipilot.ai>
    Co-authored-by: mh-zwave <mh@z-wave.eu>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/aeotec.ts
+++ src/devices/aeotec.ts
@@ -1,4 +1,16 @@
 import {Definition} from '../lib/types';
+import {
+    deviceEndpoints,
+    deviceTemperature,
+    identify,
+    onOff,
+    electricityMeter,
+    windowCovering,
+    commandsWindowCovering,
+    commandsOnOff,
+    commandsLevelCtrl,
+} from '../lib/modernExtend';
+
 import fz from '../converters/fromZigbee';
 
 const definitions: Definition[] = [
@@ -10,6 +22,51 @@ const definitions: Definition[] = [
         fromZigbee: [fz.linkquality_from_basic],
         toZigbee: [],
         exposes: [],
+    },
+    {
+        zigbeeModel: ['ZGA002'],
+        model: 'ZGA002',
+        vendor: 'AEOTEC',
+        description: 'Pico switch with power meter',
+        extend: [
+            deviceEndpoints({endpoints: {'1': 1, '2': 2, '3': 3}}),
+            deviceTemperature(),
+            identify(),
+            onOff({powerOnBehavior: false}),
+            electricityMeter(),
+            commandsOnOff({endpointNames: ['2', '3']}),
+            commandsLevelCtrl({endpointNames: ['2', '3']}),
+        ],
+    },
+    {
+        zigbeeModel: ['ZGA003'],
+        model: 'ZGA003',
+        vendor: 'AEOTEC',
+        description: 'Pico switch duo with power meter',
+        extend: [
+            deviceEndpoints({endpoints: {'1': 1, '2': 2, '3': 3, '4': 4}}),
+            deviceTemperature(),
+            identify(),
+            onOff({powerOnBehavior: false, endpointNames: ['1', '2']}),
+            electricityMeter(),
+            commandsOnOff({endpointNames: ['3', '4']}),
+            commandsLevelCtrl({endpointNames: ['3', '4']}),
+        ],
+    },
+    {
+        zigbeeModel: ['ZGA004'],
+        model: 'ZGA004',
+        vendor: 'AEOTEC',
+        description: 'Pico shutter',
+        extend: [
+            deviceEndpoints({endpoints: {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5}}),
+            deviceTemperature(),
+            identify(),
+            windowCovering({controls: ['lift', 'tilt']}),
+            commandsWindowCovering({legacyAction: false, endpointNames: ['3']}),
+            commandsOnOff({endpointNames: ['4', '5']}),
+            commandsLevelCtrl({endpointNames: ['4', '5']}),
+        ],
     },
 ];
 
