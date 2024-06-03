b8e9ec9404947cf5882815dfc5457255aa540888
Carl <zhenyuc@outlook.com>
2024-06-03 14:34:29 +1000

    [New device support]: TuYa Universal Smart IR Control - UFO-R4Z

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6073,6 +6073,18 @@ const definitions: Definition[] = [
         exposes: [ez.learn_ir_code(), ez.learned_ir_code(), ez.ir_code_to_send()],
     },
     {
+        fingerprint: [{modelID: 'TS1201', manufacturerName: '_TZ3290_rlkmy85q4pzoxobl'}],
+        model: 'UFO-R4Z',
+        vendor: 'TuYa',
+        description: 'Universal smart IR remote control',
+        fromZigbee: [
+            fzZosung.zosung_send_ir_code_00, fzZosung.zosung_send_ir_code_01, fzZosung.zosung_send_ir_code_02,
+            fzZosung.zosung_send_ir_code_03, fzZosung.zosung_send_ir_code_04, fzZosung.zosung_send_ir_code_05,
+        ],
+        toZigbee: [tzZosung.zosung_ir_code_to_send, tzZosung.zosung_learn_ir_code],
+        exposes: [ez.learn_ir_code(), ez.learned_ir_code(), ez.ir_code_to_send()],
+    },
+    {
         fingerprint: [{modelID: 'TS0201', manufacturerName: '_TZ3000_itnrsufe'}],
         model: 'KCTW1Z',
         vendor: 'Tuya',
