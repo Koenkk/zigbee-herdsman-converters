5ade3df2b7762ca80031e67eb313cefb12a15a94
liubotongs <botong.liu@itead.cc>
2024-06-03 09:24:47 +0800

    Clear the Import BuffaloZclDataType code.

--- src/devices/sonoff.ts
+++ src/devices/sonoff.ts
@@ -1,5 +1,4 @@
 import {Zcl} from 'zigbee-herdsman';
-// import BuffaloZclDataType from 'zigbee-herdsman/dist/zcl/definition/buffaloZclDataType';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
 import tz from '../converters/toZigbee';
