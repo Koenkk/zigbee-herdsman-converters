a9584642c818d1bf5785a6cd92a8a1d2e7f2c0b0
Koen Kanters <koenkanters94@gmail.com>
2024-06-08 17:47:03 +0200

    feat(add): ST8EM-CON @Dis90 https://github.com/Koenkk/zigbee2mqtt/issues/22962

--- src/devices/ledvance.ts
+++ src/devices/ledvance.ts
@@ -25,6 +25,13 @@ const definitions: Definition[] = [
         extend: [ledvanceLight({colorTemp: {range: undefined}})],
     },
     {
+        zigbeeModel: ['TUBE_T8_CON_600_7.5W_840ZBV\u0000'],
+        model: 'ST8EM-CON',
+        vendor: 'LEDVANCE',
+        description: 'SubstiTUBE T8 EM connected 7.5 W/4000 K 600 mm',
+        extend: [ledvanceLight()],
+    },
+    {
         zigbeeModel: ['Panel Light 2x2 TW'],
         model: '74746',
         vendor: 'LEDVANCE',
