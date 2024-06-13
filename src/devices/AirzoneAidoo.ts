74022a6090847ef1a1b9f36be4705a92e346cc71
GitCharlieHub <58088043+GitCharlieHub@users.noreply.github.com>
2024-06-13 23:42:38 +0200

    Update AirzoneAidoo.ts

--- src/devices/AirzoneAidoo.ts
+++ src/devices/AirzoneAidoo.ts
@@ -1,12 +1,8 @@
-const {} = require('zigbee-herdsman-converters/lib/modernExtend');
-// Add the lines below
-const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
-const exposes = require('zigbee-herdsman-converters/lib/exposes');
-const reporting = require('zigbee-herdsman-converters/lib/reporting');
-const ota = require('zigbee-herdsman-converters/lib/ota');
-const utils = require('zigbee-herdsman-converters/lib/utils');
-const globalStore = require('zigbee-herdsman-converters/lib/store');
+import {Definition} from '../lib/types';
+import * as exposes from '../lib/exposes';
+import fz from '../converters/fromZigbee';
+import tz from '../converters/toZigbee';
+import * as reporting from '../lib/reporting';
 const e = exposes.presets;
 const ea = exposes.access;
 
