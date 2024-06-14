7656c85939c4bb86ef5c458ac8ba5ad72b34bfb0
GitCharlieHub <58088043+GitCharlieHub@users.noreply.github.com>
2024-06-14 14:23:11 +0200

    Update AirzoneAidoo.ts

--- src/devices/AirzoneAidoo.ts
+++ src/devices/AirzoneAidoo.ts
@@ -5,7 +5,14 @@ import * as tz from 'zzigbee-herdsman-converters/converters/toZigbee';
 import * as reporting from 'zigbee-herdsman-converters/lib/reporting';
 import {Endpoint} from 'zigbee-herdsman/dist/controller/model';
 
+import {Definition} from 'zigbee-herdsman-converters/lib/types';
+import {presets, access} from 'zigbee2mqtt/lib/exposes';
+import * as fz from 'zigbee2mqtt/lib/converters/fromZigbee';
+import * as tz from 'zigbee2mqtt/lib/converters/toZigbee';
+import * as reporting from 'zigbee2mqtt/lib/reporting';
+import {Endpoint} from 'zigbee-herdsman/dist/controller/model';
 
+const definition: Definition = {
     zigbeeModel: ['Aidoo Zigbee'],
     model: 'Aidoo Zigbee AZAI6ZBEMHI',
     vendor: 'Airzone',
