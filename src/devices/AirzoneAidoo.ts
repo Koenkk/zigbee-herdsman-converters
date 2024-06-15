2c38838c5c16ec7578b642c0f0e407ff2e49bbbc
GitCharlieHub <58088043+GitCharlieHub@users.noreply.github.com>
2024-06-15 22:13:51 +0200

    Update AirzoneAidoo.ts

--- src/devices/AirzoneAidoo.ts
+++ src/devices/AirzoneAidoo.ts
@@ -1,3 +1,4 @@
+import {Definition, Device, Endpoint, Logger} from '../lib/types';
 import * as exposes from '../lib/exposes';
 import fz from '../converters/fromZigbee';
 import tz from '../converters/toZigbee';
