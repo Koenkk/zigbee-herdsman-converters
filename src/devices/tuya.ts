b1ee7636236dc8c6e2ba9267df26bd8bc1286dfc
Martin Hübner <marthubner@gmail.com>
2024-06-24 22:21:20 +0200

    Add support for _TZ3000_huvxrx4i

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -3025,7 +3025,7 @@ const definitions: Definition[] = [
         ],
     },
     {
-        fingerprint: tuya.fingerprint('TS0002', ['_TZ3000_aaifmpuq', '_TZ3000_irrmjcgi']),
+        fingerprint: tuya.fingerprint('TS0002', ['_TZ3000_aaifmpuq', '_TZ3000_irrmjcgi', '_TZ3000_huvxrx4i']),
         model: 'TS0002_power',
         vendor: 'Tuya',
         description: '2 gang switch with power monitoring',
