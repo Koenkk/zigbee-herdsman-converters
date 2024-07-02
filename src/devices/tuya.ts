435d7ee222e635e993f8154de4076f764502a883
krikkoo <43918347+krikkoo@users.noreply.github.com>
2024-07-02 08:44:09 +0200

    Update tuya.ts
    Added Human presence sensor 24G from vendor  '_TZE204_uxllnywp'

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -10364,6 +10364,43 @@ const definitions: Definition[] = [
             }
         },
     },
+    {
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_uxllnywp']),
+        model: 'RTC ZCZ03Z',
+        vendor: 'TuYa',
+        description: 'Human presence sensor 24G',
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        onEvent: console.log,
+    
+        exposes: [
+            e.illuminance_lux(),
+            exposes.enum('presence_state', ea.STATE, ["none","large_move"])
+                .withDescription('Presence'),
+            exposes.numeric('detection_distance_max', ea.STATE_SET).withValueMin(0).withValueMax(840).withValueStep(1)
+                .withDescription('Max detection distance').withUnit('cm'),
+            exposes.numeric('detection_distance_min', ea.STATE_SET).withValueMin(0).withValueMax(840).withValueStep(1)
+                .withDescription('Min detection distance').withUnit('cm'),
+                    e.numeric('target_distance', ea.STATE).withDescription('Distance to target').withUnit('cm'),
+            exposes.numeric('fading_time', ea.STATE_SET).withValueMin(1).withValueMax(59).withValueStep(1)
+                .withDescription('Delay time').withUnit('s'),
+            exposes.numeric('presence_sensitivity', ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1)
+                .withDescription('Presence sensitivity'),
+            e.binary('indicator', ea.STATE_SET, 'ON', 'OFF').withDescription('LED Indicator'),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [1, 'presence_state', tuya.valueConverterBasic.lookup({'None': 0,'Someone': 4})],
+                [101, 'target_distance', tuya.valueConverter.raw],
+                [102, 'illuminance_lux', tuya.valueConverter.raw],
+                [103, 'fading_time', tuya.valueConverter.raw],
+                [104, 'indicator', tuya.valueConverter.onOff],
+                [107, 'detection_distance_max', tuya.valueConverter.raw],
+                [108, 'detection_distance_min', tuya.valueConverter.raw],
+                [111, 'presence_sensitivity', tuya.valueConverter.raw],
+            ],
+        },
+    },
 ];
 
 export default definitions;
