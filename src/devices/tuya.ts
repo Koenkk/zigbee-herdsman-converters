c1e50113ff2e36a8504b313c6c9064b8956da011
Koen Kanters <koenkanters94@gmail.com>
2024-07-02 20:55:08 +0200

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -10370,18 +10370,27 @@ const definitions: Definition[] = [
         vendor: 'Tuya',
         description: 'Human presence sensor 24G',
         fromZigbee: [tuya.fz.datapoints],
-        toZigbee: [tuya.tz.datapoints],    
+        toZigbee: [tuya.tz.datapoints],
         exposes: [
-            e.illuminance_lux(), e.presence(),
-            e.numeric('detection_distance_max', ea.STATE_SET).withValueMin(0).withValueMax(840).withValueStep(1)
-                .withDescription('Max detection distance').withUnit('cm'),
-            e.numeric('detection_distance_min', ea.STATE_SET).withValueMin(0).withValueMax(840).withValueStep(1)
-                .withDescription('Min detection distance').withUnit('cm'),
-                    e.numeric('target_distance', ea.STATE).withDescription('Distance to target').withUnit('cm'),
-            e.numeric('fading_time', ea.STATE_SET).withValueMin(1).withValueMax(59).withValueStep(1)
-                .withDescription('Delay time').withUnit('s'),
-            e.numeric('presence_sensitivity', ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1)
-                .withDescription('Presence sensitivity'),
+            e.illuminance_lux(),
+            e.presence(),
+            e
+                .numeric('detection_distance_max', ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(840)
+                .withValueStep(1)
+                .withDescription('Max detection distance')
+                .withUnit('cm'),
+            e
+                .numeric('detection_distance_min', ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(840)
+                .withValueStep(1)
+                .withDescription('Min detection distance')
+                .withUnit('cm'),
+            e.numeric('target_distance', ea.STATE).withDescription('Distance to target').withUnit('cm'),
+            e.numeric('fading_time', ea.STATE_SET).withValueMin(1).withValueMax(59).withValueStep(1).withDescription('Delay time').withUnit('s'),
+            e.numeric('presence_sensitivity', ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1).withDescription('Presence sensitivity'),
             e.binary('indicator', ea.STATE_SET, 'ON', 'OFF').withDescription('LED Indicator'),
         ],
         meta: {
