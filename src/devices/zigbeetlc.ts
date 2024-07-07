b89f1c3c2b224109119523660a9a61105e128632
Rob van Oostenrijk <robvanoostenrijk@noreply.users.github.com>
2024-07-07 19:21:14 +0400

    zigbeetlc: Comfort parameters should scale at 100, while humidity/temp offset is multiplied by 10 in firmware itself.

--- src/devices/zigbeetlc.ts
+++ src/devices/zigbeetlc.ts
@@ -17,8 +17,8 @@ const extend = {
         cluster: 'hvacUserInterfaceCfg',
         attribute: {ID: 0x0105, type: Zcl.DataType.UINT16},
         valueMin: 0,
-        valueMax: 9999,
-        scale: 10,
+        valueMax: 100,
+        scale: 100,
         description: 'Comfort parameters/Humidity maximum, in 1% steps, default 60.00%.',
     }),
     comfortHumidityMin: numeric({
@@ -27,8 +27,8 @@ const extend = {
         cluster: 'hvacUserInterfaceCfg',
         attribute: {ID: 0x0104, type: Zcl.DataType.UINT16},
         valueMin: 0,
-        valueMax: 9999,
-        scale: 10,
+        valueMax: 100,
+        scale: 100,
         description: 'Comfort parameters/Humidity minimum, in 1% steps, default 40.00%',
     }),
     comfortTemperatureMin: numeric({
@@ -39,7 +39,7 @@ const extend = {
         valueMin: -50.0,
         valueMax: 120.0,
         valueStep: 0.01,
-        scale: 10,
+        scale: 100,
         description: 'Comfort parameters/Temperature minimum, in 0.01°C steps, default 20.00°C.',
     }),
     comfortTemperatureMax: numeric({
@@ -50,7 +50,7 @@ const extend = {
         valueMin: -50.0,
         valueMax: 120.0,
         valueStep: 0.01,
-        scale: 10,
+        scale: 100,
         description: 'Comfort parameters/Temperature maximum, in 0.01°C steps, default 25.00°C.',
     }),
     display: binary({
