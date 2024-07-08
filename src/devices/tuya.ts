db6050a85cf92ff7bf8c269e6f61da793990674e
CausticSoul <44197967+CausticSoul@users.noreply.github.com>
2024-07-08 16:21:57 +1000

    Update tuya.ts
    Added _TZE200_qanl25yu as Tuya TS0601_fan_and_light_switch
    
    Fixed spelling error in status_description of the above device: "Speed of the fan" should be "Speed of the fan"

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -7854,7 +7854,7 @@ const definitions: Definition[] = [
         whiteLabel: [{vendor: 'Lerlink', model: 'T2-Z67/T2-W67'}],
     },
     {
-        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_hmqzfqml']),
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_hmqzfqml', '_TZE200_qanl25yu']),
         model: 'TS0601_fan_and_light_switch',
         vendor: 'Tuya',
         description: 'Fan & light switch',
@@ -7865,7 +7865,7 @@ const definitions: Definition[] = [
             e.binary('status_indication', ea.STATE_SET, 'ON', 'OFF').withDescription('Light switch'),
             tuya.exposes.switch(),
             e.power_on_behavior(['OFF', 'ON']).withAccess(ea.STATE_SET),
-            e.enum('fan_speed', ea.STATE_SET, ['minimum', 'medium', 'maximum']).withDescription('Speed off the fan'),
+            e.enum('fan_speed', ea.STATE_SET, ['minimum', 'medium', 'maximum']).withDescription('Speed of the fan'),
         ],
         meta: {
             tuyaDatapoints: [
