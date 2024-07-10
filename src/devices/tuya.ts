30a972e19148c6f3c60615a7feb23ca3854be2c3
Koen Kanters <koenkanters94@gmail.com>
2024-07-10 18:06:30 +0200

    fix(detect): Detect `_TZ3000_hzlsaltw` as Tuya TS0001_power https://github.com/Koenkk/zigbee2mqtt/issues/23143

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -3361,6 +3361,7 @@ const definitions: Definition[] = [
             '_TZ3000_g92baclx',
             '_TZ3000_qlai3277',
             '_TZ3000_qaabwu5c',
+            '_TZ3000_hzlsaltw',
         ]),
         model: 'TS0001_power',
         description: 'Switch with power monitoring',
