5fd150ea2f3652f8812f30e4ffdbcbc7cbe887b1
Christian Bjørnbak <cbjornbak@liveintent.com>
2024-07-16 21:28:02 +0200

    feat(add): D160-ZG (#7763)
    * Add Hilux Dimmer-Switch-ZB3.0
    
    * Add fingerPrint
    
    * Update hilux.ts
    
    * Update hilux.ts
    
    ---------
    
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/hilux.ts
+++ src/devices/hilux.ts
@@ -9,6 +9,13 @@ const definitions: Definition[] = [
         description: 'Spot 7W',
         extend: [light({colorTemp: {range: [153, 370]}, powerOnBehavior: false})],
     },
+    {
+        fingerprint: [{modelID: 'Dimmer-Switch-ZB3.0', manufacturerName: 'Hilux'}],
+        model: 'D160-ZG',
+        vendor: 'Hilux',
+        description: 'Zigbee LED dimmer smart switch',
+        extend: [light({configureReporting: true})],
+    },
 ];
 
 export default definitions;
