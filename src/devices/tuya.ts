e8de6023530d7f34b058406100ed86add9beb7bb
Burkhard Kneiseler <burkhard@kneiseler.de>
2024-07-18 15:27:12 +0200

    feat(add): Wenzhi ZM-100 (_TZE204_laokfqwu)
    Tried with already implemented Radar-Sensors. But Tuya-DPs are different. 
    
    Tuya-DPs taken from here: https://github.com/wzwenzhi/Wenzhi-ZigBee2mqtt/blob/main/wenzhi_tuya_M100_240704.js
    
    Thanks @wzwenzhi for building the external converter.

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6988,6 +6988,87 @@ const definitions: Definition[] = [
         whiteLabel: [tuya.whitelabel('iHseno', 'TY_24G_Sensor_V2', 'Human presence sensor 24G', ['_TZE204_ztqnh5cg'])],
     },
     {
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_laokfqwu']),
+        model: 'WZ-M100',
+        vendor: 'Wenzhi',
+        description: 'Human presence sensor',
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        onEvent: tuya.onEventSetTime,
+        configure: tuya.configureMagicPacket,
+        exposes: [
+            e.illuminance_lux(),
+            e
+                .enum('presence_state', ea.STATE, ['none', 'presence'])
+                .withDescription('presence_state'),
+            e
+                .numeric('target_distance', ea.STATE)
+                .withDescription('Distance to target')
+                .withUnit('m'),
+            e
+                .numeric('sensitivity', ea.STATE_SET)
+                .withValueMin(1)
+                .withValueMax(9)
+                .withValueStep(1)
+                .withDescription('sensitivity of the radar'),
+            e
+                .numeric('near_detection', ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(10.0)
+                .withDescription('minimum detection range')
+                .withValueStep(0.1)
+                .withUnit('m'),
+            e
+                .numeric('far_detection', ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(10.0)
+                .withDescription('maximum detection range')
+                .withValueStep(0.1)
+                .withUnit('m'),
+            e
+                .numeric('distance', ea.STATE)
+                .withValueMin(0)
+                .withValueMax(10.0)
+                .withDescription('target distance')
+                .withValueStep(0.01)
+                .withUnit('m'),
+            e
+                .numeric('interval_time', ea.STATE_SET)
+                .withValueMin(1)
+                .withValueMax(3600)
+                .withDescription('interval_time')
+                .withValueStep(1)
+                .withUnit('s'),
+            e
+                .numeric('detection_delay', ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(10.0)
+                .withValueStep(0.1)
+                .withUnit('s')
+                .withDescription('detection delay'),
+            e
+                .numeric('fading_time', ea.STATE_SET)
+                .withValueMax(1500)
+                .withValueMin(5)
+                .withValueStep(5)
+                .withUnit('s')
+                .withDescription('presence timeout'),
+],
+        meta: {
+            tuyaDatapoints: [
+                [1, 'presence_state', tuya.valueConverterBasic.lookup({'none': tuya.enum(0), 'presence': tuya.enum(1)})],
+                [2, 'sensitivity', tuya.valueConverter.raw],
+                [3, 'near_detection', tuya.valueConverter.divideBy100],
+                [4, 'far_detection', tuya.valueConverter.divideBy100],
+				[9, 'distance', tuya.valueConverter.divideBy100],
+				[103, 'illuminance_value', tuya.valueConverter.raw],
+				[104, 'interval_time', tuya.valueConverter.raw],
+				[105, 'detection_delay', tuya.valueConverter.divideBy10],
+				[106, 'fading_time', tuya.valueConverter.divideBy10],
+            ],
+        },
+    },
+    {
         fingerprint: tuya.fingerprint('TS0225', ['_TZE200_hl0ss9oa']),
         model: 'ZG-205ZL',
         vendor: 'Tuya',
