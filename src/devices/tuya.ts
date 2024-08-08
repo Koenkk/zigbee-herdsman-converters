ef7481fbd93ca9a7a873d66d9a330420af0d55b5
electronicm <electronicm@users.noreply.github.com>
2024-08-08 12:15:33 +0200

    fix wrong fingerprint

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -2282,8 +2282,8 @@ const definitions: Definition[] = [
             return {l1: 1, l2: 1};
         },
         whiteLabel: [
-            tuya.whitelabel('Moes', 'MS-105B-M', '2 gang smart dimmer module', ['_TZE200_tsxpl0d0']),
-            tuya.whitelabel('Moes', 'ZM-105B-M', '2 gang smart dimmer module', ['_TZE200_tsxpl0d0']),
+            {vendor: 'Moes', model: 'MS-105B-M'},
+            tuya.whitelabel('Moes', 'ZM-105B-M', '2 gang smart dimmer module', ['_TZE204_bxoo2swd']),
             tuya.whitelabel('KnockautX', 'FMD2C018', '2 gang smart dimmer module', ['_TZE200_tsxpl0d0']),
         ],
     },
