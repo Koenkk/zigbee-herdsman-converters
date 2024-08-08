335979f0ba602d820f76752e083d6c49e9b853ae
Shadow Panther <panther.ru@gmail.com>
2024-08-08 23:23:45 +0200

    Add Avatto ZWSH16 Smart Temperature and Humidity Detector device definition

---
+++ src/devices/avatto.ts
@@ -0,0 +1,34 @@
+import * as exposes from '../lib/exposes';
+import * as tuya from '../lib/tuya';
+import {Definition} from '../lib/types';
+const e = exposes.presets;
+const ea = exposes.access;
+
+const definitions: Definition[] = [
+    {
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE204_s139roas',}],
+        model: 'ZWSH16',
+        vendor: 'Avatto',
+        description: 'Smart Temperature and Humidity Detector',
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        onEvent: tuya.onEventSetTime,
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await tuya.configureMagicPacket(device, coordinatorEndpoint);
+            await endpoint.command('manuSpecificTuya', 'mcuVersionRequest', {seq: 0x0002});
+        },
+        exposes: [e.battery(), e.temperature(), e.humidity()],
+        meta: {
+            tuyaDatapoints: [
+                [1, 'temperature', tuya.valueConverter.divideBy10],
+                [2, 'humidity', tuya.valueConverter.raw],
+                [9, 'temperature_scale', tuya.valueConverterBasic.lookup({celsius: 0, fahrenheit: 1})],
+                [14, 'battery_state', tuya.valueConverter.batteryState],
+                [15, 'battery', tuya.valueConverter.raw],
+            ],    },
+    },
+];
+
+export default definitions;
+module.exports = definitions;
