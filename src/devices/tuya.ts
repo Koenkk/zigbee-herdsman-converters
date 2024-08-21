b26a7c60960454a212682d01455daf9d5e3003e2
Koen Kanters <koenkanters94@gmail.com>
2024-08-21 21:41:06 +0200

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6351,7 +6351,7 @@ const definitions: Definition[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint('TS0222', ['_TZ3000_kky16aay']),
+        fingerprint: tuya.fingerprint('TS0222', ['_TZ3000_kky16aay', '_TZE204_myd45weu']),
         model: 'TS0222_temperature_humidity',
         vendor: 'Tuya',
         description: 'Temperature & humidity sensor',
