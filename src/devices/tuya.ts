941ad3cabb9a26ece89d3cfa401bdaf27f7eaac1
Burkhard Kneiseler <burkhard@kneiseler.de>
2024-08-29 17:13:36 +0200

    feat(add): THE01860A (#7914)
    * ADD: Tuya, THE01860A, 'Soil sensor with illuminance, _TZ3000_t9qqxn70
    
    * FIX: Whitelabel
    
    * FIX: Pretier

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6439,7 +6439,7 @@ const definitions: Definition[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint('TS0222', ['_TZ3000_kky16aay', '_TZE204_myd45weu']),
+        fingerprint: tuya.fingerprint('TS0222', ['_TZ3000_kky16aay', '_TZE204_myd45weu', '_TZ3000_t9qqxn70']),
         model: 'TS0222_temperature_humidity',
         vendor: 'Tuya',
         description: 'Temperature & humidity sensor',
@@ -6447,7 +6447,10 @@ const definitions: Definition[] = [
         toZigbee: [],
         configure: tuya.configureMagicPacket,
         exposes: [e.battery(), e.temperature(), e.humidity(), e.illuminance()],
-        whiteLabel: [tuya.whitelabel('Tuya', 'QT-07S', 'Soil sensor', ['_TZE204_myd45weu'])],
+        whiteLabel: [
+            tuya.whitelabel('Tuya', 'QT-07S', 'Soil sensor', ['_TZE204_myd45weu']),
+            tuya.whitelabel('Tuya', 'THE01860A', 'Soil sensor with illuminance', ['_TZ3000_t9qqxn70']),
+        ],
     },
     {
         fingerprint: [
