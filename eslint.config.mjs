0be83b68850897a1b19ea3dea0d977f824ac9cde
Koen Kanters <koenkanters94@gmail.com>
2024-09-08 22:09:13 +0200

    Update to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+            },
+        },
+        rules: {
+            '@typescript-eslint/await-thenable': 'off', // TODO error
+            '@typescript-eslint/ban-ts-comment': 'off', // TODO error
+            '@typescript-eslint/explicit-function-return-type': 'off', // TODO error
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-unused-vars': 'off', // TODO error
+            'array-bracket-spacing': ['error', 'never'],
+            'no-return-await': 'off', // TODO error
+            'object-curly-spacing': ['error', 'never'],
+            '@typescript-eslint/no-floating-promises': 'error',
+            'no-prototype-builtins': 'off', // TODO remove (error by default)
+        },
+    },
+    {
+        ignores: ['test/', 'index.*', 'converters/', 'devices/', 'lib/', 'scripts/', '**/*.mjs'],
+    },
+    eslintConfigPrettier,
+);
