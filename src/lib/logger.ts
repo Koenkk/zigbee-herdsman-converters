fd3b072b0414eab057a1aaaab278147f3fb5da91
Tomer <57483589+tomer-w@users.noreply.github.com>
2024-09-08 09:28:33 +0300

    Fix prettier issue

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,11 +1,15 @@
 import {Logger} from './types';
 
 export let logger: Logger = {
-    isEnabled: () => true, 
-    debug: (messageOrLambda, namespace) => console.debug(`${namespace}: ${(typeof messageOrLambda === "function") ? messageOrLambda() : messageOrLambda}`),
-    info: (messageOrLambda, namespace) => console.info(`${namespace}: ${(typeof messageOrLambda === "function") ? messageOrLambda() : messageOrLambda}`),
-    warning: (messageOrLambda, namespace) => console.warn(`${namespace}: ${(typeof messageOrLambda === "function") ? messageOrLambda() : messageOrLambda}`),
-    error: (messageOrLambda, namespace) => console.error(`${namespace}: ${(typeof messageOrLambda === "function") ? messageOrLambda() : messageOrLambda}`),
+    isEnabled: () => true,
+    debug: (messageOrLambda, namespace) =>
+        console.debug(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
+    info: (messageOrLambda, namespace) =>
+        console.info(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
+    warning: (messageOrLambda, namespace) =>
+        console.warn(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
+    error: (messageOrLambda, namespace) =>
+        console.error(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
 };
 
 export function setLogger(l: Logger): void {
