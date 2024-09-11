decaa92b5a8ad8ded8e2e6d7c086518f46d9a03f
Tomer <57483589+tomer-w@users.noreply.github.com>
2024-09-11 21:55:05 +0300

    fix(ignore): Improve performance when logging is disabled (#7955)
    * Improve logger performance
    
    * Align package versions
    
    * Fix eslint issue
    
    * Align built-in logger with the main implementation
    
    * Revert zigbee-herdsman package version so tests will run
    
    * Fix prettier issue
    
    * Fix CR comments
    
    * Revert package changes

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,14 @@
 import {Logger} from './types';
 
 export let logger: Logger = {
-    debug: (message, namespace) => console.debug(`${namespace}: ${message}`),
-    info: (message, namespace) => console.info(`${namespace}: ${message}`),
-    warning: (message, namespace) => console.warn(`${namespace}: ${message}`),
-    error: (message, namespace) => console.error(`${namespace}: ${message}`),
+    debug: (messageOrLambda, namespace) =>
+        console.debug(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
+    info: (messageOrLambda, namespace) =>
+        console.info(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
+    warning: (messageOrLambda, namespace) =>
+        console.warn(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
+    error: (messageOrLambda, namespace) =>
+        console.error(`${namespace}: ${typeof messageOrLambda === 'function' ? messageOrLambda() : messageOrLambda}`),
 };
 
 export function setLogger(l: Logger): void {
