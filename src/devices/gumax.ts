4b3c396a007290376c4f1e4a9bcf20d2f6867193
Bas du Pré <me@basdupre.com>
2024-09-12 12:05:14 +0200

    Added definition for the Gumax Lighting System

---
+++ src/devices/gumax.ts
@@ -0,0 +1,18 @@
+import {
+    light,
+} from '../lib/modernExtend';
+import {DefinitionWithExtend} from '../lib/types';
+
+const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ['LST103'],
+        model: 'LST103',
+        vendor: 'Gumax',
+        description: 'Gumax Lighting System',
+        extend: [light({colorTemp: {range: [153, 370]}, color: {modes: ["xy", "hs"], enhancedHue: true}})],
+        meta: {},
+    },
+];
+
+export default definitions;
+module.exports = definitions;
