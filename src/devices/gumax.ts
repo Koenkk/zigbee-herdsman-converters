599570d6bdd73f7a9bda544b12f2f5e322240788
Bas du Pré <me@basdupre.com>
2024-09-12 12:07:56 +0200

    Fix linting error

--- src/devices/gumax.ts
+++ src/devices/gumax.ts
@@ -1,6 +1,4 @@
-import {
-    light,
-} from '../lib/modernExtend';
+import {light} from '../lib/modernExtend';
 import {DefinitionWithExtend} from '../lib/types';
 
 const definitions: DefinitionWithExtend[] = [
@@ -9,7 +7,7 @@ const definitions: DefinitionWithExtend[] = [
         model: 'LST103',
         vendor: 'Gumax',
         description: 'Gumax Lighting System',
-        extend: [light({colorTemp: {range: [153, 370]}, color: {modes: ["xy", "hs"], enhancedHue: true}})],
+        extend: [light({colorTemp: {range: [153, 370]}, color: {modes: ['xy', 'hs'], enhancedHue: true}})],
         meta: {},
     },
 ];
