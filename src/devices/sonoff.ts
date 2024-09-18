5e70c3339278a083aea1cc0d386747f015270d3a
Koen Kanters <koenkanters94@gmail.com>
2024-09-18 20:31:28 +0200

    u

--- src/devices/sonoff.ts
+++ src/devices/sonoff.ts
@@ -813,11 +813,7 @@ const definitions: DefinitionWithExtend[] = [
         vendor: 'SONOFF',
         whiteLabel: [{vendor: 'Woolley', model: 'SA-029-1'}],
         description: 'Smart Plug',
-        extend: [onOff()],
-        configure: async (device, coordinatorEndpoint) => {
-            device.powerSource = 'Mains (single phase)';
-            device.save();
-        },
+        extend: [onOff(), forcePowerSource({powerSource: 'Mains (single phase)'})],
     },
     {
         zigbeeModel: ['SNZB-01P'],
