de3ad0421b606a41f520de08fe90fe779d1ef905
marcelhoogantink <marcel@hoogantink.com>
2024-09-22 15:58:12 +0200

    Updated TS0205 smoke sensor settings

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -812,18 +812,6 @@ const definitions: DefinitionWithExtend[] = [
         exposes: [e.gas(), e.tamper()],
     },
     {
-        fingerprint: [
-            {
-                modelID: 'TS0205',
-                manufacturerName: '_TZ3210_up3pngle',
-            },
-        ],
-        model: 'TS0205_smoke_2',
-        vendor: 'Tuya',
-        description: 'Smoke sensor',
-        extend: [battery(), iasZoneAlarm({zoneType: 'smoke', zoneAttributes: ['alarm_1', 'tamper']})],
-    },
-    {
         zigbeeModel: ['TS0205'],
         model: 'TS0205',
         vendor: 'Tuya',
@@ -831,13 +819,20 @@ const definitions: DefinitionWithExtend[] = [
         whiteLabel: [
             {vendor: 'Tesla Smart', model: 'TSL-SEN-SMOKE'},
             {vendor: 'Dongguan Daying Electornics Technology', model: 'YG400A'},
+            tuya.whitelabel('Tuya', 'TS0205_smoke_2', 'Smoke sensor', ['_TZ3210_up3pngle']),
         ],
-        fromZigbee: [fz.ias_smoke_alarm_1, fz.ignore_basic_report],
-        toZigbee: [],
-        exposes: [e.smoke(), e.battery_low(), e.tamper()],
         // Configure battery % fails
         // https://github.com/Koenkk/zigbee2mqtt/issues/22421
-        extend: [battery({percentageReporting: false})],
+        extend: [battery({percentageReporting: false}), iasZoneAlarm({zoneType: 'smoke', zoneAttributes: ['alarm_1', 'tamper']})],
+        configure: async (device, coordinatorEndpoint) => {
+            if (device?.manufacturerName === '_TZ3210_up3pngle') {
+                // Required for this version
+                // https://github.com/Koenkk/zigbee-herdsman-converters/pull/8004
+                const endpoint = device.getEndpoint(1);
+                await reporting.bind(endpoint, coordinatorEndpoint, ['genPowerCfg']);
+                await reporting.batteryPercentageRemaining(endpoint);
+            }
+        },
     },
     {
         zigbeeModel: ['TS0111'],
