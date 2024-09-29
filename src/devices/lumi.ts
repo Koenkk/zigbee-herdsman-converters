e7bd227ec0814af6bb91bb5c22b7f4e862acada6
Ivan Pankratov <im.pankratov@ya.ru>
2024-09-29 23:14:44 +0500

    fix: Fix Lumi pet feeder LED indicator control (#8041)
    * fix(lumi): pet feeder LED indicator control
    
    After updating to firmware version 9761, LED control doesn't work at
    all. Zigbee2mqtt reports "OFF" state, while indicator is turned on.
    Controlling the LED state either via zigbee2mqtt or via Home Assistant
    doesn't result in any change.
    
    I can only assume that previous firmware version (9505) got incorrect
    status mapping, while latest (9761) fixed this.
    
    * feat(lumi): pet feeder LED indicator mode description
    
    Based on settings from Aqara app.
    
    * fix(lumi): move some "pet feeder" props under "config" category

--- src/devices/lumi.ts
+++ src/devices/lumi.ts
@@ -3849,11 +3849,27 @@ const definitions: DefinitionWithExtend[] = [
                         .withFeature(e.numeric('size', exposes.access.STATE_SET)),
                 )
                 .withDescription('Feeding schedule'),
-            e.binary('led_indicator', ea.STATE_SET, 'ON', 'OFF').withDescription('Led indicator'),
+            e
+                .binary('led_indicator', ea.STATE_SET, 'ON', 'OFF')
+                .withLabel('Disable LED at night')
+                .withDescription('LED indicator will be disabled every day from 21:00 to 09:00')
+                .withCategory('config'),
             e.child_lock(),
             e.enum('mode', ea.STATE_SET, ['schedule', 'manual']).withDescription('Feeding mode'),
-            e.numeric('serving_size', ea.STATE_SET).withValueMin(1).withValueMax(10).withDescription('One serving size').withUnit('portion'),
-            e.numeric('portion_weight', ea.STATE_SET).withValueMin(1).withValueMax(20).withDescription('Portion weight').withUnit('g'),
+            e
+                .numeric('serving_size', ea.STATE_SET)
+                .withValueMin(1)
+                .withValueMax(10)
+                .withDescription('One serving size')
+                .withUnit('portion')
+                .withCategory('config'),
+            e
+                .numeric('portion_weight', ea.STATE_SET)
+                .withValueMin(1)
+                .withValueMax(20)
+                .withDescription('Portion weight')
+                .withUnit('g')
+                .withCategory('config'),
         ],
         extend: [lumiZigbeeOTA()],
         configure: async (device, coordinatorEndpoint) => {
