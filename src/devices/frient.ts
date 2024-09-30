dd50a25c6b027ae265d95db24043f6c0e62a50e0
Thomas Bowman Mørch <thomas.git@bowmo.dk>
2024-09-30 21:32:29 +0200

    Convert to modernExtend functions

--- src/devices/frient.ts
+++ src/devices/frient.ts
@@ -4,7 +4,7 @@ import {develcoModernExtend} from '../lib/develco';
 import * as exposes from '../lib/exposes';
 import {battery, electricityMeter, onOff, ota} from '../lib/modernExtend';
 import * as reporting from '../lib/reporting';
-import {DefinitionWithExtend, Fz, Tz} from '../lib/types';
+import {DefinitionWithExtend, Fz, ModernExtend, Tz} from '../lib/types';
 import * as utils from '../lib/utils';
 
 const e = exposes.presets;
@@ -15,57 +15,56 @@ const ea = exposes.access;
 // frient/develco specific constants
 const manufacturerOptions = {manufacturerCode: Zcl.ManufacturerCode.DEVELCO};
 
-// frient/develco specific convertors
-const frient = {
-    fz: {
-        pulse_configuration: {
-            cluster: 'seMetering',
-            type: ['attributeReport', 'readResponse'],
-            convert: (model, msg, publish, options, meta) => {
-                const result: Record<string, unknown> = {};
-                if (msg.data?.develcoPulseConfiguration) {
-                    result[utils.postfixWithEndpointName('pulse_configuration', msg, model, meta)] = msg.data['develcoPulseConfiguration'];
-                }
+function current_summation() {
+    return {
+        isModernExtend: true,
+        toZigbee: [
+            {
+                key: ['current_summation'],
+                convertSet: async (entity, key, value, meta) => {
+                    await entity.write('seMetering', {develcoCurrentSummation: value}, utils.getOptions(meta.mapped, entity));
+                    return {state: {current_summation: value}};
+                },
+            } satisfies Tz.Converter,
+        ],
+        exposes: [
+            e
+                .numeric('current_summation', ea.SET)
+                .withDescription('Current summation value sent to the display. e.g. 570 = 0,570 kWh')
+                .withValueMin(0)
+                .withValueMax(268435455),
+        ],
+    } satisfies ModernExtend;
+}
 
-                return result;
-            },
-        } satisfies Fz.Converter,
-    },
-    tz: {
-        pulse_configuration: {
-            key: ['pulse_configuration'],
-            convertSet: async (entity, key, value, meta) => {
-                await entity.write('seMetering', {develcoPulseConfiguration: value}, utils.getOptions(meta.mapped, entity));
-                return {readAfterWriteTime: 200, state: {pulse_configuration: value}};
-            },
-            convertGet: async (entity, key, meta) => {
-                await entity.read('seMetering', ['develcoPulseConfiguration'], manufacturerOptions);
-            },
-        } satisfies Tz.Converter,
-        current_summation: {
-            key: ['current_summation'],
-            convertSet: async (entity, key, value, meta) => {
-                await entity.write('seMetering', {develcoCurrentSummation: value}, utils.getOptions(meta.mapped, entity));
-                return {state: {current_summation: value}};
-            },
-        } satisfies Tz.Converter,
-    },
-};
+function pulse_configuration() {
+    return {
+        isModernExtend: true,
+        fromZigbee: [
+            {
+                cluster: 'seMetering',
+                type: ['attributeReport', 'readResponse'],
+                convert: (model, msg, publish, options, meta) => {
+                    const result: Record<string, unknown> = {};
+                    if (msg.data?.develcoPulseConfiguration) {
+                        result[utils.postfixWithEndpointName('pulse_configuration', msg, model, meta)] = msg.data['develcoPulseConfiguration'];
+                    }
 
-const definitions: DefinitionWithExtend[] = [
-    {
-        zigbeeModel: ['EMIZB-141'],
-        model: 'EMIZB-141',
-        vendor: 'Frient A/S',
-        description: 'frient Electricity Meter Interface 2 LED',
-        fromZigbee: [frient.fz.pulse_configuration],
-        toZigbee: [frient.tz.pulse_configuration, frient.tz.current_summation],
-        extend: [
-            ota(),
-            electricityMeter({cluster: 'metering', power: {divisor: 1000, multiplier: 1}, energy: {divisor: 1000, multiplier: 1}}),
-            battery(),
-            develcoModernExtend.addCustomClusterManuSpecificDevelcoGenBasic(),
-            develcoModernExtend.readGenBasicPrimaryVersions(),
+                    return result;
+                },
+            } satisfies Fz.Converter,
+        ],
+        toZigbee: [
+            {
+                key: ['pulse_configuration'],
+                convertSet: async (entity, key, value, meta) => {
+                    await entity.write('seMetering', {develcoPulseConfiguration: value}, utils.getOptions(meta.mapped, entity));
+                    return {readAfterWriteTime: 200, state: {pulse_configuration: value}};
+                },
+                convertGet: async (entity, key, meta) => {
+                    await entity.read('seMetering', ['develcoPulseConfiguration'], manufacturerOptions);
+                },
+            } satisfies Tz.Converter,
         ],
         exposes: [
             e
@@ -73,12 +72,26 @@ const definitions: DefinitionWithExtend[] = [
                 .withValueMin(0)
                 .withValueMax(65535)
                 .withDescription('Pulses per kwh. Default 1000 imp/kWh. Range 0 to 65535'),
-            e
-                .numeric('current_summation', ea.SET)
-                .withDescription('Current summation value sent to the display. e.g. 570 = 0,570 kWh')
-                .withValueMin(0)
-                .withValueMax(268435455),
         ],
+    } satisfies ModernExtend;
+}
+
+const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ['EMIZB-141'],
+        model: 'EMIZB-141',
+        vendor: 'Frient A/S',
+        description: 'frient Electricity Meter Interface 2 LED',
+        extend: [
+            ota(),
+            electricityMeter({cluster: 'metering', power: {divisor: 1000, multiplier: 1}, energy: {divisor: 1000, multiplier: 1}}),
+            battery(),
+            develcoModernExtend.addCustomClusterManuSpecificDevelcoGenBasic(),
+            develcoModernExtend.readGenBasicPrimaryVersions(),
+            pulse_configuration(),
+            current_summation(),
+        ],
+
         configure: async (device, coordinatorEndpoint, logger) => {
             const endpoint = device.getEndpoint(2);
             await reporting.bind(endpoint, coordinatorEndpoint, ['seMetering', 'genPowerCfg']);
