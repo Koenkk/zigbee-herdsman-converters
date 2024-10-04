bbb37daa177287e0cda8216746bb05f25ef1e073
Thomas Bowman Mørch <thomas.git@bowmo.dk>
2024-10-05 00:10:50 +0200

    Remove config stanza

--- src/devices/frient.ts
+++ src/devices/frient.ts
@@ -1,6 +1,5 @@
 import {develcoModernExtend} from '../lib/develco';
 import {battery, electricityMeter, onOff, ota} from '../lib/modernExtend';
-import * as reporting from '../lib/reporting';
 import {DefinitionWithExtend} from '../lib/types';
 
 // NOTE! Develco and Frient is the same company, therefore we use develco specific things in here.
@@ -20,13 +19,6 @@ const definitions: DefinitionWithExtend[] = [
             develcoModernExtend.pulse_configuration(),
             develcoModernExtend.current_summation(),
         ],
-
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const endpoint = device.getEndpoint(2);
-            await reporting.bind(endpoint, coordinatorEndpoint, ['seMetering', 'genPowerCfg']);
-            await reporting.instantaneousDemand(endpoint);
-            await reporting.readMeteringMultiplierDivisor(endpoint);
-        },
     },
     {
         zigbeeModel: ['SMRZB-153'],
@@ -34,7 +26,7 @@ const definitions: DefinitionWithExtend[] = [
         vendor: 'Frient',
         description: 'Smart Cable - Power switch with power measurement',
         extend: [onOff({configureReporting: false}), electricityMeter()],
-        endpoint: (device) => {
+        endpoint: () => {
             return {default: 2};
         },
     },
