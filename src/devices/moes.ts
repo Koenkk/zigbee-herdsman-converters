edcb773689dd2b75c1d2daad4816f80aed814dc0
Koen Kanters <koenkanters94@gmail.com>
2024-10-14 20:16:10 +0200

    add

--- src/devices/moes.ts
+++ src/devices/moes.ts
@@ -465,7 +465,7 @@ const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_rjxqso4a'}],
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_hr0tdd47', '_TZE200_rjxqso4a']),
         model: 'ZC-HM',
         vendor: 'Moes',
         description: 'Carbon monoxide alarm',
