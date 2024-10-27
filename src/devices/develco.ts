ee43a1280bcfd7a0e432bfd699646f726a332ded
Howmation <152989874+HowmationFr@users.noreply.github.com>
2024-10-27 21:28:34 +0100

    Debugging and code simplification

--- src/devices/develco.ts
+++ src/devices/develco.ts
@@ -680,35 +680,13 @@ const definitions: DefinitionWithExtend[] = [
         vendor: 'Develco',
         description: 'Motion Sensor 2 Pet',
         extend: [
+            develcoModernExtend.addCustomClusterManuSpecificDevelcoGenBasic(),
+            develcoModernExtend.readGenBasicPrimaryVersions(),
             develcoModernExtend.temperature(),
             illuminance(),
             battery(),
             iasZoneAlarm({zoneType: 'occupancy', alarmTimeout: true, zoneAttributes: ['alarm_1']}),
         ],
-        toZigbee: [],
-        exposes: [e.occupancy(), e.battery(), e.temperature(), e.illuminance_lux()],
-        endpoint: (device) => {
-            return {
-                occupancy: 40,
-                temperature: 38,
-                illuminance: 39,
-                battery: 35,
-            };
-        },
-        configure: async (device, coordinatorEndpoint, logger) => {
-            const occupancyEndpoint = device.getEndpoint(40);
-            await reporting.bind(occupancyEndpoint, coordinatorEndpoint, ['msOccupancySensing']);
-            await reporting.occupancy(occupancyEndpoint, {min: 10, max: 3600, change: 1});
-            const temperatureEndpoint = device.getEndpoint(38);
-            await reporting.bind(temperatureEndpoint, coordinatorEndpoint, ['msTemperatureMeasurement']);
-            await reporting.temperature(temperatureEndpoint, {min: 10, max: 3600, change: 50});
-            const illuminanceEndpoint = device.getEndpoint(39);
-            await reporting.bind(illuminanceEndpoint, coordinatorEndpoint, ['msIlluminanceMeasurement']);
-            await reporting.illuminance(illuminanceEndpoint, {min: 10, max: 3600, change: 50});
-            const batteryEndpoint = device.getEndpoint(35);
-            await reporting.bind(batteryEndpoint, coordinatorEndpoint, ['genPowerCfg']);
-            await reporting.batteryPercentageRemaining(batteryEndpoint, {min: 600, max: 21600, change: 1});
-        },
     },
     {
         whiteLabel: [{vendor: 'Frient', model: 'HMSZB-120', description: 'Temperature & humidity sensor', fingerprint: [{modelID: 'HMSZB-120'}]}],
