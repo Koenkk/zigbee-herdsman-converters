73d8f789f358b8d3cc40f429b01da8b15000b90f
wangling75 <513448188@qq.com>
2024-10-28 09:27:12 +0800

    Update spotmau.ts

--- src/devices/spotmau.ts
+++ src/devices/spotmau.ts
@@ -7,17 +7,28 @@ const definitions: DefinitionWithExtend[] = [
         model: 'SP-PS1-02',
         vendor: 'Spotmau',
         description: 'Smart wall switch - 1 gang',
-        extend: [onOff()],
-        endpoint: (device) => {
-            return {default: 16};
-        },
+        extend: [deviceEndpoints({endpoints: {left: 16}}), onOff({endpointNames: ['left']})],
     },
     {
         zigbeeModel: ['1719SP-PS2-02'],
         model: 'SP-PS2-02',
         vendor: 'Spotmau',
         description: 'Smart wall switch - 2 gang',
-        extend: [deviceEndpoints({endpoints: {left: 16, right: 17}}), onOff({endpointNames: ['left', 'right']})],
+        extend: [deviceEndpoints({endpoints: {left: 16, center: 17}}), onOff({endpointNames: ['left', 'center']})],
+    },
+    {
+    zigbeeModel: ['1719SP-PS3-02'],
+    model: 'SP-PS3-02',
+    vendor: 'Spotmau',
+    description: 'Smart wall switch - 3 gang',
+    extend: [deviceEndpoints({endpoints: {left: 16, center: 17, right: 18}}), onOff({endpointNames: ['left', 'center', 'right']})],
+    },
+    {
+        zigbeeModel: ['1719SP-WS-02'],
+        model: 'SP-WS-02',
+        vendor: 'Spotmau',
+        description: 'Smart wall switch - Socket',
+        extend: [deviceEndpoints({endpoints: {left: 16}}), onOff({endpointNames: ['left']})],
     },
 ];
 
