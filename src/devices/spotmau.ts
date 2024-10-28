e92fbf54813d1d1dd9bd3b6fde0132ae75ec7184
wangling75 <513448188@qq.com>
2024-10-28 12:50:29 +0800

    Update spotmau.ts

--- src/devices/spotmau.ts
+++ src/devices/spotmau.ts
@@ -19,6 +19,21 @@ const definitions: DefinitionWithExtend[] = [
         description: 'Smart wall switch - 2 gang',
         extend: [deviceEndpoints({endpoints: {left: 16, right: 17}}), onOff({endpointNames: ['left', 'right']})],
     },
+    {
+    zigbeeModel: ['1719SP-PS3-02'],
+    model: 'SP-PS3-02',
+    vendor: 'Spotmau',
+    description: 'Smart wall switch - 3 gang',
+    extend: [deviceEndpoints({endpoints: {left: 16, center: 17, right: 18}}), onOff({endpointNames: ['left', 'center', 'right']})],
+    },
+    {
+        zigbeeModel: ['1719SP-WS-02'],
+        model: 'SP-WS-02',
+        vendor: 'Spotmau',
+        description: 'Smart wall switch - Socket',
+        extend: [deviceEndpoints({endpoints: {left: 16}}), onOff({endpointNames: ['left']})],
+    },
+
 ];
 
 export default definitions;
