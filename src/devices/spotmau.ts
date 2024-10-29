42585c3ba2933ecb783b008e7303a3de3bd75260
Koen Kanters <koenkanters94@gmail.com>
2024-10-29 20:04:26 +0100

    u

--- src/devices/spotmau.ts
+++ src/devices/spotmau.ts
@@ -20,20 +20,22 @@ const definitions: DefinitionWithExtend[] = [
         extend: [deviceEndpoints({endpoints: {left: 16, right: 17}}), onOff({endpointNames: ['left', 'right']})],
     },
     {
-    zigbeeModel: ['1719SP-PS3-02'],
-    model: 'SP-PS3-02',
-    vendor: 'Spotmau',
-    description: 'Smart wall switch - 3 gang',
-    extend: [deviceEndpoints({endpoints: {left: 16, center: 17, right: 18}}), onOff({endpointNames: ['left', 'center', 'right']})],
+        zigbeeModel: ['1719SP-PS3-02'],
+        model: 'SP-PS3-02',
+        vendor: 'Spotmau',
+        description: 'Smart wall switch - 3 gang',
+        extend: [deviceEndpoints({endpoints: {left: 16, center: 17, right: 18}}), onOff({endpointNames: ['left', 'center', 'right']})],
     },
     {
         zigbeeModel: ['1719SP-WS-02'],
         model: 'SP-WS-02',
         vendor: 'Spotmau',
         description: 'Smart wall switch - Socket',
-        extend: [deviceEndpoints({endpoints: {left: 16}}), onOff({endpointNames: ['left']})],
+        extend: [onOff()],
+        endpoint: (device) => {
+            return {default: 16};
+        },
     },
-
 ];
 
 export default definitions;
