4399429e2c98a2f5061ac3705f30e32c96a35fa1
renzhe <xsp1989@126.com>
2024-10-30 15:53:43 +0800

    support ZB-WB01,ZB-WB02,ZB-WB03,ZB-WB08

--- src/devices/easyiot.ts
+++ src/devices/easyiot.ts
@@ -2,8 +2,9 @@ import * as iconv from 'iconv-lite';
 
 import * as exposes from '../lib/exposes';
 import {logger} from '../lib/logger';
-import {commandsWindowCovering, electricityMeter, onOff, windowCovering} from '../lib/modernExtend';
-import {DefinitionWithExtend, Fz, Tz} from '../lib/types';
+import {electricityMeter, onOff, windowCovering} from '../lib/modernExtend';
+import * as reporting from '../lib/reporting';
+import {DefinitionWithExtend, Fz, KeyValueAny, Tz} from '../lib/types';
 
 const NS = 'zhc:easyiot';
 const ea = exposes.access;
@@ -43,6 +44,27 @@ const fzLocal = {
                 const result = msg.data.data[4];
                 return {last_received_status: result};
             }
+        },
+    } satisfies Fz.Converter,
+
+    easyiot_action: {
+        cluster: 'genOnOff',
+        type: ['commandOn', 'commandOff', 'commandToggle'],
+        convert: (model, msg, publish, options, meta) => {
+            const lookup: KeyValueAny = {commandToggle: 'single', commandOn: 'double', commandOff: 'long'};
+            let buttonMapping: KeyValueAny = null;
+            if (model.model === 'ZB-SW01') {
+                buttonMapping = {1: '1'};
+            } else if (model.model === 'ZB-WB02') {
+                buttonMapping = {1: '1', 2: '2'};
+            } else if (model.model === 'ZB-WB03') {
+                buttonMapping = {1: '1', 2: '2', 3: '3'};
+            } else if (model.model === 'ZB-WB08') {
+                buttonMapping = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8'};
+            }
+
+            const button = buttonMapping ? `${buttonMapping[msg.endpoint.ID]}_` : '';
+            return {action: `${button}${lookup[msg.type]}`};
         },
     } satisfies Fz.Converter,
 };
@@ -218,7 +240,101 @@ const definitions: DefinitionWithExtend[] = [
         model: 'ZB-WC01',
         vendor: 'easyiot',
         description: 'Curtain motor',
-        extend: [windowCovering({controls: ['lift']}), commandsWindowCovering()],
+        extend: [windowCovering({controls: ['lift'], configureReporting: false})],
+    },
+    {
+        zigbeeModel: ['ZB-WB01'],
+        model: 'ZB-WB01',
+        vendor: 'easyiot',
+        description: '1-button remote control',
+        fromZigbee: [fzLocal.easyiot_action],
+        toZigbee: [],
+        exposes: [e.action(['1_single', '1_double', '1_long']), e.battery()],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint, {min: 30, max: 1800, change: 1});
+        },
+    },
+    {
+        zigbeeModel: ['ZB-WB02'],
+        model: 'ZB-WB02',
+        vendor: 'easyiot',
+        description: '2-button remote control',
+        fromZigbee: [fzLocal.easyiot_action],
+        toZigbee: [],
+        exposes: [e.action(['1_single', '1_double', '1_long', '2_single', '2_double', '2_long']), e.battery()],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint, {min: 30, max: 1800, change: 1});
+            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
+        zigbeeModel: ['ZB-WB03'],
+        model: 'ZB-WB03',
+        vendor: 'easyiot',
+        description: '3-button remote control',
+        fromZigbee: [fzLocal.easyiot_action],
+        toZigbee: [],
+        exposes: [e.action(['1_single', '1_double', '1_long', '2_single', '2_double', '2_long', '3_single', '3_double', '3_long']), e.battery()],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint, {min: 30, max: 1800, change: 1});
+            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(3), coordinatorEndpoint, ['genOnOff']);
+        },
+    },
+    {
+        zigbeeModel: ['ZB-WB08'],
+        model: 'ZB-WB08',
+        vendor: 'easyiot',
+        description: '8-button remote control',
+        fromZigbee: [fzLocal.easyiot_action],
+        toZigbee: [],
+        exposes: [
+            e.action([
+                '1_single',
+                '1_double',
+                '1_long',
+                '2_single',
+                '2_double',
+                '2_long',
+                '3_single',
+                '3_double',
+                '3_long',
+                '4_single',
+                '4_double',
+                '4_long',
+                '5_single',
+                '5_double',
+                '5_long',
+                '6_single',
+                '6_double',
+                '6_long',
+                '7_single',
+                '7_double',
+                '7_long',
+                '8_single',
+                '8_double',
+                '8_long',
+            ]),
+            e.battery(),
+        ],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genPowerCfg']);
+            await reporting.batteryPercentageRemaining(endpoint, {min: 30, max: 1800, change: 1});
+            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(3), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(4), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(5), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(6), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(7), coordinatorEndpoint, ['genOnOff']);
+            await reporting.bind(device.getEndpoint(8), coordinatorEndpoint, ['genOnOff']);
+        },
     },
 ];
 
