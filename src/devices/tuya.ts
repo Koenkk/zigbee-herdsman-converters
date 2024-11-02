91ed22b66b9eac6de45d6ae44a8e602a1c056735
Koen Kanters <koenkanters94@gmail.com>
2024-11-02 20:26:02 +0100

    fix(detect): Detect `_TZ3000_hy6ncvmw` as Tuya  TS0222_light https://github.com/Koenkk/zigbee2mqtt/issues/24577

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -7022,7 +7022,7 @@ const definitions: DefinitionWithExtend[] = [
         whiteLabel: [tuya.whitelabel('Tuya', 'QT-07S', 'Soil sensor', ['_TZE204_myd45weu'])],
     },
     {
-        fingerprint: tuya.fingerprint('TS0222', ['_TZ3000_8uxxzz4b']),
+        fingerprint: tuya.fingerprint('TS0222', ['_TZ3000_8uxxzz4b', '_TZ3000_hy6ncvmw']),
         model: 'TS0222_light',
         vendor: 'Tuya',
         description: 'Light sensor',
