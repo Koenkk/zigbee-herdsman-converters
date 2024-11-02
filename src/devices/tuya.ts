fb850baa331bc8b359d77441b738b57b80c0f087
VITALII UMANETS <umanetsvitaliy@gmail.com>
2024-11-02 16:45:36 +0200

    Update tuya.ts
    fix code format

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -12769,17 +12769,56 @@ const definitions: DefinitionWithExtend[] = [
         configure: tuya.configureMagicPacket,
         ota: ota.zigbeeOTA,
         exposes: [
-            e.numeric('liquid_level_percent', ea.STATE).withUnit('%').withDescription('Liquid level ratio'),
-            e.numeric('liquid_depth', ea.STATE).withUnit('m').withDescription('Liquid Depth'),
-            e.enum('liquid_state', ea.STATE, ['low', 'normal', 'high']).withDescription('Liquid level status'),
-    		e.numeric('max_set', ea.STATE_SET).withUnit('%').withDescription('Liquid max percentage').withValueMin(0).withValueMax(100).withValueStep(1),
-    		e.numeric('mini_set', ea.STATE_SET).withUnit('%').withDescription('Liquid minimal percentage').withValueMin(0).withValueMax(100).withValueStep(1),
-            e.numeric('installation_height', ea.STATE_SET).withUnit('mm').withDescription('Height from sensor to tank bottom').withValueMin(100).withValueMax(4000).withValueStep(1),
-    		e.numeric('liquid_depth_max', ea.STATE_SET).withUnit('mm').withDescription('Height from sensor to liquid level').withValueMin(100).withValueMax(2000).withValueStep(1),
+            e
+                .numeric('liquid_level_percent', ea.STATE)
+                .withUnit('%')
+                .withDescription('Liquid level ratio'),
+            e
+                .numeric('liquid_depth', ea.STATE)
+                .withUnit('m')
+                .withDescription('Liquid Depth'),
+            e
+                .enum('liquid_state', ea.STATE, ['low', 'normal', 'high'])
+                .withDescription('Liquid level status'),
+    		e
+                .numeric('max_set', ea.STATE_SET)
+                .withUnit('%')
+                .withDescription('Liquid max percentage')
+                .withValueMin(0)
+                .withValueMax(100)
+                .withValueStep(1),
+    		e
+                .numeric('mini_set', ea.STATE_SET)
+                .withUnit('%')
+                .withDescription('Liquid minimal percentage')
+                .withValueMin(0)
+                .withValueMax(100)
+                .withValueStep(1),
+            e
+                .numeric('installation_height', ea.STATE_SET)
+                .withUnit('mm')
+                .withDescription('Height from sensor to tank bottom')
+                .withValueMin(100)
+                .withValueMax(4000)
+                .withValueStep(1),
+    		e
+                .numeric('liquid_depth_max', ea.STATE_SET)
+                .withUnit('mm')
+                .withDescription('Height from sensor to liquid level')
+                .withValueMin(100)
+                .withValueMax(2000)
+                .withValueStep(1),
         ],
     	meta: {
     		tuyaDatapoints: [
-    			[1, 'liquid_state', tuya.valueConverterBasic.lookup({'low': 1, 'normal': 0, 'high': 2})],
+    			[
+                    1, 
+                    'liquid_state', tuya.valueConverterBasic.lookup({
+                        low: tuya.enum(1), 
+                        normal: tuya.enum(0), 
+                        high: tuya.enum(2),
+                    }),
+                ],
     			[2, 'liquid_depth', tuya.valueConverter.divideBy1000],
     			[22, 'liquid_level_percent', tuya.valueConverter.raw],
     			[7, 'max_set', tuya.valueConverter.raw],
