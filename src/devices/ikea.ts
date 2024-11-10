df29fa815f3a619a9b4922cd92caad0f857f963a
Koen Kanters <koenkanters94@gmail.com>
2024-11-10 15:14:56 +0100

    Update

--- src/devices/ikea.ts
+++ src/devices/ikea.ts
@@ -79,7 +79,12 @@ const definitions: DefinitionWithExtend[] = [
         extend: [addCustomClusterManuSpecificIkeaUnknown(), ikeaLight(), identify()],
     },
     {
-        zigbeeModel: ['TRADFRI bulb E26 WW globe 800lm', 'TRADFRI bulb E26 WW globe 806lm', 'TRADFRI bulb E26 WW globe 810lm', 'TRADFRI bulb E27 WW globe 806lm'],
+        zigbeeModel: [
+            'TRADFRI bulb E26 WW globe 800lm',
+            'TRADFRI bulb E26 WW globe 806lm',
+            'TRADFRI bulb E26 WW globe 810lm',
+            'TRADFRI bulb E27 WW globe 806lm',
+        ],
         model: 'LED2103G5',
         vendor: 'IKEA',
         description: 'TRADFRI bulb E26/E27, warm white, globe, 806/810 lumen',
