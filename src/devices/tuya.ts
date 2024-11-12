e96ca33e1c2c73156e1f7f0b766e79d9556a0577
Liionboy <87735636+Liionboy@users.noreply.github.com>
2024-11-12 21:59:51 +0200

    fix: Fix preset value for Tuya `_TZE200_viy9ihs7`  (#8298)
    Correction for Auto, Manule and Temporary Manual Mode

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6895,7 +6895,7 @@ const definitions: DefinitionWithExtend[] = [
                     tuya.valueConverterBasic.lookup((_, device) => {
                         // https://github.com/Koenkk/zigbee2mqtt/issues/21353#issuecomment-1938328429
                         if (device.manufacturerName === '_TZE200_viy9ihs7') {
-                            return {auto: tuya.enum(1), manual: tuya.enum(0), temporary_manual: tuya.enum(2)};
+                            return {auto: tuya.enum(0), manual: tuya.enum(1), temporary_manual: tuya.enum(2)};
                         } else {
                             return {auto: tuya.enum(0), manual: tuya.enum(1), temporary_manual: tuya.enum(2)};
                         }
