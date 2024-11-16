c0f9d9308aed7fd0fb28dc5e526fda97765c16cb
Victor <58039006+heisenberg2980@users.noreply.github.com>
2024-11-16 23:13:06 +0000

    Update somfy.ts

--- src/devices/somfy.ts
+++ src/devices/somfy.ts
@@ -1,7 +1,7 @@
 import fz from '../converters/fromZigbee';
 import tz from '../converters/toZigbee';
 import * as exposes from '../lib/exposes';
-import {deviceEndpoints, windowCovering, battery, iasZoneAlarm, identify} from '../lib/modernExtend';
+import {battery, iasZoneAlarm, identify} from '../lib/modernExtend';
 import * as reporting from '../lib/reporting';
 import {DefinitionWithExtend} from '../lib/types';
 
