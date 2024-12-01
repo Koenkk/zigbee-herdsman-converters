b7877b7d0b10f64a827e19801d39a4b021043dd3
Koen Kanters <koenkanters94@gmail.com>
2024-12-01 21:09:26 +0100

    feat(add): A6Z https://github.com/Koenkk/zigbee2mqtt/issues/24967

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -5439,6 +5439,7 @@ const definitions: DefinitionWithExtend[] = [
             tuya.whitelabel('Nous', 'A1Z', 'Smart plug (with power monitoring)', ['_TZ3000_ksw8qtmt']),
             tuya.whitelabel('Elivco', 'LSPA9', 'Smart plug (with power monitoring)', ['_TZ3000_okaz9tjs']),
             tuya.whitelabel('PSMART', 'T440', 'Smart wallsocket (with power monitoring)', ['_TZ3000_y4ona9me']),
+            tuya.whitelabel('Nous', 'A6Z', 'Outdoor smart socket', ['_TZ3000_266azbg3']),
         ],
         ota: true,
         extend: [
