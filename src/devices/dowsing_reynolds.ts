2e6e5002e76045310d0b9480f56ca956866059c8
Ahmet Taha Sakar <ahmet@200iq.com>
2024-12-02 17:47:38 +0000

    Dowsing & Reynolds

---
+++ src/devices/dowsing_reynolds.ts
@@ -0,0 +1,12 @@
+const {philipsLight} = require('zigbee-herdsman-converters/lib/philips');
+
+const definition = {
+    zigbeeModel: ['DR3000'],
+    model: 'DR3000',
+    vendor: 'Dowsing & Reynolds',
+    description: 'Automatically generated definition',
+    extend: [philipsLight()],
+    meta: {},
+};
+
+module.exports = definition;
