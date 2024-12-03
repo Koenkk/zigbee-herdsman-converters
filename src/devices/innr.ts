563b3b131a1c7a7ab74135fbb8c27f4879851180
Vincent Picking <v.picking@gmx.de>
2024-12-03 13:42:35 +0100

    Add innr E27 filament globe bulb RF 271 T

--- src/devices/innr.ts
+++ src/devices/innr.ts
@@ -815,6 +815,13 @@ const definitions: DefinitionWithExtend[] = [
         description: 'Smart E26 LED bulb',
         extend: [light({turnsOffAtBrightness1: true})],
     },
+    {
+        zigbeeModel: ['RF 271 T'],
+        model: 'RF 271 T',
+        vendor: 'Innr',
+        description: 'Smart E27 filament LED globe light bulb',
+        extend: [light({colorTemp: {range: [153, 556]}, turnsOffAtBrightness1: true})],
+    },
 ];
 
 export default definitions;
