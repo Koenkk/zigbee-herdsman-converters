00ed002588706b8bcb20f84681ff51dc0df098cc
BIGFAAT <julien.chapotot@gmail.com>
2024-12-07 15:11:45 +0100

    feat(add): RDM-35274001 (#8445)
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/rademacher.ts
+++ src/devices/rademacher.ts
@@ -16,6 +16,13 @@ const definitions: DefinitionWithExtend[] = [
         description: 'addZ white + colour',
         extend: [light({colorTemp: {range: [153, 555]}, color: true})],
     },
+    {
+        zigbeeModel: ['RDM-35274001'],
+        model: 'RDM-35274001',
+        vendor: 'Rademacher',
+        description: 'addZ white + colour E27 LED',
+        extend: [light({colorTemp: {range: [153, 555]}, color: {modes: ['xy', 'hs'], enhancedHue: true}})],
+    },
 ];
 
 export default definitions;
