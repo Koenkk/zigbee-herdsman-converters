9bb99f0c972c5a339c846ac966bf2739d591fe42
loodvin <126670371+loodvin@users.noreply.github.com>
2024-12-23 23:02:03 +0300

    fix: Improve TRV602Z integration (#8502)
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -5235,19 +5235,22 @@ const definitions: DefinitionWithExtend[] = [
             e.position(),
             e.window_detection(),
             e.binary('window', ea.STATE, 'OPEN', 'CLOSE').withDescription('Window status closed or open '),
-            e.enum('mode', ea.STATE_SET, ['standby', 'antifrost', 'eco', 'comfort', 'auto', 'on']).withDescription('Mode'),
             e
                 .climate()
                 .withLocalTemperature(ea.STATE)
                 .withSetpoint('current_heating_setpoint', 5, 35, 0.5, ea.STATE_SET)
-                .withLocalTemperatureCalibration(-30, 30, 0.1, ea.STATE_SET),
+                .withLocalTemperatureCalibration(-10, 10, 0.1, ea.STATE_SET)
+                .withPreset(['off', 'antifrost', 'eco', 'comfort', 'auto', 'on'])
+                .withRunningState(['idle', 'heat'], ea.STATE)
+                .withSystemMode(['auto', 'heat', 'off'], ea.STATE),
             ...tuya.exposes.scheduleAllDays(ea.STATE_SET, 'HH:MM/C HH:MM/C HH:MM/C HH:MM/C HH:MM/C HH:MM/C'),
             e.comfort_temperature().withValueMin(5).withValueMax(30).withDescription('Comfort mode temperature'),
             e.eco_temperature().withValueMin(5).withValueMax(30).withDescription('Eco mode temperature'),
+            e.holiday_temperature().withValueMin(5).withValueMax(30).withDescription('Antifreeze mode temperature'),
             e.enum('display_brightness', ea.STATE_SET, ['high', 'medium', 'low']).withDescription('Display brightness'),
-            e.enum('screen_orientation', ea.STATE_SET, ['up', 'right', 'down', 'left']).withDescription('Screen orientation'),
+            e.enum('screen_orientation', ea.STATE_SET, ['up', 'down']).withDescription('Screen orientation'),
             e
-                .enum('system_mode', ea.STATE_SET, ['comfort', 'eco'])
+                .enum('hysteresis', ea.STATE_SET, ['comfort', 'eco'])
                 .withDescription(
                     'Hysteresis - comfort > switches off/on exactly at reached ' +
                         'temperature with valve smooth from 0 to 100%, eco > 0.5 degrees above or below, valve either 0 or 100%',
@@ -5258,9 +5261,9 @@ const definitions: DefinitionWithExtend[] = [
             tuyaDatapoints: [
                 [
                     2,
-                    'mode',
+                    'preset',
                     tuya.valueConverterBasic.lookup({
-                        standby: tuya.enum(0),
+                        off: tuya.enum(0),
                         antifrost: tuya.enum(1),
                         eco: tuya.enum(2),
                         comfort: tuya.enum(3),
@@ -5268,6 +5271,8 @@ const definitions: DefinitionWithExtend[] = [
                         on: tuya.enum(5),
                     }),
                 ],
+                [2, 'system_mode', tuya.valueConverter.thermostatSystemModeAndPreset('system_mode')],
+                [3, 'running_state', tuya.valueConverterBasic.lookup({heat: 1, idle: 0})],
                 [4, 'current_heating_setpoint', tuya.valueConverter.divideBy10],
                 [5, 'local_temperature', tuya.valueConverter.divideBy10],
                 [6, 'battery', tuya.valueConverter.raw],
@@ -5323,22 +5328,14 @@ const definitions: DefinitionWithExtend[] = [
                         low: tuya.enum(2),
                     }),
                 ],
-                [
-                    113,
-                    'screen_orientation',
-                    tuya.valueConverterBasic.lookup({
-                        up: tuya.enum(0),
-                        right: tuya.enum(1),
-                        down: tuya.enum(2),
-                        left: tuya.enum(3),
-                    }),
-                ],
+                [113, 'screen_orientation', tuya.valueConverterBasic.lookup({up: tuya.enum(0), down: tuya.enum(1)})],
                 [114, 'position', tuya.valueConverter.divideBy10],
                 [119, 'comfort_temperature', tuya.valueConverter.divideBy10],
                 [120, 'eco_temperature', tuya.valueConverter.divideBy10],
+                [121, 'holiday_temperature', tuya.valueConverter.divideBy10],
                 [
                     127,
-                    'system_mode',
+                    'hysteresis',
                     tuya.valueConverterBasic.lookup({
                         comfort: tuya.enum(0),
                         eco: tuya.enum(1),
