faf1c43a168de15b3d1e625eae381b0f77b41827
salgarelli <luca@salgarelli.com>
2024-12-27 12:31:42 +0100

    Added two new lights from XAL (models 050-0131558M and 050-1212558H)

---
+++ src/devices/xal.ts
@@ -0,0 +1,22 @@
+const {light} = require('zigbee-herdsman-converters/lib/modernExtend');
+
+const definitions: DefinitionWithExtend[] = [
+    {
+         zigbeeModel: ['050-0131558M'],
+        model: '050-0131558M',
+        vendor: 'XAL',
+        description: 'Just 45 MOVE IT 25 spotlight',
+        extend: [light()],
+    },
+    {
+        zigbeeModel: ['050-1212558H'],
+        model: '050-1212558H',
+        vendor: 'XAL',
+        description: 'Just 45 LIGHT opal floodlight',
+        extend: [light()],
+    },
+
+];
+
+export default definitions;
+module.exports = definitions;
