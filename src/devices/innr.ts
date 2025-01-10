7b2fc3eb14cfe94c0e79cce54187b1446cd52683
Tim Prange <github@tprng-mail.de>
2025-01-10 20:58:32 +0100

    feat(add): RF 274 T (#8585)
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/innr.ts
+++ src/devices/innr.ts
@@ -837,6 +837,14 @@ const definitions: DefinitionWithExtend[] = [
         description: 'Smart E27 filament LED light bulb',
         extend: [light({colorTemp: {range: [153, 556]}, turnsOffAtBrightness1: true})],
     },
+    {
+        zigbeeModel: ['RF 274 T'],
+        model: 'RF 274 T',
+        vendor: 'innr',
+        description: 'Smart filament bulb E27 tunable edison',
+        extend: [light({colorTemp: {range: [153, 556]}})],
+        ota: true,
+    },
 ];
 
 export default definitions;
