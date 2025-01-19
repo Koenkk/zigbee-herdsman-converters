a6fe68c9c21c2740f9e3f95ce80322e3854aebb7
Raphael <raphpa@users.noreply.github.com>
2025-01-19 10:12:31 +0100

    Update philips.ts

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -130,8 +130,8 @@ const definitions: DefinitionWithExtend[] = [
     model: 'LEDtube T8 2ft V3',
     vendor: 'Philips',
     description: 'MasterConnect LEDtube EM/mains T8',
-    extend: [philipsLight(), electricityMeter({current: false, voltage: false, power: false}), 
-             numeric({"name":"Lamp burn hours", "cluster":"lightingBallastCfg", "attribute":"lampBurnHours", 
+    extend: [philipsLight(), m.electricityMeter({current: false, voltage: false, power: false}), 
+             m.numeric({"name":"Lamp burn hours", "cluster":"lightingBallastCfg", "attribute":"lampBurnHours", 
                     "reporting":{"attribute":"lampBurnHours", "min":"MIN", "max":"MAX","change":1}, 
                     "description":"Lamp burn hours", "access":"STATE_GET", "endpointName":"64", "unit": "h"
                     })
