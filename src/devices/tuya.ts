c1fe78250b00a7c7ef0a6e36b140c5d089471588
Ilya Nixan <thenixan@gmail.com>
2025-01-26 02:03:37 +0300

    ESlint fix

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -5018,9 +5018,7 @@ const definitions: DefinitionWithExtend[] = [
         fingerprint: tuya.fingerprint('TS0601', ['_TZE204_rtrmfadk', '_TZE204_cvcu2p6e']),
         model: 'TRV601',
         vendor: 'Tuya',
-        whiteLabel: [
-            tuya.whitelabel('Sber', 'SBDV-00185', 'Thermostatic radiator valve', ['_TZE204_cvcu2p6e']),
-        ],
+        whiteLabel: [tuya.whitelabel('Sber', 'SBDV-00185', 'Thermostatic radiator valve', ['_TZE204_cvcu2p6e'])],
         description: 'Thermostatic radiator valve.',
         onEvent: tuya.onEventSetLocalTime,
         fromZigbee: [tuya.fz.datapoints],
