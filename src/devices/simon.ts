4e713f75526f7b82a8c5fe0a3bfa6bc51e0009d0
Koen Kanters <koenkanters94@gmail.com>
2025-01-27 19:56:05 +0100

    u

--- src/devices/simon.ts
+++ src/devices/simon.ts
@@ -4,26 +4,10 @@ import {DefinitionWithExtend} from '../lib/types';
 const definitions: DefinitionWithExtend[] = [
     {
         zigbeeModel: ['SM0502'],
-        model: 'i7 SM0502',
+        model: 'SM0502',
         vendor: 'SIMON',
         description: 'i7 2-gang smart dimming switch',
-        extend: [
-            deviceEndpoints({
-                endpoints: {
-                    left: 1,
-                    right: 2,
-                },
-            }),
-            light({
-                endpointNames: ['left'],
-            }),
-            light({
-                endpointNames: ['right'],
-            }),
-        ],
-        meta: {
-            multiEndpoint: true,
-        },
+        extend: [deviceEndpoints({endpoints: {left: 1, right: 2}}), light({endpointNames: ['left', 'right']})],
     },
 ];
 
