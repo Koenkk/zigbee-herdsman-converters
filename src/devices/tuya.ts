cd6ea612043779554efd1bf96919b684e8fa85dc
PDV <d.pogodin76@gmail.com>
2025-02-01 01:27:19 +0200

    Add another fingerprint for Tuya TOQCB2-80 device

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -12374,7 +12374,7 @@ const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_q22avxbv', '_TZE204_mrffaamu', '_TZE204_tzreobvu']),
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE204_q22avxbv', '_TZE204_mrffaamu', '_TZE204_tzreobvu', '_TZE284_mrffaamu']),
         model: 'TOQCB2-80',
         vendor: 'Tuya',
         description: 'Smart circuit breaker',
