bcb6b21caa30d067d38403662718027a672a33cf
Koen Kanters <koenkanters94@gmail.com>
2025-02-07 20:13:25 +0100

    feat(add): SLZB-06p10 https://github.com/Koenkk/zigbee2mqtt/issues/26208

--- src/devices/smlight.ts
+++ src/devices/smlight.ts
@@ -3,13 +3,14 @@ import {DefinitionWithExtend} from '../lib/types';
 
 const definitions: DefinitionWithExtend[] = [
     {
-        zigbeeModel: ['SLZB-06M', 'SLZB-06', 'SLZB-06p7', 'SLZB-07', 'SLZB-0xp7', 'SLZB-07Mg24', 'SLZB-06Mg24'],
+        zigbeeModel: ['SLZB-06M', 'SLZB-06', 'SLZB-06p7', 'SLZB-06P10', 'SLZB-07', 'SLZB-0xp7', 'SLZB-07Mg24', 'SLZB-06Mg24'],
         model: 'SLZB-06M',
         vendor: 'SMLIGHT',
         description: 'Router',
         whiteLabel: [
             {vendor: 'SMLIGHT', model: 'SLZB-06', description: 'Router', fingerprint: [{modelID: 'SLZB-06'}]},
             {vendor: 'SMLIGHT', model: 'SLZB-06p7', description: 'Router', fingerprint: [{modelID: 'SLZB-06p7'}]},
+            {vendor: 'SMLIGHT', model: 'SLZB-06p10', description: 'Router', fingerprint: [{modelID: 'SLZB-06p10'}]},
             {vendor: 'SMLIGHT', model: 'SLZB-07', description: 'Router', fingerprint: [{modelID: 'SLZB-07'}]},
             {vendor: 'SMLIGHT', model: 'SLZB-0xp7', description: 'Router', fingerprint: [{modelID: 'SLZB-0xp7'}]},
             {vendor: 'SMLIGHT', model: 'SLZB-07Mg24', description: 'Router', fingerprint: [{modelID: 'SLZB-07Mg24'}]},
