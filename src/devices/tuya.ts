d9b5bef4419f56e90bf775da0c16f2fbed0eac2d
Arthur Bernardes <arthur.bernardes1@gmail.com>
2025-02-17 09:47:11 -0300

    feat(add): FZB-4 (#8817)

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -10592,7 +10592,7 @@ const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_k6jhsr0q']),
+        fingerprint: tuya.fingerprint('TS0601', ['_TZE200_k6jhsr0q', '_TZE204_unsxl4ir']),
         model: 'ZS-TYG3-SM-41Z',
         vendor: 'Tuya',
         description: '4 gang smart switch with backlight and neutral wire',
@@ -10632,6 +10632,7 @@ const definitions: DefinitionWithExtend[] = [
                 [16, 'backlight_mode', tuya.valueConverter.onOff],
             ],
         },
+        whiteLabel: [tuya.whitelabel('Nova Digital', 'FZB-4', 'Interruptor de 4 canais com backlight e neutro', ['TZE204_unsxl4ir'])],
     },
     {
         fingerprint: tuya.fingerprint('TS0601', ['_TZE200_nvodulvi']),
