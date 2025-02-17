b3efe1d274711b74fb3b4bad08f90dc7ec5038a9
Florian Nadaud <florian.nadaud@spendesk.com>
2025-02-17 16:10:27 +0100

    feat(add): ZB-PM-01

--- src/devices/chacon.ts
+++ src/devices/chacon.ts
@@ -1,5 +1,11 @@
+import fz from '../converters/fromZigbee';
+import tz from '../converters/toZigbee';
+import * as exposes from '../lib/exposes';
 import * as m from '../lib/modernExtend';
+import * as reporting from '../lib/reporting';
 import {DefinitionWithExtend} from '../lib/types';
+
+const e = exposes.presets;
 
 const definitions: DefinitionWithExtend[] = [
     {
@@ -11,6 +17,20 @@ const definitions: DefinitionWithExtend[] = [
             m.windowCovering({controls: ['lift'], coverInverted: true, coverMode: true}),
             m.commandsWindowCovering({commands: ['open', 'close', 'stop']}),
         ],
+    },
+    {
+        zigbeeModel: ['ZB-PM-01'],
+        model: 'ZB-PM-01',
+        vendor: 'Chacon',
+        description: 'On/Off lighting module',
+        fromZigbee: [fz.on_off],
+        toZigbee: [tz.on_off],
+        exposes: [e.switch()],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(endpoint);
+        },
     },
 ];
 
