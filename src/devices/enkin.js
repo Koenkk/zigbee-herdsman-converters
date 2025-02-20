a2857159dac31721e4ccd6b7e75339dcd72c5d6d
Raymond <1412530+encryptix@users.noreply.github.com>
2025-02-20 23:40:21 +0000

    Updates after lint checks

--- src/devices/enkin.js
+++ src/devices/enkin.js
@@ -1,33 +1,36 @@
-const {light, forcePowerSource} = require('zigbee-herdsman-converters/lib/modernExtend');
+import {light, forcePowerSource} from '../lib/modernExtend';
+import {bind, onOff} from '../lib/reporting';
+import {DefinitionWithExtend} from '../lib/types';
 
-const {bind, onOff} = require('zigbee-herdsman-converters/lib/reporting');
+const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ['ZDM150'],
+        model: 'ZDM150',
+        vendor: 'Enkin',
+        description: '150W Dimmer Module',
+        extend: [light({powerOnBehavior: false, effect: false, configureReporting: true}), forcePowerSource({powerSource: 'Mains (single phase)'})],
 
-const definitions = [{
-    zigbeeModel: ['ZDM150'],
-    model: 'ZDM150',
-    vendor: 'Enkin',
-    description: '150W Dimmer Module',
-    extend: [light({powerOnBehavior: false, effect: false, configureReporting: true}), forcePowerSource({powerSource: 'Mains (single phase)'})],
+        configure: async (device, coordinatorEndpoint) => {
+            device.powerSource = 'Mains (single phase)';
 
-    configure: async (device, coordinatorEndpoint) => {
-        device.powerSource = 'Mains (single phase)';
+            const endpoint = device.getEndpoint(1);
+            await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await onOff(endpoint, {min: 0, max: 0xfffe});
 
-        const endpoint = device.getEndpoint(1);
-        await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-        await onOff(endpoint, {min: 0, max: 0xfffe});
+            await endpoint.configureReporting('genOnOff', [
+                {attribute: 'onOff', minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 1},
+            ]);
+            await endpoint.configureReporting('genLevelCtrl', [
+                {attribute: 'currentLevel', minimumReportInterval: 3, maximumReportInterval: 3600, reportableChange: 1},
+            ]);
 
-        await endpoint.configureReporting('genOnOff', [
-            {attribute: 'onOff', minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 1},
-        ]);
-        await endpoint.configureReporting('genLevelCtrl', [
-            {attribute: 'currentLevel', minimumReportInterval: 3, maximumReportInterval: 3600, reportableChange: 1},
-        ]);
+            device.save();
 
-        device.save();
-
+        },
+        meta: {},
     },
-    meta: {},
-}];
+];
 
+export default definitions;
 module.exports = definitions;
 
