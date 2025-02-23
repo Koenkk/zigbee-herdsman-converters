d3173e34594b3900ed8a8d3320a011754450f8f7
Koen Kanters <koenkanters94@gmail.com>
2025-02-23 21:55:25 +0100

    u

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -12209,54 +12209,76 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_ya4ft0w4", "_TZE200_ya4ft0w4","_TZE204_gkfbdvyx"]),
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_ya4ft0w4", "_TZE200_ya4ft0w4", "_TZE204_gkfbdvyx"]),
         model: "ZY-M100-24GV3",
         vendor: "Tuya",
         description: "24G MmWave radar human presence motion sensor（added distance switch）",
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
         configure: tuya.configureMagicPacket,
-		exposes: (device, options) => {
-			const exps = [
-			            e.enum("state", ea.STATE, ["none", "presence", "move"]).withDescription("Presence state sensor"),
-			            e.presence().withDescription("Occupancy"),
-			            e.numeric("distance", ea.STATE).withDescription("Target distance"),
-			            e.binary("find_switch", ea.STATE_SET, "ON", "OFF").withDescription("distance switch"),
-			            e.illuminance().withDescription("Illuminance sensor"),
-			            e.numeric("move_sensitivity", ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1).withDescription("Motion Sensitivity"),
-			            e.numeric("presence_sensitivity", ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1).withDescription("Presence Sensitivity"),
-			            e.numeric("presence_timeout", ea.STATE_SET).withValueMin(1).withValueMax(15000).withValueStep(1).withUnit("s").withDescription("Fade time"),
-			        ];
-			if (!device || device.manufacturerName === "_TZE204_gkfbdvyx"){
-				exps.push(e.numeric("detection_distance_min", ea.STATE_SET)
-					.withValueMin(0)
-					.withValueMax(6)
-					.withValueStep(0.5)
-					.withUnit("m")
-					.withDescription("Minimum range"));
-				exps.push(e.numeric("detection_distance_max", ea.STATE_SET)
-					.withValueMin(0.5)
-					.withValueMax(9.0)
-					.withValueStep(0.5)
-					.withUnit("m")
-					.withDescription("Maximum range"));
-			}else{
-				exps.push(e.numeric("detection_distance_min", ea.STATE_SET)
-					.withValueMin(0)
-					.withValueMax(8.25)
-					.withValueStep(0.75)
-					.withUnit("m")
-					.withDescription("Minimum range"));
-				exps.push(e.numeric("detection_distance_max", ea.STATE_SET)
-					.withValueMin(0.75)
-					.withValueMax(9.0)
-					.withValueStep(0.75)
-					.withUnit("m")
-					.withDescription("Maximum range"));
-			}
-			return exps;
-			
-		},
+        exposes: (device, options) => {
+            const exps = [
+                e.enum("state", ea.STATE, ["none", "presence", "move"]).withDescription("Presence state sensor"),
+                e.presence().withDescription("Occupancy"),
+                e.numeric("distance", ea.STATE).withDescription("Target distance"),
+                e.binary("find_switch", ea.STATE_SET, "ON", "OFF").withDescription("distance switch"),
+                e.illuminance().withDescription("Illuminance sensor"),
+                e.numeric("move_sensitivity", ea.STATE_SET).withValueMin(1).withValueMax(10).withValueStep(1).withDescription("Motion Sensitivity"),
+                e
+                    .numeric("presence_sensitivity", ea.STATE_SET)
+                    .withValueMin(1)
+                    .withValueMax(10)
+                    .withValueStep(1)
+                    .withDescription("Presence Sensitivity"),
+                e
+                    .numeric("presence_timeout", ea.STATE_SET)
+                    .withValueMin(1)
+                    .withValueMax(15000)
+                    .withValueStep(1)
+                    .withUnit("s")
+                    .withDescription("Fade time"),
+            ];
+            if (!device || device.manufacturerName === "_TZE204_gkfbdvyx") {
+                exps.push(
+                    e
+                        .numeric("detection_distance_min", ea.STATE_SET)
+                        .withValueMin(0)
+                        .withValueMax(6)
+                        .withValueStep(0.5)
+                        .withUnit("m")
+                        .withDescription("Minimum range"),
+                );
+                exps.push(
+                    e
+                        .numeric("detection_distance_max", ea.STATE_SET)
+                        .withValueMin(0.5)
+                        .withValueMax(9.0)
+                        .withValueStep(0.5)
+                        .withUnit("m")
+                        .withDescription("Maximum range"),
+                );
+            } else {
+                exps.push(
+                    e
+                        .numeric("detection_distance_min", ea.STATE_SET)
+                        .withValueMin(0)
+                        .withValueMax(8.25)
+                        .withValueStep(0.75)
+                        .withUnit("m")
+                        .withDescription("Minimum range"),
+                );
+                exps.push(
+                    e
+                        .numeric("detection_distance_max", ea.STATE_SET)
+                        .withValueMin(0.75)
+                        .withValueMax(9.0)
+                        .withValueStep(0.75)
+                        .withUnit("m")
+                        .withDescription("Maximum range"),
+                );
+            }
+            return exps;
+        },
         meta: {
             tuyaDatapoints: [
                 [
