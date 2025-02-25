68f62a89e35ba5caa007e9ab78ac67a1aefd1d7a
Dragonfly9091 <thinhdb9091@gmail.com>
2025-02-25 15:45:11 +0700

    Create dqsmart.ts
    Add new support for DQSmart Switch

---
+++ src/devices/dqsmart.ts
@@ -0,0 +1,12 @@
+const m = require('zigbee-herdsman-converters/lib/modernExtend');
+
+const definition = {
+    zigbeeModel: ['dqhome.re4'],
+    model: 'dqhome.re4',
+    vendor: 'DQHOME',
+    description: 'DQSmart Switch 4 Gang',
+    extend: [m.deviceEndpoints({"endpoints":{"1":1,"2":2,"3":3,"4":4}}), m.onOff({"powerOnBehavior":false,"endpointNames":["1","2","3","4"]})],
+    meta: {"multiEndpoint":true},
+};
+
+module.exports = definition;
