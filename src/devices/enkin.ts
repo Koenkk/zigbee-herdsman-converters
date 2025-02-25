e8d32ee6694d539df265f5d3f4fdd71327e37f40
Raymond <1412530+encryptix@users.noreply.github.com>
2025-02-25 20:23:15 +0000

    remove reprting section

--- src/devices/enkin.ts
+++ src/devices/enkin.ts
@@ -1,5 +1,4 @@
 import {forcePowerSource, light} from '../lib/modernExtend';
-import {bind, onOff} from '../lib/reporting';
 import {DefinitionWithExtend} from '../lib/types';
 
 export const definitions: DefinitionWithExtend[] = [
@@ -9,18 +8,6 @@ export const definitions: DefinitionWithExtend[] = [
         vendor: 'Enkin',
         description: '150W Dimmer Module',
         extend: [light({powerOnBehavior: false, effect: false, configureReporting: true}), forcePowerSource({powerSource: 'Mains (single phase)'})],
-
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            await bind(endpoint, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
-            await onOff(endpoint, {min: 0, max: 0xfffe});
-
-            await endpoint.configureReporting('genLevelCtrl', [
-                {attribute: 'currentLevel', minimumReportInterval: 3, maximumReportInterval: 3600, reportableChange: 1},
-            ]);
-
-            device.save();
-        },
         meta: {},
     },
 ];
