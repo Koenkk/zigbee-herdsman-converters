94608b3476ac127ee63a062840c932771085bd6b
martyn-vesternet <development@candeo.io>
2025-02-27 14:29:16 +0000

    Update candeo.ts
    Updates Candeo C203:
    
    1) Adds additional model ID to support historically named devices
    2) Limits Level Cluster features to suit device
    3) Configures Level Cluster options to suit device
    4) Adds ability to change switch input type between momentary and toggle

--- src/devices/candeo.ts
+++ src/devices/candeo.ts
@@ -1,5 +1,56 @@
 import * as m from "../lib/modernExtend";
-import type {DefinitionWithExtend} from "../lib/types";
+import type {DefinitionWithExtend, Fz, Tz} from "../lib/types";
+
+import * as fz from "../converters/fromZigbee";
+import * as tz from "../converters/toZigbee";
+import * as exposes from "../lib/exposes";
+import * as reporting from "../lib/reporting";
+
+const e = exposes.presets;
+const ea = exposes.access;
+
+const manufacturerSpecificClusterCode = 0x1224;
+const switch_type_attribute = 0x8803;
+const data_type = 0x20;
+const value_map = {
+    0: 'momentary',
+    1: 'toggle'
+};
+const value_lookup = {
+    momentary: 0,
+    toggle: 1,
+};
+
+const fzLocal = {
+    switch_type: {
+        cluster: 'genBasic',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            if (Object.prototype.hasOwnProperty.call(msg.data, switch_type_attribute)) {
+                const value = msg.data[switch_type_attribute];
+                return {
+                    external_switch_type: value_map[value] || 'unknown',
+                    external_switch_type_numeric: value,
+                };
+            }
+            return undefined;
+        },
+    } satisfies Fz.Converter,
+};
+
+const tzLocal = {
+    switch_type: {
+        key: ['external_switch_type'],
+        convertSet: async (entity, key, value, meta) => {
+            const numericValue = value_lookup[value] ?? parseInt(value, 10);
+            await entity.write('genBasic', { [switch_type_attribute]: { value: numericValue, type: data_type } }, { manufacturerCode: manufacturerSpecificClusterCode });
+            return { state: { external_switch_type: value } };
+        },
+        convertGet: async (entity, key, meta) => {
+            await entity.read('genBasic', [switch_type_attribute], { manufacturerCode: manufacturerSpecificClusterCode });
+        },
+    } satisfies Tz.Converter,
+};
 
 export const definitions: DefinitionWithExtend[] = [
     {
@@ -180,10 +231,54 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [m.onOff({powerOnBehavior: true})],
     },
     {
-        fingerprint: [{modelID: "C203", manufacturerName: "Candeo"}],
-        model: "C203",
-        vendor: "Candeo",
-        description: "Zigbee micro smart dimmer",
-        extend: [m.light({configureReporting: true})],
+        fingerprint: [
+            { modelID: 'C203', manufacturerName: 'Candeo' },
+            { modelID: 'HK-LN-DIM-A', manufacturerName: 'Candeo' }
+        ],
+        model: 'C203',
+        vendor: 'Candeo',
+        description: 'Zigbee micro smart dimmer',
+        fromZigbee: [
+            fz.on_off,
+            fz.brightness,
+            fz.level_config,
+            fz.power_on_behavior,
+            fzLocal.switch_type,
+            fz.ignore_genOta,
+        ],
+        toZigbee: [
+            tz.light_onoff_brightness,
+            tz.level_config,
+            tz.power_on_behavior,        
+            tz.identify,
+            tzLocal.switch_type,
+        ],
+        exposes: [
+            e
+                .light()
+                .withBrightness()
+                .withLevelConfig(['on_transition_time', 'off_transition_time', 'execute_if_off']),
+            e.power_on_behavior(['off', 'on', 'toggle', 'previous']),
+            e.identify(),
+            e.enum('external_switch_type', ea.ALL, ['momentary', 'toggle']).withLabel('External switch type')
+        ],
+        meta: {},
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint1 = device.getEndpoint(1);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ['genOnOff', 'genLevelCtrl']);
+            await reporting.onOff(endpoint1);
+            await reporting.brightness(endpoint1);
+            await endpoint1.read('genOnOff', ['onOff']);
+            await endpoint1.write('genOnOff', { 0x4003: { value: 0xff, type: 0x30 } });
+            await endpoint1.read('genOnOff', ['startUpOnOff']);
+            await endpoint1.read('genLevelCtrl', ['currentLevel']);
+            await endpoint1.write('genLevelCtrl', { 0x0011: { value: 0xff, type: 0x20 } });
+            await endpoint1.read('genLevelCtrl', ['onLevel']);
+            await endpoint1.write('genLevelCtrl', { 0x0010: { value: 0x0a, type: 0x21 } });
+            await endpoint1.read('genLevelCtrl', ['onOffTransitionTime']);
+            await endpoint1.write('genLevelCtrl', { 0x4000: { value: 0xff, type: 0x20 } });
+            await endpoint1.read('genLevelCtrl', ['startUpCurrentLevel']);
+            await endpoint1.read('genBasic', [switch_type_attribute], { manufacturerCode: manufacturerSpecificClusterCode });
+        },
     },
 ];
