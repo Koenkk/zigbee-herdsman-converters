672e9eb2090232ec15608d6b870cd75f6ea82f86
Koen Kanters <koenkanters94@gmail.com>
2025-03-01 20:16:28 +0100

    Update dqsmart.ts

--- src/devices/dqsmart.ts
+++ src/devices/dqsmart.ts
@@ -1,15 +1,14 @@
 const m = require("zigbee-herdsman-converters/lib/modernExtend");
 
-const definition = {
+export const definitions: DefinitionWithExtend[] = [{
     zigbeeModel: ["dqhome.re4"],
     model: "dqhome.re4",
     vendor: "DQHOME",
     description: "DQSmart Switch 4 Gang",
     extend: [
-        m.deviceEndpoints({endpoints: {"1": 1, "2": 2, "3": 3, "4": 4}}),
-        m.onOff({powerOnBehavior: false, endpointNames: ["1", "2", "3", "4"]}),
+        m.deviceEndpoints({endpoints: {"l1": 1, "l2": 2, "l3": 3, "l4": 4}}),
+        m.onOff({powerOnBehavior: false, endpointNames: ["l1", "l2", "l3", "l4"]}),
     ],
-    meta: {multiEndpoint: true},
-};
+    },
+];
 
-module.exports = definition;
