3e1952df49c2b52b3960d6fe3c8afa5d5a38efa3
Koen Kanters <koenkanters94@gmail.com>
2025-03-01 20:40:31 +0100

    Update enkin.ts

--- src/devices/enkin.ts
+++ src/devices/enkin.ts
@@ -6,7 +6,7 @@ export const definitions: DefinitionWithExtend[] = [
         zigbeeModel: ["ZDM150"],
         model: "ZDM150",
         vendor: "Enkin",
-        description: "150W Dimmer Module",
+        description: "150W Dimmer module",
         extend: [light({powerOnBehavior: false, effect: false, configureReporting: true}), forcePowerSource({powerSource: "Mains (single phase)"})],
     },
 ];
