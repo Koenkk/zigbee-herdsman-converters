f32072257ee4ad1c96790258cce16022e181df0c
PlateMug <62430321+PlateMug@users.noreply.github.com>
2025-03-10 21:59:52 +0000

    Create cwd.ts
    There was no vendor file for CWD.

---
+++ src/devices/cwd.ts
@@ -0,0 +1,12 @@
+const m = require('zigbee-herdsman-converters/lib/modernExtend');
+
+const definition = {
+    zigbeeModel: ['ZBT-CCTLight-D0001'],
+    model: 'HLL6948V1',
+    vendor: 'CWD',
+    description: 'Collingwood H2 Pro',
+    extend: [m.light({"colorTemp":{"range":[153,370]}})],
+    meta: {},
+};
+
+module.exports = definition;
