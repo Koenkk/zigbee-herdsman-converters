a8c362893ab53b7c7451ef275747d84d5d0025f8
Koen Kanters <koenkanters94@gmail.com>
2025-03-12 20:20:37 +0100

    Update cwd.ts

--- src/devices/cwd.ts
+++ src/devices/cwd.ts
@@ -1,12 +1,9 @@
 const m = require("zigbee-herdsman-converters/lib/modernExtend");
 
-const definition = {
+export const definitions: DefinitionWithExtend[] = [{
     zigbeeModel: ["ZBT-CCTLight-D0001"],
     model: "HLL6948V1",
     vendor: "CWD",
-    description: "Collingwood H2 Pro",
+    description: "Collingwood H2 pro",
     extend: [m.light({colorTemp: {range: [153, 370]}})],
-    meta: {},
-};
-
-module.exports = definition;
+}];
