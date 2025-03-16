fc9480d1924a65b9830b0d5e07eaf7e910d179db
Koen Kanters <koenkanters94@gmail.com>
2025-03-16 21:32:01 +0100

    u

--- src/devices/quirky.ts
+++ src/devices/quirky.ts
@@ -1,14 +1,11 @@
-import * as fz from "../converters/fromZigbee";
-import * as exposes from "../lib/exposes";
-const e = exposes.presets;
+import * as m from "../lib/modernExtend";
 
-export const definitions = {
-    zigbeeModel: ["Overflow"],
-    model: "POFLW-WH02",
-    vendor: "Quirky",
-    description: "Smart water leak sensor with a long cable and a seperate sensing end and body. It accepts 2 AA batteries.",
-    fromZigbee: [fz.ias_water_leak_alarm_1],
-    toZigbee: [],
-    exposes: [e.water_leak(), e.battery_low()],
-    meta: {},
-};
+export const definitions = [
+    {
+        zigbeeModel: ["Overflow"],
+        model: "POFLW-WH02",
+        vendor: "Quirky",
+        description: "Smart water leak sensor",
+        extend: [m.iasZoneAlarm({zoneType: "water_leak", zoneAttributes: ["alarm_1", "battery_low"]})],
+    },
+];
