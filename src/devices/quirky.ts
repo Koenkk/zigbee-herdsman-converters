dd83ff3ec839938c111c282618eca712574654ad
Abhijeet <68388009+anonymousaga@users.noreply.github.com>
2025-03-15 20:35:47 -0400

    Create quirky.ts for quirky leak sensor

---
+++ src/devices/quirky.ts
@@ -0,0 +1,16 @@
+import * as fz from "zigbee-herdsman-converters/converters/fromZigbee";
+import * as exposes from "zigbee-herdsman-converters/lib/exposes";
+const e = exposes.presets;
+
+const definition = {
+    zigbeeModel: ['Overflow'],
+    model: 'Smart_Water_Leak_Sensor_Quirky_Inc',
+    vendor: 'Quirky',
+    description: 'Smart water leak sensor with a long cable and a seperate sensing end and body. It accepts 2 AA batteries.',
+    fromZigbee: [fz.ias_water_leak_alarm_1],
+    toZigbee: [],
+    exposes: [e.water_leak(), e.battery_low()],
+    meta: {},
+};
+
+module.exports = definition;
