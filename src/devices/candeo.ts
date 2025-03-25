0c88bb0d9cb88ad4b8411dc016ca67395a896f06
martyn-vesternet <development@candeo.io>
2025-03-25 12:27:59 +0000

    Update candeo.ts
    Adds support for Candeo water sensor, contact sensor, motion sensor and temperature / humidity sensor.
    
    Temperature / humidity sensor uses modern extend.
    
    Contact sensor and motion sensor use partial modern extend since they do not fully support IAS Cluster enrolment, so we need to only allow and configure ias_contact* & ias_occupancy*
    
    Water sensor also uses partial modern extend because while the device does support IAS Cluster enrolment, it requires a bind on the Cluster to trigger events from it.  Modern extend does not do this by default unless zoneStatusReporting is set to true, but the device does not support reporting on that attribute and this generates errors.
    
    Motion sensor also only partially supports Illuminance Cluster (it reports only on motion detection) so we remove / disallow reporting configuration for it.

--- src/devices/candeo.ts
+++ src/devices/candeo.ts
@@ -36,6 +36,14 @@ const fzLocal = {
             return undefined;
         },
     } satisfies Fz.Converter,
+    illuminance_report: {
+        cluster: "msIlluminanceMeasurement",
+        type: ["attributeReport", "readResponse"],
+        convert: (model, msg, publish, options, meta) => {
+            let result = 10 ** ((msg.data.measuredValue - 1) / 10000);
+            return { illuminance: result };
+        },
+    } satisfies Fz.Converter,
 };
 
 const tzLocal = {
@@ -317,6 +325,77 @@ export const definitions: DefinitionWithExtend[] = [
             await endpoint1.write("genLevelCtrl", {16384: {value: 0xff, type: 0x20}});
             await endpoint1.read("genLevelCtrl", ["startUpCurrentLevel"]);
             await endpoint1.read("genBasic", [switchTypeAttribute], {manufacturerCode: manufacturerSpecificClusterCode});
+        },
+    },
+    {
+        fingerprint: [
+            { modelID: "C-ZB-SEWA", manufacturerName: "Candeo" }
+        ],
+        model: "C-ZB-SEWA",
+        vendor: "Candeo",
+        description: "Candeo C-ZB-SEWA Water Sensor",
+        extend: [m.battery()],
+        fromZigbee: [
+            fz.ias_enroll,
+            fz.ias_water_leak_alarm_1,
+            fz.ias_water_leak_alarm_1_report,
+        ],
+        exposes: [e.water_leak()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ["ssIasZone"]);
+            await endpoint.read("ssIasZone", ["zoneStatus"]);
+        },
+    },
+    {
+        fingerprint: [{modelID: "C-ZB-SETE", manufacturerName: "Candeo"}],
+        model: "C-ZB-SETE",
+        vendor: "Candeo",
+        description: "Temperature & humidity sensor",
+        extend: [
+            m.temperature(),
+            m.humidity(),
+            m.battery(),
+        ],
+    },
+    {
+        fingerprint: [{modelID: "C-ZB-SEDC", manufacturerName: "Candeo"}],
+        model: "C-ZB-SEDC",
+        vendor: "Candeo",
+        description: "Door contact sensor",
+        extend: [m.battery()],
+        fromZigbee: [
+            fz.ias_contact_alarm_1, 
+            fz.ias_contact_alarm_1_report,
+        ],
+        exposes: [e.contact()],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ["ssIasZone"]);
+            await endpoint.read("ssIasZone", ["zoneStatus"]);
+        },
+    },
+    {
+        fingerprint: [{modelID: "C-ZB-SEMO", manufacturerName: "Candeo"}],
+        model: "C-ZB-SEMO",
+        vendor: "Candeo",
+        description: "Motion sensor",
+        extend: [m.battery()],
+        fromZigbee: [
+            fz.ias_occupancy_alarm_1, 
+            fz.ias_occupancy_alarm_1_report, 
+            fzLocal.illuminance_report, 
+        ],
+        exposes: [
+            e.occupancy(), 
+            e.illuminance(), 
+        ],
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+            await reporting.bind(endpoint, coordinatorEndpoint, ["ssIasZone", "msIlluminanceMeasurement"]);
+            await endpoint.read("ssIasZone", ["zoneStatus"]);
+            await reporting.illuminance(endpoint, { min: 1, max: 65535, change: 1 });
+            await endpoint.read("msIlluminanceMeasurement", ["measuredValue"]);
         },
     },
 ];
