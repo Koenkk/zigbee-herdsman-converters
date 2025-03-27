3b5b7ec1d62c6b9be4909848a0f296689699a924
Wojciech Krupski <wojciech.krupski@grinn-global.com>
2025-03-27 13:04:09 +0100

    feat(add): 1402790 (manuf: Namron)
    Add support for 1402790 form manufacturer Namron
    Device similar to Fire Fence Stove Guard.

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -1636,4 +1636,23 @@ export const definitions: DefinitionWithExtend[] = [
             e.text("vacation_end_date", ea.ALL).withDescription("End date").withDescription("Supports dates starting with day or year with '. - /'"),
         ],
     },
+    {
+        zigbeeModel: ["1402790"],
+        model: "1402790",
+        vendor: "NAMRON AS",
+        description: "Stove guard for safe cooking",
+        extend: [
+            m.deviceEndpoints({endpoints: {main_switch: 1, short_override: 2}}),
+            m.onOff({powerOnBehavior: false, endpointNames: ["main_switch"], description: "Main relay switch"}),
+            m.onOff({powerOnBehavior: false, endpointNames: ["short_override"], description: "Short override switch"}),
+            m.electricityMeter({
+                endpointNames: ["main_switch"],
+                power: {multiplier: 1, divisor: 1},
+                voltage: false,
+                current: false,
+            }),
+            m.battery(),
+            m.temperature({reporting: undefined}),
+        ],
+    },
 ];
