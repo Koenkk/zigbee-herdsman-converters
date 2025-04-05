1b9d7fb99b4d5bedd607d7cbf8f9377fd1999048
Koen Kanters <koenkanters94@gmail.com>
2025-04-05 13:47:17 +0200

    fix(detect): Detect `_TZE284_eekpf0ft` as Tuya TR-M3Z https://github.com/Koenkk/zigbee2mqtt/discussions/26997

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -16266,7 +16266,7 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_eekpf0ft"]),
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_eekpf0ft", "_TZE284_eekpf0ft"]),
         model: "TR-M3Z",
         vendor: "Tuya",
         description: "Thermostatic radiator valve actuator",
