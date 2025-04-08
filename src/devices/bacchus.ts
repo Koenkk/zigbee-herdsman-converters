25da622e6bdfd346867a92b5307f45acf2b03c01
Bacchus777 <ablysh@rambler.ru>
2025-04-08 17:29:49 +0300

    Presence_Sensor_v2.6

--- src/devices/bacchus.ts
+++ src/devices/bacchus.ts
@@ -1,11 +1,250 @@
-import {Zcl} from "zigbee-herdsman";
+import * as reporting from "../lib/reporting";
 
 import * as m from "../lib/modernExtend";
-import type {DefinitionWithExtend} from "../lib/types";
+import {access as ea} from "../lib/exposes";
+import type {
+    Configure,
+    Fz,
+    ModernExtend,
+    Tz,
+    DefinitionWithExtend,
+} from "../lib/types";
+
+import {
+    getEndpointName,
+    isString,
+    validateValue,
+} from "../lib/utils";
 
 const defaultReporting = {min: 0, max: 3600, change: 0};
+const defaultReportingOnOff = {min: 0, max: 3600, change: 0, attribute: 'onOff'};
+const defaultReportingOccupancy = {min: 0, max: 3600, change: 0, attribute: 'occupancy'};
+
+const time_to_str_min = (time: number) => {
+    const date = new Date(null);
+    date.setSeconds(time); 
+    return date.toISOString().slice(11, 16);		  
+};
+
+const str_min_to_time = (str_min: any) => {
+    return str_min.slice(0, 2) * 60 * 60 + str_min.slice(3, 5) * 60;
+};
+
+function time_hhmm(args: m.TextArgs): ModernExtend {
+    const {name, cluster, attribute, description, zigbeeCommandOptions, endpointName, reporting, entityCategory, validate} = args;
+    const attributeKey = isString(attribute) ? attribute : attribute.ID;
+    const access = ea[args.access ?? "ALL"];
+    const mExtend = m.text(args);
+
+    const fromZigbee: Fz.Converter[] = [
+        {
+            cluster: cluster.toString(),
+            type: ["attributeReport", "readResponse"],
+            convert: (model, msg, publish, options, meta) => {
+                if (attributeKey in msg.data && (!endpointName || getEndpointName(msg, model, meta) === endpointName)) {
+                    return {[name]: time_to_str_min(msg.data[attributeKey])};
+                }
+            },
+        },
+    ];
+
+    const toZigbee: Tz.Converter[] = [
+        {
+            key: [name],
+            convertSet:
+                access & ea.SET
+                    ? async (entity, key, value, meta) => {
+                          const value_str = str_min_to_time(value);
+                          const payload = isString(attribute) ? {[attribute]: value_str} : {[attribute.ID]: {value_str, type: attribute.type}};
+                          await m.determineEndpoint(entity, meta, cluster).write(cluster, payload, zigbeeCommandOptions);
+                          return {state: {[key]: value}};
+                      }
+                    : undefined,
+            convertGet:
+                access & ea.GET
+                    ? async (entity, key, meta) => {
+                          await m.determineEndpoint(entity, meta, cluster).read(cluster, [attributeKey], zigbeeCommandOptions);
+                      }
+                    : undefined,
+        },
+    ];
+
+    const configure: Configure[] = [];
+    configure.push(m.setupConfigureForBinding(cluster, "input"));
+
+    return {...mExtend, fromZigbee, toZigbee, configure, isModernExtend: true};
+}
+
+function binaryWithOnOffCommand(args: m.BinaryArgs): ModernExtend {
+    const {name, valueOn, valueOff, cluster, attribute, zigbeeCommandOptions, endpointName, reporting} = args;
+    const attributeKey = isString(attribute) ? attribute : attribute.ID;
+    const access = ea[args.access ?? "ALL"];
+
+    const mExtend = m.binary(args);
+
+    const toZigbee: Tz.Converter[] = [
+        {
+            key: [name],
+            convertSet:
+                access & ea.SET
+                ? async (entity, key, value, meta) => {
+                    
+                    const state = isString(meta.message[key]) ? meta.message[key].toLowerCase() : null;
+                    validateValue(state, ['toggle', 'off', 'on']);
+                    await m.determineEndpoint(entity, meta, cluster).command(cluster, state, {}, zigbeeCommandOptions);
+                    await m.determineEndpoint(entity, meta, cluster).read(cluster, [attributeKey], zigbeeCommandOptions);
+                    return {state: {[key]: value}};
+                }
+                : undefined,
+            convertGet:
+                access & ea.GET
+                    ? async (entity, key, meta) => {
+                          await m.determineEndpoint(entity, meta, cluster).read(cluster, [attributeKey], zigbeeCommandOptions);
+                      }
+                    : undefined,
+        },
+    ];
+
+    const configure: Configure[] = [];
+    if (reporting) {
+        configure.push(m.setupConfigureForReporting(cluster, attribute, reporting, access));
+    }
+
+    return {...mExtend, toZigbee, configure, isModernExtend: true};
+}
 
 export const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ["Presence_Sensor_v2.6"],
+        model: "Presence_Sensor_v2.6",
+        vendor: "Bacchus",
+        description: "Bacchus presence sensor with illuminance",
+        meta: {multiEndpoint: true},
+        extend: [
+            m.deviceEndpoints({
+                endpoints: {"1": 1, "2": 2, "3": 3}
+            }),
+            m.occupancy({
+                endpointNames: ["1"],
+            }),
+            m.illuminance({
+                reporting: defaultReporting,
+            }),
+            m.numeric({
+                name: 'target_distance',
+                unit: 'cm',
+                cluster: 'msOccupancySensing',
+                attribute: {ID: 0xF005, type: 0x21},
+                description: 'Target distance',
+                access: "STATE",
+                reporting: defaultReporting,
+            }),
+            m.enumLookup({
+                name: "target_type",
+                lookup: {"None": 0, "Moving": 1, "Stationary": 2, "Moving and stationary": 3},
+                cluster: "msOccupancySensing",
+                attribute: {ID: 0xF006, type: 0x30},
+                description: "Target type",
+                access: "STATE",
+                reporting: defaultReporting,
+            }),
+            time_hhmm({
+                name: "local_time",
+                cluster: "genTime",
+                attribute: 'localTime',
+                description: "Local time",
+                access: "STATE_GET",
+            }),
+            m.enumLookup({
+                endpointName: "3",
+                name: "led_mode",
+                lookup: {"Always": 0, "Never": 1, "Night": 2},
+                cluster: "genOnOff",
+                attribute: {ID: 0xF004, type: 0x30},
+                description: "Led working mode",
+                access: "ALL",
+            }),
+            m.numeric({
+                name: 'illuminance_threshold',
+                unit: 'raw',
+                valueMin: 0,
+                valueMax: 50000,
+                cluster: 'msIlluminanceMeasurement',
+                attribute: {ID: 0xF001, type: 0x21},
+                description: 'Illuminance threshold',
+                access: "ALL",
+            }),
+            m.numeric({
+                name: 'measurement_period',
+                unit: 'sec',
+                valueMin: 0,
+                valueMax: 30,
+                cluster: 'msOccupancySensing',
+                attribute: {ID: 0xF007, type: 0x21},
+                description: 'Illuminance threshold',
+                access: "ALL",
+            }),
+            time_hhmm({
+                name: "min_time",
+                cluster: "genTime",
+                attribute: 'dstStart',
+                description: "Day start",
+                access: "ALL",
+            }),
+            time_hhmm({
+                name: "max_time",
+                cluster: "genTime",
+                attribute: 'dstEnd',
+                description: "Day end",
+                access: "ALL",
+            }),
+            binaryWithOnOffCommand({
+                endpointName: "1",
+                name: "sensor",
+                valueOn: ["ON", 1],
+                valueOff: ["OFF", 0],
+                cluster: "genOnOff",
+                attribute: 'onOff',
+                description: "Enable sensor",
+                access: "ALL",
+                reporting: defaultReportingOnOff,
+            }),
+            binaryWithOnOffCommand({
+                endpointName: "2",
+                name: "day_output",
+                valueOn: ["ON", 1],
+                valueOff: ["OFF", 0],
+                cluster: "genOnOff",
+                attribute: 'onOff',
+                description: "Day binding output",
+                access: "STATE",
+                reporting: defaultReportingOnOff,
+            }),
+            binaryWithOnOffCommand({
+                endpointName: "3",
+                name: "night_output",
+                valueOn: ["ON", 1],
+                valueOff: ["OFF", 0],
+                cluster: "genOnOff",
+                attribute: 'onOff',
+                description: "Night binding output",
+                access: "STATE",
+                reporting: defaultReportingOnOff,
+            }),
+                                                        
+        ],
+        configure: async (device, coordinatorEndpoint) => {
+// In second endpoint onOff cluster is only output, so need this:
+            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ['genOnOff']);
+            await reporting.onOff(device.getEndpoint(2));
+
+            await device.getEndpoint(1).read('genTime', ['dstEnd', 'dstStart']);
+            await device.getEndpoint(1).read('msIlluminanceMeasurement', [0xF001]);
+            await device.getEndpoint(1).read('msOccupancySensing', [0xF007]);
+            await device.getEndpoint(1).read('genOnOff', ['onOff']);
+            await device.getEndpoint(3).read('genOnOff', [0xF004]);
+            },
+    },
     {
         zigbeeModel: ["Flower_Sensor_v2"],
         model: "Flower_Sensor_v2",
