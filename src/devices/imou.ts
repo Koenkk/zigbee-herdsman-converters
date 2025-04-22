9e2e50e902ff8d8c006de7101a5e97d398fe6667
andrei-tarnauceanu <35954370+andrei-tarnauceanu@users.noreply.github.com>
2025-04-23 01:54:39 +0300

    fix: Add support for IMOU ZGA1-EN

--- src/devices/imou.ts
+++ src/devices/imou.ts
@@ -29,4 +29,15 @@ export const definitions: DefinitionWithExtend[] = [
         description: "Door & window sensor",
         extend: [m.iasZoneAlarm({zoneType: "alarm", zoneAttributes: ["alarm_1", "tamper", "battery_low"]}), m.battery()],
     },
+    {
+        zigbeeModel: ['ZGA1-EN'],
+        model: 'ZGA1-EN',
+        vendor: 'IMOU',
+        description: 'Smart Gas Detector',
+        extend: [
+            m.forceDeviceType({type: "Router"}),
+            m.iasZoneAlarm({zoneType: "gas", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "test"], alarmTimeout: true})
+        ],
+        meta: {},
+    },
 ];
