5d0d744c68b36940a4172ec3f9d30e28319675c0
GIEX SMART <121998503+Giexperience@users.noreply.github.com>
2025-05-02 01:34:03 +0800

    fix: GIEX GX03: fix `valve_2` status (#9266)

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -15989,7 +15989,7 @@ export const definitions: DefinitionWithExtend[] = [
                 [2, "valve_2", tuya.valueConverter.onOff],
                 [
                     105,
-                    "valve_1",
+                    "valve_2",
                     tuya.valueConverterBasic.lookup({
                         OFF: tuya.enum(2),
                         ON: tuya.enum(0),
