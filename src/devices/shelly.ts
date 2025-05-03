46ccf2eeba14c18b49543b1d6a7ccbbbf40fb3eb
marce14-git <92689239+marce14-git@users.noreply.github.com>
2025-05-03 16:59:47 +0200

    Update shelly.ts

--- src/devices/shelly.ts
+++ src/devices/shelly.ts
@@ -21,13 +21,13 @@ export const definitions: DefinitionWithExtend[] = [
         model: "S4SW-001P8EU",
         vendor: "Shelly",
         description: "1PM Mini Gen 4",
-        extend: [m.onOff({powerOnBehavior: false}), m.electricityMeter({energy: true, producedEnergy: true, acFrequency: true})],
+        extend: [m.onOff({powerOnBehavior: false}), m.electricityMeter({producedEnergy: true, acFrequency: true})],
     },
     {
         zigbeeModel: ["1PM"],
         model: "S4SW-001P16EU",
         vendor: "Shelly",
         description: "1PM Gen 4",
-        extend: [m.onOff({powerOnBehavior: false}), m.electricityMeter({energy: true, producedEnergy: true, acFrequency: true})],
+        extend: [m.onOff({powerOnBehavior: false}), m.electricityMeter({producedEnergy: true, acFrequency: true})],
     },
 ];
