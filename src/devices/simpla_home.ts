1fa3caeedb9cdf58f36fd112b40c2480c688f02e
Simpla Home <development@simpla-home.at>
2025-05-05 11:16:21 +0200

    Add AnalogOutput to configure the measurement interval if the device firmware supports it

--- src/devices/simpla_home.ts
+++ src/devices/simpla_home.ts
@@ -1,11 +1,14 @@
 import * as m from "../lib/modernExtend";
 
+import * as exposes from "../lib/exposes";
 import type {DefinitionWithExtend} from "../lib/types";
+
+const e = exposes.presets;
+const ea = exposes.access;
 
 export const definitions: DefinitionWithExtend[] = [
     {
         zigbeeModel: ["Soil Pro"],
-
         fingerprint: [
             {
                 type: "EndDevice",
@@ -32,6 +35,58 @@ export const definitions: DefinitionWithExtend[] = [
             m.battery(),
             m.illuminance(),
         ],
+        exposes: (device, options) => {
+            const dynExposes = [];
+            if (Number(`0x${device?.softwareBuildID}`) > 0x01010101) {
+                dynExposes.push(
+                    e
+                        .numeric("measurementInterval", ea.ALL)
+                        .withLabel("Measurement Interval")
+                        .withUnit("s")
+                        .withValueMin(5)
+                        .withValueMax(4 * 60 * 60)
+                        .withCategory("config")
+                        .withDescription("Defines how often the device performs measurements"),
+                );
+            }
+            return dynExposes;
+        },
+        fromZigbee: [
+            {
+                cluster: "genAnalogOutput",
+                type: ["attributeReport", "readResponse"],
+                convert: (model, msg, publish, options, meta) => {
+                    if (msg.endpoint.ID === 2 && Object.hasOwn(msg.data, "presentValue")) {
+                        return {measurementInterval: msg.data.presentValue};
+                    }
+                },
+            },
+        ],
+        toZigbee: [
+            {
+                key: ["measurementInterval"],
+                convertSet: async (entity, key, value, meta) => {
+                    const endpoint = meta.device.getEndpoint(2);
+                    await endpoint.write("genAnalogOutput", {presentValue: value});
+                    return {state: {measurementInterval: value}};
+                },
+                convertGet: async (entity, key, meta) => {
+                    const endpoint = meta.device.getEndpoint(2);
+                    await endpoint.read("genAnalogOutput", ["presentValue"]);
+                },
+            },
+        ],
+
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpointId = device.getEndpoint(1);
+            await endpointId.read("genBasic", ["swBuildId"]);
+
+            if (Number(`0x${device?.softwareBuildID}`) > 0x01010101) {
+                const endpointAnalogOutput = device.getEndpoint(2);
+                await endpointAnalogOutput.bind("genAnalogOutput", coordinatorEndpoint);
+                await endpointAnalogOutput.read("genAnalogOutput", ["presentValue"]);
+            }
+        },
         meta: {multiEndpoint: true},
         ota: true,
     },
