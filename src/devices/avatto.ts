1084308f76ca97600a46c44ea96a663da2f4268b
Arthur Bernardes <arthur.bernardes1@gmail.com>
2025-05-09 22:19:40 -0300

    Correct some errors

--- src/devices/avatto.ts
+++ src/devices/avatto.ts
@@ -1,5 +1,5 @@
 import * as exposes from "../lib/exposes";
-import * as m from "../lib/module";
+import * as m from "../lib/modernExtend";
 import * as reporting from "../lib/reporting";
 import * as tuya from "../lib/tuya";
 import type {DefinitionWithExtend} from "../lib/types";
