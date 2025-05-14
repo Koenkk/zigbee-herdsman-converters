c162cec8e97113b34fd7d730057c49c5dcf8ee33
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2025-05-14 23:49:55 +0200

    Update iluminize.ts
    Fix: enhanced_move_to_hue_and_saturation

--- src/devices/iluminize.ts
+++ src/devices/iluminize.ts
@@ -204,7 +204,7 @@ export const definitions: DefinitionWithExtend[] = [
             fz.command_stop,
             fz.command_move,
             fz.command_color_loop_set,
-            fz.command_ehanced_move_to_hue_and_saturation,
+            fz.command_enhanced_move_to_hue_and_saturation,
         ],
         exposes: [
             e.battery(),
