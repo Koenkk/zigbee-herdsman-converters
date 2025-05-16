a3a8f98ff5e2e53c20f3bd0ebf3dae82d371bab6
support <117250319+3reality-support@users.noreply.github.com>
2025-05-17 03:54:06 +0800

    feat(add): 3RWP01073Z (#9352)
    Co-authored-by: jintj <jintj@3reality.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/third_reality.ts
+++ src/devices/third_reality.ts
@@ -479,11 +479,12 @@ export const definitions: DefinitionWithExtend[] = [
         ota: true,
     },
     {
-        zigbeeModel: ["3RDP01072Z"],
+        zigbeeModel: ["3RDP01072Z", "3RWP01073Z"],
         model: "3RDP01072Z",
         vendor: "Third Reality",
         description: "Smart Dual Plug ZP1",
         ota: true,
+        whiteLabel: [{vendor: "Third Reality", model: "3RWP01073Z", description: "Smart Wall Plug ZW1", fingerprint: [{modelID: "3RWP01073Z"}]}],
         extend: [
             m.deviceEndpoints({endpoints: {left: 1, right: 2}}),
             m.onOff({endpointNames: ["left", "right"]}),
