a7f135f9763fc2732d907148cda45540254dd15e
Koen Kanters <koenkanters94@gmail.com>
2025-05-21 21:50:11 +0200

    Updates

--- test/vitest-setup.ts
+++ test/vitest-setup.ts
@@ -5,7 +5,8 @@ import {buildIndex} from "../src/indexer";
 export async function setup(project: TestProject) {
     await buildIndex(true);
 
-    project.onTestsRerun(async () => {
-        await buildIndex(true);
-    });
+    // Disable re-build as it creates and endless loop
+    // project.onTestsRerun(async () => {
+    //     await buildIndex(true);
+    // });
 }
