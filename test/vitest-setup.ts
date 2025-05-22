5936cb62e676357c3a13b91d41766cccd42b03a0
Koen Kanters <koenkanters94@gmail.com>
2025-05-22 20:34:46 +0200

    Remove watch command

--- test/vitest-setup.ts
+++ test/vitest-setup.ts
@@ -4,9 +4,4 @@ import {buildIndex} from "../src/indexer";
 
 export async function setup(project: TestProject) {
     await buildIndex(true);
-
-    // Disable re-build as it creates and endless loop
-    // project.onTestsRerun(async () => {
-    //     await buildIndex(true);
-    // });
 }
