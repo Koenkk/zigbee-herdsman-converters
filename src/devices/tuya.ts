eef9fa052170d6aee4420feee646ede7374513b0
Arthur Bernardes <arthur.bernardes1@gmail.com>
2025-06-04 15:10:49 -0300

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -18009,4 +18009,25 @@ export const definitions: DefinitionWithExtend[] = [
             }),
         ],
     },
+    {
+        fingerprint: tuya.fingerprint("TS011F", ["_TZ3000_rul9yxcc"]),
+        model: "SM-PW801EZ",
+        vendor: "Tuya",
+        description: "2 gang switch",
+        ota: true,
+        extend: [
+            tuya.modernExtend.tuyaOnOff({
+                childLock: true,
+                endpoints: ["l1", "l2"],
+            }),
+        ],
+        endpoint: (device) => {
+            return { l1: 1, l2: 2 };
+        },
+        meta: {
+            multiEndpoint: true,
+            multiEndpointSkip: ["power_on_behavior"],
+        },
+        configure: tuya.configureMagicPacket,
+    },
 ];
