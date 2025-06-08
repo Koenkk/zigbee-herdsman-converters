7fdbbddd5bde23753194d7e4e81ea2bdd3bea57e
the-real-daju <smart-home1@outlook.de>
2025-06-08 18:25:23 +0200

    fix(build): allow explicit any in indexer and fix indent

--- src/indexer.ts
+++ src/indexer.ts
@@ -55,7 +55,8 @@ export async function buildIndex(fromSrc = false): Promise<void> {
 
         const filePath = path.join(devicesDir, moduleName);
         // load CommonJS or ES default export
-        const mod: any = await import(`./devices/${moduleName.slice(0, -3)}`);
+		// biome-ignore lint/suspicious/noExplicitAny: allow dynamic import here
+		const mod: any = await import(`./devices/${moduleName.slice(0, -3)}`);
         const definitions: DefinitionWithExtend[] = Array.isArray(mod.definitions)
             ? mod.definitions
             : Array.isArray(mod.default)
