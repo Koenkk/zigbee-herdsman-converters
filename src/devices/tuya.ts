c83d38795b6e0c8c2c24e021ae581c20fa7e9348
vecapetr <141762889+vecapetr@users.noreply.github.com>
2025-06-16 11:04:41 +0200

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -18057,16 +18057,16 @@ export const definitions: DefinitionWithExtend[] = [
      //const ea = exposes.access;
 
     // converter 0–1000 (Tuya) <-> 2700–6500 K
-const colorTempKelvinConverter = {
-    from: (val: number): number => {
-        const kelvin = 2700 + (val / 1000) * (6500 - 2700);
-        return Math.round(kelvin);
-    },
-    to: (val: number): number => {
-        const raw = ((val - 2700) / (6500 - 2700)) * 1000;
-        return Math.round(Math.max(0, Math.min(1000, raw)));
-    },
-};
+    const colorTempKelvinConverter = {
+        from: (val: number): number => {
+            const kelvin = 2700 + (val / 1000) * (6500 - 2700);
+            return Math.round(kelvin);
+        },
+        to: (val: number): number => {
+            const raw = ((val - 2700) / (6500 - 2700)) * 1000;
+            return Math.round(Math.max(0, Math.min(1000, raw)));
+        },
+    };
      module.exports = {
         fingerprint: [
             {
