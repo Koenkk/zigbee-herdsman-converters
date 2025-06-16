02a075a6854704d2c0d8e755d719af454e73f306
13717033460 <116949092+13717033460@users.noreply.github.com>
2025-06-16 16:34:15 +0800

    Update tuya.ts

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -9621,6 +9621,7 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
+
         zigbeeModel:["ZG-205ZL"],
         fingerprint: [
             {modelID: "TS0225", manufacturerName: "_TZE200_hl0ss9oa"},
