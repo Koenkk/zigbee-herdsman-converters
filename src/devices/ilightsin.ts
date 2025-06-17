1fa533a3664cf35d280b9515192e4715cfe80cc3
TT <random@random.random>
2025-06-17 10:37:09 +0300

    removed meta multiendpoint and changed to illuminance

--- src/devices/ilightsin.ts
+++ src/devices/ilightsin.ts
@@ -15,12 +15,10 @@ export const definitions: DefinitionWithExtend[] = [
         vendor: "Light",
         description: "iLightsin HSSA18-Z-MID zhaga module",
         extend: [
-            m.deviceEndpoints({endpoints: {"1": 1, "33": 33, "34": 34}}),
             m.light(),
-            m.illuminance({endpointNames: ["33"]}),
-            m.occupancy({endpointNames: ["34"]}),
+            m.illuminance(),
+            m.occupancy(),
             m.iasZoneAlarm({zoneType: "occupancy", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]}),
         ],
-        meta: {multiEndpoint: true},
     },
 ];
