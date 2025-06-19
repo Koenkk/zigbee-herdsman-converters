2bb568f9e37aade6d1d658ddb42696f309106466
Shane Gordon <se2gordon@students.latrobe.edu.au>
2025-06-19 23:23:17 +1000

    Added vendor file for Silicon Labs with DSP-061-3

---
+++ src/devices/silicon_labs.ts
@@ -0,0 +1,10 @@
+import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
+
+export default {
+    zigbeeModel: ['RB-ElectricityDsp-061-3'],
+    model: 'RB-ElectricityDsp-061-3',
+    vendor: 'siliconLabs',
+    description: 'Automatically generated definition',
+    extend: [m.onOff({"powerOnBehavior":false}), m.electricityMeter()],
+    meta: {},
+};
