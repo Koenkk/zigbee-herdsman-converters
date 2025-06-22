c04b3b4b8f0728db93bef28efca8e4445b47e2ae
Shane Gordon <4898021+s-gordon@users.noreply.github.com>
2025-06-22 22:10:09 +1000

    Corrected export to array and added entry to index

--- src/devices/silicon_labs.ts
+++ src/devices/silicon_labs.ts
@@ -1,10 +1,13 @@
-import * as m from "../lib/modernExtend";
+import * as m from '../lib/modernExtend';
+import type {DefinitionWithExtend} from "../lib/types";
 
-export default {
-    zigbeeModel: ["RB-ElectricityDsp-061-3"],
-    model: "RB-ElectricityDsp-061-3",
-    vendor: "siliconLabs",
-    description: "Automatically generated definition",
-    extend: [m.onOff({powerOnBehavior: false}), m.electricityMeter()],
-    meta: {},
-};
+export const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ["RB-ElectricityDsp-061-3"],
+        model: "RB-ElectricityDsp-061-3",
+        vendor: "siliconLabs",
+        description: "Automatically generated definition",
+        extend: [m.onOff({powerOnBehavior: false}), m.electricityMeter()],
+        meta: {},
+    },
+]
