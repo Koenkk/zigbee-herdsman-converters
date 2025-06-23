56425957d563609994f16c9319c5f3e2b1191dae
joliam <49640432+joliam@users.noreply.github.com>
2025-06-24 02:40:03 +0800

    feat(add): SZLMR10, CZF02, SZT06 (#9525)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/lincukoo.ts
+++ src/devices/lincukoo.ts
@@ -1,4 +1,5 @@
 import * as exposes from "../lib/exposes";
+import * as m from "../lib/modernExtend";
 import * as tuya from "../lib/tuya";
 import type {DefinitionWithExtend} from "../lib/types";
 
@@ -28,8 +29,12 @@ export const definitions: DefinitionWithExtend[] = [
             tuyaDatapoints: [
                 [4, "battery", tuya.valueConverter.raw],
                 [102, "alarm_status", tuya.valueConverterBasic.lookup({normal: 0, alarm: 1})],
-                [103, "alarm_ringtone", tuya.valueConverterBasic.lookup({mute: 0, ring1: 1, ring2: 2, ring3: 3})],
-                [101, "mode", tuya.valueConverterBasic.lookup({leakage: 0, shortage: 1})],
+                [
+                    103,
+                    "alarm_ringtone",
+                    tuya.valueConverterBasic.lookup({mute: tuya.enum(0), ring1: tuya.enum(1), ring2: tuya.enum(2), ring3: tuya.enum(3)}),
+                ],
+                [101, "mode", tuya.valueConverterBasic.lookup({leakage: tuya.enum(0), shortage: tuya.enum(1)})],
             ],
         },
     },
@@ -76,8 +81,8 @@ export const definitions: DefinitionWithExtend[] = [
                 [101, "indicator", tuya.valueConverter.onOff],
                 [104, "relay_switch", tuya.valueConverter.onOff],
                 [102, "radar_switch", tuya.valueConverter.onOff], // toggle to enable presence notifications in app is ignored
-                [106, "relay_mode", tuya.valueConverterBasic.lookup({auto: 0, manual: 1})],
-                [107, "radar_mode", tuya.valueConverterBasic.lookup({people_on: 0, people_off: 1})],
+                [106, "relay_mode", tuya.valueConverterBasic.lookup({auto: tuya.enum(0), manual: tuya.enum(1)})],
+                [107, "radar_mode", tuya.valueConverterBasic.lookup({people_on: tuya.enum(0), people_off: tuya.enum(1)})],
             ],
         },
     },
@@ -105,5 +110,100 @@ export const definitions: DefinitionWithExtend[] = [
                 [102, "usb_power", tuya.valueConverter.onOff],
             ],
         },
+    },
+    {
+        fingerprint: [{modelID: "TS0601", manufacturerName: "_TZE204_sndkanfr"}],
+        model: "SZLMR10",
+        vendor: "Lincukoo",
+        description: "Human Motion & Presence Sensor",
+        configure: tuya.configureMagicPacket,
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        onEvent: tuya.onEventSetTime,
+        exposes: [
+            e.presence(),
+            e.illuminance(),
+            e
+                .numeric("detection_distance", ea.STATE_SET)
+                .withValueMin(3)
+                .withValueMax(6)
+                .withValueStep(1.5)
+                .withUnit("m")
+                .withDescription("Maximum range"),
+            e.numeric("radar_sensitivity", ea.STATE_SET).withValueMin(0).withValueMax(9).withValueStep(1).withDescription("Sensitivity of the radar"),
+            e.numeric("fading_time", ea.STATE_SET).withValueMin(30).withValueMax(60).withValueStep(1).withDescription("Fading time").withUnit("s"),
+            e.binary("radar_switch", ea.STATE_SET, "ON", "OFF").withDescription("Radar switch"),
+            e.binary("indicator", ea.STATE_SET, "ON", "OFF").withDescription("LED indicator"),
+            e.enum("work_mode", ea.STATE_SET, ["pir_mode", "radar_mode", "combine_mode"]).withDescription("work mode of device"),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [1, "presence", tuya.valueConverter.trueFalse0],
+                [20, "illuminance", tuya.valueConverter.raw],
+                [13, "detection_distance", tuya.valueConverter.divideBy100],
+                [16, "radar_sensitivity", tuya.valueConverter.raw],
+                [103, "fading_time", tuya.valueConverter.raw],
+                [101, "indicator", tuya.valueConverter.onOff],
+                [102, "radar_switch", tuya.valueConverter.onOff],
+                [104, "work_mode", tuya.valueConverterBasic.lookup({pir_mode: tuya.enum(0), radar_mode: tuya.enum(1), combine_mode: tuya.enum(2)})],
+            ],
+        },
+    },
+
+    {
+        fingerprint: [{modelID: "TS0601", manufacturerName: "_TZE284_gw05grph"}],
+        model: "CZF02",
+        vendor: "Lincukoo",
+        description: "Finger Robot",
+        fromZigbee: [tuya.fz.datapoints],
+        toZigbee: [tuya.tz.datapoints],
+        onEvent: tuya.onEventSetTime,
+        configure: tuya.configureMagicPacket,
+        exposes: [
+            e.switch(),
+            e.enum("mode", ea.STATE_SET, ["click", "long_press"]).withDescription("work mode of the finger robot"),
+            e
+                .numeric("click_sustain_time", ea.STATE_SET)
+                .withValueMin(0.3)
+                .withValueMax(10)
+                .withValueStep(0.1)
+                .withDescription("keep times for click")
+                .withUnit("s"),
+            e
+                .numeric("arm_down_percent", ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(30)
+                .withValueStep(1)
+                .withDescription("the position for arm moving down"),
+            e
+                .numeric("arm_up_percent", ea.STATE_SET)
+                .withValueMin(0)
+                .withValueMax(30)
+                .withValueStep(1)
+                .withDescription("the position for arm moving up"),
+            e.binary("auto_adjustment", ea.STATE_SET, "ON", "OFF").withDescription("auto adjustment the arm position"),
+            e.binary("set_switch_state", ea.STATE_SET, "ON", "OFF").withDescription("set the switch display status"),
+            e.battery(),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [1, "state", tuya.valueConverter.onOff],
+                [2, "mode", tuya.valueConverterBasic.lookup({click: tuya.enum(0), long_press: tuya.enum(1)})],
+                [3, "click_sustain_time", tuya.valueConverter.divideBy10],
+                [5, "arm_down_percent", tuya.valueConverter.raw],
+                [6, "arm_up_percent", tuya.valueConverter.raw],
+                [101, "auto_adjustment", tuya.valueConverter.onOff],
+                [102, "set_switch_state", tuya.valueConverter.onOff],
+                [8, "battery", tuya.valueConverter.raw],
+            ],
+        },
+    },
+
+    {
+        fingerprint: [{modelID: "SZT06", manufacturerName: "LINCUKOO"}],
+        model: "SZT06",
+        vendor: "Lincukoo",
+        description: "Smart mini temperature and humidity sensor",
+        extend: [m.temperature(), m.humidity(), m.identify({isSleepy: true}), m.battery({voltage: true})],
     },
 ];
