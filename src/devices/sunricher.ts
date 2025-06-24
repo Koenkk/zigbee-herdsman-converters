8ad5fa167760a6c09a81511201637bea4c07ce5f
Niracler Li <i@niracler.com>
2025-06-25 03:51:22 +0800

    feat(add): SR-ZG2819S-DIM (#9540)

--- src/devices/sunricher.ts
+++ src/devices/sunricher.ts
@@ -140,6 +140,22 @@ async function syncTimeWithTimeZone(endpoint: Zh.Endpoint) {
 
 export const definitions: DefinitionWithExtend[] = [
     {
+        zigbeeModel: ["ZG2819S-DIM"],
+        model: "SR-ZG2819S-DIM",
+        vendor: "Sunricher",
+        description: "ZigBee dim remote",
+        extend: [
+            m.identify(),
+            m.deviceEndpoints({endpoints: {"1": 1, "2": 2, "3": 3, "4": 4}}),
+            m.battery(),
+            m.commandsOnOff({endpointNames: ["1", "2", "3", "4"]}),
+            m.commandsLevelCtrl({endpointNames: ["1", "2", "3", "4"]}),
+            m.commandsColorCtrl({endpointNames: ["1", "2", "3", "4"]}),
+            m.commandsScenes({endpointNames: ["1", "2", "3", "4"]}),
+        ],
+        meta: {multiEndpoint: true},
+    },
+    {
         zigbeeModel: ["HK-ZRC-K5&RS-TL-G"],
         model: "SR-ZG2836D5-G4",
         vendor: "Sunricher",
