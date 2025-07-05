603ae7675292d74c4f7769e51bcbb80d13ee9d0d
Jasper Wilhelm <wil@biba.uni-bremen.de>
2025-07-05 22:06:00 +0200

    add ledvance 625 panel

--- src/devices/ledvance.ts
+++ src/devices/ledvance.ts
@@ -67,6 +67,13 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [ledvanceLight({colorTemp: {range: [153, 370]}})],
     },
     {
+        zigbeeModel: ["PL_HCL625_01"],
+        model: "4058075364561",
+        vendor: "LEDVANCE",
+        description: "Biolux HCL Panel 625 Zigbee tunable white",
+        extend: [ledvanceLight({colorTemp: {range: [153, 370]}})],
+    },
+    {
         zigbeeModel: ["A60 RGBW Value II"],
         model: "AC25697",
         vendor: "LEDVANCE",
