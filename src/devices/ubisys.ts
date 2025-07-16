831ee2361ddd874df1faf52ffdc0d69cffd7dbec
senna1992 <149412175+senna1992@users.noreply.github.com>
2025-07-16 16:57:29 +0200

    Update ubisys.ts

--- src/devices/ubisys.ts
+++ src/devices/ubisys.ts
@@ -10,14 +10,13 @@ import * as exposes from "../lib/exposes";
 import {logger} from "../lib/logger";
 import * as m from "../lib/modernExtend";
 import * as reporting from "../lib/reporting";
-import type {DefinitionWithExtend, Fz, KeyValue, KeyValueAny, OnEventData, OnEventType, Tz, Zh} from "../lib/types";
+import type {DefinitionWithExtend, Fz, KeyValue, KeyValueAny, OnEventData, OnEventType, Tz, Zh, FromZigbeeConverter, FzMessage, KeyValueAny, Logger} from "../lib/types";
 import {ubisysModernExtend} from "../lib/ubisys";
 import * as utils from "../lib/utils";
 
 const NS = "zhc:ubisys";
 const e = exposes.presets;
 const ea = exposes.access;
-import type {FromZigbeeConverter, FzMessage, KeyValueAny, Logger, OnEventType, Zh} from '../lib/types';
 
 const ubisysVoltage: FromZigbeeConverter = {
     cluster: 'genPowerCfg',
