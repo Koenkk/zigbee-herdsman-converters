370225b6ff71835c8b953f5c6a5b0a0de0ca2548
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2025-07-18 16:03:18 +0200

    Drop "Z-Wave" mention for Eco-DIm.07/Eco-Dim.10

--- src/devices/ecodim.ts
+++ src/devices/ecodim.ts
@@ -65,7 +65,7 @@ export const definitions: DefinitionWithExtend[] = [
         ],
         model: "Eco-Dim.07/Eco-Dim.10",
         vendor: "EcoDim",
-        description: "Zigbee & Z-wave dimmer",
+        description: "Zigbee LED dimmer",
         ota: true,
         extend: [m.light({configureReporting: true})],
     },
