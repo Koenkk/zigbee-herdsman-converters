54b4b2a7766bb6b0e6c09ae03eaeb2f1d8c056cc
Arthur Bernardes <arthur.bernardes1@gmail.com>
2025-08-01 22:26:30 -0300

    feat(devices): Add new exposes to Tuya TRV601
    https://github.com/Koenkk/zigbee-herdsman-converters/issues/9725

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6212,7 +6212,7 @@ export const definitions: DefinitionWithExtend[] = [
         model: "TRV601",
         vendor: "Tuya",
         whiteLabel: [tuya.whitelabel("Sber", "SBDV-00185", "Thermostatic radiator valve", ["_TZE204_cvcu2p6e"])],
-        description: "Thermostatic radiator valve.",
+        description: "Thermostatic radiator valve",
         onEvent: tuya.onEventSetLocalTime,
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
@@ -6224,63 +6224,80 @@ export const definitions: DefinitionWithExtend[] = [
             e.min_temperature(),
             e.position(),
             e.window_detection(),
-            e.binary("window", ea.STATE, "OPEN", "CLOSE").withDescription("Window status closed or open "),
-            e.binary("alarm_switch", ea.STATE, "ON", "OFF").withDescription("Thermostat in error state"),
+            e.binary('window', ea.STATE, 'OPEN', 'CLOSE').withDescription('Window status closed or open '),
+            e.binary('alarm_switch', ea.STATE, 'ON', 'OFF').withDescription('Thermostat in error state'),
             e
                 .climate()
                 .withLocalTemperature(ea.STATE)
-                .withSetpoint("current_heating_setpoint", 5, 35, 0.5, ea.STATE_SET)
+                .withSetpoint('current_heating_setpoint', 5, 35, 0.5, ea.STATE_SET)
                 .withLocalTemperatureCalibration(-30, 30, 0.1, ea.STATE_SET)
                 .withPreset(
-                    ["auto", "manual", "off", "on"],
-                    "MANUAL MODE ☝ - In this mode, the device executes manual temperature setting. " +
+                    ['auto', 'manual', 'off', 'on'],
+                    'MANUAL MODE ☝ - In this mode, the device executes manual temperature setting. ' +
                         'When the set temperature is lower than the "minimum temperature", the valve is closed (forced closed). ' +
-                        "AUTO MODE ⏱ - In this mode, the device executes a preset week programming temperature time and temperature. " +
-                        "ON - In this mode, the thermostat stays open " +
-                        "OFF - In this mode, the thermostat stays closed",
+                        'AUTO MODE ⏱ - In this mode, the device executes a preset week programming temperature time and temperature. ' +
+                        'ON - In this mode, the thermostat stays open ' +
+                        'OFF - In this mode, the thermostat stays closed',
                 )
-                .withSystemMode(["auto", "heat", "off"], ea.STATE)
-                .withRunningState(["idle", "heat"], ea.STATE),
-            ...tuya.exposes.scheduleAllDays(ea.STATE_SET, "HH:MM/C HH:MM/C HH:MM/C HH:MM/C"),
-            e
-                .enum("mode", ea.STATE_SET, ["comfort", "eco"])
+                .withSystemMode(['auto', 'heat', 'off'], ea.STATE)
+                .withRunningState(['idle', 'heat'], ea.STATE),
+            ...tuya.exposes.scheduleAllDays(ea.STATE_SET, 'HH:MM/C HH:MM/C HH:MM/C HH:MM/C'),
+			e
+                .enum('screen_orientation', ea.STATE_SET, ['up', 'down'])
                 .withDescription(
-                    "Hysteresis - comfort > switches off/on exactly at reached " +
-                        "temperature with valve smooth from 0 to 100%, eco > 0.5 degrees above or below, valve either 0 or 100%",
+                    'How do you look at the display (up and down supported only)',
                 ),
+			e
+				.enum('display_brightness', ea.STATE_SET, ['high', 'middle', 'low'] )
+				.withDescription('how strong the LED diplay is light up (high is default)',
+				),
+
+            e
+                .enum('mode', ea.STATE_SET, ['comfort', 'eco'])
+                .withDescription(
+                    'Hysteresis - comfort > switches off/on exactly at reached ' +
+                        'temperature with valve smooth from 0 to 100%, eco > 0.5 degrees above or below, valve either 0 or 100%',
+                ),
+			e
+				.numeric('switch_deviation_eco', ea.STATE_SET )
+				.withValueMin(0.5)
+				.withValueMax(5.0)
+				.withValueStep(0.1)
+				.withUnit('°C')
+				.withDescription(
+                    'Switch deviation (energy-saving mode only)',
+                ),
+
+
         ],
         meta: {
             tuyaDatapoints: [
                 [1, null, tuya.valueConverter.thermostatSystemModeAndPreset(null)],
-                [1, "system_mode", tuya.valueConverter.thermostatSystemModeAndPreset("system_mode")],
-                [1, "preset", tuya.valueConverter.thermostatSystemModeAndPreset("preset")],
-                [2, "current_heating_setpoint", tuya.valueConverter.divideBy10],
-                [3, "local_temperature", tuya.valueConverter.divideBy10],
-                [6, "running_state", tuya.valueConverterBasic.lookup({heat: 1, idle: 0})],
-                [7, "window", tuya.valueConverterBasic.lookup({OPEN: 1, CLOSE: 0})],
-                [8, "window_detection", tuya.valueConverter.onOff],
-                [12, "child_lock", tuya.valueConverter.lockUnlock],
-                [13, "battery", tuya.valueConverter.raw],
-                [14, "alarm_switch", tuya.valueConverter.onOff],
-                [15, "min_temperature", tuya.valueConverter.divideBy10],
-                [16, "max_temperature", tuya.valueConverter.divideBy10],
-                [17, "schedule_monday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(1)],
-                [18, "schedule_tuesday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(2)],
-                [19, "schedule_wednesday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(3)],
-                [20, "schedule_thursday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(4)],
-                [21, "schedule_friday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(5)],
-                [22, "schedule_saturday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(6)],
-                [23, "schedule_sunday", tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(7)],
-                [101, "local_temperature_calibration", tuya.valueConverter.localTempCalibration1],
-                [108, "position", tuya.valueConverter.divideBy10],
-                [
-                    114,
-                    "mode",
-                    tuya.valueConverterBasic.lookup({
-                        comfort: tuya.enum(0),
-                        eco: tuya.enum(1),
-                    }),
-                ],
+                [1, 'system_mode', tuya.valueConverter.thermostatSystemModeAndPreset('system_mode')],
+                [1, 'preset', tuya.valueConverter.thermostatSystemModeAndPreset('preset')],
+                [2, 'current_heating_setpoint', tuya.valueConverter.divideBy10],
+                [3, 'local_temperature', tuya.valueConverter.divideBy10],
+                [6, 'running_state', tuya.valueConverterBasic.lookup({heat: 1, idle: 0})],
+                [7, 'window', tuya.valueConverterBasic.lookup({OPEN: 1, CLOSE: 0})],
+                [8, 'window_detection', tuya.valueConverter.onOff],
+                [12, 'child_lock', tuya.valueConverter.lockUnlock],
+                [13, 'battery', tuya.valueConverter.raw],
+                [14, 'alarm_switch', tuya.valueConverter.onOff],
+                [15, 'min_temperature', tuya.valueConverter.divideBy10],
+                [16, 'max_temperature', tuya.valueConverter.divideBy10],
+                [17, 'schedule_monday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(1)],
+                [18, 'schedule_tuesday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(2)],
+                [19, 'schedule_wednesday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(3)],
+                [20, 'schedule_thursday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(4)],
+                [21, 'schedule_friday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(5)],
+                [22, 'schedule_saturday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(6)],
+                [23, 'schedule_sunday', tuya.valueConverter.thermostatScheduleDayMultiDPWithDayNumber(7)],
+                [101, 'local_temperature_calibration', tuya.valueConverter.localTempCalibration1],
+                [108, 'position', tuya.valueConverter.divideBy10],
+				[111, 'display_brightness',tuya.valueConverterBasic.lookup({'high': tuya.enum(0), 'middle': tuya.enum(1), 'low': tuya.enum(2)})],
+				[113, 'screen_orientation',tuya.valueConverterBasic.lookup({up: tuya.enum(0), down: tuya.enum(2)})],
+                [114, 'mode', tuya.valueConverterBasic.lookup({comfort: tuya.enum(0), eco: tuya.enum(1)})],
+				[115, 'switch_deviation_eco',tuya.valueConverter.divideBy10],
             ],
         },
     },
