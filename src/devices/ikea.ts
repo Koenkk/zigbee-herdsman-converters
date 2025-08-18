31bb16c067c8817f85e4272384c194ecd89b076b
AlexanderLeporiday <166955433+AlexanderLeporiday@users.noreply.github.com>
2025-08-18 20:12:08 +0100

    fix(detect): Detect `JETSTROM 6060 NA` as IKEA L2207 (#9815)

--- src/devices/ikea.ts
+++ src/devices/ikea.ts
@@ -438,7 +438,7 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [addCustomClusterManuSpecificIkeaUnknown(), ikeaLight({colorTemp: true}), m.identify()],
     },
     {
-        zigbeeModel: ["JETSTROM 6060", "JETSTROM 6060 JP"],
+        zigbeeModel: ["JETSTROM 6060", "JETSTROM 6060 JP", "JETSTROM 6060 NA"],
         model: "L2207",
         vendor: "IKEA",
         description: "JETSTROM ceiling light panel, white spectrum, 60x60 cm",
