8ad13072ee8d8b09e658ea8e5d3d150b5607ed0b
Rich Hemke <rjh@homeseer.com>
2025-08-21 14:52:10 -0400

    fix: HomeSeer: Fixed file format
    Updated file to the correct format

--- src/devices/homeseer.ts
+++ src/devices/homeseer.ts
@@ -1,10 +1,12 @@
 import * as m from "../lib/modernExtend";
+import type {DefinitionWithExtend} from "../lib/types";
 
-export default {
-    zigbeeModel: ["DS150ZB"],
-    model: "DS150ZB",
-    vendor: "HomeSeer",
-    description: "DS150ZB Door Sensor",
-    extend: [m.battery(), m.iasZoneAlarm({zoneType: "contact", zoneAttributes: ["alarm_1", "battery_low"]})],
-    meta: {},
-};
+export const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ["DS150ZB"],
+        model: "DS150ZB",
+        vendor: "HomeSeer",
+        description: "DS150ZB Door Sensor",
+        extend: [m.battery(), m.iasZoneAlarm({zoneType: "contact", zoneAttributes: ["alarm_1", "battery_low"]})],
+    },
+];
