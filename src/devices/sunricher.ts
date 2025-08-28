0ee6ffe9098681c6eef30c1387ce7d10d01b7ba9
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2025-08-28 22:19:21 +0200

    Update SR-ZG9101SAC-HP-SWITCH-2CH
    Fix power measurement depending on the firmware used by the device

--- src/devices/sunricher.ts
+++ src/devices/sunricher.ts
@@ -1346,16 +1346,30 @@ export const definitions: DefinitionWithExtend[] = [
         configure: async (device, coordinatorEndpoint) => {
             const endpoint1 = device.getEndpoint(1);
             const endpoint2 = device.getEndpoint(2);
-            await reporting.bind(endpoint1, coordinatorEndpoint, ["genOnOff", "haElectricalMeasurement", "seMetering"]);
+            await reporting.bind(endpoint1, coordinatorEndpoint, ["genOnOff"]);
             await reporting.bind(endpoint2, coordinatorEndpoint, ["genOnOff"]);
             await reporting.onOff(endpoint1);
             await reporting.onOff(endpoint2);
-            await reporting.readEletricalMeasurementMultiplierDivisors(endpoint1);
-            await reporting.activePower(endpoint1);
-            await reporting.rmsCurrent(endpoint1, {min: 10, change: 10});
-            await reporting.rmsVoltage(endpoint1, {min: 10});
-            await reporting.readMeteringMultiplierDivisor(endpoint1);
-            await reporting.currentSummDelivered(endpoint1);
+            if (
+                (device && device.softwareBuildID >= "2.9.2_r3")) {
+                // newer firmware version power reports are on endpoint 11
+                const endpoint11 = device.getEndpoint(11);
+                await reporting.bind(endpoint11, coordinatorEndpoint, ["haElectricalMeasurement", "seMetering"]);
+                await reporting.readEletricalMeasurementMultiplierDivisors(endpoint11);
+                await reporting.activePower(endpoint11);
+                await reporting.rmsCurrent(endpoint11, {min: 10, change: 10});
+                await reporting.rmsVoltage(endpoint11, {min: 10});
+                await reporting.readMeteringMultiplierDivisor(endpoint11);
+            } else if (device && device.softwareBuildID <= "2.5.3_r2") {
+                // older firmware version power reports are on endpoint 1
+                await reporting.bind(endpoint1, coordinatorEndpoint, ["haElectricalMeasurement", "seMetering"]);
+                await reporting.readEletricalMeasurementMultiplierDivisors(endpoint1);
+                await reporting.activePower(endpoint1);
+                await reporting.rmsCurrent(endpoint1, {min: 10, change: 10});
+                await reporting.rmsVoltage(endpoint1, {min: 10});
+                await reporting.readMeteringMultiplierDivisor(endpoint1);
+                await reporting.currentSummDelivered(endpoint1);
+            },
         },
     },
     {
