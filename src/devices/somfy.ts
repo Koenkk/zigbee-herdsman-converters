a3b6dd7c99dd6924ce322abaab983d95d979baf9
Luuk van den Ouweland <info@luukvdo.nl>
2025-09-07 20:37:00 +0200

    Add SOMFY Sonesse2 28 WF Li-Ion Roller.

--- src/devices/somfy.ts
+++ src/devices/somfy.ts
@@ -23,6 +23,13 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [m.battery(), m.windowCovering({controls: ["lift"]})],
     },
     {
+        zigbeeModel: ["Sonesse2 28 WF Li-Ion Roller"],
+        model: "Sonesse2 28 WF Li-Ion Roller",
+        vendor: "1245943",
+        description: "Sonesse2 28 WF Li-Ion roller shades",
+        extend: [m.battery(), m.windowCovering({controls: ["lift"]})],
+    },
+    {
         zigbeeModel: ["Sonesse Ultra 30 WF Li-Ion Rolle"],
         model: "SOMFY-1241752",
         vendor: "SOMFY",
