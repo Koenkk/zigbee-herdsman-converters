6d9b09428df24c508f0fe092a9eaf54bbf0e6eaf
Arthur Bernardes <arthur.bernardes1@gmail.com>
2025-09-07 16:48:54 -0300

    fix(detect): Detect `_TZE284_9xstqowh` as Tongou TOQCB2-80 (#9940)
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -15473,7 +15473,14 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_q22avxbv", "_TZE204_mrffaamu", "_TZE204_tzreobvu", "_TZE284_mrffaamu", "TZE284_tzreobvu"]),
+        fingerprint: tuya.fingerprint("TS0601", [
+            "_TZE204_q22avxbv",
+            "_TZE204_mrffaamu",
+            "_TZE204_tzreobvu",
+            "_TZE284_mrffaamu",
+            "_TZE284_tzreobvu",
+            "_TZE284_9xstqowh",
+        ]),
         model: "TOQCB2-80",
         vendor: "Tongou",
         description: "Smart circuit breaker",
