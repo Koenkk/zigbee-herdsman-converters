07f6605d3103c2fa18656c8425fc372012f7e415
ashp8i <ashp8@hotmail.com>
2025-09-08 22:36:39 +0100

    Create espressif.ts

---
+++ src/devices/espressif.ts
@@ -0,0 +1,21 @@
+import * as fz from "../converters/fromZigbee";
+import * as reporting from "../lib/reporting";
+import type {DefinitionWithExtend} from "../lib/types";
+
+export const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ['ZigbeeRangeExtender'],
+        model: 'ZigbeeRangeExtender',
+        vendor: 'Espressif',
+        description: "Espressif ESP32-C6/H2 Router",
+        fromZigbee: [fz.linkquality_from_basic],
+        toZigbee: [],
+        exposes: [],
+        configure: async (device, coordinatorEndpoint) => {
+            const endpoint = device.getEndpoint(8);
+            const payload = [{attribute: "zclVersion" as const, minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 0}];
+            await reporting.bind(endpoint, coordinatorEndpoint, ["genBasic"]);
+            await endpoint.configureReporting("genBasic", payload);
+        },
+    },
+];
