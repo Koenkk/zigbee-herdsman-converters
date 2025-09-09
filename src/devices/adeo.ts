2d1bd76f2cf6c559b1a945412f20e97909288f31
Marek Krawczyk <bigmanpl@gmail.com>
2025-09-09 19:23:07 +0200

    Add ADEO ZB-WaterSensor-D0001

--- src/devices/adeo.ts
+++ src/devices/adeo.ts
@@ -450,4 +450,11 @@ export const definitions: DefinitionWithExtend[] = [
         description: "ENKI LEXMAN wireless smart door window sensor",
         extend: [m.battery(), m.iasZoneAlarm({zoneType: "contact", zoneAttributes: ["alarm_1", "tamper", "battery_low"]})],
     },
+    {
+        zigbeeModel: ['ZB-WaterSensor-D0001'],
+        model: 'ZB-WaterSensor-D0001',
+        vendor: 'ADEO',
+        description: 'ENKI LEXMAN water leak sensor',
+        extend: [m.battery(), m.iasZoneAlarm({"zoneType":"generic","zoneAttributes":["alarm_1","alarm_2","tamper","battery_low"]})],
+        },
 ];
