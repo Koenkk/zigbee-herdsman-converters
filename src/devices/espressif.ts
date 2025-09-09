65a00bb6b31e3c896519059e9299e4717ecc20af
ashp8i <ashp8@hotmail.com>
2025-09-09 20:35:05 +0100

    Update espressif.ts
    make the definition more resilient

--- src/devices/espressif.ts
+++ src/devices/espressif.ts
@@ -13,9 +13,15 @@ export const definitions: DefinitionWithExtend[] = [
         exposes: [],
         configure: async (device, coordinatorEndpoint) => {
             const endpoint = device.getEndpoint(1);
-            const payload = [{attribute: "zclVersion" as const, minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 0}];
-            await reporting.bind(endpoint, coordinatorEndpoint, ["genBasic"]);
-            await endpoint.configureReporting("genBasic", payload);
+            if (!endpoint) {
+                return; // Exit if endpoint 1 is not found
+            }
+            // Check if the endpoint supports the genBasic cluster
+            if (endpoint.supportsInputCluster('genBasic')) {
+                const payload = [{attribute: "zclVersion" as const, minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 0}];
+                await reporting.bind(endpoint, coordinatorEndpoint, ["genBasic"]);
+                await endpoint.configureReporting("genBasic", payload);
+            }
         },
     },
 ];
