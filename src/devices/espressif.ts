d3f2b56761df576dc6148e87033b028faa1d83a0
ashp8i <ashp8@hotmail.com>
2025-09-09 20:27:22 +0100

    Update espressif.ts
    no need for manual / duplicate linkquality

--- src/devices/espressif.ts
+++ src/devices/espressif.ts
@@ -8,11 +8,11 @@ export const definitions: DefinitionWithExtend[] = [
         model: 'ZigbeeRangeExtender',
         vendor: 'Espressif',
         description: "Espressif ESP32-C6/H2 Router",
-        fromZigbee: [fz.linkquality_from_basic],
+        fromZigbee: [],
         toZigbee: [],
         exposes: [],
         configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(8);
+            const endpoint = device.getEndpoint(1);
             const payload = [{attribute: "zclVersion" as const, minimumReportInterval: 0, maximumReportInterval: 3600, reportableChange: 0}];
             await reporting.bind(endpoint, coordinatorEndpoint, ["genBasic"]);
             await endpoint.configureReporting("genBasic", payload);
