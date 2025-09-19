ea52b4eaf1d37b1ce0d8b8727d53ad05d376e5f5
jintj <jintj@3reality.com>
2025-09-19 08:43:45 +0800

    UPDATE

--- src/devices/third_reality.ts
+++ src/devices/third_reality.ts
@@ -73,8 +73,8 @@ export const definitions: DefinitionWithExtend[] = [
                 ID: 0xff02,
                 manufacturerCode: 0x1233,
                 attributes: {
-                    on_to_off_delay: {ID: 0x0001, type: Zcl.DataType.UINT16},
-                    off_to_on_delay: {ID: 0x0002, type: Zcl.DataType.UINT16},
+                    onToOffDelay: {ID: 0x0001, type: Zcl.DataType.UINT16},
+                    offToOnDelay: {ID: 0x0002, type: Zcl.DataType.UINT16},
                 },
                 commands: {},
                 commandsResponse: {},
