2a08575b69f336ebf646bd845fc95ab158254f9b
1tigris1 <79746182+1tigris1@users.noreply.github.com>
2025-09-30 16:06:42 +0300

    Update tuya.ts
    Device: Nous A10Z

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -7080,6 +7080,7 @@ export const definitions: DefinitionWithExtend[] = [
             tuya.whitelabel("Nous", "A9Z", "Smart ZigBee Socket", ["_TZ3210_ddigca5n"]),
             tuya.whitelabel("Girier", "JR-ZPM01", "Smart Plug", ["_TZ3000_ww6drja5"]),
             tuya.whitelabel("Nous", "A7Z", "Smart ZigBee Socket", ["_TZ3210_rwmitwj4"]),
+            tuya.whitelabel("Nous", "A10Z", "Smart ZigBee Socket", ["_TZ3210_jlf1nepw"]),
             tuya.whitelabel("Zbeacon", "TS011F_plug_1_1", "Smart plug (with power monitoring)", ["Zbeacon"]),
             tuya.whitelabel("NEO", "NAS-WR01B", "Smart plug (with electrical measurements)", ["_TZ3000_gjnozsaz"]),
         ],
