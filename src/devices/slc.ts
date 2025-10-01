0ba4bc1e62dc45f04f207ed07e535e6da52c1087
Stu Yarrow <stu@flyozone.com>
2025-10-01 14:13:39 +0200

    Fix quotes

--- src/devices/slc.ts
+++ src/devices/slc.ts
@@ -6,10 +6,10 @@ import type {DefinitionWithExtend} from "../lib/types";
 
 export const definitions: DefinitionWithExtend[] = [
     {
-        zigbeeModel: ['S32052'],
-        model: 'S32052',
-        vendor: 'SLC',
-        description: 'SmartOne Driver CC 2CH 350-1500mA TW 50W',
-        extend: [m.light({"colorTemp": {"range": [160,450]}})]
+        zigbeeModel: ["S32052"],
+        model: "S32052",
+        vendor: "SLC",
+        description: "SmartOne Driver CC 2CH 350-1500mA TW 50W",
+        extend: [m.light({colorTemp: {range: [160,450]}})]
     }
 ];
