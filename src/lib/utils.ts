ecb04da93a2e723050b2f0ccf0514be1aecbf88a
Koen Kanters <koenkanters94@gmail.com>
2025-10-03 21:13:21 +0200

    fix: Fix race condition in `getClusterAttributeValue` https://github.com/Koenkk/zigbee2mqtt/issues/28808

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -513,13 +513,15 @@ export async function getClusterAttributeValue<
     fallback: ClusterOrRawAttributes<Cl, Custom>[Attr] = undefined,
 ): Promise<ClusterOrRawAttributes<Cl, Custom>[Attr]> {
     try {
-        if (endpoint.getClusterAttributeValue(cluster, attribute) == null) {
-            await endpoint.read<Cl, Custom>(cluster, [attribute] as ClusterOrRawAttributeKeys<Cl, Custom>, {
+        const value = endpoint.getClusterAttributeValue(cluster, attribute);
+        if (value == null) {
+            const result = await endpoint.read<Cl, Custom>(cluster, [attribute] as ClusterOrRawAttributeKeys<Cl, Custom>, {
                 sendPolicy: "immediate",
                 disableRecovery: true,
             });
+            return result[attribute] ?? fallback;
         }
-        return endpoint.getClusterAttributeValue(cluster, attribute) as ClusterOrRawAttributes<Cl, Custom>[Attr];
+        return value as ClusterOrRawAttributes<Cl, Custom>[Attr];
     } catch (error) {
         if (fallback !== undefined) return fallback;
         throw error;
