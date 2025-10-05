a4c9ef1df462ada67d912429838982160621e5b5
Koen Kanters <koenkanters94@gmail.com>
2025-10-05 15:06:06 +0200

    fix(detect): Detect `_TZE284_v5xjyphj` as IOTPerfect PF-PM02D-TYZ https://github.com/Koenkk/zigbee-herdsman-converters/issues/10198

--- src/devices/iotperfect.ts
+++ src/devices/iotperfect.ts
@@ -9,7 +9,7 @@ const ea = exposes.access;
 
 export const definitions: DefinitionWithExtend[] = [
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_vrjkcam9", "_TZE200_d0ypnbvn", "_TZE204_v5xjyphj", "_TZE204_d0ypnbvn"]),
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_vrjkcam9", "_TZE200_d0ypnbvn", "_TZE204_v5xjyphj", "_TZE204_d0ypnbvn", "_TZE284_v5xjyphj"]),
         model: "PF-PM02D-TYZ",
         vendor: "IOTPerfect",
         description: "Smart water/gas valve",
