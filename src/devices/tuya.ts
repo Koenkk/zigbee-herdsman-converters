da5f39d2f91fa3fc21f6d9b4007ce06c32cd1dcb
Xenomes <xenomes@outlook.com>
2025-10-14 21:17:56 +0200

    Update tuya.ts TS0601_TZE284_gyzlwu5q
    Same picture as TS0601_smoke_4
    https://www.zigbee2mqtt.io/images/devices/TS0601_smoke_4.png

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -20129,4 +20129,33 @@ export const definitions: DefinitionWithExtend[] = [
             ],
         },
     },
+    {
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE284_gyzlwu5q"]),
+        zigbeeModel: ["TS0601"],
+        model: "TS0601_TZE284_gyzlwu5q",
+        vendor: "Tuya",
+        description: "Smoke detector with temperature and humidity sensor",
+        extend: [tuya.modernExtend.tuyaBase({dp: true})],
+        exposes: [
+            e.smoke(),
+            e.temperature(),
+            e.humidity(),
+            tuya.exposes.batteryState(),
+            e.enum("self_test", ea.STATE, ["Checking", "Check success", "Check failure"]).withDescription('Test button status'),
+            e.enum('silence', ea.STATE_SET, ['ON']).withDescription('Silence alarm'),
+            e.text("version", ea.STATE_GET).withDescription('Device version')
+        ],
+        meta: {
+            // All datapoints go in here
+            tuyaDatapoints: [
+                [1, "smoke", tuya.valueConverter.trueFalse0],
+                [9, "self_test", tuya.valueConverterBasic.lookup({ checking: 0, check_success: 1, check_failure: 2 })],
+                [14, "battery_state", tuya.valueConverter.batteryState],
+                [16, "silence", tuya.valueConverter.onOff],
+                [23, "temperature", tuya.valueConverter.divideBy10],
+                [24, "humidity", tuya.valueConverter.raw],
+                [103, "version", tuya.valueConverter.raw], // maybe!?
+            ],
+        },
+    },
 ];
