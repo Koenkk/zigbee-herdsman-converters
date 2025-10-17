a937e3c1f990df67ca01f9902f2b304048b25121
Adam Paul <74373613+adampaul012@users.noreply.github.com>
2025-10-17 11:33:06 +0200

    Add Niceboy ION temperature and humidity sensor support

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -1602,6 +1602,23 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_t3xd7l44"]),
+        model: "TS0601_temperature_humidity_sensor_with_clock",
+        vendor: "Tuya",
+        description: "Temperature & humidity sensor",
+        extend: [tuya.modernExtend.tuyaBase({dp: true, forceTimeUpdates: true, queryOnConfigure: true})],
+        exposes: [e.temperature(), e.humidity(), e.battery(), tuya.exposes.temperatureUnit()],
+        meta: {
+            tuyaDatapoints: [
+                [1, "temperature", tuya.valueConverter.divideBy10],
+                [2, "humidity", tuya.valueConverter.raw],
+                [4, "battery", tuya.valueConverter.raw], // maybe?
+                [9, "temperature_unit", tuya.valueConverter.temperatureUnitEnum],
+                [19, "temperature_sensitivity", tuya.valueConverter.raw], // maybe? commented this out for now
+            ],
+        },
+    },
+    {
         fingerprint: tuya.fingerprint("TS0601", ["_TZE284_9ern5sfh"]),
         model: "TS0601_temperature_humidity_sensor_4",
         vendor: "Tuya",
