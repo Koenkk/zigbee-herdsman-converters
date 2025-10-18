b09db911d2b56058666b6fd5382748df274ac1f9
joliam <49640432+joliam@users.noreply.github.com>
2025-10-18 19:54:34 +0800

    feat(add): CZB01 (#10393)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/lincukoo.ts
+++ src/devices/lincukoo.ts
@@ -327,4 +327,11 @@ export const definitions: DefinitionWithExtend[] = [
             ],
         },
     },
+    {
+        zigbeeModel: ["CZB01"],
+        model: "CZB01",
+        vendor: "Lincukoo",
+        description: "Wireless switch with 1 button",
+        extend: [m.battery(), m.commandsOnOff()],
+    },
 ];
