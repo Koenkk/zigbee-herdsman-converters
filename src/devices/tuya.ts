739d9c1598df68dbe147186f49afa98f951d0d7b
alimac87 <23415521+alimac87@users.noreply.github.com>
2025-10-20 19:59:22 +0100

    feat(add): TZE284_4cqhd2ha (#10443)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -9638,34 +9638,6 @@ export const definitions: DefinitionWithExtend[] = [
         ],
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_8ply8mjj"]),
-        model: "COZIGVS",
-        vendor: "Conecto",
-        description: "Vibration sensor",
-        fromZigbee: [tuya.fz.datapoints],
-        toZigbee: [tuya.tz.datapoints],
-        configure: tuya.configureMagicPacket,
-        exposes: [
-            e.vibration(),
-            e
-                .numeric("sensitivity", ea.STATE_SET)
-                .withValueMin(0)
-                .withValueMax(2)
-                .withDescription(
-                    "Sensitivity of the sensor (single press the button when muted to switch between" +
-                        " low (one beep), medium (two beeps) and max (three beeps))",
-                ),
-            e.text("buzzer_mute", ea.STATE).withDescription("ON when buzzer is muted (double press the button on device to toggle)"),
-        ],
-        meta: {
-            tuyaDatapoints: [
-                [1, "vibration", tuya.valueConverter.trueFalse1],
-                [101, "sensitivity", tuya.valueConverter.raw],
-                [103, "buzzer_mute", tuya.valueConverter.onOff],
-            ],
-        },
-    },
-    {
         fingerprint: tuya.fingerprint("TS011F", ["_TZ3000_8bxrzyxz", "_TZ3000_ky0fq4ho"]),
         model: "TS011F_din_smart_relay",
         description: "Din smart relay (with power monitoring)",
@@ -20469,6 +20441,33 @@ export const definitions: DefinitionWithExtend[] = [
                 [23, "temperature", tuya.valueConverter.divideBy10],
                 [24, "humidity", tuya.valueConverter.raw],
                 [103, "version", tuya.valueConverter.raw], // maybe!?
+            ],
+        },
+    },
+    {
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE284_4cqhd2ha", "_TZE200_8ply8mjj"]),
+        model: "TZE284_4cqhd2ha",
+        vendor: "Girier",
+        description: "Vibration sensor",
+        extend: [tuya.modernExtend.tuyaBase({dp: true})],
+        whiteLabel: [tuya.whitelabel("Conecto", "COZIGVS", "Vibration sensor", ["_TZE200_8ply8mjj"])],
+        exposes: [
+            e.vibration(),
+            e
+                .numeric("sensitivity", ea.STATE)
+                .withValueMin(0)
+                .withValueMax(2)
+                .withDescription(
+                    "Sensitivity of the sensor (single press the button when muted to switch between" +
+                        " low (one beep), medium (two beeps) and max (three beeps))",
+                ),
+            e.text("buzzer_mute", ea.STATE).withDescription("ON when buzzer is muted (double press the button on device to toggle)"),
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [1, "vibration", tuya.valueConverter.trueFalse1],
+                [101, "sensitivity", tuya.valueConverter.raw],
+                [103, "buzzer_mute", tuya.valueConverter.onOff],
             ],
         },
     },
