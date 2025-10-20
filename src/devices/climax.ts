fd4c8c858950b4fd1f82fdbca4dccd52d3123951
Koen Kanters <koenkanters94@gmail.com>
2025-10-20 20:50:18 +0200

    fix(detect): Detect `SRACB_00.00.03.07TC` as Climax SRAC-23B-ZBSR https://github.com/Koenkk/zigbee2mqtt/issues/29193

--- src/devices/climax.ts
+++ src/devices/climax.ts
@@ -85,7 +85,7 @@ export const definitions: DefinitionWithExtend[] = [
         exposes: [e.temperature(), e.humidity()],
     },
     {
-        zigbeeModel: ["SRACBP5_00.00.03.06TC", "SRAC_00.00.00.16TC", "SRACBP5_00.00.05.10TC"],
+        zigbeeModel: ["SRACBP5_00.00.03.06TC", "SRAC_00.00.00.16TC", "SRACBP5_00.00.05.10TC", "SRACB_00.00.03.07TC"],
         model: "SRAC-23B-ZBSR",
         vendor: "Climax",
         description: "Smart siren",
