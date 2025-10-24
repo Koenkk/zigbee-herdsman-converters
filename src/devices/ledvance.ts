107011b524708b5bb2c88d3bcba94efce765d682
Gianmarco Mameli <57061995+gianmarco-mameli@users.noreply.github.com>
2025-10-24 18:44:55 +0200

    Add LED Tube T8 EM Connected to devices list

--- src/devices/ledvance.ts
+++ src/devices/ledvance.ts
@@ -360,4 +360,11 @@ export const definitions: DefinitionWithExtend[] = [
         description: "SMART+ Outdoor Plug EU",
         extend: [ledvanceOnOff({powerOnBehavior: false})],
     },
+    {
+        zigbeeModel: ['TUBE_T8_CON_1200_16W_840ZBV'],
+        model: '4058075823976',
+        vendor: 'LEDVANCE',
+        description: 'LED Tube T8 EM Connected 1200 mm 16W 840',
+        extend: [ledvanceOnOff({powerOnBehavior: false})],
+    },
 ];
