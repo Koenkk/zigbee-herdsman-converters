1532d67b80cb3fb6de24a36a492edba1be3d8683
bapesupreme <roundup.minibus_7f@icloud.com>
2025-10-27 07:36:28 -0700

    Add files via upload

---
+++ src/devices/eria.ts
@@ -0,0 +1,10 @@
+import * as m from 'zigbee-herdsman-converters/lib/modernExtend';
+
+export default {
+    zigbeeModel: ['AD-CTW143001'],
+    model: 'AD-CTW143001',
+    vendor: 'ERIA',
+    description: 'Automatically generated definition',
+    extend: [m.light({"colorTemp":{"range":[153,500]}})],
+    meta: {},
+};
