c0d3f0867b73bf2a0f2914f67734debd38867bc6
jintj <jintj@3reality.com>
2025-10-31 16:55:21 +0800

    update

--- src/devices/third_reality.ts
+++ src/devices/third_reality.ts
@@ -337,6 +337,8 @@ export const definitions: DefinitionWithExtend[] = [
                     infraredEnable: {ID: 0x0000, type: Zcl.DataType.UINT8},
                     compensationSpeed: {ID: 0x0001, type: Zcl.DataType.INT8},
                     limitPosition: {ID: 0x0002, type: Zcl.DataType.UINT16},
+                    totalCycleTimes: {ID: 0x0003, type: Zcl.DataType.UINT16}, 
+                    lastRemainingBatteryPercentage: {ID: 0x0004, type: Zcl.DataType.UINT8}, 
                 },
                 commands: {},
                 commandsResponse: {},
