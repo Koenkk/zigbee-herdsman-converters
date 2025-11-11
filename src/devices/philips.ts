cfe8de84ca6f0d40c5a1cdf3f95fb3b6bdd5346e
Wombatzoner <erf2@wombatzone.com>
2025-11-11 16:05:21 -0500

    feat(add): 929003856303 (#10716)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -780,6 +780,13 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [philips.m.light()],
     },
     {
+        zigbeeModel: ["LWA035"],
+        model: "929003856303",
+        vendor: "Philips",
+        description: "Hue White A19 bulb E26 810lm with Bluetooth",
+        extend: [philips.m.light()],
+    },
+    {
         zigbeeModel: ["LCT026", "7602031P7", "7602031U7", "7602031PU", "7602031J6", "915005822501", "915005822001"],
         model: "7602031P7",
         vendor: "Philips",
