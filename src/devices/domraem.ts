32274f2ee58d4bc3391137ffdf7a6579c841b644
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2025-11-21 17:51:33 +0100

    Update DOMRAEM device definitions and fingerprints
    Refactor DOMRAEM device definitions to include additional models and update fingerprint structure.
    
    It should resolve:
    https://github.com/Koenkk/zigbee2mqtt/issues/29699
    https://github.com/Koenkk/zigbee2mqtt/issues/29645
    https://github.com/Koenkk/zigbee2mqtt/issues/29631
    https://github.com/Koenkk/zigbee2mqtt/issues/29236
    https://github.com/Koenkk/zigbee2mqtt/issues/29138

--- src/devices/domraem.ts
+++ src/devices/domraem.ts
@@ -3,13 +3,31 @@ import type {DefinitionWithExtend} from "../lib/types";
 
 export const definitions: DefinitionWithExtend[] = [
     {
-        fingerprint: [
-            {modelID: "RGB", manufacturerName: "DOMRAEM"},
-            {modelID: "DIMMER", manufacturerName: "DOMRAEM"},
-        ],
+        fingerprint: [{modelID: "RGB", manufacturerName: "DOMRAEM"}],
         model: "DOM-Z-105P",
         vendor: "DOMRAEM",
         description: "LED controller",
         extend: [m.light({color: {modes: ["hs"], enhancedHue: false}})],
+    },
+    {
+        fingerprint: [{modelID: "RGBWC", manufacturerName: "DOMRAEM"}],
+        model: "DOM-Z-105P",
+        vendor: "DOMRAEM",
+        description: "LED controller 5 in 1 (RGBW)",
+        extend: [m.light({colorTemp: {range: [158, 495]}, color: {modes: ["xy", "hs"], enhancedHue: true}})],
+    },
+    {
+        fingerprint: [{modelID: "WW/CW", manufacturerName: "DOMRAEM"}],
+        model: "DOM-Z-105P",
+        vendor: "DOMRAEM",
+        description: "LED controller 5 in 1 (WW/CW)",
+        extend: [m.light({colorTemp: {range: [158, 500]}})],
+    },
+    {
+        fingerprint: [{modelID: "DIMMER", manufacturerName: "DOMRAEM"}],
+        model: "DOM-Z-105P",
+        vendor: "DOMRAEM",
+        description: "LED controller 5 in 1 (DIMMER)",
+        extend: [m.light()],
     },
 ];
