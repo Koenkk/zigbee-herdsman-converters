f6f4c94d4aff2f4e7c73af90a7075321a6f8b672
Koen Kanters <koenkanters94@gmail.com>
2025-11-25 20:17:58 +0100

    Fix description and zoneType for LDSENK06 siren

--- src/devices/adeo.ts
+++ src/devices/adeo.ts
@@ -41,8 +41,8 @@ export const definitions: DefinitionWithExtend[] = [
         zigbeeModel: ["LDSENK06"],
         model: "LDSENK06",
         vendor: "ADEO",
-        description: "ENKI LEXMAN Indoor Siren 85db",
-        extend: [m.iasZoneAlarm({zoneType: "generic", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]}), m.iasWarning()],
+        description: "ENKI LEXMAN indoor siren 85db",
+        extend: [m.iasZoneAlarm({zoneType: "alarm", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]}), m.iasWarning()],
     },
     {
         zigbeeModel: ["LDSENK08"],
