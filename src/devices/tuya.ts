95874b1162bb0dbbb5b9c56a64155ff7c06bc050
muzo178 <muzo178@gmail.com>
2025-12-02 11:44:00 +0300

    Change onEvent to use setLocalTime for _TZE200_sfqyhvpv

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -21737,7 +21737,7 @@ export const definitions: DefinitionWithExtend[] = [
         description: "Tubular shade motor",
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
-        onEvent: tuya.onEventSetTime,
+        onEvent: tuya.onEventSetLocalTime,
         configure: tuya.configureMagicPacket,
         exposes: [
             e.cover_position().setAccess("position", ea.STATE_SET),
