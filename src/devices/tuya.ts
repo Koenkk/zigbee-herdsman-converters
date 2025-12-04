6b9ab375a14b1e6874ca8f9496a2eb29cfc70e36
Koen Kanters <koenkanters94@gmail.com>
2025-12-04 21:29:07 +0100

    fix(detect): Detect `_TZ3000_bgsigers` as Tuya ZTH05_1 https://github.com/Koenkk/zigbee2mqtt/issues/30003

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -4430,6 +4430,7 @@ export const definitions: DefinitionWithExtend[] = [
             tuya.whitelabel("Tuya", "HS09", "Hanging temperature humidity sensor", ["_TZ3000_1twfmkcc"]),
             tuya.whitelabel("Nedis", "ZBSC10WT", "Temperature and humidity sensor", ["_TZ3000_fie1dpkm"]),
             tuya.whitelabel("Tuya", "TH09Z", "Temperature and humidity sensor", ["_TZ3000_isw9u95y", "_TZ3000_yupc0pb7"]),
+            tuya.whitelabel("Tuya", "ZTH05_1", "Temperature and humidity sensor", ["_TZ3000_bgsigers"]),
         ],
     },
     {
