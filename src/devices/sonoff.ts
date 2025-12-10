46ffb8f6c6f30a2f795ff34488d6dc4e089311a7
Oliver Cervera <olivercervera@yahoo.it>
2025-12-10 14:20:32 +0100

    Update src/devices/sonoff.ts
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/sonoff.ts
+++ src/devices/sonoff.ts
@@ -1402,7 +1402,7 @@ export const definitions: DefinitionWithExtend[] = [
                 const bindClusters = ["msTemperatureMeasurement", "msRelativeHumidity", "genPowerCfg"];
                 await reporting.bind(endpoint, coordinatorEndpoint, bindClusters);
                 await reporting.temperature(endpoint, {min: 30, max: 3600, change: 20});
-                await reporting.humidity(endpoint, {min: 30, max: 3600, change: 100});
+                await reporting.humidity(endpoint, {min: 30, max: constants.repInterval.HOUR, change: 100});
                 await reporting.batteryVoltage(endpoint, {min: 3600, max: 65000});
                 await reporting.batteryPercentageRemaining(endpoint, {min: 3600, max: 65000});
             } catch (e) {
