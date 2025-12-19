6bcbdd702940b783e696258284dc1142387d1776
dtswk <100445670+dtswk@users.noreply.github.com>
2025-12-19 17:20:12 +1100

    Add support for A-OK AM25 Blind motor
    My first attempt at adding to Zigbee2MQTT so hopefully ok.
    There were two other AM25 devices previously however it appears the battery has changed to a new ID 13.   When I made this I found the default behaviour open and closed was reversed and couldn't work out how to flip that so ended up adding in the option via     options: [exposes.options.invert_cover()].

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -3971,6 +3971,52 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [m.battery(), m.windowCovering({controls: ["lift"]})],
         whiteLabel: [tuya.whitelabel("Yookee", "D10110_1", "Smart blind", ["_TZE200_9caxna4s"])],
     },
+        fingerprint: tuya.fingerprint("TS0301", [
+        "_TZE210_m6lwazh9",
+    ]),
+    model: 'TS0301_a_ok',
+    vendor: 'Tuya',
+    description: 'A-OK AM25 Blind motor',
+    options: [exposes.options.invert_cover()],
+    extend: [
+        tuya.modernExtend.tuyaBase({
+            dp: true,
+            // Enable this line in case the device has a clock, if time is incorrect with
+            // `1970`, try with `2000`.
+            // timeStart: "1970",
+        }),
+    ],
+    exposes: [
+        e.battery(),
+        e.cover_position().setAccess("position", ea.STATE_SET),
+        e.enum("reverse_direction", ea.STATE_SET, ["forward", "back"]).withDescription("Reverse the motor direction"),
+    ],
+    meta: {
+        // All datapoints go in here
+        tuyaDatapoints: [
+            [
+                1,
+                "state",
+                tuya.valueConverterBasic.lookup({
+                    OPEN: tuya.enum(0),
+                    STOP: tuya.enum(1),
+                    CLOSE: tuya.enum(2),
+                }),
+            ],
+            [2, "position", tuya.valueConverter.coverPosition],
+            [3, "position", tuya.valueConverter.coverPosition],
+            [
+                5,
+                "reverse_direction",
+                tuya.valueConverterBasic.lookup({
+                    forward: tuya.enum(0),
+                    back: tuya.enum(1),
+                }),
+            ],
+            [12, "motor_fault", tuya.valueConverter.trueFalse1],
+            [13, "battery", tuya.valueConverter.raw],
+        ],
+    },
     {
         fingerprint: tuya.fingerprint("TS0601", [
             "_TZE200_aqnazj70",
