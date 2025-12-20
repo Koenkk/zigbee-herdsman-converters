fb7a0e51095671f93b830942c975ac2640215719
Joscha Arenz <joscha@arenz.co>
2025-12-20 11:52:46 +0100

    Update tuya.ts: Add Support for Temperature Sensor with external probe
    Add support for Temperature sensor with external probe as sold by aliexpress by unknown vendor:
    
    https://de.aliexpress.com/item/1005010178257258.html
    
    _TZE284_8se38w3c
    
    Addresses:
    https://github.com/Koenkk/zigbee2mqtt/issues/30230

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -22401,4 +22401,26 @@ Ensure all 12 segments are defined and separated by spaces.`,
             ],
         },
     },
+    {
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE284_8se38w3c'}],
+        model: 'TZ-ZT01_GA4',
+        vendor: 'Tuya',
+        description: 'Temperature & Humidity Sensor with external Probe',
+        extend: [tuya.modernExtend.tuyaBase({dp: true})],
+        exposes: [
+	        e.numeric("temperature_Env", ea.STATE).withUnit("°C").withDescription("Environment temperature"),
+		    e.numeric("temperature_Probe", ea.STATE).withUnit("°C").withDescription("Probe temperature"),
+            e.humidity().withDescription("Environment humidity"),
+            tuya.exposes.batteryState()
+        ],
+        meta: {
+            tuyaDatapoints: [
+                [1, "temperature_Env", tuya.valueConverter.divideBy10],
+                [2, "humidity", tuya.valueConverter.raw],
+                [3, 'battery_state', tuya.valueConverter.batteryState],
+				[38,"temperature_Probe", tuya.valueConverter.divideBy10]
+            ],
+        },
+    },
+    
 ];
