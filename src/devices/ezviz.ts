b303ee1beef40b0884fefecdd710eee714d06dfd
Koen Kanters <koenkanters94@gmail.com>
2025-12-22 19:43:05 +0100

    Update ezviz.ts

--- src/devices/ezviz.ts
+++ src/devices/ezviz.ts
@@ -6,11 +6,10 @@ export const definitions: DefinitionWithExtend[] = [
         zigbeeModel: ["CS-T2C-A0-BG"],
         model: "CS-T2C",
         vendor: "EZVIZ",
-        description: "Open/Close Sensor",
+        description: "Open/close sensor",
         extend: [
-            m.deviceEndpoints({endpoints: {"0": 0, "1": 1}}),
             m.battery(),
-            m.iasZoneAlarm({zoneType: "generic", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]}),
+            m.iasZoneAlarm({zoneType: "contact", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]}),
         ],
     },
 ];
