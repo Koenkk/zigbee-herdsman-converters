9ef8b112271626fd90ceeba7142156718887660d
urlu75 <urlu75@gmail.com>
2025-12-26 15:41:49 +0100

    Update sunricher.ts

--- src/devices/sunricher.ts
+++ src/devices/sunricher.ts
@@ -2469,4 +2469,11 @@ export const definitions: DefinitionWithExtend[] = [
             await endpoint.read("hvacThermostat", [0x1008], options);
         },
     },
+    {
+       zigbeeModel: ['ZGRC-KEY-047'],
+       model: 'ZGRC-KEY-047',
+       vendor: 'Sunricher',
+       description: 'Automatically generated definition',
+       extend: [m.deviceEndpoints({"endpoints":{"1":1,"2":2,"3":3,"4":4}}), m.battery(), m.commandsOnOff({"endpointNames":["1","2","3","4"]}), m.commandsLevelCtrl({"endpointNames":["1","2","3","4"]}), m.commandsColorCtrl({"endpointNames":["1","2","3","4"]})],
+    },    
 ];
