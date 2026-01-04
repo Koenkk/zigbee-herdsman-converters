665a4fd5711a0ae2c8b9781bf3643851636e85bb
jintj <jintj@3reality.com>
2026-01-04 13:43:53 +0800

    update

--- src/devices/third_reality.ts
+++ src/devices/third_reality.ts
@@ -1032,12 +1032,12 @@ export const definitions: DefinitionWithExtend[] = [
         vendor: "Third Reality",
         description: "Smart Air Pressure Sensor",
         extend: [
-            m.battery(), 
+            m.battery(),
             m.pressure(),
-            m.pressure({attribute: {ID: 0xff01, type: 0x23}, name: "Pressure", unit: "Pa", scale: 1 }),
+            m.pressure({attribute: {ID: 0xff01, type: 0x23}, name: "Pressure", unit: "Pa", scale: 1}),
             m.commandsOnOff(),
-            m.deviceAddCustomCluster('3rAirsensorSpecialCluster', {
-                ID: 0xFF01,
+            m.deviceAddCustomCluster("3rAirsensorSpecialCluster", {
+                ID: 0xff01,
                 manufacturerCode: 0x1407,
                 attributes: {
                     sendCommandUpThreshold: {ID: 0x0040, type: 0x21},
