6bdf4a7f3903bf4cb1c820458995360a7b3694c6
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2026-01-05 23:09:46 +0100

    Add: Buckram definition

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -3572,7 +3572,7 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
     },
     {
-        zigbeeModel: ["5047430P6", "929003048201_01"],
+        zigbeeModel: ["5047430P6"],
         model: "5047430P6",
         vendor: "Philips",
         description: "Hue White ambiance Buckram quadruple spotlight",
@@ -4102,10 +4102,17 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
     },
     {
+        zigbeeModel: ["929003048201_01", "929003048201_02", "929003048201_03", "929003048201_04"],
+        model: "929003048201",
+        vendor: "Philips",
+        description: "Hue white ambiance Buckram quadruple spotlight with Bluetooth (White)",
+        extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
+    },
+    {
         zigbeeModel: ["929003048301_01", "929003048301_02", "929003048301_03", "929003048301_04"],
         model: "8719514339163",
         vendor: "Philips",
-        description: "Hue white ambiance Buckram quadruple spotlight with Bluetooth",
+        description: "Hue white ambiance Buckram quadruple spotlight with Bluetooth (Black)",
         extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
     },
     {
