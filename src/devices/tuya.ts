410c084e61cf90794f4dac8c2b2e7ce9b11e1730
curtash <stuff@curtash.co.uk>
2026-01-06 20:37:32 +0000

    fix(detect): Detect `_TZE200_dcnsggvz` as Tuya TS0601_dimmer_5 (#11217)

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -4017,7 +4017,7 @@ export const definitions: DefinitionWithExtend[] = [
         ],
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_dcnsggvz", "_TZE200_dcnsggv"]),
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_dcnsggvz", "_TZE200_dcnsggv", "_TZE200_dcnsggvz"]),
         model: "TS0601_dimmer_5",
         vendor: "Tuya",
         description: "1 gang smart dimmer module",
