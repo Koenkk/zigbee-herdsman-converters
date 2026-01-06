f09f9167f6a4bbaf8128c8421b0aae3852f63e7f
michot312 <122319777+michot312@users.noreply.github.com>
2026-01-06 19:05:38 +0100

    Update unknown_0x54ef.ts

--- src/devices/unknown_0x54ef.ts
+++ src/devices/unknown_0x54ef.ts
@@ -1,7 +1,9 @@
-export default {
+export default [
+  {
     zigbeeModel: [""],
     model: "",
     vendor: "",
     description: "Automatically generated definition",
     extend: [],
-};
+  },
+];
