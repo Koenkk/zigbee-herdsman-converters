3e927e145aad51f6e3127ff42db32099cdb87238
Benoit29200 <32896883+Benoit29200@users.noreply.github.com>
2026-01-13 22:18:56 +0100

    Update ezviz.ts : add T55 thermostatic valve

--- src/devices/ezviz.ts
+++ src/devices/ezviz.ts
@@ -9,4 +9,11 @@ export const definitions: DefinitionWithExtend[] = [
         description: "Open/close sensor",
         extend: [m.battery(), m.iasZoneAlarm({zoneType: "contact", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]})],
     },
+    {
+        zigbeeModel: ['CS-T55-R100-G'],
+        model: 'CS-T55-R100-G',
+        vendor: 'EZVIZ',
+        description: 'EZVIZ T55 thermostatic valve',
+        extend: [m.battery(), m.iasZoneAlarm({"zoneType":"generic","zoneAttributes":["alarm_1","alarm_2","tamper","battery_low"]}), m.onOff({"powerOnBehavior":false}), m.temperature()],
+    }
 ];
