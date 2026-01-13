f98acb324483e1aeb844b7a46a420c3fe53f3b80
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2026-01-13 16:33:39 +0100

    Fix indentation in ls.ts extend array

--- src/devices/ls.ts
+++ src/devices/ls.ts
@@ -108,12 +108,13 @@ export const definitions: DefinitionWithExtend[] = [
         model: "756200643",
         vendor: "L&S Lighting",
         description: "Zigbee remote",
-            extend: [
-        m.battery(),
-        lsModernExtend.groupIdExpose(),
-        lsModernExtend.commandsOnOffDouble(),
-        m.commandsOnOff({commands: ["on", "off"]}),
-        m.commandsLevelCtrl({commands: ["brightness_step_up", "brightness_step_down", "brightness_stop"]}),
-        m.commandsColorCtrl({commands: ["color_temperature_step_up", "color_temperature_step_down", "color_temperature_move_stop"]}),
+        extend: [
+            m.battery(),
+            lsModernExtend.groupIdExpose(),
+            lsModernExtend.commandsOnOffDouble(),
+            m.commandsOnOff({commands: ["on", "off"]}),
+            m.commandsLevelCtrl({commands: ["brightness_step_up", "brightness_step_down", "brightness_stop"]}),
+            m.commandsColorCtrl({commands: ["color_temperature_step_up", "color_temperature_step_down", "color_temperature_move_stop"]}),
+        ],
     },
 ];
