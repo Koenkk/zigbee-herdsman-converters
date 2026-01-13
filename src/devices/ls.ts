50d88bc0416287b377cddf92176ce1348da41b85
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2026-01-13 18:51:51 +0100

    Add import for fromZigbee converters

--- src/devices/ls.ts
+++ src/devices/ls.ts
@@ -1,4 +1,5 @@
 import * as exposes from "../lib/exposes";
+import * as fz from "../converters/fromZigbee";
 import * as m from "../lib/modernExtend";
 import {setupConfigureForBinding} from "../lib/modernExtend";
 import type {DefinitionWithExtend} from "../lib/types";
