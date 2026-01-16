f511da14c4fb1a87c17ba93db78af11fe7aad77d
Benoit29200 <32896883+Benoit29200@users.noreply.github.com>
2026-01-16 22:17:47 +0100

    Update description EZVIZ T55-R100-G

--- src/devices/ezviz.ts
+++ src/devices/ezviz.ts
@@ -13,7 +13,7 @@ export const definitions: DefinitionWithExtend[] = [
         zigbeeModel: ["CS-T55-R100-G"],
         model: "CS-T55-R100-G",
         vendor: "EZVIZ",
-        description: "EZVIZ T55 thermostatic valve",
+        description: "T55 thermostatic valve",
         extend: [
             m.battery(),
             m.iasZoneAlarm({zoneType: "generic", zoneAttributes: ["alarm_1", "alarm_2", "tamper", "battery_low"]}),
