5d81e16b8c2974e6c4e1e845e6d985eceb4f4b9e
chris-1243 <142902565+chris-1243@users.noreply.github.com>
2026-01-27 09:21:28 +0100

    Add: TIS Control
    From issue:
     - Koenkk/zigbee2mqtt/issues/30795

---
+++ src/devices/tis_control.ts
@@ -0,0 +1,12 @@
+import * as m from "../lib/modernExtend";
+import type {DefinitionWithExtend} from "../lib/types";
+
+export const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ["ED6XX"],
+        model: "BEE PIR 1",
+        vendor: "TIS Control",
+        description: "PIR Sensor",
+        extend: [m.battery(), m.iasZoneAlarm({zoneType: "generic", zoneAttributes: ["alarm_1", "tamper", "battery_low"]})],
+    },
+];
