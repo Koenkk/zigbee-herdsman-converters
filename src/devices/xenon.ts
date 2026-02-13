b1ca782933400be36bd6dddec665aa0714e9e385
fatihaslan1990 <fatihaslan1990@gmail.com>
2026-02-13 16:43:54 +0300

    Refactor definitions export and improve formatting

--- src/devices/xenon.ts
+++ src/devices/xenon.ts
@@ -5,7 +5,7 @@ import type {Definition} from "../lib/types";
 const e = exposes.presets;
 const ea = exposes.access;
 
-const definitions: Definition[] = [
+export const definitions: Definition[] = [
     {
         fingerprint: tuya.fingerprint("TS0601", ["_TZE284_hbjwgkdh"]),
         model: "X7726",
@@ -13,15 +13,31 @@ const definitions: Definition[] = [
         description: "Xenon Smart Zigbee curtain motor",
         fromZigbee: [tuya.fz.datapoints],
         toZigbee: [tuya.tz.datapoints],
-        onEvent: tuya.onEventSetTime,
         configure: tuya.configureMagicPacket,
-        exposes: [e.cover_position().setAccess("position", ea.STATE_SET), e.enum("calibration", ea.STATE_SET, ["start", "finish"]), e.temperature()],
+
+        exposes: [
+            e.cover_position().setAccess("position", ea.STATE_SET),
+            exposes.enum("calibration", ea.STATE_SET, ["start", "finish"]),
+            e.temperature(),
+        ],
+
         meta: {
             tuyaDatapoints: [
-                [1, "state", tuya.valueConverterBasic.lookup({OPEN: tuya.enum(0), STOP: tuya.enum(1), CLOSE: tuya.enum(2)})],
-                [2, "position", tuya.valueConverter.coverPositionInverted],
-                [3, "position", tuya.valueConverter.coverPositionInverted],
-                [102, "calibration", tuya.valueConverterBasic.lookup({start: tuya.enum(0), finish: tuya.enum(1)})],
+                [1, "state", tuya.valueConverterBasic.lookup({
+                    OPEN: tuya.enum(0),
+                    STOP: tuya.enum(1),
+                    CLOSE: tuya.enum(2),
+                })],
+
+                // Koenkk suggestion: use standard coverPosition converter
+                [2, "position", tuya.valueConverter.coverPosition],
+                [3, "position", tuya.valueConverter.coverPosition],
+
+                [102, "calibration", tuya.valueConverterBasic.lookup({
+                    start: tuya.enum(0),
+                    finish: tuya.enum(1),
+                })],
+
                 [103, "temperature", tuya.valueConverter.raw],
             ],
         },
