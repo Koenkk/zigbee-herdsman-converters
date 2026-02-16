e9ea0ad58e86ffe71ee40362e2abba8702f978d5
Mike <25049399+MikeAndHAL9000@users.noreply.github.com>
2026-02-16 12:30:55 -0800

    Add Sonoma Style Fan Heater support in stelpro.ts
    Pretty much just copying one of the above and bending it to support the Sonoma Style version.

--- src/devices/stelpro.ts
+++ src/devices/stelpro.ts
@@ -287,6 +287,58 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
+        zigbeeModel: ['SonomaStyle'],
+        model: 'SonomaStyle',
+        vendor: 'Stelpro',
+        description: 'Sonoma Style Fan Heater',
+        extend: [],
+        meta: {},
+        fromZigbee: [
+            fz.stelpro_thermostat,
+            fz.hvac_user_interface
+        ],
+        toZigbee: [
+            tz.thermostat_local_temperature,
+            tz.thermostat_occupancy,
+            tz.thermostat_occupied_heating_setpoint,
+            tz.thermostat_temperature_display_mode,
+            tz.thermostat_keypad_lockout,
+            tz.thermostat_system_mode,
+            tz.thermostat_running_state,
+        ],
+        exposes: [
+                e.local_temperature(),
+                e.keypad_lockout(),
+                e
+                    .climate()
+                    .withSetpoint('occupied_heating_setpoint', 5, 30, 0.5)
+                    .withLocalTemperature()
+                    .withSystemMode(['off', 'auto', 'heat'])
+                    .withRunningState(['idle', 'heat'])
+                    .withPiHeatingDemand(),
+            ],
+            configure: async (device, coordinatorEndpoint) => {
+                const endpoint = device.getEndpoint(25);
+                const binds = ['genBasic', 'genIdentify', 'genGroups', 'hvacThermostat', 'hvacUserInterfaceCfg', 'msTemperatureMeasurement'];
+                await reporting.bind(endpoint, coordinatorEndpoint, binds);
+                // Those exact parameters (min/max/change) are required for reporting to work with Stelpro Ki
+                await reporting.thermostatTemperature(endpoint);
+                await reporting.thermostatOccupiedHeatingSetpoint(endpoint);
+                await reporting.thermostatSystemMode(endpoint);
+                await reporting.thermostatPIHeatingDemand(endpoint);
+                await reporting.thermostatKeypadLockMode(endpoint);
+                // cluster 0x0201 attribute 0x401c
+                await endpoint.configureReporting('hvacThermostat', [
+                    {
+                        attribute: 'StelproSystemMode',
+                        minimumReportInterval: constants.repInterval.MINUTE,
+                        maximumReportInterval: constants.repInterval.HOUR,
+                        reportableChange: 1,
+                    },
+              ]);
+         },
+    },
+    {
         zigbeeModel: ["SMT402AD"],
         model: "SMT402AD",
         vendor: "Stelpro",
