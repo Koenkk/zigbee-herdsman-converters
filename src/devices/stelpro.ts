86a4b32ecac4afee010665b983dc388a0f16c1ad
Koen Kanters <koenkanters94@gmail.com>
2026-02-17 21:03:52 +0100

    u

--- src/devices/stelpro.ts
+++ src/devices/stelpro.ts
@@ -85,11 +85,12 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        zigbeeModel: ["ST218"],
+        zigbeeModel: ["ST218", "SonomaStyle"],
         model: "ST218",
         vendor: "Stelpro",
         description: "Ki convector, line-voltage thermostat",
         fromZigbee: [fz.stelpro_thermostat, fz.hvac_user_interface],
+        whiteLabel: [{description: "Style Fan Heater", model: "SonomaStyle", fingerprint: [{modelID: "SonomaStyle"}]}],
         toZigbee: [
             tz.thermostat_local_temperature,
             tz.thermostat_occupancy,
@@ -270,55 +271,6 @@ export const definitions: DefinitionWithExtend[] = [
             await reporting.bind(endpoint, coordinatorEndpoint, binds);
 
             // Those exact parameters (min/max/change) are required for reporting to work with Stelpro SORB
-            await reporting.thermostatTemperature(endpoint);
-            await reporting.thermostatOccupiedHeatingSetpoint(endpoint);
-            await reporting.thermostatSystemMode(endpoint);
-            await reporting.thermostatPIHeatingDemand(endpoint);
-            await reporting.thermostatKeypadLockMode(endpoint);
-            // cluster 0x0201 attribute 0x401c
-            await endpoint.configureReporting("hvacThermostat", [
-                {
-                    attribute: "StelproSystemMode",
-                    minimumReportInterval: constants.repInterval.MINUTE,
-                    maximumReportInterval: constants.repInterval.HOUR,
-                    reportableChange: 1,
-                },
-            ]);
-        },
-    },
-    {
-        zigbeeModel: ["SonomaStyle"],
-        model: "SonomaStyle",
-        vendor: "Stelpro",
-        description: "Sonoma Style Fan Heater",
-        extend: [],
-        meta: {},
-        fromZigbee: [fz.stelpro_thermostat, fz.hvac_user_interface],
-        toZigbee: [
-            tz.thermostat_local_temperature,
-            tz.thermostat_occupancy,
-            tz.thermostat_occupied_heating_setpoint,
-            tz.thermostat_temperature_display_mode,
-            tz.thermostat_keypad_lockout,
-            tz.thermostat_system_mode,
-            tz.thermostat_running_state,
-        ],
-        exposes: [
-            e.local_temperature(),
-            e.keypad_lockout(),
-            e
-                .climate()
-                .withSetpoint("occupied_heating_setpoint", 5, 30, 0.5)
-                .withLocalTemperature()
-                .withSystemMode(["off", "auto", "heat"])
-                .withRunningState(["idle", "heat"])
-                .withPiHeatingDemand(),
-        ],
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(25);
-            const binds = ["genBasic", "genIdentify", "genGroups", "hvacThermostat", "hvacUserInterfaceCfg", "msTemperatureMeasurement"];
-            await reporting.bind(endpoint, coordinatorEndpoint, binds);
-            // Those exact parameters (min/max/change) are required for reporting to work with Stelpro Ki
             await reporting.thermostatTemperature(endpoint);
             await reporting.thermostatOccupiedHeatingSetpoint(endpoint);
             await reporting.thermostatSystemMode(endpoint);
