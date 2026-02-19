eb098d4255ffdef4e41faa80d6954934115e0d11
Koen Kanters <koenkanters94@gmail.com>
2026-02-19 20:31:13 +0100

    feat(add): TS0721 https://github.com/Koenkk/zigbee2mqtt/issues/31075

--- src/devices/zbeacon.ts
+++ src/devices/zbeacon.ts
@@ -9,4 +9,11 @@ export const definitions: DefinitionWithExtend[] = [
         description: "ZBeacon TH01 v2.0 temperature & humidity sensor",
         extend: [m.temperature(), m.humidity(), m.battery()],
     },
+    {
+        zigbeeModel: ["TS0721"],
+        model: "TS0721",
+        vendor: "Zbeacon",
+        description: "On/off switch",
+        extend: [m.onOff({powerOnBehavior: false})],
+    },
 ];
