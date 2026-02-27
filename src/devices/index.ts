7e66d48023d5b8af1865418b2c71e861b53b40f8
alblcl <caojuan@ezsmart.onaliyun.com>
2026-02-27 12:33:40 +0800

    move definitions to avatto.ts move converter to tuyaDatapoints

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -19,7 +19,6 @@ import {definitions as aubess} from "./aubess";
 import {definitions as auroraLighting} from "./aurora_lighting";
 import {definitions as automaton} from "./automaton";
 import {definitions as avatto} from "./avatto";
-import {definitions as avattoEz} from "./avatto_ez";
 import {definitions as awox} from "./awox";
 import {definitions as axis} from "./axis";
 import {definitions as bacchus} from "./bacchus";
@@ -387,7 +386,6 @@ const definitions: DefinitionWithExtend[] = [
     ...auroraLighting,
     ...automaton,
     ...avatto,
-    ...avattoEz,
     ...awox,
     ...axis,
     ...bacchus,
