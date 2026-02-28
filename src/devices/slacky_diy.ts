79395dbce2e58721c543487f26ea8ecb72e0bb93
Fedor Lizunkov <lfa1@yandex.ru>
2026-02-28 22:25:44 +0300

    feat(add): TS0044-z-SlD (#11621)

--- src/devices/slacky_diy.ts
+++ src/devices/slacky_diy.ts
@@ -98,6 +98,18 @@ const attrDoorDelayOff = 0xf004;
 const attrDoorOnCmdOff = 0xf005;
 const attrDoorOffCmdOff = 0xf006;
 
+const attrSceneId = 0xf000;
+const attrGroupId = 0xf001;
+
+interface SlackyDiyScene {
+    attributes: {
+        customSceneId: number;
+        customGroupId: number;
+    };
+    commands: never;
+    commandResponses: never;
+}
+
 interface SlackyDiyOnOffCfg {
     attributes: {
         customSwitchType: number;
@@ -621,6 +633,7 @@ const localToZigbeeThermostat = [
 
 interface LocalActionExtendArgs {
     localAction?: string[]; //("hold" | "single" | "double" | "triple" | "quadruple" | "quintuple" | "release")[];
+    bind?: boolean;
     reporting?: boolean;
     reportingConfig?: m.ReportingConfigWithoutAttribute;
     endpointNames?: string[];
@@ -629,6 +642,7 @@ interface LocalActionExtendArgs {
 function localActionExtend(args: LocalActionExtendArgs = {}): ModernExtend {
     const {
         localAction = ["hold", "single", "double", "triple", "quadruple", "quintuple", "release"],
+        bind = true,
         reporting = true,
         reportingConfig = {min: 10, max: 0, change: 1},
         endpointNames = undefined,
@@ -665,11 +679,13 @@ function localActionExtend(args: LocalActionExtendArgs = {}): ModernExtend {
         } satisfies Fz.Converter<"genMultistateInput", undefined, ["attributeReport", "readResponse"]>,
     ];
     const result: ModernExtend = {exposes, fromZigbee, isModernExtend: true};
+
     if (reporting)
         result.configure = [
             m.setupConfigureForBinding("genMultistateInput", "input", endpointNames),
             m.setupConfigureForReporting("genMultistateInput", "presentValue", {config: reportingConfig, access: ea.GET, endpointNames}),
         ];
+    else if (bind) result.configure = [m.setupConfigureForBinding("genMultistateInput", "input", endpointNames)];
 
     return result;
 }
@@ -3573,6 +3589,177 @@ export const definitions: DefinitionWithExtend[] = [
                 cluster: "genOnOffSwitchCfg",
                 attribute: "customOffCmdOff",
                 description: "Disable command 'Off'",
+            }),
+        ],
+        meta: {},
+        ota: true,
+    },
+    {
+        zigbeeModel: ["TS0044-z-SlD"],
+        model: "TS0044-z-SlD",
+        vendor: "Slacky-DIY",
+        description: "Tuya wireless switch with 4 buttons with custom firmware",
+        extend: [
+            m.deviceAddCustomCluster("genOnOffSwitchCfg", {
+                ID: 0x0007,
+                attributes: {
+                    customSwitchType: {
+                        ID: attrSwitchType,
+                        type: 0x30,
+                        write: true,
+                        max: 0xff,
+                    },
+                    customDeviceModelNumber: {
+                        ID: attrDeviceModelNumber,
+                        type: 0x30,
+                        write: true,
+                        max: 0xff,
+                    },
+                },
+                commands: {},
+                commandsResponse: {},
+            }),
+            m.deviceAddCustomCluster("genScenes", {
+                ID: 0x0005,
+                attributes: {
+                    customSceneId: {
+                        ID: attrSceneId,
+                        type: 0x20,
+                        write: true,
+                        max: 0xff,
+                    },
+                    customGroupId: {
+                        ID: attrGroupId,
+                        type: 0x21,
+                        write: true,
+                        max: 0xffff,
+                    },
+                },
+                commands: {},
+                commandsResponse: {},
+            }),
+            m.deviceEndpoints({endpoints: {"1": 1, "2": 2, "3": 3, "4": 4}}),
+            m.battery({
+                percentageReportingConfig: {min: 3600, max: 14400, change: 0},
+            }),
+            m.commandsOnOff({endpointNames: ["1", "2", "3", "4"]}),
+            localActionExtend({
+                endpointNames: ["1", "2", "3", "4"],
+                reporting: false,
+            }),
+            m.commandsLevelCtrl({endpointNames: ["1", "2", "3", "4"]}),
+            m.enumLookup({
+                name: "switch_actions",
+                endpointName: "1",
+                lookup: {off: 0, on: 1, toggle: 2},
+                cluster: "genOnOffSwitchCfg",
+                attribute: "switchActions",
+                description: "Actions switch",
+            }),
+            m.enumLookup({
+                name: "switch_actions",
+                endpointName: "2",
+                lookup: {off: 0, on: 1, toggle: 2},
+                cluster: "genOnOffSwitchCfg",
+                attribute: "switchActions",
+                description: "Actions switch",
+            }),
+            m.enumLookup({
+                name: "switch_actions",
+                endpointName: "3",
+                lookup: {off: 0, on: 1, toggle: 2},
+                cluster: "genOnOffSwitchCfg",
+                attribute: "switchActions",
+                description: "Actions switch",
+            }),
+            m.enumLookup({
+                name: "switch_actions",
+                endpointName: "4",
+                lookup: {off: 0, on: 1, toggle: 2},
+                cluster: "genOnOffSwitchCfg",
+                attribute: "switchActions",
+                description: "Actions switch",
+            }),
+            m.enumLookup<"genOnOffSwitchCfg", SlackyDiyOnOffCfg>({
+                name: "switch_type",
+                endpointName: "1",
+                lookup: {
+                    toggle: 0,
+                    momentary: 1,
+                    multifunction: 2,
+                    brightness_level_up: 3,
+                    brightness_level_down: 4,
+                    scene: 5,
+                },
+                cluster: "genOnOffSwitchCfg",
+                attribute: "customSwitchType",
+                description: "Switch type",
+            }),
+            m.enumLookup<"genOnOffSwitchCfg", SlackyDiyOnOffCfg>({
+                name: "switch_type",
+                endpointName: "2",
+                lookup: {
+                    toggle: 0,
+                    momentary: 1,
+                    multifunction: 2,
+                    brightness_level_up: 3,
+                    brightness_level_down: 4,
+                    scene: 5,
+                },
+                cluster: "genOnOffSwitchCfg",
+                attribute: "customSwitchType",
+                description: "Switch type",
+            }),
+            m.enumLookup<"genOnOffSwitchCfg", SlackyDiyOnOffCfg>({
+                name: "switch_type",
+                endpointName: "3",
+                lookup: {
+                    toggle: 0,
+                    momentary: 1,
+                    multifunction: 2,
+                    brightness_level_up: 3,
+                    brightness_level_down: 4,
+                    scene: 5,
+                },
+                cluster: "genOnOffSwitchCfg",
+                attribute: "customSwitchType",
+                description: "Switch type",
+            }),
+            m.enumLookup<"genOnOffSwitchCfg", SlackyDiyOnOffCfg>({
+                name: "switch_type",
+                endpointName: "4",
+                lookup: {
+                    toggle: 0,
+                    momentary: 1,
+                    multifunction: 2,
+                    brightness_level_up: 3,
+                    brightness_level_down: 4,
+                    scene: 5,
+                },
+                cluster: "genOnOffSwitchCfg",
+                attribute: "customSwitchType",
+                description: "Switch type",
+            }),
+            m.commandsScenes({endpointNames: ["1", "2", "3", "4"]}),
+            m.numeric<"genScenes", SlackyDiyScene>({
+                name: "scene_id",
+                endpointNames: ["1", "2", "3", "4"],
+                access: "ALL",
+                cluster: "genScenes",
+                attribute: "customSceneId",
+                valueMin: 0,
+                valueMax: 255,
+                description: "Scene ID",
+            }),
+            m.numeric<"genScenes", SlackyDiyScene>({
+                name: "group_id",
+                endpointNames: ["1", "2", "3", "4"],
+                access: "ALL",
+                cluster: "genScenes",
+                attribute: "customGroupId",
+                valueMin: 0,
+                valueMax: 65527,
+                description: "Group ID for scenes",
             }),
         ],
         meta: {},
