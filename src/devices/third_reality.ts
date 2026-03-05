253b410ec2495fbaf24e5e95995ebd9de64b78f4
jintj <jintj@3reality.com>
2026-03-05 08:51:35 +0800

    update

--- src/devices/third_reality.ts
+++ src/devices/third_reality.ts
@@ -615,9 +615,9 @@ export const definitions: DefinitionWithExtend[] = [
                 ID: 0xff01,
                 manufacturerCode: 0x1407,
                 attributes: {
-                    celsiusDegreeCalibration: {ID: 0x0031, type: 0x29, write: true, min: -32768},
-                    humidityCalibration: {ID: 0x0032, type: 0x29, write: true, min: -32768},
-                    fahrenheitDegreeCalibration: {ID: 0x0033, type: 0x29, write: true, min: -32768},
+                    celsiusDegreeCalibration: {ID: 0x0031, type: Zcl.DataType.INT16, write: true, min: -32768},
+                    humidityCalibration: {ID: 0x0032, type: Zcl.DataType.INT16, write: true, min: -32768},
+                    fahrenheitDegreeCalibration: {ID: 0x0033, type: Zcl.DataType.INT16, write: true, min: -32768},
                 },
                 commands: {},
                 commandsResponse: {},
