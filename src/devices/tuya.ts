8985ee4d1272858ccee7f182838c47af0f606e03
Koen Kanters <koenkanters94@gmail.com>
2026-03-09 06:22:42 +0100

    fix(detect): Detect `_TZE284_3regm3h6` as Tuya _TZE204_3regm3h6

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -22480,7 +22480,7 @@ export const definitions: DefinitionWithExtend[] = [
         ],
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_3regm3h6", "_TZE204_0hcjew5p", "_TZE204_6vwfjkcj"]),
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE204_3regm3h6", "_TZE204_0hcjew5p", "_TZE204_6vwfjkcj", "_TZE284_3regm3h6"]),
         model: "_TZE204_3regm3h6",
         vendor: "Tuya",
         description: "Smart thermostat for electric radiator with pilot wire",
