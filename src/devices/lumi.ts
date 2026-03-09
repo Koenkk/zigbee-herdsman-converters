62235c016b7aa5c8d93bbed064e9da5e048a0c14
Kamil Pustelnik <147668889+kpustelnik@users.noreply.github.com>
2026-03-09 20:42:30 +0100

    Adjust the max value of humidity reporting treshold for Aqara FP300

--- src/devices/lumi.ts
+++ src/devices/lumi.ts
@@ -5373,7 +5373,7 @@ export const definitions: DefinitionWithExtend[] = [
             m.numeric({
                 name: "humidity_reporting_threshold",
                 valueMin: 2, // Min: 2%
-                valueMax: 10, // Max: 10%
+                valueMax: 15, // Max: 15%
                 valueStep: 0.5, // Step: 0,5%
                 scale: 100,
                 unit: "%",
