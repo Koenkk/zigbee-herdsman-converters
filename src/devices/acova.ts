771d2aab77643b1b1a7632524ae84138d01b5c2b
Ken-dono <63581309+Ken-dono@users.noreply.github.com>
2026-03-12 09:00:31 +0100

    Add modernExtend import to acova.ts

--- src/devices/acova.ts
+++ src/devices/acova.ts
@@ -2,6 +2,7 @@ import * as fz from "../converters/fromZigbee";
 import * as tz from "../converters/toZigbee";
 import * as exposes from "../lib/exposes";
 import * as reporting from "../lib/reporting";
+import * as m from "../lib/modernExtend";
 import type {DefinitionWithExtend} from "../lib/types";
 
 const e = exposes.presets;
