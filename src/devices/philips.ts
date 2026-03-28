631022a17a896e5ee80f386ac56a2cded22f97cb
Brian <suspended@users.noreply.github.com>
2026-03-28 17:54:54 +0800

    feat(add): 4505848C5 (#11817)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -1746,6 +1746,13 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
     },
     {
+        zigbeeModel: ["LTT002"],
+        model: "4505848C5",
+        vendor: "Philips",
+        description: "Hue white ambiance Within LED table light ",
+        extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
+    },
+    {
         zigbeeModel: ["3417831P6", "929003056101"],
         model: "3417831P6",
         vendor: "Philips",
