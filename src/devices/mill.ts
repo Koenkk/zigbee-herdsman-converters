c407a9331155f4ff0e5b0c0982adde44d4ac40eb
CodingMonkey <soloiskrem@pm.me>
2026-04-05 23:54:11 +0200

    Refactor Mill device definitions to use modernExtend

--- src/devices/mill.ts
+++ src/devices/mill.ts
@@ -1,9 +1,5 @@
-import * as fz from "../converters/fromZigbee";
-import * as tz from "../converters/toZigbee";
-import * as exposes from "../lib/exposes";
+import * as m from "../lib/modernExtend";
 import type {DefinitionWithExtend} from "../lib/types";
-
-const e = exposes.presets;
 
 export const definitions: DefinitionWithExtend[] = [
     {
@@ -11,18 +7,37 @@ export const definitions: DefinitionWithExtend[] = [
         model: "Mill-gen-4",
         vendor: "Mill",
         description: "WiFi heating panel gen4",
-        fromZigbee: [fz.thermostat, fz.identify],
-        toZigbee: [tz.thermostat_occupied_heating_setpoint, tz.thermostat_system_mode, tz.identify],
-        exposes: [e.climate().withSetpoint("occupied_heating_setpoint", 5, 35, 0.5).withLocalTemperature().withSystemMode(["off", "heat"])],
+        extend: [
+            m.identify(),
+            m.thermostat({
+                setpoints: {
+                    values: {
+                        occupiedHeatingSetpoint: {min: 5, max: 35, step: 0.5},
+                    },
+                },
+                systemMode: {
+                    values: ["off", "heat"],
+                },
+            }),
+        ],
     },
     {
         fingerprint: [{manufacturerName: "Mill International\u0000Threa"}, {manufacturerName: "Mill InternationalThrea"}],
         zigbeeModel: ["Mill International\u0000Threa", "Mill InternationalThrea"],
-        model: "mill_smart_floor_thermostat",
+        model: "MFTWIFI",
         vendor: "Mill",
         description: "Mill Smart Floor Thermostat WiFi & Zigbee",
-        fromZigbee: [fz.thermostat],
-        toZigbee: [tz.thermostat_occupied_heating_setpoint, tz.thermostat_system_mode],
-        exposes: [e.climate().withSetpoint("occupied_heating_setpoint", 5, 35, 0.5).withLocalTemperature().withSystemMode(["off", "heat"])],
+        extend: [
+            m.thermostat({
+                setpoints: {
+                    values: {
+                        occupiedHeatingSetpoint: {min: 5, max: 35, step: 0.5},
+                    },
+                },
+                systemMode: {
+                    values: ["off", "heat"],
+                },
+            }),
+        ],
     },
 ];
