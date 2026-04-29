972ffd8086629b8794df4d8f8dcd891d77c12597
rubempoli <66323535+rubempoli@users.noreply.github.com>
2026-04-28 22:17:44 -0300

    Merge Nova Digital TS0002 into TS0002_basic
    Merge Nova Digital TS0002_1_switch_1_socket_tz3210 into TS0002_basic

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -6551,6 +6551,7 @@ export const definitions: DefinitionWithExtend[] = [
             "_TZ3000_fbjdkph9",
             "_TZ3000_zbfya6h0",
             "_TZ3000_hznzbl0x",
+            "_TZ3210_6smingw0",
         ]),
         model: "TS0002_basic",
         vendor: "Tuya",
@@ -6565,14 +6566,17 @@ export const definitions: DefinitionWithExtend[] = [
             tuya.whitelabel("Aubess", "TMZ02", "2 gang switch", ["_TZ3000_lmlsduws"]),
             tuya.whitelabel("RSH", "TS0002_basic_2", "2 gang switch", ["_TZ3000_zbfya6h0"]),
             tuya.whitelabel("EKAZA", "EKAC-T3092Z", "2 gang switch", ["_TZ3000_hznzbl0x"]),
+            tuya.whitelabel("Nova Digital", "TS0002_1_switch_1_socket_tz3210", "1 switch and 1 socket with backlight", ["_TZ3210_6smingw0"]),
         ],
         extend: [
             tuya.modernExtend.tuyaBase(),
             tuya.modernExtend.tuyaOnOff({
                 switchType: true,
                 endpoints: ["l1", "l2"],
-                powerOutageMemory: true,
-                onOffCountdown: (m) => m === "_TZ3000_hznzbl0x",
+                powerOutageMemory: (m) => m !== "_TZ3210_6smingw0",
+                onOffCountdown: (m) => m === "_TZ3000_hznzbl0x" || m === "_TZ3210_6smingw0",
+                indicatorMode: (m) => m === "_TZ3210_6smingw0",
+                backlightModeOffOn: (m) => m === "_TZ3210_6smingw0",
             }),
         ],
         endpoint: (device) => {
@@ -6632,28 +6636,7 @@ export const definitions: DefinitionWithExtend[] = [
         ],
     },
 
-    {
-        fingerprint: tuya.fingerprint("TS0002", ["_TZ3210_6smingw0"]),
-        model: "TS0002_1_switch_1_socket_tz3210",
-        vendor: "Nova Digital",
-        description: "1 switch and 1 socket with backlight",
-        extend: [
-            tuya.modernExtend.tuyaBase(),
-            tuya.modernExtend.tuyaMagicPacket(),
-            m.deviceEndpoints({endpoints: {l1: 1, l2: 2}}),
-            tuya.modernExtend.tuyaOnOff({
-                endpoints: ["l1", "l2"],
-                onOffCountdown: true,
-                indicatorMode: true,
-                backlightModeOffOn: true,
-            }),
-        ],
-        configure: async (device, coordinatorEndpoint) => {
-            await tuya.configureMagicPacket(device, coordinatorEndpoint);
-            await reporting.bind(device.getEndpoint(1), coordinatorEndpoint, ["genOnOff"]);
-            await reporting.bind(device.getEndpoint(2), coordinatorEndpoint, ["genOnOff"]);
-        },
-    },
+
 
     {
         // TS0002 2 gang switch module with all available features. This is the default for TS0002 devices.
