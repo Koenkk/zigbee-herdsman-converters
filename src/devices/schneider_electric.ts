40001fcb2ee46e8091fad1605cd1e74e551121e3
Svhelge <svein.helge@hoydal.net>
2026-05-12 21:21:24 +0200

    fix reporting

--- src/devices/schneider_electric.ts
+++ src/devices/schneider_electric.ts
@@ -2071,29 +2071,11 @@ export const definitions: DefinitionWithExtend[] = [
             m.onOff(),
             m.electricityMeter({
                 power: {min: 5, change: 1},
-                energy: {change: 1},
+                energy: {min: "1_MINUTE", change: 1},
                 voltage: false,
                 current: false,
             }),
         ],
-        // fromZigbee: [fz.on_off, fz.electrical_measurement, fz.metering, fz.power_on_behavior],
-        // toZigbee: [tz.on_off, tz.power_on_behavior, tz.electrical_measurement_power],
-        // exposes: [
-        //     e.switch(),
-        //     e.power().withAccess(ea.STATE_GET),
-        //     e.energy(),
-        //     e.enum("power_on_behavior", ea.ALL, ["off", "previous", "on"]).withDescription("Controls the behaviour when the device is powered on"),
-        // ],
-        // configure: async (device, coordinatorEndpoint) => {
-        //     const endpoint = device.getEndpoint(1);
-        //     await reporting.bind(endpoint, coordinatorEndpoint, ["genOnOff", "haElectricalMeasurement", "seMetering"]);
-        //     await reporting.onOff(endpoint);
-        //     // only activePower seems to be support, although compliance document states otherwise
-        //     await endpoint.read("haElectricalMeasurement", ["acPowerMultiplier", "acPowerDivisor"]);
-        //     await reporting.activePower(endpoint);
-        //     await reporting.readMeteringMultiplierDivisor(endpoint);
-        //     await reporting.currentSummDelivered(endpoint, {min: 60, change: 1});
-        // },
     },
     {
         zigbeeModel: ["U201DST600ZB"],
