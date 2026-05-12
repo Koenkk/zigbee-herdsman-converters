733df8a3483620fffaef0b1314f80227363ab577
Björn Siebler <28015256+Bjoern3D@users.noreply.github.com>
2026-05-12 19:54:35 +0200

    Update fingerprint for Megaman device definition

--- src/devices/megaman.ts
+++ src/devices/megaman.ts
@@ -3,7 +3,7 @@ import type {DefinitionWithExtend} from "../lib/types";
 
 const definitions: DefinitionWithExtend[] = [
     {
-        zigbeeModel: ["ZLL-DimmableLight"],
+        fingerprint: [{modelID: "ZLL-DimmableLight", manufacturerName: "Megaman\u0000"}],
         model: "LC201060",
         vendor: "Megaman",
         description: "LED Candle 6W 2700K (Dimmbar)",
