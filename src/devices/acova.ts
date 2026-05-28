1eec478864d14a886e6d617681acc363fbc78d53
DeadlockState <5185967+DeadlockState@users.noreply.github.com>
2026-05-28 20:56:34 +0200

    fix: Acova Alcantara 2 and Alcantara 3: fix system mode and off mode display (#12267)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/devices/acova.ts
+++ src/devices/acova.ts
@@ -1,11 +1,34 @@
 import * as fz from "../converters/fromZigbee";
 import * as tz from "../converters/toZigbee";
+import * as constants from "../lib/constants";
 import * as exposes from "../lib/exposes";
 import * as m from "../lib/modernExtend";
 import * as reporting from "../lib/reporting";
-import type {DefinitionWithExtend} from "../lib/types";
+import type {DefinitionWithExtend, Fz, KeyValueAny} from "../lib/types";
+import * as utils from "../lib/utils";
 
 const e = exposes.presets;
+
+const acova = {
+    fz: {
+        thermostat: {
+            ...fz.thermostat,
+            convert: (model, msg, publish, options, meta) => {
+                const result = fz.thermostat.convert(model, msg, publish, options, meta) as KeyValueAny;
+                if (result && msg.data.systemMode !== undefined) {
+                    result.system_mode = utils.getFromLookup(
+                        msg.data.systemMode,
+                        constants.acovaThermostatSystemModes as Record<string | number, string>,
+                    );
+                    if (result.system_mode === "off") {
+                        result.hvac_action = "off";
+                    }
+                }
+                return result;
+            },
+        } satisfies Fz.Converter<"hvacThermostat", undefined, ["attributeReport", "readResponse"]>,
+    },
+};
 
 export const definitions: DefinitionWithExtend[] = [
     {
@@ -16,10 +39,10 @@ export const definitions: DefinitionWithExtend[] = [
         model: "ALCANTARA2",
         vendor: "Acova",
         description: "Alcantara 2 heater",
-        fromZigbee: [fz.thermostat, fz.hvac_user_interface],
+        fromZigbee: [acova.fz.thermostat, fz.hvac_user_interface],
         toZigbee: [
             tz.thermostat_local_temperature,
-            tz.thermostat_system_mode,
+            tz.acova_thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_unoccupied_heating_setpoint,
             tz.thermostat_running_state,
@@ -47,10 +70,10 @@ export const definitions: DefinitionWithExtend[] = [
         model: "ALCANTARA3",
         vendor: "Acova",
         description: "Alcantara 3 heater",
-        fromZigbee: [fz.thermostat, fz.hvac_user_interface, fz.electrical_measurement],
+        fromZigbee: [acova.fz.thermostat, fz.hvac_user_interface, fz.electrical_measurement],
         toZigbee: [
             tz.thermostat_local_temperature,
-            tz.thermostat_system_mode,
+            tz.acova_thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_unoccupied_heating_setpoint,
             tz.thermostat_running_state,
