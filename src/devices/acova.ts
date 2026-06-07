0749526b28ecb181b2a7c6f03cd311e3b8f3ff2a
svhelge <79386304+svhelge@users.noreply.github.com>
2026-06-07 22:39:35 +0200

    Add acova thermostat system mode conversion methods

--- src/devices/acova.ts
+++ src/devices/acova.ts
@@ -29,6 +29,22 @@ const acova = {
             },
         } satisfies Fz.Converter<"hvacThermostat", undefined, ["attributeReport", "readResponse"]>,
     },
+    tz: {
+        acova_thermostat_system_mode: {
+            key: ["system_mode"],
+            convertSet: async (entity, key, value, meta) => {
+                let systemMode = utils.getKey(constants.acovaThermostatSystemModes, value, undefined, Number);
+                if (systemMode === undefined) {
+                    systemMode = utils.getKey(legacy.thermostatSystemModes, value, value as number, Number);
+                }
+                await entity.write("hvacThermostat", {systemMode});
+                return {state: {system_mode: value}};
+            },
+            convertGet: async (entity, key, meta) => {
+                await entity.read("hvacThermostat", ["systemMode"]);
+            },
+        } satisfies Tz.Converter,
+    },    
 };
 
 export const definitions: DefinitionWithExtend[] = [
@@ -43,7 +59,7 @@ export const definitions: DefinitionWithExtend[] = [
         fromZigbee: [acova.fz.thermostat, fz.hvac_user_interface],
         toZigbee: [
             tz.thermostat_local_temperature,
-            tz.acova_thermostat_system_mode,
+            acova.tz.acova_thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_unoccupied_heating_setpoint,
             tz.thermostat_running_state,
@@ -74,7 +90,7 @@ export const definitions: DefinitionWithExtend[] = [
         fromZigbee: [acova.fz.thermostat, fz.hvac_user_interface, fz.electrical_measurement],
         toZigbee: [
             tz.thermostat_local_temperature,
-            tz.acova_thermostat_system_mode,
+            acova.tz.acova_thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_unoccupied_heating_setpoint,
             tz.thermostat_running_state,
@@ -115,7 +131,7 @@ export const definitions: DefinitionWithExtend[] = [
         fromZigbee: [fz.thermostat, fz.hvac_user_interface, fz.occupancy],
         toZigbee: [
             tz.thermostat_local_temperature,
-            tz.acova_thermostat_system_mode,
+            acova.tz.acova_thermostat_system_mode,
             tz.thermostat_occupied_heating_setpoint,
             tz.thermostat_unoccupied_heating_setpoint,
             tz.thermostat_running_state,
