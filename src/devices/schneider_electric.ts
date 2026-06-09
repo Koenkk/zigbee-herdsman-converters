c79bee904baa1f43f7cb4fc5102e01917f222bd7
Nerivec <62446222+Nerivec@users.noreply.github.com>
2026-06-09 20:23:44 +0200

    fix: use coord address when `gppNwkAddr` not present in GP frame for `tempMaster` (#12395)

--- src/devices/schneider_electric.ts
+++ src/devices/schneider_electric.ts
@@ -1,5 +1,6 @@
 import {Zcl} from "zigbee-herdsman";
 import type {PartialClusterOrRawAttributes} from "zigbee-herdsman/dist/controller/tstype";
+import {COORDINATOR_ADDRESS} from "zigbee-herdsman/dist/zspec";
 import type {GpdManufAttributeReport} from "zigbee-herdsman/dist/zspec/zcl/definition/tstype";
 import * as fz from "../converters/fromZigbee";
 import * as tz from "../converters/toZigbee";
@@ -1476,7 +1477,7 @@ const fzLocal = {
                     "response",
                     {
                         options: 0b000,
-                        tempMaster: msg.data.gppNwkAddr,
+                        tempMaster: msg.data.gppNwkAddr ?? COORDINATOR_ADDRESS,
                         tempMasterTx: networkParameters.channel - 11,
                         srcID: msg.data.srcID,
                         gpdCmd: 0xfe,
