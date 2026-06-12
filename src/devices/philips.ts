f03ebb84b20667283bfec33bf8b2ae1864aaaae0
Sjrd83 <61553813+Sjrd83@users.noreply.github.com>
2026-06-12 15:30:38 +0200

    Update philips.ts

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -4351,7 +4351,7 @@ export const definitions: DefinitionWithExtend[] = [
         model: "929004611301",
         vendor: "Philips",
         description: "Hue Xamento White and Color Ambiance GU10 (white)",
-        extend: [philipsLight({colorTemp: {range: [153, 500]}, color: {modes: ["xy", "hs"], enhancedHue: true}})],
+        extend: [philips.m.light({colorTemp: {range: [153, 500]}, color: {modes: ["xy", "hs"], enhancedHue: true}})],
     },
     {
         zigbeeModel: ["929003074701"],
