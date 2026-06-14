aed08c6b059187cb84e245edc021d051ecd75b5b
Fenix <f.fenix@gmail.com>
2026-06-14 11:42:48 +0300

    migrate most devices to modernExtend

--- src/devices/msh.ts
+++ src/devices/msh.ts
@@ -2,7 +2,6 @@ import * as fz from "../converters/fromZigbee";
 import * as ptvo from "../devices/custom_devices_diy";
 import * as exposes from "../lib/exposes";
 import * as m from "../lib/modernExtend";
-import * as reporting from "../lib/reporting";
 import type {DefinitionWithExtend, Tz} from "../lib/types";
 
 const e = exposes.presets;
@@ -62,90 +61,92 @@ export const definitions: DefinitionWithExtend[] = [
         model: "msh.ina226",
         vendor: "MySmartHouse",
         description: "MSH 9-26V, 5A DC Power Meter",
-        fromZigbee: [fz.electrical_measurement, ptvo.fzLocal.ptvo_switch_analog_input],
-        toZigbee: [ptvo.tzLocal.ptvo_switch_analog_input],
-        exposes: [
-            e.current().withEndpoint("l2"),
-            e.voltage().withEndpoint("l2"),
-            e.power().withEndpoint("l2"),
-            exposes.numeric("l4", ea.STATE).withLabel("Uptime").withDescription("Uptime (seconds)").withUnit("s"),
+        extend:[
+            m.deviceEndpoints({endpoints: {l1: 1, l2: 2}}),
+            m.electricityMeter({
+                endpointNames: ["l2"],
+                electricalMeasurementType: "dc",
+                energy: false,
+            }),
+            m.numeric({
+                name: "l4",
+                attribute: "presentValue",
+                cluster: "genAnalogInput",
+                endpointNames: ["l4"],
+                access: "STATE",
+                label: "Uptime",
+                description: "Uptime (seconds)",
+                unit: "s",
+            }),
         ],
-        meta: {multiEndpoint: true},
-        endpoint: (device) => ({l2: 2, l4: 4, l5: 5}),
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            const binds = ["genBasic", "haElectricalMeasurement", "seMetering"];
-            await reporting.bind(endpoint, coordinatorEndpoint, binds);
-            await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
-            await reporting.rmsVoltage(endpoint);
-            await reporting.rmsCurrent(endpoint);
-            await reporting.activePower(endpoint);
-        },
     },
     {
         zigbeeModel: ["msh.ina226m"],
         model: "msh.ina226m",
         vendor: "MySmartHouse",
         description: "MSH 9-26V, 5A DC Power Meter, with CPU temperature support",
-        fromZigbee: [fz.electrical_measurement, fz.temperature, ptvo.fzLocal.ptvo_switch_analog_input],
-        toZigbee: [ptvo.tzLocal.ptvo_switch_analog_input],
-        exposes: [
-            e.current().withEndpoint("l2"),
-            e.voltage().withEndpoint("l2"),
-            e.power().withEndpoint("l2"),
-            e.cpu_temperature().withProperty("temperature").withEndpoint("l5"),
-            exposes.numeric("l4", ea.STATE).withLabel("Uptime").withDescription("Uptime (seconds)").withUnit("s"),
+        extend:[
+            m.deviceEndpoints({endpoints: {l2: 2, l4: 4, l5: 5}}),
+            m.electricityMeter({
+                endpointNames: ["l2"],
+                electricalMeasurementType: "dc",
+                energy: false,
+            }),
+            m.temperature({
+                endpointNames: ["l5"],
+                access: "STATE",
+                label: "CPU Temperature",
+                description: "Temperature of the CPU",
+            }),
+            m.numeric({
+                name: "l4",
+                attribute: "presentValue",
+                cluster: "genAnalogInput",
+                endpointNames: ["l4"],
+                access: "STATE",
+                label: "Uptime",
+                description: "Uptime (seconds)",
+                unit: "s",
+            }),
         ],
-        meta: {multiEndpoint: true},
-        endpoint: (device) => ({l2: 2, l4: 4, l5: 5}),
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            const binds = ["genBasic", "haElectricalMeasurement", "seMetering", "msTemperatureMeasurement"];
-            await reporting.bind(endpoint, coordinatorEndpoint, binds);
-            await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
-            await reporting.rmsVoltage(endpoint);
-            await reporting.rmsCurrent(endpoint);
-            await reporting.activePower(endpoint);
-            await reporting.temperature(endpoint);
-        },
     },
     {
         zigbeeModel: ["msh.pzem"],
         model: "msh.pzem",
         vendor: "MySmartHouse",
-        description: "MSH 100А AC DIN Power Meter",
-        fromZigbee: [fz.temperature, ptvo.fzLocal.ptvo_switch_analog_input],
-        toZigbee: [ptvo.tzLocal.ptvo_switch_analog_input],
-        exposes: [
-            e.current().withEndpoint("l2"),
-            e.voltage().withEndpoint("l2"),
-            e.energy().withUnit("Wh").withEndpoint("l2"),
-            e.power().withEndpoint("l2"),
-            e.ac_frequency().withEndpoint("l2"),
-            e.power_factor().withEndpoint("l2"),
-            e.cpu_temperature().withProperty("temperature").withEndpoint("l1"),
-            exposes.numeric("l3", ea.STATE).withLabel("Uptime").withDescription("Uptime (seconds)").withUnit("s"),
+        description: "MSH 100A AC DIN Power Meter",
+        extend:[
+            m.deviceEndpoints({endpoints: {l1: 1, l2: 2, l3: 3}}),
+            m.electricityMeter({
+                endpointNames: ["l2"],
+                electricalMeasurementType: "ac",
+                energy: {divisor: 1000, multiplier: 1},
+                acFrequency: true,
+                powerFactor: true,
+            }),
+            m.temperature({
+                endpointNames: ["l1"],
+                access: "STATE",
+                label: "CPU Temperature",
+                description: "Temperature of the CPU",
+            }),
+            m.numeric({
+                name: "l3",
+                attribute: "presentValue",
+                cluster: "genAnalogInput",
+                endpointNames: ["l3"],
+                access: "STATE",
+                label: "Uptime",
+                description: "Uptime (seconds)",
+                unit: "s",
+            }),
         ],
-        meta: {multiEndpoint: true},
-        endpoint: (device) => ({l1: 1, l2: 2, l3: 3}),
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            const binds = ["genBasic", "haElectricalMeasurement", "seMetering", "msTemperatureMeasurement"];
-            await reporting.bind(endpoint, coordinatorEndpoint, binds);
-            await reporting.readEletricalMeasurementMultiplierDivisors(endpoint);
-            await reporting.rmsVoltage(endpoint);
-            await reporting.rmsCurrent(endpoint);
-            await reporting.activePower(endpoint);
-            await reporting.acFrequency(endpoint);
-            await reporting.powerFactor(endpoint);
-            await reporting.temperature(endpoint);
-        },
     },
     {
         zigbeeModel: ["msh.pzem.dc"],
         model: "msh.pzem.dc",
         vendor: "MySmartHouse",
-        description: "MSH 9‐30V, 50-300A DC Power Meter",
+        description: "MSH 9-30V, 50-300A DC Power Meter",
         fromZigbee: [ptvo.fzLocal.ptvo_switch_analog_input, ptvo.fzLocal.ptvo_switch_uart],
         toZigbee: [ptvo.tzLocal.ptvo_switch_analog_input, ptvo.tzLocal.ptvo_switch_uart],
         exposes: [
