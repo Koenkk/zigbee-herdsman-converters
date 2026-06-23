5c76af286958acf5c89c18f91135af4831f3e205
zyjsmile857 <332618664@qq.com>
2026-06-23 15:59:23 +0800

    Add AOYAN AY02SZ white label

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -22705,6 +22705,14 @@ export const definitions: DefinitionWithExtend[] = [
         model: "ZG-102ZM",
         vendor: "HOBEIAN",
         description: "Vibration sensor",
+        whiteLabel: [
+            {
+                model: "AY02SZ",
+                vendor: "AOYAN",
+                description: "Vibration sensor",
+                fingerprint: [{modelID: "AY02SZ", manufacturerName: "AOYAN"}],
+            },
+        ],
         extend: [tuya.modernExtend.tuyaBase({dp: true})],
         exposes: [
             e.vibration(),
