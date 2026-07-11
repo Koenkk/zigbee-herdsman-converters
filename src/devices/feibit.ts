86ec8651f239953f344bfedb1c5417123ce8fab1
svhelge <79386304+svhelge@users.noreply.github.com>
2026-07-11 11:51:05 +0200

    extend existing converter

--- src/devices/feibit.ts
+++ src/devices/feibit.ts
@@ -79,7 +79,7 @@ export const definitions: DefinitionWithExtend[] = [
         exposes: [e.smoke(), e.battery_low(), e.tamper(), e.battery()],
     },
     {
-        zigbeeModel: ["FNB56-COS06FB1.7", "FNB56-COS06FB2.1"],
+        zigbeeModel: ["FNB56-COS06FB1.7", "FNB56-COS06FB2.1", "FB56-COS02HM1.4"],
         model: "SCA01ZB",
         vendor: "Feibit",
         description: "Smart carbon monoxide sensor",
@@ -297,15 +297,5 @@ export const definitions: DefinitionWithExtend[] = [
         vendor: "Feibit",
         description: "Power plug",
         extend: [m.onOff({powerOnBehavior: false})],
-    },
-    {
-        zigbeeModel: ["FB56-COS02HM1.4"],
-        model: "FB56-COS02HM1.4",
-        vendor: "Feibit",
-        description: "Carbon monoxide sensor",
-        extend: [
-            m.battery({voltageToPercentage: {min: 2500, max: 3000}, voltage: true}),
-            m.iasZoneAlarm({zoneType: "carbon_monoxide", zoneAttributes: ["alarm_1", "battery_low", "tamper"]}),
-        ],
     },
 ];
