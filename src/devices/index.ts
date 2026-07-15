fac18ff114d75bf76819ac99f7451906c1487e5b
DmitryMolokanov <142439388+DmitryMolokanov@users.noreply.github.com>
2026-07-15 09:10:38 +0300

    Add import for pulsar definitions

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -253,6 +253,7 @@ import {definitions as plaid} from "./plaid";
 import {definitions as plugwise} from "./plugwise";
 import {definitions as profalux} from "./profalux";
 import {definitions as prolight} from "./prolight";
+import {definitions as pulsar} from "./pulsar";
 import {definitions as purmo} from "./purmo";
 import {definitions as pushok} from "./pushok";
 import {definitions as qa} from "./qa";
