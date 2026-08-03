423abda1a6c94ac8a24d2bfc8b44b02b7d7a3dea
Henry_Codes <124015980+henrique981@users.noreply.github.com>
2026-08-02 23:13:16 -0300

    Create fortezza.js
    Add support for Fortezza FZE01BK smart door lock
    
    - Device: Fortezza FZE01BK
    - Model: TY0A01
    - Manufacturer: _TYST12_udaoihrc
    - Features: Battery monitoring, lock state detection (read-only)

---
+++ src/devices/fortezza.js
@@ -0,0 +1,23 @@
+const fz = require('../converters/fromZigbee');
+const exposes = require('../lib/exposes');
+const tuya = require('../lib/tuya');
+const e = exposes.presets;
+
+const definition = {
+    fingerprint: [{
+        modelID: 'TY0A01',
+        manufacturerName: '_TYST12_udaoihrc',
+    }],
+    model: 'Fortezza-FZE01BK',
+    vendor: 'Fortezza',
+    description: 'Smart Door Lock',
+    onEvent: tuya.onEventSetTime,
+    fromZigbee: [fz.battery],
+    toZigbee: [],
+    exposes: [e.battery(), e.lock()],
+    meta: {
+        tuyaDatapoints: [],
+    },
+};
+
+module.exports = definition;
