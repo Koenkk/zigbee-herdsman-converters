58b50bb9b45b5d35ebc46074e1aa791a273d6e6d
Henry_Codes <124015980+henrique981@users.noreply.github.com>
2026-08-03 00:12:19 -0300

    Fix fortezza.js

--- src/devices/fortezza.js
+++ src/devices/fortezza.js
@@ -1,18 +1,16 @@
-const fz = require("../converters/fromZigbee");
-const exposes = require("../lib/exposes");
-const tuya = require("../lib/tuya");
+const fz = require('../converters/fromZigbee');
+const exposes = require('../lib/exposes');
+const tuya = require('../lib/tuya');
 const e = exposes.presets;
 
 const definition = {
-    fingerprint: [
-        {
-            modelID: "TY0A01",
-            manufacturerName: "_TYST12_udaoihrc",
-        },
-    ],
-    model: "Fortezza-FZE01BK",
-    vendor: "Fortezza",
-    description: "Smart Door Lock",
+    fingerprint: [{
+        modelID: 'TY0A01',
+        manufacturerName: '_TYST12_udaoihrc',
+    }],
+    model: 'Fortezza-FZE01BK',
+    vendor: 'Fortezza',
+    description: 'Smart Door Lock',
     onEvent: tuya.onEventSetTime,
     fromZigbee: [fz.battery],
     toZigbee: [],
