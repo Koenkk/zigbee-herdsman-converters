e6bde891f2c3318b39839258800abce72c69dc4c
Igor Clemencov <clemencov@gmail.com>
2026-08-08 22:22:28 +0300

    fix: Home Assistant: set contact name to `null` to avoid duplicate door (#12850)

--- src/lib/exposes.ts
+++ src/lib/exposes.ts
@@ -21,6 +21,7 @@ export interface HomeAssistant {
     deviceClass?: string;
     enabledByDefault?: boolean;
     icon?: string;
+    name?: string | null;
     valueTemplate?: string | null;
 }
 
@@ -1024,7 +1025,10 @@ export const presets = {
                 "Indicates whether a plug is physically attached. Device does not have to pull power or even be connected electrically (state of this binary switch can be ON even if main power switch is OFF)",
             )
             .withCategory("diagnostic"),
-    contact: () => new Binary("contact", access.STATE, false, true).withDescription("Indicates if the contact is closed (= true) or open (= false)"),
+    contact: () =>
+        new Binary("contact", access.STATE, false, true)
+            .withDescription("Indicates if the contact is closed (= true) or open (= false)")
+            .withHomeAssistant({name: null}), // Prevents HA from appending the device class to the device name (e.g. "Garage Door Door")
     cover_position: () => new Cover().withPosition(),
     cover_position_tilt: () => new Cover().withPosition().withTilt(),
     cover_tilt: () => new Cover().withTilt(),
