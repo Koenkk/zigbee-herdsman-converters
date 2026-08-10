a13da3eeb32381c73e21b80d058ecccb9059ddaf
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2026-08-10 20:34:39 +0200

    feat: Ubisys J1: Add `operational_status` converter (#12872)

--- src/devices/ubisys.ts
+++ src/devices/ubisys.ts
@@ -109,6 +109,20 @@ const ubisys = {
                 return {configure_device_setup: result};
             },
         } satisfies Fz.Converter<"manuSpecificUbisysDeviceSetup", UbisysDeviceSetup, ["attributeReport", "readResponse"]>,
+        operational_status: {
+            cluster: "closuresWindowCovering",
+            type: ["attributeReport", "readResponse"],
+            convert: (model, msg, publish, options, meta) => {
+                if (msg.data.operationalStatus !== undefined) {
+                    const status = msg.data.operationalStatus;
+                    const operationalStatus = status & 3;
+                    const operationalStatusValues = {0: "stopped", 1: "opening", 2: "closing"};
+                    return {
+                        operational_status: utils.getFromLookup(operationalStatus, operationalStatusValues),
+                    };
+                }
+            },
+        } satisfies Fz.Converter<"closuresWindowCovering", UbisysClosuresWindowCovering, ["attributeReport", "readResponse"]>,
     },
     tz: {
         configure_j1: {
@@ -1011,7 +1025,7 @@ export const definitions: DefinitionWithExtend[] = [
         model: "J1",
         vendor: "Ubisys",
         description: "Shutter control J1",
-        fromZigbee: [fz.cover_position_tilt, fz.metering, ubisys.fz.configure_device_setup],
+        fromZigbee: [fz.cover_position_tilt, fz.metering, ubisys.fz.configure_device_setup, ubisys.fz.operational_status],
         toZigbee: [
             tz.cover_state,
             tz.cover_position_tilt,
@@ -1051,6 +1065,7 @@ export const definitions: DefinitionWithExtend[] = [
             ubisysModernExtend.addCustomClusterManuSpecificUbisysDeviceSetup(),
             ubisysModernExtend.addCustomClusterClosuresWindowCovering(),
             ubisysModernExtend.pollCurrentSummDelivered(3),
+            ubisysModernExtend.operationalStatus(),
         ],
         configure: async (device, coordinatorEndpoint) => {
             const endpoint1 = device.getEndpoint(1);
@@ -1060,6 +1075,8 @@ export const definitions: DefinitionWithExtend[] = [
             await reporting.instantaneousDemand(endpoint3);
             await reporting.bind(endpoint1, coordinatorEndpoint, ["closuresWindowCovering"]);
             await reporting.currentPositionLiftPercentage(endpoint1);
+            const payload = reporting.payload<"closuresWindowCovering", UbisysClosuresWindowCovering>("operationalStatus", 0, 65000, 0);
+            await endpoint1.configureReporting("closuresWindowCovering", payload);
         },
         onEvent: (event) => {
             /*
