de4e5a7921bcca665a7274a6d6677f8908fa9a75
Cheyne <47581324+goldstar19@users.noreply.github.com>
2026-08-10 18:29:40 -0500

    Update philips.ts
    I think I have it fixed, please reach out to me and I can provide a better export if this isn't correct, its from the installed new device.

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -5168,7 +5168,7 @@ export const definitions: DefinitionWithExtend[] = [
         extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
     },
     {
-        zigbeeModel: ["929004291501"],
+        zigbeeModel: ['LCD018'],
         model: "929004291501",
         vendor: "Signify Netherlands B.V.",
         description: "Hue white and color ambiance slim downlight 4 inch",
