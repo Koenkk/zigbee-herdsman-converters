8c62ea05553926dd0a66ba87625c2349358e4cd1
Nikolay Minaylov <nm29719@gmail.com>
2026-08-12 15:13:36 +0300

    Better model name

--- src/devices/custom_devices_diy.ts
+++ src/devices/custom_devices_diy.ts
@@ -1671,7 +1671,7 @@ export const definitions: DefinitionWithExtend[] = [
     },
     {
         zigbeeModel: ["LIGHT-CCT-TRK", "LIGHT-CCT-STR", "LIGHT-CCT-CUST"],
-        model: "CCT LED Light controller",
+        model: "DIY-LIGHT-CCT",
         vendor: "Custom devices (DiY)",
         description: "CCT LED lamp/dimmer with custom firmware",
         ota: true,
