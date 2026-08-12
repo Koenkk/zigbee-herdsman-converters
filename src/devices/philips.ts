786763680dcdd79ce1c517a10473f2f2b234e48f
Cheyne <47581324+goldstar19@users.noreply.github.com>
2026-08-12 14:16:25 -0500

    feat(add): 929004291501 (#12882)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/philips.ts
+++ src/devices/philips.ts
@@ -5167,4 +5167,11 @@ export const definitions: DefinitionWithExtend[] = [
         description: "Runner single spotlight (White)",
         extend: [philips.m.light({colorTemp: {range: [153, 454]}})],
     },
+    {
+        zigbeeModel: ["LCD018"],
+        model: "929004291501",
+        vendor: "Philips",
+        description: "Hue white and color ambiance slim downlight 4 inch",
+        extend: [philips.m.light({colorTemp: {range: [50, 1000]}, color: {modes: ["xy", "hs"], enhancedHue: true}})],
+    },
 ];
