7bbb234685f9b4ffa61c286ed8ce1d0d6085d4fd
support <117250319+3reality-support@users.noreply.github.com>
2026-08-15 02:49:59 +0800

    fix: Third Reality 3RCB01057Z: fix color temp range (#12909)

--- src/devices/third_reality.ts
+++ src/devices/third_reality.ts
@@ -1602,7 +1602,7 @@ export const definitions: DefinitionWithExtend[] = [
         ],
         ota: true,
         extend: [
-            m.light({colorTemp: {range: [154, 500]}, color: {modes: ["xy", "hs"], enhancedHue: false}}),
+            m.light({colorTemp: {range: [154, 454]}, color: {modes: ["xy", "hs"], enhancedHue: false}}),
             m.deviceAddCustomCluster("3rColorLightSpecialCluster", {
                 name: "3rColorLightSpecialCluster",
                 ID: 0xff04,
