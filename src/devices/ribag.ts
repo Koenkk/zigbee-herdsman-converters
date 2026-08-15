f3b63339c2b379745113e2da6024d26b75c0ea7c
Rüdiger Murach <17394572+rmurach@users.noreply.github.com>
2026-08-15 17:03:54 +0200

    Refactor Ribag device definition to use array format.

--- src/devices/ribag.ts
+++ src/devices/ribag.ts
@@ -1,10 +1,12 @@
 import * as m from "zigbee-herdsman-converters/lib/modernExtend";
 
-export default {
-    zigbeeModel: ["Ribag Air O"],
-    model: "Ribag Air O",
-    vendor: "RIBAG Licht",
-    description: "Ribag Vertico Air Pendant Light",
-    extend: [m.light({colorTemp: {range: [153, 500]}})],
-    meta: {omitOptionalLevelAndColorParams: true},
-};
+export const definitions: DefinitionWithExtend[] = [
+    {
+        zigbeeModel: ["Ribag Air O"],
+        model: "Ribag Air O",
+        vendor: "RIBAG Licht",
+        description: "Ribag Vertico Air Pendant Light",
+        extend: [m.light({colorTemp: {range: [153, 500]}})],
+        meta: {omitOptionalLevelAndColorParams: true},
+    },
+];
