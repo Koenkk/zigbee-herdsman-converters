b67421447c7d4a4ff18f756a8040cfa943974c38
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2026-08-20 05:29:50 +0200

    Convert SMSZB-120 & Co. to modernExtend

--- src/devices/develco.ts
+++ src/devices/develco.ts
@@ -830,13 +830,12 @@ export const definitions: DefinitionWithExtend[] = [
             {vendor: "Frient", model: "94430", description: "Smart Intelligent Smoke Alarm"},
             {vendor: "Cavius", model: "2103", description: "RF SMOKE ALARM, 5 YEAR 65MM"},
         ],
-        fromZigbee: [develco.fz.ias_smoke_alarm_1_develco, fz.ias_enroll, fz.ias_wd, develco.fz.fault_status],
-        toZigbee: [tz.warning, tz.ias_max_duration, tz.warning_simple],
         ota: true,
         extend: [
             develcoModernExtend.addCustomClusterManuSpecificDevelcoGenBasic(),
             develcoModernExtend.readGenBasicPrimaryVersions(),
-            develcoModernExtend.temperature(), // TODO: ep 38
+            develcoModernExtend.faultStatus(),
+            develcoModernExtend.temperature({endpointNames: ["38"]}),
             m.battery({
                 voltageToPercentage: {min: 2500, max: 3000},
                 percentage: true,
@@ -845,17 +844,22 @@ export const definitions: DefinitionWithExtend[] = [
                 voltageReporting: true,
                 percentageReporting: false,
             }),
+            m.iasZoneAlarm({
+                zoneType: "smoke",
+                zoneAttributes: ["alarm_1", "battery_low", "supervision_reports", "restore_reports", "test"],
+                zoneStatusReporting: true,
+            }),
+            m.iasWarning({reversePayload: true, maxDuration: {min: 0, max: 600}}),
         ],
         configure: async (device, coordinatorEndpoint) => {
             const endpoint = device.getEndpoint(35);
 
             // Device supports only 4 binds (otherwise you get TABLE_FULL error)
             // https://github.com/Koenkk/zigbee2mqtt/issues/23684
-            if (endpoint.binds.some((b) => b.cluster.name === "genPollCtrl")) {
-                await endpoint.unbind("genPollCtrl", coordinatorEndpoint);
-            }
+            //
+            // Bindings of non-reportable clusters have been removed.
 
-            await reporting.bind(endpoint, coordinatorEndpoint, ["ssIasZone", "ssIasWd", "genBinaryInput"]);
+            await reporting.bind(endpoint, coordinatorEndpoint, ["genBinaryInput"]);
             await endpoint.read("ssIasZone", ["iasCieAddr", "zoneState", "zoneId"]);
             await endpoint.read("genBinaryInput", ["reliability", "statusFlags"]);
             await endpoint.read("ssIasWd", ["maxDuration"]);
@@ -863,17 +867,6 @@ export const definitions: DefinitionWithExtend[] = [
         endpoint: (device) => {
             return {default: 35};
         },
-        exposes: [
-            e.smoke(),
-            e.battery_low(),
-            e.test(),
-            e.numeric("max_duration", ea.ALL).withUnit("s").withValueMin(0).withValueMax(600).withDescription("Duration of Siren"),
-            e.binary("alarm", ea.SET, "START", "OFF").withDescription("Manual Start of Siren"),
-            e
-                .enum("reliability", ea.STATE, ["no_fault_detected", "unreliable_other", "process_error"])
-                .withDescription("Indicates reason if any fault"),
-            e.binary("fault", ea.STATE, true, false).withDescription("Indicates whether the device are in fault state"),
-        ],
     },
     {
         zigbeeModel: ["SPLZB-141"],
@@ -907,8 +900,6 @@ export const definitions: DefinitionWithExtend[] = [
         vendor: "Develco",
         description: "Fire detector with siren",
         whiteLabel: [{vendor: "Frient", model: "94431", description: "Smart Intelligent Heat Alarm"}],
-        fromZigbee: [],
-        toZigbee: [],
         ota: true,
         extend: [
             develcoModernExtend.addCustomClusterManuSpecificDevelcoGenBasic(),
@@ -938,7 +929,9 @@ export const definitions: DefinitionWithExtend[] = [
             //
             // Bindings of non-reportable clusters have been removed.
 
+            await reporting.bind(endpoint, coordinatorEndpoint, ["genBinaryInput"]);
             await endpoint.read("ssIasZone", ["iasCieAddr", "zoneState", "zoneId"]);
+            await endpoint.read("genBinaryInput", ["reliability", "statusFlags"]);
             await endpoint.read("ssIasWd", ["maxDuration"]);
         },
         endpoint: (device) => {
