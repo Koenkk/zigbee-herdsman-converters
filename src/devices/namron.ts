59702b527b3d661c46b82b997853c874b0050a53
Koen Kanters <koenkanters94@gmail.com>
2026-08-28 18:21:40 +0000

    u

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -2402,15 +2402,10 @@ export const definitions: DefinitionWithExtend[] = [
         model: "4512763",
         vendor: "Namron",
         description: "Zigbee movement sensor",
-        fromZigbee: [fz.ias_occupancy_alarm_1, fz.battery],
+        fromZigbee: [fz.ias_occupancy_alarm_1],
         toZigbee: [],
-        exposes: [e.occupancy(), e.tamper(), e.battery_low(), e.battery(), e.battery_voltage()],
-        configure: async (device, coordinatorEndpoint) => {
-            const endpoint = device.getEndpoint(1);
-            await reporting.bind(endpoint, coordinatorEndpoint, ["genPowerCfg"]);
-            await reporting.batteryPercentageRemaining(endpoint);
-            await reporting.batteryVoltage(endpoint);
-        },
+        exposes: [e.occupancy(), e.tamper()],
+        extend: [m.battery({voltage: true, lowStatus: true})],
     },
     {
         zigbeeModel: ["4512764"],
