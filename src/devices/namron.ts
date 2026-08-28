e5c0f9c52c6cbc2eec18da7a9c586e2feadb20fd
Marius <50834194+mariusvollen@users.noreply.github.com>
2026-08-28 20:20:08 +0200

    fix: Namron 4512792: fix power and current scaling (#12984)
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- src/devices/namron.ts
+++ src/devices/namron.ts
@@ -2697,12 +2697,13 @@ export const definitions: DefinitionWithExtend[] = [
         model: "4512792",
         vendor: "Namron",
         description: "Simplify 1-2p relay (Zigbee / BT)",
+        version: "0.0.1",
         extend: [
             m.onOff(),
             m.electricityMeter({
-                power: {multiplier: 1, divisor: 10}, // W
+                power: {multiplier: 1, divisor: 1}, // W
                 voltage: {multiplier: 1, divisor: 10}, // V -> 2383 -> 238.3
-                current: {multiplier: 1, divisor: 100}, // A
+                current: {multiplier: 1, divisor: 1000}, // mA -> 4700 -> 4.7
                 energy: {multiplier: 1, divisor: 100}, // kWh
             }),
         ],
