ff0a8bf46326e39be77655df92ea5086b1f8cf04
Nico <142723292+nicolasglg@users.noreply.github.com>
2026-09-01 19:42:25 +0200

    feat(add): HPZERAD-V1 (#13050)
    Co-authored-by: Heiwa Contributor <noreply@users.noreply.github.com>
    Co-authored-by: hermes-agent <hermes-agent@users.noreply.github.com>

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -125,6 +125,7 @@ import {definitions as handshakeFinland} from "./handshake_finland";
 import {definitions as heatit} from "./heatit";
 import {definitions as heiman} from "./heiman";
 import {definitions as heimgardTechnologies} from "./heimgard_technologies";
+import {definitions as heiwa} from "./heiwa";
 import {definitions as hej} from "./hej";
 import {definitions as hfh} from "./hfh";
 import {definitions as hilux} from "./hilux";
@@ -513,6 +514,7 @@ const definitions: DefinitionWithExtend[] = [
     ...heatit,
     ...heiman,
     ...heimgardTechnologies,
+    ...heiwa,
     ...hej,
     ...hfh,
     ...hilux,
