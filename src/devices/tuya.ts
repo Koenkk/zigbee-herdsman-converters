b80569c78e0da8d52633d03baf2e5627c96e6996
isaackennylee <isaackenny@gmail.com>
2026-09-02 13:11:23 +0800

    feat: add ECODO 12W Sunset Smart Light as white label of TS0502B

--- src/devices/tuya.ts
+++ src/devices/tuya.ts
@@ -7214,6 +7214,7 @@ export const definitions: DefinitionWithExtend[] = [
             tuya.whitelabel("EcoDim", "ED-10032", "Zigbee LED filament lamp dimmable E27, bulb A60, Smokey 2000K-4000K", ["_TZ3210_09hzmirw"]),
             tuya.whitelabel("Mercator Ikuü", "SMCL01-ZB", "Ikon ceiling light", ["_TZ3000_6dwfra5l"]),
             tuya.whitelabel("LUUMR", "10024773", "Smart LED C35 matt E14 4,2 W", ["_TZ3210_claeh5ds"]),
+            tuya.whitelabel("ECODO", "12W Sunset Smart Light", "Sunset smart downlight 12 W, 1800-5700K", ["_TZ3210_rnj5wxxg"]),
         ],
         extend: [
             tuya.modernExtend.tuyaLight({
