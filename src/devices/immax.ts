fa8e3f1e0c4e8951ae3dae899f877851f02d9f47
Michal Kočárek <762095+michal-kocarek@users.noreply.github.com>
2026-09-09 13:43:23 +0200

    Update src/devices/immax.ts
    Co-authored-by: Andy <andrei_lazarov@yahoo.com>

--- src/devices/immax.ts
+++ src/devices/immax.ts
@@ -451,7 +451,7 @@ export const definitions: DefinitionWithExtend[] = [
             e.water_leak(),
             e.battery(),
             e
-                .binary("alarm_sound", ea.STATE_SET, "ON", "OFF")
+                .binary("silent_mode", ea.STATE_SET, "ON", "OFF")
                 .withDescription("Enable or disable buzzer sound on leak detection (does not mute ongoing alarm)")
                 .withCategory("config"),
             e
