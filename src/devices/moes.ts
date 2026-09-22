b4562e71e94280726d37bd8f02cb6983a8371cb8
Badr Mahfoud <badr@domadoo.com>
2026-09-22 11:46:04 +0200

    Update moes.ts

--- src/devices/moes.ts
+++ src/devices/moes.ts
@@ -1503,7 +1503,7 @@ export const definitions: DefinitionWithExtend[] = [
         },
     },
     {
-        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_hr0tdd47", "_TZE200_rjxqso4a", "_TZE284_rjxqso4a"]),
+        fingerprint: tuya.fingerprint("TS0601", ["_TZE200_hr0tdd47", "_TZE200_rjxqso4a", "_TZE284_rjxqso4a", "JM720ES-EF-3.0"]),
         model: "ZC-HM",
         vendor: "Moes",
         description: "Carbon monoxide alarm",
@@ -1518,7 +1518,7 @@ export const definitions: DefinitionWithExtend[] = [
                 [16, "silence", tuya.valueConverter.raw],
             ],
         },
-        whiteLabel: [tuya.whitelabel("Heiman", "HS-720ES", "Carbon monoxide alarm", ["_TZE200_hr0tdd47"])],
+        whiteLabel: [tuya.whitelabel("Heiman", "HS-720ES", "Carbon monoxide alarm", ["_TZE200_hr0tdd47,"])],
     },
     {
         fingerprint: tuya.fingerprint("TS0601", ["_TZE200_vawy74yh", "_TZE204_vawy74yh", "_TZE284_vawy74yh"]),
